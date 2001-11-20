363c7979b74d7c1f7f8c7ca94b8c723d4c2c30f3
(no author) <(no author)@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2001-11-20 03:48:48 +0000

    This commit was manufactured by cvs2svn to create branch 'yb'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/yb@37 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/essential_functions.php
+++
@@ -1,9 +0,0 @@
-<?
-function getDBconnection () {
-	$server="127.0.0.1" ;
-	$user="manske" ;
-	$passwd="test" ;
-	$connection=mysql_connect ( $server , $user , $passwd ) ;
-	return $connection ;
-	}
-?>
