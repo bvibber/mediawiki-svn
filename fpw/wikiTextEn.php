6777362f9a94d4cb0a053c7f18341f1d3e02ca62
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-04 19:37:23 +0000

    Remove /fpw/ from paths, use SERVER_NAME in wikiArticleSource
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@64 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/wikiTextEn.php
+++ fpw/wikiTextEn.php
@@ -3,12 +3,13 @@
 #---------------------------------------------------------------
 
 # Essential stuff, so the script knows where it is, whar database to use, etc.
-$wikiCurrentServer = "http://127.0.0.1" ;
+#$wikiCurrentServer = "http://127.0.0.1" ;
+$wikiCurrentServer = "http://" . getenv("SERVER_NAME");
 $wikiSQLServer = "wikipedia" ; # The name of the database, actually...
 $wikiArticleSource = "$wikiCurrentServer/wiki/$1" ;
-$wikiLogoFile = "/fpw/wiki.png" ;
-$wikiStarTrekImage = "/fpw/startrek.png" ;
-$THESCRIPT = "/fpw/wiki.phtml" ; # The name and location of the script. The $PHP_SELF variable doesn't work with Netscape
+$wikiLogoFile = "/wiki.png" ;
+$wikiStarTrekImage = "/startrek.png" ;
+$THESCRIPT = "/wiki.phtml" ; # The name and location of the script. The $PHP_SELF variable doesn't work with Netscape
 $wikiCharset = "iso-8859-1" ;
 
 # Misc stuff
