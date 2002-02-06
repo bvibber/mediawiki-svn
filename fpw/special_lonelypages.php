58726b7a6e314ea4c9e830dd689af6125e2f5c2e
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-06 21:06:43 +0000

    *** empty log message ***
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@74 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ fpw/special_lonelypages.php
@@ -0,0 +1,39 @@
+<?
+function LonelyPages () {
+	global $THESCRIPT ;
+	global $linkedLinks , $unlinkedLinks , $vpage ;
+	global $wikiLonelyPagesTitle , $wikiLonelyPagesText ;
+	$vpage->special ( $wikiLonelyPagesTitle ) ;
+	$vpage->namespace = "" ;
+	$allPages = array () ;
+	$ret = $wikiLonelyPagesText ;
+
+	global $wikiSQLServer ;
+	$connection = getDBconnection () ;
+	mysql_select_db ( $wikiSQLServer , $connection ) ;
+	$sql = "SELECT cur_title,cur_linked_links,cur_unlinked_links FROM cur WHERE cur_title NOT LIKE \"Talk:%\" AND cur_text NOT LIKE \"#redirect%\" AND cur_text != \"\"" ;
+	$result = mysql_query ( $sql , $connection ) ;
+	while ( $s = mysql_fetch_object ( $result ) ) {
+		$allPages[ucfirst($s->cur_title)] = $allPages[ucfirst($s->cur_title)] * 1 ;
+		$u = explode ( "\n" , $s->cur_linked_links ) ; foreach ( $u as $x ) $allPages[ucfirst($x)] += 1 ;
+		$u = explode ( "\n" , $s->cur_unlinked_links ) ; foreach ( $u as $x ) $allPages[ucfirst($x)] += 1 ;
+		}
+	if ( $result != false ) mysql_free_result ( $result ) ;
+	#mysql_close ( $connection ) ;
+
+	asort ( $allPages ) ;
+#	$allPages = array_slice ( $allPages , 0 , 50 ) ;
+
+	$orphans = array () ;
+	$v = array_keys ( $allPages ) ;
+	foreach ( $v as $x ) {
+		if ( $allPages[$x] == 0 )
+			array_push ( $orphans , $x ) ;
+		}
+
+	asort ( $orphans ) ;
+	foreach ( $orphans as $x )
+		$ret .= "# [[$x|".$vpage->getNiceTitle($x)."]]<br>\n" ;
+	return $ret ;
+	}
+?>
