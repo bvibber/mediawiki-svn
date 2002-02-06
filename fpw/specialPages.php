fb8389aeb966435530091a1403bc05fa0a229b38
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-06 20:17:18 +0000

    special_wantedpages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@73 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/specialPages.php
+++ fpw/specialPages.php
@@ -19,6 +19,8 @@
 # - askSQL (for sysops only!)
 # and many others...
 
+include_once ( "special_wantedpages.php" ) ;
+
 function userLogout () {
 	global $user , $vpage ;
 	$vpage->title = "User logout" ;
@@ -274,93 +276,6 @@ function editUserSettings () {
 	$ret .= "<td><center><input type=reset value=\"$wikiReset\" name=ButtonReset></center></td></tr>" ;
 
 	$ret .= "</table></FORM>$msg</nowiki>" ;
-	return $ret ;
-	}
-
-function refreshWantedPages () {
-	global $showNumberPages , $linkedLinks , $unlinkedLinks , $vpage , $wikiWantedText , $wikiWantedLine ;
-	global $wikiWantedToggleNumbers ;
-	$vpage->special ( "The Most Wanted Pages" ) ;
-	$vpage->namespace = "" ;
-	if ( $showNumberPages == "" ) $showNumberPages = "off" ;
-	if ( $showNumberPages == "off" ) $nsnp = "on" ;
-	else $nsnp = "off" ;
-	$allPages = array () ;
-	$ret = $wikiWantedText ;
-#	$ret .= "<nowiki><a href=\"".wikiLink("special:WantedPages?showNumberPages=$nsnp")."\">";
-#	$ret .= str_replace("$1",$showNumberPages,$wikiWantedToggleNumbers)."</a></nowiki><br>\n" ;
-
-	global $wikiSQLServer ;
-	$connection = getDBconnection () ;
-	mysql_select_db ( $wikiSQLServer , $connection ) ;
-	$sql = "SELECT cur_title,cur_linked_links,cur_unlinked_links FROM cur" ;
-	$result = mysql_query ( $sql , $connection ) ;
-	while ( $s = mysql_fetch_object ( $result ) ) {
-		$allPages[ucfirst($s->cur_title)] = -999999999999 ; # Effectively removing existing topics from list
-		$fc = substr ( $s->cur_title , 0 , 1 ) ;
-		if ( $showNumberPages == "on" OR $fc < "0" OR $fc > "9" ) {
-			$u = explode ( "\n" , $s->cur_unlinked_links ) ;
-			$v = array () ;
-			foreach ( $u as $x ) {
-				$w = ucfirst ( $x ) ;
-				if ( $v[$w] != true ) # Count only one link per page
-					{
-					$allPages[$w] += 1 ;
-					$v[$w] = true ;
-					}
-				}
-			unset ( $v ) ;
-			}
-		}
-	mysql_free_result ( $result ) ;
-	#mysql_close ( $connection ) ;
-
-	arsort ( $allPages ) ;
-	$somePages = array_slice ( $allPages , 0 , 400 ) ; # Reducing needed memory
-	unset ( $allPages ) ;
-	$allPages = $somePages ;
-	unset ( $somePages ) ;
-
-	$ti = new wikiTitle ;
-	$k = array_keys ( $allPages ) ;
-
-	$a = 0 ;
-	$o = array () ;
-	while ( count ( $o ) < 50 ) {
-		$x = $k[$a] ;
-		$a++ ;
-		$ti->setTitle ( $x ) ;
-		if ( $x != "" and !$ti->doesTopicExist() ) {
-			$n = str_replace ( "$1" , "[[$x|".$ti->getNiceTitle($x)."]]" , $wikiWantedLine ) ;
-			$n = str_replace ( "$2" , $allPages[$x] , $n ) ;
-			$n = str_replace ( "$3" , wikiLink("special:whatlinkshere&target=".nurlencode($x)) , $n ) ;
-			$n = str_replace ( "$4" , $ti->getNiceTitle($x) , $n ) ;
-			array_push ( $o , "*$n\n" ) ;
-			}
-		}
-	$ret .= implode ( "" , $o ) ;
-
-	return $ret ;
-	}
-
-function WantedPages () {
-	global $doRefresh , $wikiRefreshThisPage , $wikiResourcesWarning ;
-	$pn = "Log:Most_Wanted" ;
-	$ret = "<nowiki>" ;
-
-	$ret .= "<p align=center><font size='+1'><b><a href=\"" ;
-	$ret .= wikiLink ( "special:WantedPages&doRefresh=yes" ) ;
-	$ret .= "\">$wikiRefreshThisPage</a></b></font><br>$wikiResourcesWarning</p></nowiki>\n" ;
-	if ( $doRefresh == "yes" ) {
-		$o = refreshWantedPages () ;
-		$ret .= $o ;
-		$p = new wikiPage ;
-		$p->setTitle ( $pn ) ;
-		$p->ensureExistence () ;
-		$p->setEntry ( $o , "Refresh" , 0 , "System" , 1 , ",cur_timestamp=cur_timestamp" ) ; # Storing, don't show on RC
-	} else {
-		$ret .= getMySQL ( "cur" , "cur_text" , "cur_title=\"$pn\"" ) ;
-		}
 	return $ret ;
 	}
 
