e92ed00f3bcd901281be7edad97f666471acee52
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-06 22:02:08 +0000

    *** empty log message ***
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@78 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phpwiki/fpw/special_contributions.php
@@ -0,0 +1,49 @@
+<?
+# This function list the contributions of a user
+function contributions () {
+	global $THESCRIPT , $target , $user , $protecting , $newrestrictions ;
+	global $vpage , $theuser , $wikiSQLServer ;
+	global $wikiContribTitle , $wikiContribText , $wikiContribDenied ;
+	$vpage = new WikiPage ;
+	$vpage->title = $title ;
+	$vpage->makeSecureTitle () ;
+	$ti = $vpage->secureTitle ;
+	$vpage->special ( str_replace ( "$1" , $theuser , $wikiContribTitle ) ) ;
+	$vpage->makeSecureTitle () ;
+	if ( $theuser == "" ) return "<nowiki><h1>$wikiContribDenied</h1></nowiki>" ;
+	$theuser = str_replace ( "_" , " " , $theuser ) ;
+	$ret = "<nowiki>".str_replace("$1",$theuser,$wikiContribText)."</nowiki>\n" ;
+
+	$ac = array () ;
+	$connection = getDBconnection () ;
+	mysql_select_db ( $wikiSQLServer , $connection ) ;
+
+	$question = "SELECT cur_title FROM cur WHERE cur_user_text=\"$theuser\" AND cur_minor_edit<>1" ;
+	$result = mysql_query ( $question , $connection ) ;
+	while ( $s = mysql_fetch_object ( $result ) ) array_push ( $ac , $s->cur_title ) ;
+	mysql_free_result ( $result ) ;
+
+	$question = "SELECT old_title FROM old WHERE old_user_text=\"$theuser\" AND old_minor_edit<>1" ;
+	$result = mysql_query ( $question , $connection ) ;
+	while ( $s = mysql_fetch_object ( $result ) )
+		if ( !in_array ( $s->cur_title , $ac ) )
+			array_push ( $ac , $s->cur_title ) ;
+	mysql_free_result ( $result ) ;
+	#mysql_close ( $connection ) ;
+
+	if ( count ( $ac ) == 0 AND $theuser == ucfirst ( $theuser ) ) { # Rerun with lowercase name
+		$theuser = strtolower(substr($theuser,0,1)).substr($theuser,1) ;
+		return contributions() ;
+		}
+
+
+	asort ( $ac ) ;
+	foreach ( $ac as $x ) {
+		$b = spliti ( "talk:" , $x ) ;
+		if ( $x != "" and substr ( $x , 0 , 4 ) != "Log:" and count ( $b ) == 1 )
+			$ret .= "* [[".$vpage->getNiceTitle($x)."]]\n" ;
+		}
+
+	return $ret ;
+	}
+?>
