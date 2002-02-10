b1829026210ee39ef6e8db60f4c6f269a805e8a4
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-10 20:51:03 +0000

    new implementation of Recent Changes / History pages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@112 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/special_dohistory.php
+++ fpw/special_dohistory.php
@@ -2,55 +2,55 @@
 include_once ( "special_recentchangeslayout.php" ) ;
 
 function doHistory ( $title ) {
-	global $THESCRIPT , $vpage , $wikiSQLServer , $wikiHistoryTitle , $wikiCurrentVersion , $wikiHistoryHeader ;
-	$vpage = new WikiPage ;
-	$vpage->SetTitle ( $title ) ;
-	$ti = $vpage->secureTitle ;
-	$url = $vpage->url;
-	$vpage->special ( str_replace ( "$1" , $title , $wikiHistoryTitle ) ) ;
-	$vpage->makeSecureTitle () ;
+    global $THESCRIPT , $vpage , $wikiSQLServer , $wikiHistoryTitle , $wikiCurrentVersion , $wikiHistoryHeader ;
+    $vpage = new WikiPage ;
+    $vpage->SetTitle ( $title ) ;
+    $ti = $vpage->secureTitle ;
+    $url = $vpage->url;
+    $vpage->special ( str_replace ( "$1" , $title , $wikiHistoryTitle ) ) ;
+    $vpage->makeSecureTitle () ;
 
-	$a = array () ;
-	$connection = getDBconnection () ;
-	mysql_select_db ( $wikiSQLServer , $connection ) ;
-	$sql = "SELECT * FROM cur WHERE cur_title=\"$ti\"" ;
-	$result = mysql_query ( $sql , $connection ) ;
-	$s = mysql_fetch_object ( $result ) ;
-	array_push ( $a , $s ) ;
-	mysql_free_result ( $result ) ;
-	$o = $s->cur_old_version ;
-	while ( $o != 0 ) {
-		$sql = "SELECT * FROM old WHERE old_id=$o" ;
-		$result = mysql_query ( $sql , $connection ) ;
-		$s = mysql_fetch_object ( $result ) ;
-#		print "<font color=red>$s->old_timestamp:</font> ".$s->old_text."<br>" ;
-		$s->cur_timestamp = $s->old_timestamp ;
-		$s->cur_title = $s->old_title ;
-		$s->cur_user = $s->old_user ;
-		$s->cur_comment = $s->old_comment ;
-		$s->cur_user_text = $s->old_user_text ;
-		$s->cur_minor_edit = $s->old_minor_edit ;
-		array_push ( $a , $s ) ;
-		$o = $s->old_old_version ;
-		mysql_free_result ( $result ) ;
-		}
-	#mysql_close ( $connection ) ;
+    $a = array () ;
+    $connection = getDBconnection () ;
+    mysql_select_db ( $wikiSQLServer , $connection ) ;
+    
+    # first we get the current record
+    $sql1 = "SELECT cur_timestamp, cur_title, cur_comment, cur_user,
+                     cur_user_text, cur_minor_edit
+             FROM cur
+             WHERE cur_title=\"$ti\"" ;
+    $result = mysql_query ( $sql1 , $connection ) ;
+    $s = mysql_fetch_object ( $result ) ;
+    array_push ( $a , $s ) ;
+    mysql_free_result ( $result ) ;
+    
+    # then we get the old records
+    $sql2 = "SELECT old_timestamp AS cur_timestamp, old_title AS cur_title, old_comment AS cur_comment,
+                    old_user AS cur_user, old_user_text AS cur_user_text, old_minor_edit AS cur_minor_edit,
+                    old_id
+             FROM old
+             WHERE old_title=\"$ti\"
+             ORDER BY cur_timestamp DESC" ;
+    $result = mysql_query ( $sql2 , $connection ) ;
+    while ( $s = mysql_fetch_object ( $result ) ) array_push ( $a , $s ) ;
+    mysql_free_result ( $result ) ;
 
-	$i = count ( $a ) ;
-	$k = array_keys ( $a ) ;
-	foreach ( $k as $x ) {
-		if ( $i != count ( $a ) ) $a[$x]->version = $i ;
-		else $a[$x]->version = $wikiCurrentVersion ;
-		$i-- ;
-		}
+    # now we number the versions
+    $version = 0;
+    foreach ( array_keys ( $a ) as $i ) {
+      if ( $version > 0 ) $a[$i]->version = $version;
+      $version++;
+    }
 
-	$t = recentChangesLayout ( $a ) ;
-	$t = "<b>".str_replace(array("$1","$2"),array($url,$title),$wikiHistoryHeader)."</b>".$t ;
+    #mysql_close ( $connection ) ;
 
-	$ret = $vpage->getHeader() ;
-	$ret .= $vpage->getMiddle($t) ;
-	$ret .= $vpage->getFooter() ;
-	return $ret ;
-	}
+    $t = recentChangesLayout ( $a ) ;
+    $t = "<b>".str_replace(array("$1","$2"),array($url,$title),$wikiHistoryHeader)."</b>".$t ;
+
+    $ret = $vpage->getHeader() ;
+    $ret .= $vpage->getMiddle($t) ;
+    $ret .= $vpage->getFooter() ;
+    return $ret ;
+    }
 
 ?>
