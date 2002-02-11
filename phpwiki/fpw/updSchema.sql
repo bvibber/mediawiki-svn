2e31d0073a2f302f35f9f3085fb5569808fc800a
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-11 15:30:22 +0000

    add removal of superfluous indexes, and creation of index on old_title
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@121 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/updSchema.sql
+++ phpwiki/fpw/updSchema.sql
@@ -2,5 +2,17 @@
 # It adapts the table definitions adds indexes and, if necessary,
 # fills new columns with appropriate values.
 
+# adding the indexes on the timestamps
+
 ALTER TABLE cur ADD INDEX timeind (cur_timestamp);
 ALTER TABLE old ADD INDEX timeind (old_timestamp);
+
+# adding index on old_title
+
+ALTER TABLE old MODIFY old_title VARCHAR(255) BINARY NOT NULL DEFAULT '';
+ALTER TABLE old ADD INDEX old_title (old_title);
+
+# dropping the superfluous indexes on cur_id and old_id
+
+ALTER TABLE cur DROP key cur_id_2;
+ALTER TABLE old DROP key old_id_2;
