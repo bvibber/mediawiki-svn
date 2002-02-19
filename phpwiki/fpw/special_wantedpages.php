90e8e0ecd41494281e80d7aa3a510824b1452927
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-19 21:02:14 +0000

    the new special wanted page based on SQL
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@171 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_wantedpages.php
+++ phpwiki/fpw/special_wantedpages.php
@@ -1,102 +1,39 @@
 <?
-function refreshWantedPages () {
-	global $showNumberPages , $linkedLinks , $unlinkedLinks , $vpage , $wikiWantedText , $wikiWantedLine ;
-	global $wikiWantedToggleNumbers, $wikiLastRefreshed , $wikiGetDate ;
-	$vpage->special ( "The Most Wanted Pages" ) ;
-	$vpage->namespace = "" ;
-	if ( $showNumberPages == "" ) $showNumberPages = "off" ;
-	if ( $showNumberPages == "off" ) $nsnp = "on" ;
-	else $nsnp = "off" ;
-	$allPages = array () ;
-	$ret = $wikiWantedText ;
-#	$ret .= "<nowiki><a href=\"".wikiLink("special:WantedPages?showNumberPages=$nsnp")."\">";
-#	$ret .= str_replace("$1",$showNumberPages,$wikiWantedToggleNumbers)."</a></nowiki><br>\n" ;
-
-	$connection = getDBconnection () ;
-	$sql = "SELECT cur_title,cur_linked_links,cur_unlinked_links FROM cur" ;
-	$result = mysql_query ( $sql , $connection ) ;
-	while ( $s = mysql_fetch_object ( $result ) ) {
-		$allPages[ucfirst($s->cur_title)] = -999999999999 ; # Effectively removing existing topics from list
-		$fc = substr ( $s->cur_title , 0 , 1 ) ;
-		if ( $showNumberPages == "on" OR $fc < "0" OR $fc > "9" ) {
-			$u = explode ( "\n" , $s->cur_unlinked_links ) ;
-			$v = array () ;
-			foreach ( $u as $x ) {
-				$w = ucfirst ( $x ) ;
-				if ( $v[$w] != true ) # Count only one link per page
-					{
-					$allPages[$w] += 1 ;
-					$v[$w] = true ;
-					}
-				}
-			unset ( $v ) ;
-			}
-		}
-	mysql_free_result ( $result ) ;
-
-	arsort ( $allPages ) ;
-	$somePages = array_slice ( $allPages , 0 , 400 ) ; # Reducing needed memory
-	unset ( $allPages ) ;
-	$allPages = $somePages ;
-	unset ( $somePages ) ;
-
-	$ti = new wikiTitle ;
-	$k = array_keys ( $allPages ) ;
-
-	$a = 0 ;
-	$o = array () ;
-	while ( count ( $o ) < 50 ) {
-		$x = $k[$a] ;
-		$a++ ;
-		$ti->setTitle ( $x ) ;
-		if ( $x != "" and !$ti->doesTopicExist() ) {
-			$n = str_replace ( "$1" , "[[$x|".$ti->getNiceTitle($x)."]]" , $wikiWantedLine ) ;
-			$n = str_replace ( "$2" , $allPages[$x] , $n ) ;
-			$n = str_replace ( "$3" , wikiLink("special:whatlinkshere&target=".nurlencode($x)) , $n ) ;
-			$n = str_replace ( "$4" , $ti->getNiceTitle($x) , $n ) ;
-			array_push ( $o , "*$n\n" ) ;
-			}
-		}
-	$ret .= implode ( "" , $o ) ;
-	
-	$now = time () ;
-	$lc = $wikiGetDate ( $now ) . date ( ", H:i" , $now ) ;
-	#$lc .= ", ".substr ( $now , 8 , 2 ) ;
-	#$lc .= ":".substr ( $now , 10 , 2 ) ;
-	$ret .= "\n<p>" . str_replace ( "$1", $lc, $wikiLastRefreshed ) . "</p>\n" ;
-
-	return $ret ;
-	}
 
 function WantedPages () {
-	global $doRefresh , $wikiRefreshThisPage , $wikiResourcesWarning , $wikiNoRefresh ;
-	$pn = "Log:Most_Wanted" ;
-	$minRefreshPeriod = 5*60; # Refuse to update more often than every 5 minutes
-	$timeOffset = 10000000; # Drop our date back a few months to keep us out of RecentChanges
-	$ret = "<nowiki>" ;
+    global $vpage , $wikiWantedText , $wikiWantedLine ;
+    global $wikiGetDate ;
 
-	$ret .= "<p align=center><font size='+1'><b><a href=\"" ;
-	$ret .= wikiLink ( "special:WantedPages&doRefresh=yes" ) ;
-	$ret .= "\">$wikiRefreshThisPage</a></b></font><br>$wikiResourcesWarning</p></nowiki>\n" ;
-	if ( $doRefresh == "yes" ) {
-		$timestamp = getMySQL ( "cur" , "UNIX_TIMESTAMP(cur_timestamp)" , "cur_title=\"$pn\"" ) + $timeOffset ;
-		$lastrefreshed = time() - $timestamp ;
-		if ( $lastrefreshed < 0 ) $lastrefreshed = $minRefreshPeriod ;
-		if ( $lastrefreshed < $minRefreshPeriod ) {
-			$ret .= "<p><i>" . str_replace ( array ( "$1", "$2" ) ,
-				array ( round ( $lastrefreshed / 60 ) , round ( ( $minRefreshPeriod - $lastrefreshed ) / 60 ) ),
-				$wikiNoRefresh ) . "</i></p>\n\n";
-		} else {
-			$o = refreshWantedPages () ;
-			$ret .= $o ;
-			$p = new wikiPage ;
-			$p->setTitle ( $pn ) ;
-			$p->ensureExistence () ;
-			$p->setEntry ( $o , "Refresh" , 0 , "System" , 1 , ",cur_timestamp=FROM_UNIXTIME(" . (time() - $timeOffset) . ")" ) ;
-			return $ret;
-			}
-		}
-	$ret .= getMySQL ( "cur" , "cur_text" , "cur_title=\"$pn\"" ) ;
-	return $ret ;
-	}
+    $vpage->special ( "The Most Wanted Pages" ) ;
+    $vpage->namespace = "" ;
+
+    $ret = $wikiWantedText ;
+
+    $connection = getDBconnection () ;
+    $sql = "SELECT unlinked_to, COUNT( unlinked_from ) AS ulf
+            FROM unlinked
+            GROUP BY unlinked_to
+            ORDER BY ulf DESC
+            LIMIT 50 " ;
+
+    $result = mysql_query ( $sql , $connection ) ;
+    
+    $ti = new wikiTitle ;
+    while ( $s = mysql_fetch_object ( $result ) ) {
+        $n = str_replace ( "$1" , "[[$s->unlinked_to|".$ti->getNiceTitle($s->unlinked_to)."]]" , $wikiWantedLine ) ;
+        $n = str_replace ( "$2" , $s->ulf , $n ) ;
+        $n = str_replace ( "$3" , wikiLink("special:whatlinkshere&target=".nurlencode($s->unlinked_to)) , $n ) ;
+        $n = str_replace ( "$4" , $ti->getNiceTitle($s->unlinked_to) , $n ) ;
+        $ret .= "*$n\n" ;
+    }                
+            
+    $now = time () ;
+    $lc = $wikiGetDate ( $now ) . date ( ", H:i" , $now ) ;
+    #$lc .= ", ".substr ( $now , 8 , 2 ) ;
+    #$lc .= ":".substr ( $now , 10 , 2 ) ;
+    $ret .= "\n<p>" . str_replace ( "$1", $lc, $wikiLastRefreshed ) . "</p>\n" ;
+
+    return $ret ;
+}
+
 ?>
