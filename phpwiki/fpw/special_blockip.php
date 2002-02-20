021012ad13618c2a1d5f2ba5dabbdf78df78b227
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-20 21:05:08 +0000

    blocking IPs, part 2 of 3
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@180 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phpwiki/fpw/special_blockip.php
@@ -0,0 +1,19 @@
+<?
+include_once ( "special_makelog.php" ) ;
+
+function blockIP () {
+	 global $target , $vpage , $wikiBlockIPTitle ,
+	 $wikiBlockIPText , $user ;
+	 $vpage->special ( $wikiBlockIPTitle ) ;
+
+	global $REMODE_ADDR ;
+#	$now = date ( "Y-m-d H:i:s" , time () ) ;
+#	$userText = "[[$wikiUser:$user->name|$user->name]]" ;
+	$logText = "$target (".time().")" ;
+	$line = str_replace ( array("$1","$2") ,
+	array($target,$user->name) , $wikiBlockIPText ) ;
+	makeLog ( "wikipedia:Blocked IPs" , "*$logText : $line\n" , $line ) ;
+
+	 return "<font size='+3' color=red>$line</font>" ;
+	 }
+?>
