0a6d7d0875c20d2b2b826fd87b233cd4b62bc483
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-23 22:17:15 +0000

    added some code to avoid errors when watched page no longer exists
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@191 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_watchlist.php
+++ phpwiki/fpw/special_watchlist.php
@@ -2,64 +2,77 @@
 include_once ( "special_recentchanges.php" ) ;
 
 function modifyArray ( $a , $sep , $rem , $add = "" ) {
-	$b = explode ( $sep , $a ) ;
-	$c = array () ;
-	foreach ( $b as $x ) {
-		if ( $x != "" and $x != $rem )
-			array_push ( $c , $x ) ;
-		}
-	if ( $add != "" ) array_push ( $c , $add ) ;
-	return implode ( $sep , $c ) ;
-	}
+    $b = explode ( $sep , $a ) ;
+    $c = array () ;
+    foreach ( $b as $x ) {
+        if ( $x != "" and $x != $rem )
+            array_push ( $c , $x ) ;
+        }
+    if ( $add != "" ) array_push ( $c , $add ) ;
+    return implode ( $sep , $c ) ;
+    }
 
 function watch ( $t , $m ) {
-	global $THESCRIPT , $user , $wikiUserSettingsError , $wikiWatchYes , $wikiWatchNo ;
-	if ( !$user->isLoggedIn ) return $wikiUserSettingsError ;
+    global $THESCRIPT , $user , $wikiUserSettingsError , $wikiWatchYes , $wikiWatchNo ;
+    global $vpage;
+    
+    if ( !$user->isLoggedIn ) return $wikiUserSettingsError ;
 
-	# Modifying user_watch
-	$separator = "\n" ;
-	$a = getMySQL ( "user" , "user_watch" , "user_id=$user->id" ) ;
-	if ( $m == "yes" ) $a = modifyArray ( $a , $separator , $t , $t ) ;
-	else $a = modifyArray ( $a , $separator , $t ) ;
-	setMySQL ( "user" , "user_watch" , $a , "user_id=$user->id" ) ;
+    # Modifying user_watch
+    $separator = "\n" ;
+    $a = getMySQL ( "user" , "user_watch" , "user_id=$user->id" ) ;
+    if ( $m == "yes" ) $a = modifyArray ( $a , $separator , $t , $t ) ;
+    else $a = modifyArray ( $a , $separator , $t ) ;
+    setMySQL ( "user" , "user_watch" , $a , "user_id=$user->id" ) ;
 
-	if ( $m == "yes" ) $ret = str_replace ( "$1" , $t , $wikiWatchYes ) ;
-	else str_replace ( "$1" , $t , $wikiWatchNo ) ;
-	$ret .= "<META HTTP-EQUIV=Refresh CONTENT=\"0; URL='".wikiLink(nurlencode($t))."'\">" ;
-	return $ret ;
-	}
+    if ( $m == "yes" ) $ret = str_replace ( "$1" , $t , $wikiWatchYes ) ;
+    else str_replace ( "$1" , $t , $wikiWatchNo ) ;
+    $ret .= "<META HTTP-EQUIV=Refresh CONTENT=\"0; URL='".wikiLink(nurlencode($t))."'\">" ;
+    return $ret ;
+    }
 
 function WatchList () {
-	global $THESCRIPT ;
-	global $vpage , $user , $wikiWatchlistTitle , $wikiWatchlistText ;
-	$vpage->special ( $wikiWatchlistTitle ) ;
-	$ret = $wikiWatchlistText ;
-	$a = getMySQL ( "user" , "user_watch" , "user_id=$user->id" ) ;
-	$separator = "\n" ;
-	$b = explode ( $separator , $a ) ;
-	$vpage->namespace = "" ;
+    global $THESCRIPT ;
+    global $vpage , $user , $wikiWatchlistTitle , $wikiWatchlistExistText , $wikiWatchlistNotExistText;
 
-	$n = array () ;
-	foreach ( $b as $x )
-		$n[$x] = getMySQL ( "cur" , "cur_timestamp" , "cur_title=\"$x\"" ) ;
-	arsort ( $n ) ;
-	$k = array_keys ( $n ) ;
+    $vpage->special ( $wikiWatchlistTitle ) ;
+    $ret = "$wikiWatchlistExistText\n\n" ;
+    $a = getMySQL ( "user" , "user_watch" , "user_id=$user->id" ) ;
+    $separator = "\n" ;
+    $b = explode ( $separator , $a ) ;
+    $vpage->namespace = "" ;
 
-	$connection=getDBconnection() ;
-	$arr = array () ;
-	$any = false ;
-	foreach ( $k as $x ) {
-		if ( $x != "" ) {
-			$sql = "SELECT * FROM cur WHERE cur_title=\"$x\"" ;
-			$result = mysql_query ( $sql , $connection ) ;
-			$s = mysql_fetch_object ( $result ) ;
-			array_push ( $arr , $s ) ;
-			mysql_free_result ( $result ) ;
-			$any = true ;
-			}
-		}
-	if ( $any ) $ret .= recentChangesLayout ( $arr ) ;
+    $n = array () ;
+    foreach ( $b as $x )
+        $n[$x] = getMySQL ( "cur" , "cur_timestamp" , "cur_title=\"$x\"" ) ;
+    arsort ( $n ) ;
+    $k = array_keys ( $n ) ;
 
-	return $ret ;
-	}
+    $connection=getDBconnection() ;
+    $arr = array () ;
+    $any = false ;
+    $notexist = "" ;
+    foreach ( $k as $x ) {
+        if ( $x != "" ) {
+            $sql = "SELECT cur_timestamp, cur_title, cur_comment, cur_user, cur_user_text, cur_minor_edit
+                    FROM cur
+                    WHERE cur_title=\"$x\"" ;
+            $result = mysql_query ( $sql , $connection ) ;
+            $s = mysql_fetch_object ( $result ) ;
+            if ( $s )
+                array_push ( $arr , $s ) ;    # don't push if page no longer exists
+            else
+                $notexist .= "\n* [[".$vpage->getNiceTitle( $x )."]]" ;
+            mysql_free_result ( $result ) ;
+            $any = true ;
+            }
+        }
+    if ( $any )
+        $ret .= recentChangesLayout ( $arr ) ;
+        
+    if ( $notexist )
+        $ret .= "$wikiWatchlistNotExistText\n\n$notexist" ;
+
+    return $ret ;
+    }
 ?>
