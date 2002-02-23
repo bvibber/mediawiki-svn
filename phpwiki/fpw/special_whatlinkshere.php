c8ebf4e291f224f6594fe5b2e5afb6ba59d48cc3
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-23 22:16:20 +0000

    SQL optimized
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@190 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_whatlinkshere.php
+++ phpwiki/fpw/special_whatlinkshere.php
@@ -1,63 +1,81 @@
 <?
 function whatLinksHere () {
-	global $THESCRIPT , $target , $user , $protecting , $newrestrictions ;
-	global $vpage , $target , $wikiLinkhereTitle ;
-	global $wikiLinkhereBacklink , $wikiLinkhereNoBacklink , $wikiBacklinkNolink , $wikiBacklinkFollowing ;
-	$vpage = new WikiPage ;
-	$vpage->title = $title ;
-	$vpage->makeSecureTitle () ;
-	$ti = $vpage->secureTitle ;
-	$niceTarget = $vpage->getNiceTitle ( $target ) ;
-	$vpage->special ( str_replace ( "$1" , $niceTarget , $wikiLinkhereTitle ) ) ;
-	$vpage->makeSecureTitle () ;
+    global $THESCRIPT , $target , $user , $protecting , $newrestrictions ;
+    global $vpage , $target , $wikiLinkhereTitle ;
+    global $wikiLinkhereBacklink , $wikiLinkhereNoBacklink , $wikiBacklinkNolink , $wikiBacklinkFollowing ;
+    
+    $vpage = new WikiPage ;
+    $vpage->title = $title ;
+    $vpage->makeSecureTitle () ;
+    $ti = $vpage->secureTitle ;
+    $niceTarget = $vpage->getNiceTitle ( $target ) ;
+    $vpage->special ( str_replace ( "$1" , $niceTarget , $wikiLinkhereTitle ) ) ;
+    $vpage->makeSecureTitle () ;
 
-	$connection = getDBconnection () ;
+    $connection = getDBconnection () ;
+    # first we get all pages linked to $target
+    $sql1 = "SELECT DISTINCT linked_from AS lfrom
+            FROM linked
+            WHERE linked_to = \"$target\"
+            ORDER BY lfrom " ;
+    $result1 = mysql_query ( $sql1 , $connection ) ;
 
-	# The question is kinda long, but I don't want to sort everything out manually, so...
-	$question = "SELECT cur_title FROM cur WHERE" ;
-	$question .= " cur_linked_links LIKE \"$target\" OR " ;
-	$question .= " cur_linked_links LIKE \"$target\n%\" OR" ;
-	$question .= " cur_linked_links LIKE \"%\n$target\n%\" OR" ;
-	$question .= " cur_linked_links LIKE \"%\n$target\" OR " ;
-	$question .= " cur_unlinked_links LIKE \"$target\" OR " ;
-	$question .= " cur_unlinked_links LIKE \"$target\n%\" OR" ;
-	$question .= " cur_unlinked_links LIKE \"%\n$target\n%\" OR" ;
-	$question .= " cur_unlinked_links LIKE \"%\n$target\"" ;
+    # we also get all pages that have an unlinked link to $target
+    # Note that at least one of these two queries is empty
+    $sql2 = "SELECT DISTINCT unlinked_from AS lfrom
+            FROM unlinked
+            WHERE unlinked_to = \"$target\"
+            ORDER BY lfrom " ;
+    $result2 = mysql_query ( $sql2 , $connection ) ;
+    
+    # then we get all pages linked from $target
+    $sql3 = "SELECT DISTINCT linked_to
+            FROM linked
+            WHERE linked_from = \"$target\"
+            ORDER BY linked_to " ;
+    $result3 = mysql_query ( $sql3 , $connection ) ;
+    
+    $dlb = "" ;     # the pages that $target links back to
+    $dnlb = "" ;    # the pages that $target does not link back to
+    
+    # we find out if $target has links
+    $s3 = mysql_fetch_object ( $result3 ) ;
+    if ( $s3 ) {    # yes, so we take $result1 and $result2 and split it into $dlb and $ndlb
+        foreach ( array ( $result1, $result2 ) as $aresult ) {
+            while ( $s1 =  mysql_fetch_object ( $aresult ) ) {
+                $nt = $vpage->getNiceTitle ( $s1->lfrom ) ;
+                while ( $s3 and $s3->linked_to < $s1->lfrom )
+                    $s3 = mysql_fetch_object ( $result3 ) ;
+                if ( $s3 )
+                    if ( $s3->linked_to > $s1->lfrom )
+                        $dnlb .= "\n* [[$nt]]" ;
+                    else
+                        $dlb .= "\n* [[$nt]]" ;
+                else
+                    $dnlb .= "\n* [[$nt]]" ;
+            }
+        }
+    } else {        # no, so we take $result1 and $result2 and put it in $dnlb
+        foreach ( array ( $result1, $result2 ) as $aresult ) {
+            while ( $s1 =  mysql_fetch_object ( $aresult ) ) {
+                $nt = $vpage->getNiceTitle ( $s1->lfrom ) ;
+                $dnlb .= "\n* [[$nt]]" ;
+            }
+            
+        }    
+    }
+    mysql_free_result ( $result1 ) ;
+    mysql_free_result ( $result2 ) ;
+    mysql_free_result ( $result3 ) ;
+    
+    if ( $dlb ) $dlb = "<h3>".str_replace("$1",$niceTarget,$wikiLinkhereBacklink)."</h3>$dlb\n" ;
+    if ( $dnlb ) $dnlb = "<h3>".str_replace("$1",$niceTarget,$wikiLinkhereNoBacklink)."</h3>$dnlb\n" ;
 
-	$result = mysql_query ( $question , $connection ) ;
-	$p = array () ;
-	if ( $result != "" ) {
-		while ( $s = mysql_fetch_object ( $result ) ) array_push ( $p , $s->cur_title ) ;
-		mysql_free_result ( $result ) ;
-		}
+    $ret = $dnlb.$dlb ;
+    if ( $ret == "" ) $ret = "<h1>".str_replace("$1",$niceTarget,$wikiBacklinkNolink)."</h1>" ;
+    else $ret = "<h1>".str_replace("$1",$niceTarget,$wikiBacklinkFollowing)."</h1>\n$ret" ;
 
-	$question = "SELECT cur_linked_links,cur_unlinked_links FROM cur WHERE cur_title=\"$target\"" ;
-	$result = mysql_query ( $question , $connection ) ;
-	$s = mysql_fetch_object ( $result ) ;
-	mysql_free_result ( $result ) ;
+    return $ret ;
+}
 
-	$out = explode ( "\n" , $s->cur_linked_links."\n".$s->cur_unlinked_links ) ;
-	$dlb = array () ;
-	$dnlb = array () ;
-
-	foreach ( $p as $x ) {
-		$y = $vpage->getNiceTitle ( $x ) ;
-		if ( in_array ( $x , $out ) ) array_push ( $dlb , $y ) ;
-		else array_push ( $dnlb , $y ) ;
-		}
-
-	asort ( $dlb ) ;
-	$dlb = implode ( "]]\n*[[" , $dlb ) ;
-	if ( $dlb != "" ) $dlb = "<h3>".str_replace("$1",$niceTarget,$wikiLinkhereBacklink)."</h3>\n*[[$dlb]]\n" ;
-
-	asort ( $dnlb ) ;
-	$dnlb = implode ( "]]\n*[[" , $dnlb ) ;
-	if ( $dnlb != "" ) $dnlb = "<h3>".str_replace("$1",$niceTarget,$wikiLinkhereNoBacklink)."</h3>\n*[[$dnlb]]\n" ;
-
-	$ret = $dnlb.$dlb ;
-	if ( $ret == "" ) $ret = "<h1>".str_replace("$1",$niceTarget,$wikiBacklinkNolink)."</h1>" ;
-	else $ret = "<h1>".str_replace("$1",$niceTarget,$wikiBacklinkFollowing)."</h1>\n$ret" ;
-
-	return $ret ;
-	}
 ?>
