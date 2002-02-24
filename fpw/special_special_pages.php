be3c531bfbd4f6a437944a60b2e33a8df76760a2
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-24 17:13:00 +0000

    extended special pages list
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@198 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/special_special_pages.php
+++ fpw/special_special_pages.php
@@ -4,7 +4,9 @@ function special_pages () {
 	$vpage->special ( $wikiSpecialTitle ) ;
 	$ret = $wikiSpecialText ;
 	$ret .= $vpage->getQuickBar () ;
-	$ret = "<nowiki>$ret</nowiki>" ;
+	$ret = "<nowiki>$ret</nowiki>\n----\n" ;
+	$ret .= "[[log:Uploads|Upload log]]<br>\n" ;
+	$ret .= "[[log:Page_Deletions|Deleted pages log]]<br>\n" ;
 	return $ret ;
 	}
 ?>
