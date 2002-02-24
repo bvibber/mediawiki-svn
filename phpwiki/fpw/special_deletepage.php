6a92dab8bbf00aef324c678e09042ae8411d5f51
hidders <hidders@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-24 15:57:29 +0000

    using tables (un)linked instead of the cur_(un)linked_links columns
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@197 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_deletepage.php
+++ phpwiki/fpw/special_deletepage.php
@@ -1,32 +1,6 @@
 <?
 include_once ( "special_makelog.php" ) ;
 
-function removeFromLinkList ( $item , $link ) {
-    $connection = getDBconnection () ;
-    $sql = "SELECT cur_id FROM cur WHERE $item LIKE \"%$link%\"" ;
-    $result = mysql_query ( $sql , $connection ) ;
-    $ids = array () ;
-    while ( $s = mysql_fetch_object ( $result ) ) array_push ( $ids , $s->cur_id ) ;
-    mysql_free_result ( $result ) ;
-
-    foreach ( $ids as $x ) {
-        $sql = "SELECT cur_timestamp,$item FROM cur WHERE cur_id=$x" ;
-        $result = mysql_query ( $sql , $connection ) ;
-        $s = mysql_fetch_object ( $result )  ;
-        mysql_free_result ( $result ) ;
-        $y = explode ( "\n" , $s->$item ) ;
-        $z = array () ;
-        foreach ( $y as $u ) {
-            if ( $u != $link )
-                array_push ( $z , $u ) ;
-            }
-        $y = implode ( "\n" , $z ) ;
-        $sql = "UPDATE cur SET cur_timestamp=\"$s->cur_timestamp\",$item=\"$y\" WHERE cur_id=$x" ;
-        $result = mysql_query ( $sql , $connection ) ;
-        }
-
-    }
-
 function deletepage () {
     global $THESCRIPT , $target , $user , $iamsure ;
     global $vpage , $wikiSQLServer ;
@@ -53,9 +27,6 @@ function deletepage () {
         $logText = str_replace("$1",$now,str_replace("$2",$user->name,str_replace("$3",$logTarget,$wikiDeleteMsg1))) ;
         makeLog ( "log:Page Deletions" , $logText , str_replace("$1",$logTarget,$wikiDeleteMsg2)) ;
 
-        removeFromLinkList ( "cur_linked_links" , $target ) ;
-        removeFromLinkList ( "cur_unlinked_links" , $target ) ; # !! this is strange, if a page is removed it shouldn't be in any unlinked_links array, but if it is then it might as well stay, right? JH
-        
         # links to $target are moved from table linked to table unlinked
         $sql = "INSERT INTO unlinked ( unlinked_from, unlinked_to )
                 SELECT linked_from, linked_to
