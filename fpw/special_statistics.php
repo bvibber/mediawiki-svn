6207e573c43ccf6c046a81d1f182dacaea319863
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-02-27 19:43:51 +0000

    statistics update
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@218 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/special_statistics.php
+++ fpw/special_statistics.php
@@ -3,7 +3,7 @@ function statistics () {
 	global $THESCRIPT , $vpage , $wikiStatisticsTitle , $wikiStatTotalPages , $wikiStatistics ;
 	global $wikiStatTalkPages , $wikiStatCommaPages , $wikiStatWikipediaNoTalk , $wikiStatSubNoTalk ,
 		$wikiStatNoTalk , $wikiStatArticles , $wikiStatJunk , $wikiStatOld , $wikiStatUsers , $wikiStatSysops ,
-		$wikiTalk , $wikiUserStatistics;
+		$wikiTalk , $wikiUserStatistics , $wikiStatRedirect , $wikiStatSkin , $wikiSkins ;
 	
 	$vpage->special ( $wikiStatistics ) ;
 	$vpage->namespace = "" ;
@@ -58,12 +58,21 @@ function statistics () {
 	$ret .= "<li>".str_replace ( "$1" , "$nf1$subPages$nf2" , $wikiStatSubNoTalk )."</li>" ;
 	mysql_free_result ( $result ) ;
 
+	# REDIRECT PAGES
+	$sql = "SELECT COUNT(*) as number FROM cur WHERE cur_text LIKE \"#REDIRECT%\"" ;
+	$result = mysql_query ( $sql , $connection ) ;
+	$s = mysql_fetch_object ( $result ) ;
+	$redirectPages = $s->number ;
+	$ret .= "<li>".str_replace ( "$1" , "$nf1$redirectPages$nf2" , $wikiStatRedirect )."</li>" ;
+	mysql_free_result ( $result ) ;
+
+
 	# RESULT
 	$x = $commaPages - $wikiPages ; # Comma (no /Talk) - wiki pages = articles, including subpages
 	$ret .= "<li>".str_replace ( "$1" , "$nf1$x$nf2" , $wikiStatNoTalk )."</li>" ;
 	$y = $x - $subPages ;
 	$ret .= "<li>".str_replace ( "$1" , "$nf1$y$nf2" , $wikiStatArticles )."</li>" ;
-	$z = $totalPages - $talkPages - $commaPages ;
+	$z = $totalPages - $talkPages - $commaPages - $redirectPages ;
 	$ret .= "<li>".str_replace ( "$1" , "$nf1$z$nf2" , $wikiStatJunk )."</li>" ;
 
 	# OLD PAGES
@@ -94,6 +103,17 @@ function statistics () {
 	$numEditors = $s->number ;
 	$ret .= "<li>".str_replace ( "$1" , "$nf1$numEditors$nf2" , $wikiStatSysops )."</li>" ;
 	mysql_free_result ( $result ) ;
+
+	foreach ( array_keys ( $wikiSkins ) AS $skin ) {
+		$iskin = $wikiSkins[$skin] ;
+		$u = urlencode ( "skin=$iskin" ) ;
+		$sql = "SELECT COUNT(*) as number FROM user WHERE user_options LIKE \"%$u%\"" ;
+		$result = mysql_query ( $sql , $connection ) ;
+		$s = mysql_fetch_object ( $result ) ;
+		$ret .= "<li>".str_replace ( array("$1","$2") , array($s->number,$skin) , $wikiStatSkin )."</li>\n" ;
+		mysql_free_result ( $result ) ;
+		}
+
 
 	$ret .= "</ul>" ;
 	return $ret ;
