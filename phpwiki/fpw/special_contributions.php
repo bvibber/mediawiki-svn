cade095b0b1d7af92e46c5b996dbc4bf17a5e66f
axelboldt <axelboldt@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-03-08 17:15:21 +0000

    List all pages that a user has made non-minor edits on. Reformulated as a single SQL query.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@235 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_contributions.php
+++ phpwiki/fpw/special_contributions.php
@@ -14,33 +14,19 @@ function contributions () {
 	$theuser = str_replace ( "_" , " " , $theuser ) ;
 	$ret = "<nowiki>".str_replace("$1",$theuser,$wikiContribText)."</nowiki>\n" ;
 
-	$ac = array () ;
 	$connection = getDBconnection () ;
 
-	$question = "SELECT cur_title FROM cur WHERE cur_user_text=\"$theuser\" AND cur_minor_edit<>1" ;
+	$question = "select DISTINCT cur_title
+                        FROM  cur LEFT OUTER JOIN old ON cur_title = old_title
+                        WHERE cur_title NOT REGEXP \"([tT]alk|^Log):\" 
+                          AND 
+                           ((cur_user_text = \"$theuser\" AND cur_minor_edit <> 1) 
+                               OR 
+                            (old_user_text = \"$theuser\" AND old_minor_edit <> 1))
+                        ORDER BY cur_title";
 	$result = mysql_query ( $question , $connection ) ;
-	while ( $s = mysql_fetch_object ( $result ) ) array_push ( $ac , $s->cur_title ) ;
-	mysql_free_result ( $result ) ;
-
-	$question = "SELECT old_title FROM old WHERE old_user_text=\"$theuser\" AND old_minor_edit<>1" ;
-	$result = mysql_query ( $question , $connection ) ;
-	while ( $s = mysql_fetch_object ( $result ) )
-		if ( !in_array ( $s->cur_title , $ac ) )
-			array_push ( $ac , $s->cur_title ) ;
-	mysql_free_result ( $result ) ;
-
-	if ( count ( $ac ) == 0 AND $theuser == ucfirst ( $theuser ) ) { # Rerun with lowercase name
-		$theuser = strtolower(substr($theuser,0,1)).substr($theuser,1) ;
-		return contributions() ;
-		}
-
-
-	asort ( $ac ) ;
-	foreach ( $ac as $x ) {
-		$b = spliti ( "talk:" , $x ) ;
-		if ( $x != "" and substr ( $x , 0 , 4 ) != "Log:" and count ( $b ) == 1 )
-			$ret .= "* [[".$vpage->getNiceTitle($x)."]]\n" ;
-		}
+	while ( $s = mysql_fetch_object ( $result ) ) 
+		$ret .= "* [[".$vpage->getNiceTitle($s->cur_title)."]]\n" ;
 
 	return $ret ;
 	}
