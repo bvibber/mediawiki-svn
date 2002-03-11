2263eba34f7e92e5328ed16320f9f60ba4012095
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-03-11 08:33:30 +0000

    Support for Polish in conversion script, messages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@241 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/convertWiki2SQL.php
+++ fpw/convertWiki2SQL.php
@@ -12,7 +12,7 @@ include ( "./wikiUser.php" ) ;
 
 ## Default/English:
 $wikiTalk = "Talk";
-$fieldSeparator = "³" ;
+$fieldSeparator = "\xb3" ;
 function recodeCharsetStub ( $text ) {
 	# Some languages may change the internal coding used in the database
 	return $text;
@@ -20,32 +20,58 @@ function recodeCharsetStub ( $text ) {
 	$recodeCharset = recodeCharsetStub ;
 
 ## Esperanto:
-#$wikiTalk = "Priparolu" ;
-#function recodeCharsetEo ( $text ) {
-#	  $x = array(
-#	  	"Cx", "cx",  "CX", "cX",
-#		"Gx", "gx",  "GX", "gX",
-#		"Hx", "hx",  "HX", "hX",
-#		"Jx", "jx",  "JX", "jX",
-#		"Sx", "sx",  "SX", "sX",
-#		"Ux", "ux",  "UX", "uX");
-#	  $u = array(
-#		"\xc4\x88", "\xc4\x89",  "\xc4\x88", "\xc4\x89",
-#		"\xc4\x9c", "\xc4\x9d",  "\xc4\x9c", "\xc4\x9d",
-#		"\xc4\xa4", "\xc4\xa5",  "\xc4\xa4", "\xc4\xa5",
-#		"\xc4\xb4", "\xc4\xb5",  "\xc4\xb4", "\xc4\xb5",
-#		"\xc5\x9c", "\xc5\x9d",  "\xc5\x9c", "\xc5\x9d",
-#		"\xc5\xac", "\xc5\xad",  "\xc5\xac", "\xc5\xad");
-#	return str_replace ( $x , $u , $text ) ;
-#	}
-#	$recodeCharset = recodeCharsetEo ;
+/*
+$wikiTalk = "Priparolu" ;
+function recodeCharsetEo ( $text ) {
+	  $x = array(
+	  	"Cx", "cx",  "CX", "cX",
+		"Gx", "gx",  "GX", "gX",
+		"Hx", "hx",  "HX", "hX",
+		"Jx", "jx",  "JX", "jX",
+		"Sx", "sx",  "SX", "sX",
+		"Ux", "ux",  "UX", "uX");
+	  $u = array(
+		"\xc4\x88", "\xc4\x89",  "\xc4\x88", "\xc4\x89",
+		"\xc4\x9c", "\xc4\x9d",  "\xc4\x9c", "\xc4\x9d",
+		"\xc4\xa4", "\xc4\xa5",  "\xc4\xa4", "\xc4\xa5",
+		"\xc4\xb4", "\xc4\xb5",  "\xc4\xb4", "\xc4\xb5",
+		"\xc5\x9c", "\xc5\x9d",  "\xc5\x9c", "\xc5\x9d",
+		"\xc5\xac", "\xc5\xad",  "\xc5\xac", "\xc5\xad");
+	return str_replace ( $x , $u , $text ) ;
+	}
+	$recodeCharset = recodeCharsetEo ;
+*/
 
 ## French
 #$wikiTalk = "Discuter" ;
 
 ## Polish:
-#$wikiTalk = "Dyskusja" ;
-#$fieldSeparator = "\xff";
+/*
+$wikiTalk = "Dyskusja" ;
+$fieldSeparator = "\xff";
+function RecodeCharsetPl ( $text ) {
+	# Convert iso8859-2 to UTF-8
+	# In a happy world, we could use iconv for this
+	$l2u8 = array ( "\xA0" => "\xc2\xa0", "\xA1" => "\xc4\x84", "\xA2" => "\xcb\x98", "\xA3" => "\xc5\x81", "\xA4" => "\xc2\xa4", "\xA5" => "\xc4\xbd",
+	"\xA6" => "\xc5\x9a", "\xA7" => "\xc2\xa7", "\xA8" => "\xc2\xa8", "\xA9" => "\xc5\xa0", "\xAA" => "\xc5\x9e", "\xAB" => "\xc5\xa4",
+	"\xAC" => "\xc5\xb9", "\xAD" => "\xc2\xad", "\xAE" => "\xc5\xbd", "\xAF" => "\xc5\xbb", "\xB0" => "\xc2\xb0", "\xB1" => "\xc4\x85",
+	"\xB2" => "\xcb\x9b", "\xB3" => "\xc5\x82", "\xB4" => "\xc2\xb4", "\xB5" => "\xc4\xbe", "\xB6" => "\xc5\x9b", "\xB7" => "\xcb\x87",
+	"\xB8" => "\xc2\xb8", "\xB9" => "\xc5\xa1", "\xBA" => "\xc5\x9f", "\xBB" => "\xc5\xa5", "\xBC" => "\xc5\xba", "\xBD" => "\xcb\x9d",
+	"\xBE" => "\xc5\xbe", "\xBF" => "\xc5\xbc", "\xC0" => "\xc5\x94", "\xC1" => "\xc3\x81", "\xC2" => "\xc3\x82", "\xC3" => "\xc4\x82", 
+	"\xC4" => "\xc3\x84", "\xC5" => "\xc4\xb9", "\xC6" => "\xc4\x86", "\xC7" => "\xc3\x87", "\xC8" => "\xc4\x8c", "\xC9" => "\xc3\x89", 
+	"\xCA" => "\xc4\x98", "\xCB" => "\xc3\x8b", "\xCC" => "\xc4\x9a", "\xCD" => "\xc3\x8d", "\xCE" => "\xc3\x8e", "\xCF" => "\xc4\x8e", 
+	"\xD0" => "\xc4\x90", "\xD1" => "\xc5\x83", "\xD2" => "\xc5\x87", "\xD3" => "\xc3\x93", "\xD4" => "\xc3\x94", "\xD5" => "\xc5\x90", 
+	"\xD6" => "\xc3\x96", "\xD7" => "\xc3\x97", "\xD8" => "\xc5\x98", "\xD9" => "\xc5\xae", "\xDA" => "\xc3\x9a", "\xDB" => "\xc5\xb0", 
+	"\xDC" => "\xc3\x9c", "\xDD" => "\xc3\x9d", "\xDE" => "\xc5\xa2", "\xDF" => "\xc3\x9f", "\xE0" => "\xc5\x95", "\xE1" => "\xc3\xa1", 
+	"\xE2" => "\xc3\xa2", "\xE3" => "\xc4\x83", "\xE4" => "\xc3\xa4", "\xE5" => "\xc4\xba", "\xE6" => "\xc4\x87", "\xE7" => "\xc3\xa7", 
+	"\xE8" => "\xc4\x8d", "\xE9" => "\xc3\xa9", "\xEA" => "\xc4\x99", "\xEB" => "\xc3\xab", "\xEC" => "\xc4\x9b", "\xED" => "\xc3\xad", 
+	"\xEE" => "\xc3\xae", "\xEF" => "\xc4\x8f", "\xF0" => "\xc4\x91", "\xF1" => "\xc5\x84", "\xF2" => "\xc5\x88", "\xF3" => "\xc3\xb3", 
+	"\xF4" => "\xc3\xb4", "\xF5" => "\xc5\x91", "\xF6" => "\xc3\xb6", "\xF7" => "\xc3\xb7", "\xF8" => "\xc5\x99", "\xF9" => "\xc5\xaf", 
+	"\xFA" => "\xc3\xba", "\xFB" => "\xc5\xb1", "\xFC" => "\xc3\xbc", "\xFD" => "\xc3\xbd", "\xFE" => "\xc5\xa3", "\xFF" => "\xcb\x99" ) ;
+	return strtr ( $text , $l2u8 ) ;
+	}
+	$recodeCharset = recodeCharsetPl ;
+*/
 
 ## Spanish
 #$wikiTalk = "Discusión" ;
@@ -208,9 +234,9 @@ function storeInDB ( $title , $text ) {
 	if ( count ( $talk ) == 2 and $talk[1] == $wikiTalk ) $st = $wikiTalk.":".$talk[0] ;
 	if ( count ( $talk ) == 2 and $talk[1] == strtolower($wikiTalk) ) return ;
 
-	$sql = "INSERT INTO cur (cur_title,cur_ind_title,cur_text,cur_comment,cur_user,cur_user_text,cur_old_version,cur_minor_edit,cur_linked_links,cur_unlinked_links) VALUES ";
+	$sql = "INSERT INTO cur (cur_title,cur_ind_title,cur_text,cur_comment,cur_user,cur_user_text,cur_old_version,cur_minor_edit) VALUES ";
 	$sql .= "(\"" . $recodeCharset ( $st ) . "\",\"" . $recodeCharset ( $st ) . "\",\"" . $recodeCharset ( $text ) . "\",";
-	$sql .= "\"Automated conversion\",0,\"conversion script\",0,1,\"" . $recodeCharset ( $ll1 ) . "\",\"" . $recodeCharset ( $ull1 ) . "\");\n" ;
+	$sql .= "\"Automated conversion\",0,\"conversion script\",0,1);\n" ; # ,\"" . $recodeCharset ( $ll1 ) . "\",\"" . $recodeCharset ( $ull1 ) . "\"
 	fwrite ( $of , $sql ) ;
 	}
 
