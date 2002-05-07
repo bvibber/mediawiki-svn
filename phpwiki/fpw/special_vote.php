d14508668950984e6c654ffe3767bd271bd24a64
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-05-07 06:38:33 +0000

    Bug fixes to voting system.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@350 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/special_vote.php
+++ phpwiki/fpw/special_vote.php
@@ -1,6 +1,7 @@
 <?
 
-function addVote ( $log , $article ) {
+function addVote ( $log , $article )
+{
 	global $user , $wikiGetBriefDate , $CommentBox , $wikiVoteBecause , $wikiVoteMessage ;
 
 	$npage = new WikiPage ;
@@ -14,44 +15,43 @@ function addVote ( $log , $article ) {
 	if ( $CommentBox != "" ) $u .= str_replace ( "$1" , $CommentBox , $wikiVoteBecause ) ;
 	$v = explode ( "\n" , $s ) ;
 	$s = array () ;
-	foreach ( $v AS $x ) {
+	foreach ( $v as $x ) {
 		array_push ( $s , $x ) ;
 		if ( $x == $t ) {
 			$newEntry = false ;
 			array_push ( $s , $u ) ;
-			}
 		}
+	}
 	$s = implode ( "\n" , $s ) ;
 
-	if ( $newEntry )
-		$s .= "$t\n$u\n" ;
+	if ( $newEntry ) $s .= "$t\n$u\n" ;
 
 	$npage->setEntry ( $s , str_replace ( "$1" , $article , str_replace ( "$2" , $user->name , $wikiVoteMessage ) ) , $user->id , $user->name , 1 ) ;
-	}
+}
 
-function vote () {
+function vote ()
+{
 	global $vpage , $target , $doVote , $voted ;
 	global $wikiVoteReason , $wikiVoteWarn , $wikiVotes , $wikiVoteAdded , $wikiVoteError , $wikiVoteChoices ;
+    global $wikiVoteHeadingPre, $wikiVoteHeadingPost;
 
 	if ( isset ( $doVote ) ) {
 		if ( $voted == "" ) {
 			$ret = str_replace ( "$1" , wikiLink("special:vote&target=".urldecode($target)) , $wikiVoteWarn ) ;
-
 		} else {
 			$log = "" ;
 			$log = "wikipedia:".$wikiVotes[$voted] ;
 			if ( $log != "" ) {
 				addVote ( $log , $vpage->getNiceTitle ( urldecode ( $target ) ) ) ;
-				$ret = str_replace ( array("$1","$2","$3") , array(urldecode($target),wikiLink($log),$log) , $wikiVoteAdded ) ;
-			} else $ret = $vikiVoteError ;
-			}
-	} else {
-		$ret = "<font size=+2>I want to vote for \"".$vpage->getNiceTitle(urldecode($target))."\" to be<br><br>\n" ;
-		$ret .= "<FORM method=post>\n" ;
-		$ret .= $wikiVoteChoices ;
-		$ret .= "</FORM>\n</font>\n" ;
+				$ret = str_replace ( array("$1","$2","$3"), array(urldecode($target), wikiLink($log), $log), $wikiVoteAdded ) ;
+			} else $ret = $wikiVoteError ;
 		}
-
-	return $ret ;
+	} else {
+		$ret = "<font size='+2'>" . $wikiVoteHeadingPre . "\"" . $vpage->getNiceTitle(urldecode($target)) . "\"" . $wikiVoteHeadingPost . "<br><br>\n";
+		$ret .= "<form method=post>\n" ;
+		$ret .= $wikiVoteChoices ;
+		$ret .= "</form>\n</font>\n" ;
 	}
+	return $ret ;
+}
 ?>
