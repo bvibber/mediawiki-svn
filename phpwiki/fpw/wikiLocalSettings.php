137110aa75755cd9080a10e130eb94ea0bf16cb9
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-05-09 16:16:01 +0000

    Undoing changes in wikiLocalSettings
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@354 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/fpw/wikiLocalSettings.php
+++ phpwiki/fpw/wikiLocalSettings.php
@@ -10,32 +10,32 @@
 $useCachedPages = false; # Disable page cache
 # $wikiReadOnly = true; # Don't allow page editing
 
-$wikiThisDBserver = "127.0.0.1";
-$wikiSQLServer = "wikidb";
-$wikiThisDBuser = "wikiuser";
-$wikiThisDBpassword = "xxx";
+#$wikiThisDBserver = "127.0.0.1";
+#$wikiSQLServer = "wiki";
+#$wikiThisDBuser = "root";
+#$wikiThisDBpassword = "";
 
 # Preferred URL of the web server running Wikipedia
-$wikiCurrentServer = "http://www.piclab.com";
+#$wikiCurrentServer = "http://127.0.0.1";
 
 # Location of the main wiki.phtml script on that server:
-$THESCRIPT = "/wiki/wiki.phtml";
+#$THESCRIPT = "/";
 
 # How to access an article on your site. Change this if you use
 # apache's rewrite module to produce nicer URLs on your site (as is
 # done on wikipedia.com)
-$wikiArticleSource = "$wikiCurrentServer$THESCRIPT?title=$1";
+#$wikiArticleSource = "$wikiCurrentServer$THESCRIPT?title=$1";
 
-$wikiLogoFile = "/images/emblogo1.gif" ;
+#$wikiLogoFile = "/wiki.png" ;
 
 # Namespace backgrounds
-$wikiNamespaceBackground = array () ;
-$wikiNamespaceBackground[$wikiTalk] = "#CCFFFF" ;
-$wikiNamespaceBackground["user_talk"] = $wikiNamespaceBackground["talk"] ;
-$wikiNamespaceBackground["wikipedia_talk"] = $wikiNamespaceBackground["talk"] ;
-$wikiNamespaceBackground[$wikiUser] = "#CCFFFF" ;
-$wikiNamespaceBackground[$wikiWikipedia] = "#CCFFFF" ;
-$wikiNamespaceBackground["log"] = "#CCFFFF" ;
-$wikiNamespaceBackground["special"] = "#CCFFFF" ;
+#$wikiNamespaceBackground = array () ;
+#$wikiNamespaceBackground[$wikiTalk] = "#CCFFFF" ;
+#$wikiNamespaceBackground["user_talk"] = $wikiNamespaceBackground["talk"] ;
+#$wikiNamespaceBackground["wikipedia_talk"] = $wikiNamespaceBackground["talk"] ;
+#$wikiNamespaceBackground[$wikiUser] = "#CCFFFF" ;
+#$wikiNamespaceBackground[$wikiWikipedia] = "#CCFFFF" ;
+#$wikiNamespaceBackground["log"] = "#CCFFFF" ;
+#$wikiNamespaceBackground["special"] = "#CCFFFF" ;
 
 ?>
