ad0cdf24fd0b79bd848a6a8056341e17e2a52982
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-05-21 22:16:21 +0000

    Fix the cookie path to work for /wiki.phtml and /wiki/ both
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@411 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- fpw/special_userlogout.php
+++ fpw/special_userlogout.php
@@ -3,7 +3,7 @@ function userLogout () {
 	global $user , $vpage , $WikiLoggedIn , $wikiGoodbye ;
 	$vpage->title = "User logout" ;
 	setcookie ( "WikiLoggedIn" , "" , time()-3600 ) ;
-	if ( $user->options["rememberPassword"] != "on" ) setcookie ( "WikiUserPassword" , "" , time()-3600 ) ;
+	if ( $user->options["rememberPassword"] != "on" ) setcookie ( "WikiUserPassword" , "" , time()-3600 , "/") ;
 	$user->isLoggedIn = false ;
 	$WikiLoggedIn = false ;
 	return str_replace ( "$1" , $user->name , $wikiGoodbye ) ;
