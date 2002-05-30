f281791e38fd45e508ace05621bf077337a3c82b
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-05-30 23:45:31 +0000

    Implement allpages, randompage, update README.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@449 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/SpecialRandompage.php
+++ newcodebase/SpecialRandompage.php
@@ -2,9 +2,28 @@
 
 function wfSpecialRandompage()
 {
-	global $wgUser, $wgOut;
+	global $wgOut, $wgTitle, $wgArticle;
 
-	$wgOut->addHTML( "<p>(TODO: Random page)" );
+	$conn = wfGetDB();
+	$sql = "SELECT cur_id FROM cur WHERE (cur_namespace=0 " .
+	  "AND cur_is_redirect=0)";
+	wfDebug( "Rand: $sql\n" );
+
+	$res = mysql_query( $sql, $conn );
+	if ( ( false === $res ) || ( 0 == mysql_num_rows( $res ) ) ) {
+		$wgOut->databaseError( wfMsg( "findrandom" ) );
+		return;
+	}
+	$n = mysql_num_rows( $res ) - 1;
+	wfSeedRandom();
+	mysql_data_seek( $res, mt_rand( 0, $n ) );
+
+	$s = mysql_fetch_object( $res );
+	$newid = $s->cur_id;
+
+	$wgArticle = Article::newFromID( $newid );
+	$wgOut->setArticleFlag( true );
+	$wgArticle->view();
 }
 
 ?>
