c7d2307f9c48aa3b103ec73b02878569ded1243d
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-06-18 00:27:47 +0000

    Did some database tuning.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@524 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/newcodebase/buildtables.sql
+++ phpwiki/newcodebase/buildtables.sql
@@ -1,16 +1,18 @@
 # MySQL script for creating wikipedia database
+# Non-unique indexes are created separatedly by
+# the buildindexes script, so that data loading
+# can be done faster.
 
 CREATE TABLE user (
   user_id int(5) unsigned NOT NULL auto_increment,
-  user_name varchar(40) binary NOT NULL,
+  user_name varchar(64) binary NOT NULL,
   user_rights tinyblob,
   user_password tinyblob,
   user_newpassword tinyblob,
   user_email tinytext,
   user_options blob,
   user_watch mediumblob,
-  UNIQUE KEY user_id (user_id),
-  UNIQUE KEY user_name (user_name(20))
+  UNIQUE KEY user_id (user_id)
 ) TYPE=MyISAM PACK_KEYS=1;
 
 # "Current" version of each article
@@ -30,11 +32,7 @@ CREATE TABLE cur (
   cur_ind_title varchar(255) default NULL,
   cur_ind_text mediumtext,
   cur_is_redirect tinyint(1) unsigned NOT NULL default '0',
-  UNIQUE KEY cur_id (cur_id),
-  INDEX cur_title (cur_title(30)),
-  INDEX cur_timestamp (cur_timestamp),
-  FULLTEXT INDEX cur_ind_title (cur_ind_title),
-  FULLTEXT INDEX cur_ind_text (cur_ind_text)
+  UNIQUE KEY cur_id (cur_id)
 ) TYPE=MyISAM PACK_KEYS=1;
 
 # Historical versions of articles
@@ -50,32 +48,24 @@ CREATE TABLE old (
   old_timestamp char(14) binary NOT NULL default '',
   old_minor_edit tinyint(1) default '0',
   old_flags tinyblob,
-  UNIQUE KEY old_id (old_id),
-  INDEX old_title (old_title(30)),
-  INDEX old_timestamp (old_timestamp)
+  UNIQUE KEY old_id (old_id)
 ) TYPE=MyISAM PACK_KEYS=1;
 
 # Internal links
 #
 CREATE TABLE links (
-  l_from varchar(64) binary NOT NULL default '',
-  l_to int(8) unsigned NOT NULL default '0',
-  INDEX l_from (l_from),
-  INDEX l_to (l_to)
+  l_from varchar(255) binary NOT NULL default '',
+  l_to int(8) unsigned NOT NULL default '0'
 ) TYPE=MyISAM;
 
 CREATE TABLE brokenlinks (
   bl_from int(8) unsigned NOT NULL default '0',
-  bl_to varchar(64) binary NOT NULL default '',
-  INDEX bl_from (bl_from),
-  INDEX bl_to (bl_to)
+  bl_to varchar(255) binary NOT NULL default ''
 ) TYPE=MyISAM;
 
 CREATE TABLE imagelinks (
-  il_from varchar(64) binary NOT NULL default '',
-  il_to varchar(64) binary NOT NULL default '',
-  INDEX il_from (il_from(10)),
-  INDEX il_to (il_to(10))
+  il_from varchar(255) binary NOT NULL default '',
+  il_to varchar(255) binary NOT NULL default ''
 ) TYPE=MyISAM;
 
 # Site-wide statistics.
@@ -95,31 +85,29 @@ CREATE TABLE ipblocks (
   ipb_user int(8) unsigned default '0',
   ipb_by int(8) unsigned default '0',
   ipb_reason blob default '',
-  ipb_timestamp char(14) binary NOT NULL default '',
-  INDEX ipb_address (ipb_address),
-  INDEX ipb_user (ipb_user)
+  ipb_timestamp char(14) binary NOT NULL default ''
 ) TYPE=MyISAM PACK_KEYS=1;
 
+# Uploaded images
+#
 CREATE TABLE image (
-  img_name varchar(128) binary NOT NULL,
+  img_name varchar(255) default NULL,
   img_size int(8) unsigned default '0',
   img_description tinyblob,
   img_user int(5) unsigned default '0',
   img_user_text varchar(40) binary NOT NULL,
-  img_timestamp char(14) binary NOT NULL default '',
-  UNIQUE KEY img_name (img_name),
-  INDEX img_size (img_size),
-  INDEX img_timestamp (img_timestamp)
+  img_timestamp char(14) binary NOT NULL default ''
 ) TYPE=MyISAM PACK_KEYS=1;
 
+# Historical version of uploaded images
+#
 CREATE TABLE oldimage (
-  oi_name varchar(64) binary NOT NULL,
-  oi_archive_name varchar(80) binary NOT NULL,
+  oi_name varchar(255) binary NOT NULL,
+  oi_archive_name varchar(255) binary NOT NULL,
   oi_size int(8) unsigned default 0,
   oi_description tinyblob,
   oi_user int(5) unsigned default '0',
-  oi_user_text varchar(40) binary NOT NULL,
-  oi_timestamp char(14) binary NOT NULL default '',
-  INDEX oi_name (oi_name(8))
+  oi_user_text varchar(64) binary NOT NULL,
+  oi_timestamp char(14) binary NOT NULL default ''
 ) TYPE=MyISAM PACK_KEYS=1;
 
