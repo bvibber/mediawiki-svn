6686ba09c027980dde6f4273aafb30ed773d7eb7
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-06-28 18:23:21 +0000

    A few pre-install tweaks.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@568 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ newcodebase/renameoldtables.sql
@@ -0,0 +1,5 @@
+ALTER TABLE user RENAME TO old_user;
+ALTER TABLE cur RENAME TO old_cur;
+ALTER TABLE old RENAME TO old_old;
+ALTER TABLE linked RENAME TO old_linked;
+ALTER TABLE unlinked RENAME TO old_unlinked;
