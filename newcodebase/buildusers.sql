76f727fe5a14fffd8eb036b4a14e1793d9ef5a53
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-06-29 21:38:14 +0000

    Tweaking conversion scripts
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@572 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/buildusers.sql
+++ newcodebase/buildusers.sql
@@ -2,7 +2,9 @@
 # with proper access rights.  Must be run as root!
 #
 
-GRANT ALL ON wikidb.* TO wikiadmin@localhost IDENTIFIED BY 'adminpwd';
-GRANT ALL ON wikidb.* TO wikiadmin@'%' IDENTIFIED BY 'adminpwd';
-GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost IDENTIFIED BY 'userpwd';
-GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@'%' IDENTIFIED BY 'userpwd';
+GRANT ALL ON wikidb.* TO wikiadmin@'%' IDENTIFIED BY 'admin7399';
+GRANT ALL ON wikidb.* TO wikiadmin@localhost IDENTIFIED BY 'admin7399';
+GRANT ALL ON wikidb.* TO wikiadmin@localhost.localdomain IDENTIFIED BY 'admin7399';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@'%' IDENTIFIED BY 'user2682';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost IDENTIFIED BY 'user2682';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost.localdomain IDENTIFIED BY 'user2682';
--- newcodebase/renameoldtables.sql
+++ newcodebase/renameoldtables.sql
@@ -1,5 +1,5 @@
 ALTER TABLE user RENAME TO old_user;
 ALTER TABLE cur RENAME TO old_cur;
 ALTER TABLE old RENAME TO old_old;
-ALTER TABLE linked RENAME TO old_linked;
-ALTER TABLE unlinked RENAME TO old_unlinked;
+DROP TABLE IF EXISTS linked;
+DROP TABLE IF EXISTS unlinked;
