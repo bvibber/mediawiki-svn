e3d389b0b6d0d259ee5eba277f8c5a89c2b19055
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-06-30 01:58:04 +0000

    Gotta change passwords now...
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@576 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/newcodebase/buildusers.sql
+++ phpwiki/newcodebase/buildusers.sql
@@ -2,9 +2,9 @@
 # with proper access rights.  Must be run as root!
 #
 
-GRANT ALL ON wikidb.* TO wikiadmin@'%' IDENTIFIED BY 'admin7399';
-GRANT ALL ON wikidb.* TO wikiadmin@localhost IDENTIFIED BY 'admin7399';
-GRANT ALL ON wikidb.* TO wikiadmin@localhost.localdomain IDENTIFIED BY 'admin7399';
-GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@'%' IDENTIFIED BY 'user2682';
-GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost IDENTIFIED BY 'user2682';
-GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost.localdomain IDENTIFIED BY 'user2682';
+GRANT ALL ON wikidb.* TO wikiadmin@'%' IDENTIFIED BY 'adminpass';
+GRANT ALL ON wikidb.* TO wikiadmin@localhost IDENTIFIED BY 'adminpass';
+GRANT ALL ON wikidb.* TO wikiadmin@localhost.localdomain IDENTIFIED BY 'adminpass';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@'%' IDENTIFIED BY 'userpass';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost IDENTIFIED BY 'userpass';
+GRANT DELETE,INSERT,SELECT,UPDATE ON wikidb.* TO wikiuser@localhost.localdomain IDENTIFIED BY 'userpass';
