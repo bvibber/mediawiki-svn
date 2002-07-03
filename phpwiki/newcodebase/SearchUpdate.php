0ed306f9ef2c454e9c36a7595213ceed2b5a3c96
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-07-03 19:13:37 +0000

    Search fix.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@597 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/newcodebase/SearchUpdate.php
+++ phpwiki/newcodebase/SearchUpdate.php
@@ -20,16 +20,9 @@ class SearchUpdate {
 	function doUpdate()
 	{
 		$lc = SearchEngine::legalSearchChars() . "&#;";
-		$t = preg_replace( "/[^{$lc}]+/", " ", $this->mTitle );
-		$t = preg_replace( "/\\b[{$lc}][{$lc}]\\b/", " ", $t );
-		$t = preg_replace( "/\\b[{$lc}]\\b/", " ", $t );
-		$t = preg_replace( "/\\s+/", " ", $t );
-
-		$words = explode( " ", strtolower( trim( $t ) ) );
-		foreach ( $words as $w ) { $this->mTitleWords[$w] = 1; }
 
 		$text = preg_replace( "/<\\/?\\s*[A-Za-z][A-Za-z0-9]*\\s*([^>]*?)>/",
-		  " ", strtolower( $this->mText ) ); # Strip HTML markup
+		  " ", strtolower( " " . $this->mText . " " ) ); # Strip HTML markup
 		$text = preg_replace( "/(^|\\n)\\s*==\\s+([^\\n]+)\\s+==\\s/sD",
 		  "\\2 \\2 \\2 ", $text ); # Emphasize headings
 
@@ -39,13 +32,13 @@ class SearchUpdate {
 		$pat = "/(^|[^\\[])({$protos}):[{$uc}]+([^{$uc}]|$)/";
 		$text = preg_replace( $pat, "\\1 \\3", $text );
 
-		$p1 = "/(^|[^\\[])\\[({$protos}):[{$uc}]+]/";
-		$p2 = "/(^|[^\\[])\\[({$protos}):[{$uc}]+\\s+([^\\]]+)]/";
+		$p1 = "/([^\\[])\\[({$protos}):[{$uc}]+]/";
+		$p2 = "/([^\\[])\\[({$protos}):[{$uc}]+\\s+([^\\]]+)]/";
 		$text = preg_replace( $p1, "\\1 ", $text );
 		$text = preg_replace( $p2, "\\1 \\3 ", $text );
 
 		# Internal image links
-		$pat2 = "/\\[\\[image:([{$uc}]+)\\.(png|jpg|jpeg)([^{$uc}])/i";
+		$pat2 = "/\\[\\[image:([{$uc}]+)\\.(gif|png|jpg|jpeg)([^{$uc}])/i";
 		$text = preg_replace( $pat2, " \\1 \\3", $text );
 
 		$text = preg_replace( "/([^{$lc}])([{$lc}]+)]]([a-z]+)/",
@@ -58,20 +51,14 @@ class SearchUpdate {
 		$text = preg_replace( "/([{$lc}]+)'s /", "\\1 \\1's ", $text );
 		$text = preg_replace( "/([{$lc}]+)s' /", "\\1s ", $text );
 
-		# Strip 1- and 2-letter words
-		$text = preg_replace( "/(^|[^{$lc}])[{$lc}][{$lc}]([^{$lc}]|$)/",
-		  "\\1 \\2", $text );
-		$text = preg_replace( "/(^|[^{$lc}])[{$lc}]([^{$lc}]|$)/",
-		  "\\1 \\2", $text );
-
 		# Strip wiki '' and '''
 		$text = preg_replace( "/''[']*/", " ", $text );
 
-		# Remove title words: those have already been found
-		foreach ( $this->mTitleWords as $w => $val ) {
-			$text = str_replace( $w, " ", $text );
-		}
-		$text = preg_replace( "/\\s+/", " ", $text );
+		# Strip 1- and 2-letter words
+		$text = preg_replace( "/\\s[{$lc}][{$lc}]\\s/", " ", $text );
+		$text = preg_replace( "/\\s[{$lc}][{$lc}]\\s/", " ", $text );
+		$text = preg_replace( "/\\s[{$lc}]\\s/", " ", $text );
+		$text = preg_replace( "/\\s[{$lc}]\\s/", " ", $text );
 
 		$sql = "UPDATE cur SET cur_timestamp=cur_timestamp,cur_ind_text='" .
 		  wfStrencode( $text ) . "' WHERE cur_id={$this->mId}";
