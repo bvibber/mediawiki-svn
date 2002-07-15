c00522043c671ce511880afef68bec5897b3a3f2
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-07-15 06:42:17 +0000

    Random page speedup.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@643 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/SpecialRandompage.php
+++ newcodebase/SpecialRandompage.php
@@ -3,22 +3,32 @@
 function wfSpecialRandompage()
 {
 	global $wgOut, $wgTitle, $wgArticle;
+	$fname = "wfSpecialRandompage";
 
-	$sql = "SELECT cur_id FROM cur WHERE (cur_namespace=0 " .
-	  "AND cur_is_redirect=0)";
-	$res = wfQuery( $sql, "wfSpecialRandompage" );
-
-	$n = wfNumRows( $res ) - 1;
 	wfSeedRandom();
-	wfDataSeek( $res, mt_rand( 0, $n ) );
+	if ( 0 == mt_rand( 0, 899 ) ) { # Time to reload
+		# Reload 1000 random titles into random table.  Doing it this
+		# roundabout way avoids the need for any locking.
+		#
+		$sql = "INSERT INTO random(ra_current,ra_title) SELECT 0,cur_title " .
+		  "FROM cur WHERE cur_namespace=0 AND cur_is_redirect=0 " .
+		  "ORDER BY RAND() LIMIT 1000";
+		wfQuery( $sql, $fname );
 
+		$sql = "UPDATE random SET ra_current=(ra_current+1)";
+		wfQuery( $sql, $fname );
+
+		$sql = "DELETE FROM random WHERE ra_current>1";
+		wfQuery( $sql, $fname );
+	}
+	$sql = "SELECT ra_title FROM random WHERE ra_current=1 " .
+	  "ORDER BY RAND() LIMIT 1";
+	$res = wfQuery( $sql, $fname );
 	$s = wfFetchObject( $res );
-	$newid = $s->cur_id;
-	wfFreeResult( $res );
+	$rt = $s->ra_title;
 
 	$wgOut->reportTime(); # for logfile
-	$t = Article::nameOf( $newid );
-	$wgOut->redirect( wfLocalUrl( $t ) );
+	$wgOut->redirect( wfLocalUrl( $rt ) );
 }
 
 ?>
