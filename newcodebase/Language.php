1be738ccf3dd12bd7de49514d88544212f8f23d7
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-09-13 10:21:40 +0000

    User option for full width edit box
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@756 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/Language.php
+++ newcodebase/Language.php
@@ -31,7 +31,8 @@
 	"justify"	=> "Justify paragraphs",
 	"hideminor" => "Hide minor edits in recent changes",
 	"numberheadings" => "Auto-number headings",
-	"rememberpassword" => "Remember password across sessions"
+	"rememberpassword" => "Remember password across sessions",
+	"editwidth" => "Edit box has full width"
 );
 
 /* private */ $wgLanguageNamesEn = array(
