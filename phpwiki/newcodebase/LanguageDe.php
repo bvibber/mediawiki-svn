6f9e65f1f7199084d11504f03a5ce57da967aace
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-09-13 09:52:21 +0000

    Book sources text
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@755 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/newcodebase/LanguageDe.php
+++ phpwiki/newcodebase/LanguageDe.php
@@ -485,6 +485,8 @@ Davon haben <b>$2</b> Administrator-Rechte (siehe $3).",
 "newpages"		=> "Neue Artikel",
 "movethispage"	=> "Artikel verschieben",
 "unusedimagestext" => "<p>Bitte beachten Sie, dass andere Wikipedias, die noch die alte Software verwenden, möglicherweise einige dieser Bilder benutzen.",
+"booksources"	=> "Buchhandlungen",
+"booksourcetext" => "Dies ist eine Liste mit Links zu Internetseiten, die neue und gebrauchte Bücher verkaufen. Dort kann es auch weitere Informationen über die Bücher geben, die Sie interessieren. Wikipedia ist mit keinem dieser Anbieter geschäftlich verbunden.",
 
 # Email this user
 #
