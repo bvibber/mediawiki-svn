9a094f5a36ea0fae01a480336851d82b5a605cd6
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2002-10-26 10:17:47 +0000

    Prevent empty titles in namespaces; interwiki link in URL creates duplicate page bugs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@870 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/Language.php
+++ newcodebase/Language.php
@@ -365,6 +365,9 @@ Please report this to an administrator, making note of the URL.",
 "formerror"		=> "Error: could not submit form",	
 "badarticleerror" => "This action cannot be performed on this page.",
 "cannotdelete"	=> "Could not delete the page or image specified.",
+"badtitle"		=> "Bad title",
+"badtitletext"	=> "The requested page title was invalid, empty, or
+an incorrectly linked inter-language or inter-wiki title.",
 
 # Login and logout pages
 #
