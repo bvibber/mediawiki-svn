16f941a81cf2497c106d2e9447140c94a6e88437
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2003-02-09 23:29:55 +0000

    Move random queue directly into cur table
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@1161 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/newcodebase/SpecialRandompage.php
+++ phpwiki/newcodebase/SpecialRandompage.php
@@ -6,31 +6,20 @@ function wfSpecialRandompage()
 	$fname = "wfSpecialRandompage";
 
 	wfSeedRandom();
-	$sqlget = "SELECT ra_title FROM random WHERE ra_current=1 " .
-	  "ORDER BY RAND() LIMIT 1";
+	$sqlget = "SELECT cur_id,cur_title
+		FROM cur USE INDEX (cur_random)
+		WHERE cur_namespace=0 AND cur_is_redirect=0
+		ORDER BY cur_random
+		LIMIT 1";
 	$res = wfQuery( $sqlget, $fname );
-	if ( 0 == wfNumRows( $res ) or $force == "yes") { # Time to reload
-		# Reload 1000 random titles into random table.  Doing it this
-		# roundabout way avoids the need for any locking.
-		#
-		$sql = "INSERT INTO random(ra_current,ra_title) SELECT 0,cur_title " .
-		  "FROM cur WHERE cur_namespace=0 AND cur_is_redirect=0 " .
-		  "ORDER BY RAND() LIMIT 1000";
+	if( $s = wfFetchObject( $res ) ) {
+		$sql = "UPDATE cur SET cur_random=RAND() WHERE cur_id={$s->cur_id}";
 		wfQuery( $sql, $fname );
-
-		$sql = "UPDATE random SET ra_current=(ra_current+1)";
-		wfQuery( $sql, $fname );
-
-		$sql = "DELETE FROM random WHERE ra_current>1";
-		wfQuery( $sql, $fname );
-
-		$res = wfQuery( $sqlget, $fname );
+		$rt = wfUrlEncode( $s->cur_title );
+	} else {
+		# No articles?!
+		$rt = "";
 	}
-	$s = wfFetchObject( $res );
-	$sql = "UPDATE random SET ra_current=(ra_current+1) WHERE ra_title='"
-		. wfStrencode( $s->ra_title ) . "'";
-	wfQuery( $sql, $fname );
-	$rt = wfUrlEncode( $s->ra_title );
 
 	$wgOut->reportTime(); # for logfile
 	$wgOut->redirect( wfLocalUrl( $rt ) );
