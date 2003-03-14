79f28d55bcb6de7c04058ceef6aa521357f0eba2
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2003-03-14 23:38:07 +0000

    Big update. Suite is pretty well laid out now.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@1238 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phpwiki/testsuite/src/com/piclab/wikitest/EditTest.java
+++ phpwiki/testsuite/src/com/piclab/wikitest/EditTest.java
@@ -15,42 +15,171 @@ public EditTest( WikiSuite ws ) { super(ws); }
 public String testName() { return "Editing"; }
 
 protected boolean runTest() throws Exception {
+	m_suite.clearCookies();
+	if ( ! part1() ) { throw new WikiSuiteFailureException( "Part 1" ); }
+	if ( ! part2() ) { throw new WikiSuiteFailureException( "Part 2" ); }
+	if ( ! part3() ) { throw new WikiSuiteFailureException( "Part 3" ); }
+	if ( ! part4() ) { throw new WikiSuiteFailureException( "Part 4" ); }
+	if ( ! part5() ) { throw new WikiSuiteFailureException( "Part 5" ); }
+
+	return true;
+}
+
+private boolean part1() throws Exception {
+	/*
+	 * Add a line to some pages.  See that the change
+	 * shows up in Recent Changes, and the new text appears.
+	 */
 	boolean result = true;
 
-	WebResponse wr = m_suite.editPage( "Agriculture" );
-	WebForm editform = null;
-	WebRequest req = null;
+	WebResponse wr = addText( "Agriculture",
+	  "Edited for testing: 85769476243364759655." );
 
-	editform = WikiSuite.getFormByName( wr, "editform" );
-	req = editform.getRequest( "wpSave" );
-	String text = req.getParameter( "wpTextbox1" );
-	req.setParameter( "wpTextbox1", text + "\nEdited for testing." );
-	wr = m_suite.getResponse( req );
+	wr = m_suite.viewPage( "Special:Recentchanges" );
+	String text = wr.getText();
+	if ( text.indexOf( "Agriculture" ) < 0 ) { result = false; }
+
+	wr = m_suite.viewPage( "Agriculture" );
+	text = wr.getText();
+	if ( text.indexOf( "85769476243364759655" ) < 0 ) { result = false; }
+
+	wr = addText( "Physics", "Edited for testing: 98762415237651243634" );
+	wr = addText( "Mathematics", "Edited for testing: 54637465888374655394" );
 
 	wr = m_suite.viewPage( "Special:Recentchanges" );
 	text = wr.getText();
-	if ( text.indexOf( "Agriculture" ) < 0 ) { result = false; }
+	if ( text.indexOf( "Physics" ) < 0 ) { result = false; }
+	if ( text.indexOf( "Mathematics" ) < 0 ) { result = false; }
 
-	wr = m_suite.viewPage( "Omaha" ); /* Not preloaded */
+	wr = m_suite.viewPage( "Physics" );
 	text = wr.getText();
-	if ( text.indexOf( "no text in this page" ) < 0 ) { result = false; }
+	if ( text.indexOf( "98762415237651243634" ) < 0 ) { result = false; }
+	if ( text.indexOf( "54637465888374655394" ) >= 0 ) { result = false; }
 
-	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT,
-	  "Edit this page" );
-	wr = l.click();
-
-	editform = WikiSuite.getFormByName( wr, "editform" );
-	req = editform.getRequest( "wpSave" );
-	req.setParameter( "wpTextbox1", "'''Omaha''' is a city in [[Nebraska]]" );
-	wr = m_suite.getResponse( req );
-
-	wr = m_suite.viewPage( "Omaha" );
+	wr = m_suite.viewPage( "Mathematics" );
 	text = wr.getText();
-	if ( text.indexOf( "no text in this page" ) >= 0 ) { result = false; }
-	if ( text.indexOf( "Nebraska" ) < 0 ) { result = false; }
+	if ( text.indexOf( "54637465888374655394" ) < 0 ) { result = false; }
+	if ( text.indexOf( "98762415237651243634" ) >= 0 ) { result = false; }
 
 	return result;
 }
+
+private boolean part2() throws Exception {
+	/*
+	 * Create a new page, verify it, add to it, replace it.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "Omaha" ); /* Not preloaded */
+	String text = wr.getText();
+	if ( text.indexOf( "no text in this page" ) < 0 ) { result = false; }
+
+	wr = addText( "Omaha", "'''Omaha''' is a city in [[Florida]]" );
+	wr = m_suite.viewPage( "Omaha" );
+	text = wr.getText();
+
+	if ( text.indexOf( "no text in this page" ) >= 0 ) { result = false; }
+	if ( text.indexOf( "Florida" ) < 0 ) { result = false; }
+
+	wr = addText( "Omaha", "And a [[poker]] game for masochists." );
+	wr = m_suite.viewPage( "Special:Recentchanges" );
+	text = wr.getText();
+	if ( text.indexOf( "Omaha" ) < 0 ) { result = false; }
+
+	wr = m_suite.viewPage( "Omaha" );
+	text = wr.getText();
+	if ( text.indexOf( "Florida" ) < 0 ) { result = false; }
+	if ( text.indexOf( "poker" ) < 0 ) { result = false; }
+
+	wr = m_suite.editPage( "Omaha" );
+	WebForm editform = WikiSuite.getFormByName( wr, "editform" );
+	WebRequest req = editform.getRequest( "wpSave" );
+	req.setParameter( "wpTextbox1", "See: \n" +
+	  "* [[Omaha, Nebraska]]\n* [[Omaha holdem|Omaha hold'em]]" );
+	wr = m_suite.getResponse( req );
+
+	text = wr.getText();
+	if ( text.indexOf( "Florida" ) >= 0 ) { result = false; }
+	if ( text.indexOf( "poker" ) >= 0 ) { result = false; }
+	if ( text.indexOf( "Nebraska" ) < 0 ) { result = false; }
+	if ( text.indexOf( "holdem" ) < 0 ) { result = false; }
+
+	return result;
+}
+
+private boolean part3() throws Exception {
+	/*
+	 * Log in and make some edits as a user.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.loginAs( "Fred", "Fred" );
+	wr = addText( "Talk:Language", "This page sucks!" );
+
+	wr = m_suite.viewPage( "Special:Recentchanges" );
+	String text = wr.getText();
+	if ( text.indexOf( "Fred" ) < 0 ) { result = false; }
+
+	wr = m_suite.loginAs( "Barney", "Barney" );
+	wr = addText( "Talk:Language", "No it doesn't" );
+
+	wr = m_suite.viewPage( "Special:Recentchanges" );
+	text = wr.getText();
+	if ( text.indexOf( "Barney" ) < 0 ) { result = false; }
+
+	wr = m_suite.viewPage( "Talk:Language" );
+	text = wr.getText();
+	if ( text.indexOf( "sucks" ) < 0 ) { result = false; }
+	if ( text.indexOf( "doesn't" ) < 0 ) { result = false; }
+
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Older versions" );
+	wr = l.click();
+	text = wr.getText();
+	if ( text.indexOf( "Fred" ) < 0 ) { result = false; }
+	if ( text.indexOf( "Barney" ) < 0 ) { result = false; }
+
+	return result;
+}
+
+private boolean part4() throws Exception {
+	/*
+	 * Verify edit conflict handling.
+	 */
+	boolean result = true;
+
+	return result;
+}
+
+private boolean part5() throws Exception {
+	/*
+	 * Verify page protection features.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "Wikipedia:Upload_log" );
+	String text = wr.getText();
+	if ( text.indexOf( "Protected page" ) < 0 ) { result = false; }
+	if ( text.indexOf( "Edit this page" ) >= 0 ) { result = false; }
+
+	return result;
+}
+
+/*
+ * Add a given piece of text to the given page
+ */
+
+private WebResponse addText( String page, String text )
+throws Exception {
+	WebResponse wr = m_suite.editPage( page );
+
+	WebForm editform = WikiSuite.getFormByName( wr, "editform" );
+	WebRequest req = editform.getRequest( "wpSave" );
+	String old = req.getParameter( "wpTextbox1" );
+	req.setParameter( "wpTextbox1", old + "\n" + text );
+
+	return m_suite.getResponse( req );
+}
+
 
 public static void main( String[] params ) {
 	WikiSuite ws = new WikiSuite();
--- phpwiki/testsuite/src/com/piclab/wikitest/LinkTest.java
+++ phpwiki/testsuite/src/com/piclab/wikitest/LinkTest.java
@@ -15,23 +15,179 @@ public LinkTest( WikiSuite ws ) { super(ws); }
 public String testName() { return "Links"; }
 
 protected boolean runTest() throws Exception {
-	boolean result = true;
+	m_suite.clearCookies(); /* Make sure we aren't logged in */
+
+	if ( ! part1() ) { throw new WikiSuiteFailureException( "Part 1" ); }
+	if ( ! part2() ) { throw new WikiSuiteFailureException( "Part 2" ); }
+	if ( ! part3() ) { throw new WikiSuiteFailureException( "Part 3" ); }
+	if ( ! part4() ) { throw new WikiSuiteFailureException( "Part 4" ); }
+	if ( ! part5() ) { throw new WikiSuiteFailureException( "Part 5" ); }
+
+	return true;
+}
+
+private boolean part1() throws Exception {
+	/*
+	 * Check that we can click through from main page to games,
+	 * card games, poker, world series.
+	 */
 	WebResponse wr = m_suite.viewPage( "" ); /* Main page */
-
-	WebLink l = wr.getFirstMatchingLink(
-	  WebLink.MATCH_CONTAINED_TEXT, "game" );
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Game" );
 	wr = l.click();
-	WikiSuite.showResponseTitle( wr );
-
-	l = wr.getFirstMatchingLink(
-	  WebLink.MATCH_CONTAINED_TEXT, "card" );
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Card" );
 	wr = l.click();
-	WikiSuite.showResponseTitle( wr );
-
-	l = wr.getFirstMatchingLink(
-	  WebLink.MATCH_CONTAINED_TEXT, "poker" );
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Poker" );
 	wr = l.click();
-	WikiSuite.showResponseTitle( wr );
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "World Series" );
+	wr = l.click();
+
+	return true;
+}
+
+private boolean part2() throws Exception {
+	/* 
+	 * Poker page should have some standard links on it, and should
+	 * _not_ have an upload link or user stat links on it because we
+	 * aren't logged in.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "Poker" );
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Printable version" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Related changes" );
+	result = (l != null);
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Upload file" );
+	result = (l == null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "My watchlist" );
+	result = (l == null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "My contributions" );
+	result = (l == null);
+
+	return result;
+}
+
+private boolean part3() throws Exception {
+	/*
+	 * Talk:Poker was not preloaded, so we should be on an edit form
+	 * when we click that link from the Poker page.  Add a comment,
+	 * then check for some standard links on the new talk page and
+	 * the resulting history page.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "Poker" );
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Discuss this page" );
+	wr = l.click();
+
+	WebForm editform = WikiSuite.getFormByName( wr, "editform" );
+    WebRequest req = editform.getRequest( "wpSave" );
+    req.setParameter( "wpTextbox1", "Great article!" );
+    wr = m_suite.getResponse( req );
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "View article" );
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Older versions" );
+	wr = l.click();
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Current revision" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "View discussion" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "View article" );
+	wr = l.click();
+
+	return result;
+}
+
+private boolean part4() throws Exception {
+	/*
+	 * Let's log in now and verify that things are changed.
+	 */
+	boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "Poker" );
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Log in" );
+	wr = l.click();
+
+	WebForm loginform = WikiSuite.getFormByName( wr, "userlogin" );
+	WebRequest req = loginform.getRequest( "wpLoginattempt" );
+	req.setParameter( "wpName", "Fred" );
+	req.setParameter( "wpPassword", "Fred" );
+	wr = m_suite.getResponse( req );
+
+	String text = wr.getText();
+	if ( text.indexOf( "successful" ) < 0 ) { throw new
+	  WikiSuiteFailureException( "Could not log in" ); }
+
+	/*
+	 * Should have a "return to" link.
+	 */
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Poker" );
+	wr = l.click();
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "My watchlist" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "My contributions" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "new messages" );
+	result = (l == null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Upload file" );
+	wr = l.click();
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "list of uploaded images" );
+	result = (l != null);
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "upload log" );
+	result = (l != null);
+
+	return result;
+}
+
+private boolean part5() throws Exception {
+	/*
+	 * Verify that the user page and user talk page are OK.
+	 */
+    boolean result = true;
+
+	WebResponse wr = m_suite.viewPage( "" );
+	WebLink l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Fred" );
+	wr = l.click();
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "User contributions" );
+	wr = l.click();
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Talk" );
+	wr = l.click();
+
+	/*
+	 * Log out, clear cookies, edit talk page, then log back in and
+	 * verify "new messages" link.
+	 */
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Log out" );
+	wr = l.click();
+	m_suite.clearCookies();
+
+	wr = m_suite.editPage( "User talk:Fred" );
+	WebForm editform = WikiSuite.getFormByName( wr, "editform" );
+    WebRequest req = editform.getRequest( "wpSave" );
+    req.setParameter( "wpTextbox1", "Wake up!" );
+    wr = m_suite.getResponse( req );
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Main Page" );
+	wr = l.click();
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Log in" );
+	wr = l.click();
+
+	WebForm loginform = WikiSuite.getFormByName( wr, "userlogin" );
+	req = loginform.getRequest( "wpLoginattempt" );
+	req.setParameter( "wpName", "Fred" );
+	req.setParameter( "wpPassword", "Fred" );
+	wr = m_suite.getResponse( req );
+
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "new messages" );
+	wr = l.click();
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "Main Page" );
+	wr = l.click();
+	l = wr.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "new messages" );
+	result = (l == null);
 
 	return result;
 }
