9c5bfe9eeba238cb7d943580fb5561c255340ec2
lcrocker <lcrocker@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2003-03-20 22:37:22 +0000

    Update createdb.php script for easier use, setup for tests.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phpwiki@1248 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- newcodebase/maintenance/createdb.php
+++ newcodebase/maintenance/createdb.php
@@ -12,9 +12,26 @@ set_time_limit(0);
 #$wgDBuser			= "wikiadmin";
 #$wgDBpassword		= "adminpass";
 
-buildTables();
-initializeTables();
-buildIndexes();
+print "\n  * * *\nWarning! This script will completely erase the\n" .
+  "existing database '" . $wgDBname . "' and all its contents.\n" .
+  "Are you sure you want to do this? (yes/no) ";
+
+$fp = fopen( "php://stdin", "r" );
+$response = trim( fgets( $fp ) );
+fclose( $fp );
+
+if ( 0 == strcasecmp( "yes", $response ) ) {
+	$sql = "DROP DATABASE IF EXISTS " . $wgDBname;
+	wfQuery( $sql );
+	$sql = "CREATE DATABASE " . $wgDBname;
+	wfQuery( $sql );
+
+	buildTables();
+	initializeTables();
+	buildIndexes();
+} else {
+	print "You did not respond with 'yes'; exiting.\n";
+}
 
 print "Done.\n";
 exit();
