dea91c8cf90bca54b64f4b10bc57608971a1bae2
e23 <e23@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2003-12-05 03:29:39 +0000

    Added some translations. Reintroduced some changes that was mistakenly reverted.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/stable@2137 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/languages/LanguageSv.php
+++ phase3/languages/LanguageSv.php
@@ -9,6 +9,7 @@
 // 
 
 /* private */ $wgNamespaceNamesSv = array(
+	-2	=> "Media",
         -1      => "Special",
         0       => "",
         1       => "Diskussion",
@@ -17,7 +18,9 @@
         4       => "Wikipedia",
         5       => "Wikipedia_diskussion",
         6       => "Bild",
-        7       => "Bild_diskussion"
+        7       => "Bild_diskussion",
+	8	=> "MediaWiki",
+	9	=> "MediaWiki_diskussion",
 );
 
 /* inherit standard defaults */
@@ -36,18 +39,18 @@
         "highlightbroken"  => "Röda länkar till tomma sidor",
         "justify"          => "Justera indrag",
         "hideminor"        => "Göm små redigeringar vid senaste ändring",
-	"usenewrc" => "Enhanced recent changes (not for all browsers)",
+	"usenewrc"         => "Avancerad 'Senaste ändringar'",
         "numberheadings"   => "Automatisk numrering av överskrifter",
         "rememberpassword" => "Kom ihåg lösenord till nästa besök",
         "editwidth"        => "Redigeringsboxen har full bredd",
         "editondblclick"   => "Redigera sidor med dubbelklick (JavaScript)",
-	"editsection"=>"Enable section editing via [edit] links",
-	"editsectiononrightclick"=>"Enable section editing by right clicking<br> on section titles (JavaScript)",
-	"showtoc"=>"Show table of contents<br>(for articles with more than 3 headings)",
+	"editsection"      => "Visa [edit]-länkar för att redigera sektioner",
+	"editsectiononrightclick" => "Högerklick på rubriker redigerar sektioner",
+	"showtoc"          => "Visa automatisk innehållsförteckning (på sidor med mer än 3 sektioner)",
         "watchdefault"     => "Övervaka nya och ändrade artiklar",
         "minordefault"     => "Markera som standard alla ändringer som mindre",
-	"previewontop" => "Show preview before edit box and not after it",
-	"nocache" => "Disable page caching"
+	"previewontop"     => "Visa förhandsgranskning före textfältet istället för efter",
+	"nocache"          => "Slå av cachning av sidor"
 );
 
 /* private */ $wgBookstoreListSv = array(
@@ -103,6 +106,7 @@ $wgValidSpecialPagesSv = array(
         "Shortpages"    => "Korta artiklar",
         "Longpages"     => "Långa artiklar",
         "Newpages"      => "De nyaste artiklarna",
+	"Ancientpages"	=> "De äldsta artiklarna",
         "Allpages"      => "Alla sidor efter titel",
 
         "Ipblocklist"   => "Blockerade IP adresser",
@@ -113,7 +117,8 @@ $wgValidSpecialPagesSv = array(
         "Whatlinkshere" => "",
         "Recentchangeslinked" => "",
         "Movepage"      => "",
-        "Booksources"   => "Externa bokkällor"
+	"Booksources"   => "Externa bokkällor",
+	"Export"        => "Exportera till XML",
 );
 
 /* private */ $wgSysopSpecialPagesSv = array(
@@ -143,6 +148,8 @@ $wgValidSpecialPagesSv = array(
 "wikititlesuffix"       => "Wikipedia",
 "bugreports"            => "Felrapporter",
 "bugreportspage"        => "Wikipedia:Felrapporter",
+"sitesupport"           => "Donationer",
+"sitesupportpage"       => "", # If not set, won't appear. Can be wiki page or URL
 "faq"                   => "FAQ",
 "faqpage"               => "Wikipedia:FAQ",
 "edithelp"              => "Redigeringshjälp",
@@ -153,7 +160,7 @@ $wgValidSpecialPagesSv = array(
 "qbedit"                => "Redigera",
 "qbpageoptions"         => "Sidinställningar",
 "qbpageinfo"            => "Sidinformation",
-"qbmyoptions"           => "Mina inställingar",
+"qbmyoptions"           => "Mina inställningar",
 "mypage"                => "Min sida",
 "mytalk"                => "Min diskussion",
 "currentevents"         => "-",
@@ -167,9 +174,12 @@ $wgValidSpecialPagesSv = array(
 "printableversion"      => "Skrivarvänlig version",
 "editthispage"          => "Redigera den här sidan",
 "deletethispage"        => "Radera den här sidan",
-"protectthispage"       => "Skydda den här sida",
+"protectthispage"       => "Skydda den här sidan",
 "unprotectthispage"     => "Ta bort skydd av denna sida",
+"newpage"               => "Ny sida",
 "talkpage"              => "Diskussionssida",
+"postcomment"           => "Skriv ett inlägg",
+"articlepage"           => "Visa artikel",
 "subjectpage"           => "Ämnessida",
 "userpage"              => "Visa användarsida",
 "wikipediapage"         => "Visa metasida",
@@ -194,6 +204,14 @@ Se $1.",
 "sitetitle"             => "Wikipedia",
 "sitesubtitle"          => "Den fria encyklopedin",
 "retrievedfrom"         => "Hämtat från \"$1\"",
+"newmessages"           => "Du har $1.",
+"newmessageslink"       => "nya meddelanden",
+"editsection"           => "edit",
+"toc"                   => "Innehåll",
+"showtoc"               => "visa",
+"hidetoc"               => "göm",
+"thisisdeleted"         => "Visa eller återställ $1?",
+"restorelink"           => "$1 raderade versioner",
 
 // Main script and global functions
 //
@@ -262,14 +280,15 @@ igen som samma eller annan användare.\n",
 
 "areyounew"             => "Om du är ny på Wikipedia och önskar ett användarkonto, så skriv in ett användarnamn, därefter skriver du ett lösenord som du sedan upprepar.
 
-Observera att det finns bättre och sämre val av användarnamn, försök välja något unikt, och där du löper mindre risk att en dag behöva dela detta användarnamn med ett större antal användare som kan ha rätt till det. Exempel: Johansson, eller Peter. Allra helst ser vi attt du använder ditt eget för och efternamn, men du bestämmer naturligtvis själv.
+Observera att det finns bättre och sämre val av användarnamn, försök välja något unikt, och där du löper mindre risk att en dag behöva dela detta användarnamn med ett större antal användare som kan ha rätt till det. Exempel: Johansson, eller Peter. Allra helst ser vi att du använder ditt eget för och efternamn, men du bestämmer naturligtvis själv.
 
-Din e-postadress är frivillig; men om du glömmer ditt lösenord kan du till exempel be om att få det tillsänt till den adress du har anget.<br>\n",
+Att ange e-postadress är frivilligt; men om du glömmer ditt lösenord kan du till exempel be om att få det tillsänt till den adress du har angett.<br>\n",
 
 "login"                 => "Logga in",
 "userlogin"             => "Logga in",
 "logout"                => "Logga ut",
 "userlogout"            => "Logga ut",
+"notloggedin" 		=> "Ej inloggad",
 "createaccount"         => "Skapa ett konto",
 "badretype"             => "De lösenord du uppgett överenstämmer inte med varandra.",
 "userexists"            => "Detta användarnamn används redan. Ange ett annat användarnamn.",
@@ -307,7 +326,8 @@ Var snäll och logga in igen när du fått meddelandet.",
 Anledning är att:<br>''$2''<p>Ta kontakt med $1 eller en av de andra
 [[Wikipedia:Administratörer|administratörerna]] för att diskutera varför du blivit spärrad", // "
 "newarticle"            => "(Ny)",
-"newarticletext"        => "Skriv den nya sidan här.",
+"newarticletext"        => "Du har klickat på en röd länk, en sida som inte finns ännu. Du kan hjälpa till genom att själv skriva vad du vet om ämnet i fältet nedan. Om du inte vill skriva något kan du bara trycka på \"tillbaka\" i din webbläsare.",
+"anontalkpagetext"      => "---- ''Detta är en diskussionssida för en anonym användare, en användare som inte har skapat sig ett konto, eller som inte har loggat in på det. Vi måste därför använda personens numeriska [[IP-adress]] för identifiera honom eller henne. En sådan IP-adress kan ibland användas av flera olika personer. Om du är en anonym användare och ser meddelanden på den här sidan som inte tycks vara riktade till dig, var vänlig [[Special:Userlogin|logga in]] så du undviker förväxling med andra anonyma användare i framtiden.'' ",
 "noarticletext"         => "(Det finns för tillfället ingen text på den här sidan.)",
 "updated"               => "(Uppdaterad)",
 "note"                  => "<strong>Notera:</strong> ",
@@ -327,17 +347,21 @@ Du måste infoga dina ändringar i den existerande texten.
 "editingold"            => "<strong>VARNING: Du redigerar en gammal version
 av denna sida. Om du sparar den, kommer alla ändringar på denns sida föregående revison att bli överskrivna.</strong>\n",
 "yourdiff"              => "Skillnader",
-"copyrightwarning"      => "Lägg märke till att alla bidrag till Wikipedia är
+"copyrightwarning"      => "Observera att alla bidrag till Wikipedia är
 att betrakta som utgivna under GNU Free Documentation License
 (se $1 för detaljer).
 Om du inte vill ha din text redigerad och kopierad efter andras gottfinnade så skall du inte skriva någon text här.<br>
-Du lovar oss också att du skrev texten själv, eller kopierade från en
-public domain eller liknande fri resurs.
+Du lovar oss också att du skrev texten själv, eller kopierade från public domain eller liknande fri resurs.<BR>
+<strong>LÄGG ALDRIG UT UPPHOVSRÄTTSSKYDDAT MATERIAL HÄR UTAN FÖRFATTARENS TILLÅTELSE!</strong>",
 
-<strong>
-LÄGG ALDRIG UT MATERIAL HÄR SOM SKYDDAS AV UPPHOVSRÄTTSLAGEN UTAN FÖRFATTARENS TILLÅTELSE!
-</strong>", //'"
-
+"longpagewarning"       => "VARNING: Den här artikeln är $1 kilobytes lång; vissa äldre webbläsare
+kan ha problem med att redigera sidor som är omkring 32 kb eller större.
+Du kanske vill överväga att dela upp artikeln i mindre delar.",
+"readonlywarning"       => "VARNING: Databasen är tillfälligt låst för underhåll. Du kommer inte att kunna spara 
+dina ändringar just nu. För säkerhets skull kanske du vill överväga att kopiera över texten till din egen dator tills
+databasen är upplåst igen.",
+"protectedpagewarning"  => "VARNING:  Den här sidan har låsts så att bara administratörer kan redigera den. 
+Försäkra dig om att du följer rekommendationerna för <a href='/wiki/Wikipedia:Skyddade sidor'>skyddade sidor</a>",
 
 // History pages
 //
@@ -347,7 +371,7 @@ LÄGG ALDRIG UT MATERIAL HÄR SOM SKYDDAS AV UPPHOVSRÄTTSLAGEN UTAN FÖRFATTARENS T
 "revnotfoundtext"       => "Den gamla versionen av den sida du frågade efter kan inte hittas. Kontrollera den URL du använde för att nå denna sida.\n",
 "loadhist"              => "Läser sidans versioner",
 "currentrev"            => "Nuvarande version",
-"revisionasof"          => "Versionen från",
+"revisionasof"          => "Versionen från $1",
 "cur"                   => "nuvarande",
 "next"                  => "nästa",
 "last"                  => "föregående",
@@ -380,7 +404,7 @@ och texter i $3 artiklar.",
 "textmatches"           => "Artikel texter som överensstämmer med sökordet",
 "notextmatches"         => "Ingen artikel texter överensstämmer med sökordet",
 "prevn"                 => "förra $1",
-"nextn"                 => "nästa $2",
+"nextn"                 => "nästa $1",
 
 "viewprevnext"          => "Om ($1) ($2) ($3).",
 "showingresults"        => "Nedan visas <b>$1</b> resultat som startar med nummer <b>$2</b>.",
@@ -428,33 +452,17 @@ serverns klocka (UTC).
 Den blir automatiskt inställd efter svensk tid eller skulle man till exempel för svensk vintertid, endast ha \"1\" (och \"2\" när vi har sommartid).",
 "localtime"             => "Lokal tid",
 "timezoneoffset"        => "Utjämna",
-"emailflag"             => "Stoppa andra användare att skicka e-post till dig",
+"emailflag"             => "Hindra andra användare från att skicka e-post till dig",
 
 // Recent changes
 //
 "recentchanges"         => "Senaste ändringarna",
-"recentchangestext"     => "Se de senaste redigerade sidorna i Wikipedia på denna 
-sida.
-
-[[Wikipedia:Välkommen|Välkommen]]!
-Vänligen läs dessa sidor: [[wikipedia:FAQ|Ofta ställda 
-frågor]], [[Wikipedia:Policy|policy]]
-
-(särskilt [[wikipedia:Namngivning|namngivning]],
-[[wikipedia:Skriv Wikipedia ifrån en neutral synpunkt|Skriv Wikipedia ifrån en neutral synpunkt]],
-och [[wikipedia:Mest vanliga nybörjarfel på Wikipedia|Mest vanliga nybörjarfel på Wikipedia]]).
-
-Det är mycket viktigt att du inte lägger material på 
-Wikipedia som andra äger upphovsrätten till. De rättsliga konsekvenserna kan bli 
-mycket kostbara och skadligt för projektet, så vänligen låt bli, om du är osäker.
-Se också [http://meta.wikipedia.org/wiki/Special:Recentchanges 
-recent meta discussion] (oftast på engelska).",
+"recentchangestext"     => "Se de senaste redigerade sidorna i Wikipedia på denna sida.",
 "rcloaderr"             => "Läser senaste redigerade sidor",
-"rcnote"                => "Nedanför är de senaste <strong>$1</strong> ändringarna under de 
-sista <strong>$2</strong> dagarna.",
+"rcnote"                => "Nedanför är de senaste <strong>$1</strong> ändringarna under de sista <strong>$2</strong> dagarna.",
 "rcnotefrom"            => "Nedanför är ändringarna från <b>$2</b> till <b>$1</b> visade.",
 "rclistfrom"            => "Visa nya ändringar från och med $1",
-"rclinks"               => "Visa de senaste $1 ändringarna under de senaste $2 timmarna / $3 dagarna",
+"rclinks"               => "Visa de senaste $1 ändringarna under de senaste $2 dagarna",
 // "rclinks"             => "Visa de senaste $1 ändringarna under de senaste $2 dagarna",
 "rchide"                => "i $4 form; $1 mindre ändringar; $2 andra namnrum; $3 mer än en redigering.",
 "diff"                  => "skillnad",
@@ -680,6 +688,9 @@ och sidan kommer att markeras med <b>fet stil</b> i <a href=\"" .
 //
 "deletepage"            => "Ta bort sida",
 "confirm"               => "Bekräfta",
+"excontent"             => "före radering:",
+"exbeforeblank"         => "före tömning:",
+"exblank"               => "sidan var tom",
 "confirmdelete"         => "Bekräfta borttagning",
 "deletesub"             => "(Tar bort \"$1\")",
 "historywarning"        => "Varning: Sidan du håller på att ta bort har tidigare versioner: ",
@@ -716,13 +727,14 @@ Om en ny sida med samma namn har blivit skapad sedan borttagningen så kommer den
 hamna i den äldre versionshanteraren och den senaste versionen av sidan kommer inte blir automatiskt ersatt.",
 "undeleterevision"      => "Tog bort version $1",
 "undeletebtn"           => "Återställd!",
-"undeletedarticle"      => "återställd \"$1\"",
+"undeletearticle"      => "återställd \"$1\"",
 "undeletedtext"         => "Artikeln [[$1]] har blivit återställd
 Se [[Wikipedia:Borttagningslogg]] för en lista över nyligen gjorda borttagningar och återställningar",
 
 // Contributions
 //
 "contributions"         => "Användarbidrag",
+"mycontris"             => "Mina bidrag",
 "contribsub"            => "För $1",
 "nocontribs"            => "Inga ändringar var funna som motsvarar dessa kriterier",
 "ucnote"                => "Nedan visas dennes användares senaste <b>$1</b> ändringar, under de senaste <b>$2</b> dagarna.",
@@ -800,14 +812,13 @@ Detta belastar ofta servern hårt, så använd denna funktion med omtanke.",
 // Move page
 //
 "movepage"              => "Flytta sida",
+"movearticle"           => "Flytta sida",
 "movepagetext"          => "Formuläret nedan byter namn på sidan och flyttar hela dess historia till det nya namnet. Den gamla sidan blir en omdirigeringssida till den nya. Länkar till den gamla sidan kommer inte att ändras. Om det finns en diskussionssida kommer den inte att flyttas. <b>OBS!</b> Detta kan vara en drastisk ändring på en populär sida; försäkra dig om att du har förstått konsekvenserna innan du fortsätter.",
-"movepagetalktext" => "Den tillhörande diskussionssidan kommer att flyttas med till det nya namnet '''såvida inte:'''
-* Du flyttar sidan till en annan namnrymd (namnrymden är det som står före kolonet i vissa titlar, exempelvis i "Wikipedia:Nyheter").
+"movepagetalktext"	=> "Den tillhörande diskussionssidan kommer att flyttas med till det nya namnet '''såvida inte:'''
+* Du flyttar sidan till en annan namnrymd (namnrymden är det som står före kolonet i vissa titlar, exempelvis i \"Wikipedia:Nyheter\").
 * Det redan finns en diskussionssida för namnet du flyttar till.
 * Du kan även hindra flytt av diskussionssida genom att kryssa i rutan nedan.
 I dessa fall får du flytta diskussionssidan manuellt, om du vill att den ska följa med.",
-
-"movearticle"           => "Flytta sida",
 "movenologin"           => "Ej inloggad",
 "movenologintext"       => "Du måste vara registrerad användare och ha <a href=\"" .
   wfLocalUrl( "Speciel:Userlogin" ) . "\">loggat in</a>
