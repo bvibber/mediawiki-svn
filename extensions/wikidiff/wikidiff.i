f31bdc114cccfc2dbfe9891ef3d67b4193be67f7
(no author) <(no author)@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-09-23 18:43:35 +0000

    This commit was manufactured by cvs2svn to create branch 'dammit'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/dammit@5439 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/wikidiff/wikidiff.i
+++
@@ -1,5 +0,0 @@
-%module wikidiff
-
-%inline {
- 	const char *wikidiff_do_diff(const char *text1, const char *text2, int num_lines_context);
-}
--- phase3/maintenance/archives/patch-image_name_unique.sql
+++
@@ -1,6 +0,0 @@
--- Make the image name index unique
-
-ALTER TABLE image DROP INDEX img_name;
-
-ALTER TABLE image
-  ADD UNIQUE INDEX img_name (img_name);
