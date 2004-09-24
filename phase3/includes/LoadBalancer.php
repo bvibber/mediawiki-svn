15f431b415aa8309d5749c2997f05145f4146a46
kateturner <kateturner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-09-24 06:19:48 +0000

    syntax
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/REL1_3A@5458 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/LoadBalancer.php
+++ phase3/includes/LoadBalancer.php
@@ -108,7 +108,7 @@ class LoadBalancer {
 							unset( $loads[$i] );
 						}
 					}
-				} while ( $i !== false && ( $conn === false || !$conn->isOpen() );
+				} while ( $i !== false && ( $conn === false || !$conn->isOpen() ) );
 				if ( $conn !== false && $conn->isOpen() ) {
 					$this->mReadIndex = $i;
 				}
