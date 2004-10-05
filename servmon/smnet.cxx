61c4edb1142159540136365636407e4b1ca6f5a7
(no author) <(no author)@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-10-05 04:22:36 +0000

    This commit was manufactured by cvs2svn to create tag 'merged_to_schema_work'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/merged_to_schema_work@5696 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- servmon/smnet.cxx
+++
@@ -1,9 +0,0 @@
-#include "smstdinc.hxx"
-#include "smnet.hxx"
-
-namespace smnet {
-
-std::map<int, int> bsd::refs;
-std::map<int, std::vector<u_char> > bsd::rdbufs;
-
-} // namespace smnet
--- servmon/smstdrt.cxx
+++
@@ -1,6 +0,0 @@
-template<class tt>
-struct cmd_show_version : handler<tt> {
-	void execute(comdat<tt> const& cd) {
-		cd.wrtln("servmon pre-release");
-	}
-};
