83e8b416565b43be0dd0bd7d036e619b07505240
timstarling <timstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-10-16 07:06:36 +0000

    History compression by concatenating revisions and then gzipping
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/SCHEMA_WORK@5940 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/HistoryBlob.php
+++ phase3/includes/HistoryBlob.php
@@ -10,21 +10,39 @@
  */
 class HistoryBlob
 {
-	function setMeta() {}
+	# setMeta and getMeta currently aren't used for anything, I just thought they might be useful in the future
+	# The meta value is a single string
+	function setMeta( $meta ) {}
+
+	# Gets the meta-value
 	function getMeta() {}
+
+	# Adds an item of text, returns a stub object which points to the item
+	# You must call setLocation() on the stub object before storing it to the database
 	function addItem() {}
-	function getItem() {}
+
+	# Get item by hash
+	function getItem( $hash ) {}
+	
+	# Set the "default text"
+	# This concept is an odd property of the current DB schema, whereby each text item has a revision
+	# associated with it. The default text is the text of the associated revision. There may, however, 
+	# be other revisions in the same object
+	function setText() {}
+
+	# Get default text. This is called from Article::getRevisionText()
+	function getText() {}
 }
 
 /**
  * The real object
  * @package MediaWiki
  */
-class ConcatenatedGzipHistoryBlob
+class ConcatenatedGzipHistoryBlob extends HistoryBlob
 {
-	/* private */ var $mVersion = 0, $mCompressed = false, $mItems = array();
+	/* private */ var $mVersion = 0, $mCompressed = false, $mItems = array(), $mDefaultHash = '';
 
-	function HistoryBlob() {
+	function ConcatenatedGzipHistoryBlob() {
 		if ( !function_exists( 'gzdeflate' ) ) {
 			die( "Need zlib support to read or write this kind of history object (ConcatenatedGzipHistoryBlob)\n" );
 		}
@@ -42,11 +60,15 @@ class ConcatenatedGzipHistoryBlob
 	
 	function addItem( $text ) {
 		$this->uncompress();
-		$this->mItems[md5($text)] = $text;
+		$hash = md5( $text );
+		$this->mItems[$hash] = $text;
+
+		$stub = new HistoryBlobStub( $hash );
+		return $stub;
 	}
 
 	function getItem( $hash ) {
-		$this->compress();
+		$this->uncompress();
 		return $this->mItems[$hash];
 	}
 
@@ -60,15 +82,71 @@ class ConcatenatedGzipHistoryBlob
 	function uncompress() { 
 		if ( $this->mCompressed ) {
 			$this->mItems = unserialize( gzinflate( $this->mItems ) );
+			$this->mCompressed = false;
 		}
 	}
 
+	function getText() {
+		$this->uncompress();
+		return $this->getItem( $this->mDefaultHash );
+	}
+	
+	function setText( $text ) {
+		$this->uncompress();
+		$stub = $this->addItem( $text );
+		$this->mDefaultHash = $stub->mHash;
+	}
+
 	function __sleep() {
-		compress();
+		$this->compress();
+		return array( 'mVersion', 'mCompressed', 'mItems', 'mDefaultHash' );
 	}
 
 	function __wakeup() {
-		uncompress();
+		$this->uncompress();
+	}
+
+	# Determines if this object is happy
+	function isHappy( $maxFactor, $factorThreshold ) {
+		if ( count( $this->mItems ) == 0 ) {
+			return true;
+		}
+
+		$this->uncompress();
+		$record = serialize( $this->mItems );
+		$size = strlen( $record );
+		$avgUncompressed = $size / count( $this->mItems );
+		$compressed = strlen( gzdeflate( $record ) );
+
+		if ( $compressed < $factorThreshold ) {
+			return true;
+		} else {
+			return $avgUncompressed * $maxFactor < $compressed;
+		}
+	}
+}
+
+class HistoryBlobStub
+{
+	var $mOldId, $mHash;
+
+	function HistoryBlobStub( $hash = '', $oldid = 0 ) {
+		$this->mHash = $hash;
+	}
+	
+	# Sets the location (old_id) of the main object to which this object points
+	function setLocation( $id ) {
+		$this->mOldId = $id;
+	}
+	
+	function getText() {
+		$dbr =& wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow( 'text', array( 'old_flags', 'old_text' ), array( 'old_id' => $this->mOldId ) );
+		if ( !$row || $row->old_flags != 'object' ) {
+			return false;
+		}
+		$obj = unserialize( $row->old_text );
+		return $obj->getItem( $this->mHash );
 	}
 }
 ?>
