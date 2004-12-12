2d4a76fb970e9b7cd6132e237fe305eab3fca83b
(no author) <(no author)@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-12-12 23:31:24 +0000

    This commit was manufactured by cvs2svn to create tag 'REL1_4beta3'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/REL1_4beta3@6662 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/zhdaemon/dict.h
+++
@@ -1,6 +0,0 @@
-#ifndef DICT_H
-#define DICT_H
-Tnode *loadSegmentationDictionary(const char *fname);
-Tnode *loadConversionDictionary(const char *fname);
-Tnode *loadAdditionalConversionDictionary(Tnode *tree, const char *fname);
-#endif
