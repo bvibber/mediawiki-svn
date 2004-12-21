4261643c1380012605ef5f3daeb2e4988c631b5c
kateturner <kateturner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2004-12-21 17:22:13 +0000

    show prev, next links, misc fixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@6738 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LuceneSearch.php
+++ extensions/LuceneSearch.php
@@ -15,7 +15,7 @@
 if (defined('MEDIAWIKI')) {
 $wgExtensionFunctions[] = "wfLuceneSearch";
 
-define('LS_PER_PAGE', 10);
+define('LS_PER_PAGE', 20);
 
 function wfLuceneSearch() {
 global $IP;
@@ -33,21 +33,103 @@ class LuceneSearch extends SpecialPage
 		$this->setHeaders();
 
 		$q = $wgRequest->getText( 'search' );
-		$ok = htmlspecialchars( wfMsg( "ok" ) );
-
 		$r = $this->doLuceneSearch($q);
 		$numresults = $r[0];
 		$results = $r[1];
-		$showresults = min(LS_PER_PAGE, count($results));
+
+		list($limit, $offset) = $wgRequest->getLimitOffset(LS_PER_PAGE, 'searchlimit');
+                if($numresults || $offset) {
+                        $prevnext = wfViewPrevNext($offset, $limit,
+                                'Special:Search',
+                                wfArrayToCGI(
+                                        array('search' => $q)));
+                }
+
+		$showresults = min($limit, count($results)-$numresults);
 		$wgOut->setSubtitle(wfMsg('searchquery', htmlspecialchars($q)));
-		$wgOut->addWikiText(wfMsg('searchnumber', $showresults, $numresults));
-		$i = 0;
-		foreach ($results as $result) {
-			if ($i++ > LS_PER_PAGE)
-				break;
-			$wgOut->addWikiText("*[[:" . $result->getPrefixedText() . "]]<br/>");
+		$i = $offset;
+		$contextWords = join('|', split(" ", $q));
+                if ($numresults >= $limit)
+                        $top = wfShowingResults($offset, $limit);
+                else
+                        $top = wfShowingResultsNum($offset, $limit, $numresults);
+		$wgOut->addHTML($prevnext . "<hr/>");
+		$out = "<ol start=$offset>";
+		$chunks = array_chunk($results, $limit);
+		$numchunks = $numresults / $limit;
+		$whichchunk = ($offset / $limit);
+		foreach ($chunks[$whichchunk] as $result) {
+			$out .= $this->showHit($result, $contextWords);
 		}
+		$out .= "</ol>";
+		$wgOut->addWikiText(wfMsg('searchresulttext'));
+		$wgOut->addHTML($top . $out);
+		$wgOut->addHTML("<hr/>" . $prevnext);
+		$wgOut->setRobotpolicy('noindex,nofollow');
 	}
+
+        function showHit($t, $terms) {
+                $fname = 'LuceneSearch::showHit';
+                wfProfileIn( $fname );
+                global $wgUser, $wgContLang;
+
+                if(is_null($t)) {
+                        wfProfileOut( $fname );
+                        return "<!-- Broken link in search result -->\n";
+                }
+                $sk =& $wgUser->getSkin();
+
+                $contextlines = $wgUser->getOption( 'contextlines' );
+                if ( '' == $contextlines ) 
+			$contextlines = 5;
+                $contextchars = $wgUser->getOption( 'contextchars' );
+                if ( '' == $contextchars ) 
+			$contextchars = 50;
+
+                $link = $sk->makeKnownLinkObj($t, '');
+		$rev = Revision::newFromTitle($t);
+		if ($rev === null)
+			return "<b>Broken link in search results: ".$t->getPrefixedText()."</b>";
+
+		$text = $rev->getText();
+                $size = wfMsg('nbytes', strlen($text));
+
+                $lines = explode("\n", $text);
+
+                $max = IntVal( $contextchars ) + 1;
+                $pat1 = "/(.*)($terms)(.{0,$max})/i";
+
+                $lineno = 0;
+
+                $extract = '';
+                wfProfileIn("$fname-extract");
+                foreach ($lines as $line) {
+                        if (0 == $contextlines) 
+                                break;
+                        ++$lineno;
+                        if (!preg_match($pat1, $line, $m))
+                                continue;
+                        --$contextlines;
+                        $pre = $wgContLang->truncate($m[1], -$contextchars, '...');
+
+                        if (count($m) < 3)
+                                $post = '';
+                        else
+                                $post = $wgContLang->truncate($m[3], $contextchars, '...');
+
+                        $found = $m[2];
+
+                        $line = htmlspecialchars($pre . $found . $post);
+                        $pat2 = '/(' . $terms . ")/i";
+                        $line = preg_replace($pat2,
+                          "<span class='searchmatch'>\\1</span>", $line);
+
+                        $extract .= "<br /><small>{$lineno}: {$line}</small>\n";
+                }
+                wfProfileOut( "$fname-extract" );
+                wfProfileOut( $fname );
+                return "<li>{$link} ({$size}){$extract}</li>\n";
+        }
 
 	function doLuceneSearch( $query ) {
 		global $wgLuceneHost, $wgLucenePort;
@@ -57,6 +139,7 @@ class LuceneSearch extends SpecialPage
 		$results = array();
 
 		$numresults = @socket_read($sock, 1024, PHP_NORMAL_READ);
+		wfDebug("total [$numresults] hits\n");
 		if ($numresults === FALSE)
 			return array();
 		$numresults = chop($numresults);
@@ -64,6 +147,7 @@ class LuceneSearch extends SpecialPage
 		while (($result = @socket_read($sock, 1024, PHP_NORMAL_READ)) != FALSE) {
 			$result = chop($result);
 			list($namespace, $title) = split(" ", $result);
+			wfDebug("result: $namespace $title\n");
 			$fulltitle = Title::makeTitle($namespace, $title);
 			$results[] = $fulltitle;
 		}
@@ -74,7 +158,6 @@ class LuceneSearch extends SpecialPage
 
 global $wgMessageCache;
 SpecialPage::addPage( new LuceneSearch );
-$wgMessageCache->addMessage("searchnumber", "Showing \$1 of \$2 results\n\n");
 
 } # End of extension function
 } # End of invocation guard
