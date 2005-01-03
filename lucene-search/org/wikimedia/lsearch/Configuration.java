41178a0663c49a026315f52f25abb7dd658aa1d1
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-01-03 20:39:19 +0000

    Rearrange things so project can be built, run, and debugged directly in Eclipse.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@6911 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- lucene-search/org/wikimedia/lsearch/Configuration.java
+++ lucene-search/org/wikimedia/lsearch/Configuration.java
@@ -28,7 +28,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Iterator;
 
 /**
  * @author Kate Turner
