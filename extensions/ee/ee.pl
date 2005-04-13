97814f533a1dc8ec10f7b0215a3217a24e8de3db
eloquence <eloquence@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-04-13 04:04:54 +0000

    hmz, moving this into main script avoids mucking with includes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@8302 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ee/ee.pl
+++ extensions/ee/ee.pl
@@ -10,13 +10,14 @@
 #
 # To do: Edit conflicts
 #
-require("language.pl");
-
 use Config::IniFiles;  # Module for config files in .ini syntax
 use LWP::UserAgent;    # Web agent module for retrieving and posting HTTP data
 use URI::Escape;       # Urlencode functions
 use Gtk2 '-init';      # Graphical user interface, requires GTK2 libraries
 use Encode qw(encode); # UTF-8/iso8859-1 encoding
+
+# Load interface messages
+initmsg();
 
 # By default, config will be searched for in your Unix home directory 
 # (e.g. ~/.ee-helper/ee.ini). Change path of the configuration file if needed!
@@ -187,6 +188,7 @@ if($type eq "Edit file") {
 	
 }
 
+
 # Search for extension-associated application
 @extensionlists=$cfg->Parameters("Editors");
 foreach $extensionlist(@extensionlists) {
@@ -201,7 +203,7 @@ foreach $extensionlist(@extensionlists) {
 # In most cases, we'll want to run the GUI for managing saves & previews,
 # and run the external editor application.
 if($type ne "Diff text") {
-
+		
  	system("$app $tempdir/$filename &");
 	makegui();
 
@@ -370,7 +372,117 @@ sub save {
 }
 sub cancel {
 
-	print "Quitting!\n";
 	Gtk2->main_quit;
+
+}
+
+sub _{
+	my $message=shift;
+	@subst=@_;	
+	my $suffix;
+	if($LANGUAGE ne "en") { $suffix = "_".$LANGUAGE; }
+	$msg=$messages{$message.$suffix};
+	foreach $substi(@subst) {
+		$msg=~s/____/$substi/s;	
+	}
+	return $msg;
+}
+
+sub initmsg {
+
+%messages=(
+
+notemppath=>
+"No path for temporary files specified. Please edit ____ 
+and add an entry like this:
+
+[Settings]
+Temp Path=/tmp\n",
+notemppath_de=>
+"Kein Pfad für temporäre Dateien festgelegt. 
+Bitte bearbeiten Sie ____
+und fügen Sie einen Eintrag wie folgt ein:
+
+[Settings]
+Temp Path=/tmp\n",
+
+nocontrolfile=>
+"No control file specified.
+Syntax: perl ee.pl <control file>\n",
+nocontrolfile_de=>
+"Keine Kontrolldatei angegeben.
+Syntax: perl ee.pl <Kontrolldatei>\n",
+
+twofordiff=>
+"Process is diff, but no second URL contained in control file\n",
+twofordiff_de=>
+"Dateien sollen verglichen werden, Kontrolldatei enthält aber nur eine URL.",
+
+nodifftool=>
+"Process is diff, but ee.ini does not contain a 'Diff=' definition line
+in the [Settings] section where the diff tool is defined.\n",
+nodifftool_de=>
+"Dateien sollen verglichen werden, ee.ini enthält aber keine
+'Diff='-Zeile im Abschnitt Settings, in der das Diff-Werkzeug 
+definiert wird.\n",
+
+unknownprocess=>
+"The process type defined in the input file (Type= in the [Process] section) 
+is not known to this implementation of the External Editor interface. Perhaps 
+you need to upgrade to a newer version?\n",
+unknownprocess_de=>
+"Der in der Kontrolldatei definierte Prozesstyp (Type= im Abschnitt [Process])
+ist dieser Implementierung des application/x-external-editor-Interface nicht
+bekannt. Vielleicht müssen Sie Ihre Version des Skripts aktualisieren.\n",
+
+loginfailed=>
+"Could not login to 
+____ 
+with username '____' and password '____'.
+
+Make sure you have a definition for this website in your ee.ini, and that
+the 'URL match=' part of the site definition contains a string that is part
+of the URL above.\n",
+
+loginfailed_de=>
+"Anmeldung bei 
+____ 
+gescheitert. Benutzername: ____ Passwort: ____
+
+Stellen Sie sicher, dass Ihre ee.ini eine Definition für diese Website
+enthält, und in der 'URL match='-Zeile ein Text steht, der Bestandteil der
+obigen URL ist.\n",
+
+summary=>
+"Summary",
+summary_de=>
+"Zusammenfassung",
+
+save=>
+"Save",
+save_de=>
+"Speichern",
+
+savecont=>
+"Save and continue",
+savecont_de=>
+"Speichern und weiter",
+
+preview=>
+"Preview",
+preview_de=>
+"Vorschau",
+
+cancel=>
+"Cancel",
+cancel_de=>
+"Abbruch",
+
+entersummary=>
+"Enter edit summary",
+entersummary_de=>
+"Zusammenfassung eingeben",
+
+);
 
 }
