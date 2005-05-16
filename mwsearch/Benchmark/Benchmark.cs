352f363b437cf3aed1463bcfd572be7c7fe70105
vibber <vibber@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-05-16 09:01:45 +0000

    Fix up the benchmark to work with HTTP version, and add some cmdline options
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@9078 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mwsearch/Benchmark/Benchmark.cs
+++ mwsearch/Benchmark/Benchmark.cs
@@ -22,134 +22,145 @@
  * $Id$
  */
 
-using System;
-using System.Collections;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Web;
+namespace MediaWiki.Search.Benchmark {
+	using System;
+	using System.Collections;
+	using System.IO;
+	using System.Net;
+	using System.Net.Sockets;
+	using System.Text;
+	using System.Threading;
+	using System.Web;
 
-class Benchmark
-{
-	public static void Main(string[] args)
-	{
-		Benchmark bench = new Benchmark("127.0.0.1", 8123, "entest");
-		bench.runs = 100;
-		//bench.Run();
-		bench.RunSets(10);
-		bench.Report();
-	}
-	
-	private string host;
-	private ushort port;
-	private string database;
-	private int runs;
-	
-	/* Access these only in lock(times){} */
-	private ArrayList times;
-	private int totalRequests;
-	private int totalResults;
-	private TimeSpan totalTime;
-	/* -- */
-	
-	private int runningThreads = 0;
-	private readonly object threadlock = new object();
-	
-	private Benchmark(string host, ushort port, string database) {
-		this.host = host;
-		this.port = port;
-		this.database = database;
-		this.times = new ArrayList();
-	}
-	
-	private void RunSets(int threads) {
-		lock (threadlock) {
-			runningThreads = threads;
+	class Benchmark {
+		/**
+		 * Example: MWBenchmark --host 10.0.0.1 --database entest --threads 10 --count 1000
+		 */
+		public static void Main(string[] args) {
+			string host = "127.0.0.1";
+			ushort port = 8123;
+			string database = "entest";
+			string verb = "search";
+			int runs = 20;
+			int threads = 1;
+			
+			for(int i = 0; i < args.Length; i++) {
+				if (args[i].Equals("--host")) {
+					host = args[++i];
+				} else if (args[i].Equals("--port")) {
+					port = ushort.Parse(args[++i]);
+				} else if (args[i].Equals("--database")) {
+					database = args[++i];
+				} else if (args[i].Equals("--threads")) {
+					threads = int.Parse(args[++i]);
+				} else if (args[i].Equals("--count")) {
+					runs = int.Parse(args[++i]);
+				} else if (args[i].Equals("--verb")) {
+					verb = args[++i];
+				}
+			}
+			
+			Benchmark bench = new Benchmark(host, port, database, verb);
+			bench.RunSets(runs, threads);
+			bench.Report();
 		}
 		
-		for(int i = 0; i < threads; i++) {
-			ThreadPool.QueueUserWorkItem(Run);
+		private string host;
+		private ushort port;
+		private string database;
+		private string verb;
+		private int runs;
+		
+		/* Access these only in lock(times){} */
+		private ArrayList times;
+		private int totalRequests;
+		private int totalResults;
+		private TimeSpan totalTime;
+		/* -- */
+		
+		private int runningThreads = 0;
+		private readonly object threadlock = new object();
+		
+		private Benchmark(string host, ushort port, string database, string verb) {
+			this.host = host;
+			this.port = port;
+			this.database = database;
+			this.verb = verb;
+			this.times = new ArrayList();
 		}
-		// Wait for threads to clean up
-		lock (threadlock) {
-			while (runningThreads > 0) {
-				Monitor.Wait(threadlock);
-			}
-		}
-	}
-	
-	private void Run(object wtf) {
-		Console.WriteLine("Starting thread w/ " + runs + " runs ...");
-		Console.Out.Flush();
-		try {
-			for (int i = 0; i < runs; i++) {
-				Search(SampleTerms.Next);
-			}
-		} finally {
+		
+		private void RunSets(int runs, int threads) {
+			this.runs = runs;
 			lock (threadlock) {
-				--runningThreads;
-				Monitor.Pulse(threadlock);
+				runningThreads = threads;
+			}
+			
+			for(int i = 0; i < threads; i++) {
+				ThreadPool.QueueUserWorkItem(Run);
+			}
+			// Wait for threads to clean up
+			lock (threadlock) {
+				while (runningThreads > 0) {
+					Monitor.Wait(threadlock);
+				}
 			}
 		}
-	}
-	
-	private void Search(string term) {
-		DateTime start = DateTime.UtcNow;
 		
-		//Console.WriteLine("Connecting...");
-		TcpClient client = new TcpClient(host, port);
-		Stream stream = client.GetStream();
-		StreamReader reader = new StreamReader(stream);
-		StreamWriter writer = new StreamWriter(stream);
-		
-		//Console.WriteLine("Requesting...");
-		string encterm = HttpUtility.UrlEncode(term, Encoding.UTF8);
-		string req = database + "\n" +
-			"SEARCH\n" +
-			encterm + "\n";
-		//Console.Write(req);
-		writer.Write(req);
-		writer.Flush();
-		
-		//Console.WriteLine("Getting results...");
-		
-		string numResults = reader.ReadLine();
-		//Console.WriteLine(numResults);
-		
-		string remainder = reader.ReadToEnd();
-		string[] lines = remainder.Split('\n'); // last is empty, as \n is terminator
-		int numReceived = lines.Length - 1;
-		
-		// ...
-		reader.Close();
-		writer.Close();
-		stream.Close();
-		client.Close();
-		
-		TimeSpan delta = DateTime.UtcNow - start;
-		Console.WriteLine("[{0}] '{1}' received {2} of {3} lines ({4} chars) in {5}.",
-			Thread.CurrentThread.GetHashCode(),
-			encterm, numResults, numReceived, remainder.Length, delta);
-		
-		lock(times) {
-			times.Add(delta);
-			totalTime += delta;
-			totalResults += numReceived;
-			++totalRequests;
+		private void Run(object wtf) {
+			Console.WriteLine("Starting thread w/ " + runs + " runs ...");
+			Console.Out.Flush();
+			try {
+				for (int i = 0; i < runs; i++) {
+					Search(SampleTerms.Next);
+				}
+			} finally {
+				lock (threadlock) {
+					--runningThreads;
+					Monitor.Pulse(threadlock);
+				}
+			}
 		}
-	}
-	
-	void Report() {
-		Console.WriteLine("Made {0} total requests", totalRequests);
-		Console.WriteLine("Received {0} total result lines", totalResults);
-		Console.WriteLine("Spent {0} total on all requests", totalTime);
-		Console.WriteLine("Average time per request: {0}", new TimeSpan(totalTime.Ticks / totalRequests));
-		Console.WriteLine("Average time per result:  {0}", new TimeSpan(totalTime.Ticks / totalResults));
 		
-		times.Sort();
-		Console.WriteLine("Fastest request: {0}", times[0]);
-		Console.WriteLine("Slowest request: {0}", times[times.Count-1]); 
+		private void Search(string term) {
+			DateTime start = DateTime.UtcNow;
+			
+			string encterm = HttpUtility.UrlEncode(term, Encoding.UTF8);
+			string req = "http://" + host + ":" + port + "/" + verb + "/" + database + "/" + encterm;
+			WebRequest web = WebRequest.Create(req);
+			WebResponse response = web.GetResponse();
+			StreamReader reader = new StreamReader(response.GetResponseStream());
+
+			string numResults = reader.ReadLine();
+			string remainder = reader.ReadToEnd();
+			reader.Close();
+			response.Close();
+			
+			string[] lines = remainder.Split('\n'); // last is empty, as \n is terminator
+			int numReceived = lines.Length - 1;
+			
+			TimeSpan delta = DateTime.UtcNow - start;
+			Console.WriteLine("[{0}] '{1}' received {2} of {3} lines ({4} chars) in {5}.",
+				Thread.CurrentThread.GetHashCode(),
+				encterm, numReceived, numResults, remainder.Length, delta);
+			
+			lock(times) {
+				times.Add(delta);
+				totalTime += delta;
+				totalResults += numReceived;
+				++totalRequests;
+			}
+		}
+		
+		void Report() {
+			Console.WriteLine("Made {0} total requests", totalRequests);
+			Console.WriteLine("Received {0} total result lines", totalResults);
+			Console.WriteLine("Spent {0} total on all requests", totalTime);
+			Console.WriteLine("Average time per request: {0}", new TimeSpan(totalTime.Ticks / totalRequests));
+			Console.WriteLine("Average time per result:  {0}", new TimeSpan(totalTime.Ticks / totalResults));
+			
+			times.Sort();
+			Console.WriteLine("Fastest request: {0}", times[0]);
+			Console.WriteLine("Slowest request: {0}", times[times.Count-1]); 
+		}
 	}
 }
