3d5a7dd403dbf8e57088ff6495031777b74ba8d2
timstarling <timstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-06-25 15:57:58 +0000

    delete button
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@9653 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/InspectCache.php
+++ extensions/InspectCache.php
@@ -24,6 +24,7 @@ class InspectCache extends SpecialPage
 		$this->setHeaders();
 
 		$key = $wgRequest->getVal( 'key' );
+		$delete = $wgRequest->getBool( 'delete' ) && $wgRequest->wasPosted();
 		$encQ = htmlspecialchars( $key );
 		$action = $wgTitle->escapeLocalUrl();
 		$ok = htmlspecialchars( wfMsg( "ok" ) );
@@ -31,13 +32,20 @@ class InspectCache extends SpecialPage
 		$wgOut->addHTML( <<<END
 <form name="ucf" method="post" action="$action">
 <input type="text" size="80" name="key" value="$encQ"/><br />
-<input type="submit" name="submit" value="$ok" /><br /><br />
+<input type="submit" name="submit" value="Get" />
+<input type="submit" name="delete" value="Delete" /><br /><br />
 </form>
 END
 );
 
-		if ( !is_null( $key ) ) {
-			$value = var_export( $wgMemc->get( $key ), true );
+		if ( $delete && !is_null( $key ) ) {
+			$wgMemc->delete( $key );
+			$wgOut->addHTML( "Deleted $key\n" );
+		} else if ( !is_null( $key ) ) {
+			$value = $wgMemc->get( $key );
+			if ( !is_string( $value ) ) {
+				$value = var_export( $value, true );
+			}
 			$wgOut->addHTML( "<pre>" . htmlspecialchars( $value ) . "</pre>" );
 		}
 	}
