5816d695849b05af85d5a079e955a089ccab96d2
ilyah <ilyah@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-06-27 04:01:55 +0000

    Updated to current table format added a number of features (notcategory, suppresserrors, ordermethod, order, mode)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@9715 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/intersection/DynamicPageList.php
+++ extensions/intersection/DynamicPageList.php
@@ -1,73 +1,84 @@
 <?php
-/*
-
- Purpose:       outputs a bulleted list of most recent
-                items residing in a category, or a union
-                of several categories.
-
-
- Contributors: n:en:User:IlyaHaykinson n:en:User:Amgine
- http://en.wikinews.org/wiki/User:Amgine
- http://en.wikinews.org/wiki/User:IlyaHaykinson
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or 
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- http://www.gnu.org/copyleft/gpl.html
-
- Current feature request list
-     1. Unset cached of calling page
-     2. Alternative formatting (not just unordered list)
-     3. Configurable sort order, ascending/descending
-     4. RSS feed output?
-
- To install, add following to LocalSettings.php
-   include("extensions/intersection/DynamicPageList.php");
-
+/*                                                                                                                                  
+                                                                                                                                    
+ Purpose:       outputs a bulleted list of most recent                                                                              
+                items residing in a category, or a union                                                                            
+                of several categories.                                                                                              
+                                                                                                                                    
+                                                                                                                                    
+ Contributors: n:en:User:IlyaHaykinson n:en:User:Amgine                                                                             
+ http://en.wikinews.org/wiki/User:Amgine                                                                                            
+ http://en.wikinews.org/wiki/User:IlyaHaykinson                                                                                     
+                                                                                                                                    
+ This program is free software; you can redistribute it and/or modify                                                               
+ it under the terms of the GNU General Public License as published by                                                               
+ the Free Software Foundation; either version 2 of the License, or                                                                  
+ (at your option) any later version.                                                                                                
+                                                                                                                                    
+ This program is distributed in the hope that it will be useful,                                                                    
+ but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                     
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                                                                       
+ GNU General Public License for more details.                                                                                       
+                                                                                                                                    
+ You should have received a copy of the GNU General Public License along                                                            
+ with this program; if not, write to the Free Software Foundation, Inc.,                                                            
+ 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.                                                                           
+ http://www.gnu.org/copyleft/gpl.html                                                                                               
+                                                                                                                                    
+ Current feature request list                                                                                                       
+     1. Unset cached of calling page                                                                                                
+     2. Alternative formatting (not just unordered list)                                                                            
+     3. Configurable sort order, ascending/descending                                                                               
+     4. RSS feed output?                                                                                                            
+                                                                                                                                    
+ To install, add following to LocalSettings.php                                                                                     
+   include("extensions/intersection/DynamicPageList.php");                                                                          
+                                                                                                                                    
 */
 
-$wgDLPminCategories = 1;                // Minimum number of categories to look for
-$wgDLPmaxCategories = 3;                // Maximum number of categories to look for
-$wgDLPMinResultCount = 1;               // Minimum number of results to allow
-$wgDLPMaxResultCount = 50;              // Maximum number of results to allow
-$wgDLPAllowUnlimitedResults = true;     // Allow unlimited results
-$wgDLPAllowUnlimitedCategories = false; // Allow unlimited categories
+$wgDLPminCategories = 1;                // Minimum number of categories to look for                                                 
+$wgDLPmaxCategories = 4;                // Maximum number of categories to look for 
+$wgDLPMinResultCount = 1;               // Minimum number of results to allow                                                       
+$wgDLPMaxResultCount = 50;              // Maximum number of results to allow                                                       
+$wgDLPAllowUnlimitedResults = true;     // Allow unlimited results                                                                  
+$wgDLPAllowUnlimitedCategories = false; // Allow unlimited categories                                                               
 
 
 $wgExtensionFunctions[] = "wfDynamicPageList";
 
- 
+
 function wfDynamicPageList() {
     global $wgParser, $wgMessageCache;
-   
+
     $wgMessageCache->addMessages( array(
-					'dynamicpagelist_toomanycats' => 'DynamicPageList: Too many categories!',
-					'dynamicpagelist_toofewcats' => 'DynamicPageList: Too few categories!',
-					'dynamicpagelist_noresults' => 'DynamicPageList: No results!',
-					)
-				  );
+                                        'dynamicpagelist_toomanycats' => 'DynamicPageList: Too many categories!',
+                                        'dynamicpagelist_toofewcats' => 'DynamicPageList: Too few categories!',
+                                        'dynamicpagelist_noresults' => 'DynamicPageList: No results!',
+                                        'dynamicpagelist_noincludecats' => 'DynamicPageList: You need to include at least one category!',
+                                        )
+                                  );
 
     $wgParser->setHook( "DynamicPageList", "DynamicPageList" );
 }
- 
-// The callback function for converting the input text to HTML output
+
+// The callback function for converting the input text to HTML output                                                               
 function DynamicPageList( $input ) {
     global $wgUser;
     global $wgDLPminCategories, $wgDLPmaxCategories,$wgDLPMinResultCount, $wgDLPMaxResultCount;
     global $wgDLPAllowUnlimitedResults, $wgDLPAllowUnlimitedCategories;
-     
+
     $aParams = array();
     $bCountSet = false;
+
+    $sStartList = '<ul>';
+    $sEndList = '</ul>';
+    $sStartItem = '<li>';
+    $sEndItem = '</li>';
+    
+    $sOrderMethod = 'categoryadd';
+    $sOrder = 'descending';
+
+    $bSuppressErrors = false;
 
     $aParams = explode("\n", $input);
 
@@ -83,23 +94,106 @@ function DynamicPageList( $input ) {
         $title = Title::newFromText( $sArg );
         if( is_null( $title ) )
           continue;
-        $aCategories[] = $title; 
+        $aCategories[] = $title;
+      }
+      if ($sType == 'notcategory')
+      {
+        $title = Title::newFromText( $sArg );
+        if( is_null( $title ) )
+          continue;
+        $aExcludeCategories[] = $title;
       }
       else if ('count' == $sType)
       {
-        //ensure that $iCount is a number;
+        //ensure that $iCount is a number;                                                                                          
         $iCount = IntVal( $sArg );
         $bCountSet = true;
+      }
+      else if ('mode' == $sType)
+      {
+        switch ($sArg)
+        {
+        case 'none':
+          $sStartList = '';
+          $sEndList = '';
+          $sStartItem = '';
+          $sEndItem = '<br/>';
+          break;
+        case 'ordered':
+          $sStartList = '<ol>';
+          $sEndList = '</ol>';
+          $sStartItem = '<li>';
+          $sEndItem = '</li>';
+          break;
+        case 'unordered':
+        default:
+          $sStartList = '<ul>';
+          $sEndList = '</ul>';
+          $sStartItem = '<li>';
+          $sEndItem = '</li>';
+          break;
+        }
+      }
+      else if ('order' == $sType)
+      {
+        switch ($sArg)
+        {
+        case 'ascending':
+          $sOrder = 'ascending';
+          break;
+        case 'descending':
+        default:
+          $sOrder = 'descending';
+          break;
+        }
+      }
+      else if ('ordermethod' == $sType)
+      {
+        switch ($sArg)
+        {
+        case 'lastedit':
+          $sOrderMethod = 'lastedit';
+          break;
+        case 'categoryadd':
+        default:
+          $sOrderMethod = 'categoryadd';
+          break;
+        }
+      }
+      else if ('suppresserrors' == $sType)
+      {
+        if ('true' == $sArg)
+          $bSuppressErrors = true;
       }
     }
 
     $iCatCount = count($aCategories);
+    $iExcludeCatCount = count($aExcludeCategories);
+    $iTotalCatCount = $iCatCount + $iExcludeCatCount;
 
-    if ($iCatCount < $wgDLPminCategories)
-      return htmlspecialchars( wfMsg( 'dynamicpagelist_toofewcats' ) ); // "!!too few categories!!";
+    if ($iCatCount < 1)
+    {
+      if (false == $bSuppressErrors)
+        return htmlspecialchars( wfMsg( 'dynamicpagelist_noincludecats' ) ); // "!!no included categories!!";                       
+      else
+        return '';
+    }
 
-    if ( $iCatCount > $wgDLPmaxCategories && !$wgDLPAllowUnlimitedCategories )
-      return htmlspecialchars( wfMsg( 'dynamicpagelist_toomanycats' ) ); // "!!too many categories!!";
+    if ($iTotalCatCount < $wgDLPminCategories)
+    {
+      if (false == $bSuppressErrors)
+        return htmlspecialchars( wfMsg( 'dynamicpagelist_toofewcats' ) ); // "!!too few categories!!";                              
+      else
+        return '';
+    }
+
+    if ( $iTotalCatCount > $wgDLPmaxCategories && !$wgDLPAllowUnlimitedCategories )
+    {
+      if (false == $bSuppressErrors)
+        return htmlspecialchars( wfMsg( 'dynamicpagelist_toomanycats' ) ); // "!!too many categories!!";                            
+      else
+        return '';
+    }
 
     if ($bCountSet)
     {
@@ -118,52 +212,80 @@ function DynamicPageList( $input ) {
     }
 
 
-    //build the SQL query
+    //build the SQL query                                                                                                           
     $dbr =& wfGetDB( DB_SLAVE );
-    $cur = $dbr->tableName( 'cur' );
+    $sPageTable = $dbr->tableName( 'page' );
     $categorylinks = $dbr->tableName( 'categorylinks' );
-    $sSqlSelectFrom = "SELECT cur_namespace, cur_title FROM $cur";
-    $sSqlWhere = ' WHERE cur_id = c1.cl_from ';
+    $sSqlSelectFrom = "SELECT page_namespace, page_title FROM $sPageTable";
+    $sSqlWhere = ' WHERE 1=1 ';
+
+    $iCurrentTableNumber = 0;
 
     for ($i = 0; $i < $iCatCount; $i++) {
-      $sSqlSelectFrom .= ", $categorylinks AS c" . ($i+1);
-
-      if ($i > 0)
-        $sSqlWhere .= ' AND c1.cl_from = c'.($i+1).'.cl_from';
-      $sSqlWhere .= ' AND c'.($i+1).'.cl_to = ' .
+      $sSqlSelectFrom .= " INNER JOIN $categorylinks AS c" . ($iCurrentTableNumber+1);
+      $sSqlSelectFrom .= ' ON page_id = c'.($iCurrentTableNumber+1).'.cl_from';
+      $sSqlSelectFrom .= ' AND c'.($iCurrentTableNumber+1).'.cl_to='.
         $dbr->addQuotes( $aCategories[$i]->getDbKey() );
+      $iCurrentTableNumber++;
     }
 
-    $sSqlWhere .= ' ORDER BY cur_timestamp DESC';
-    
+    for ($i = 0; $i < $iExcludeCatCount; $i++) {
+      $sSqlSelectFrom .= " LEFT OUTER JOIN $categorylinks AS c" . ($iCurrentTableNumber+1);
+      $sSqlSelectFrom .= ' ON page_id = c'.($iCurrentTableNumber+1).'.cl_from';
+      $sSqlSelectFrom .= ' AND c'.($iCurrentTableNumber+1).'.cl_to='.
+        $dbr->addQuotes( $aExcludeCategories[$i]->getDbKey() );
+
+      $sSqlWhere .= ' AND c'.($iCurrentTableNumber+1).'.cl_to IS NULL';
+
+      $iCurrentTableNumber++;
+    }
+
+    if ('descending' == $sOrder)
+      $sSqlOrder = 'DESC';
+    else
+      $sSqlOrder = 'ASC';
+
+    if ('lastedit' == $sOrderMethod)
+      $sSqlWhere .= ' ORDER BY page_touched ';
+    else
+      $sSqlWhere .= ' ORDER BY c1.cl_timestamp ';
+
+    $sSqlWhere .= $sSqlOrder;
+
+
     if ($bCountSet)
     {
       $sSqlWhere .= ' LIMIT ' . $iCount;
     }
 
-    //DEBUG: output SQL query 
-    //$output = $sSqlSelectFrom . $sSqlWhere . "<br />";    
+    //DEBUG: output SQL query                                                                                                       
+    //$output .= 'QUERY: [' . $sSqlSelectFrom . $sSqlWhere . "]<br />";                                                             
 
-    // process the query
+    // process the query                                                                                                            
     $res = $dbr->query($sSqlSelectFrom . $sSqlWhere);
-	
+
     $sk =& $wgUser->getSkin();
 
-    if ($dbr->numRows( $res ) == 0) {
-      return htmlspecialchars( wfMsg( 'dynamicpagelist_noresults' ) );
-    }
-    
-    //start unordered list
-    $output = "<ul>\n";
-	
-    //process results of query, outputing equivalent of <li>[[Article]]</li> for each result
-    while ($row = $dbr->fetchObject( $res ) ) {
-      $title = Title::makeTitle( $row->cur_namespace, $row->cur_title);
-      $output .= '<li>' . $sk->makeKnownLinkObj($title) . '</li>' . "\n";
+    if ($dbr->numRows( $res ) == 0)
+    {
+      if (false == $bSuppressErrors)
+        return htmlspecialchars( wfMsg( 'dynamicpagelist_noresults' ) );
+      else
+        return '';
     }
 
-    //end unordered list
-    $output .= "</ul>\n";
+    //start unordered list                                                                                                          
+    $output .= $sStartList . "\n";
+
+    //process results of query, outputing equivalent of <li>[[Article]]</li> for each result,                                       
+    //or something similar if the list uses other startlist/endlist                                                                 
+    while ($row = $dbr->fetchObject( $res ) ) {
+      $title = Title::makeTitle( $row->page_namespace, $row->page_title);
+      $output .= $sStartItem . $sk->makeKnownLinkObj($title) . $sEndItem . "\n";
+    }
+
+    //end unordered list                                                                                                            
+    $output .= $sEndList . "\n";
 
     return $output;
 }
