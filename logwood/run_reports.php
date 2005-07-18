1922f013038c4d839134b53c0b3536545cd7a2bd
kateturner <kateturner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-07-18 06:09:57 +0000

    generate all urls report
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@10193 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- logwood/run_reports.php
+++ logwood/run_reports.php
@@ -3,41 +3,77 @@ ini_set("display_errors", 1);
 
 require_once('/etc/logwood-fe.conf');
 
-$dbh = mysql_connect($server, $username, $password);
-mysql_select_db($database, $dbh);
+$db = new mysqli($server, $username, $password, $database);
 
-mysql_query("DELETE FROM topurls", $dbh);
-mysql_query("DELETE FROM topagents", $dbh);
-mysql_query("DELETE FROM toprefs", $dbh);
+$stmt = $db->prepare("DELETE FROM topurls");
+$stmt->execute();
+$stmt = $db->prepare("DELETE FROM topagents");
+$stmt->execute();
+$stmt = $db->prepare("DELETE FROM toprefs");
+$stmt->execute();
 
-$res = mysql_query("SELECT si_id FROM sites", $dbh);
-while ($row = mysql_fetch_assoc($res)) {
+$insert_topurls = $db->prepare("
+	INSERT INTO topurls(tu_site, tu_url, tu_count)
+		SELECT si_id, ur_id, uc_count
+		FROM sites, url_id, url_count
+		WHERE si_id=? AND ur_site=si_id AND uc_url_id=ur_id
+		ORDER BY uc_count DESC LIMIT 500
+	");
+$qallurls = $db->prepare("
+	SELECT si_name, ur_path, uc_count
+	FROM sites, url_id, url_count
+	WHERE si_id=? AND ur_site=si_id AND uc_url_id=ur_id
+	ORDER BY uc_count DESC
+	");
+$insert_topagents = $db->prepare("
+	INSERT INTO topagents(ta_site, ta_agent, ta_count) 
+		SELECT si_id, ag_id, ac_count 
+		FROM sites, agent_ids, agent_count 
+		WHERE si_id=? AND ag_site=si_id AND ac_id=ag_id 
+		ORDER BY ac_count DESC LIMIT 500
+	");
+
+$insert_toprefs = $db->prepare("
+	INSERT INTO toprefs(tr_site, tr_ref, tr_count)
+		SELECT si_id, ref_ids.ref_id, ref_count
+		FROM sites, ref_ids, ref_count
+		WHERE si_id=? AND ref_site=si_id AND ref_count.ref_id=ref_ids.ref_id
+		ORDER BY ref_count DESC LIMIT 500
+	");
+
+$si_id = $si_name = false;
+$qsites = $db->prepare("SELECT si_id, si_name FROM sites");
+$qsites->bind_result($si_id, $si_name);
+$qsites->execute();
+$qsites->store_result();
+while ($qsites->fetch()) {
 	$site = $row["si_id"];
-	mysql_query("
-		INSERT INTO topurls(tu_site, tu_url, tu_count)
-			SELECT si_id, ur_id, uc_count
-			FROM sites, url_id, url_count
-			WHERE si_id=$site AND ur_site=si_id AND uc_url_id=ur_id
-			ORDER BY uc_count DESC LIMIT 500
-		", $dbh);
 
-	mysql_query("
-		INSERT INTO topagents(ta_site, ta_agent, ta_count) 
-			SELECT si_id, ag_id, ac_count 
-			FROM sites, agent_ids, agent_count 
-			WHERE si_id=$site AND ag_site=si_id AND ac_id=ag_id 
-			ORDER BY ac_count DESC LIMIT 500
-		", $dbh);
+	$insert_topurls->bind_param("i", $site);
+	$insert_topurls->execute();
 
-	mysql_query("
-		INSERT INTO toprefs(tr_site, tr_ref, tr_count)
-			SELECT si_id, ref_ids.ref_id, ref_count
-			FROM sites, ref_ids, ref_count
-			WHERE si_id=$site AND ref_site=si_id AND ref_count.ref_id=ref_ids.ref_id
-			ORDER BY ref_count DESC LIMIT 500
-		", $dbh);
+	$insert_topagents->bind_param("i", $site);
+	$insert_topagents->execute();
+
+	$insert_toprefs->bind_param("i", $site);
+	$insert_toprefs->execute();
+
+	$date = date('Y-m');
+	$fdate = date('Y-m-d H:i:s');
+	fwrite($fh, "# This is a list of all URLs for ". $row["si_name"] . "\n");	
+	fwrite($fh, "# It was last updated on $fdate.\n");
+
+	$fh = fopen("$lwbase/$date.all_urls.txt");
+	
+	$a_name = $a_path = $a_count = false;
+	$qallurls->bind_param("i", $site);
+	$qallurls->bind_result($a_name, $a_path, $a_count);
+	$qallurls->execute();
+	while ($qallurls->fetch())
+		fwrite($fh, "$a_count\t$a_name\t$a_path\n");
+	fclose($fh);
+	$qallurls->free_result();
 }
-mysql_free_result($res);
-
-mysql_close($dbh);
+$qsites->close();
+$db->close();
 ?>
