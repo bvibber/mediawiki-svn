0059162b79e3eca7877b8c5527af50f20febcc51
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-08-26 16:28:31 +0000

    * New extension to display an edit count at the top of Special:Contributions
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@10789 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Contributionseditcount/Contributionseditcount.php
@@ -0,0 +1,40 @@
+<?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * Display an edit count at the top of Special:Contributions
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @bug 1725
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionFunctions[] = 'wfContributionseditcount';
+$wgExtensionCredits['other'][] = array(
+	'name' => 'Contributionseditcount',
+	'description' => 'displays an edit count on Special:Contributions',
+	'author' => 'Ævar Arnfjörð Bjarmason'
+);
+
+function wfContributionseditcount() {
+	global $wgMessageCache, $wgHooks;
+	
+	$wgMessageCache->addMessage( 'contributionseditcount', 'This user has $1 edits.' );
+
+	# Run this hook on new account creation
+	$wgHooks['SpecialContributionsBeforeMainOutput'][] = 'wfContributionseditcountHook';
+}
+
+function wfContributionseditcountHook( $uid ) {
+	global $wgOut, $wgLang;
+
+	if ($uid != 0)
+		$wgOut->addWikiText( wfMsg( 'contributionseditcount', $wgLang->formatNum( User::edits( $uid ) ) ) );
+
+	return true;
+}
+
+?>
