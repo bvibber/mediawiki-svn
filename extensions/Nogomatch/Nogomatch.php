accc1e65535cca0a2257519dc2ef0b9eac2e749e
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-09-19 13:45:23 +0000

    * Adding an extension to log nogomatch
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@11084 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Nogomatch/Nogomatch.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgHooks['SpecialSearchNogomatch'][] = 'wfNogomatchHook';
+$wgExtensionCredits['other'][] = array(
+	'name' => 'Nogomatch hook',
+	'author' => 'Ævar Arnfjörð Bjarmason',
+);
+
+function wfNogomatchHook( &$title ) {
+	wfDebugLog( 'nogomatch', $title->getText(), false );
+}
