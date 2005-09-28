6864491b15cb4829d52e8714979c47777b1d9208
(no author) <(no author)@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-09-28 21:20:30 +0000

    This commit was manufactured by cvs2svn to create tag 'WITH_CSHARP'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/WITH_CSHARP@11147 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- mwblocker/checklog-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE checklog (
-  check_timestamp DATETIME,
-  check_ip VARCHAR(40),
-  check_blocked TINYINT(1),
-  check_log TEXT,
-  KEY (check_ip,check_blocked),
-  KEY (check_blocked,check_IP),
-  KEY (check_timestamp)
-);
