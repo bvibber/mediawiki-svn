e4a3969a9c1c11e635e0c1f30c9ac834f7f4d057
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-10-09 19:47:54 +0000

    * A not-as-superfluous way to do the same thing
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@11265 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/nopurgeRC.php
+++ extensions/nopurgeRC.php
@@ -7,18 +7,12 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-$wgExtensionFunctions[] = 'wfNopurgeRC';
+$wgHooks['ArticleEditUpdatesDeleteFromRecentchanges'][] = 'wfNopurgeRCHook';
 $wgExtensionCredits['other'][] = array(
 	'name' => 'NopurgeRC',
 	'description' => 'A hook that prevents old recentchanges entries from being deleted',
 	'author' => 'Ævar Arnfjörð Bjarmason',
 );
-
-function wfNopurgeRC() {
-	global $wgHooks;
-
-	$wgHooks['ArticleEditUpdatesDeleteFromRecentchanges'][] = 'wfNopurgeRCHook';
-}
 
 function wfNopurgeRCHook() {
 	return false;
