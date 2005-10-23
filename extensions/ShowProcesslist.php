8dd3314d069ba457245f9d45adc7016d49427082
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-10-23 18:15:04 +0000

    quotes, tweaked output
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@11466 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ShowProcesslist.php
+++ extensions/ShowProcesslist.php
@@ -2,7 +2,7 @@
 # Not a valid entry point, skip unless MEDIAWIKI is defined
 if (defined('MEDIAWIKI')) {
 
-$wgExtensionFunctions[] = "wfShowProcesslist";
+$wgExtensionFunctions[] = 'wfShowProcesslist';
 $wgExtensionCredits['specialpage'][] = array(
 	'name' => 'ShowProcesslist',
 	'description' => 'display the output of SHOW FULL PROCESSLIST'
@@ -10,12 +10,11 @@ $wgExtensionCredits['specialpage'][] = array(
 
 function wfShowProcesslist() {
 global $IP;
-require_once( "$IP/includes/SpecialPage.php" );
+require_once( $IP.'/includes/SpecialPage.php' );
 
-class ShowProcesslistPage extends UnlistedSpecialPage
-{
+class ShowProcesslistPage extends UnlistedSpecialPage {
 	function ShowProcesslistPage() {
-		UnlistedSpecialPage::UnlistedSpecialPage("ShowProcesslist");
+		UnlistedSpecialPage::UnlistedSpecialPage('ShowProcesslist');
 	}
 
 	function execute( $par ) {
@@ -23,24 +22,24 @@ class ShowProcesslistPage extends UnlistedSpecialPage
 		
 		$this->setHeaders();
 		if ( !$wgUser->isDeveloper() ) {
-			$wgOut->addWikiText( "You're not allowed, go away" );
+			$wgOut->addWikiText( 'You\'re not allowed, go away' );
 			return;
 		}
 
-		$res=wfQuery("SHOW FULL PROCESSLIST",DB_READ);
-		$output=array();
-		$output = '<table border="1">';
-		while ( $row=wfFetchObject($res)){
-			$output .= "<tr>";
+		$res = wfQuery( 'SHOW FULL PROCESSLIST' , DB_READ );
+		$output = array();
+		$output = '<table border="1" cellspacing="0">'."\n";
+		$output .= '<tr><th>Id</th><th>User</th><th>Host</th><th>db</th><th>Command</th><th>Time</th><th>State</th><th>Info</th>'."\n";
+		while ( $row = wfFetchObject($res) ) {
+			$output .= '<tr>';
 			$fields = get_object_vars($row);
 			foreach ($fields as $name => $value ) {
-				$output .= "<td>" . htmlspecialchars( $value ) . "</td>";
+				$output .= '<td>' . htmlspecialchars( $value ) . '</td>';
 			}
-			$output .= "</tr>";
+			$output .= "</tr>\n";
 		}
-		$output .= "</table>";
+		$output .= '</table>';
 		$wgOut->addHTML( $output );
-
 	}
 }
 
