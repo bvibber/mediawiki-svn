905ad1454a93fcfff42739b88eb871207a398324
kateturner <kateturner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-10-31 13:40:09 +0000

    import current live version... - db error message (no more blank page) - anti-editcountitis warning page
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@11597 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/edit_counter/count_edits.py
+++ tools/edit_counter/count_edits.py
@@ -11,9 +11,11 @@ import sys
 #sys.path.append('/home/kate/pylib64/lib64/python2.3/site-packages')
 sys.path.append('/home/kate/pylib/lib/python2.2/site-packages')
 import MySQLdb
-
+import os
 import cgi
-#import cgitb; cgitb.enable()
+import sha
+import time
+
 f = cgi.FieldStorage()
 
 def getdblist():
@@ -73,15 +75,41 @@ table.edittable tr.nsn {
 }
 </style>
 </head>
-<body>"""
+<body>
+<h2>edit count news!!</h2>
+<p>the edit counter was broken for a few days.  apparently this caused quite a
+bit of concern, so in future, it'll print a nice error message instead of
+that nasty blank page.
+</p>
+"""
+
+def thishour():
+    n = time.gmtime()
+    return n[3]
+def lasthour():
+    n = thishour()
+    if n > 0:
+        return n -1
+    return 24
+def mktoken(name, this):
+    ip = os.getenv("REMOTE_ADDR")
+    ip = ".".join(ip.split('.')[:2])
+    hash = sha.new("%s%s%s%s" % (secret, ip, this, name)).hexdigest()
+    return hash
+def checkhash(name):
+    try:
+        theirhash = f['hash'].value
+    except KeyError:
+        return False
+    return (theirhash == mktoken(name, thishour())) or (theirhash == mktoken(name, lasthour()))
 
 namespaces = {
 	0: 'Articles',
 	1: 'Talk',
 	2: 'User',
 	3: 'User talk',
-	4: 'Wikipedia',
-	5: 'Wikipedia talk',
+	4: 'Project *',
+	5: 'Project talk *',
 	6: 'Image',
 	7: 'Image talk',
 	8: 'MediaWiki',
@@ -113,10 +141,74 @@ def ns2name(ns, wiki):
 def fmttime(t):
     return "%s-%s-%s %s:%s:%s" % (t[0:4], t[4:6], t[6:8], t[8:10], t[10:12], t[12:14])
 
+def confirmcount(user):
+    if checkhash(user):
+        return True
+    print """
+<form action="count_edits" method="get">
+<p>(Note: some people (particularly AOL users) were having problems with the
+"new" version not working.  that should be fixed now, please tell me if it's
+still broken.)</p>
+<p>
+users are reminded (in a nice way, of course) that number of edits is only a
+very small part of a user's Wikipedia contributions.  any number of factors -
+the ability to work with other users, write good articles, etc. - are far
+more important than this single number, and you can't find out about those
+from a user's edit count.
+</p>
+<p>
+if you're here because you're about to vote on an RfA, consider instead
+<em>not</em> looking at their edit count, but instead their contributions.
+there's now an "Earliest" link there that you can use to find their first
+edit, if you're worried about how long they've been around.  have a look at
+a few diffs, see how good their edits are, how well they conduct themselves
+on talk pages, and so on.  maybe edit count isn't that important, after all.
+</p>
+<p>
+if you're checking your <em>own</em> edit count&hellip; well, do you really care? ;-)
+</p>
+<input type="hidden" name="hash" value="%s" />
+<input type="hidden" name="user" value="%s" />
+<input type="hidden" name="dbname" value="%s" />
+<input type="submit" value="i understand, show me the edit count" />
+<p>
+(PS: some people seem to dislike this warning.  well, i am sorry about that,
+but perhaps you can see it from my point of view.</p>
+<p>i wrote the edit counter
+because, sometimes, it <em>is</em> interesting to view someone's edit count,
+and the old method (using Special:Contributions) was a real pain.  unfortunately,
+what i didn't foresee is that an easy way to count people's edit would lead
+to edit counts becoming quite a bit more important in the general scheme
+of things.</p>
+<p>
+this makes me quite sad, but i don't want to remove the edit counter; like i say,
+it does have legitimite uses, and since MW 1.5, the Special:Contributions method
+doesn't work at all. (unless you're really, really patient).  but i did want
+to make people stop and think about how useful an edit count really is.</p>
+<p>did i
+manage that?  i don't know... people who agree with me have said they like the
+warning, but people who disagree just focus their attention on disliking the
+warnings, rather than reconsidering edits.  still, even if i can annoy people into
+using the edit counter less, that's a minor victory ;-)
+</p>
+<p>and lastly... yes, this is just my personal opinion.  but then, it is my 
+edit counter :-)</p>
+</form>
+""" % (mktoken(user, thishour()), cgi.escape(user), cgi.escape(dbname));
+    return False
+
 def editcount(user):
+    if not confirmcount(user):
+        return
     ns = dict()
     total = 0
-    db = MySQLdb.connect(db=dbname, host=dbserver, user=dbuser, passwd=dbpassword)
+    try:
+        db = MySQLdb.connect(db=dbname, host=dbserver, user=dbuser, passwd=dbpassword)
+    except:
+        print """
+        <p><strong>onoes!!</strong> some kind of database error occured... please try again later</p>
+        """;
+        return
     c = db.cursor()
     c.execute("SELECT COUNT(DISTINCT page_id) FROM user, revision, page WHERE "
               "user_name=%s AND rev_user = user_id AND rev_page = page_id", user);
@@ -137,6 +229,10 @@ def editcount(user):
         total += t[1]
         edits[t[0]] = t[1]
         t = c.fetchone()
+    if total == 0:
+        print """<p><strong>this user does not seem to have any edits, or they don't exist</strong></p>""";
+        return
+
     print """
 <table class="edittable">
 <tr><th>Username</th><td colspan='2'>%s</td></tr>
@@ -154,6 +250,8 @@ def editcount(user):
                 % (ns2name(nsn, dbname), edits[nsn])
     print """
 </table>
+<br clear='all'/>
+<p>* 'Project' and 'Project talk' refer to the project namespace: Wikipedia, Wiktionary, Meta, etc.</p>
     """
     return
 
