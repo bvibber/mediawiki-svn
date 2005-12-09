5ad8008d8f83b64cab80b8ed30a870746f5c1c9e
magnus_manske <magnus_manske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-12-09 23:13:27 +0000

    Improvements; still far from finished, please do not touch yet
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@12038 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Tasks/Tasks.php
+++ extensions/Tasks/Tasks.php
@@ -8,17 +8,18 @@ To activate, put something like this in your LocalSettings.php:
 
 Also, you need to run the following SQL statement:
 CREATE TABLE `tasks` (
-	`task_id` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
+	`task_id` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0' AUTO_INCREMENT,
 	`task_page_id` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
-	`task_page_revision` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
-	`task_user_id` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
+	`task_page_title` varchar(255) NOT NULL default '',
+  	`task_user_id` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
 	`task_user_text` VARCHAR( 255 ) NOT NULL ,
 	`task_user_assigned` INT( 8 ) UNSIGNED NOT NULL DEFAULT '0',
 	`task_status` INT( 4 ) UNSIGNED NOT NULL DEFAULT '0',
 	`task_comment` MEDIUMTEXT NOT NULL ,
 	`task_type` INT( 4 ) UNSIGNED NOT NULL DEFAULT '0',
+	`task_timestamp` VARCHAR( 14 ) BINARY NOT NULL,
 	PRIMARY KEY ( `task_id` ) ,
-	INDEX ( `task_article_id` , `task_status` )
+	INDEX ( `task_page_id` , `task_status` , `task_type` )
 	) TYPE = innodb;
 */
 
@@ -39,7 +40,7 @@ $wgHooks['UnknownAction'][] = 'wfTasksExtensionAction' ;
 function wfTasksExtensionTab ( &$skin , &$content_actions ) {
 	global $wgTitle , $action ;
 	wfTasksAddCache() ;
-	$content_actions['validate'] = array(
+	$content_actions['tasks'] = array(
 		'class' => ($action == 'tasks') ? 'selected' : false,
 		'text' => wfMsg('tasks_tab'),
 		'href' => $wgTitle->getLocalUrl( "action=tasks" )
@@ -70,6 +71,12 @@ function wfTasksAddCache () {
 			'tasks_tab' => 'Tasks',
 			'tasks_form_new' => "Create new task",
 			'tasks_form_comment' => "Comment",
+			'tasks_error1' => "Task was not created: there is already such a task!<br/>",
+			'tasks_ok1' => "New task has been created!<br/>",
+			'tasks_create_header' => "Create a new task",
+			'tasks_existing_header' => "Existing tasks",
+			'tasks_existing_table_header' => "<th>Status</th><th>Type</th><th>Created</th><th>Comment</th><th>Assigned to</th>",
+			'tasks_noone' => "Noone",
 			
 			'tasks_status_open' => "Open" ,
 			'tasks_status_assigned' => "Assigned" ,
@@ -124,14 +131,17 @@ function wfTasksExtension() {
 			$this->includable( true );
 		}
 
-		function get_valid_new_tasks ( &$title , &$tasks , $exists ) {
+		function get_valid_new_tasks ( &$title , &$tasks ) {
+			$exists = $title->exists() ;
+			if ( $exists )
+				$tasks = $this->get_tasks_for_page ( $title->getArticleID() ) ;
 			$new_tasks = array () ;
 			if ( $exists ) {
 				$tg = array () ;
 				foreach ( $tasks AS $t )
 					$tg[$t->task_type] = 1 ;
 				for ( $a = min ( array_keys ( $this->task_types ) ) ; $a <= max ( array_keys ( $this->task_types ) ) ; $a++ ) {
-					if ( isset ( $tg[$a] ) ) continue ;
+					if ( isset ( $tg[$a] ) AND ( $tg[$a]->task_status < 3 ) ) continue ; # Task exists and is not closed
 					$tk = $this->task_types[$a] ;
 					if ( $tk == 'create' && $exists ) continue ; # Can't create an existing article...
 					$new_tasks[$a] = $tk ;
@@ -140,38 +150,117 @@ function wfTasksExtension() {
 			return $new_tasks ;
 		}
 
+		function create_from_form ( $title ) {
+			global $wgRequest , $wgUser ;
+			if ( $wgRequest->getText('create_task', "") == "" ) return "" ; # No form
+			
+			$out = "" ;
+			$tasks = array () ;
+			$type = $wgRequest->getText('type', "") ;
+			$comment = $wgRequest->getText('text', "") ;
+			$new_tasks = $this->get_valid_new_tasks ( $title , $tasks ) ;
+			if ( !isset ( $new_tasks[$type] ) ) # Trying to create a task that isn't available
+				$out .= wfMsg('tasks_error1') ;
+			else {
+				$dbw =& wfGetDB( DB_MASTER );
+				$dbw->insert ( 'tasks',
+					array (
+						'task_page_id' => $title->getArticleID() ,
+						'task_page_title' => $title->getPrefixedText() ,
+						'task_user_id' => $wgUser->getID() ,
+						'task_user_text' => $wgUser->getName() ,
+						'task_user_assigned' =>  '0' , # default
+						'task_status' => '1' , # Hack, should check for the numerical value assigned to 'open'
+						'task_comment' => $comment ,
+						'task_type' => $type ,
+						'task_timestamp' => $dbw->timestamp()
+						) ) ;
+				$out .= wfMsg('tasks_ok1') ;
+			}
+			return $out ;
+		}
+		
+		function get_task_table_row ( &$task ) {
+			global $wgContLang , $wgUser ;
+			$out = "" ;
+			$ct = $wgContLang->timeanddate ( $task->task_timestamp ) ;
+			$cu = Title::makeTitleSafe( NS_USER, $task->task_user_text );
+			$comment = htmlspecialchars ( $task->task_comment ) ;
+			$comment = str_replace ( "\n" , "<br/>" , $comment ) ;
+
+			$out .= "<tr>" ;
+			$out .= "<td valign='top' align='left' nowrap>" ;
+			$out .= wfMsg ( 'tasks_status_' . $this->status_types[$task->task_status] ) ;
+			$out .= "</td>" ;
+			$out .= "<th align='left' valign='top' nowrap>" . wfMsg('tasks_type_'.$this->task_types[$task->task_type]) . "</th>" ;
+			$out .= "<td align='left' valign='top' nowrap><a href=\"" . $cu->getFullUrl() . '">' . $cu->getText() . "</a> ({$ct})</td>" ;
+			$out .= "<td>" . $comment . "</td>" ;
+			$out .= "<td align='left' valign='top' nowrap>" ;
+			if ( $task->task_user_assigned == 0 ) { # Noone is assigned this task
+				$out .= wfMsg('tasks_noone') ;
+			} else { # Someone is assigned this task
+				
+			}
+			if ( $wgUser->isLoggedIn() AND $task->task_status < 3 ) { # Open or assigned, can assign this to myself as a logged-in user
+				
+			}
+			$out .="</td>" ;
+			$out .= "</tr>" ;
+			return $out ;
+		}
+
+		function show_existing_tasks ( &$title , &$tasks ) {
+			$out = "" ;
+			foreach ( $tasks AS $task )
+				$out .= $this->get_task_table_row ( $task ) ;
+			if ( $out == "" ) return "" ;
+
+			$out = "<h2>" . wfMsg('tasks_existing_header') . "</h2>\n" .
+				"<table border='1' cellspacing='1' cellpadding='2'>" . 
+				"<tr>" . wfMsg('tasks_existing_table_header') . "</tr>" .
+				$out . "</table>" ;
+			return $out ;
+		}
 		
 		function page_management ( $title ) {
 			global $wgOut ;
 			$out = "" ;
+			$tasks = array() ;
 			
-			if ( $title->getArticleID() == 0 ) $exists = false ;
-			else $exists = true ;
+			# Create from form
+			$out = $this->create_from_form ( $title ) ;
 			
-			if ( $exists )
-				$tasks = $this->get_tasks_for_page ( $title->getArticleID() ) ;
-			$new_tasks = $this->get_valid_new_tasks ( $title , $tasks , $exists ) ;
+			# Get list of tasks that can be created
+			$new_tasks = $this->get_valid_new_tasks ( $title , $tasks ) ;
 			
-			if ( count ( $new_tasks ) > 0 )
-				$out .= $this->generate_form ( $new_tasks ) ;
+			# Show task creation form, if tasks can be created
+			$out .= $this->generate_form ( $new_tasks ) ;
+			
+			# Existing tasks
+			$out .= $this->show_existing_tasks ( $title , $tasks ) ;
 
+			# And ... out!
 			$this->setHeaders();
 			$wgOut->addHtml( $out );
 		}
 
 		# Generates a form for creating a new task
 		function generate_form ( &$new_tasks ) {
-			$out = "" ;
+			if ( count ( $new_tasks ) == 0 ) return "" ;
+			$out = "<h2>" . wfMsg('tasks_create_header') . "</h2>\n" ; ;
 			$out .= "<form method='post'>" ;
 			$out .= "<table border='0' width='100%'><tr><th nowrap>" ;
 			$out .= wfMsg ( 'tasks_form_new' ) ;
 			$out .= "</th><td width='100%'>" ;
 			$out .= "<select name='type'>" ;
+			$o = array () ;
 			foreach ( $new_tasks AS $k => $v ) {
-				$out .= "<option value='{$k}'>" . wfMsg ( 'tasks_type_' . $v ) . "</option>" ;
+				$o[$v] = "<option value='{$k}'>" . wfMsg ( 'tasks_type_' . $v ) . "</option>" ;
 			}
+			ksort ( $o ) ;
+			$out .= implode ( "" , $o ) ;
 			$out .= "</select>" ;
-			$out .= "<input type=submit name='doit' value='" . wfMsg ( 'ok' ) . "'/>" ;
+			$out .= "<input type='submit' name='create_task' value='" . wfMsg ( 'ok' ) . "'/>" ;
 			$out .= "</td></tr><tr><th nowrap>" ;
 			$out .= wfMsg ( 'tasks_form_comment' ) ;
 			$out .= "</th><td>" ;
@@ -192,7 +281,7 @@ function wfTasksExtension() {
 			
 			$ret = array () ;
 			while ( $line = $dbr->fetchObject( $res ) )
-				$ret[] = line ;
+				$ret[] = $line ;
 			$dbr->freeResult($res);
 			return $ret ;
 		}
