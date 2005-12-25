4e4df0ce258dc6e89ac5a7315bc43dd76cd9cae8
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-12-25 22:45:10 +0000

    * An extension to get the full url of a file from its name, see http://meta.wikimedia.org/wiki/Filepath
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@12242 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Filepath/SpecialFilepath.php
@@ -0,0 +1,102 @@
+<?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionFunctions[] = 'wfSpecialFilepath';
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Filepath',
+	'author' => 'Ævar Arnfjörð Bjarmason',
+	'description' => '[[Special:Filepath|a special page]] to get the full path of a file from its name',
+	'url' => 'http://meta.wikimedia.org/wiki/Filepath'
+);
+
+function wfSpecialFilepath() {
+	global $IP, $wgMessageCache;
+
+	$wgMessageCache->addMessages(
+		array(
+			'filepath' => 'File path',
+			'filepath_page' => 'File: ',
+			'filepath_submit' => 'Path',
+		)
+	);
+
+	require_once "$IP/includes/SpecialPage.php";
+	class SpecialFilepath extends SpecialPage {
+		function SpecialFilepath() {
+			SpecialPage::SpecialPage( 'Filepath' );
+		}
+		
+		function execute( $par ) {
+			global $wgRequest, $wgOut;
+
+			$file = isset( $par ) ? $par : $wgRequest->getText( 'file' );
+			
+			$title = Title::makeTitleSafe( NS_IMAGE, $file );
+			$article = new Article( $title );
+			
+			if ( is_null( $title ) || ! $article->exists() ) {
+				$this->setHeaders();
+				$cform = new FilepathForm( $title );
+				$cform->execute();
+			} else {
+				$file = new Image( $title ); 
+				$wgOut->redirect( $file->getURL() );
+			}
+		}
+	}
+
+	class FilepathForm {
+		var $mTitle;
+		
+		function FilepathForm( &$title ) {
+			$this->mTitle =& $title;
+		}
+		
+		function execute() {
+			global $wgOut, $wgTitle;
+
+			$wgOut->addHTML(
+				wfElement( 'form',
+					array(
+						'id' => 'specialfilepath',
+						'method' => 'get',
+						'action' => $wgTitle->escapeLocalUrl()
+					),
+					null
+				) .
+					wfOpenElement( 'label' ) .
+						wfMsgHtml( 'filepath_page' ) .
+						wfElement( 'input',
+							array(
+								'type' => 'text',
+								'size' => 25,
+								'name' => 'file',
+								'value' => is_object( $this->mTitle ) ? $this->mTitle->getText() : ''
+							),
+							''
+						) .
+						' ' .
+						wfElement( 'input',
+							array(
+								'type' => 'submit',
+								'value' => wfMsgHtml( 'filepath_submit' )
+							),
+							''
+						) .
+					wfCloseElement( 'label' ) .
+				wfCloseElement( 'form' )
+			);
+		}
+
+	}
+
+	SpecialPage::addPage( new SpecialFilepath );
+}
