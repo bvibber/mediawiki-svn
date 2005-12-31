90b18d0e299395b38e4e3b577d6f87baa32ca917
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2005-12-31 18:55:27 +0000

    * Rewrote the code using MediaWiki's PersistentObject class and the new   object system in PHP 5.0 and type hinting system in PHP 5.0
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@12320 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/HTTPRedirect.php
+++ extensions/HTTPRedirect.php
@@ -7,17 +7,32 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-$wgHooks['ArticleViewRedirect'][] = 'wfHTTPRedirectHook';
 $wgExtensionCredits['other'][] = array(
 	'name' => 'HTTP redirect',
 	'description' => 'A hook to make the wiki issue HTTP redirects rather than wiki redirects',
 	'author' => 'Ævar Arnfjörð Bjarmason',
 );
 
-function wfHTTPRedirectHook( &$article ) {
-	global $wgOut;
+function wfHTTPRedirect() {
+	wfUsePHP( 5.0 );
+	wfUseMW( '1.6alpha' );
 	
-	$wgOut->redirect( $article->mTitle->escapeFullURL(), 302 );
-	
-	return false;
+	class HTTPRedirect {
+		public function __construct() {
+			global $wgHooks;
+
+			$wgHooks['ArticleViewRedirect'][] = array( &$this, 'redirectHook' );
+		}
+
+		
+		public static function redirectHook( Article &$article ) {
+			global $wgOut;
+
+			$wgOut->redirect( $article->mTitle->escapeFullURL(), 302 );
+
+			return false;
+		}
+	}
+
+	new PersistentObject( new HTTPRedirect );
 }
