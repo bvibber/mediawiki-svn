042c2f81162d647943d19b229eea4ede366bb9b0
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-01-09 05:07:28 +0000

    Username blacklist extension Version 1.0
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@12539 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UsernameBlacklist/UsernameBlacklist.php
@@ -0,0 +1,57 @@
+﻿<?php
+
+/**
+ * Extension to provide a global "bad username" list
+ *
+ * @author Rob Church <robchur@gmail.com>
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @copyright  2006 Rob Church
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0
+ */
+
+if( defined( 'MEDIAWIKI' ) ) {
+
+	$wgExtensionFunctions[] = 'UsernameBlacklist_Init';
+	$wgExtensionCredits['other'][] = array( 'name' => 'Username blacklist', 'description' => 'provides a regex.-compatible blacklist of usernames', 'author' => 'Rob Church' );
+	
+	function UsernameBlacklist_Init() {
+		global $wgMessageCache, $wgHooks;
+		$wgHooks['AbortNewAccount'][] = 'UsernameBlacklist_Hook';
+		$wgMessageCache->addMessage( 'blacklistedusername', 'Blacklisted username' );
+		$wgMessageCache->addMessage( 'blacklistedusernametext', 'The username you have chosen matches the [[MediaWiki:Usernameblacklist|list of blacklisted usernames]].' );
+	}
+	
+	function UsernameBlacklist_Hook( $user ) {
+		global $wgOut;
+		$username  = $user->getName();
+		$blacklist = wfMsg( 'usernameblacklist' );
+		if( !wfEmptyMsg( 'usernameblacklist', $blacklist ) ) {
+			$list = explode( "\n", $blacklist );
+			foreach( $list as $item ) {
+				$regex = '/' . UsernameBlacklist_Trim( $item ) . '/';
+				if( preg_match( $regex, $username ) > 0 ) {
+					$rt_title = Title::makeTitle( NS_SPECIAL, 'Userlogin' );
+					$wgOut->errorPage( 'blacklistedusername', 'blacklistedusernametext' );
+					$wgOut->returnToMain( false, $rt_title->getPrefixedText() );
+					return( false );
+				}
+			}
+			return( true );
+		} else {
+			return( true );
+		}
+	}
+	
+	function UsernameBlacklist_Trim( $text ) {
+		while( ( substr( $text, 0, 1 ) == '*' ) || ( substr( $text, 0, 1 ) == ' ' ) ) {
+			$text = substr( $text, 1, strlen( $text ) - 1 );
+		}
+		return( $text );
+	}	
+	
+} else {
+	die( 'This file is an extension to the MediaWiki package, and cannot be executed separately.' );
+}
+
+?>
