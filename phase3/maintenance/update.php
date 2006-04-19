6b67f12c9a83d7400a45dc6252eb1db535d29ba3
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-04-19 23:35:27 +0000

    Branch from trunk, r13753
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/namespace-manager@13754 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/maintenance/update.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'counter.php';
+/**
+ * Run all updaters.
+ *
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Maintenance
+ */
+
+/** */
+$wgUseMasterForMaintenance = true;
+$options = array( 'quick' );
+require_once( "commandLine.inc" );
+require_once( "updaters.inc" );
+$wgTitle = Title::newFromText( "MediaWiki database updater" );
+$dbclass = 'Database'.ucfirst($wgDBtype);
+require_once("$dbclass.php");
+$dbc = new $dbclass;
+$wgDatabase = $dbc->newFromParams( $wgDBserver, $wgDBadminuser, $wgDBadminpassword, $wgDBname );
+
+print "Going to run database updates for $wgDBname\n";
+print "Depending on the size of your database this may take a while!\n";
+
+if( !isset( $options['quick'] ) ) {
+	print "Abort with control-c in the next five seconds... ";
+
+	for ($i = 6; $i >= 1;) {
+		print_c($i, --$i);
+		sleep(1);
+	}
+	echo "\n";
+}
+
+if ( isset( $options['doshared'] ) ) {
+	$doShared = true;
+} else {
+	$doShared = false;
+}
+
+do_all_updates( $doShared );
+
+print "Done.\n";
+
+?>
