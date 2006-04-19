7cd84e8ee58f13c022b32ab01ddf1ba49228d27e
yurik <yurik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-04-19 23:34:12 +0000

    Code reorganization, plug-able property generators, minor bug fixes, usage information.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@13752 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BotQuery/query.php
+++ extensions/BotQuery/query.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Bot Query extension for MediaWiki 1.7+
+ *
+ * Copyright (C) 2006 Yuri Astrakhan <FirstnameLastname@gmail.com>
+ * Uses bits from the original query.php code written by Tim Starling.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
 
 define( 'MEDIAWIKI', true );
 unset( $IP );
@@ -22,13 +44,34 @@ $bqp->output();
 
 
 class BotQueryProcessor {
+	var $classname = 'BotQueryProcessor';
+
 	var $mimeTypes = array(
-		'txt' => 'text/plain',
-		'dbg' => 'text/plain',
-		'tsv' => 'text/tab-separated-values',
-		'json' => 'application/json',
 		'xml' => 'text/xml',
+		'txt' => 'text/plain',
+		'json' => 'application/json',
 		'php' => 'application/vnd.php.serialized',
+		'dbg' => 'text/plain',
+//		'tsv' => 'text/tab-separated-values',
+	);
+
+	/**
+	* Properties generators - each property points to an array of the following parameters:
+	*     1) True/False - does this property work on individual pages?  (False for site's metadata)
+	*     2) Function to call
+	*     3) property description
+	*/
+	var $propGenerators = array(
+
+		// Site-wide Generators
+		'siteinfo'       => array( False, "genMetaSiteInfo", "basic site information" ),
+		'sitenamespaces' => array( False, "genMetaNamespaceInfo", "list of localized namespaces" ),
+
+		// Page-specific Generators
+		'langlinks'      => array( True, "genPageLangLinks", "interlanguage links" ),
+		'templates'      => array( True, "genPageTemplates", "template names" ),
+		'links'          => array( True, "genPageLinks", "regular links to other pages" ),
+		'revisions'      => array( True, "genPageHistory", "revision history (see Notes)" ),
 	);
 
 	function BotQueryProcessor( $db ) {
@@ -40,6 +83,27 @@ class BotQueryProcessor {
 		$this->properties = $this->parseProperties( $wgRequest->getText('properties'));
 
 		$this->data = array();
+	}
+
+	function execute() {
+		// Process metadata generators
+		$this->callGenerators( False, 'meta' );
+
+		// Query page table and initialize page ids.
+		if ( $this->linkBatch === null || $this->genPageInfo()) {
+			return;   // No titles were given, skip any page generation
+		}
+
+		// Process only page-related generators
+		$this->callGenerators( True, $this->data['pages'] );
+	}
+
+	function callGenerators( $callPageGenerators ) {
+		foreach( $this->propGenerators as $property => $generator ) {
+			if( $generator[0] === $callPageGenerators && in_array( $property, $this->properties )) {
+				$this->{$generator[1]}();
+			}
+		}
 	}
 
 	function output() {
@@ -74,25 +138,15 @@ class BotQueryProcessor {
 		}
 	}
 
+
+	//
+	// ************************************* INPUT PARSERS *************************************
+	//
 	function parseFormat( $format ) {
-		switch ( $format ) {
-			case 'php':
-				return 'php';
-			case 'json':
-				return 'json';
-			case 'xml':
-				return 'xml';
-			case 'tsv':
-				die( 'TSV support is not yet implemented' );
-			case '':
-			case 'txt':
-			case 'text':
-				return 'txt';
-			case 'dbg':
-			case 'phpcode':
-				return 'dbg';
-			default:
-				die( 'Unrecognised format' );
+		if( array_key_exists($format, $this->mimeTypes) ) {
+			return $format;
+		} else {
+			$this->dieUsage( "Unrecognised format '$format'" );
 		}
 	}
 
@@ -106,11 +160,11 @@ class BotQueryProcessor {
 		$titles = explode( '|', $titles );
 		if ( $wgUser->isBot() ) {
 			if ( count( $titles ) > 1000 ) {
-				die ( 'Error, too many titles specified' );
+				$this->dieUsage( 'Error, too many titles specified' );
 			}
 		} else {
 			if ( count( $titles ) > 20 ) {
-				die( 'Error, too many titles specified' );
+				$this->dieUsage( 'Error, too many titles specified' );
 			}
 		}
 		$linkBatch = new LinkBatch;
@@ -127,83 +181,23 @@ class BotQueryProcessor {
 		global $wgUser;
 
 		if ( $properties == '' ) {
-			die( 'Error, must specify one or more valid properties in the "properties" parameter' );
+			$this->dieUsage( 'No properties given' );
 		}
-		return explode( '|', $properties );
-	}
 
-
-	function execute() {
-		$fname = 'BotQueryProcessor';
-
-		$knownProperties = array( 'siteinfo', 'sitenamespaces', 'langlinks', 'templates', 'links', 'history', 'barehistory' );
-		$unknownProperties = array_diff( $this->properties, $knownProperties);
+		$propList = explode( '|', $properties );
+		$unknownProperties = array_diff( $propList, array_keys( $this->propGenerators ));
 		if( $unknownProperties ) {
-			die( "Unrecognised property " . implode(',', $unknownProperties) .
-				".\nCurrently supported properties: " . implode(',', $knownProperties));
+			$this->dieUsage( "Unrecognised property " . implode(',', $unknownProperties) );
 		}
 
-		// basic site information
-		if ( $this->processProp('siteinfo') ) {
-			$this->genMetaSiteInfo( $this->data['meta'] );
-		}
-		// the list of localized namespaces defined for this site
-		if ( $this->processProp('sitenamespaces') ) {
-			$this->genMetaNamespaceInfo( $this->data['meta'] );
-		}
-
-		if ( $this->linkBatch === null ) {
-			return;   // No titles were given, skip any page generation
-		}
-
-		// Query page table
-		// This call initializes *PageIds member variables
-		// We stop if no pages need to be processed
-		if( $this->genPageInfo( $this->data['pages'], $fname )) {
-			return;
-		}
-
-		// gather interlanguage links
-		if ( $this->processProp('langlinks') ) {
-			$this->genPageLangLinks( $this->data['pages'], $fname );
-		}
-
-		// gather templates
-		if ( $this->processProp('templates') ) {
-			$this->genPageTemplates( $this->data['pages'], $fname );
-		}
-
-		// gather regular links to other pages
-		if ( $this->processProp('links') ) {
-			$this->genPageLinks( $this->data['pages'], $fname );
-		}
-
-		// gather revision history
-		if ( $this->processProp('history') || $this->processProp('barehistory') ) {
-			$this->genPageHistory( $this->data['pages'], $fname, $this->processProp('history') );
-		}
+		return $propList;
 	}
 
-	function genMetaSiteInfo( &$data ) {
-		global $wgSitename, $wgVersion, $wgCapitalLinks;
-		$data['sitename']  = $wgSitename;
-		$data['generator'] = "MediaWiki $wgVersion";
-		$data['case']      = $wgCapitalLinks ? 'first-letter' : 'case-sensitive'; // "case-insensitive" option is reserved for future
 
-		$mainPage = Title::newFromText( wfMsgForContent( 'mainpage' ) );
-		$data['mainpage']  = $mainPage->getText();
-		$data['base']      = $mainPage->getFullUrl();
-	}
-
-	function genMetaNamespaceInfo( &$data ) {
-		global $wgContLang;
-		$data['namespaces']['_element'] = 'ns';
-		foreach( $wgContLang->getFormattedNamespaces() as $ns => $title ) {
-			$data['namespaces'][$ns] = array( "id"=>$ns, "_content" => $title );
-		}
-	}
-
-	function genPageInfo( &$data, $fname ) {
+	//
+	// ************************************* GENERATORS *************************************
+	//
+	function genPageInfo() {
 		// Create a list of pages to query
 		$where = $this->linkBatch->constructSet( 'page', $this->db );
 		if ( !$where ) {
@@ -213,14 +207,14 @@ class BotQueryProcessor {
 		$res = $this->db->select( 'page',
 			array( 'page_id', 'page_namespace', 'page_title', 'page_is_redirect' ),
 			$where,
-			$fname . '::genPageInfo' );
+			$this->classname . '::genPageInfo' );
 
 		$redirects = array();
 		$nonexistentPages = $this->linkBatch->data;
-
+		$this->data['pages'] = array();
 		while ( $row = $this->db->fetchObject( $res ) ) {
 			$title = Title::makeTitle( $row->page_namespace, $row->page_title );
-			$data[$row->page_id] = array(
+			$this->data['pages'][$row->page_id] = array(
 				'title' => $title->getPrefixedText(),
 				'id' => $row->page_id,
 			);
@@ -235,23 +229,28 @@ class BotQueryProcessor {
 		$this->db->freeResult( $res );
 
 		// This list can later be used to filter other tables by page Id
-		$this->allPageIds = array_keys( $data );
+		$this->allPageIds = array_keys( $this->data['pages'] );
 		$this->inAllPageIds = $this->db->makeList( $this->allPageIds );
 		$this->inRealPageIds = $this->db->makeList( array_diff_key($this->allPageIds, $redirects) );
 
-		// Must not alter $data[] until generating Page Ids
-		$data['_element'] = 'page';
+		// Must not alter $this->data['pages'][] until done generating Page Ids
+		$this->data['pages']['_element'] = 'page';
 
 		// Add records for non-existent page titles
 		$i = -1;
 		foreach ( $nonexistentPages as $namespace => $stuff ) {
 			foreach ( $stuff as $dbk => $arbitrary ) {
 				$title = Title::makeTitle( $namespace, $dbk );
-				$data[$i--] = array(
+				$this->data['pages'][$i--] = array(
 					'title' => $title->getPrefixedText(),
 					'id' => 0
 				);
 			}
+		}
+		
+		// All pages do not exist, prevent further processing
+		if( count($this->allPageIds) === 0 ) {
+			return true;
 		}
 
 		// Process redirects
@@ -260,53 +259,74 @@ class BotQueryProcessor {
 				'pagelinks',
 				array( 'pl_from', 'pl_namespace', 'pl_title' ),
 				'pl_from IN (' . implode( ',', $redirects ) . ')',
-				$fname . '::genPageRedirects' );
+				$this->classname . '::genPageRedirects' );
 			$redirectTargets = array();
 			while ( $row = $this->db->fetchObject( $res ) ) {
-				$data[$row->pl_from]['redirect'] = $this->getLinkInfo( $row->pl_namespace, $row->pl_title );
+				$this->data['pages'][$row->pl_from]['redirect'] = $this->getLinkInfo( $row->pl_namespace, $row->pl_title );
 			}
 			$this->db->freeResult( $res );
 		}
 	}
 
-	function genPageLangLinks( &$data, $fname ) {
+	function genMetaSiteInfo() {
+		global $wgSitename, $wgVersion, $wgCapitalLinks;
+		$this->data['meta']['sitename']  = $wgSitename;
+		$this->data['meta']['generator'] = "MediaWiki $wgVersion";
+		$this->data['meta']['case']	  = $wgCapitalLinks ? 'first-letter' : 'case-sensitive'; // "case-insensitive" option is reserved for future
+
+		$mainPage = Title::newFromText( wfMsgForContent( 'mainpage' ) );
+		$this->data['meta']['mainpage']  = $mainPage->getText();
+		$this->data['meta']['base']	  = $mainPage->getFullUrl();
+	}
+
+	function genMetaNamespaceInfo() {
+		global $wgContLang;
+		$this->data['meta']['namespaces']['_element'] = 'ns';
+		foreach( $wgContLang->getFormattedNamespaces() as $ns => $title ) {
+			$this->data['meta']['namespaces'][$ns] = array( "id"=>$ns, "_content" => $title );
+		}
+	}
+
+	function genPageLangLinks() {
 		$res = $this->db->select(
 			array( 'langlinks' ),
 			array( 'll_from', 'll_lang', 'll_title' ),
 			"ll_from IN ({$this->inRealPageIds})",
-			$fname . '::genPageLangLinks' );
+			$this->classname . '::genPageLangLinks' );
 		while ( $row = $this->db->fetchObject( $res ) ) {
 			$this->addPageSubElement( $row->ll_from, 'langlinks', 'll', array('lang' => $row->ll_lang, '_content' => $row->ll_title));
 		}
 		$this->db->freeResult( $res );
 	}
 
-	function genPageTemplates( &$data, $fname ) {
+	function genPageTemplates() {
 		$res = $this->db->select(
 			'templatelinks',
 			array( 'tl_from', 'tl_namespace', 'tl_title' ),
 			"tl_from IN ({$this->inRealPageIds})",
-			$fname . '::genPageTemplates' );
+			$this->classname . '::genPageTemplates' );
 		while ( $row = $this->db->fetchObject( $res ) ) {
 			$this->addPageSubElement( $row->tl_from, 'templates', 'tl', $this->getLinkInfo( $row->tl_namespace, $row->tl_title ));
 		}
 		$this->db->freeResult( $res );
 	}
 
-	function genPageLinks( &$data, $fname ) {
+	function genPageLinks() {
 		$res = $this->db->select(
 			'pagelinks',
 			array( 'pl_from', 'pl_namespace', 'pl_title' ),
 			"pl_from IN ({$this->inRealPageIds})",
-			$fname . '::genPageLinks' );
+			$this->classname . '::genPageLinks' );
 		while ( $row = $this->db->fetchObject( $res ) ) {
 			$this->addPageSubElement( $row->pl_from, 'links', 'l', $this->getLinkInfo( $row->pl_namespace, $row->pl_title ));
 		}
 		$this->db->freeResult( $res );
 	}
 
-	function genPageHistory( &$data, $fname, $includeComments ) {
+	function genPageHistory() {
 		global $wgRequest;
+
+		$includeComments = 0 === strcasecmp( 'true', $wgRequest->getText('rvcomments') );
 
 		// select *:  rev_page, rev_text_id, rev_comment, rev_user, rev_user_text, rev_timestamp, rev_minor_edit, rev_deleted
 		$fields = array('rev_timestamp','rev_user','rev_user_text','rev_minor_edit');
@@ -320,9 +340,9 @@ class BotQueryProcessor {
 
 		$start = $wgRequest->getText( 'rvstart' );
 		if ( $start != '' ) {
-			$conds[] = 'rev_timestamp <= ' . $this->prepareTimestamp($start);
+			$conds[] = 'rev_timestamp >= ' . $this->prepareTimestamp($start);
 		}
-		
+
 		$end = $wgRequest->getText( 'rvend' );
 		if ( $end != '' ) {
 			$conds[] = 'rev_timestamp <= ' . $this->prepareTimestamp($end);
@@ -335,12 +355,12 @@ class BotQueryProcessor {
 		);
 
 		if( $limit * count($this->allPageIds) > 20000 ) {
-			die( "limit multiplied by number of requested titles must be <= 20000" );
+			$this->dieUsage( "rvlimit multiplied by number of requested titles must be less than 20000" );
 		}
 
 		foreach( $this->allPageIds as $pageId ) {
 			$conds['rev_page'] = $pageId;
-			$res = $this->db->select( 'revision', $fields, $conds, $fname . '::genPageHistory', $options );
+			$res = $this->db->select( 'revision', $fields, $conds, $this->classname . '::genPageHistory', $options );
 			while ( $row = $this->db->fetchObject( $res ) ) {
 				$vals = array(
 					'timestamp' => wfTimestamp( TS_ISO_8601, $row->rev_timestamp ),
@@ -359,6 +379,9 @@ class BotQueryProcessor {
 		}
 	}
 
+	//
+	// ************************************* UTILITIES *************************************
+	//
 	function getTitleInfo( $title ) {
 		$data = array();
 		if( $title->getNamespace() != NS_MAIN ) {
@@ -382,16 +405,51 @@ class BotQueryProcessor {
 		$data[] = $params;
 	}
 
-	function processProp( $property ) {
-		return in_array( $property, $this->properties );
-	}
-
 	function prepareTimestamp( $value ) {
 		if ( preg_match( '/^[0-9]{14}$/', $value ) ) {
 			return $this->db->addQuotes( $value );
 		} else {
-			die( 'Error, rvstart and rvend parameters must be a 14-character numeric timestamp, e.g. 20060409000000' );
+			$this->dieUsage( 'Incorrect timestamp format' );
 		}
+	}
+
+	function dieUsage( $message ) {
+		$props = "";
+		foreach( $this->propGenerators as $property => $generator ) {
+			$props .= sprintf( "  %-20s - %s\n", $generator[0] ? $property : $property." (*)", $generator[2]);
+		}
+		$props .= "  (*) These properties return information about the whole site\n";
+
+		die( "\n   ------ Error: $message ------\n\n"
+			."Usage:\n"
+			."  query.php ? format=a & properties=b|c|d & titles=e|f|g & rvstart=timestamp & rvend=timestamp & rvlimit=num\n"
+			."\n"
+			."Example:\n"
+			."    query.php?format=xml&properties=links|templates&titles=User:Yurik\n"
+			."  This query will return a list of all links and templates used on the User:Yurik\n"
+			."\n"
+			."Supported Formats:\n"
+			."  " . implode(', ', array_keys( $this->mimeTypes )) . "\n"
+			."\n"
+			."Supported Properties:\n"
+			.$props
+			."\n"
+			."Notes:\n"
+			."  - spaces above are just for ease of reading\n"
+			."  - properties and format are the only required parameters\n"
+			."  - revisions property supports additional parameters:\n"
+			."      rvstart, rvend - limits revisions by start and/or end time. The timestamp format is 14 characters.\n"
+			."                       example: '20060409000000' (year month date hour minute second)\n"
+			."      rvlimit        - the number of revisions per title to return. Default is 50.\n"
+			."      rvcomments     - (true/false) - if true, includes the revision comment in the output. Default is false.\n"
+			."\n"
+			."Credits:\n"
+			."  This extension came as the result of IRC discussion between Yuri Astrakhan (en:Yurik), Tim Starling (en:Tim Starling), and Daniel Kinzler(de:Duesentrieb)\n"
+			."  The extension was first implemented by Tim to provide interlanguage links and history.\n"
+			."  It was later completelly rewritten by Yuri to allow for modular properties, meta information, and various formatting options.\n"
+			."\n"
+			."  The code is maintained by Yurik. You can leave your comments at http://en.wikipedia.org/wiki/User_talk:Yurik\n"
+			);
 	}
 }
 
