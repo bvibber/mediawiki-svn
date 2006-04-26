182b33f835acf95815e1bf894090399b7f8df97a
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-04-26 10:05:34 +0000

    Liberate extension I find useful for development and testing
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@13871 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PurgeCache.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Special page used to wipe the OBJECTCACHE table
+ * I use it on test wikis when I'm fiddling about with things en masse that could be cached
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Rob Church <robchur@gmail.com>
+ * @licence Public domain
+ */
+ 
+if( defined( 'MEDIAWIKI' ) ) {
+	
+	require_once( 'SpecialPage.php' );
+	$wgExtensionFunctions[] = 'efPurgeCache';
+	$wgAvailableRights[] = 'purgecache';
+	$wgGroupPermissions['developer']['purgecache'] = true;
+	
+	function efPurgeCache() {
+		global $wgMessageCache;
+		$wgMessageCache->addMessage( 'purgecache', 'Purge cache' );
+		SpecialPage::addPage( new PurgeCache() );
+	}
+	
+	class PurgeCache extends SpecialPage {
+	
+		function PurgeCache() {
+			SpecialPage::SpecialPage( 'PurgeCache', 'purgecache' );
+		}
+		
+		function execute() {
+			global $wgUser, $wgRequest, $wgOut;
+			$this->setHeaders();
+			if( $wgUser->isAllowed( 'purgecache' ) ) {
+				if( $wgRequest->getCheck( 'purge' ) && $wgRequest->wasPosted() ) {
+					$dbw =& wfGetDB( DB_MASTER );
+					$dbw->delete( 'objectcache', '*', 'PurgeCache::execute' );
+					$wgOut->addWikiText( 'The cache has been purged.' );
+				} else {
+					$wgOut->addWikiText( 'This will purge the cache tables.' );
+					$wgOut->addHtml( $this->makeForm() );
+				}
+			} else {
+				$wgOut->permissionRequired( 'purgecache' );
+			}
+		}
+		
+		function makeForm() {
+			$self = $this->getTitle();
+			$form  = wfOpenElement( 'form', array( 'method' => 'post', 'action' => $self->getLocalUrl() ) );
+			$form .= wfElement( 'input', array( 'type' => 'submit', 'name' => 'purge', 'value' => 'Purge' ) );
+			$form .= wfCloseElement( 'form' );
+			return $form;
+		}
+	
+	}
+	
+} else {
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	die( -1 );
+}
+
+?>
