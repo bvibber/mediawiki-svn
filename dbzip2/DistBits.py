ccf8548baaf92285417f1a75ff942cb106d82152
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-06-04 22:15:14 +0000

    Quick hack to support decompression. Will fail on files with multiple blocks so far, but this gets the remote server ready to test once input splitting is available.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@14573 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- dbzip2/DistBits.py
+++ dbzip2/DistBits.py
@@ -10,7 +10,7 @@ import struct
 # Client sends:
 # COMP <version>
 #   Requests remote compression. Version is 32-bit big-endian integer,
-#   should be 1. Unsupported version on the server should drop or return
+#   should be 2. Unsupported version on the server should drop or return
 #   error.
 # ALGO <algorithm>
 #   "bzip2": Create a full bzip2 stream. Default block size is 900k.
@@ -23,12 +23,18 @@ import struct
 #   the last set ALGO and BLOK settings continue to apply.
 #   Multiple requests may be pipelined if the sides support it, but this
 #   is not required; both clients and servers may block on each request.
+# SMAL <data>
+#   Compressed input data. For each SMAL atom sent by the client, the
+#   server will return a HUGE atom containing uncompressed data. You may
+#   issue multiple such requests, and can interleave SMAL and HUGE requests.
 # CLOS <no data>
 #   Close the connection. (Optional?)
 #
 # Server sends back one of:
 # SMAL <data>
 #   Compressed output data.
+# HUGE <data>
+#   Uncompressed output data.
 # -or-
 # EROR <error string>
 #   Some error condition which can be reported gracefully.
