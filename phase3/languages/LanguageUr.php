8ff2fc815e397ce0332adcbb5431b5372a45b7e1
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-06-22 00:37:21 +0000

    copy over base phase 3 to embed media
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/embed_media@14890 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/languages/LanguageUr.php
@@ -0,0 +1,49 @@
+<?php
+/** Urdu (اردو)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+require_once( "LanguageUtf8.php" );
+
+if (!$wgCachedMessageArrays) {
+	require_once('MessagesUr.php');
+}
+
+class LanguageUr extends LanguageUtf8 {
+	private $mMessagesUr = null;
+
+	function __construct() {
+		parent::__construct();
+
+		global $wgAllMessagesUr;
+		$this->mMessagesUr =& $wgAllMessagesUr;
+	}
+
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesUr[$key] ) ) {
+			return $this->mMessagesUr[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
+
+	function getAllMessages() {
+		return $this->mMessagesUr;
+	}
+
+	function getDefaultUserOptions() {
+		$opt = parent::getDefaultUserOptions();
+		$opt["quickbar"] = 2; # Right-to-left
+		$opt["underline"] = 0; # Underline is hard to read in Arabic script
+		return $opt;
+	}
+
+	# For right-to-left language support
+	function isRTL() {
+		return true;
+	}
+}
+
+?>
