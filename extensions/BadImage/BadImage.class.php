9e37dbfe9093e694097daf2aea29f90e7d17ffa4
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-07-02 22:39:24 +0000

    * Class to manipulate the extension table
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@15252 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/BadImage/BadImage.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Class for manipulating the bad_image table
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Rob Church <robchur@gmail.com>
+ * @copyright © 2006 Rob Church
+ * @licence Copyright holder allows use of the code for any purpose
+ */
+
+class BadImageList {
+
+	function check( &$image ) {
+		$dbr =& wfGetDB( DB_SLAVE ); # This might need to be DB_MASTER in future
+		$res = $dbr->selectField( 'bad_images', 'COUNT(*) AS count', array( 'bil_name' => $image->getName() ), __METHOD__ );
+		return $res > 0;
+	}
+
+	function add( &$image, &$user, $reason ) {
+		$dbw =& wfGetDB( DB_MASTER );
+		$dbw->insert( 'bad_images', array( 'bil_name' => $image->getName(), 'bil_user' => $user->getId(), 'bil_reason' => $reason ), __METHOD__, 'IGNORE' );
+	}
+	
+	function remove( &$image ) {
+		$dbw =& wfGetDB( DB_MASTER );
+		$dbw->delete( 'bad_images', array( 'bil_name' => $image->getName() ), __METHOD__ );
+	}
+
+}
+
+?>
