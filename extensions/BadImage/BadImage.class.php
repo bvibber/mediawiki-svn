05cd1ac1a918ffcb3cc30363ddfa2d58308b675b
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-07-06 00:40:36 +0000

    * Support caching in shared memory to reduce database hits
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@15362 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BadImage/BadImage.class.php
+++ extensions/BadImage/BadImage.class.php
@@ -16,26 +16,50 @@ class BadImageList {
 		wfProfileIn( __METHOD__ );
 		static $titles = array();
 		if( !isset( $titles[$name] ) ) {
-			$dbr =& wfGetDB( DB_SLAVE ); # This might need to be DB_MASTER in future
-			$res = $dbr->selectField( 'bad_images', 'COUNT(*)', array( 'bil_name' => $name ), __METHOD__ );
-			$titles[$name] = $res > 0;
+			global $wgMemc;
+			$res = $wgMemc->get( BadImageList::key( $name ) );
+			if( $res ) {
+				$titles[$name] = $res == 'yes';
+			} else {
+				$titles[$name] = BadImageList::checkReal( $name );
+				BadImageList::cache( $name, $titles[$name] );
+			}			
 		}
 		wfProfileOut( __METHOD__ );
 		return $titles[$name];
 	}
 
+	function checkReal( $name ) {
+		$dbr =& wfGetDB( DB_SLAVE );
+		$res = $dbr->selectField( 'bad_images', 'COUNT(*)', array( 'bil_name' => $name ), __METHOD__ );
+		return $res > 0;
+	}
+	
 	function add( $name, $user, $reason ) {
 		wfProfileIn( __METHOD__ );
 		$dbw =& wfGetDB( DB_MASTER );
 		$dbw->insert( 'bad_images', array( 'bil_name' => $name, 'bil_user' => $user, 'bil_timestamp' => wfTimestampNow(), 'bil_reason' => $reason ), __METHOD__, 'IGNORE' );
+		$wgMemc->delete( BadImageList::key( $name ) );
 		wfProfileOut( __METHOD__ );
 	}
 	
 	function remove( $name ) {
+		global $wgMemc;
 		wfProfileIn( __METHOD__ );
 		$dbw =& wfGetDB( DB_MASTER );
 		$dbw->delete( 'bad_images', array( 'bil_name' => $name ), __METHOD__ );
+		$wgMemc->delete( BadImageList::key( $name ) );
 		wfProfileOut( __METHOD__ );
+	}
+
+	function cache( $name, $value ) {
+		global $wgMemc;
+		$wgMemc->set( BadImageList::key( $name ), $value ? 'yes' : 'no', 900 );
+	}
+	
+	function key( $name ) {
+		global $wgDBname;
+		return "{$wgDBname}:badimage:{$name}";	
 	}
 
 }
