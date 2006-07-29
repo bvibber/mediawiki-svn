a866ea2d3641058e6784d4a16c65b123bbd51037
david <david@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-07-29 11:40:09 +0000

    oops, forgot to add this in the last commit
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/liquidthreads@15878 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ skins/common/lqt.js
@@ -0,0 +1,29 @@
+function lqt_on_load() {
+    // Expand a thread initially on request from query string:
+    var query_regexp = /.*[?&]lqt_expand=([^&]*).*/;
+    var query_val = query_regexp.exec(location.search);
+    
+    if ( query_val.length == 2 ) {
+	var expand_id = 'lqt_thread_' + query_val[1];
+	var elem = (document.getElementById) ? document.getElementById(expand_id) : 
+               ((document.all) ? document.all(expand_id) : null);
+	if(elem) {
+	    elem.style.display = "block";
+	}
+    }
+    
+}
+
+addOnloadHook(lqt_on_load);
+
+function lqt_hide_show(that_id) {
+    var elem = (document.getElementById) ? document.getElementById(that_id) : 
+               ((document.all) ? document.all(that_id) : null);
+    if (elem) {
+        if ( elem.style.display == "none" ) {
+            elem.style.display = "block";
+        } else {
+            elem.style.display = "none";
+        }
+    }
+}
