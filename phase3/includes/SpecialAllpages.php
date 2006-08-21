bvibber/mediawiki-svn

commit 255fac513edf73abac39045536a02364fd5698bc

* (bug 7061) Format title on "from (page)" links on Special:Allpages
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16124 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/SpecialAllpages.php
@@ -276,11 +276,9 @@ function showChunk( $namespace = NS_MAIN, $from, $including = false ) {
 				$sk->makeKnownLink( $wgContLang->specialPage( "Allpages" ),
 					wfMsgHtml ( 'allpages' ) );
 		if ( isset($dbr) && $dbr && ($n == $this->maxPerPage) && ($s = $dbr->fetchObject( $res )) ) {
-			$namespaceparam = $namespace ? "&namespace=$namespace" : "";
-			$out2 .= " | " . $sk->makeKnownLink(
-				$wgContLang->specialPage( "Allpages" ),
-				wfMsgHtml ( 'nextpage', $s->page_title ),
-				"from=" . wfUrlEncode ( $s->page_title ) . $namespaceparam );
+			$self = Title::makeTitle( NS_SPECIAL, 'Allpages' );
+			$q = 'from=' . $t->getPrefixedUrl() . ( $namespace ? '&namespace=' . $namespace : '' );
+			$out2 .= ' | ' . $sk->makeKnownLinkObj( $self, wfMsgHtml( 'nextpage', $t->getText() ), $q );
 		}
 		$out2 .= "</td></tr></table><hr />";
 	}

phase3/RELEASE-NOTES
@@ -143,6 +143,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * Added [[:Image:Foo.png]] style links to the pagelinks table
 * Avoid duplicate revision imports with Special:Import
 * (bug 7054) Validate email address before sending email confirmation message
+* (bug 7061) Format title on "from (page)" links on Special:Allpages
 
 == Languages updated ==
 

commit 03597ba0f6c3c9acbd76396b82377e697f27827b

(bug 7085) Fix regression introduced in r16124
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16155 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/SpecialAllpages.php
@@ -277,7 +277,7 @@ function showChunk( $namespace = NS_MAIN, $from, $including = false ) {
 					wfMsgHtml ( 'allpages' ) );
 		if ( isset($dbr) && $dbr && ($n == $this->maxPerPage) && ($s = $dbr->fetchObject( $res )) ) {
 			$self = Title::makeTitle( NS_SPECIAL, 'Allpages' );
-			$q = 'from=' . $t->getPrefixedUrl() . ( $namespace ? '&namespace=' . $namespace : '' );
+			$q = 'from=' . $t->getPartialUrl() . ( $namespace ? '&namespace=' . $namespace : '' );
 			$out2 .= ' | ' . $sk->makeKnownLinkObj( $self, wfMsgHtml( 'nextpage', $t->getText() ), $q );
 		}
 		$out2 .= "</td></tr></table><hr />";