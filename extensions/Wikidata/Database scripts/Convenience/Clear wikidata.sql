85e9dfd4ff1fa9a73f78ebcd2e0b56d0058ce444
proes <proes@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-09-01 09:09:47 +0000

    Truncate tables to reset auto_increment value
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16317 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Convenience/Clear wikidata.sql
+++ extensions/Wikidata/Database scripts/Convenience/Clear wikidata.sql
@@ -1,16 +1,18 @@
-DELETE FROM uw_alt_meaningtexts;
-DELETE FROM uw_collection_contents;
-DELETE FROM uw_collection_language;
-DELETE FROM uw_collection_ns;
-DELETE FROM uw_defined_meaning;
-DELETE FROM uw_dm_text_attribute_values;
-DELETE FROM uw_expression_ns;
-DELETE FROM uw_meaning_relations;
-DELETE FROM uw_syntrans;
-DELETE FROM uw_syntrans_relations;
-DELETE FROM uw_versions_ns_collection;
-DELETE FROM uw_versions_ns_gemet;
-DELETE FROM transactions;
+TRUNCATE TABLE uw_alt_meaningtexts;
+TRUNCATE TABLE uw_collection_contents;
+TRUNCATE TABLE uw_collection_language;
+TRUNCATE TABLE uw_collection_ns;
+TRUNCATE TABLE uw_defined_meaning;
+TRUNCATE TABLE uw_dm_text_attribute_values;
+TRUNCATE TABLE uw_expression_ns;
+TRUNCATE TABLE uw_meaning_relations;
+TRUNCATE TABLE uw_syntrans;
+TRUNCATE TABLE uw_syntrans_relations;
+TRUNCATE TABLE uw_versions_ns_collection;
+TRUNCATE TABLE uw_versions_ns_gemet;
+TRUNCATE TABLE transactions;
 
 DELETE translated_content, text FROM translated_content, text WHERE translated_content.text_id=text.old_id;
 DELETE page, revision FROM page, revision WHERE page.page_namespace >= 16 AND revision.rev_page=page.page_id;
+
+TRUNCATE TABLE translated_content;
