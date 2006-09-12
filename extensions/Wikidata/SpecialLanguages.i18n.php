8da65253eb8d638cb4507e0198b75a4982e9334a
collinj <collinj@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-09-12 19:49:08 +0000

    Added German translation
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16497 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/SpecialLanguages.i18n.php
+++ extensions/Wikidata/SpecialLanguages.i18n.php
@@ -11,6 +11,9 @@ $wgLanguageManagerMessages = array();
 $wgLanguageManagerMessages['en'] = array(
 	'langman_title' => 'Language manager',
 );
+$wgLanguageManagerMessages['de'] = array(
+	'langman_title' => 'Sprachmanager',
+);
 $wgLanguageManagerMessages['he'] = array(
 	'langman_title' => 'מנהל שפות',
 );
