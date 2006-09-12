1fc11535eccea85aa14103ae05a2b673ea59a9a5
erik <erik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-09-12 10:20:39 +0000

    quick hack for adding languages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16488 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Wikidata/SpecialLanguages.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation file for Renameuser extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+*/
+
+$wgLangMessages = array();
+
+$wgLangMessages['en'] = array(
+	'langman_title'=>'Language manager',
+);
+?>
