4cdead171e2ed463b40e504bc625b0b49ca59a53
mark <mark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-10-04 18:01:46 +0000

    Initial import.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16800 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ debs/wikimedia-lvs-realserver/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the wikimedia-lvs-realserver package
+#
+0 4	* * *	root	wikimedia-lvs-realserver_maintenance
---
+++ debs/wikimedia-lvs-realserver/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(wikimedia-lvs-realserver):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="wikimedia-lvs-realserver" command="/usr/bin/wikimedia-lvs-realserver"
