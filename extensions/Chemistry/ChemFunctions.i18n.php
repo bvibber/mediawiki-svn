9271ac43c9d12a3850901d6a3a8e6a8383009a1e
collinj <collinj@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-10-07 13:03:24 +0000

    * Added German localisation * replaced <br> with <br />
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@16834 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Chemistry/ChemFunctions.i18n.php
+++ extensions/Chemistry/ChemFunctions.i18n.php
@@ -52,10 +52,30 @@ $wgChemFunctions_Messages['en'] = array(
 	'ChemFunctions_ECNumber' => 'ECNumber',
 	'ChemFunctions_Formula' => 'Formula',
 	'ChemFunctions_Name' => 'IUPAC Name',
-	'ChemFunctions_ChemicalDataList' => 'Below is a list of links to sites that may provide information about the chemical substance you are looking for.<br><br>
-						   * <a href="http://webbook.nist.gov/cgi/cbook.cgi?ID=$MIXCASNameFormula&Units=SI">Compound $MIXCASNameFormula at NIST</a><br>
-						   * <a href="http://ptcl.chem.ox.ac.uk/MSDS/">MSDS at the Oxford University (UK) (not searchable)</a><br>
-						  '
+	'ChemFunctions_ChemicalDataList' => 'Below is a list of links to sites that may provide information about the chemical substance you are looking for.<br /><br />
+	* <a href="http://webbook.nist.gov/cgi/cbook.cgi?ID=$MIXCASNameFormula&Units=SI">Compound $MIXCASNameFormula at NIST</a><br />
+	* <a href="http://ptcl.chem.ox.ac.uk/MSDS/">MSDS at the Oxford University (UK) (not searchable)</a><br />'
+);
+
+$wgChemFunctions_Messages['de'] = array(
+	'ChemFunctions_Chemicalsources' => 'Chemische Quellen',
+	'ChemFunctions_ChemicalsourcesPage' => 'Chemische Quellen',
+	'ChemFunctions_CAS' => 'CAS Nummer',
+	'ChemFunctions_EINECS' => 'Einecs',
+	'ChemFunctions_CHEBI' => 'CHEBI',
+	'ChemFunctions_PubChem' => 'PubChem',
+	'ChemFunctions_SMILES' => 'SMILES',
+	'ChemFunctions_InChI' => 'InChI',
+	'ChemFunctions_RTECS' => 'RTECS',
+	'ChemFunctions_KEGG' => 'KEGG',
+	'ChemFunctions_ATCCode' => 'ATCCode',
+	'ChemFunctions_DrugBank' => 'DrugBank',
+	'ChemFunctions_ECNumber' => 'ECNumber',
+	'ChemFunctions_Formula' => 'Formula',
+	'ChemFunctions_Name' => 'IUPAC Name',
+	'ChemFunctions_ChemicalDataList' => 'Nachfolgend finden Sie Links zu Seiten, die eventuell Informationen über chemische Substanzen anbieten, nach denen Sie suchen:<br /><br />
+	* <a href="http://webbook.nist.gov/cgi/cbook.cgi?ID=$MIXCASNameFormula&Units=SI">Verbindung $MIXCASNameFormula auf NIST</a><br />
+	* <a href="http://ptcl.chem.ox.ac.uk/MSDS/">MSDS an der Oxford University (UK) (nicht durchsuchbar)</a><br />'
 );
 
 $wgChemFunctions_Messages['nl'] = array(
@@ -74,10 +94,9 @@ $wgChemFunctions_Messages['nl'] = array(
 	'ChemFunctions_ECNumber' => 'EC Nummer',
 	'ChemFunctions_Formula' => 'Formula',
 	'ChemFunctions_Name' => 'IUPAC Naam',
-	'ChemFunctions_ChemicalDataList' => 'Hieronder staat een lijst van pagina\'s, die meer informatie over de chemische verbinding kunnen verschaffen.<br><br>
-						   * <a href="http://webbook.nist.gov/cgi/cbook.cgi?ID=$MIXCASNameFormula&Units=SI">Verbinding $MIXCASNameFormula op de pagina van het NIST</a><br>
-						   * <a href="http://ptcl.chem.ox.ac.uk/MSDS/">MSDS op de pagina van de Oxford University (UK) (geen zoekresultaten)</a><br>
-						  '
+	'ChemFunctions_ChemicalDataList' => 'Hieronder staat een lijst van pagina\'s, die meer informatie over de chemische verbinding kunnen verschaffen.<br /><br />
+	* <a href="http://webbook.nist.gov/cgi/cbook.cgi?ID=$MIXCASNameFormula&Units=SI">Verbinding $MIXCASNameFormula op de pagina van het NIST</a><br />
+	* <a href="http://ptcl.chem.ox.ac.uk/MSDS/">MSDS op de pagina van de Oxford University (UK) (geen zoekresultaten)</a><br />'
 );
 
 ?>
