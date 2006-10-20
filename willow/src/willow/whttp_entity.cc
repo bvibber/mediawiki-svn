168c10064614f3aa65270aa6821a4fcd216e3c7a
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-10-20 18:36:33 +0000

    provide i386 assembler version of find_rn() for gcc & icc
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17134 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- willow/src/willow/whttp_entity.cc
+++ willow/src/willow/whttp_entity.cc
@@ -303,20 +303,47 @@ write_zlib_eof(http_entity *entity)
 	return 0;
 }
 
+#if (defined(__GNUC__) || defined(__INTEL_COMPILER)) && defined(__i386__)
+static char *
+find_rn(char *buf, char *end)
+{
+char *res;
+__asm__ (
+"1:\n"
+"cmpl %1,%2\n"
+"je 3f\n"
+"cmpb $13,(%1)\n"
+"jne 2f\n"
+"cmpb $10, 1(%1)\n"
+"jne 2f\n"
+"movl %1, %0\n"
+"jmp 4f\n"
+"2:\n"
+"incl %1\n"
+"jmp 1b\n"
+"3:\n"
+"movl $0, %1\n"
+"4:\n"
+: "=d" (res) : "a" (buf), "b" (end)
+);
+        return res;
+}
+#else
 static char *
 find_rn(char *buf, char *end)
 {
 char	*s;
 	for (s = buf; s < end; s += 2) {
-		if (*s != '\r' && *s != '\n')
+		if (likely(*s != '\r' && *s != '\n'))
 			continue;
-		if ((s + 1 < end) && s[0] == '\r' && s[1] == '\n')
+		if (likely((s + 1 < end) && s[0] == '\r' && s[1] == '\n'))
 			return s;
-		if (s > buf && s[-1] == '\r' && s[0] == '\n')
+		if (likely(s > buf && s[-1] == '\r' && s[0] == '\n'))
 			return s - 1;
 	}
 	return NULL;
 }
+#endif
 
 static char *
 find_rnrn(char *buf, char *end)
