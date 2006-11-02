a38d9bf85bdba3e668b19e80e468a8b2a2b45923
nickj <nickj@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-11-02 06:25:00 +0000

    Chemistry Extension update patch from Dirk Beetstra; includes armouring against outputting multi-line HREF strings when given malicious <chemform> wikitext input.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17359 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Chemistry/ChemFunctions.php
+++ extensions/Chemistry/ChemFunctions.php
@@ -17,28 +17,28 @@
 if (!defined('MEDIAWIKI')) die();
 
 $wgExtensionCredits['Extension'][] = array(
-	'name' => 'ChemFunctions',
-	'description' => 'Adds the <chemform> tag, for chemical formulae',
-	'author' => 'Dirk Beetstra',
-	'url' => 'http://meta.wikimedia.org/wiki/Chemistry/ChemFunctions.php'
+    'name' => 'ChemFunctions',
+    'description' => 'Adds the <chemform> tag, for chemical formulae',
+    'author' => 'Dirk Beetstra',
+    'url' => 'http://meta.wikimedia.org/wiki/Chemistry/ChemFunctions.php'
 );
 
 /** Chemform wikipedia extension.
  *
  *   Formats the text between the tags as a chemical formula,
- *	First: all numericals are put in subscript.
- *	Second:  and - are put in superscript
- *	Third: all numericals preceding a + or a - are converted from subscript to superscript.
+ *    First: all numericals are put in subscript.
+ *    Second:  and - are put in superscript
+ *    Third: all numericals preceding a + or a - are converted from subscript to superscript.
  *
  * usage: <chemform searchfor="formula to be searched for" noprocess link wikilink>formula</chemform>
  *  (all parameters are optional).
  * Parameters:
  *   query: alternate (e.g. CAS-sorted) formula to search for (plain formula, e.g. "C12H22O11")
  *   noprocess: results in the text between the tags not to be processed, for 'difficult' formula's like:
- *	 "CuSO<sub>4</sub> . 10 H<sub>2</sub>O" , where the 10 should not be subscripted.
+ *     "CuSO<sub>4</sub> . 10 H<sub>2</sub>O" , where the 10 should not be subscripted.
  *   link: results in the text between the tags to be a link to special:chemicalsources.
- *	 N.B.  : use noprocess with the searchfor parameter, otherwise search results may (!) be garbage/broken links.
- *	 N.B.2 : the text between the tags is interpreted as HTML, not as wikitext!
+ *     N.B.  : use noprocess with the searchfor parameter, otherwise search results may (!) be garbage/broken links.
+ *     N.B.2 : the text between the tags is interpreted as HTML, not as wikitext!
  *   wikilink: makes the text between the tags a wikilink.
  *
  *   The query takes the value of query, or link, or wikilink (in that order)
@@ -49,85 +49,88 @@ $wgExtensionCredits['Extension'][] = array(
 $wgExtensionFunctions[] = "wfChemFormExtension";
 
 function wfChemFormExtension() {
-	global $wgParser;
-	$wgParser->setHook( "chemform", "RenderChemForm" );
+    global $wgParser;
+    $wgParser->setHook( "chemform", "RenderChemForm" );
 }
 
 function RenderChemForm( $input, $argv ) {
-	global $wgChemFunctions_Messages, $wgMessageCache;
+    global $wgChemFunctions_Messages, $wgMessageCache;
 
-	require_once( 'ChemFunctions.i18n.php' );
+    require_once( 'ChemFunctions.i18n.php' );
 
-	# add messages
-	global $wgMessageCache, $wgChemFunctions_Messages;
-	foreach( array_keys($wgChemFunctions_Messages) as $key ) {
-		$wgMessageCache->addMessages( $wgChemFunctions_Messages[$key], $key );
-	}
+    # add messages
+    global $wgMessageCache, $wgChemFunctions_Messages;
+    foreach( array_keys($wgChemFunctions_Messages) as $key ) {
+        $wgMessageCache->addMessages( $wgChemFunctions_Messages[$key], $key );
+    }
 
-	$link = false;
-	$wikilink = false;
-	$noprocess = false;
-	$searchfor = "";
+    $link = false;
+    $wikilink = false;
+    $noprocess = false;
+    $searchfor = "";
 
-	if ( isset( $argv["noprocess"] ) )
-		$noprocess = $argv["noprocess"];
+    if ( isset( $argv["noprocess"] ) )
+        $noprocess = $argv["noprocess"];
 
-	if ( isset( $argv["link"] ) )
-		$link =  $argv["link"];
+    if ( isset( $argv["link"] ) )
+        $link =  $argv["link"];
 
-	if ( isset($argv["wikilink"] ) )
-	   $wikilink = $argv["wikilink"];
+    if ( isset($argv["wikilink"] ) )
+       $wikilink = $argv["wikilink"];
 
-	if ( isset( $argv["query"] ) )
-		$searchfor = $argv["query"];
+    if ( isset( $argv["query"] ) )
+        $searchfor = $argv["query"];
 
-	if (!$searchfor) {
-		$searchfor = $input;
-		$searchfor = preg_replace( "/<.*?>/", "", $searchfor );
-		$searchfor = preg_replace( "/[\[\]]/", "", $searchfor );
-	}
-	$searchfor = str_replace(" ", "", $searchfor );
+    if (!$searchfor) {
+        $searchfor = $input;
+        $searchfor = preg_replace( "/<.*?>/", "", $searchfor );
+        $searchfor = preg_replace( "/[\[\]]/", "", $searchfor );
+    }
+    $searchfor = str_replace(" ", "", $searchfor );
 
-	if (!$searchfor )
-		$searchfor = $link;
-	if (!$searchfor )
-		$searchfor = $wikilink;
+    if (!$searchfor )
+        $searchfor = $link;
+    if (!$searchfor )
+        $searchfor = $wikilink;
 
-	$showthis = $input;
-	if (!$noprocess) {
-		$showthis = $input;
-		$showthis = preg_replace( "/<.*?>/", "", $showthis );								# Remove all tags
-		$showthis = preg_replace("/[0-9]+/", "<sub>$0</sub>", $showthis);					# All numbers down
-		$showthis = preg_replace("/[\+\-]/", "<sup>$0</sup>", $showthis);					# + and - up
-		$showthis = preg_replace("/<\/sub><sup>/", "", $showthis);						   # </sub><sup> should not occur
-		$showthis = preg_replace("/<sub>([0-9\+\-]+)<\/sup>/", "<sup>$1</sup>", $showthis);  # and <sub>whatever</sup> to <sup>..</sup>
-	}
+    $showthis = $input;
+    if (!$noprocess) {
+        $showthis = $input;
+        $showthis = preg_replace( "/<.*?>/", "", $showthis );                                # Remove all tags
+        $showthis = htmlentities( Sanitizer::StripAllTags ( $showthis ) );                   # tagstripping
+        $showthis = preg_replace("/[0-9]+/", "<sub>$0</sub>", $showthis);                    # All numbers down
+        $showthis = preg_replace("/[\+\-]/", "<sup>$0</sup>", $showthis);                    # + and - up
+        $showthis = preg_replace("/<\/sub><sup>/", "", $showthis);                           # </sub><sup> should not occur
+        $showthis = preg_replace("/<sub>([0-9\+\-]+)<\/sup>/", "<sup>$1</sup>", $showthis);  # and <sub>whatever</sup> to <sup>..</sup>
+    } else {
+        $showthis = htmlentities( Sanitizer::StripAllTags ( $showthis ) );
+    }
 
-	$showthis = htmlentities( $showthis);
-	$searchfor = htmlentities( $searchfor);
-	if (!$searchfor) {
-	   $searchfor = "Error, no text given";
-	}
-	if ( $link ) {
-		$title = Title::makeTitleSafe( NS_SPECIAL, 'Chemicalsources' );
-		$output = "<a href=\"" . $title->getFullUrl() . "?Formula=" . $searchfor .  "\">" . $showthis . "</a>";
-	} elseif ( $wikilink) {
-		$title = Title::makeTitleSafe( NS_MAIN, $searchfor );
-		if ($title) {
-			$revision = Revision::newFromTitle( $title );
-			if ($revision ) {
-			   $output = "<a href=\"" . $title->getFullUrl() . "\">" . $showthis . "</a>";
-			} else {
-			   $output = "<a href=\"" . $title->getFullUrl() . "?action=edit\" class=\"new\">" . $showthis . "</a>";
-			}
-		} else {
-			$output = wfMsg('ChemFunctions_ChemFormInputError');
-		}
-	} else {
-		$output = $showthis;
-	}
+    $searchfor = htmlentities( Sanitizer::StripAllTags ( $searchfor ) );
 
-	return $output;
+    if (!$searchfor) {
+       $searchfor = "Error, no text given";
+    }
+    if ( $link ) {
+        $title = Title::makeTitleSafe( NS_SPECIAL, 'Chemicalsources' );
+        $output = "<a href=\"" . $title->getFullUrl() . "?Formula=" . $searchfor .  "\">" . $showthis . "</a>";
+    } elseif ( $wikilink) {
+        $title = Title::makeTitleSafe( NS_MAIN, $searchfor );
+        if ($title) {
+            $revision = Revision::newFromTitle( $title );
+            if ($revision ) {
+               $output = "<a href=\"" . $title->getFullUrl() . "\">" . $showthis . "</a>";
+            } else {
+               $output = "<a href=\"" . $title->getFullUrl() . "?action=edit\" class=\"new\">" . $showthis . "</a>";
+            }
+        } else {
+            $output = wfMsg('ChemFunctions_ChemFormInputError');
+        }
+    } else {
+        $output = $showthis;
+    }
+
+    return $output;
 }
 
 #End of php.
