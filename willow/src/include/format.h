6c4920e382d1fa0725526411e099d1114c94cb7a
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-11-12 16:03:13 +0000

    missing files
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17572 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ willow/src/include/format.h
@@ -0,0 +1,85 @@
+/* @(#) $Id$ */
+/* This source code is in the public domain. */
+/*
+ * Willow: Lightweight HTTP reverse-proxy.
+ * format: typesafe printf-style string formatter.
+ */
+
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#if defined __SUNPRO_C || defined __DECC || defined __HP_cc
+# pragma ident "@(#)$Id$"
+#endif
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include <cerrno>
+using std::vector;
+using std::string;
+using std::ios;
+using std::runtime_error;
+using std::ostringstream;
+using std::strerror;
+
+#include "willow.h"
+
+struct invalid_format_string : runtime_error {
+	invalid_format_string(void) : runtime_error("invalid format string") {}
+};
+
+struct format
+{
+	format	(string const &);
+
+	template<typename T>
+	format& operator%(T const &);
+
+	string	str(void) const;
+		operator string(void) const;
+
+private:
+	void	parse_format_string(void);
+
+	enum argtype {
+		at_string,
+		at_decint,
+		at_hexint,
+		at_octint,
+		at_errno,
+	};
+	vector<argtype>	_argtypes;
+	vector<string>	_args;
+	string		_fmt;
+};
+
+template<typename T>
+format&
+format::operator % (T const &arg)
+{
+ostringstream	strm;
+	switch (_argtypes[_args.size()]) {
+	case at_string:
+		_args.push_back(lexical_cast<string>(arg));
+		return *this;
+	case at_decint:
+		break;
+	case at_octint:
+		strm << std::oct;
+		break;
+	case at_hexint:
+		strm << std::hex;
+		break;
+	case at_errno:
+		_args.push_back(strerror(errno));
+		return *this;
+	}
+	strm << arg;
+	_args.push_back(strm.str());
+	return *this;
+}
+
+#endif
---
+++ willow/src/willow/format.cc
@@ -0,0 +1,94 @@
+/* @(#) $Id$ */
+/* This source code is in the public domain. */
+/*
+ * Willow: Lightweight HTTP reverse-proxy.
+ * format: typesafe printf-style string formatter.
+ */
+
+#if defined __SUNPRO_C || defined __DECC || defined __HP_cc
+# pragma ident "@(#)$Id$"
+#endif
+
+#include "format.h"
+
+format::format(string const &str)
+	: _fmt(str)
+{
+	parse_format_string();
+}
+
+void
+format::parse_format_string(void)
+{
+int	i, len = _fmt.length();
+	for (i = 0; i < len; ++i) {
+		switch (_fmt[i]) {
+		case '%':
+			if (++i == len)
+				throw invalid_format_string();
+			switch (_fmt[i]) {
+			case 's':
+				_argtypes.push_back(at_string);
+				break;
+			case 'd':
+				_argtypes.push_back(at_decint);
+				break;
+			case 'x':
+				_argtypes.push_back(at_hexint);
+				break;
+			case 'o':
+				_argtypes.push_back(at_octint);
+				break;
+			case 'e':
+				_argtypes.push_back(at_errno);
+				break;
+			default:
+				throw invalid_format_string();
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+string
+format::str(void) const
+{
+int	i, len = _fmt.length();
+string	result;
+int	argn = 0;
+	for (i = 0; i < len; ++i) {
+		switch (_fmt[i]) {
+		case '%':
+			if (++i == len)
+				throw invalid_format_string();
+			result += _args[argn++];
+			break;
+		default:
+			result += _fmt[i];
+			break;
+		}
+	}
+	return result;
+}
+
+format::operator string (void) const
+{
+	return str();
+}
+
+#ifdef TEST
+int
+main(int argc, char *argv[])
+{
+char	**a = &argv[2];
+	format o(argv[1]);
+	while (*a)
+		if (atoi(*a) != 0)
+			o % atoi(*a++);
+		else
+			o % *a++;
+	std::cout << o.str() << '\n';
+}
+#endif
