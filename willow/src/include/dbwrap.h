6ab64eb3cecd658ebcea8935a6f61adf7c5cdedc
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-11-16 19:59:27 +0000

    template db::database on Datastore so the user can store value data outside bdb.  provide inline_data_store for default behaviour (putting data inside bdb)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17727 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- willow/src/include/dbwrap.h
+++ willow/src/include/dbwrap.h
@@ -24,184 +24,6 @@ using std::back_inserter;
 
 namespace db {
 
-template<typename Key, typename Value>
-struct database;
-struct transaction;
-
-struct environment : noncopyable {
-	static environment	*open(string const &path);
-	static environment	*create(string const &path);
-	~environment();
-
-	template<typename Key, typename Value>
-	database<Key, Value>  *open_database(string const &path);
-
-	template<typename Key, typename Value>
-	database<Key, Value> *create_database(string const &path);
-
-	int	error		(void) const;
-	string	strerror	(void) const;
-	void	close		(void);
-
-	struct transaction *transaction(void);
-
-private:
-	template<typename Key, typename Value>
-	friend struct database;
-	friend struct transaction;
-
-	explicit environment(string const &path, uint32_t flags);
-
-	DB_ENV	*_env;
-	int	 _error;
-};
-
-template<typename Key, typename Value>
-struct database : noncopyable {
-	int	error		(void) const;
-	string	strerror	(void) const;
-	void	close		(void);
-
-	~database();
-
-	bool	 put(Key const &key, Value const &value, transaction *);
-	bool	 put(Key const &key, Value const &value);
-	Value	*get(Key const &key, transaction *);
-	Value	*get(Key const &key);
-
-private:
-	friend struct environment;
-
-	explicit database(environment *env, string const &path, uint32_t flags);
-	static void errcall(DB_ENV const *, char const *pfx, char const *msg);
-
-	DB		*_db;
-	environment	*_env;
-	int		 _error;
-};
-
-struct transaction {
-	~transaction();
-
-	bool	 commit(void);
-	bool	 rollback(void);
-
-	int	 error(void) const;
-	string	 strerror(void) const;
-
-private:
-	friend struct environment;
-	template<typename Key, typename Value>
-	friend struct database;
-
-	transaction(environment *);
-	DB_TXN	*_txn;
-	int	 _error;
-};
-
-struct marshalling_buffer {
-	marshalling_buffer()
-		: _buf(NULL)
-		, _size(0)
-		, _bufsz(0)
-	{}
-
-	marshalling_buffer(char const *buf, uint32_t sz)
-		: _buf(const_cast<char *>(buf))
-		, _size(0)
-		, _bufsz(sz)
-	{}
-		
-	~marshalling_buffer(void) {
-	}
-
-	void reserve(size_t size) {
-		_bufsz = size;
-		_buf = new char[size];
-	}
-
-	template<typename T>
-	void append(T const &);
-
-	template<typename charT, typename traits, typename allocator>
-	void append(basic_string<charT, traits, allocator> const &);
-
-	void append_bytes(char const *buf, size_t s) {
-std::cout<<"appending "<<s<<" bytes\n";
-		assert(_size + s <= _bufsz);
-		memcpy(_buf + _size, buf, s);
-		_size += s;
-	}
-
-	char const *buffer(void) const {
-		return _buf;
-	}
-
-	size_t size(void) const {
-		return _size;
-	}
-
-	template<typename T>
-	bool extract(T &);
-
-	template<typename charT, typename traits, typename allocator>
-	bool extract(basic_string<charT, traits, allocator> &);
-
-	bool extract_bytes(char *b, size_t s) {
-std::cout<<"extracting "<<s<<" bytes\n";
-		if (_size + s > _bufsz)
-			return false;
-		memcpy(b, _buf + _size, s);
-		_size += s;
-		return true;
-	}
-
-private:
-	char	*_buf;
-	size_t	 _size;
-	size_t	 _bufsz;
-	bool	 _delete;
-};
-
-template<typename T>
-void
-marshalling_buffer::append(T const &o) {
-	append_bytes((char const *)&o, sizeof(o));
-}
-
-template<>
-void
-marshalling_buffer::append<imstring>(imstring const &o);
-
-template<typename charT, typename traits, typename allocator>
-void
-marshalling_buffer::append(basic_string<charT, traits, allocator> const &s) {
-	append<size_t>(s.size() * sizeof(charT));
-	append_bytes(s.data(), s.size() * sizeof(charT));
-}
-
-template<typename T>
-bool
-marshalling_buffer::extract(T &o)
-{
-	return extract_bytes((char *) &o, sizeof(o));
-}
-
-template<typename charT, typename traits, typename allocator>
-bool
-marshalling_buffer::extract(basic_string<charT, traits, allocator> &s)
-{
-size_t	sz;
-	if (!extract<size_t>(sz))
-		return false;
-	if (_size + sz > _bufsz)
-		return false;
-	s.reserve(sz);
-	copy(_buf + _size, _buf + _size + sz, back_inserter<charT>(s));
-	_size += sz;
-	return true;
-}
-
 template<typename T>
 struct marshaller {
 };
@@ -248,11 +70,187 @@ struct marshaller<string> {
 	}
 };
 
-template<typename Key, typename Value>		
+template<typename T>
+struct inline_data_store {
+	pair<char const *, uint32_t> store(T const &o) {
+	marshaller<T>	m;
+		return m.marshall(o);
+	}
+
+	T *retrieve(pair<char const *, uint32_t> const &d) {
+	marshaller<T>	m;
+		return m.unmarshall(d);
+	}
+};
+
+
+template<typename Key, typename Value, typename Datastore = inline_data_store<Value> >
+struct database;
+struct transaction;
+
+struct environment : noncopyable {
+	static environment	*open(string const &path);
+	static environment	*create(string const &path);
+	~environment();
+
+	template<typename Key, typename Value>
+	database<Key, Value>  *open_database(string const &path);
+	template<typename Key, typename Value, typename Datastore>
+	database<Key, Value, Datastore>  *open_database(string const &path);
+
+	template<typename Key, typename Value>
+	database<Key, Value> *create_database(string const &path);
+	template<typename Key, typename Value, typename Datastore>
+	database<Key, Value, Datastore> *create_database(string const &path);
+
+	int	error		(void) const;
+	string	strerror	(void) const;
+	void	close		(void);
+
+	struct transaction *transaction(void);
+
+private:
+	template<typename Key, typename Value, typename Datastore>
+	friend struct database;
+	friend struct transaction;
+
+	explicit environment(string const &path, uint32_t flags);
+
+	DB_ENV	*_env;
+	int	 _error;
+};
+
+template<typename Key, typename Value, typename Datastore>
+struct database : noncopyable {
+	int	error		(void) const;
+	string	strerror	(void) const;
+	void	close		(void);
+
+	~database();
+
+	bool	 put(Key const &key, Value const &value, transaction *);
+	bool	 put(Key const &key, Value const &value);
+	Value	*get(Key const &key, transaction *);
+	Value	*get(Key const &key);
+
+private:
+	friend struct environment;
+
+	explicit database(environment *env, string const &path, uint32_t flags);
+	static void errcall(DB_ENV const *, char const *pfx, char const *msg);
+
+	DB		*_db;
+	environment	*_env;
+	int		 _error;
+};
+
+struct transaction {
+	~transaction();
+
+	bool	 commit(void);
+	bool	 rollback(void);
+
+	int	 error(void) const;
+	string	 strerror(void) const;
+
+private:
+	friend struct environment;
+	template<typename Key, typename Value, typename Datastore>
+	friend struct database;
+
+	transaction(environment *);
+	DB_TXN	*_txn;
+	int	 _error;
+};
+
+struct marshalling_buffer {
+	marshalling_buffer()
+		: _buf(NULL)
+		, _size(0)
+		, _bufsz(0)
+	{}
+
+	marshalling_buffer(char const *buf, uint32_t sz)
+		: _buf(const_cast<char *>(buf))
+		, _size(0)
+		, _bufsz(sz)
+	{}
+		
+	~marshalling_buffer(void) {
+	}
+
+	void reserve(size_t size) {
+		_bufsz = size;
+		_buf = new char[size];
+	}
+
+	template<typename T>
+	void append(T const &);
+
+	template<typename charT, typename traits, typename allocator>
+	void append(basic_string<charT, traits, allocator> const &);
+
+	void append_bytes(char const *buf, size_t s) {
+		assert(_size + s <= _bufsz);
+		memcpy(_buf + _size, buf, s);
+		_size += s;
+	}
+
+	char const *buffer(void) const {
+		return _buf;
+	}
+
+	size_t size(void) const {
+		return _size;
+	}
+
+	template<typename T>
+	bool extract(T &);
+
+	template<typename charT, typename traits, typename allocator>
+	bool extract(basic_string<charT, traits, allocator> &);
+
+	bool extract_bytes(char *b, size_t s) {
+		if (_size + s > _bufsz)
+			return false;
+		memcpy(b, _buf + _size, s);
+		_size += s;
+		return true;
+	}
+
+private:
+	char	*_buf;
+	size_t	 _size;
+	size_t	 _bufsz;
+	bool	 _delete;
+};
+
+template<typename T>
+void
+marshalling_buffer::append(T const &o)
+{
+	append_bytes((char const *)&o, sizeof(o));
+}
+
+template<typename T>
+bool
+marshalling_buffer::extract(T &o)
+{
+	return extract_bytes((char *) &o, sizeof(o));
+}
+
+template<typename Key, typename Value>
 database<Key, Value> *
 environment::open_database(string const &name)
 {
 	return new database<Key, Value>(this, name, 0);
+}
+
+template<typename Key, typename Value, typename Datastore>
+database<Key, Value, Datastore> *
+environment::open_database(string const &name)
+{
+	return new database<Key, Value, Datastore>(this, name, 0);
 }
 
 template<typename Key, typename Value>
@@ -262,8 +260,15 @@ environment::create_database(string const &name)
 	return new database<Key, Value>(this, name, DB_CREATE);
 }
 
-template<typename Key, typename Value>
-database<Key, Value>::database(environment *env, string const &path, uint32_t flags)
+template<typename Key, typename Value, typename Datastore>
+database<Key, Value, Datastore> *
+environment::create_database(string const &name)
+{
+	return new database<Key, Value, Datastore>(this, name, DB_CREATE);
+}
+
+template<typename Key, typename Value, typename Datastore>
+database<Key, Value, Datastore>::database(environment *env, string const &path, uint32_t flags)
 	: _env(env)
 {
 	_error = db_create(&_db, env->_env, 0);
@@ -284,9 +289,9 @@ database<Key, Value>::database(environment *env, string const &path, uint32_t fl
 	_db->set_errcall(_db, &database::errcall);
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 void
-database<Key, Value>::errcall(DB_ENV const *, char const *pfx, char const *msg)
+database<Key, Value, Datastore>::errcall(DB_ENV const *, char const *pfx, char const *msg)
 {
 	if (pfx)
 		wlog(WLOG_WARNING, format("%s: %s") % pfx % msg);
@@ -294,37 +299,37 @@ database<Key, Value>::errcall(DB_ENV const *, char const *pfx, char const *msg)
 		wlog(WLOG_WARNING, msg);
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 int
-database<Key, Value>::error(void) const
+database<Key, Value, Datastore>::error(void) const
 {
 	return _error;
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 string
-database<Key, Value>::strerror(void) const
+database<Key, Value, Datastore>::strerror(void) const
 {
 	return db_strerror(_error);
 }
 
-template<typename Key, typename Value>
-database<Key, Value>::~database(void)
+template<typename Key, typename Value, typename Datastore>
+database<Key, Value, Datastore>::~database(void)
 {
 	if (_db)
 		_db->close(_db, 0);
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 bool
-database<Key, Value>::put(Key const &key, Value const &value)
+database<Key, Value, Datastore>::put(Key const &key, Value const &value)
 {
 	return put (key, value, NULL);
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 bool
-database<Key, Value>::put(Key const &key, Value const &value, transaction *txn)
+database<Key, Value, Datastore>::put(Key const &key, Value const &value, transaction *txn)
 {
 pair<char const *, uint32_t>	mkey, mvalue;
 DBT				dbkey, dbvalue;
@@ -348,16 +353,16 @@ marshaller<Value>		valuemarsh;
 	return true;
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 Value *
-database<Key, Value>::get(Key const &key)
+database<Key, Value, Datastore>::get(Key const &key)
 {
 	return get(key, NULL);
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 Value *
-database<Key, Value>::get(Key const &key, transaction *txn)
+database<Key, Value, Datastore>::get(Key const &key, transaction *txn)
 {
 pair<char const *, uint32_t>	mkey;
 DBT				dbkey, dbvalue;
@@ -379,9 +384,9 @@ Value	*ret;
 	return ret;
 }
 
-template<typename Key, typename Value>
+template<typename Key, typename Value, typename Datastore>
 void
-database<Key, Value>::close(void)
+database<Key, Value, Datastore>::close(void)
 {
 	_db->close(_db, 0);
 	_db = NULL;
