9b79f3094ee76763b3f39624d01bc20d508231cf
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-11-25 07:23:11 +0000

    expression parser can be vastly simplified using Phoenix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17919 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- willow/src/willow/expr.cc
+++ willow/src/willow/expr.cc
@@ -21,7 +21,12 @@
 #include <stack>
 #include <functional>
 #include <stdexcept>
+
 #include <boost/spirit.hpp>
+#include <boost/spirit/attribute.hpp>
+#include <boost/spirit/phoenix.hpp>
+#include <boost/spirit/phoenix/casts.hpp>
+#include <boost/spirit/phoenix/closures.hpp>
 #include <boost/shared_ptr.hpp>
 
 #include "util.h"
@@ -40,186 +45,18 @@ using boost::spirit::space_p;
 using boost::spirit::str_p;
 using boost::spirit::chset;
 using boost::spirit::symbols;
+using boost::spirit::closure;
 using boost::shared_ptr;
+using phoenix::var;
+using phoenix::arg1;
+using phoenix::arg2;
 
-template<typename T>
-struct bitwise_and {
-	T operator() (T const &a, T const &b) const {
-		return a & b;
-	}
+struct expr_closure : closure<expr_closure, int64_t> {
+	member1	val;
 };
 
-template<typename T>
-struct bitwise_or {
-	T operator() (T const &a, T const &b) const {
-		return a | b;
-	}
-};
-
-template<typename T>
-struct bitwise_xor {
-	T operator() (T const &a, T const &b) const {
-		return a ^ b;
-	}
-};
-
-template<typename T>
-struct bitwise_not {
-	T operator() (T const &a) const {
-		return ~a;
-	}
-};
-
-template<typename T>
-struct shift_left {
-	T operator() (T const &a, T const &b) const {
-		return a << b;
-	}
-};
-
-template<typename T>
-struct shift_right {
-	T operator() (T const &a, T const &b) const {
-		return a >> b;
-	}
-};
-
-struct expression_parser_impl : public grammar<expression_parser_impl> {
-	typedef stack<int64_t> stack_t;
-	struct operation {
-		virtual ~operation() {}
-		virtual void execute(stack_t &stack) = 0;
-	};
-
-	typedef vector<shared_ptr<operation> > program_t;
-
-	template<int64_t value>
-	struct op_literal : operation {
-		virtual void execute(stack_t &s) {
-			s.push(value);
-		}
-
-		struct add {
-		program_t &_program;
-			add(program_t &p)
-				: _program(p)
-			{}
-		
-			void operator() (int64_t) const {
-				_program.push_back(shared_ptr<operation>(
-					new op_literal<value>));
-			}
-			void operator() (string::const_iterator,
-					 string::const_iterator) const {
-				_program.push_back(shared_ptr<operation>(
-					new op_literal<value>));
-			}
-		};
-	};
-
-	template<typename func>
-	struct binary_operator : operation {
-	func		 _f;
-		virtual void execute(stack_t &s) {
-		int64_t	a, b, r;
-			/* note that args are pushed in reverse order -- so 
-			 * the first op argument is the second on the stack
-			 */
-			b = s.top();
-			s.pop();
-			a = s.top();
-			s.pop();
-			r = _f(a, b);
-			s.push(r);
-		}
-
-		struct add {
-		program_t &_program;
-			add(program_t &p)
-				: _program(p)
-			{}
-		
-			void operator() (string::const_iterator,
-					 string::const_iterator) const {
-				_program.push_back(shared_ptr<operation>(
-					new binary_operator<func>));
-			}
-		};
-	};
-
-	template<typename func>
-	struct unary_operator : operation {
-	func	_f;
-		virtual void execute(stack_t &s) {
-		int64_t	v, r;
-			v = s.top();
-			s.pop();
-			r = _f(v);
-			s.push(r);
-		}
-
-		struct add {
-		program_t &_program;
-			add(program_t &p)
-				: _program(p)
-			{}
-		
-			void operator() (string::const_iterator,
-					 string::const_iterator) const {
-				_program.push_back(shared_ptr<operation>(
-					new unary_operator<func>));
-			}
-		};
-	};
-
-	struct op_value : operation {
-	int64_t	v;
-		op_value(int64_t v_) : v(v_) {}
-		virtual void execute(stack_t &s) {
-			s.push(v);
-		}
-
-		struct add {
-		program_t &_program;
-			add(program_t &p)
-				: _program(p)
-			{}
-	
-			void operator() (int64_t v) const {
-				_program.push_back(shared_ptr<operation>(
-					new op_value(v)));
-			}
-		};
-	};
-
-	typedef binary_operator<std::plus           <int64_t> > op_add;
-	typedef binary_operator<std::minus          <int64_t> > op_sub;
-	typedef binary_operator<std::multiplies     <int64_t> > op_mul;
-	typedef binary_operator<std::divides        <int64_t> > op_div;
-	typedef binary_operator<std::modulus        <int64_t> > op_mod;
-	typedef binary_operator<std::logical_and    <int64_t> > op_land;
-	typedef binary_operator<std::logical_or     <int64_t> > op_lor;
-	typedef binary_operator<std::less           <int64_t> > op_lt;
-	typedef binary_operator<std::greater        <int64_t> > op_gt;
-	typedef binary_operator<std::less_equal     <int64_t> > op_le;
-	typedef binary_operator<std::greater_equal  <int64_t> > op_ge;
-	typedef binary_operator<std::equal_to       <int64_t> > op_eq;
-	typedef binary_operator<std::not_equal_to   <int64_t> > op_neq;
-	typedef binary_operator<bitwise_and         <int64_t> > op_band;
-	typedef binary_operator<bitwise_or          <int64_t> > op_bor;
-	typedef binary_operator<bitwise_xor         <int64_t> > op_bxor;
-	typedef binary_operator<shift_left          <int64_t> > op_lsht;
-	typedef binary_operator<shift_right         <int64_t> > op_rsht;
-
-	typedef unary_operator<bitwise_not          <int64_t> > op_bnot;
-	typedef unary_operator<std::negate          <int64_t> > op_neg;
-	typedef unary_operator<std::logical_not     <int64_t> > op_not;
-	
-	mutable program_t _program;
-	stack_t stack;
-	program_t &program(void) const {
-		return _program;
-	}
+struct expression_parser_impl : 
+	grammar<expression_parser_impl, expr_closure::context_t> {
 
 	symbols<int64_t> variables;
 
@@ -237,57 +74,96 @@ struct expression_parser_impl : public grammar<expression_parser_impl> {
 	}
 
 	int64_t run(string const &str) {
-		if (!boost::spirit::parse(str.begin(), str.end(), *this, space_p).full)
+	int64_t	n;
+		if (!boost::spirit::parse(str.begin(), str.end(), 
+			(*this)[var(n) = arg1], space_p).full)
 			throw expr::syntax_error();
-
-	program_t::iterator it = _program.begin(), end = _program.end();
-		for (; it != end; ++it)
-			(*it)->execute(stack);
-		return stack.top();
+		return n;
 	}
 
 	template<typename scanner>
 	struct definition {
-		rule<scanner> const &start(void) const { return expr; }
-		rule<scanner>	add_expr, atom, expr, mod_expr, eq_expr, neq_expr,
+		rule<scanner> 
+			const &start(void) const { return top; }
+
+		rule<scanner, expr_closure::context_t>
+			add_expr, atom, mod_expr, eq_expr, neq_expr,
 			mul_expr, div_expr, sub_expr, land_expr, lor_expr,
 			lt_expr, gt_expr, le_expr, ge_expr, band_expr,
-			bxor_expr, bor_expr, lsht_expr, rsht_expr, identifier;
+			bxor_expr, bor_expr, lsht_expr, rsht_expr, expr;
+		rule<scanner> top, identifier;
 
 		definition(expression_parser_impl const &self) {
 identifier = chset<>("a-zA-Z_") >> *chset<>("a-zA-Z0-9_");
 
-expr = lor_expr;
+top = expr[self.val = arg1];
 
-lor_expr  = land_expr >> *(("||" >> land_expr)[ op_lor::add(self.program())]);
-land_expr = band_expr >> *(("&&" >> band_expr)[op_land::add(self.program())]);
-band_expr = bxor_expr >> *(('&'  >> bxor_expr)[op_band::add(self.program())]);
-bxor_expr =  bor_expr >> *(('^'  >>  bor_expr)[op_bxor::add(self.program())]);
-bor_expr  =   lt_expr >> *(('|'  >>   lt_expr)[ op_bor::add(self.program())]);
-lt_expr   =   gt_expr >> *(('<'  >>   gt_expr)[  op_lt::add(self.program())]);
-gt_expr   =   le_expr >> *(('>'  >>   le_expr)[  op_gt::add(self.program())]);
-le_expr   =   ge_expr >> *(("<=" >>   ge_expr)[  op_le::add(self.program())]);
-ge_expr   =  neq_expr >> *((">=" >>  neq_expr)[  op_ge::add(self.program())]);
-neq_expr  =   eq_expr >> *(("!=" >>   eq_expr)[ op_neq::add(self.program())]);
-eq_expr   = lsht_expr >> *(("==" >> lsht_expr)[  op_eq::add(self.program())]);
-lsht_expr = rsht_expr >> *(("<<" >> rsht_expr)[op_lsht::add(self.program())]);
-rsht_expr =  add_expr >> *((">>" >>  add_expr)[op_rsht::add(self.program())]);
-add_expr  =  sub_expr >> *(('+'  >>  sub_expr)[ op_add::add(self.program())]);
-sub_expr  =  mul_expr >> *(('-'  >>  mul_expr)[ op_sub::add(self.program())]);
-mul_expr  =  mod_expr >> *(('*'  >>  mod_expr)[ op_mul::add(self.program())]);
-mod_expr  =  div_expr >> *(('%'  >>  div_expr)[ op_mod::add(self.program())]);
-div_expr  =      atom >> *(('/'  >>      atom)[ op_div::add(self.program())]);
+expr = lor_expr[expr.val = arg1];
 
-atom =    int_p[op_value::add(self.program())] 
-	| ( '(' >> expr >> ')' )
-	| ( '-' >> expr )[op_neg::add(self.program())]
-	| ( '!' >> expr )[op_not::add(self.program())]
-	| ( '~' >> expr )[op_bnot::add(self.program())]
-	| self.variables[op_value::add(self.program())]
+lor_expr  = land_expr[lor_expr.val = arg1] 
+		>> *("||" >> land_expr[lor_expr.val = lor_expr.val || arg1]);
+
+land_expr = band_expr[land_expr.val = arg1]
+		>> *("&&" >> band_expr[land_expr.val = land_expr.val && arg1]);
+
+band_expr = bxor_expr[band_expr.val = arg1]
+		>> *('&'  >> bxor_expr[band_expr.val &= arg1]);
+
+bxor_expr =  bor_expr[bxor_expr.val = arg1]
+		>> *('^'  >>  bor_expr[bxor_expr.val ^= arg1]);
+
+bor_expr  =   lt_expr[bor_expr.val = arg1]
+		>> *('|'  >>   lt_expr[bor_expr.val |= arg1]);
+
+lt_expr   =   gt_expr[lt_expr.val = arg1]
+		>> *('<'  >>   gt_expr[lt_expr.val = lt_expr.val < arg1]);
+
+gt_expr   =   le_expr[gt_expr.val = arg1]
+		>> *('>'  >>   le_expr[gt_expr.val = gt_expr.val > arg1]);
+
+le_expr   =   ge_expr[le_expr.val = arg1]
+		>> *("<=" >>   ge_expr[le_expr.val = le_expr.val <= arg1]);
+
+ge_expr   =  neq_expr[ge_expr.val = arg1]
+		>> *(">=" >>  neq_expr[ge_expr.val = ge_expr.val >= arg1]);
+
+neq_expr  =   eq_expr[neq_expr.val = arg1]
+		>> *("!=" >>   eq_expr[neq_expr.val = neq_expr.val != arg1]);
+
+eq_expr   = lsht_expr[eq_expr.val = arg1]
+		>> *("==" >> lsht_expr[eq_expr.val = eq_expr.val == arg1]);
+
+lsht_expr = rsht_expr[lsht_expr.val = arg1]
+		>> *("<<" >> rsht_expr[lsht_expr.val <<= arg1]);
+
+rsht_expr =  add_expr[rsht_expr.val = arg1]
+		>> *(">>" >>  add_expr[rsht_expr.val >>= arg1]);
+
+add_expr  =  sub_expr[add_expr.val = arg1]
+		>> *('+'  >>  sub_expr[add_expr.val += arg1]);
+
+sub_expr  =  mul_expr[sub_expr.val = arg1]
+		>> *('-'  >>  mul_expr[sub_expr.val -= arg1]);
+
+mul_expr  =  mod_expr[mul_expr.val = arg1]
+		>> *('*'  >>  mod_expr[mul_expr.val *= arg1]);
+
+mod_expr  =  div_expr[mod_expr.val = arg1]
+		>> *('%'  >>  div_expr[mod_expr.val %= arg1]);
+
+div_expr  =      atom[div_expr.val = arg1]
+		>> *('/'  >>      atom[div_expr.val /= arg1]);
+
+atom =    int_p[atom.val = arg1]
+	| ( '(' >> expr[atom.val = arg1] >> ')' )
+	| ( '-' >> expr[atom.val = -arg1])
+	| ( '!' >> expr[atom.val = !arg1])
+	| ( '~' >> expr[atom.val = ~arg1])
+	| self.variables[atom.val = arg1]
 	| ( str_p("defined") >> '(' >> 
-			self.variables[op_literal<1>::add(self.program())] >> ')' )
+			self.variables[atom.val = 1] >> ')' )
 	| ( str_p("defined") >> '(' >>
-			identifier[op_literal<0>::add(self.program())] >> ')')
+			identifier[atom.val = 0] >> ')')
 	;
 
 BOOST_SPIRIT_DEBUG_RULE(identifier);
