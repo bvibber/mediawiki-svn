47bc437676dfffddca4ff33141d35a224a2ef921
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-11-26 03:53:46 +0000

    * Add radio buttons to select which cache group to pull from * Fix XSS in deletion
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@17933 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/InspectCache/InspectCache_body.php
+++ extensions/InspectCache/InspectCache_body.php
@@ -15,18 +15,40 @@ class InspectCache extends SpecialPage
 	}
 
 	function execute( $par ) {
-		global $wgRequest, $wgOut, $wgTitle, $wgMemc;
+		global $wgRequest, $wgOut, $wgTitle;
 
 		$this->setHeaders();
 
 		$key = $wgRequest->getVal( 'key' );
 		$delete = $wgRequest->getBool( 'delete' ) && $wgRequest->wasPosted();
+		$group = $wgRequest->getVal( 'group' );
+		
 		$encQ = htmlspecialchars( $key );
 		$action = $wgTitle->escapeLocalUrl();
 		$ok = htmlspecialchars( wfMsg( "ok" ) );
+		
+		$groups = array(
+			'main' => array( 'General cache', 'wfGetMainCache' ),
+			'parser' => array( 'Parser cache', 'wfGetParserCacheStorage' ),
+			'message' => array( 'Message cache', 'wfGetMessageCacheStorage' ),
+		);
+		if( !isset( $groups[$group] ) ) {
+			$group = 'main';
+		}
+		$cache = $groups[$group][1]();
+		
+		$radios = '';
+		foreach( $groups as $type => $bits ) {
+			list( $desc ) = $bits;
+			$radios .=
+				Xml::radioLabel( $desc, 'group', $type, "mw-cache-$type",
+					$group == $type ) . " ";
+		}
+			
 
 		$wgOut->addHTML( <<<END
 <form name="ucf" method="post" action="$action">
+<div>$radios</div>
 <input type="text" size="80" name="key" value="$encQ"/><br />
 <input type="submit" name="submit" value="Get" />
 <input type="submit" name="delete" value="Delete" /><br /><br />
@@ -35,10 +57,10 @@ END
 );
 
 		if ( $delete && !is_null( $key ) ) {
-			$wgMemc->delete( $key );
-			$wgOut->addHTML( "Deleted $key\n" );
+			$cache->delete( $key );
+			$wgOut->addHTML( "Deleted " . htmlspecialchars( $key ) . "\n" );
 		} else if ( !is_null( $key ) ) {
-			$value = $wgMemc->get( $key );
+			$value = $cache->get( $key );
 			if ( !is_string( $value ) ) {
 				$value = var_export( $value, true );
 			}
