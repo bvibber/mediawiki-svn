c90dde736caf395a405a255eebeb90bb2793ad3e
travis <travis@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-01 15:20:50 +0000

    add initial version
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18090 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/BlockTitles/BlockTitles.php
@@ -0,0 +1,61 @@
+<?php
+if ( ! defined( 'MEDIAWIKI' ) )
+	die();
+    
+/**#@+
+ * Provides a basic way of preventing articles with certain titles from being saved or created
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @link http://www.mediawiki.org/wiki/BlockTitles_Extension Documentation
+ *
+ *
+ * @author Travis Derouin <travis@wikihow.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionFunctions[] = 'wfBlockTitles';
+
+$wgExtensionCredits['other'][] = array(
+	'name' => 'BlockTitles',
+	'author' => 'Travis Derouin',
+	'description' => 'Provides a basic way of preventing articles with certain titles from being saved or created',
+	'url' => 'http://www.mediawiki.org/wiki/BlockTitles_Extension',
+);
+
+// CONFIGURE - place any regular expressions you want here.  
+$wgBlockTitlePatterns = array (
+		#"/^http/i",  // if you want to block titles of articles that are URLs
+	);
+
+$wgHooks['ArticleSave'][] = 'wfCheckBlockTitles';
+
+function wfBlockTitles() {
+	global $wgMessageCache;
+	 $wgMessageCache->addMessages(
+ 	array(
+			'block_title_error_page_title' => 'Invalid Title',
+			'block_title_error' => 'Sorry, the title of this article is not allowed to be saved.',
+		)
+	);
+}
+
+function wfCheckBlockTitles (&$article ) {
+	global $wgBlockTitlePatterns;
+	global $wgOut;
+	$title = $article->getTitle();
+	$t = $title->getFullText();
+	foreach ($wgBlockTitlePatterns as $re=>$reason) {
+		if (preg_match($re, $t)) {
+			// too bad you can't pass parameter to errorpage
+			$wgOut->errorpage('block_title_error_page_title', 'block_title_error' );
+			return false;
+		}	
+	}
+
+	return true;
+}
+
+
+?>
+
