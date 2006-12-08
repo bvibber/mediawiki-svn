09e3608c7165f13571c9b613b74675a74aa774c8
travis <travis@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-08 20:52:29 +0000

    add initial version
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18232 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ImportFreeImages/ImportFreeImages.php
@@ -0,0 +1,201 @@
+<?php
+if ( ! defined( 'MEDIAWIKI' ) )
+    die();
+
+/**#@+
+ *  Provides a way of importing properly licensed photos from flickr
+ * 
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @link http://www.mediawiki.org/wiki/Extension:ImportFreeImages Documentation
+ *
+ *
+ * @author Travis Derouin <travis@wikihow.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionFunctions[] = 'wfImportFreeImages';
+$wgFlickrAPIKey = '43000ba217bff99960c3bb07843d74b3';
+$wgResultsPerPage = 20;
+$wgResultsPerRow = 5;
+// see the flickr api page for more information on these params
+// for licnese info http://www.flickr.com/services/api/flickr.photos.licenses.getInfo.html
+// default 4 is CC Attribution License
+$wgFlickrLicense = 4;
+$wgFlickrSort = "interestingness-desc";
+
+require_once("SpecialPage.php");
+
+
+
+
+$wgExtensionCredits['other'][] = array(
+    'name' => 'ImportFreeImages',
+    'author' => 'Travis Derouin',
+    'description' => 'Provides a way of importing properly licensed photos from flickr.',
+    'url' => 'http://www.mediawiki.org/wiki/Extension:ImportFreeImages',
+);
+
+
+function wfImportFreeImages() {
+
+
+	SpecialPage::AddPage(new SpecialPage('ImportFreeImages'));
+
+    global $wgMessageCache;
+     $wgMessageCache->addMessages(     array(
+			'importfreeimages' => 'Import Free Images',
+			'importfreeimages_description' => 'This page allows you to search properly licensed photos from flickr and import them into your wiki.',	
+			'importfreeimages_noapikey' => 'You have not configured your Flickr API Key. To do so, please obtain a API key from  [http://www.flickr.com/services/api/misc.api_keys.html here] and set wgFlickrAPIKey in ImportFreeImages.php.',
+			'importfreeimages_nophotosfound' => 'No photos were found for your search criteria \'$1\', please try again.',
+			'importfreeimages_owner' => 'Author',
+			'importfreeimages_importthis' => 'import this',
+			'importfreeimages_next' => 'Next $1',
+			'importfreeimages_filefromflickr' => '$1 by user <b>[$2]</b> from flickr. Original URL',
+        )
+    );
+
+
+}
+
+
+function wfSpecialImportFreeImages( $par )
+{
+	global $wgUser, $wgOut, $wgScriptPath, $wgRequest, $wgLang, $wgFlickrAPIKey, $wgTmpDirectory;
+	global $wgResultsPerPage, $wgFlickrSort, $wgFlickrLicense, $wgResultsPerRow;
+	
+	$fname = "wfSpecialImportFreeImages";
+
+	if (empty($wgFlickrAPIKey)) {
+		// error - need to set $wgFlickrAPIKey to use this extension
+		$wgOut->showErrorPage('error', 'importfreeimages_noapikey');
+		return;
+	}	
+	$q = '';	
+	if (isset($_GET['q']) && !$wgRequest->wasPosted() ) {
+		$q = $_GET['q'];
+	}
+
+	$wgOut->addHTML(wfMsg ('importfreeimages_description') . "<br/><br/><form method=GET>".wfMsg('search').
+		": <input type=text name=q value='" . htmlspecialchars($q) . "'><input type=submit value=".wfMsg('search')."></form>");
+
+	$import = '';
+	if ($wgRequest->wasPosted() && isset($_POST['url'])) {
+		$import = $_POST['url'];
+		if (strpos($import, "http://static.flickr.com/") !== 0) {
+			// avoid hack attempts
+			 echo "not supported.";
+			exit;
+		}
+		$pageContents = file_get_contents($import); 	
+		$name =$wgTmpDirectory . "/flickr-" . rand(0,999999);
+		$r = fopen($name, "w");
+		$size = fwrite ( $r, $pageContents);	
+		fclose($r);
+		chmod( $name, 0777 );
+		$caption = wfMsg('importfreeimages_filefromflickr', $_POST['t'], "http://www.flickr.com/people/" . urlencode($_POST['owner']) . " " . $_POST['name']) . " <nowiki>$import</nowiki>. {{CC by 2.0}} ";
+		$caption = trim($caption);
+		$t = $_POST['title'];
+
+		// handle duplicate filenames
+		$i = strrpos($import, "/");
+		if ($i !== false) {
+			$import = substr($import, $i + 1);
+		}
+
+		// pretty dumb way to make sure we're not overwriting previously uploaded images
+		$c = 0;
+		$nt =& Title::makeTitle( NS_IMAGE, $import);
+		$fname = $import;
+		while( $nt->getArticleID() && $c < 20) {
+			$fname = $c . "_" . $import;
+			$nt =& Title::makeTitle( NS_IMAGE, $fname);
+			$c++;
+		}
+		$import = $fname;
+
+/*
+		$arr = array ( "size" => $size, "tempname" => $name, 
+				"caption" => $caption,
+				"url" => $import, "title" => $_POST['t'] );
+*/
+		$u = new UploadForm($wgRequest);
+        $u->mUploadTempName = $name;
+        $u->mUploadSize     = $size; 
+		$u->mUploadDescription = $caption;
+		$u->mRemoveTempFile = true;
+		$u->mIgnoreWarning =  true;
+        $u->mOname = $_POST['title'] . ".jpg";
+
+		$u->execute();
+	}
+	if ($q != '') { 
+		$page = $_GET['p'];
+		if ($page == '') $page = 1;
+        	require_once("phpFlickr-2.0.0/phpFlickr.php");
+        	$f = new phpFlickr($wgFlickrAPIKey);
+        	$q = $_GET['q'];
+		// TODO: get the right licenses
+        	$photos = $f->photos_search(array(
+				"tags"=>"$q", "tag_mode"=>"any", 
+				"page" => $page, 
+				"per_page" => $wgResultsPerPage, "license" => $wgFlickrLicense, 
+				"sort" => $wgFlickrSort  ));
+
+		$i = 0;
+		if ($photos == null || !is_array($photos) || sizeof($photos) == 0 || !isset($photos['photo']) ) {
+			$wgOut->addHTML(wfMsg("importfreeimages_nophotosfound",$q));
+			return;
+		}
+		$importPage = Title::makeTitle(NS_SPECIAL, "ImportFreeImages");
+		$sk = $wgUser->getSkin();
+
+		$wgOut->addHTML("<table cellpadding=4>
+			<form method='POST' name='uploadphotoform'>
+				<input type=hidden name='url' value=''>
+				<input type=hidden name='action' value='submit'>
+				<input type=hidden name='owner' value=''>
+				<input type=hidden name='name' value=''>
+				<input type=hidden name='title' value=''>
+
+	<script type=\"text/javascript\">
+
+		function s2 (url, owner, name, title) {
+			document.uploadphotoform.url.value = url;
+			document.uploadphotoform.owner.value = owner;
+			document.uploadphotoform.name.value = name;
+			document.uploadphotoform.title.value = title;
+			document.uploadphotoform.submit();
+		}
+
+	</script>
+ 
+			");
+        	foreach ($photos['photo'] as $photo) {
+			if ($i % $wgResultsPerRow == 0) $wgOut->addHTML("<tr>");
+                	$owner = $f->people_getInfo($photo['owner']);
+                	$wgOut->addHTML( "<td align=center style='padding-top: 15px; border-bottom: 1px solid #ccc;'><font size=-2><a href='http://www.flickr.com/photos/" . $photo['owner'] . "/" . $photo['id'] . "/'>" );
+                	$wgOut->addHTML( $photo['title'] );
+                	$wgOut->addHTML( "</a><br/>".wfMsg('importfreeimages_owner').": " );
+                	$wgOut->addHTML( "<a href='http://www.flickr.com/people/" . $photo['owner'] . "/'>") ;
+                	$wgOut->addHTML( $owner['username'] );
+                	$wgOut->addHTML( "</a><br/>" );
+                	//$wgOut->addHTML( "<img  src=http://static.flickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'] . "." . "jpg>" );
+                	$url="http://static.flickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'] . "." . "jpg";
+                	$wgOut->addHTML( "<img src=http://static.flickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'] . "_s." . "jpg>" );
+			$wgOut->addHTML( "<br/>(<a href='#' onclick=\"s2('$url', '{$photo['owner']}', '" 
+						. urlencode($owner['username']  ) . "', '" . urlencode($photo['title']) . "');\">" . 
+								wfMsg('importfreeimages_importthis') . "</a>)\n" );
+			$wgOut->addHTML("</td>");
+			if ($i % $wgResultsPerRow == ($wgResultsPerRow - 1) ) $wgOut->addHTML("</tr>");
+			$i++;
+		}
+		$wgOut->addHTML("</form></table>");
+		$page = $page + 1;
+
+		$wgOut->addHTML("<br/>" .  $sk->makeLinkObj($importPage, wfMsg('importfreeimages_next', $wgResultsPerPage) ) );
+                //print_r($photo);
+	}
+}
+?>
