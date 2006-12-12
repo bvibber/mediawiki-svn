8858806dc247396d86a424612e4d5ba48c1aa534
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-12 06:05:51 +0000

    Better attribute-fetching query (fixes problem with attributes of default classes not showing up only if one object explicitly included it)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18290 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/WiktionaryZ/SpecialSuggest.php
+++ extensions/Wikidata/WiktionaryZ/SpecialSuggest.php
@@ -54,7 +54,6 @@ function getSuggestions() {
 			break;
 		case 'translated-text-attribute':
 		case 'text-attribute':	
-//			$sql = getSQLForCollectionOfType('TATT');
 			$sql = getSQLToSelectPossibleAttributes($objectId, $attributesLevel);
 			break;
 		case 'language':
@@ -149,17 +148,16 @@ function getSQLToSelectPossibleAttributes($objectId, $attributesLevel) {
 	global $wgDefaultClassMids;
 
 	$dbr = & wfGetDB(DB_SLAVE);
-	$sql = 'SELECT attribute_mid, spelling' .
-		' FROM ((uw_class_membership INNER JOIN (uw_class_attributes' .
-		' INNER JOIN bootstrapped_defined_meanings' .
-		' ON uw_class_attributes.level_mid = bootstrapped_defined_meanings.defined_meaning_id)' .
-		' ON uw_class_membership.class_mid = uw_class_attributes.class_mid)' .
-		' INNER JOIN uw_syntrans ON uw_class_attributes.attribute_mid = uw_syntrans.defined_meaning_id)' .
-		' INNER JOIN uw_expression_ns ON uw_syntrans.expression_id = uw_expression_ns.expression_id' .
-		' WHERE bootstrapped_defined_meanings.name = ' . $dbr->addQuotes($attributesLevel) .
-		' AND ' . getLatestTransactionRestriction('uw_class_membership') .
+	$sql = 'SELECT attribute_mid,spelling' .
+		' FROM uw_class_attributes' .
+		' JOIN bootstrapped_defined_meanings ON bootstrapped_defined_meanings.defined_meaning_id = uw_class_attributes.level_mid' .
+		' JOIN uw_syntrans ON uw_syntrans.defined_meaning_id = uw_class_attributes.attribute_mid' .
+		' JOIN uw_expression_ns ON uw_expression_ns.expression_id = uw_syntrans.expression_id' .
+		' JOIN uw_class_membership ON uw_class_membership.class_mid = uw_class_attributes.class_mid' .
+		' WHERE bootstrapped_defined_meanings.name LIKE ' . $dbr->addQuotes($attributesLevel) .
 		' AND ' . getLatestTransactionRestriction('uw_class_attributes') .
-		' AND (uw_class_membership.class_member_mid = ' . $objectId;
+		' AND ((uw_class_membership.class_member_mid = ' . $objectId .
+		' AND ' . getLatestTransactionRestriction('uw_class_membership') . ')';
 	foreach ($wgDefaultClassMids as $mid)
 		$sql .= ' OR uw_class_attributes.class_mid = ' . $mid;
 	$sql .= ')';
