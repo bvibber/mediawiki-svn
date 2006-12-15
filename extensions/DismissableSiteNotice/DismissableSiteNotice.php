e98b357589f728e258bbddb28808da25f7ac02bb
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-15 20:46:41 +0000

    commit this thing, with my tweak to localize the close button
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18353 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/DismissableSiteNotice/DismissableSiteNotice.php
@@ -0,0 +1,88 @@
+<?php
+function wfDismissableSiteNotice( &$notice ) {
+	global $wgMajorSiteNoticeID, $wgUser;
+	
+	if ( !$notice ) {
+		return true;
+	}
+
+
+	wfInitSiteNoticeMessage();
+	$encNotice = Xml::escapeJsString($notice);
+	$encClose = Xml::escapeJsString( wfMsg( 'sitenotice_close' ) );
+	$id = intval( $wgMajorSiteNoticeID ) . "." . intval( wfMsgForContent( 'sitenotice_id' ) );
+
+	// No dismissal for anons
+	if ( $wgUser->isAnon() ) {
+		$notice = <<<EOT
+<script language="JavaScript">
+<!--
+document.writeln("$encNotice");
+-->
+</script>
+EOT;
+		return true;
+	}
+
+	$notice = <<<EOT
+<script language="JavaScript">
+<!--
+var cookieName = "dismissSiteNotice=";
+var cookiePos = document.cookie.indexOf(cookieName);
+var siteNoticeID = "$id";
+var siteNoticeValue = "$encNotice";
+var cookieValue = "";
+var msgClose = "$encClose";
+
+if (cookiePos > -1) {
+	cookiePos = cookiePos + cookieName.length;
+	var endPos = document.cookie.indexOf(";", cookiePos);
+	if (endPos > -1) {
+		cookieValue = document.cookie.substring(cookiePos, endPos);
+	} else {
+		cookieValue = document.cookie.substring(cookiePos);
+	}
+}
+if (cookieValue != siteNoticeID) {
+	function dismissNotice() {
+		var date = new Date();
+		date.setTime(date.getTime() + 30*86400*1000);
+		document.cookie = cookieName + siteNoticeID + "; expires="+date.toGMTString() + "; path=/";
+		var element = document.getElementById('siteNotice');
+		element.parentNode.removeChild(element);
+	}
+	document.writeln('<table width="100%"><tr><td width="80%">'+siteNoticeValue+'</td>');
+	document.writeln('<td width="20%" align="right">[<a href="javascript:dismissNotice();">'+msgClose+'</a>]</td></tr></table>');
+}
+-->
+</script>
+EOT;
+	// Compact the string a bit
+	/*
+	$notice = strtr( $notice, array(
+		"\r\n" => '',
+		"\n" => '',
+		"\t" => '',
+		'cookieName' => 'n',
+		'cookiePos' => 'p',
+		'siteNoticeID' => 'i',
+		'siteNoticeValue' => 'sv',
+		'cookieValue' => 'cv',
+		'msgClose' => 'c',
+		'endPos' => 'e',
+	));*/
+	return true;
+}
+
+function wfInitSiteNoticeMessage() {
+	global $wgMessageCache;
+	$wgMessageCache->addMessage( 'sitenotice_id', '0' );
+	$wgMessageCache->addMessage( 'sitenotice_close', 'close' );
+}
+
+$wgHooks['SiteNoticeAfter'][] = 'wfDismissableSiteNotice';
+$wgHooks['LoadAllMessages'][] = 'wfInitSiteNoticeMessage';
+
+$wgMajorSiteNoticeID = 1;
+
+?>
