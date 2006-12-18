00315cb4a22ab8bd2fdcaa53374cfecbe29ab484
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-18 11:28:51 +0000

    Revert quick fix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18426 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/WiktionaryZ/HTMLtable.php
+++ extensions/Wikidata/WiktionaryZ/HTMLtable.php
@@ -87,7 +87,7 @@ function getHTMLClassForType($type,$attribute) {
 	else if ($type instanceof RecordType) 
 		return $attribute->id;
 	else 
-		return '';
+		return $type;
 }
 
 function getRecordAsTableCells($idPath, $editor, $record, &$startColumn = 0) {
