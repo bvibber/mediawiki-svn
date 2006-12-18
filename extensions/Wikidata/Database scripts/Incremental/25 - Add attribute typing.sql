fe0bdce81f8d67149bbfe51379c9099f89dd6d2e
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-18 11:05:03 +0000

    script_log entry
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18425 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Incremental/25 - Add attribute typing.sql
+++ extensions/Wikidata/Database scripts/Incremental/25 - Add attribute typing.sql
@@ -15,3 +15,6 @@ CREATE TABLE `uw_option_attribute_values` (
 	`add_transaction_id`	int(11) NOT NULL DEFAULT 0,
 	`remove_transaction_id`	int(11) NULL
 );
+
+INSERT INTO `script_log` (`time`, `script_name`)
+        VALUES (NOW(), '25 - Add attribute typing.sql');
