d50b3a7c15d085625f42bf4cef5f32519f6ea2a4
leon <leon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-19 18:07:03 +0000

    Added German l10n to the Duplicator extensions.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18457 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Duplicator/Duplicator.i18n.php
+++ extensions/Duplicator/Duplicator.i18n.php
@@ -38,7 +38,34 @@ copies of all histories. This is useful for article forking, etc.',
 'duplicator-toomanyrevisions' => '[[$1]] has too many ($2) revisions and cannot be copied. The current limit is $3.',
 ),
 	
-	);
+'de' => array(
+	'duplicator' => 'Einen Artikel duplizieren',
+	'duplicator-header' => 'Mit dieser Spezialseite können Artikel komplett dupliziert werden. Dabei wird die gesamte ' .
+				'Versionsgeschichte übernommen. Dies kann beispielsweise nützlich sein, um eine Seite in ' .
+				'Unterartikel aufzuteilen.',
+	'duplicator-options' => 'Optionen',
+	'duplicator-source' => 'Quelle:',
+	'duplicator-dest' => 'Ziel:',
+	'duplicator-dotalk' => 'Diskussionsseite mitkopieren (wenn möglich)',
+	'duplicator-submit' => 'Artikel duplizieren',
+	
+	'duplicator-summary' => '[[$1]] wurde dupliziert',
+
+	'duplicator-success' => "<big>'''[[$1]] wurde nach [[$2]] kopiert.'''</big>\n\n",
+	'duplicator-success-revisions' => '$1 Versionen wurden dupliziert.',
+	'duplicator-success-talkcopied' => 'Die Diskussionsseite wurde auch dupliziert.',
+	'duplicator-success-talknotcopied' => 'Die Diskussionsseite konnte nicht dupliziert werden.',
+	'duplicator-failed' => 'Der Artikel konnte nicht dupliziert werden, da ein unbekannter Fehler auftrat.',
+	
+	'duplicator-source-invalid' => 'Bitte geben Sie einen gültigen Quell-Artikel an.',
+	'duplicator-source-notexist' => 'Der Artikel [[$1]] existiert nicht. Bitte geben Sie einen existierenden Artikel an.',
+	'duplicator-dest-invalid' => 'Bitte geben Sie einen gültigen Ziel-Artikel an.',
+	'duplicator-dest-exists' => 'Der Artikel [[$1]] existiert bereits. Bitte geben Sie einen nicht existierenden Artikel an.',
+	'duplicator-toomanyrevisions' => 'Der Artikel [[$1]] hat zu viele ($2) Versionen, um dupliziert zu werden, da nur Artikel mit ' .
+					'maximal $3 Versionen dupliziert werden können.',
+),
+	
+);
 }
 
 ?>
