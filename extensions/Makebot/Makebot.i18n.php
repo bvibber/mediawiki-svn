6e171b1e027636c80460bae83cbaeb4c195a094c
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-19 12:37:22 +0000

    Italian translations for Makebot, NewestPages and Patroller (BrokenArrow, from patch 8138)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18441 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Makebot/Makebot.i18n.php
+++ extensions/Makebot/Makebot.i18n.php
@@ -13,6 +13,7 @@
 function efMakeBotMessages() {
 	$messages = array(
 	
+/* English (Rob Church) */
 'en' => array(
 'makebot' => 'Grant or revoke bot status',
 'makebot-header' => "'''A local bureaucrat can use this page to grant or revoke [[Help:Bot|bot status]] to another user account.'''<br />Bot status hides a user's edits from [[Special:Recentchanges|recent changes]] and similar lists, and is useful for flagging users who make automated edits. This should be done in accordance with applicable policies.",
@@ -33,6 +34,28 @@ function efMakeBotMessages() {
 'makebot-logentryrevoke' => 'removed bot status from [[$1]]',
 ),
 
+/* Italian (BrokenArrow) */
+'it' => array(
+'makebot' => 'Assegna o revoca lo status di bot',
+'makebot-header' => "'''Questa pagina consente ai burocrati di assegnare o revocare lo [[{{ns:help}}:Bot|status di bot]] a un'altra utenza.'''<br /> Tale status nasconde le modifiche effettuate dall'utenza nell'elenco delle [[{{ns:special}}:Recentchanges|ultime modifiche]] e nelle liste simili; è utile per contrassegnare le utenze che effettuano modifiche in automatico. Tale operazione dev'essere effettuata in conformità con le policy del sito.",
+'makebot-username' => 'Nome utente:',
+'makebot-search' => 'Vai',
+'makebot-isbot' => 'L\'utente [[{{ns:user}}:$1|$1]] ha lo status di bot.',
+'makebot-notbot' => 'L\'utente [[{{ns:user}}:$1|$1]] non ha lo status di bot.',
+'makebot-privileged' => 'L\'utente [[{{ns:user}}:$1|$1]] possiede i privilegi di [[Special:Listadmins|amministratore o burocrate privileges]], che sono incompatibili con lo status di bot.',
+'makebot-change' => 'Modifica lo status:',
+'makebot-grant' => 'Concedi',
+'makebot-revoke' => 'Revoca',
+'makebot-comment' => 'Commento:',
+'makebot-granted' => 'L\'utente [[{{ns:user}}:$1|$1]] ha ora lo status di bot.',
+'makebot-revoked' => 'L\'utente [[{{ns:user}}:$1|$1]] non ha più lo status di bot.',
+'makebot-logpage' => 'Registro dei bot',
+'makebot-logpagetext' => 'Qui di seguito viene riportata la lista dei cambiamenti di status dei [[{{ns:help}}:bot]].',
+'makebot-logentrygrant' => 'ha concesso lo status di bot a [[$1]]',
+'makebot-logentryrevoke' => 'ha revocato lo status di bot a [[$1]]',
+),
+
+/* Hebrew (Rotem Liss) */
 'he' => array(
 'makebot'          => 'הענק או בטל הרשאת בוט',
 'makebot-header'   => "'''ביורוקרט מקומי יכול להשתמש בדף זה כדי להעניק או לבטל [[{{ns:help}}:בוט|הרשאת בוט]] למשתמש אחר.'''<br />הרשאת בוט מסתירה את עריכותיו של המשתמש מ[[{{ns:special}}:Recentchanges|השינויים האחרונים]] ורשימות דומות, ושימושי למשתמשים המבצעים עריכות אוטומטיות. יש להעניק הרשאת בוט אך ורק לפי הנהלים המתאימים.",
@@ -52,6 +75,7 @@ function efMakeBotMessages() {
 'makebot-logentrygrant'  => 'העניק הרשאת בוט למשתמש [[$1]]',
 'makebot-logentryrevoke' => 'ביטל את הרשאת הבוט למשתמש [[$1]]',
 ),
+
 	);
 	return $messages;
 }
--- extensions/Patroller/Patroller.i18n.php
+++ extensions/Patroller/Patroller.i18n.php
@@ -12,7 +12,8 @@
 
 function efPatrollerMessages() {
 	$messages = array(
-	
+
+/* English (Rob Church) */
 'en' => array(
 'patrol' => 'Patrol edits',
 'patrol-endorse' => 'Endorse',
@@ -30,6 +31,26 @@ function efPatrollerMessages() {
 'patrol-another' => 'Show another edit, if available.',
 'patrol-stopped' => 'You have opted not to patrol another edit. $1',
 'patrol-resume' => 'Click here to resume.',
+),
+
+/* Italian (BrokenArrow) */
+'it' => array(
+'patrol' => 'Verifica delle modifiche',
+'patrol-endorse' => 'Approva',
+'patrol-revert' => 'Ripristina',
+'patrol-revert-reason' => 'Motivo:',
+'patrol-skip' => 'Salta',
+'patrol-reverting' => 'Ripristino: $1',	
+'patrol-nonefound' => 'Non vi sono modifiche da verificare.',
+'patrol-endorsed-ok' => 'La modifica è stata segnata come verificata.',
+'patrol-endorsed-failed' => 'Impossibile segnare la modifica come verificata.',
+'patrol-reverted-ok' => 'La modifica è stata annullata.',
+'patrol-reverted-failed' => 'Impossibile annullare la modifica.',
+'patrol-skipped-ok' => 'Modifica ignorata.',
+'patrol-reasons' => "* Vandalismo semplice\n* Prova di nuovo utente\n* Vedi pagina di discussione",
+'patrol-another' => 'Mostra un\'altra modifica, se disponibile.',
+'patrol-stopped' => 'Si è scelto di non verificare altre modifiche. $1',
+'patrol-resume' => 'Fare clic qui per riprendere.',
 ),
 	
 	);
