a6e43a351d4080abf2259b2b136096f62f50be11
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-23 20:33:09 +0000

    Final (hopefully) structure for option attribute tables
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18538 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Incremental/25 - Add attribute typing.sql
+++ extensions/Wikidata/Database scripts/Incremental/25 - Add attribute typing.sql
@@ -1,7 +1,8 @@
 ALTER TABLE `uw_class_attributes` ADD COLUMN `attribute_type` char(4) NOT NULL DEFAULT 'TEXT' AFTER `attribute_mid`;
 
 CREATE TABLE `uw_option_attribute_options` (
-	`attribute_mid`			int(11)	NOT NULL DEFAULT 0,
+	`option_id`				int(11) NOT NULL DEFAULT 0,
+	`attribute_id`			int(11)	NOT NULL DEFAULT 0,
 	`option_mid`			int(11) NOT NULL DEFAULT 0,
 	`language_id`			int(11) NOT NULL DEFAULT 0,
 	`add_transaction_id`	int(11) NOT NULL DEFAULT 0,
@@ -11,8 +12,7 @@ CREATE TABLE `uw_option_attribute_options` (
 CREATE TABLE `uw_option_attribute_values` (
 	`value_id`				int(11) NOT NULL DEFAULT 0,
 	`object_id`				int(11) NOT NULL DEFAULT 0,
-	`attribute_mid`			int(11) NOT NULL DEFAULT 0,
-	`option_mid`			int(11) NOT NULL DEFAULT 0,
+	`option_id`				int(11) NOT NULL DEFAULT 0,
 	`add_transaction_id`	int(11) NOT NULL DEFAULT 0,
 	`remove_transaction_id`	int(11) NULL
 );
