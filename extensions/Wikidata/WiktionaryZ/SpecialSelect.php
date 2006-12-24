774cea53050cf8d3b4549958330ee68ea0841004
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2006-12-24 01:54:06 +0000

    Semi-temporary fix for incorrect fetching of localized option names
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18545 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/WiktionaryZ/SpecialSelect.php
+++ extensions/Wikidata/WiktionaryZ/SpecialSelect.php
@@ -27,30 +27,10 @@ function wfSpecialSelect() {
 
 function getSelectOptions() {
 	global
-		$idAttribute;
+		$wgUser;
 
 	$optionAttribute = $_GET['option-attribute'];
 	$attributeObject = $_GET['attribute-object'];
-
-	$dbr =& wfGetDB(DB_SLAVE);
-
-	$sql = getSQLForOptions($optionAttribute, $attributeObject);
-	$result = $dbr->query($sql);
-
-	$optionString = '';
-	while ($row = $dbr->fetchObject($result)) {
-		if ($optionString != '')
-			$optionString .= "\n";
-		$optionString .= $row->row_id . ';' . $row->spelling;
-	}
-	return $optionString;
-}
-
-/* Return SQL query string for fetching language names. */
-function getSQLForOptions($optionAttribute, $attributeObject) {
-	global
-		$wgUser;
-
 	$lang_code = $wgUser->getOption('language');
 
 	$dbr =& wfGetDB(DB_SLAVE);
@@ -60,37 +40,58 @@ function getSQLForOptions($optionAttribute, $attributeObject) {
 			' WHERE uw_syntrans.syntrans_sid = ' . $attributeObject .
 			' AND ' . getLatestTransactionRestriction('uw_syntrans') .
 			' AND ' . getLatestTransactionRestriction('uw_expression_ns');
-	$result = $dbr->query($sql);
-	$objectLanguage = $dbr->fetchObject($result)->language_id;
+	$lang_res = $dbr->query($sql);
+	$objectLanguage = $dbr->fetchObject($lang_res)->language_id;
 
-	/* Use a simpler query if the user's language is English. */
-	if ($lang_code == 'en' || !($lang_id = getLanguageIdForCode($lang_code)))
-		return 'SELECT option_id AS row_id,spelling' .
+	$sql = 'SELECT uw_option_attribute_options.option_id,uw_option_attribute_options.option_mid' .
 			' FROM uw_option_attribute_options' .
-			' JOIN uw_syntrans ON uw_syntrans.defined_meaning_id = uw_option_attribute_options.option_mid' .
-			' JOIN uw_expression_ns ON uw_expression_ns.expression_id = uw_syntrans.expression_id' .
 			' JOIN uw_class_attributes ON uw_class_attributes.object_id = uw_option_attribute_options.attribute_id' .
-			' WHERE uw_expression_ns.language_id = ' . getLanguageIdForCode('en') .
-			' AND uw_class_attributes.attribute_mid = ' . $optionAttribute .
+			' WHERE uw_class_attributes.attribute_mid = ' . $optionAttribute .
 			' AND (uw_option_attribute_options.language_id = ' . $objectLanguage .
 			' OR uw_option_attribute_options.language_id = 0)' .
 			' AND ' . getLatestTransactionRestriction('uw_option_attribute_options') .
-			' AND ' . getLatestTransactionRestriction('uw_syntrans') .
-			' AND ' . getLatestTransactionRestriction('uw_expression_ns') .
-			' GROUP BY option_id';
-	/* Fall back on English in cases where an option name is not present in the
-		user's preferred language. */
-	else
-		return 'SELECT option_id AS row_id,COALESCE(uwens1.spelling,uwens2.spelling) AS spelling' .
-			' FROM uw_option_attribute_options' .
-			' JOIN uw_syntrans ON uw_syntrans.defined_meaning_id = uw_option_atribute_options.option_mid' .
-			' LEFT JOIN uw_expression_ns AS uwens1 ON uwens1.expression_id = uw_syntrans.expression_id AND uwens1.language_id = ' . $lang_id .
-			' JOIN uw_expression_ns AS uwens2 ON uwens2.expression_id = uw_syntrans.expression_id AND uwens2.language_id = ' . getLanguageIdForCode('en') .
-			' AND ' . getLatestTransactionRestriction('uw_option_attribute_options') .
-			' AND ' . getLatestTransactionRestriction('uw_syntrans') .
-			' AND ' . getLatestTransactionRestriction('uwens1') .
-			' AND ' . getLatestTransactionRestriction('uwens2') .
-			' GROUP BY option_id';
+			' AND ' . getLatestTransactionRestriction('uw_class_attributes');
+	$options_res = $dbr->query($sql);
+
+	$optionsString = '';
+	while ($options_row = $dbr->fetchObject($options_res)) {
+		/* Use a simpler query if the user's language is English. */
+		if ($lang_code == 'en' || !($lang_id = getLanguageIdForCode($lang_code))) {
+			$sql = 'SELECT uw_expression_ns.spelling' .
+					' FROM uw_syntrans' .
+					' JOIN uw_expression_ns ON uw_expression_ns.expression_id = uw_syntrans.expression_id' .
+					' WHERE uw_syntrans.defined_meaning_id = ' . $options_row->option_mid .
+					' AND uw_expression_ns.language_id = ' . getLanguageIdForCode('en') .
+					' AND ' . getLatestTransactionRestriction('uw_expression_ns');
+		}
+		/* Fall back on English in cases where an option name is not present in the
+			user's preferred language. */
+		else {
+			/* XXX: This setup is really hacked together. It NEEDS to be improved. */
+			$sql = 'SELECT uw_expression_ns.spelling' .
+					' FROM uw_syntrans' .
+					' JOIN uw_expression_ns ON uw_expression_ns.expression_id = uw_syntrans.expression_id' .
+					' WHERE uw_syntrans.defined_meaning_id = ' . $options_row->option_mid .
+					' AND uw_expression_ns.language_id = ' . $lang_id .
+					' AND ' . getLatestTransactionRestriction('uw_expression_ns');
+			$res = $dbr->query($sql);
+			if (!$dbr->fetchObject($res)->spelling)
+				$sql = 'SELECT uw_expression_ns.spelling' .
+						' FROM uw_syntrans' .
+						' JOIN uw_expression_ns ON uw_expression_ns.expression_id = uw_syntrans.expression_id' .
+						' WHERE uw_syntrans.defined_meaning_id = ' . $options_row->option_mid .
+						' AND uw_expression_ns.language_id = ' . getLanguageIdForCode .
+						' AND ' . getLatestTransactionRestriction('uw_expression_ns');
+		}
+
+		$spelling_res = $dbr->query($sql);
+		$spelling_row = $dbr->fetchObject($spelling_res);
+		if ($optionString != '')
+			$optionString .= "\n";
+		$optionString .= $options_row->option_id . ';' . $spelling_row->spelling;
+	}
+
+	return $optionString;
 }
 
 ?>
