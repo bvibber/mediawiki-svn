a3ef837885958a808ea21a296ce3df4957f5c0ac
magnusmanske <magnusmanske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-01 12:02:03 +0000

    typo fix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18751 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- yawr/base.cpp
+++ yawr/base.cpp
@@ -217,7 +217,7 @@ ZenoArticle MainFrame::GetArticle ( wxString title , ZenoFile &file , bool va )
 
 wxString MainFrame::GetIP() { return _T("127.0.0.1") ; }
 wxString MainFrame::GetPort() { return port_line->GetValue() ; }
-ZenoFile *MainFrame::GetIndexPointer() { return &zf_index() ; }
+ZenoFile *MainFrame::GetIndexPointer() { return &zf_index ; }
 
 ZenoArticle MainFrame::RandomArticle ( wxString begin )
 {
