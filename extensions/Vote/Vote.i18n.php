fe4076c3ad12e78c49fb469e17f43fe8a4c4155d
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-06 19:03:04 +0000

    Simple vote extension I've had hanging around for a while
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18871 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Vote/Vote.i18n.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Internationalisation file for the Vote extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Rob Church <robchur@gmail.com>
+ * Please see the LICENCE file for terms of use and redistribution
+ */
+ 
+function efVoteMessages() {
+	return array(
+	
+'en' => array(
+'vote' => 'Vote',
+'vote-header' => "You can vote for '''Supreme Overlord of the World''' here!",
+'vote-current' => "Your current vote is for '''$1'''.",
+'vote-legend' => 'Place or amend vote',
+'vote-caption' => 'Your selection:',
+'vote-choices' => "joker|The Joker
+penguin|The Penguin
+riddler|Riddler",
+'vote-submit' => 'Vote',
+'vote-registered' => 'Your vote has been registered.',
+'vote-view-results' => 'View results',
+'vote-results' => 'Vote results',
+'vote-results-choice' => 'Choice',
+'vote-results-count' => 'Count',
+'vote-results-none' => 'No votes have been placed at this time.',
+'vote-login' => 'You must $1 to vote.',
+'vote-login-link' => 'log in',
+'vote-invalid-choice' => 'You must select one of the available options.',
+),
+	
+	);
+}
+
+?>
---
+++ extensions/Vote/vote.sql
@@ -0,0 +1,7 @@
+CREATE TABLE /*wgDBprefix*/vote (
+  vote_id int(11) NOT NULL auto_increment,
+  vote_user int(11) NOT NULL,
+  vote_choice varchar(255) NOT NULL,
+  PRIMARY KEY  (vote_id),
+  KEY vote_choice (vote_choice)
+);
