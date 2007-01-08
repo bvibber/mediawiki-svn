954f394db1b16af2eb1ebe28085ae79152712c13
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-08 22:52:43 +0000

    Finds differences of sequence numbers
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@18985 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ squid-log/delta.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <stdint.h>
+#include <boost/lexical_cast.hpp>
+
+int main(int argc, char** argv)
+{
+	using namespace std;
+	const size_t bufSize = 65536;
+	map<string, uint64_t> seqs;
+	typedef map<string, uint64_t>::iterator MapIterator;
+	char buffer[bufSize];
+	
+	while (cin.good()) {
+		cin.getline(buffer, bufSize);
+		if (!cin.gcount()) {
+			continue;
+		}
+		if (cin.gcount() > (streamsize)(bufSize - 2)) {
+			cerr << "delta: oversized line detected" << endl;
+			continue;
+		}
+		char * strConn = strtok(buffer, " ");
+		char * strSeq = strtok(NULL, " ");
+		char * restOfLine = strtok(NULL, "\n");
+		if (!strConn || !strSeq || !restOfLine) {
+			cerr << "delta: invalid line detected" << endl;
+			continue;
+		}
+		uint64_t seq;
+		try {
+			seq = boost::lexical_cast<uint64_t>(strSeq);
+		} catch (...) {
+			cerr << "delta: invalid sequence number" << endl;
+			continue;
+		}
+		MapIterator iter = seqs.find(strConn);
+		if (iter == seqs.end()) {
+			seqs[strConn] = seq;
+		} else {
+			if (iter->second > seq) {
+				cerr << "delta: out-of-order packet detected" << endl;
+				seqs.erase(iter);
+			} else {
+				uint64_t delta = seq - iter->second;
+				cout << delta << " " << restOfLine << endl;
+				iter->second = seq;
+			}
+		}
+	}
+}
+
+// vim: ts=4 sw=4:
+
