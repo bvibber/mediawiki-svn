faa1162c552f8baa73025b682702676b4ff9e1a0
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-10 19:02:35 +0000

    re-add namespace restriction for NS_MEDIA - imagelinks doesn't make any sense elsewhere.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/werdna@19082 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- restrictions-separation/includes/Title.php
+++ restrictions-separation/includes/Title.php
@@ -1118,7 +1118,7 @@ class Title {
 		}
 
 		if ( ( $this->isCascadeProtectedPage() ) || 
-				( $this->isCascadeProtectedImage() ) ) {
+				($this->getNamespace == NS_MEDIA && $this->isCascadeProtectedImage() ) ) {
 			# We /could/ use the protection level on the source page, but it's fairly ugly
 			#  as we have to establish a precedence hierarchy for pages included by multiple
 			#  cascade-protected pages. So just restrict it to people with 'protect' permission,
