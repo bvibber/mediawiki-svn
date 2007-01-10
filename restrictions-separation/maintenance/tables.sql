0897af12b17aed4c9904e872c30a1ea9e201af11
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-10 06:22:04 +0000

    Add two fields for support of planned features. pr_user for restricting a single user, and pr_expiry for having time-limited protection
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/werdna@19047 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- restrictions-separation/maintenance/tables.sql
+++ restrictions-separation/maintenance/tables.sql
@@ -1085,6 +1085,10 @@ CREATE TABLE /*$wgDBprefix*/page_restrictions (
   pr_level varchar(255) NOT NULL,
   -- Whether or not to cascade the protection down to pages transcluded.
   pr_cascade tinyint(4) NOT NULL,
+  -- Field for future support of per-user restriction.
+  pr_user int(8) NULL,
+  -- Field for future support of time-limited protection.
+  pr_expiry char(14) binary NULL,
 
   PRIMARY KEY  (pr_page,pr_type),
 
