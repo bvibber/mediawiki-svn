ab84261fac9b106d920db010601edb29e445bdfe
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-14 13:22:52 +0000

    Disallow refresh for 5 seconds. We do not want users to hammer the server :o)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/hashar@19233 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- skins/common/ajaxshoweditors.js
+++ skins/common/ajaxshoweditors.js
@@ -1,4 +1,5 @@
 var sajax_debug_mode = false;
+var canRefresh = null;
 var wgAjaxShowEditors = {} ;
 
 // The loader. Look at bottom for the sajax hook registration
@@ -6,11 +7,17 @@ wgAjaxShowEditors.onLoad = function() {
 	var elEditors = document.getElementById( 'ajax-se' );
 	// wgAjaxShowEditors.refresh();
 	elEditors.onclick = function() { wgAjaxShowEditors.refresh(); } ;
+	wgAjaxShowEditors.allowRefresh();
 }
 
 
 // Ask for new data & update UI
 wgAjaxShowEditors.refresh = function() {
+	if( !canRefresh ) { return; }
+
+	// Disable new requests for 5 seconds
+	canRefresh = false;
+	setTimeout( 'wgAjaxShowEditors.allowRefresh()', 5000 );
 
 	// Load the editors list element, it will get rewrote
 	var elEditorsList = document.getElementById( 'ajax-se-editors' );
@@ -23,6 +30,10 @@ wgAjaxShowEditors.refresh = function() {
 	sajax_do_call( "wfAjaxShowEditors", [ wgArticleId, wgUserName ], elEditorsList );
 }
 
+// callback to allow refresh
+wgAjaxShowEditors.allowRefresh = function() {
+	canRefresh = true;
+}
 
 // Register our initialization function.
 hookEvent( "load", wgAjaxShowEditors.onLoad);
