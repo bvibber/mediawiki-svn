c266af27ad1fa11af11ffc55ea792d766635dd1b
ivanlanin <ivanlanin@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-15 06:24:01 +0000

    Indonesian (id) translation for ImageMap extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19285 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ImageMap/ImageMap.i18n.php
+++ extensions/ImageMap/ImageMap.i18n.php
@@ -3,10 +3,10 @@
 /**
  * Internationalisation file for ImageMap extension
  */
- 
+
 function efImageMapMessages() {
 	return array(
-	
+
 /* English (Tim Starling) */
 'en' => array(
 'imagemap_no_image'             => '&lt;imagemap&gt;: must specify an image in the first line',
@@ -23,7 +23,24 @@ function efImageMapMessages() {
 # Note to translators: keep the same order
 'imagemap_desc_types'           => 'top-right, bottom-right, bottom-left, top-left, none',
 ),
-	
+
+/* Indonesian (Ivan Lanin) */
+'id' => array(
+'imagemap_no_image'             => '&lt;imagemap&gt;: harus memberikan suatu gambar di baris pertama',
+'imagemap_invalid_image'        => '&lt;imagemap&gt;: gambar tidak sah atau tidak ditemukan',
+'imagemap_no_link'              => '&lt;imagemap&gt;: tidak ditemukan pranala yang sah di akhir baris $1',
+'imagemap_invalid_title'        => '&lt;imagemap&gt;: judul tidak sah pada pranal di baris $1',
+'imagemap_missing_coord'        => '&lt;imagemap&gt;: tidak cukup koordinat untuk bentuk pada baris $1',
+'imagemap_unrecognised_shape'   => '&lt;imagemap&gt;: bentuk tak dikenali pada baris $1, tiap baris harus dimulai dengan salah satu dari: '.
+								   'default, rect, circle atau poly',
+'imagemap_no_areas'             => '&lt;imagemap&gt;: harus diberikan paling tidak satu spesifikasi area',
+'imagemap_invalid_coord'        => '&lt;imagemap&gt;: koordinat tidak sah pada baris $1, haruslah berupa angka',
+'imagemap_invalid_desc'         => '&lt;imagemap&gt;: spesifikasi desc tidak sah, harus salah satu dari: $1',
+'imagemap_description'          => 'Tentang gambar ini',
+# Note to translators: keep the same order
+'imagemap_desc_types'           => 'top-right, bottom-right, bottom-left, top-left, none',
+),
+
 	);
 }
 
