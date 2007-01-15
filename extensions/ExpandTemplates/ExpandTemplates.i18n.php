ca8ffd03bc015c31c43901e81b8f8503b20c8323
rotem <rotem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-15 14:35:18 +0000

    Spacing.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19295 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ExpandTemplates/ExpandTemplates.i18n.php
+++ extensions/ExpandTemplates/ExpandTemplates.i18n.php
@@ -63,7 +63,7 @@ telles que <nowiki>{{</nowiki>#if:...}} et <nowiki>{{</nowiki>CURRENTDAY}} sont 
 $wgExpandTemplatesMessages['he'] = array(
 	'expandtemplates'                  => 'פריסת תבניות',
 	'expand_templates_intro'           => 'דף זה מקבל כמות מסוימת של טקסט ופורס ומפרש את כל התבניות שבתוכו באופן רקורסיבי. בנוסף, הוא פורס הוראות פירוש כגון <nowiki>{{</nowiki>#תנאי:...}}, ומשתנים כגון <nowiki>{{</nowiki>יום נוכחי}}, ולמעשה בערך כל דבר בסוגריים מסולסלות כפולות. הוא עושה זאת באמצעות קריאה לפונקציות הפענוח המתאימות מתוך תוכנת מדיה־ויקי עצמה.',
-	'expand_templates_title'           => 'כותרת ההקשר לפענוח, בשביל משתנים כגון{{שם הדף}} וכדומה:',
+	'expand_templates_title'           => 'כותרת ההקשר לפענוח, בשביל משתנים כגון {{שם הדף}} וכדומה:',
 	'expand_templates_input'           => 'טקסט:',
 	'expand_templates_output'          => 'תוצאה:',
 	'expand_templates_ok'              => 'פרוס תבניות',
