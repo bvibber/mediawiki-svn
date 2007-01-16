35d2770c932e5ce5d1e845849fd535a3fcd2677c
ivanlanin <ivanlanin@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-16 03:06:04 +0000

    SyntaxHighlight extension enhancements, tested with 1.10alpha (r19324) * Adds 6 parameters for geshi (line numbering, tab width, etc) * Provides internationalisation file * Some additional README NOTE: The extension is still in old format. Do we want to change the code style?
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19325 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Highlight/SyntaxHighlight.i18n.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+/**
+ * Internationalisation file for the SyntaxHighlight extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ */
+
+function efSyntaxHighlightMessages() {
+	return array(
+
+/* English (Brion Vibber) */
+'en' => array(
+'syntaxhighlight-specify' => 'You need to specify a language like this:',
+'syntaxhighlight-supported' => 'Supported languages for syntax highlighting:',
+'syntaxhighlight-err-loading' => '(error loading support language list)',
+),
+
+/* Indonesian (Ivan Lanin) */
+'id' => array(
+'syntaxhighlight-specify' => 'Anda harus menentukan suatu bahasa seperti ini:',
+'syntaxhighlight-supported' => 'Bahasa-bahasa yang didukung oleh pewarnaan sintaks:',
+'syntaxhighlight-err-loading' => '(kesalahan pemuatan daftar bahasa yang didukung)',
+),
+
+	);
+}
+
+?>
--- extensions/Highlight/SyntaxHighlight.php
+++ extensions/Highlight/SyntaxHighlight.php
@@ -1,19 +1,19 @@
-<?php
+﻿<?php
 /**
  * Syntax highlighting extension for MediaWiki 1.5 using GeSHi
  * Copyright (C) 2005 Brion Vibber <brion@pobox.com>
  * http://www.mediawiki.org/
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -31,7 +31,7 @@
  * use of the new extension parameter support in MediaWiki 1.5 so it only
  * has to register one tag, <source>.
  *
- * A language is specified like: <source lang="c">void main() {}</source> 
+ * A language is specified like: <source lang="c">void main() {}</source>
  * If you forget, or give an unsupported value, the extension spits out
  * some help text and a list of all supported languages.
  *
@@ -49,22 +49,44 @@ if( !defined( 'MEDIAWIKI' ) )
 require_once( 'geshi/geshi.php' );
 
 $wgExtensionFunctions[] = 'syntaxSetup';
+$wgExtensionCredits['parserhook']['SyntaxHighlight'] = array(
+	'name'          => 'SyntaxHighlight',
+	'author'        => 'Brion Vibber',
+	'description'   => 'Provides syntax highlighting using [http://qbnz.com/highlighter/ GeSHi Higlighter]',
+);
 
 function syntaxSetup() {
-	global $wgParser;
+	global $wgParser, $wgMessageCache;
+	require_once( dirname( __FILE__ ) . '/SyntaxHighlight.i18n.php' );
+	foreach( efSyntaxHighlightMessages() as $lang => $messages )
+		$wgMessageCache->addMessages( $messages, $lang );
 	$wgParser->setHook( 'source', 'syntaxHook' );
 }
 
 function syntaxHook( $text, $params = array() ) {
 	return isset( $params['lang'] )
-		? syntaxFormat( trim( $text ), $params['lang'] )
+		? syntaxFormat( trim( $text ), $params )
 		: syntaxHelp();
 }
 
-function syntaxFormat( $text, $lang ) {
-	$geshi = new GeSHi( $text, $lang );
+function syntaxFormat( $text, &$params ) {
+	$geshi = new GeSHi( $text, $params['lang'] );
 	$geshi->set_encoding( 'UTF-8' );
-	
+
+	$methods = array(
+		'line'		=> 'enable_line_numbers',
+		'start'		=> 'start_line_numbers_at',
+		'case'		=> 'set_case_keywords',
+		'tab'		=> 'set_tab_width',
+		'header'	=> 'set_header_type',
+		'strict'	=> 'enable_strict_mode',
+	);
+	foreach( $methods as $key => $method ) {
+		if( isset( $params[$key] ) && method_exists( $geshi, $method ) ) {
+			eval( '$geshi->' . $method . '( $params[\'' . $key . '\'] );' );
+		}
+	}
+
 	$out   = $geshi->parse_code();
 	$error = $geshi->error();
 	return $error
@@ -73,20 +95,20 @@ function syntaxFormat( $text, $lang ) {
 }
 
 function syntaxHelp() {
-	return "<div style='border: solid red 1px'>" .
-		"<p>You need to specify a language like this: " .
-		"<samp>&lt;source lang=&quot;html&quot;&gt;...&lt;/source&gt;</samp> " .
-		"Supported languages for syntax highlighting:</p>\n" .
+	return '<div style="border:solid red 1px; padding:.5em;">' .
+		'<p>' . wfMsg( 'syntaxhighlight-specify' ) . ' ' .
+		'<samp>&lt;source lang=&quot;html&quot;&gt;...&lt;/source&gt;</samp></p>' .
+		'<p>' . wfMsg( 'syntaxhighlight-supported' ) . '</p>' .
 		syntaxFormatList( syntaxLanguageList() ) .
-		"</div>\n";
+		'</div>';
 }
 
 function syntaxFormatList( $list ) {
 	return empty( $list )
-		? "<p>(error loading support language list)</p>\n"
-		:  "<p style='margin-left: 0.5in; margin-right: 0.5in'>" .
-			implode( ", ", array_map( 'syntaxListItem', $list ) ) .
-			"</p>\n";
+		? wfMsg( 'syntaxhighlight-err-loading' )
+		: '<p style="padding:0em 1em;">' .
+			implode( ', ', array_map( 'syntaxListItem', $list ) ) .
+			'</p>';
 }
 
 function syntaxListItem( $item ) {
