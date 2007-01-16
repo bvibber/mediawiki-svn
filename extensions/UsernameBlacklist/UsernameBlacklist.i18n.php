3c8daf24e52d4e4df7b9731febd3abaf8b418d9d
ivanlanin <ivanlanin@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-16 03:23:32 +0000

    Indonesian (id) translation for UsernameBlacklist extension.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19326 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsernameBlacklist/UsernameBlacklist.i18n.php
+++ extensions/UsernameBlacklist/UsernameBlacklist.i18n.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 /**
  * Internationalisation file for the Username Blacklist extension
@@ -10,14 +10,21 @@
 
 function efUsernameBlacklistMessages( $single = false ) {
 	$messages = array(
-	
+
 /* English (Rob Church) */
 'en' => array(
 'blacklistedusername' => 'Blacklisted username',
 'blacklistedusernametext' => 'The user name you have chosen matches the [[MediaWiki:Usernameblacklist|
 list of blacklisted usernames]]. Please choose another name.',
 ),
-	
+
+/* Indonesian (Ivan Lanin) */
+'id' => array(
+'blacklistedusername' => 'Daftar hitam nama pengguna',
+'blacklistedusernametext' => 'Nama pengguna yang Anda pilih berada dalam [[MediaWiki:Usernameblacklist|
+daftar hitam nama pengguna]]. Harap pilih nama lain.',
+),
+
 	);
 	return $single ? $messages['en'] : $messages;
 }
