949f1d93f95c12c38a47d56f77b7c1a1bce145eb
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-17 23:35:44 +0000

    * Finish documenting the BookInformationDriver interface
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19422 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BookInformation/drivers/Driver.php
+++ extensions/BookInformation/drivers/Driver.php
@@ -22,16 +22,46 @@ interface BookInformationDriver {
 	 */
 	public function submitRequest( $isbn );
 	
+	/**
+	 * Get the title
+	 *
+	 * @return mixed Title string or false if unavailable
+	 */
 	public function getTitle();
 	
+	/**
+	 * Get the author
+	 *
+	 * @return mixed Author string or false if unavailable
+	 */
 	public function getAuthor();
 	
+	/**
+	 * Get the publisher
+	 *
+	 * @return mixed Publisher string or false if unavailable
+	 */
 	public function getPublisher();
 	
+	/**
+	 * Get the year
+	 *
+	 * @return mixed Year string or false if unavailable
+	 */
 	public function getYear();
 	
+	/**
+	 * Get a link allow users to purchase the item
+	 *
+	 * @return mixed Purchase link or false if not applicable
+	 */
 	public function getPurchaseLink();
 	
+	/**
+	 * Get a link to the provider's information page
+	 *
+	 * @return string Provider link
+	 */
 	public function getProviderLink();
 	
 }
