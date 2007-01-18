c8369da6d173ddd3c361b57c0846eb9f7403bd33
ivanlanin <ivanlanin@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-18 11:12:43 +0000

    Indonesian (id) localisation for BookInformation extension.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19445 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BookInformation/BookInformation.i18n.php
+++ extensions/BookInformation/BookInformation.i18n.php
@@ -7,7 +7,7 @@
  * @subpackage Extensions
  * @author Rob Church <robchur@gmail.com>
  */
- 
+
 function efBookInformationMessages() {
 	$messages = array(
 
@@ -39,19 +39,33 @@ function efBookInformationMessages() {
 'bookinfo-provider' => 'Daten-Lieferant: $1',
 ),
 
-/* Dutch (Dirk Beetstra) */ 
-'nl' => array( 
-'bookinfo-header' => 'Boek informatie', 
-'bookinfo-result-title' => 'Titel:', 
-'bookinfo-result-author' => 'Auteur:', 
-'bookinfo-result-publisher' => 'Uitgever:', 
-'bookinfo-result-year' => 'Jaar:', 
-'bookinfo-error-invalidisbn' => 'Incorrect ISBN ingegeven.', 
-'bookinfo-error-nodriver' => 'Kon de juiste Boek Informatie Driver niet initialiseren.', 
-'bookinfo-error-noresponse' => 'Geen respons of een time-out.', 
-'bookinfo-purchase' => 'Koop dit boek bij $1', 
-'bookinfo-provider' => 'Data geleverd door: $1', 
-), 
+/* Indonesian (Ivan Lanin) */
+'id' => array(
+'bookinfo-header' => 'Informasi buku',
+'bookinfo-result-title' => 'Judul:',
+'bookinfo-result-author' => 'Pengarang:',
+'bookinfo-result-publisher' => 'Penerbit:',
+'bookinfo-result-year' => 'Tahun:',
+'bookinfo-error-invalidisbn' => 'ISBN yang dimasukkan tidak sah.',
+'bookinfo-error-nodriver' => 'Tidak dapat menginisiasi Book Information Driver.',
+'bookinfo-error-noresponse' => 'Tak ada respons atau respons terlalu lama.',
+'bookinfo-purchase' => 'Beli buku ini dari $1',
+'bookinfo-provider' => 'Penyedia data: $1',
+),
+
+/* Dutch (Dirk Beetstra) */
+'nl' => array(
+'bookinfo-header' => 'Boek informatie',
+'bookinfo-result-title' => 'Titel:',
+'bookinfo-result-author' => 'Auteur:',
+'bookinfo-result-publisher' => 'Uitgever:',
+'bookinfo-result-year' => 'Jaar:',
+'bookinfo-error-invalidisbn' => 'Incorrect ISBN ingegeven.',
+'bookinfo-error-nodriver' => 'Kon de juiste Boek Informatie Driver niet initialiseren.',
+'bookinfo-error-noresponse' => 'Geen respons of een time-out.',
+'bookinfo-purchase' => 'Koop dit boek bij $1',
+'bookinfo-provider' => 'Data geleverd door: $1',
+),
 
 /* Finnish (Niklas Laxström) */
 'fi' => array(
@@ -66,7 +80,7 @@ function efBookInformationMessages() {
 'bookinfo-purchase' => 'Osta tämä kirja: $1',
 'bookinfo-provider' => 'Tietolähde: $1',
 ),
-	
+
 	);
 	return $messages;
 }
