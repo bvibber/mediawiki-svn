2a6eb75e052a3aaa7ba1adb5f833aca6b58b784f
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-21 16:56:53 +0000

    Fi i18n, patch by nikerabbit backported from trunk r19534
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19556 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/AjaxShowEditors/AjaxShowEditors.i18n.php
+++ extensions/AjaxShowEditors/AjaxShowEditors.i18n.php
@@ -8,4 +8,9 @@ $wgAjaxShowEditorsMessages['en'] = array(
 	'ajax-se-pending' => 'pending refresh ... (click this box or start editing)',
 	'ajax-se-idling' => '($1s ago)',
 );
+$wgAjaxShowEditorsMessages['fi'] = array(
+	'ajax-se-title'   => 'Samanaikaiset muokkaajat:',
+	'ajax-se-pending' => 'odotetaan päivitystä… (napsauta tästä tai aloita muokkaaminen)',
+	'ajax-se-idling'  => '($1 s sitten)',
+);
 ?>
