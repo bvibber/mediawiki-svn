3ab117bbb210514a0c13e70d23b1c1dab21e965b
hashar <hashar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-01-21 16:55:19 +0000

    The 'Ajax show editors' based on an idea by Tim Starling. This is r19529 implemented as an extension.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19555 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AjaxShowEditors/AjaxShowEditors.i18n.php
@@ -0,0 +1,11 @@
+<?php
+
+global $wgAjaxShowEditorsMessages;
+$wgAjaxShowEditorsMessages = array();
+
+$wgAjaxShowEditorsMessages['en'] = array(
+	'ajax-se-title' => 'Currently editing:',
+	'ajax-se-pending' => 'pending refresh ... (click this box or start editing)',
+	'ajax-se-idling' => '($1s ago)',
+);
+?>
