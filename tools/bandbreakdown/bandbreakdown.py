e31789bd04edb01810de9606ce12b1da4ad011e3
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-02-07 01:02:37 +0000

    multiple reports...
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19809 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/bandbreakdown/bandbreakdown.py
+++ tools/bandbreakdown/bandbreakdown.py
@@ -15,21 +15,36 @@ matchTypes = [
 groups = [group for (group, regex) in matchTypes]
 groups.append("other")
 
-totalHits = 0
-totalBytes = 0
-hits = {}
-bytes = {}
+interval = 1000
+
+hits = {"total": 0}
+bytes = {"total": 0}
 for group in groups:
 	hits[group] = 0
 	bytes[group] = 0
+
+def dump(groups, hits, bytes):
+	print "--"
+	if hits["total"] == 0 or bytes["total"] == 0:
+		print "no hits detected :("
+
+	for group in groups:
+		if hits[group] > 0:
+			print "%12s %8d hits (%6.2f%%) %16d bytes (%6.2f%%) %10.1f bytes/hit" % (
+				group,
+				hits[group],
+				100.0 * float(hits[group]) / float(hits["total"]),
+				bytes[group],
+				100.0 * float(bytes[group]) / float(bytes["total"]),
+				float(bytes[group]) / float(hits[group]))
 
 for line in sys.stdin:
 	matches = lineSplit.match(line)
 	if matches:
 		size = int(matches.group(1))
 		url = matches.group(2)
-		totalHits += 1
-		totalBytes += size
+		hits["total"] += 1
+		bytes["total"] += size
 		for (group, regex) in matchTypes:
 			if regex.match(url):
 				hits[group] += 1 
@@ -38,16 +53,8 @@ for line in sys.stdin:
 		else:
 			hits["other"] += 1
 			bytes["other"] += size
+	if hits["total"] % interval == 0:
+		dump(groups, hits, bytes)
 
-if totalHits == 0 or totalBytes == 0:
-	print "no hits detected :("
-
-for group in groups:
-	if hits[group] > 0:
-		print "%12s %8d hits (%6.2f%%) %16d bytes (%6.2f%%) %10.1f bytes/hit" % (
-			group,
-			hits[group],
-			100.0 * float(hits[group]) / float(totalHits),
-			bytes[group],
-			100.0 * float(bytes[group]) / float(totalBytes),
-			float(bytes[group]) / float(hits[group]))
+if hits["total"] % interval != 0:
+	dump(groups, hits, bytes)
