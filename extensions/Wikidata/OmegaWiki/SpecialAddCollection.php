b3149af2691c7454ff9ada141a979b3990694829
sburke <sburke@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-02-14 05:58:28 +0000

    Add simple collection creation script.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@19926 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Wikidata/OmegaWiki/SpecialAddCollection.php
@@ -0,0 +1,50 @@
+<?php
+	if (!defined('MEDIAWIKI')) die();
+
+	$wgAvailableRights[] = 'addcollection';
+	$wgGroupPermissions['bureaucrat']['addcollection'] = true;
+
+	$wgExtensionFunctions[] = 'wfSpecialAddCollection';
+
+	function wfSpecialAddCollection() {
+		class SpecialAddCollection extends SpecialPage {
+			function SpecialAddCollection() {
+				SpecialPage::SpecialPage('AddCollection');
+			}
+
+			function execute($par) {
+				global $wgOut, $wgUser, $wgRequest;
+
+				$wgOut->setPageTitle('Add Collection');
+
+				if (!$wgUser->isAllowed('addcollection')) {
+					$wgOut->addHTML('You do not have permission to add a collection.');
+					return false;
+				}
+
+				$dbr = &wfGetDB(DB_MASTER);
+
+				if ($wgRequest->getText('collection')) {
+					require_once('Expression.php');
+					require_once('Transaction.php');
+
+					$collectionName = $wgRequest->getText('collection');
+					startNewTransaction($wgUser->getID(), wfGetIP(), 'Add collection ' . $collectionName);
+					bootstrapCollection($collectionName,$wgRequest->getText('language'),$wgRequest->getText('type'));
+					$wgOut->addHTML('<strong>Collection ' . $collectionName . ' added.</strong><br />');	
+				}
+
+				$wgOut->addHTML(getOptionPanel(
+					array(
+						'Collection name' => getTextBox('collection'),
+						'Language of name' => getSuggest('language','language'),
+						'Collection type' => getSelect('type',array('' => 'None','RELT' => 'RELT','LEVL' => 'LEVL','CLAS' => 'CLAS'))
+					),
+					'',array('create' => 'Create')
+				));
+			}
+		}
+
+		SpecialPage::addPage(new SpecialAddCollection);
+	}
+?>
