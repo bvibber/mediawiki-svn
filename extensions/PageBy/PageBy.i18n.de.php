0da0dbaadf2d58f118e28c99101139be999bcb39
daniel <daniel@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-03-06 23:52:21 +0000

    PageBy extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@20184 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PageBy/PageBy.i18n.de.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Internationalisation file for the ContactPage extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['pageby-first']= 'Seite angelegt von $1, $2';
+$messages['pageby-last']= 'Zuletzt <a href="$3">geändert</a> von $1, $2';
+$messages['pageby-contributors']= 'Beiträge:';
+$messages['pageby-anon']= '<i>anonym</i>';
+?>
