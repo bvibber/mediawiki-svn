8fb36e2848d5376dd59017f115f9dfa215fc183c
jhb <jhb@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-03-13 20:25:34 +0000

    my working copy
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/jhb@20406 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/languages/messages/MessagesJv.php
@@ -0,0 +1,50 @@
+<?php
+/** Javanese (Basa Jawa)
+ *
+ * @addtogroup Language
+ *
+ * @author Niklas Laxström
+ *
+ * @copyright Copyright © 2006, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+$fallback = 'id';
+
+$namespaceNames = array(
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Astamiwa',
+	NS_MAIN             => '',
+	NS_TALK             => 'Dhiskusi',
+	NS_USER             => 'Panganggo',
+	NS_USER_TALK        => 'Dhiskusi_Panganggo',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK     => 'Dhiskusi_$1',
+	NS_IMAGE            => 'Gambar',
+	NS_IMAGE_TALK       => 'Dhiskusi_Gambar',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Dhiskusi_MediaWiki',
+	NS_TEMPLATE         => 'Cithakan',
+	NS_TEMPLATE_TALK    => 'Dhiskusi_Cithakan',
+	NS_HELP             => 'Pitulung',
+	NS_HELP_TALK        => 'Dhiskusi_Pitulung',
+	NS_CATEGORY         => 'Kategori',
+	NS_CATEGORY_TALK    => 'Dhiskusi_Kategori'
+);
+
+$namespaceAliases = array(
+	'Gambar_Dhiskusi' => NS_IMAGE_TALK,
+	'MediaWiki_Dhiskusi' => NS_MEDIAWIKI_TALK,
+	'Cithakan_Dhiskusi' => NS_TEMPLATE_TALK,
+	'Pitulung_Dhiskusi' => NS_HELP_TALK,
+	'Kategori_Dhiskusi' => NS_CATEGORY_TALK,
+);
+
+$messages = array(
+# Preferences page
+'qbsettings-none'	=> 'Ora ana',
+'qbsettings-fixedleft'	=> 'Tetep sisih kiwa',
+'qbsettings-fixedright'	=> 'Tetep sisih tengen',
+'qbsettings-floatingleft'	=> 'Ngambang sisih kiwa',
+);
+
+?>
