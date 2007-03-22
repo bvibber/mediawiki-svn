46a41700b747b3590359372347742a206c9b4a50
80686 <80686@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-03-22 08:09:31 +0000

    changed the internal structure a bit
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@20610 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BreadCrumbs/BreadCrumbs.php
+++ extensions/BreadCrumbs/BreadCrumbs.php
@@ -42,64 +42,6 @@ function fnBreadCrumbs() {
   $wgHooks['OutputPageParserOutput'][] = 'fnBreadCrumbsOutputHook';
 }
 
-function fnBreadCrumbsShowHook( &$m_pageObj ) {
-  global $wgTitle;
-  global $wgOut;
-  global $wgUser;
-  global $wgBreadCrumbsDelimiter;
-  global $wgBreadCrumbsCount;
-  
-  # deserialize data from session into array:
-  $m_BreadCrumbs = array();
-  if( isset( $_SESSION['BreadCrumbs'] ) ) $m_BreadCrumbs = $_SESSION['BreadCrumbs'];
-  # cache index of last element:
-  $m_count = count( $m_BreadCrumbs ) - 1;
-  
-  # check for doubles:
-  if( $m_BreadCrumbs[ $m_count ] != $wgTitle->getPrefixedText() ) {
-    # reduce the array set, remove older elements:
-    $m_BreadCrumbs = array_slice( $m_BreadCrumbs, ( 1 - $wgBreadCrumbsCount ) );
-    # add new page:
-    array_push( $m_BreadCrumbs, $wgTitle->getPrefixedText() );
-    # serialize data from array to session:
-    $_SESSION['BreadCrumbs'] = $m_BreadCrumbs;
-  }
-  # update cache:
-  $m_count = count( $m_BreadCrumbs ) - 1;
-  
-  # acquire a skin object:
-  $m_skin =& $wgUser->getSkin();
-  # build the breadcrumbs trail:
-  $m_trail = '<div id="BreadCrumbsTrail">';
-  for( $i = 0; $i <= $m_count; $i++ ) {
-    $m_trail .= $m_skin->makeLink( $m_BreadCrumbs[$i] );
-    if( $i < $m_count ) $m_trail .= $wgBreadCrumbsDelimiter;
-  }
-  $m_trail .= '</div>';
-  $wgOut->addHTML( $m_trail );
-  
-  # invalidate internal MediaWiki cache:
-  $wgTitle->invalidateCache();
-  $wgUser->invalidateCache();
-  
-  # Return true to let the rest work:
-  return true;
-}
-
-## Entry point for the hook for printing the CSS:
-function fnBreadCrumbsOutputHook( &$m_pageObj, &$m_parserOutput ) {
-  global $wgScriptPath;
-
-  # Register CSS file for our select box:
-  $m_pageObj->addLink(
-    array(
-      'rel'   => 'stylesheet',
-      'type'  => 'text/css',
-      'href'  => $wgScriptPath . '/extensions/BreadCrumbs/BreadCrumbs.css'
-    )
-  );
-
-  # Be nice:
-  return true;
-}
+## Load the file containing the hook functions:
+require_once( 'BreadCrumbsFunctions.php' );
 ?>
