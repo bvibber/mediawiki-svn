c7c2b42142409b834da4dcd542fe1d5a90e1bf3b
daniel <daniel@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-04-03 16:49:15 +0000

    new extension that shows talk page inlined on article page
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@20969 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/TalkHere/TalkHere.i18n.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Internationalisation file for the TalkHere extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Daniel Kinzler
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages['talkhere-title']= '$1';
+$messages['talkhere-headtext']= 'The following comments may have been left by visitors.<br/> 
+	This site\'s operators can not take responsibility for the content of such comments.';
+$messages['talkhere-foottext']= 'The above comments may have been left by visitors. <br/>
+	This site\'s operators can not take responsibility for the content of such comments.';
+$messages['talkhere-talkpage']= 'talk page';
+$messages['talkhere-notalk']= '\'\'(no comments yet)\'\'';
+$messages['talkhere-addcomment']= 'add comment';
+
+$messages['talkhere-beforeinput']= 'You can add a comment below';
+$messages['talkhere-afterinput']= '..................';
+$messages['talkhere-afterform']= '~~~~~~~~~~~~~~~';
+
+$messages['talkhere-loading']= 'loading editor...';
+$messages['talkhere-collapse']= 'hide editor';
+?>
