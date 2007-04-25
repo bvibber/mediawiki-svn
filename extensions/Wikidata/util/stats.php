a067fb745bcd900cd4b3b57702293e02e78490c6
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-04-25 20:45:56 +0000

    Updated stats.php version from author. Modified to use LocalSettings.php for passwords
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@21590 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/util/stats.php
+++ extensions/Wikidata/util/stats.php
@@ -1,15 +1,19 @@
 <?php
+//header("Content-type: text/html; charset=UTF-8");
 
-header("Content-type: text/html; charset=iso-8859-1");
+define('MEDIAWIKI', true );
+include_once("../../../LocalSettings.php");
+global $wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname;
 
-$db1="localhost";  # hostname
-$db2="root";  # user
-$db3="nicheGod";  # pass
-$db4="omegawiki";  # db-name
+$db1=$wgDBserver;  # hostname
+$db2=$wgDBuser;  # user
+$db3=$wgDBpassword;  # pass
+$db4=$wgDBname;  # db-name
 
-@$connection=MySQL_connect($db1,$db2,$db3);
+$connection=MySQL_connect($db1,$db2,$db3);
 if (!$connection)die("Cannot connect to SQL server. Try again later.");
 MySQL_select_db($db4)or die("Cannot open database");
+
 echo "
 <style type=\"text/css\"><!--
 body {font-family:arial,sans-serif}
@@ -52,20 +56,32 @@ group by language_id
 order by count(*) desc
  ")or die ("error ".mysql_error());
 
-echo "<h1>Number of expressions per language</h1>";
+echo "<center>
 
-echo "<center><table cellpadding=0><tr><td><b>Language</b></td><td><b>Entries</b></td><td></td></tr>";
+<h1>Number of expressions per language</h1>
+<hr width=950 size=1 noshade><br>
+<table cellpadding=0 width=950><tr><td width=200><b>Language</b></td><td><b>Entries</b></td><td></td></tr>";
 $width=600;
+$limit=500;
 $max=0;
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
 if($max<$row[1])$max=$row[1];
 $wi=ceil((($row[1]/$max)*$width));
 $per=ceil((($row[1]/$max)*100));
-echo "<tr><td >".$lang[$row[0]]."</td><td>".$row[1]."</td><td><img src=sc1.png width=\"$wi\" height=20> $per %</td></tr>";
+if($row[1]>$limit)echo "<tr><td >".$lang[$row[0]]."</td><td>".$row[1]."</td><td><img src=sc1.png width=\"$wi\" height=20> $per %</td></tr>";
+else $tx.=$lang[$row[0]].", ";
 //$ar[$row[0]].=$row[1]."	".$row[2]."\n";
 //filewrite("out/".$row[0].".txt",$row[1]."	".$row[2]);
 }
-echo "</table><center>";
+echo "
+<tr><td colspan=4>
+<div align=justify>
+
+<h3>Languages with less than $limit entries:</h3>
+$tx
+</div>
+</td>
+</table><center>";
 
 /*
 for($i=0;$i<250;$i++){
@@ -78,7 +94,26 @@ if(strlen($ar[$i])>20)filewrite("out/".$lang[$i].".txt",$ar[$i]);
 
 //echo "<pre>".$ar[85]."</pre>";
 
-echo substr((stopwatch()-$start),0,4)." seconds";
+echo "
+<br>
+<hr size=1 noshade width=950>
+<table width=950><tr><td>
+<small>Page time: ".substr((stopwatch()-$start),0,5)." seconds</small>
+<td align=right>
+
+<small>Script contributed by <a href=http://www.dicts.info/>Zdenek Broz</a>
+</small>
+</td>
+</tr></table>
+<br>";
+
+function filewrite($file,$txt){
+$fw=fopen($file,"w+");
+fwrite($fw,$txt."\n");
+fclose($fw);
+}
+
+
 
 
 ?>
