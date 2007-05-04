10c916ed736dbc26af9c2ac1080cd5534f2c16e1
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-04 23:47:00 +0000

    Some more table indexes made to help with stats (so sue me!)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@21880 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Convenience/Create uw_defined_meaning indices.sql
+++ extensions/Wikidata/Database scripts/Convenience/Create uw_defined_meaning indices.sql
@@ -4,8 +4,8 @@ ALTER TABLE `uw_defined_meaning`
 	ADD INDEX `versioned_end_meaning_text` (`remove_transaction_id`, `meaning_text_tcid`, `defined_meaning_id`),
 	ADD INDEX `versioned_start_meaning` (`add_transaction_id`, `defined_meaning_id`, `expression_id`),
 	ADD INDEX `versioned_start_expression` (`add_transaction_id`, `expression_id`, `defined_meaning_id`),
-	ADD INDEX `versioned_start_meaning_text` (`add_transaction_id`, `meaning_text_tcid`, `defined_meaning_id`);
-	
+	ADD INDEX `versioned_start_meaning_text` (`add_transaction_id`, `meaning_text_tcid`, `defined_meaning_id`),
+	ADD INDEX `defined_meaning_idx` (`defined_meaning_id`);
 --	ADD INDEX `unversioned_meaning` (`defined_meaning_id`, `expression_id`),
 --	ADD INDEX `unversioned_expression` (`expression_id`, `defined_meaning_id`),
 --	ADD INDEX `unversioned_meaning_text` (`meaning_text_tcid`, `defined_meaning_id`);
