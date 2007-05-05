c0bb5889e51eb55c44d86e8260aac4f5d8ebdc85
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-05 01:46:13 +0000

    So why was th header not set?
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@21886 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/util/stats.php
+++ extensions/Wikidata/util/stats.php
@@ -1,5 +1,5 @@
 <?php
-//header("Content-type: text/html; charset=UTF-8");
+header("Content-type: text/html; charset=UTF-8");
 
 define('MEDIAWIKI', true );
 include_once("../../../LocalSettings.php");
