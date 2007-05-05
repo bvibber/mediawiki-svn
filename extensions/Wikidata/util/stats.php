ffdfc2fba35585d790adf1767f6397d3ef58eb98
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-05 08:53:02 +0000

    reverted to older query, as gerard thinks it is more accurate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@21896 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/util/stats.php
+++ extensions/Wikidata/util/stats.php
@@ -67,11 +67,16 @@ $lang[$row[0]]=$row[2];
 
 
 ////////////////////////////////////////////////////////
-$result = mysql_query("SELECT 
+$result = mysql_query("
+SELECT 
 language_id, count(*)
-FROM uw_expression_ns, uw_syntrans
-WHERE uw_expression_ns.expression_id=uw_syntrans.expression_id
-AND uw_syntrans.remove_transaction_id IS NULL
+FROM uw_expression_ns
+WHERE expression_id IN
+(
+	SELECT DISTINCT expression_id
+	FROM uw_syntrans
+	WHERE remove_transaction_id IS NULL
+)
 group by language_id
 order by count(*) desc
  ")or die ("error ".mysql_error());
