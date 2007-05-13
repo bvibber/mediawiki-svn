1dc3b27556a35168878768b90442a45cf1d85850
mark <mark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-13 18:40:10 +0000

    Add and delete files...
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@22152 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- debs/wikimedia-lvs-realserver/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the wikimedia-lvs-realserver package
-#
-0 4	* * *	root	wikimedia-lvs-realserver_maintenance
--- debs/wikimedia-lvs-realserver/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(wikimedia-lvs-realserver):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="wikimedia-lvs-realserver" command="/usr/bin/wikimedia-lvs-realserver"
