56a2fb2f6c5c3269f33227dc2f7fe714b5ba9281
magnusmanske <magnusmanske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-17 20:22:46 +0000

    New extension, wanted by commons; might have some display problems in IE
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@22233 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/MiniPreview/MiniPreview.i18n.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages['minipreview-files_in_category'] = '$1 total files in this category.';
+$messages['minipreview-files_in_gallery'] = '$1 total files in this gallery.';
+$messages['minipreview-no_more_files_here'] = 'No more files in this direction.';
+$messages['minipreview-ignore_categories'] = '
+The following is a list of category names or name beginnings. These categories will
+not be displayed by MiniPreview. For example, "* PD-" will not display all categories
+that start with "PD-", such as "PD-old".
+* GFDL-
+* GFDL
+* CC-
+* PD-
+* Self
+' ;
+
+?>
