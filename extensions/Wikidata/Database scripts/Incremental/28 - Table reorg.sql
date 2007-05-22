1903d7ad5ebde876df1dadf248104f31fce0fa0b
erik <erik@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-22 09:23:00 +0000

    further prep for datasets
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@22320 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Wikidata/Database scripts/Incremental/28 - Table reorg.sql
@@ -0,0 +1,13 @@
+-- These tables can exist within each dataset.
+RENAME TABLE translated_content to uw_translated_content;
+RENAME TABLE transactions to uw_transactions;
+
+-- We used to share this with MediaWiki, but it makes more sense to
+-- have our own within each data set.
+CREATE TABLE `uw_text` (
+  `text_id` int(8) unsigned NOT NULL auto_increment,
+  `text_text` mediumblob NOT NULL,
+  `text_flags` tinyblob NOT NULL,
+  PRIMARY KEY  (`text_id`)
+) DEFAULT CHARSET=utf8;
+INSERT INTO `script_log` (`time`, `script_name`) VALUES (NOW(), '28 - Table reorg.sql');
