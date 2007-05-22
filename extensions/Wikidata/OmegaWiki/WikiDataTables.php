c488adff3b594eeaff515e1b5f23e0d11fe44fbc
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-05-22 10:48:33 +0000

    dc WikiDataTables
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@22328 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/OmegaWiki/WikiDataTables.php
+++ extensions/Wikidata/OmegaWiki/WikiDataTables.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once("Wikidata.php");
+$wdDataSetContext=DefaultWikidataApplication::getDataSetContext();
+$dc=$wdDataSetContext; // See? We're keeping this readable :-P
+
 class Table {
 	public $name;
 	public $isVersioned;
@@ -19,19 +23,19 @@ global
 	$urlAttributeValuesTable;
 	
 $transactionsTable = new Table('transactions', false, array('transaction_id'));
-$meaningRelationsTable = new Table('uw_meaning_relations', true, array('relation_id'));
-$classMembershipsTable = new Table('uw_class_membership', true, array('class_membership_id'));
-$collectionMembershipsTable = new Table('uw_collection_contents', true, array('collection_id', 'member_mid'));
-$syntransTable = new Table('uw_syntrans', true, array('syntrans_sid'));
+$meaningRelationsTable = new Table("{$dc}_meaning_relations", true, array('relation_id'));
+$classMembershipsTable = new Table("{$dc}_class_membership", true, array('class_membership_id'));
+$collectionMembershipsTable = new Table("{$dc}_collection_contents", true, array('collection_id', 'member_mid'));
+$syntransTable = new Table("{$dc}_syntrans", true, array('syntrans_sid'));
 $translatedContentTable = new Table('translated_content', true, array('translated_content_id', 'language_id'));
-$alternativeDefinitionsTable = new Table('uw_alt_meaningtexts', true, array('meaning_mid', 'meaning_text_tcid'));
-$classAttributesTable = new Table('uw_class_attributes', true, array('object_id'));
+$alternativeDefinitionsTable = new Table("{$dc}_alt_meaningtexts", true, array('meaning_mid', 'meaning_text_tcid'));
+$classAttributesTable = new Table("{$dc}_class_attributes", true, array('object_id'));
 
-$translatedContentAttributeValuesTable = new Table('uw_translated_content_attribute_values', true, array('value_id'));
-$textAttributeValuesTable = new Table('uw_text_attribute_values', true, array('value_id'));
-$urlAttributeValuesTable = new Table('uw_url_attribute_values', true, array('value_id'));
-$optionAttributeOptionsTable = new Table('uw_option_attribute_options', true, array('attribute_id', 'option_mid'));
-$optionAttributeValuesTable = new Table('uw_option_attribute_values', true, array('value_id'));
-$urlAttributeValuesTable = new Table('uw_url_attribute_values', true, array('value_id'));
+$translatedContentAttributeValuesTable = new Table("{$dc}_translated_content_attribute_values", true, array('value_id'));
+$textAttributeValuesTable = new Table("{$dc}_text_attribute_values", true, array('value_id'));
+$urlAttributeValuesTable = new Table("{$dc}_url_attribute_values", true, array('value_id'));
+$optionAttributeOptionsTable = new Table("{$dc}_option_attribute_options", true, array('attribute_id', 'option_mid'));
+$optionAttributeValuesTable = new Table("{$dc}_option_attribute_values", true, array('value_id'));
+$urlAttributeValuesTable = new Table("{$dc}_url_attribute_values", true, array('value_id'));
 
 ?>
