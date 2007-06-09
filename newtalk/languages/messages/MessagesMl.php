a2c048590ef1142c78de770b8e431171611d0768
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-09 22:42:17 +0000

    Branch to poke about with new talk flags, fix up some issues and clean up the code a bit
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/robchurch@22872 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ newtalk/languages/messages/MessagesMl.php
@@ -0,0 +1,22 @@
+<?php
+/** Malayalam (മലയാളം)
+  *
+  * @addtogroup Language
+  *
+  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+  */
+
+$digitTransformTable = array(
+	'0' => '൦', # &#x0d66;
+	'1' => '൧', # &#x0d67;
+	'2' => '൨', # &#x0d68;
+	'3' => '൩', # &#x0d69;
+	'4' => '൪', # &#x0d6a;
+	'5' => '൫', # &#x0d6b;
+	'6' => '൬', # &#x0d6c;
+	'7' => '൭', # &#x0d6d;
+	'8' => '൮', # &#x0d6e;
+	'9' => '൯', # &#x0d6f;
+);
+
+?>
