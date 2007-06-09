fa6e19e407ce64a765e45682028e01e18c8d0070
david <david@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-09 05:53:39 +0000

    JS now makes sure date ranges are sane, CORRECTLY.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/liquidthreads@22860 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- skins/common/lqt.js
+++ skins/common/lqt.js
@@ -11,6 +11,37 @@ function lqt_add_event(obj, evType, fn){
 	} 
 }
 
+var LqtDateRangeRectifier = function( startsel, endsel ) {
+	this.startsel = startsel;
+	this.endsel = endsel;
+	
+	this.oldstart = this.startsel.selectedIndex;
+	this.oldend = this.endsel.selectedIndex;
+
+	this.handle_start_changed = function(e) {
+		newi = this.startsel.selectedIndex;
+		oldi = this.oldstart;
+		endi = this.endsel.selectedIndex;
+		goal = newi - (oldi - endi); // seem backwards?  it's because
+		this.endsel.selectedIndex = Math.max(goal, 0);
+		this.oldend = endi;          // later months have smaller
+		this.oldstart = newi;        // indexes.
+	}
+
+	this.handle_end_changed = function(e){
+		this.startsel.selectedIndex = Math.max(this.endsel.selectedIndex,
+                                                       this.startsel.selectedIndex)
+		this.oldend = this.endsel.selectedIndex;
+		this.oldstart = this.startsel.selectedIndex;
+	}
+
+	// In order for this instance to recieve the events, we need to capture the
+	// current value of 'this' with a closure, because this = the target object
+	// in event handlers.
+	me = this;
+	lqt_add_event( this.startsel, 'change', function(e) { me.handle_start_changed(e) });
+	lqt_add_event( this.endsel, 'change', function(e) { me.handle_end_changed(e) });
+}
 
 function lqt_on_load() {
 	if(!document.getElementById) return;
@@ -28,9 +59,8 @@ function lqt_on_load() {
 
 	var searchform = document.getElementById("lqt_archive_search_form");
 	if ( searchform ) {
-		lqt_add_event( document.getElementById("lqt_archive_start"), 'change', function(e) {
-			alert(e.target);
-		} );
+		new LqtDateRangeRectifier( document.getElementById("lqt_archive_start"),
+					   document.getElementById("lqt_archive_end"));
 	}
 }
 
