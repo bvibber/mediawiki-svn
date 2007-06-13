34b1b9c2bfcbcfefee07a41a205595dd85ca2df5
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-13 00:59:12 +0000

    - prog.mk: support installing manpage if present - add manpage for pwtool
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@22943 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/tsutils/mk/prog.mk
+++ tools/tsutils/mk/prog.mk
@@ -4,6 +4,9 @@ BINDIR	?= /usr/local/bin
 BINMODE	?= 755
 OWNER	?= root
 GROUP	?= bin
+MANSECT	?= 1
+
+MANDIR	?= /usr/local/man/man$(MANSECT)
 
 all: $(PROG)
 $(PROG): $(OBJS)
@@ -17,5 +20,9 @@ install: all realinstall $(INSTALLEXTRA)
 realinstall:
 	install -d -m 755 $(BINDIR)
 	install -m $(BINMODE) -o $(OWNER) -g $(GROUP) $(PROG) $(BINDIR)
+	if test -f $(PROG).$(MANSECT); then \
+		install -d -m 755 $(MANDIR); \
+		install -m 644 $(PROG).$(MANSECT) $(MANDIR); \
+	fi
 
 .PHONY: clean all install realinstall lint
