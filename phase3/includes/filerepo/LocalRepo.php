c182680b41e159faab2afb143f9edee1d4177578
mikeb <mikeb@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-19 03:00:32 +0000

    Copied remotely to create new branch mikeb
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/mikeb@23085 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/includes/filerepo/LocalRepo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * A repository that stores files in the local filesystem and registers them
+ * in the wiki's own database. This is the most commonly used repository class.
+ */
+class LocalRepo extends FSRepo {
+	var $fileFactory = array( 'LocalFile', 'newFromTitle' );
+	var $oldFileFactory = array( 'OldLocalFile', 'newFromTitle' );
+
+	function getSlaveDB() {
+		return wfGetDB( DB_SLAVE );
+	}
+
+	function getMasterDB() {
+		return wfGetDB( DB_MASTER );
+	}
+
+	function newFileFromRow( $row ) {
+		if ( isset( $row->img_name ) ) {
+			return LocalFile::newFromRow( $row, $this );
+		} elseif ( isset( $row->oi_name ) ) {
+			return OldLocalFile::newFromRow( $row, $this );
+		} else {
+			throw new MWException( __METHOD__.': invalid row' );
+		}
+	}
+	
+	function newFromArchiveName( $title, $archiveName ) {
+		return OldLocalFile::newFromArchiveName( $title, $this, $archiveName );
+	}
+}
