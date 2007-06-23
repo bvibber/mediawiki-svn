0fa92ded59b13bceccdb9f5aea3c87b01dee2b03
Mark Bergsma <Mark Bergsma@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-23 18:49:32 +0000

    Script to move mail boxes of removed accounts out of the way
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23299 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ wmfmailadmin/mbcleanup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""
+WMF mailadmin mbcleanup
+Cleans up mailboxes for no longer existing accounts
+Written by Mark Bergsma <mark@wikimedia.org>
+"""
+
+import sys, os, sqlite3, time
+
+def get_accounts(dbname):
+    """
+    Retrieves a list of current accounts
+    """
+    
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute("SELECT domain, localpart FROM account")
+    
+    return set(cur.fetchall())
+    
+def get_mailboxes(mbroot):
+    """
+    Retrieves a list of mail boxes
+    """
+    
+    # 2 levels: domain/localpart
+    mailboxes = set()
+    for domain in os.listdir(mbroot):
+        domaindir = os.path.join(mbroot, domain)
+        if os.path.isdir(domaindir):
+            for localpart in os.listdir(domaindir):
+                if os.path.isdir(os.path.join(domaindir, localpart)):
+                    mailboxes.add((unicode(domain), unicode(localpart)))
+
+    return mailboxes
+
+def move_mailboxes(mbroot, mbbackuproot, mailboxes):
+    """
+    Moves a set of mailboxes to a backup location
+    """
+    
+    for mb in mailboxes:
+        date = time.strftime('%Y%m%d%H%M')
+        oldpath = os.path.join(mbroot, mb[0], mb[1])
+        newpath = os.path.join(mbbackuproot, mb[0], mb[1]) + '.' + date
+        print 'Moving', oldpath, '=>', newpath
+        os.makedirs(newpath)
+        os.rename(oldpath, newpath)
+
+def main():
+    """
+    Main function
+    """
+    
+    try:
+        dbname, mbroot, mbbackuproot = sys.argv[1:4]
+    except ValueError:
+        print >> sys.stderr, "Usage:", sys.argv[0], "{user.db file} {mail boxes root path} {backup root path}"
+        sys.exit(1)
+        
+    accounts, mailboxes = get_accounts(dbname), get_mailboxes(mbroot)
+    
+    # Move mailboxes without a corresponding account
+    move_mailboxes(mbroot, mbbackuproot, mailboxes - accounts)
+
+if __name__ == '__main__':
+    main()
