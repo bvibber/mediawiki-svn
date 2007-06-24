814179e9377b2375a56ba4d2a859c6d02c063524
mark <mark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-06-24 15:21:28 +0000

    wikimedia-task-dns-auth (0.05) feisty; urgency=medium
      * Depend on pdns >= 2.9.21
      * Update the configuration file to use the bind(2) backend
      * Move the countries file to a subdir owned by user pdns,
        so the nonprivileged cron job can update the zone file
      * Make the postrm script work with dash
    
    wikimedia-task-dns-auth (0.04) edgy; urgency=medium
    
      * Fix bug in authdns-update where the /etc/powerdns/geomaps sanity check
        only succeeds when in /etc/powerdns
      * .svn dirs were ending up in the package, remove them in debian/rules
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23322 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- debs/wikimedia-task-dns-auth/debian/cron.d
+++ debs/wikimedia-task-dns-auth/debian/cron.d
@@ -1,2 +1,1 @@
-07 04 * * *     pdns    rsync -qt rsync://countries-ns.mdc.dk/zone/zz.countries.nerd.dk.rbldnsd \
-                        /etc/powerdns/zz.countries.nerd.dk.rbldnsd && pdns_control rediscover > /dev/null
+7 4 * * *     pdns    rsync -qt "rsync://countries-ns.mdc.dk/zone/zz.countries.nerd.dk.rbldnsd'" /etc/powerdns/ip-map/zz.countries.nerd.dk.rbldnsd && pdns_control rediscover > /dev/null
