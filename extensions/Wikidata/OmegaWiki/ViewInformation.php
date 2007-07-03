7ea32c8c6da07fda462df60d35119677a0cb2c63
proes <proes@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-07-03 08:53:17 +0000

    * Added some comments to ViewInformation.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23665 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/OmegaWiki/ViewInformation.php
+++ extensions/Wikidata/OmegaWiki/ViewInformation.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * ViewInformation is used to capture various settings that influence the way a page will be viewed
+ * depending on different use case scenarios. For instance, by specifying a filterLanguageId a page
+ * with be filtered entirely on one language, collapsing record sets to records where appropiate.
+ * 
+ * A ViewInformation can be constructed based on various conditions. The language filtering for instance
+ * could be an application wide setting, or a setting that can be controlled by the user. Functions that
+ * use ViewInformation do not care about this. They are supposed to respect the settings provided wherever
+ * possible.  
+ */
+
 class ViewInformation {
 	public $filterLanguageId;
 	public $possiblySynonymousRelationTypeId;
