2b039227e5bef19dc8251da32f794d876bb20c7c
mulligen <mulligen@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-07-04 12:06:19 +0000

    create bootstrapped defined meanings in php script (in stead of in wikidataSourceTables sql template
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23703 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Convenience/Create wikidata.php
+++ extensions/Wikidata/Database scripts/Convenience/Create wikidata.php
@@ -3,6 +3,9 @@
 # this module create a wikidata extension for mediawiki
 # it generates the tables in a database (passed as parameter) with a defined prefix (passed as parameter)
 
+define('MEDIAWIKI', true );
+ob_end_flush();
+
 $wgUseMasterForMaintenance = true;
 
 $sep = PATH_SEPARATOR;
@@ -10,13 +13,23 @@ $IP = realpath( dirname( __FILE__ ) . "../../../../../" );
 $currentdir = dirname( __FILE__ );
 chdir( $IP );
 
-ini_set( 'include_path', ".$sepIP$sep$IP/includes$sep$IP/languages$sep$IP/maintenance" );
+ini_set( 'include_path', ".$sepIP$sep$IP/extensions/Wikidata/OmegaWiki$sep$IP/includes$sep$IP/languages$sep$IP/maintenance" );
 
+require_once( "Defines.php");
+require_once( "ProfilerStub.php");
+require_once( "LocalSettings.php");
+require_once( "Setup.php");
+require_once( "Attribute.php" );
+require_once( "Transaction.php");
+require_once( "WikiDataBootstrappedMeanings.php");
 require_once( "StartProfiler.php" );
-require_once( "includes/Exception.php");
-require_once( "includes/GlobalFunctions.php");
-require_once( "includes/Database.php");
+require_once( "Exception.php");
+require_once( "GlobalFunctions.php");
+require_once( "Database.php");
 
+
+global
+	$wgCommandLineMode, $wgUser, $numberOfBytes;
 
 function ReadSQLFile( $database, $pattern, $prefix, $filename ){
 	$fp = fopen( $filename, 'r' );
@@ -59,6 +72,94 @@ function ReadSQLFile( $database, $pattern, $prefix, $filename ){
 	return true;
 }
 
+function getUserId( $userName ){
+	$dbr = &wfGetDB(DB_SLAVE);
+	$result = $dbr->query( "select user_id from user where user_name = '$userName'" );
+	if ( $row = $dbr->fetchObject( $result ) ){
+		return $row->user_id;
+	}
+	else {
+		return -1;
+	}
+}
+
+function bootStrappedDefinedMeanings( $dc ){
+	global
+		$definedMeaningMeaningName, $definitionMeaningName,
+		$relationMeaningName, $synTransMeaningName,
+		$annotationMeaningName, $wgCommandLineMode;
+
+	$wgCommandLineMode = true;
+
+	$dbr =& wfGetDB(DB_MASTER);
+	
+	$dbr->query("DROP TABLE IF EXISTS `{$dc}_bootstrapped_defined_meanings`;");
+
+	$dbr->query("CREATE TABLE `{$dc}_bootstrapped_defined_meanings` (
+			`name` VARCHAR(255) NOT NULL ,
+			`defined_meaning_id` INT NOT NULL);");
+
+
+	$userId = getUserId( 'Root' );
+	if ( $userId == -1 ){
+		echo "root user undefined\n";
+		return;
+	}
+
+	setUser( $userId );
+
+	setDefaultDC( $dc );
+	
+	$rdc = wdGetDataSetContext();
+
+	startNewTransaction($userId, 0, "Script bootstrap class attribute meanings", $dc);
+
+	$languageId = 85;
+	$collectionId = bootstrapCollection("Class attribute levels", $languageId, "LEVL");
+	$meanings = array();
+	$meanings[$definedMeaningMeaningName] = bootstrapDefinedMeaning($definedMeaningMeaningName, $languageId, "The combination of an expression and definition in one language defining a concept.");
+	$meanings[$definitionMeaningName] = bootstrapDefinedMeaning($definitionMeaningName, $languageId, "A paraphrase describing a concept.");
+	$meanings[$synTransMeaningName] = bootstrapDefinedMeaning($synTransMeaningName, $languageId, "A translation or a synonym that is equal or near equal to the concept defined by the defined meaning.");
+	$meanings[$relationMeaningName] = bootstrapDefinedMeaning($relationMeaningName, $languageId, "The association of two defined meanings through a specific relation type.");
+	$meanings[$annotationMeaningName] = bootstrapDefinedMeaning($annotationMeaningName, $languageId, "Characteristic information of a concept.");
+
+	foreach($meanings as $internalName => $meaningId) {
+		addDefinedMeaningToCollection($meaningId, $collectionId, $internalName);
+	
+		$dbr->query("INSERT INTO `{$dc}_bootstrapped_defined_meanings` (name, defined_meaning_id) " . 
+				"VALUES (" . $dbr->addQuotes($internalName) . ", " . $meaningId . ")");
+	}
+
+	$timestamp = wfTimestampNow();
+	$dbr->query("INSERT INTO {$dc}_script_log (time, script_name, comment) " .
+		    "VALUES (". $timestamp . "," . $dbr->addQuotes('23 - Bootstrap class attribute meanings.php') .  "," . $dbr->addQuotes('create bootstrap class attribute meanings') . ")");
+	
+}
+
+function bootstrapDefinedMeaning($spelling, $languageId, $definition) {
+	$expression = findOrCreateExpression($spelling, $languageId); 
+	$definedMeaningId = createNewDefinedMeaning($expression->id, $languageId, $definition);
+
+	return $definedMeaningId;
+}
+
+function setUser( $userid ){
+	global $wgUser;
+	$wgUser->setId( $userid );
+	$wgUser->loadFromId();
+}
+
+function setDefaultDC( $dc ){
+	global $wgUser, $wdDefaultViewDataSet;
+
+	$groups=$wgUser->getGroups();
+	foreach($groups as $group) {
+		$wdGroupDefaultView[$group] = $dc;
+	}
+	$wdDefaultViewDataSet = $dc;
+}
+
+	
 $dbclass  = 'Database' . ucfirst( $wgDBtype ) ;
 $comment  = '';
 $database = $wgDBname;
@@ -126,9 +227,16 @@ if( !$wdDatabase->isOpen() ) {
 	exit();
 }
 
+
+
 ReadSQLFile( $wdDatabase, "/*\$wgWDprefix*/", $wgWDprefix, $currentdir . DIRECTORY_SEPARATOR . $wgWDtemplate );
+
 $wdDatabase->query( "DELETE FROM wikidata_sets WHERE set_prefix = '$prefix'" );
 $wdDatabase->query( "INSERT INTO wikidata_sets (set_prefix,set_fallback_name,set_dmid) VALUES ('$prefix','$comment',0)" );
+
+if ( $template == "wikidataSourceTables.sql" ){
+	bootStrappedDefinedMeanings( $prefix );
+}
 
 $queryResult = $wdDatabase->query( "SELECT user_name FROM user WHERE user_real_name = '$prefix'" );
 if ( $row = $wdDatabase->fetchObject( $queryResult ) ){
@@ -139,3 +247,5 @@ else{
 }
 $wdDatabase->close();
 
+
+?>
