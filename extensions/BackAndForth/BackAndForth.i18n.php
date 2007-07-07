55f6fa2df3ba080881a083afe4b10fc531e3a27e
robchurch <robchurch@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-07-07 02:23:51 +0000

    'Back-And-Forth': Extension adds a pair of alphabetic paging links to the top of the page on article views
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23815 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/BackAndForth/BackAndForth.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalisation file for the BackAndForth extension
+ *
+ * @author Rob Church <robchur@gmail.com>
+ */
+
+/**
+ * Fetch extension messages indexed per language
+ *
+ * @return array
+ */
+function efBackAndForthMessages() {
+	$messages = array(
+
+/**
+ * English
+ */
+'en' => array(
+	'backforth-next' => 'Next ($1)',
+	'backforth-prev' => 'Previous ($1)',
+),
+	
+	);	
+	return $messages;
+}
