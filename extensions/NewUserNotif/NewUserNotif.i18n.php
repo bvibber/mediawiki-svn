aa3a4d702dd6a2fe6ffd8a1f89632cf5a0bfc264
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-07-10 12:01:06 +0000

    Updates German
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@23951 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/NewUserNotif/NewUserNotif.i18n.php
+++ extensions/NewUserNotif/NewUserNotif.i18n.php
@@ -14,6 +14,11 @@ function efNewUserNotifMessages() {
 	'newusernotifsubj' => 'New User Notification for $1',
 	'newusernotifbody' => "Hello $1,\n\nA new user account, $2, has been created on $3 at $4.",
 ),
+
+'de' => array(
+	'newusernotifsubj' => 'Benachrichtung für $1 über die Einrichtung eines neuen Benutzerskontos',
+	'newusernotifbody' => "Hallo $1,\n\nEin neues Benutzerkonto, $2, wurde am $4 auf $3 angelegt.",
+),
 	
 	);
 	return $messages;
