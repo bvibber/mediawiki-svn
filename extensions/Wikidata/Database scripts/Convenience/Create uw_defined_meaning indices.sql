31d7932cbecb9e54b1f926efd1f16d96d9963cde
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-07-20 21:39:17 +0000

    Will this go faster?
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@24290 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/Database scripts/Convenience/Create uw_defined_meaning indices.sql
+++ extensions/Wikidata/Database scripts/Convenience/Create uw_defined_meaning indices.sql
@@ -5,6 +5,7 @@ ALTER TABLE `%dc%_defined_meaning`
 	ADD INDEX `versioned_start_meaning` (`add_transaction_id`, `defined_meaning_id`, `expression_id`),
 	ADD INDEX `versioned_start_expression` (`add_transaction_id`, `expression_id`, `defined_meaning_id`),
 	ADD INDEX `versioned_start_meaning_text` (`add_transaction_id`, `meaning_text_tcid`, `defined_meaning_id`),
+	ADD INDEX `defining_expression_idx` (`defined_meaning_id`,`expression_id`),
 	ADD INDEX `defined_meaning_idx` (`defined_meaning_id`);
 --	ADD INDEX `unversioned_meaning` (`defined_meaning_id`, `expression_id`),
 --	ADD INDEX `unversioned_expression` (`expression_id`, `defined_meaning_id`),
