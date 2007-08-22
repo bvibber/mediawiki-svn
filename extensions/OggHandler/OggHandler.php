10fbf39a0d01e8a552b659cd2c144402ac15b564
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-08-22 16:55:21 +0000

    Swapping out the debug build of cortado for a stripped build; saves about 70k of client download
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25062 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/OggHandler/OggHandler.php
+++ extensions/OggHandler/OggHandler.php
@@ -22,7 +22,7 @@ $wgOggScriptVersion = '1';
 $wgFFmpegLocation = 'ffmpeg';
 $wgExtensionMessagesFiles['OggHandler'] = "$oggDir/OggHandler.i18n.php";
 $wgParserOutputHooks['OggHandler'] = array( 'OggHandler', 'outputHook' );
-$wgCortadoJarFile = "cortado-ovt-debug-0.2.2.1-patched.jar";
+$wgCortadoJarFile = "cortado-ovt-stripped-0.2.2.1-patched.jar";
 #$wgCortadoJarFile = "cortado-ovt-stripped-0.2.2.jar";
 
 ?>
