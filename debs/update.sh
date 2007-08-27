ed1538aee6fb36f530f8cea68de4071956ec0f0c
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-08-27 23:12:19 +0000

    Don't overwrite old packages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25217 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- debs/update.sh
+++ debs/update.sh
@@ -11,15 +11,21 @@ package=$1
 
 cd $thisdir
 svn up $package
-cd $package
 
 echo "Building package..."
+cd $package
+prebuild_date=`TZ=UTC0 date +'%Y-%m-%d %H:%M:%SZ'`
+sleep 1
 dpkg-buildpackage -rfakeroot
-echo "Publishing..."
+
+echo
+echo "Uploading files..."
 cd ..
-tar -c $package''_* | ssh -A root@khaldun.wikimedia.org "
+tar -v -N"$prebuild_date" -c $package''_* | ssh -A root@khaldun.wikimedia.org "
 	test -e /srv/wikimedia/pool/main/$package || mkdir /srv/wikimedia/pool/main/$package
 	tar -C /srv/wikimedia/pool/main/$package -x && \
+	echo && \
+	echo Updating the repository || \
 	/root/update-repository 2>/dev/null && \
 	echo Success || \
 	echo update-repository failed
