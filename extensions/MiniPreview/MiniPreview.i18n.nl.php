86c9316ad039f3ae24cab19a950f3223f1e8cd49
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-05 23:01:45 +0000

    Updating/adding nl messages for extensions UserMerge and MiniPreview
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25568 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/MiniPreview/MiniPreview.i18n.nl.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages['minipreview-files_in_category'] = 'In totaal bevinden zich $1 bestanden in deze categorie.';
+$messages['minipreview-files_in_gallery'] = 'In totaal bevinden zich $1 bestanden in deze galerij.';
+$messages['minipreview-no_more_files_here'] = 'Er zijn geen bestanden meer in deze richting.';
+$messages['minipreview-ignore_categories'] = '
+Hieronder staat een lijst met (het begin van) categorienamen. Deze categorieën worden niet getoond
+door by MiniPreview. "* PD-" toont bijvoorbeeld niet alle categorieën die beginnen met
+"PD-", zoals "PD-old".
+* GFDL-
+* GFDL
+* CC-
+* PD-
+* Self
+' ;
+$messages['minipreview-no_category_gallery'] = "Geen categorieën of galerijen!";
+
