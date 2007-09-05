42f591150f3bb371eabd97f1a41f0810bd03ce2e
ilabarg1 <ilabarg1@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-05 09:07:52 +0000

    [ApiEditPage.php]-Some comments put by eclipse that produces an error due to a "\n" at the beginning of the class has been cleaned.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/ApiEdit_Vodafone@25532 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/api/ApiEditPage.php
+++ includes/api/ApiEditPage.php
@@ -1,13 +1,4 @@
 <?php
-/*
- * Created on 17/07/2007
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-?>
-
-<?php
 
 /*
  * Created on August 16, 2007
