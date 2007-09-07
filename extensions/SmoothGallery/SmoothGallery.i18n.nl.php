2a9539b9a6decb97cf9a9e6f461140d8a5457880
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-07 07:01:21 +0000

    Fix typo in message in SmoothGallery.i18n.nl.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25609 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SmoothGallery/SmoothGallery.i18n.nl.php
+++ extensions/SmoothGallery/SmoothGallery.i18n.nl.php
@@ -1,6 +1,8 @@
 <?php
 
-$messages['smoothgallery-error'] = "<b>SmoothGallery fout:</b> ";
-$messages['smoothgallery-not-found'] = "Er zijn geen afbeeldingen opgenomen in een galerij. Voeg alstublieft tenminste één afbeelding toe.";
-$messages['smoothgallery-no-images'] = "Er zijn geen afbeeldingen aangetroffen in deze galerij. Zorg ervoor dat alle afbeeldingen bestaan. De volgende afbeeldingen zijn niet aangetroffen: $1";
-$messages['smoothgallery-unexpected-error'] = "Er is een onverwachte fout opgetreden. Dient alstublieft een foutrapport in.";
+$messages = array(
+	'smoothgallery-error' => '<b>SmoothGallery fout:</b> ',
+	'smoothgallery-not-found' => 'Er zijn geen afbeeldingen opgenomen in een galerij. Voeg alstublieft tenminste één afbeelding toe.',
+	'smoothgallery-no-images' => 'Er zijn geen afbeeldingen aangetroffen in deze galerij. Zorg ervoor dat alle afbeeldingen bestaan. De volgende afbeeldingen zijn niet aangetroffen: $1',
+	'smoothgallery-unexpected-error' => 'Er is een onverwachte fout opgetreden. Dien alstublieft een foutrapport in.',
+);
