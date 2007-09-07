ffc66e894865a0255b8de03d48842496b25ed4cf
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-07 06:59:15 +0000

    Fix encoding issue in messages in SelectCategory.i18n.nl.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25608 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SelectCategory/i18n/SelectCategory.i18n.nl.php
+++ extensions/SelectCategory/i18n/SelectCategory.i18n.nl.php
@@ -7,6 +7,7 @@
 # @copyright Â© 2006 by Leon Weber, Manuel Schneider
 # @licence GNU General Public Licence 2.0 or later
 
-# translation done by Siebrand Mazeland <s.mazeland@xs4all.nl>
-$messages['selectcategory-title']	= 'Selecteer categorieÃ«n';
-$messages['selectcategory-subtitle']	= 'Shift-muis om meerdere opeenvolgende categorieÃ«n te selecteren, Ctrl-muis om niet-openvolgende categorieÃ«n te selecteren.';
+$messages = array(
+	'selectcategory-title' => 'Selecteer categorieën',
+	'selectcategory-subtitle' => 'Shift-muis om meerdere opeenvolgende categorieën te selecteren, Ctrl-muis om niet-openvolgende categorieën te selecteren.',
+);
