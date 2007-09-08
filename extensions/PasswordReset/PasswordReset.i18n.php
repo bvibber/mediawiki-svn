ca46499defa6f0a85d04631e01140c85ebd10a1c
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-08 21:30:44 +0000

    * Update to Dutch localisation of ContributionScores and PasswordReset * Fix indentation of PasswordReset.i18n.php * Add support for PasswordReset in extension Translate * Fix export for extension ContributionScores in extension Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@25675 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PasswordReset/PasswordReset.i18n.php
+++ extensions/PasswordReset/PasswordReset.i18n.php
@@ -4,8 +4,8 @@
 */
 
 $allMessages = array(
-        'en' => array( 
-                'passwordreset' => 'Password Reset',
+	'en' => array( 
+		'passwordreset' => 'Password Reset',
 		'passwordreset-invalidusername' => 'Invalid Username',
 		'passwordreset-emptyusername' => 'Empty Username',
 		'passwordreset-nopassmatch' => 'Passwords do not match',
@@ -15,55 +15,22 @@ $allMessages = array(
 		'passwordreset-confirmpass' => 'Confirm Password',
 		'passwordreset-submit' => 'Reset Password',
 		'passwordreset-success' => 'Password has been reset for user_id: $1'
-        ),
-        'de' => array( 
-                'passwordreset' => 'Passwort zurücksetzen',
-        ),
-
-        'nl' => array( 
-                'passwordreset' => 'Wachtwoord opnieuw instellen',
-        ),
-        'yue' => array( 
-                'passwordreset' => '密碼重設',
-		'passwordreset-invalidusername' => '無效嘅用戶名',
-		'passwordreset-emptyusername' => '空白嘅用戶名',
-		'passwordreset-nopassmatch' => '密碼唔對',
-		'passwordreset-badtoken' => '無效嘅編輯幣',
-		'passwordreset-username' => '用戶名',
-		'passwordreset-newpass' => '新密碼',
-		'passwordreset-confirmpass' => '確認新密碼',
-		'passwordreset-submit' => '重設密碼',
-		'passwordreset-success' => 'User_id: $1 嘅密碼已經重設咗'
 	),
-        'zh-hans' => array( 
-                'passwordreset' => '密码重设',
-		'passwordreset-invalidusername' => '无效的用户名',
-		'passwordreset-emptyusername' => '空白的用户名',
-		'passwordreset-nopassmatch' => '密码不匹配',
-		'passwordreset-badtoken' => '无效的编辑币',
-		'passwordreset-username' => '用户名',
-		'passwordreset-newpass' => '新密码',
-		'passwordreset-confirmpass' => '确认新密码',
-		'passwordreset-submit' => '重设密码',
-		'passwordreset-success' => 'User_id: $1 的密码已经重设'
-        ),
-        'zh-hant' => array( 
-                'passwordreset' => '密碼重設',
-		'passwordreset-invalidusername' => '無效的用戶名',
-		'passwordreset-emptyusername' => '空白的用戶名',
-		'passwordreset-nopassmatch' => '密碼不匹配',
-		'passwordreset-badtoken' => '無效的編輯幣',
-		'passwordreset-username' => '用戶名',
-		'passwordreset-newpass' => '新密碼',
-		'passwordreset-confirmpass' => '確認新密碼',
-		'passwordreset-submit' => '重設密碼',
-		'passwordreset-success' => 'User_id: $1 的密碼已經重設'
-        ),
-);
 
-$allMessages['zh'] = $allMessages['zh-hans'];
-$allMessages['zh-cn'] = $allMessages['zh-hans'];
-$allMessages['zh-hk'] = $allMessages['zh-hant'];
-$allMessages['zh-sg'] = $allMessages['zh-hans'];
-$allMessages['zh-tw'] = $allMessages['zh-hant'];
-$allMessages['zh-yue'] = $allMessages['yue'];
+	'de' => array( 
+		'passwordreset' => 'Passwort zurücksetzen',
+	),
+
+	'nl' => array(
+		'passwordreset' => 'Wachtwoord opnieuw instellen',
+		'passwordreset-invalidusername' => 'Onjuiste gebruiker',
+		'passwordreset-emptyusername' => 'Gebruiker niet ingegeven',
+		'passwordreset-nopassmatch' => 'De wachtwoorden komen niet overeen',
+		'passwordreset-badtoken' => 'Ongeldig bewerkingstoken',
+		'passwordreset-username' => 'Gebruiker',
+		'passwordreset-newpass' => 'Nieuw wachtwoord',
+		'passwordreset-confirmpass' => 'Bevestig wachtwoord',
+		'passwordreset-submit' => 'Wachtwoord opnieuw instellen',
+		'passwordreset-success' => 'Wachtwoord voor gebruikers-ID $1 is opnieuw ingesteld',
+	),
+);
