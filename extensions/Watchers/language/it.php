ef382b2367e029d635947c0ec4b55e8453b75d80
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-09-21 15:25:32 +0000

    * Fix encoding
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@26002 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Watchers/language/it.php
+++ extensions/Watchers/language/it.php
@@ -12,9 +12,9 @@ $wgMessageCache->addMessages(
 		'watchers_link_title' => "Chi osserva questa pagina?",
 		'watchers_error_article' => "<b>Errore:</b> la pagina richiesta non esiste.",
 		'watchers_header' => "Utenti che osservano la pagina \"$1\"",
-		'watchers_noone_watches' => "La pagina non è osservata da alcun utente.",
-		'watchers_x_or_more' => "La pagina è osservata da almeno $1 utenti.",
-		'watchers_less_than_x' => "La pagina è osservata da meno di $1 utenti.",
+		'watchers_noone_watches' => "La pagina non Ã¨ osservata da alcun utente.",
+		'watchers_x_or_more' => "La pagina Ã¨ osservata da almeno $1 utenti.",
+		'watchers_less_than_x' => "La pagina Ã¨ osservata da meno di $1 utenti.",
 	)
 );
 
