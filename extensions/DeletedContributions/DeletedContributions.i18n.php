9715dd6a841dcd1e7abd8f649d758373b036ffee
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-10-15 19:07:33 +0000

    Added translations for 27-or-so languages. Submitted by Betawiki's SPQRobin.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@26754 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/DeletedContributions/DeletedContributions.i18n.php
+++ extensions/DeletedContributions/DeletedContributions.i18n.php
@@ -2,28 +2,72 @@
 $wgDeletedContribsMessages = array();
 
 $wgDeletedContribsMessages['en'] = array(
-	'deletedcontributions'		 => 'Deleted user contributions'
+	'deletedcontributions' => 'Deleted user contributions',
+);
+
+$wgDeletedContribsMessages['af'] = array(
+	'deletedcontributions' => 'Geskrapte gebruikersbydraes',
 );
 
 /* Arabic (Meno25) */
 $wgDeletedContribsMessages['ar'] = array(
 	'deletedcontributions' => 'مساهمات المستخدم المحذوفة',
 );
+	
+$wgDeletedContribsMessages['ba'] = array(
+	'deletedcontributions' => 'Ҡулланыусыларҙың юйылған өлөшө',
+);
+
+$wgDeletedContribsMessages['be'] = array(
+	'deletedcontributions' => 'Выдалены ўклад удзельніка',
+);
 
 $wgDeletedContribsMessages['bcl'] = array(
 	'deletedcontributions' => 'Parâon an mga kontribusyon kan parágamit',
+);
+
+$wgDeletedContribsMessages['bg'] = array(
+	'deletedcontributions' => 'Изтрити приноси на потребител',
+);
+
+$wgDeletedContribsMessages['ca'] = array(
+	'deletedcontributions' => 'Contribucions esborrades',
 );
 
 $wgDeletedContribsMessages['cs'] = array(
 	'deletedcontributions' => 'Smazané editace uživatele',
 );
 
+$wgDeletedContribsMessages['cy'] = array(
+	'deletedcontributions'         => 'Cyfraniadau\'r defnyddiwr a ddilëwyd',
+);
+
+$wgDeletedContribsMessages['da'] = array(
+	'deletedcontributions' => 'Slettede brugerbidrag',
+);
+
 $wgDeletedContribsMessages['de'] = array(
-	'deletedcontributions'		 => 'Gelöschte Benutzerbeiträge'
+	'deletedcontributions' => 'Gelöschte Benutzerbeiträge',
+);
+
+$wgDeletedContribsMessages['el'] = array(
+	'deletedcontributions' => 'Διαγραμμένες συνεισφορές χρήστη',
+);
+
+$wgDeletedContribsMessages['eo'] = array(
+	'deletedcontributions' => 'Forigitaj kontribuoj de la uzulo',
 );
 
 $wgDeletedContribsMessages['es'] = array(
 	'deletedcontributions' => 'Contribuciones de usuario borradas',
+);
+
+$wgDeletedContribsMessages['eu'] = array(
+	'deletedcontributions' => 'Ezabatutako ekarpenak',
+);
+
+$wgDeletedContribsMessages['fa'] = array(
+	'deletedcontributions' => 'مشارکت‌های حذف ش',
 );
 
 $wgDeletedContribsMessages['fi'] = array(
@@ -31,11 +75,23 @@ $wgDeletedContribsMessages['fi'] = array(
 );
 
 $wgDeletedContribsMessages['fr'] = array(
-	'deletedcontributions'		 => 'Contributions supprimées d’un utilisateur'
+	'deletedcontributions' => 'Contributions supprimées d’un utilisateur',
+);
+
+$wgDeletedContribsMessages['ga'] = array(
+	'deletedcontributions' => 'Dréachtaí úsáideora scriosta',
 );
 
 $wgDeletedContribsMessages['he'] = array(
 	'deletedcontributions' => 'תרומות משתמש מחוקות',
+);
+
+$wgDeletedContribsMessages['hr'] = array(
+	'deletedcontributions' => 'Obrisani suradnički doprinosi',
+);
+
+$wgDeletedContribsMessages['hu'] = array(
+	'deletedcontributions' => 'Törölt szerkesztések',
 );
 
 $wgDeletedContribsMessages['hsb'] = array(
@@ -43,34 +99,58 @@ $wgDeletedContribsMessages['hsb'] = array(
 );
 
 $wgDeletedContribsMessages['id'] = array(
-	'deletedcontributions'		 => 'Kontribusi yang dihapus'
+	'deletedcontributions' => 'Kontribusi yang dihapus',
+);
+
+$wgDeletedContribsMessages['is'] = array(
+	'deletedcontributions' => 'Eyddar breytingar notenda',
 );
 
 $wgDeletedContribsMessages['it'] = array(
 	'deletedcontributions' => 'Contributi utente cancellati',
 );
 
+$wgDeletedContribsMessages['ja'] = array(
+	'deletedcontributions' => '利用者の削除された投稿履歴',
+);
+
 $wgDeletedContribsMessages['kk-kz'] = array(
-	'deletedcontributions'		 => 'Пайдаланушының жойлған үлестері'
+	'deletedcontributions' => 'Пайдаланушының жойлған үлестері',
 );
 $wgDeletedContribsMessages['kk-tr'] = array(
-	'deletedcontributions'		 => 'Paýdalanwşınıñ joýlğan ülesteri'
+	'deletedcontributions' => 'Paýdalanwşınıñ joýlğan ülesteri',
 );
 $wgDeletedContribsMessages['kk-cn'] = array(
-	'deletedcontributions'		 => 'پايدالانۋشىنىڭ جويلعان ٷلەستەرٸ'
+	'deletedcontributions' => 'پايدالانۋشىنىڭ جويلعان ٷلەستەرٸ',
 );
 $wgDeletedContribsMessages['kk'] = $wgDeletedContribsMessages['kk-kz'];
 
 $wgDeletedContribsMessages['nan'] = array(
-	'deletedcontributions'		 => 'Hō͘ lâng thâi tiāu ê kòng-hiàn'
+	'deletedcontributions' => 'Hō͘ lâng thâi tiāu ê kòng-hiàn',
 );
 
 $wgDeletedContribsMessages['la'] = array(
 	'deletedcontributions' => 'Conlationes usoris deletae',
 );
 
+$wgDeletedContribsMessages['lt'] = array(
+	'deletedcontributions' => 'Ištrintas naudotojo indėlis',
+);
+
+$wgDeletedContribsMessages['mk'] = array(
+	'deletedcontributions' => 'Избришани кориснички придонеси',
+);
+
+$wgDeletedContribsMessages['ms'] = array(
+	'deletedcontributions' => 'Sumbangan pengguna yang telah dipadam',
+);
+
 $wgDeletedContribsMessages['nl'] = array(
 	'deletedcontributions' => 'Verwijderde gebruikersbijdragen',
+);
+
+$wgDeletedContribsMessages['no'] = array(
+	'deletedcontributions' => 'Slettede brukerbidrag',
 );
 
 $wgDeletedContribsMessages['oc'] = array(
@@ -89,28 +169,64 @@ $wgDeletedContribsMessages['pt'] = array(
 	'deletedcontributions' => 'Edições eliminadas',
 );
 
+$wgDeletedContribsMessages['ro'] = array(
+	'deletedcontributions' => 'Contribuţii şterse',
+);
+
 $wgDeletedContribsMessages['ru'] = array(
-	'deletedcontributions'		 => 'Удалённый вклад'
+	'deletedcontributions' => 'Удалённый вклад',
 );
 
 $wgDeletedContribsMessages['sk'] = array(
 	'deletedcontributions' => 'Zmazané príspevky používateľa',
 );
 
+$wgDeletedContribsMessages['sr-ec'] = array(
+	'deletedcontributions' => 'Обрисане измене',
+);
+
+$wgDeletedContribsMessages['sr-el'] = array(
+	'deletedcontributions' => 'Obrisane izmene',
+);
+
 $wgDeletedContribsMessages['sv'] = array(
-	'deletedcontributions'		 => 'Raderade användarbidrag'
+	'deletedcontributions' => 'Raderade användarbidrag',
+);
+
+$wgDeletedContribsMessages['sq'] = array(
+	'deletedcontributions' => 'Kontribute të grisura',
+);
+
+$wgDeletedContribsMessages['te'] = array(
+	'deletedcontributions' => 'తొలగించబడిన సభ్యుని రచనలు',
+);
+
+$wgDeletedContribsMessages['tr'] = array(
+	'deletedcontributions' => 'Silinen kullanıcı katkıları',
+);
+
+$wgDeletedContribsMessages['uk'] = array(
+	'deletedcontributions' => 'Вилучені редагування користувачів',
+);
+
+$wgDeletedContribsMessages['vi'] = array(
+	'deletedcontributions' => 'Đóng góp đã bị xóa của thành viên',
+);
+
+$wgDeletedContribsMessages['vo'] = array(
+	'deletedcontributions' => 'Gebanakeblünots pemoüköl',
 );
 
 $wgDeletedContribsMessages['yue'] = array(
-	'deletedcontributions'		 => '已經刪除咗嘅用戶貢獻'
+	'deletedcontributions' => '已經刪除咗嘅用戶貢獻',
 );
 
 $wgDeletedContribsMessages['zh-hans'] = array(
-	'deletedcontributions'		 => '已删除的用户贡献'
+	'deletedcontributions' => '已删除的用户贡献',
 );
 
 $wgDeletedContribsMessages['zh-hant'] = array(
-	'deletedcontributions'		 => '已刪除的用戶貢獻'
+	'deletedcontributions' => '已刪除的用戶貢獻',
 );
 
 $wgDeletedContribsMessages['zh'] = $wgDeletedContribsMessages['zh-hans'];
