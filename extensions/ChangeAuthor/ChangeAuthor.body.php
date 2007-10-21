9a1b52256dfe3bbe1da0849d27fd2e27da61b216
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-10-21 16:21:34 +0000

    ChangeAuthor: Adding GPL v3 header, updating README
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@26863 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ChangeAuthor/ChangeAuthor.body.php
+++ extensions/ChangeAuthor/ChangeAuthor.body.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Roan Kattouw <roan.kattouw@home.nl>
+ * @copyright Copyright (C) 2007 Roan Kattouw 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ *
+ * An extension that allows changing the author of a revision
+ * Written for the Bokt Wiki <http://www.bokt.nl/wiki/> by Roan Kattouw <roan.kattouw@home.nl>
+ * For information how to install and use this extension, see the README file.
+ *
+ */
 # Alert the user that this is not a valid entry point to MediaWiki if they try to access the extension file directly.
 if (!defined('MEDIAWIKI')) {
 		echo <<<EOT
