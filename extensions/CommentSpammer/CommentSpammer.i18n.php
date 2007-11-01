b10723305589e929906be36dff561806de4a6f22
simetrical <simetrical@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-01 18:01:36 +0000

    Improve the "save blocked" message for CommentSpammer.  Let me try this on my wiki . . .
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27118 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CommentSpammer/CommentSpammer.i18n.php
+++ extensions/CommentSpammer/CommentSpammer.i18n.php
@@ -12,7 +12,7 @@ function efCommentSpammerMessages() {
 
 /* English */
 'en' => array(
-'commentspammer-save-blocked' => 'Your IP address is a suspected comment spammer - save blocked. Create an account to avoid this.',
+'commentspammer-save-blocked' => 'Your IP address is a suspected comment spammer, so the page has not been saved. [[Special:Userlogin|Log in or create an account]] to avoid this.',
 'commentspammer-log-msg'      => 'edit from [[Special:Contributions/$1|$1]] to [[$2]]. ',
 'commentspammer-log-msg-info' => 'Last spammed $1 {{PLURAL:$1|day|days}} ago, threat level is $2, and offence code is $3. [http://www.projecthoneypot.org/search_ip.php?ip=$4 View details], or [[Special:Blockip/$4|block]].',
 'cspammerlogpagetext'         => 'Record of edits that have been allowed or denied based on whether the source was a known comment spammer.',
