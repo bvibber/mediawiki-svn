196eb048c280b4946b572b4e978887f8f5d399d5
evan <evan@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-01 18:22:53 +0000

    Page name localisation
    The localisation of the page name was working on the user's language,
    not the site's content language. This is changed, so that you can put
    links to [[Special:Formulaire/Foo]] and expect them to work for all
    users.
    
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27122 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SpecialForm/SpecialForm.setup.php
+++ extensions/SpecialForm/SpecialForm.setup.php
@@ -43,7 +43,7 @@ $wgExtensionCredits['specialpage'][] = array('name' => 'Form',
 function formLocalizedPageName(&$specialPageArray, $code) {
 	# The localized title of the special page is among the messages of the extension:
 	SpecialForm::loadMessages();
-	$text = wfMsg('form');
+	$text = wfMsgForContent('form');
 
 	# Convert from title in text form to DBKey and put it into the alias array:
 	$title = Title::newFromText($text);
