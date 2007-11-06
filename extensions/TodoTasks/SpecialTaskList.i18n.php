9880dc2558018829d2033d342b0adefe947de3a3
gri6507 <gri6507@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-06 01:37:25 +0000

    v0.9.0 - made the extension internationalized
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27233 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/TodoTasks/SpecialTaskList.i18n.php
@@ -0,0 +1,18 @@
+<?php
+$allMessages = array(
+    'en' => array(
+        'tasklist'                => 'Task List',
+        'tasklistbyproject'       => 'Task List By Project',
+        'tasklistunknownproject'  => 'Unknown project',
+        'tasklistunspecuser'      => 'Unspecified user',
+        'tasklistincorrectuser'   => 'Incorrect username',
+        'tasklistemail'           => 'Dear %s',
+        'tasklistemailsubject'    => '[%s] Task List Change',
+        'tasklistmytasks'         => 'My tasks',
+        'tasklistbyprojectbad'    => "Project '''%s''' is not a valid project. For a list of valid projects, see [[MediaWiki:TodoTasksValidProjects]].",
+        'tasklistbyprojname'      => "Assigned Tasks for '''%s'''",
+        'tasklistchooseproj'      => 'Select Project: ',
+        'tasklistprojdisp'        => 'Display',
+        'tasklistbyname'          => '== Todo List for %s ==',
+     )
+);
