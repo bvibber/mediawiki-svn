3d076790d60f491b02a40a0e2255bd059399a8ad
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-22 11:44:38 +0000

    Add i18n file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27749 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AssertEdit/AssertEdit.i18n.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Internationalisation file for AssertEdit extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'assert_edit_title' => 'Assert failed',
+	'assert_edit_message' => 'The specified assertion ($1) failed.',
+);
