711a8abd7210ca39a9c96c029c313887fb4ca88f
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-23 14:05:52 +0000

    * adding i18n file * remove trailing whitespace
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27782 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Blahtex/Blahtex.php
+++ extensions/Blahtex/Blahtex.php
@@ -9,7 +9,7 @@
  * @copyright 2005, 2006 David Harvey and Jitse Niesen
  * @licence GNU General Public Licence
  */
- 
+
 if( !defined( 'MEDIAWIKI' ) ) {
 	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
 	die( 1 );
@@ -25,85 +25,25 @@ $wgBlahtexOptions = '--texvc-compatible-commands --mathml-version-1-fonts --disa
 /* Register the extension */
 
 $wgExtensionFunctions[] = 'efBlahtex';
-$wgExtensionCredits['other'][] = array( 'name' => 'Blahtex', 
-					'author' => 'David Harvey and Jitse Niesen', 
-					'url' => 'http://www.mediawiki.org/wiki/Extension:Blahtex',
-					'description' => 'MathML output for &lt;math&gt; tags' );
+$wgExtensionCredits['other'][] = array(
+	'name' => 'Blahtex',
+	'author' => 'David Harvey and Jitse Niesen',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Blahtex',
+	'description' => 'MathML output for &lt;math&gt; tags'
+);
+
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['Blahtex'] = $dir . 'Blahtex.i18n.php';
 
 /**
- * Initialize Blahtex 
- */	
+ * Initialize Blahtex
+ */
 function efBlahtex() {
-	global $wgHooks, $wgMessageCache;
+	global $wgHooks;
 	$wgHooks['MathAfterTexvc'][] = 'efBlahtexMathAfterTexvc';
 	$wgHooks['ParserBeforeTidy'][] = 'efBlahtexParserBeforeTidy';
 	$wgHooks['ParserAfterTidy'][] = 'efBlahtexParserAfterTidy';
-	$wgMessageCache->addMessages(array(
-                'math_noblahtex'                        => 'Can\'t execute blahtex, which should be at $1',
-                'math_AmbiguousInfix'                   => 'Ambiguous placement of "$1" ' .
-                                                           '(try using additional braces "{ ... }" to disambiguate)',
-                'math_CannotChangeDirectory'            => 'Cannot change working directory',
-                'math_CannotCreateTexFile'              => 'Cannot create tex file',
-                'math_CannotRunDvipng'                  => 'Cannot run dvipng',
-                'math_CannotRunLatex'                   => 'Cannot run latex',
-                'math_CannotWritePngDirectory'          => ' Cannot write to output PNG directory',
-                'math_CannotWriteTexFile'               => 'Cannot write to tex file',
-                'math_CasesRowTooBig'                   => 'There can only be two entries in each row of a "cases" block',
-                'math_DoubleSubscript'                  => 'Encountered two subscripts attached to the same base ' .
-                                                           '(only one is allowed)',
-                'math_DoubleSuperscript'                => 'Encountered two superscripts attached to the same base ' .
-                                                           '(only one is allowed)',
-                'math_IllegalCharacter'                 => 'Illegal character in input',
-                'math_IllegalCommandInMathMode'         => 'The command "$1" is illegal in math mode',
-                'math_IllegalCommandInMathModeWithHint' => 'The command "$1" is illegal in math mode ' .
-                                                           '(perhaps you intended to use "$2" instead?)',
-                'math_IllegalCommandInTextMode'         => 'The command "$1" is illegal in text mode',
-                'math_IllegalCommandInTextModeWithHint' => 'The command "$1" is illegal in text mode ' .
-                                                           '(perhaps you intended to use "$2" instead?)',
-                'math_IllegalDelimiter'                 => 'Illegal delimiter following "$1"',
-                'math_IllegalFinalBackslash'            => 'Illegal backslash "\\" at end of input',
-                'math_IllegalNestedFontEncodings'       => 'Font encoding commands may not be nested',
-                'math_IllegalRedefinition'              => 'The command "$1" has already been defined; you cannot redefine it',
-                'math_InvalidColour'                    => 'The colour "$1" is invalid',
-                'math_InvalidUtf8Input'                 => 'The input string was not valid UTF-8',
-                'math_LatexFontNotSpecified'            => 'No LaTeX font has been specified for "$1"',
-                'math_LatexPackageUnavailable'          => 'Unable to render PNG because the LaTeX package "$1" is unavailable',
-                'math_MismatchedBeginAndEnd'            => 'Commands "$1" and "$2" do not match',
-                'math_MisplacedLimits'                  => 'The command "$1" can only appear after a math operator ' .
-                                                           '(consider using "\\mathop")',
-                'math_MissingCommandAfterNewcommand'    => 'Missing or illegal new command name after "\\newcommand" ' .
-                                                           '(there must be precisely one command defined; it must begin ' .
-                                                           'with a backslash "\\" and contain only alphabetic characters)',
-                'math_MissingDelimiter'                 => 'Missing delimiter after "$1"',
-                'math_MissingOpenBraceAfter'            => 'Missing open brace "{" after "$1"',
-                'math_MissingOpenBraceAtEnd'            => 'Missing open brace "{" at end of input',
-                'math_MissingOpenBraceBefore'           => 'Missing open brace "{" before "$1"',
-                'math_MissingOrIllegalParameterCount'   => 'Missing or illegal parameter count in definition of "$1" ' .
-                                                           '(must be a single digit between 1 and 9 inclusive)',
-                'math_MissingOrIllegalParameterIndex'   => 'Missing or illegal parameter index in definition of "$1"',
-                'math_NonAsciiInMathMode'               => 'Non-ASCII characters may only be used in text mode ' .
-                                                           '(try enclosing the problem characters in "\\text{...}")',
-                'math_NotEnoughArguments'               => 'Not enough arguments were supplied for "$1"',
-                'math_PngIncompatibleCharacter'         => 'Unable to correctly generate PNG containing the character $1',
-                'math_ReservedCommand'                  => 'The command "$1" is reserved for internal use by blahtex',
-                'math_SubstackRowTooBig'                => 'There can only be one entry in each row of a "substack" block',
-                'math_TooManyMathmlNodes'               => 'There are too many nodes in the MathML tree',
-                'math_TooManyTokens'                    => 'The input is too long',
-                'math_UnavailableSymbolFontCombination' => 'The symbol "$1" is not available in the font "$2"',
-                'math_UnexpectedNextCell'               => 'The command "&" may only appear inside a "\\begin ... \\end" block',
-                'math_UnexpectedNextRow'                => 'The command "\\\\" may only appear inside a "\\begin ... \\end" block',
-                'math_UnmatchedBegin'                   => 'Encountered "\\begin" without matching "\\end"',
-                'math_UnmatchedCloseBrace'              => 'Encountered close brace "}" without matching open brace "{"',
-                'math_UnmatchedEnd'                     => 'Encountered "\\end" without matching "\\begin"',
-                'math_UnmatchedLeft'                    => 'Encountered "\\left" without matching "\\right"',
-                'math_UnmatchedOpenBrace'               => 'Encountered open brace "{" without matching close brace "}"',
-                'math_UnmatchedOpenBracket'             => 'Encountered open bracket "[" without matching close bracket "]"',
-                'math_UnmatchedRight'                   => 'Encountered "\\right" without matching "\\left"',
-                'math_UnrecognisedCommand'              => 'Unrecognised command "$1"',
-                'math_WrongFontEncoding'                => 'The symbol "$1" may not appear in font encoding "$2"',
-                'math_WrongFontEncodingWithHint'        => 'The symbol "$1" may not appear in font encoding "$2" ' .
-		                                           '(try using the "$3{...}" command)', )
-	);
+	wfLoadExtensionMessages( 'Blahtex' );
 }
 
 /**
@@ -118,7 +58,7 @@ function efBlahtexMathAfterTexvc( &$mathRenderer, &$errmsg ) {
 	 $errmsg = $br->getErrmsg();
 	 wfDebug('Blahtex MathML: ' . $br->mr->mathml . "\n");
 	 wfDebug('Blahtex errmsg: ' . $errmsg . "\n");
-	 
+
 	 return true;
 }
 
@@ -154,7 +94,7 @@ function efBlahtexParserBeforeTidy( &$parser, &$text ) {
 	}
 	$parser->blahtexMathtags = $mathtags;
 	$text = $stripped . substr( $text, $pos );
-	
+
 	return true;
 }
 
@@ -166,12 +106,12 @@ function efBlahtexParserBeforeTidy( &$parser, &$text ) {
 function efBlahtexParserAfterTidy( &$parser, &$text ) {
 	global $wgBlahtexMathContent, $wgBlahtexMathTags;
 	$text = strtr( $text, $parser->blahtexMathtags );
-	
+
 	return true;
 }
 
 class BlahtexRenderer {
-	 
+
 	/** @privatesection */
 	var $mode = MW_MATH_MODERN; /**< @User preference for maths */
 	var $tex = '';              /**< LaTeX fragment */
@@ -192,7 +132,7 @@ class BlahtexRenderer {
 		 $this->html = $mathRenderer->html; */
 		 /* mathml skipped; do not use the MathML generated by texvc */
 		 /* $this->conservativeness = $mathRenderer->conservativeness; */
-		 $this->errmsg = $errmsg; 
+		 $this->errmsg = $errmsg;
 	}
 
 	function getErrmsg() {
@@ -221,8 +161,8 @@ class BlahtexRenderer {
 	 * options may be specified in $wgBlahtexOptions.
 	 * @param $tex LaTeX fragment to be rendered (string)
 	 * @param $makePNG Whether blahtex should generate both MathML and
-	 *    PNG (@c true) or only MathML (@c false). 
-	 * @return A 2-tuple. 
+	 *    PNG (@c true) or only MathML (@c false).
+	 * @return A 2-tuple.
 	 *  - If an error occurred, then the first element is @c false and
 	 *    the second element is a string containing an HTML fragment
 	 *    with the error message.
@@ -236,7 +176,7 @@ class BlahtexRenderer {
 		$descriptorspec = array( 0 => array( "pipe", "r" ),
 					 1 => array( "pipe", "w" ) );
 		$options = '--mathml ' . $wgBlahtexOptions;
-		if ( $makePNG ) 
+		if ( $makePNG )
 			$options .= " --png --temp-directory $wgTmpDirectory --png-directory $wgTmpDirectory";
 
 		if ( function_exists( 'is_executable' ) && !is_executable( $wgBlahtex ) )
@@ -251,7 +191,7 @@ class BlahtexRenderer {
 		fwrite( $pipes[0], '\\displaystyle ' );
 		fwrite( $pipes[0], $tex );
 		fclose( $pipes[0] );
-		
+
 		$contents = '';
 		while ( !feof($pipes[1] ) ) {
 			$contents .= fgets( $pipes[1], 4096 );
@@ -262,7 +202,7 @@ class BlahtexRenderer {
 			// exit code of blahtex is not zero; this shouldn't happen
 			return array( false, $this->error( 'math_unknown_error' ) );
 		}
-		
+
 		return array( true, $contents );
 	}
 
@@ -270,7 +210,7 @@ class BlahtexRenderer {
 	 * Process blahtex output.
 	 * Parse the output and fill the mathml field in the database. If
 	 * blahtex has also generated a PNG image, then update the hash
-	 * field as well move the PNG image to its final destination. 
+	 * field as well move the PNG image to its final destination.
 	 * @param $contents Blahtex output (string)
 	 * @return HTML fragment with error message if an error
 	 *    occurred, @c false otherwise (string or boolean)
@@ -287,11 +227,11 @@ class BlahtexRenderer {
 
 		} elseif ( isset( $results["blahtex:png:error:id"] ) ) {
 			// There was an error while generating the PNG
-			return $this->blahtexError( $results, "blahtex:png:error" ); 
+			return $this->blahtexError( $results, "blahtex:png:error" );
 
 		} elseif (isset($results["mathmlMarkup"]) || isset($results["blahtex:png:md5"])) {
 			// We got some results
-			if ( isset( $results["mathmlMarkup"] ) )	 
+			if ( isset( $results["mathmlMarkup"] ) )
 				$this->mr->mathml = $results['mathmlMarkup'];
 			if ( isset( $results["blahtex:png:md5"] ) ) {
 				$this->mr->hash = $results["blahtex:png:md5"];
@@ -358,7 +298,7 @@ class BlahtexRenderer {
 		wfDebug("Blahtex _error(): msg = $msg, arg1 = $arg1\n");
 		$mf = htmlspecialchars( wfMsg( 'math_failure' ) );
 		if ( $msg ) {
-			if ( $fallback && wfMsg( $msg ) == '&lt;' . htmlspecialchars( $msg ) . '&gt;' ) 
+			if ( $fallback && wfMsg( $msg ) == '&lt;' . htmlspecialchars( $msg ) . '&gt;' )
 				$errmsg = htmlspecialchars( $fallback );
 			else
 				$errmsg = htmlspecialchars( wfMsg( $msg, $arg1, $arg2, $arg3 ) );
@@ -375,7 +315,7 @@ class BlahtexRenderer {
 
 
 /**
- * %Parser for the blahtex's output. 
+ * %Parser for the blahtex's output.
  */
 class blahtexOutputParser  {
    var $parser;  /**< \private */
@@ -388,7 +328,7 @@ class blahtexOutputParser  {
 		$this->stack = array();
 		$this->results = array();
 		$this->prevCdata = false;
-		
+
 		xml_set_object( $this->parser, $this );
 		xml_parser_set_option( $this->parser, XML_OPTION_CASE_FOLDING, 0 );
 		xml_set_element_handler( $this->parser, "startElement", "stopElement" );
@@ -411,12 +351,12 @@ class blahtexOutputParser  {
 	 */
 	function parse( $data )
 	{
-		// We splice out any segment between <markup> and </markup>  
+		// We splice out any segment between <markup> and </markup>
 		// so that the XML parser doesn't have to deal with all the MathML tags.
 		$markupBegin = strpos( $data, "<markup>" );
 		if ( !( $markupBegin === false ) ) {
 			$markupEnd = strpos( $data, "</markup>" );
-			$this->results["mathmlMarkup"] = 
+			$this->results["mathmlMarkup"] =
 				trim( substr( $data, $markupBegin + 8, $markupEnd - $markupBegin - 8 ) );
 			$data = substr( $data, 0, $markupBegin + 8 ) . substr( $data, $markupEnd );
 		}
@@ -433,25 +373,25 @@ class blahtexOutputParser  {
 		else
 			array_push( $this->stack, $this->stack[count( $this->stack ) - 1] . ":$name" );
 	}
-	
+
 	function stopElement($parser, $name)
 	{
 		$this->prevCdata = false;
 		array_pop( $this->stack );
 	}
-	
+
 	function characterData($parser, $data)
 	{
 		$index = $this->stack[count( $this->stack ) - 1];
 		if ( $this->prevCdata ) {
 			// Merge subsequent CDATA blocks
 			if ( is_array( $this->results[$index] ) )
-				array_push( $this->results[$index], 
+				array_push( $this->results[$index],
 					    array_pop( $this->results[$index] ) . $data);
 			else
 				$this->results[$index] .= $data;
 		} else {
-			if ( !isset( $this->results[$index] ) ) 
+			if ( !isset( $this->results[$index] ) )
 				$this->results[$index] = $data;
 			elseif ( is_array( $this->results[$index] ) )
 				array_push( $this->results[$index], $data );
