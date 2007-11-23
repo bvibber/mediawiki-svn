9ea5979583f2de46ba28506bc0770fb788484850
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-23 10:26:44 +0000

    * put messages in i18n file * add 2 messages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27772 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Postcomment/SpecialPostcomment.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation file for extension Post Comment.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'postcomment_discussionprotected' => 'This discussion page has been protected from further postings.', 
+	'postcomment_notloggedin' => 'You are not logged in.',
+	'postcomment_youareloggedinas' => 'You are logged in as $1',
+	'postcomment_addcommentdiscussionpage' => 'Add your comment on this discussion page',
+	'postcomment_leavemessagefor' => 'Leave a message for $1',
+	'postcomment_post' => 'Post',
+	'postcomment_on' => 'On',
+	'postcomment_said' => 'said',
+	'postcomment_invalidrequest' => 'Invalid request.',
+	'postcomment_replyto' => 'Reply to $1',
+	'postcomment_nopostingtoadd' => 'No posting to add.',
+);
