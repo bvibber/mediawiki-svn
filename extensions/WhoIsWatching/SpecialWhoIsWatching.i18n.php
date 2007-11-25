791c73e307663910ed30feec4fa17ac4aebec2e6
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-25 18:38:14 +0000

    Add localisation for ar, hsb, nl
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27840 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WhoIsWatching/SpecialWhoIsWatching.i18n.php
+++ extensions/WhoIsWatching/SpecialWhoIsWatching.i18n.php
@@ -1,9 +1,26 @@
 <?php
 
 $allMessages = array(
-    'en' => array(
-        'whoiswatching'               => 'Who Is Watching a wiki page',
-        'specialwhoiswatchingthepage' => 'Who is watching %s',
-        'specialwhoiswatchingusage'   => 'This special page cannot be used on its own. Please use the page [[MediaWiki:Number_of_watching_users_pageview]] to define an entry point to this special page.',
-    ),
+	'en' => array(
+		'whoiswatching'               => 'Who Is Watching a wiki page',
+		'specialwhoiswatchingthepage' => 'Who is watching %s',
+		'specialwhoiswatchingusage'   => 'This special page cannot be used on its own. Please use the page [[MediaWiki:Number_of_watching_users_pageview]] to define an entry point to this special page.',
+	),
+
+	'ar' = array(
+		'specialwhoiswatching'        => 'من يراقب صفحة ويكي',
+		'specialwhoiswatchingthepage' => 'من يراقب %s',
+		'specialwhoiswatchingusage'   => 'هذه الصفحة الخاصة لا يمكن استخدامها بمفردها. من فضلك استخدم الصفحة [[MediaWiki:Number_of_watching_users_pageview]] لتعريف نقطة مدخل إلى هذه الصفحة الخاصة.',
+	),
+
+	'hsb' = array(
+		'specialwhoiswatching'        => 'Štó wobkedźbuje wiki-stronu',
+		'specialwhoiswatchingthepage' => 'Štó wobkedźbuje %s',
+		'specialwhoiswatchingusage'   => 'Tuta specialna strona njeda so na so wužiwać. Prošu wužij stronu [[MediaWiki:Number_of_watching_users_pageview]], zo by přistupny dypk za tutu specialnu stronu definował.',
+	),
+
+	'nl' = array(
+		'specialwhoiswatching'        => 'Wie volgt een pagina',
+		'specialwhoiswatchingthepage' => 'Wie volgt %s',
+	),
 );
