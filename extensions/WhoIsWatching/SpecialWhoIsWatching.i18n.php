5d02a15aae13ba55989478402889635fedb41114
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-11-25 18:49:15 +0000

    Fix syntax errors ("=" -> "=>" (4x))
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@27842 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WhoIsWatching/SpecialWhoIsWatching.i18n.php
+++ extensions/WhoIsWatching/SpecialWhoIsWatching.i18n.php
@@ -7,19 +7,19 @@ $allMessages = array(
 		'specialwhoiswatchingusage'   => 'This special page cannot be used on its own. Please use the page [[MediaWiki:Number_of_watching_users_pageview]] to define an entry point to this special page.',
 	),
 
-	'ar' = array(
+	'ar' => array(
 		'specialwhoiswatching'        => 'من يراقب صفحة ويكي',
 		'specialwhoiswatchingthepage' => 'من يراقب %s',
 		'specialwhoiswatchingusage'   => 'هذه الصفحة الخاصة لا يمكن استخدامها بمفردها. من فضلك استخدم الصفحة [[MediaWiki:Number_of_watching_users_pageview]] لتعريف نقطة مدخل إلى هذه الصفحة الخاصة.',
 	),
 
-	'hsb' = array(
+	'hsb' => array(
 		'specialwhoiswatching'        => 'Štó wobkedźbuje wiki-stronu',
 		'specialwhoiswatchingthepage' => 'Štó wobkedźbuje %s',
 		'specialwhoiswatchingusage'   => 'Tuta specialna strona njeda so na so wužiwać. Prošu wužij stronu [[MediaWiki:Number_of_watching_users_pageview]], zo by přistupny dypk za tutu specialnu stronu definował.',
 	),
 
-	'nl' = array(
+	'nl' => array(
 		'specialwhoiswatching'        => 'Wie volgt een pagina',
 		'specialwhoiswatchingthepage' => 'Wie volgt %s',
 	),
