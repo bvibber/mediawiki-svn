9e65c773a1ed1cb78eb01b3321a21eed1da16793
gri6507 <gri6507@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-03 13:30:52 +0000

    v0.5 - numerous style improvements
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28103 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Tooltip/Tooltip.php
+++ extensions/Tooltip/Tooltip.php
@@ -6,11 +6,82 @@ $wgExtensionCredits['parserhook'][] = array(
     'name'        => 'ToolTip',
     'author'      => 'Paul Grinberg',
     'description' => 'adds <nowiki><tooltip></nowiki> and <nowiki>{{#tooltip:}}</nowiki>tag',
-    'version'     => '0.4.1'
+    'version'     => '0.5'
 );
 
 function wfToolTipExtension() {
     global $wgParser;
+    global $wgOut;
+    global $wgScriptPath;
+    
+    $output .= <<< END
+<style type="text/css">
+    .xstooltip{
+        visibility: hidden;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+        font: normal 8pt sans-serif;
+        color: white;
+        padding: 3px;
+        border: solid 1px;
+        background-repeat: repeat;
+        background-image: url($wgScriptPath/images/ttbg.png);
+    }
+    .xstooltip_body{
+        color: green;
+    }
+</style>
+<script type= "text/javascript">
+    function xstooltip_findPosX(obj) {
+        var curleft = 0;
+        if (obj.offsetParent) {
+            while (obj.offsetParent){
+                curleft += obj.offsetLeft;
+                obj = obj.offsetParent;
+            }
+        } else if (obj.x)
+            curleft += obj.x;
+        return curleft - 200;
+    }
+    function xstooltip_findPosY(obj) {
+        var curtop = 0;
+        if (obj.offsetParent) {
+            while (obj.offsetParent) {
+                curtop += obj.offsetTop;
+                obj = obj.offsetParent;
+            }
+        } else if (obj.y)
+            curtop+= obj.y;
+        return curtop - 25
+    }
+    function xstooltip_show(tooltipId, parentId, posX, posY) {
+        it = document.getElementById(tooltipId);
+        if (it.style.top == '' || it.style.top == 0) {
+            if (it.style.left == '' || it.style.left == 0) {
+                it.style.width = it.offsetWidth + 'px';
+                it.style.height = it.offsetHeight + 'px';
+                img = document.getElementById(parentId);
+                x = xstooltip_findPosX(img) + posX;
+                y = xstooltip_findPosY(img) + posY;
+                if (x < 0 )
+                    x = 0;
+                if (x + it.offsetWidth > img.offsetParent.offsetWidth )
+                    x = img.offsetParent.offsetWidth - it.offsetWidth - 1;
+                it.style.top = y + 'px';
+                it.style.left = x + 'px';
+            }
+        }
+        it.style.visibility = 'visible';
+    }
+    function xstooltip_hide(id) {
+        it = document.getElementById(id);
+        it.style.visibility = 'hidden';
+    }
+</script>
+END;
+    $wgOut->addScript($output);
     $wgParser->setHook( "tooltip", "renderToolTip" );
     $wgParser->setFunctionHook( 'tooltip', 'wfTooltipParserFunction_Render' );
 }
@@ -24,14 +95,12 @@ function wfTooltipParserFunction_Magic( &$magicWords, $langCode ) {
         return true;
 }
 
-function wfTooltipParserFunction_Render( &$parser, $basetext = '', $tooltiptext = '', $x = 0, $y = 0) {
+function wfTooltipParserFunction_Render( $parser, $basetext = '', $tooltiptext = '', $x = 0, $y = 0) {
     $output = renderToolTip($tooltiptext, array('text'=>$basetext, 'x'=>$x, 'y'=>$y), &$parser);
     return array($output, 'nowiki' => false, 'noparse' => true, 'isHTML' => false);
 }
 
 function renderToolTip($input, $argv, &$parser) {
-    global $wgToolTip,$wgScriptPath;
-
     $text = 'see tooltip';
     $xoffset = 0;
     $yoffset = 0;
@@ -52,23 +121,14 @@ function renderToolTip($input, $argv, &$parser) {
         }
     }
 
-    $output = '<span>';
-
-    if ($wgToolTip != 1) {
-        // This code was borrowed from http://www.texsoft.it/index.php?%20m=sw.js.htmltooltip&c=software&l=it
-        $output .= <<< END
-<style type="text/css">.xstooltip{visibility: hidden;position: absolute;top: 0;left: 0;z-index: 2;font: normal 8pt sans-serif;padding: 3px;border: solid 1px;background-repeat: repeat;background-image: url($wgScriptPath/images/ttbg.png);}</style><script type= "text/javascript">function xstooltip_findPosX(obj){var curleft = 0;if (obj.offsetParent){while (obj.offsetParent){curleft += obj.offsetLeft;obj = obj.offsetParent;}}else if (obj.x)curleft += obj.x;return curleft - 200;}function xstooltip_findPosY(obj){var curtop = 0;if (obj.offsetParent){while (obj.offsetParent){curtop += obj.offsetTop;obj = obj.offsetParent;}}else if (obj.y)curtop+= obj.y;return curtop - 25}function xstooltip_show(tooltipId, parentId, posX, posY){it = document.getElementById(tooltipId);if (it.style.top == '' || it.style.top == 0){if (it.style.left == '' || it.style.left == 0){it.style.width = it.offsetWidth + 'px';it.style.height = it.offsetHeight + 'px';img = document.getElementById(parentId);x = xstooltip_findPosX(img) + posX;y = xstooltip_findPosY(img) + posY;if (x < 0 ) x = 0;if (x + it.offsetWidth > img.offsetParent.offsetWidth ) x = img.offsetParent.offsetWidth - it.offsetWidth - 1;it.style.top = y + 'px';it.style.left = x + 'px';}}it.style.visibility = 'visible';}function xstooltip_hide(id){it = document.getElementById(id);it.style.visibility = 'hidden';}</script>
-END;
-        $wgToolTip = 1;
-    }
+    $output = '';
 
     if ($input != '') {
         $tooltipid = uniqid('tooltipid');
         $parentid = uniqid('parentid');
-        $output .= "<div id='$tooltipid' class='xstooltip'><font color=white>" . $parser->unstrip($parser->recursiveTagParse($input),$parser->mStripState) . "</font></div>";
-        $output .= "<font color='green'><span id='$parentid' onmouseover=\"xstooltip_show('$tooltipid', '$parentid', $xoffset, $yoffset);\" onmouseout=\"xstooltip_hide('$tooltipid');\">" . $parser->unstrip($parser->recursiveTagParse($text),$parser->mStripState) . "</span></font>";
+        $output .= "<span id='$tooltipid' class='xstooltip'>" . $parser->unstrip($parser->recursiveTagParse($input),$parser->mStripState) . "</span>";
+        $output .= "<span id='$parentid' class='xstooltip_body' onmouseover=\"xstooltip_show('$tooltipid', '$parentid', $xoffset, $yoffset);\" onmouseout=\"xstooltip_hide('$tooltipid');\">" . $parser->unstrip($parser->recursiveTagParse($text),$parser->mStripState) . "</span>";
     }
-    $output .= "</span>";
 
     return $output;
 }
