765375c5e7541521d50f865cebe3830662c69762
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-03 15:09:41 +0000

    * Adding i18n file (submitted by Bertrand Grondin) * EOL whitespace and indentation fixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28105 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UserContactLinks/UserSignature.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation file for extension UserContactLinks.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'usercontactlink-baduser' => 'incorrect username',
+);
+
+#Translation in french language by Bertrand GRONDIN
+$messages ['fr'] = array(
+	'usercontactlink-baduser' => 'nom d’utilisateur incorrect',
+);
--- extensions/UserContactLinks/UserSignature.php
+++ extensions/UserContactLinks/UserSignature.php
@@ -19,71 +19,76 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 global $wgHooks, $wgOut;
 $wgHooks['ParserAfterStrip'][]      = 'parseUserSignatures';
 $wgExtensionCredits['parserhook'][] = array(
-        'version'     => '0.3',
-        'name'        => 'UserSignature',
-        'author'      => 'Paul Grinberg',
+	'version'     => '0.4',
+	'name'        => 'UserSignature',
+	'author'      => 'Paul Grinberg',
 	'email'       => 'gri6507 at yahoo dot com',
 	'url'         => 'http://www.mediawiki.org/wiki/Extension:User_Contact_Links',
-        'description' => 'provides the ability to simply and consistantly add other user names using ^^^user^^^ syntax');
+	'description' => 'provides the ability to simply and consistantly add other user names using ^^^user^^^ syntax',
+);
+
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['UserContactLinks'] = $dir . 'UserSignature.i18n.php';
 
 function parseUserSignatures(&$parser, &$text, &$strip_state) {
-    while (preg_match('/\^\^\^(.+?)\^\^\^/', $text, $matches)) {
-        $userid = getUserIDFromUserText($matches[1]);
-        if ($userid != 0) {                         // successfully found the user based on first word
-            $u = newFromId($userid);
-            $text = str_replace("^^^$matches[1]^^^", "[[:user:" . $u->getName() . "|" . $u->getRealName() . "]]", $text);
-        } else {
-            $text = str_replace("^^^$matches[1]^^^", "'''{incorrect username}'''", $text);
-        }
-    }
+	wfLoadExtensionMessages( 'UserContactLinks' );
+	while (preg_match('/\^\^\^(.+?)\^\^\^/', $text, $matches)) {
+		$userid = getUserIDFromUserText($matches[1]);
+			if ($userid != 0) { // successfully found the user based on first word
+				$u = newFromId($userid);
+				$text = str_replace("^^^$matches[1]^^^", "[[:user:" . $u->getName() . "|" . $u->getRealName() . "]]", $text);
+			} else {
+				$text = str_replace("^^^$matches[1]^^^", "'''{". wfMsg('usercontactlink-baduser') ."}'''", $text);
+			}
+		}
 
-    return true;
+	return true;
 }
 
 function newFromId( $id ) {
-    $u = new User;
-    $u->mId = $id;
-    $u->mFrom = 'id';
-    return $u;
+	$u = new User;
+	$u->mId = $id;
+	$u->mFrom = 'id';
+	return $u;
 }
 
 # only create the following function if it was not already installed with the Todo Tasks extension
 if (!function_exists('getUserIDFromUserText')) {
-    function getUserIDFromUserText($user) {
-        $dbr = wfGetDB( DB_SLAVE );
-        $userid = 0;
+	function getUserIDFromUserText($user) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$userid = 0;
 
-        if (preg_match('/^\s*(.*?)\s*$/', $user, $matches))
-            $user = $matches[1];
+		if (preg_match('/^\s*(.*?)\s*$/', $user, $matches))
+		$user = $matches[1];
 
-        $u = User::newFromName($user);
-        if ($u) {
-            $userid = $u->idForName();               // valid userName
-        }
-        if (!$userid) {                              // if not a valid userName, try as a userRealName
-            $userid = $dbr->selectField( 'user', 'user_id', array( 'user_real_name' => $user ), 'renderTodo' );
-            if (!$userid) {                          // if not valid userRealName, try case insensitive userRealName
-                $sql = "SELECT user_id FROM ". $dbr->tableName('user') ." WHERE UPPER(user_real_name) LIKE '%" . strtoupper($user) . "%'";
-                $res = $dbr->query( $sql, __METHOD__ );
-                if ($dbr->numRows($res)) {
-                    $row = $dbr->fetchRow($res);
-                    $userid = $row[0];
-                }
-                $dbr->freeResult($res);
-                if (!$userid) {                      // if not case insensitive userRealName, try case insensitive lastname
-                    list ($first, $last) = preg_split('/\s+/', $user);
-                    if ($last != '') {
-                        $sql = "SELECT user_id FROM ". $dbr->tableName('user') ." WHERE UPPER(user_real_name) LIKE '%" . strtoupper($last) . "%'";
-                        $res = $dbr->query( $sql, __METHOD__ );
-                        if ($dbr->numRows($res)) {
-                            $row = $dbr->fetchRow($res);
-                            $userid = $row[0];
-                        }
-                        $dbr->freeResult($res);
-                    }
-                }
-            }
-        }
-        return $userid;
-    }
+		$u = User::newFromName($user);
+		if ($u) {
+			$userid = $u->idForName(); // valid userName
+		}
+		if (!$userid) { // if not a valid userName, try as a userRealName
+			$userid = $dbr->selectField( 'user', 'user_id', array( 'user_real_name' => $user ), 'renderTodo' );
+			if (!$userid) { // if not valid userRealName, try case insensitive userRealName
+				$sql = "SELECT user_id FROM ". $dbr->tableName('user') ." WHERE UPPER(user_real_name) LIKE '%" . strtoupper($user) . "%'";
+				$res = $dbr->query( $sql, __METHOD__ );
+				if ($dbr->numRows($res)) {
+					$row = $dbr->fetchRow($res);
+					$userid = $row[0];
+				}
+				$dbr->freeResult($res);
+				if (!$userid) { // if not case insensitive userRealName, try case insensitive lastname
+					list ($first, $last) = preg_split('/\s+/', $user);
+					if ($last != '') {
+						$sql = "SELECT user_id FROM ". $dbr->tableName('user') ." WHERE UPPER(user_real_name) LIKE '%" . strtoupper($last) . "%'";
+						$res = $dbr->query( $sql, __METHOD__ );
+						if ($dbr->numRows($res)) {
+							$row = $dbr->fetchRow($res);
+							$userid = $row[0];
+						}
+						$dbr->freeResult($res);
+					}
+				}
+			}
+		}
+		return $userid;
+	}
 }
