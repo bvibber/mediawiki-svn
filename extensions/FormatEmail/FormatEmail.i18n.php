4bc6c0eecf2b5a90114322487edc57bd008e48f3
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-03 13:05:41 +0000

    * Adding i18n file (submitted by Bertrand Grondin) * EOL whitespace and indentation fixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28100 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FormatEmail/FormatEmail.i18n.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Internationalisation file for extension FormatEmail.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'email_header' => '',
+	'email_footer' => '
+
+----------------------------------------------------------------------------
+This email has been sent to you through the {{SITENAME}} email system by $1.
+
+$2',
+);
+
+$messages ['fr'] = array (
+	'email_footer' => '
+
+----------------------------------------------------------------------------
+Ce courrier a vous a été envoyé grâce au systeme de messagerie de {{SITENAME}} par $1.
+
+$2',
+);
--- extensions/FormatEmail/FormatEmail.php
+++ extensions/FormatEmail/FormatEmail.php
@@ -1,14 +1,13 @@
 <?php
 if ( ! defined( 'MEDIAWIKI' ) )
 	die();
-    
+
 /**#@+
- * Allows custom headers/footers to be added to user to user emails. 
- * 
+ * Allows custom headers/footers to be added to user to user emails.
+ *
  * @addtogroup Extensions
  *
  * @link http://www.mediawiki.org/wiki/Extension:FormatEmail Documentation
- *
  *
  * @author Travis Derouin <travis@wikihow.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
@@ -23,30 +22,18 @@ $wgExtensionCredits['other'][] = array(
 	'url' => 'http://www.mediawiki.org/wiki/Extension:FormatEmail',
 );
 
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['EmailUser'] = $dir . 'FormatEmail.i18n.php';
+
 $wgHooks['EmailUser'][] = 'wfFormatEmail';
 
 function wfFormatEmailInit() {
-	global $wgMessageCache;
-	 $wgMessageCache->addMessages(
- 	array(
-			'email_header' => '',
-			'email_footer' => '
-
-----------------------------------------------------------------------------
-This email has been sent to you through the {{SITENAME}} email system by $1.
-
-$2',
-		)
-	);
+	wfLoadExtensionMessages( 'EmailUser' );
 }
 
 function wfFormatEmail (&$to, &$from, &$subject, &$text ) {
-	global $wgUser; 
+	global $wgUser;
 	$ul = $wgUser->getUserPage();
 	$text = wfMsg('email_header') . $text . wfMsg('email_footer',$wgUser->getName(), $ul->getFullURL());
 	return true;
 }
-
-
-
-
