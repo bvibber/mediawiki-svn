bvibber/mediawiki-svn

commit a2c520ffa81f7b8192a2f802d4feba7a54681b44

* (bug 10869) Use proper decimal and thousands separators
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/LuceneSearch@28141 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

LuceneSearch_body.php
@@ -439,7 +439,7 @@ class LuceneSearch extends SpecialPage
 	function showHit($result, $terms) {
 		$fname = 'LuceneSearch::showHit';
 		wfProfileIn($fname);
-		global $wgUser, $wgContLang, $wgTitle, $wgOut, $wgDisableSearchContext;
+		global $wgUser, $wgLang, $wgContLang, $wgTitle, $wgOut, $wgDisableSearchContext;
 
 		$t = $result->getTitle();
 		if(is_null($t)) {
@@ -465,7 +465,7 @@ class LuceneSearch extends SpecialPage
 			}
 
 			$text = $rev->getText();
-			$size = wfMsg('searchsize', sprintf("%.1f", strlen($text) / 1024), str_word_count($text));
+			$size = wfMsg( 'search-result-size', $this->mSkin->formatSize( sprintf( "%.1f", strlen( $text ) / 1024 ), str_word_count( $text ) ) );
 			$text = $this->removeWiki($text);
 			$date = $wgContLang->timeanddate($rev->getTimestamp());
 		} else {
@@ -510,7 +510,7 @@ class LuceneSearch extends SpecialPage
 		if (!$wgDisableSearchContext) { $date = $wgContLang->timeanddate($rev->getTimestamp()); }
 		else { $date = ''; }
 		$percent = sprintf('%2.1f%%', $result->getScore() * 100);
-		$score = wfMsg('searchscore', $percent);
+		$score = wfMsg('searchscore', $wgLang->formatNum( $percent ) );
 		//$url = $t->getFullURL();
 		return '<li style="padding-bottom: 1em;">'.$link.$extract.'<br />'
 			.'<span style="color: green; font-size: small;">'
@@ -903,5 +903,3 @@ class LuceneSearchSet {
 		return $out;
 	}
 }
-
-

commit 03a50ef7a59ce234b873f2171d4419b3d1706464

Fix regression caused in r28141: * reported byte sizes were a factor of 1024 off * word count was missing, listing only $2
Note that 28141 (as fixed) uses formatSize() which in most cases will not include any decimals or thousands separators, as it rounds off to integral values.


git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/LuceneSearch@28154 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

LuceneSearch_body.php
@@ -465,7 +465,9 @@ class LuceneSearch extends SpecialPage
 			}
 
 			$text = $rev->getText();
-			$size = wfMsg( 'search-result-size', $this->mSkin->formatSize( sprintf( "%.1f", strlen( $text ) / 1024 ), str_word_count( $text ) ) );
+			$size = wfMsgHtml( 'search-result-size',
+				$this->mSkin->formatSize( strlen( $text ) ),
+				str_word_count( $text ) );
 			$text = $this->removeWiki($text);
 			$date = $wgContLang->timeanddate($rev->getTimestamp());
 		} else {