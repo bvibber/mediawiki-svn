fe9cca3d69b1a873e022af6b1eea646bfc2a8546
amidaniel <amidaniel@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-07 03:46:28 +0000

    Load user data using User.load() rather than User.loadFromDatabase().
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28234 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GiveRollback/GiveRollback.page.php
+++ extensions/GiveRollback/GiveRollback.page.php
@@ -48,7 +48,7 @@ class GiveRollback extends SpecialPage {
 			$wgOut->addHtml( wfElement( 'p', NULL, NULL ) );
 			$user = User::newFromName( $this->target );
 			if( is_object( $user ) && !is_null( $user ) ) {
-				$user->loadFromDatabase();
+				$user->load();
 				# Valid username, check existence
 				if( $user->getID() ) {
 					# Exists; check current privileges
