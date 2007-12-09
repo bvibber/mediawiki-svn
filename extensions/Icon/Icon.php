e312fd05a9028f89bef4647532f7e22025ba647a
tlaqua <tlaqua@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-09 19:51:51 +0000

    New Extension - Based on ImageLink Extension. * Supports linkless images and linked images * Intended to simplify use of uploaded images as icons
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28323 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Icon/Icon.php
@@ -0,0 +1,79 @@
+<?php
+
+$wgExtensionCredits['other'][] = array( 
+	'name'        	=> 'Icon', 
+	'version'     	=> '1.0',
+	'author'      	=> 'Tim Laqua', 
+	'description' 	=> 'Allows you to use Images as Icons and Icon Links',
+	'url' 			=> 'http://mediawiki.org/wiki/Extension:Icon',			
+);
+
+$wgExtensionFunctions[] = 'efIcon_Setup';
+$wgHooks['LanguageGetMagic'][]       = 'efIcon_LanguageGetMagic';
+
+function efIcon_Setup() {
+        global $wgParser;
+        # Set a function hook associating the "example" magic word with our function
+        $wgParser->setFunctionHook( 'icon', 'efIcon_Render' );
+		return true;
+}
+
+function efIcon_LanguageGetMagic( &$magicWords, $langCode ) {
+        # Add the magic word
+        # The first array element is case sensitive, in this case it is not case sensitive
+        # All remaining elements are synonyms for our parser function
+        $magicWords['icon'] = array( 0, 'icon' );
+        # unless we return true, other parser functions extensions won't get loaded.
+        return true;
+}
+
+function efIcon_Render(&$parser, $img, $alt=null, $width=null, $page=null) {
+	$ititle = Title::newFromText( $img );
+
+	// this really shouldn't happen... not much we can do here.		
+	if (!is_object($ititle)) 
+		return '';
+
+	// check if we are dealing with an InterWiki link
+	if ( $ititle->isLocal() ) {
+		$image = Image::newFromName( $img );
+		if (!$image->exists()) 
+			return '[[Image:'.$img.']]';
+
+		$iURL = $image->getURL();
+	} else {
+		$iURL = $ititle->getFullURL();
+	}
+
+	// Optional parameters
+	if (!empty($alt))		$alt    = "alt='${alt}'"; 		else $alt='';
+	if (!empty($width))		$width  = "width='${width}'"; 	else $width='';
+	//if (!empty($height))	$height = "height='${height}'";	else $height='';
+	if (!empty($border))	$border = "border='${border}'";	else $border='';
+
+	$imageString = "<img src='${iURL}' $alt $width $height $border />";
+	
+	if (!empty($page)) {
+		$ptitle = Title::newFromText( $page );
+		
+		// this might happen in templates...
+		if (!is_object( $ptitle ))
+			return 'IconLink: invalid title name.';
+
+		if ( $ptitle->isLocal() )
+		{
+			$tURL = $ptitle->getLocalUrl();
+			$aClass=''; 			
+		}
+		else
+		{
+			$tURL = $ptitle->getFullURL();
+			$aClass = 'class="extiw"';
+		}
+		$output = "<a ".$aClass." href='${tURL}'>{$imageString}</a>";
+	} else {
+		$output = $imageString;
+	}
+	
+	return array($output, 'noparse' => true, 'isHTML' => true);
+}
