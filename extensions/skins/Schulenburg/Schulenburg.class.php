6774dcf8cd174b7b72afc855b2e9835a355a194a
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-14 08:44:15 +0000

    Editable site JS/CSS
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28464 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/skins/Schulenburg/Schulenburg.class.php
+++ extensions/skins/Schulenburg/Schulenburg.class.php
@@ -20,6 +20,9 @@ class SkinSchulenburg extends Skin {
 		$bodyText = preg_replace( '!(<img[^>]*src=")schstock/!', "$1{$this->path}/images/", $bodyText );
 		$bodyText = preg_replace( '!(<[^>]*style="[^"]*url\()schstock/!', "$1{$this->path}/images/", $bodyText );
 		$bodyText = preg_replace( '!(<input[^>]*src=")schstock/!', "$1{$this->path}/images/", $bodyText );
+		$query = 'usemsgcache=yes&action=raw&ctype=text/css&smaxage=2678400';
+		$siteCss = Title::newFromText( 'MediaWiki:Schulenburg.css' )->getLocalUrl( $query );
+		$siteJs = Title::newFromText( 'MediaWiki:Schulenburg.js' )->getLocalUrl( $query );
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -28,8 +31,10 @@ class SkinSchulenburg extends Skin {
 		<meta http-equiv="Content-Language" content="<?php echo htmlspecialchars( $lang ) ?>">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title><?php echo htmlspecialchars( $out->getPageTitle() )?> </title>
-		<link rel="stylesheet" href="<?php echo htmlspecialchars( $this->getMainCss() ); ?>" type="text/css">
+		<link rel="stylesheet" href="<?php echo htmlspecialchars( $this->getMainCss() ); ?>" type="text/css"/>
 		<script type="text/javascript" src="<?php echo htmlspecialchars( $this->getMainJs() ); ?>"></script>
+		<link rel="stylesheet" href="<?php echo htmlspecialchars( $siteCss ); ?>" type="text/css"/>
+		<script type="text/javascript" src="<?php echo htmlspecialchars( $siteJs ); ?>"></script>
 		<style type="text/css">
 			body {
 				margin: 0px;
