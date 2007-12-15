83e4a5de10ff14aa0e41c4d9d128b651699e92c0
gri6507 <gri6507@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-12-15 17:09:14 +0000

    added Hebrew translations
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@28518 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TodoTasks/SpecialTaskList.i18n.php
+++ extensions/TodoTasks/SpecialTaskList.i18n.php
@@ -17,6 +17,23 @@ $allMessages = array(
 		'tasklistnoprojects'      => "ERROR: It looks like you enabled '''\$wgUseProjects''', but did not create [[MediaWiki:TodoTasksValidProjects]]. See [http://www.mediawiki.org/wiki/Extension:Todo_Tasks#Step_8 Installation Instructions] for more details.",
 	),
 
+	'he' => array(
+		'tasklist'                => 'רשימת מטלות',
+		'tasklistbyproject'       => 'רשימת מטלות לפי פרוייקטים',
+		'tasklistunknownproject'  => 'פרוייקט לא ידוע',
+		'tasklistunspecuser'      => 'משתמש לא מוגדר',
+		'tasklistincorrectuser'   => 'משתמש לא נכון',
+		'tasklistemail'           => '%s היקר והחביב',
+		'tasklistemailsubject'    => '[%s] שיוני ברשימת המטלות',
+		'tasklistmytasks'         => 'המטלות שלי',
+		'tasklistbyprojectbad'    => "פרוייקט '''%s''' איננו קיים. לקבלת רשימת הפרוייקטים, צפה כאן [[MediaWiki:TodoTasksValidProjects]].",
+		'tasklistbyprojname'      => "מטלה הוגדרה ל'''%s'''",
+		'tasklistchooseproj'      => 'בחר פרוייקט: ',
+		'tasklistprojdisp'        => 'תצוגה',
+		'tasklistbyname'          => '== רשימת מטלות עבור %s ==',
+		'tasklistnoprojects'      => "שגיאה: נראה שאפשרת את '''\$wgUseProjects''', אבל לא יצרת [[MediaWiki:TodoTasksValidProjects]]. ראה [http://www.mediawiki.org/wiki/Extension:Todo_Tasks#Step_8 Installation Instructions] for more details.",
+	),
+
 	'ar' => array(
 		'tasklist'                => 'قائمة المهام',
 		'tasklistbyproject'       => 'قائمة المهام حسب المشروع',
