0f9b7964bac07db62d2bbd7548f258747991faba
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-08 19:35:49 +0000

    * use wfLoadExtensionMessages for BackAndForth * update structure in BackAndForth.php * add version and description in Extension credits for BackAndForth * update Translate extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@29447 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BackAndForth/BackAndForth.i18n.php
+++ extensions/BackAndForth/BackAndForth.i18n.php
@@ -1,261 +1,251 @@
 <?php
-
 /**
- * Internationalisation file for the BackAndForth extension
+ * Internationalisation file for extension BackAndForth
  *
+ * @addtogroup Extensions
  * @author Rob Church <robchur@gmail.com>
  */
 
-/**
- * Fetch extension messages indexed per language
- *
- * @return array
- */
-function efBackAndForthMessages() {
-	$messages = array(
+$messages = array();
 
-'en' => array(
+/* English
+ * @author Rob Church
+ */
+$messages['en'] = array(
 	'backforth-next' => 'Next ($1)',
 	'backforth-prev' => 'Previous ($1)',
-),
+);
 
-'af' => array(
+$messages['af'] = array(
 	'backforth-next' => 'Volgende ($1)',
 	'backforth-prev' => 'Vorige ($1)',
-),
+);
 
-'ang' => array(
+$messages['ang'] = array(
 	'backforth-next' => 'Nīehst ($1)',
 	'backforth-prev' => 'Beforan ($1)',
-),
+);
 
-'ar' => array(
+$messages['ar'] = array(
 	'backforth-next' => 'التالي ($1)',
 	'backforth-prev' => 'السابق ($1)',
-),
+);
 
-'bcl' => array(
+$messages['bcl'] = array(
 	'backforth-next' => 'Sunod ($1)',
 	'backforth-prev' => 'Nakaagi ($1)',
-),
+);
 
-'bg' => array(
+$messages['bg'] = array(
 	'backforth-next' => 'Следващи ($1)',
 	'backforth-prev' => 'Предишни ($1)',
-),
+);
 
 /** Bengali (বাংলা)
  * @author Zaheen
  */
-'bn' => array(
+$messages['bn'] = array(
 	'backforth-next' => 'পরবর্তী ($1)',
 	'backforth-prev' => 'পূর্ববর্তী ($1)',
-),
+);
 
-'br' => array(
+$messages['br'] = array(
 	'backforth-next' => 'War-lerc\'h ($1)',
 	'backforth-prev' => 'Kent ($1)',
-),
+);
 
-'de' => array(
+$messages['de'] = array(
 	'backforth-next' => 'Nächste ($1)',
 	'backforth-prev' => 'Vorherige ($1)',
-),
+);
 
-'el' => array(
+$messages['el'] = array(
 	'backforth-next' => 'Επόμενο ($1)',
 	'backforth-prev' => 'Προηγούμενο ($1)',
-),
+);
 
-'eo' => array(
+$messages['eo'] = array(
 	'backforth-next' => 'Sekva ($1)',
 	'backforth-prev' => 'Antaŭa ($1)',
-),
+);
 
-'ext' => array(
+$messages['ext'] = array(
 	'backforth-next' => 'Siguienti ($1)',
-),
+);
 
-'fi' => array(
+$messages['fi'] = array(
 	'backforth-next' => 'Seuraava ($1)',
 	'backforth-prev' => 'Edellinen ($1)',
-),
+);
 
-'fr' => array(
+$messages['fr'] = array(
 	'backforth-next' => 'Suivant ($1)',
 	'backforth-prev' => 'Précédent ($1)',
-),
+);
 
-'frp' => array(
+$messages['frp'] = array(
 	'backforth-next' => 'Siuvent ($1)',
 	'backforth-prev' => 'Prècèdent ($1)',
-),
+);
 
-'gl' => array(
+$messages['gl'] = array(
 	'backforth-next' => 'Seguinte ($1)',
 	'backforth-prev' => 'Anterior ($1)',
-),
+);
 
-'hr' => array(
+$messages['hr'] = array(
 	'backforth-next' => 'Slijedeći ($1)',
 	'backforth-prev' => 'Prethodni ($1)',
-),
+);
 
-'hsb' => array(
+$messages['hsb'] = array(
 	'backforth-next' => 'Přichodne ($1)',
 	'backforth-prev' => 'Předchadne ($1)',
-),
+);
 
 /** Hungarian (Magyar)
  * @author Bdanee
  */
-'hu' => array(
+$messages['hu'] = array(
 	'backforth-next' => 'Következő ($1)',
 	'backforth-prev' => 'Előző ($1)',
-),
+);
 
-'hy' => array(
+$messages['hy'] = array(
 	'backforth-next' => 'Հաջորդ ($1)',
 	'backforth-prev' => 'Նախորդ ($1)',
-),
+);
 
-'is' => array(
+$messages['is'] = array(
 	'backforth-next' => 'Næsta ($1)',
 	'backforth-prev' => 'Síðasta ($1)',
-),
+);
 
-'it' => array(
+$messages['it'] = array(
 	'backforth-next' => 'Successivi ($1)',
 	'backforth-prev' => 'Precedenti ($1)',
-),
+);
 
-'ja' => array(
+$messages['ja'] = array(
 	'backforth-next' => '次 ($1)',
 	'backforth-prev' => '前 ($1)',
-),
+);
 
-'kaa' => array(
+$messages['kaa'] = array(
 	'backforth-next' => 'Keyingi ($1)',
 	'backforth-prev' => 'Aldın\'g\'ı ($1)',
-),
+);
 
-'kn' => array(
+$messages['kn'] = array(
 	'backforth-next' => 'ಮುಂದಿನ ($1)',
 	'backforth-prev' => 'ಹಿಂದಿನ ($1)',
-),
+);
 
 /** Lithuanian (Lietuvių)
  * @author Matasg
  */
-'lt' => array(
+$messages['lt'] = array(
 	'backforth-next' => 'Kitas ($1)',
 	'backforth-prev' => 'Ankstesnis ($1)',
-),
+);
 
-'nl' => array(
+$messages['nl'] = array(
 	'backforth-next' => 'Volgende ($1)',
 	'backforth-prev' => 'Vorige ($1)',
-),
+);
 
-'no' => array(
+$messages['no'] = array(
 	'backforth-next' => 'Neste ($1)',
 	'backforth-prev' => 'Forrige ($1)',
-),
+);
 
-'oc' => array(
+$messages['oc'] = array(
 	'backforth-next' => 'Seguent ($1)',
 	'backforth-prev' => 'Precedent ($1)',
-),
+);
 
-'pl' => array(
+$messages['pl'] = array(
 	'backforth-next' => 'Następny ($1)',
 	'backforth-prev' => 'Poprzedni ($1)',
-),
+);
 
-'pms' => array(
+$messages['pms'] = array(
 	'backforth-next' => 'Anans ($1)',
 	'backforth-prev' => 'André ($1)',
-),
+);
 
-'pt' => array(
+$messages['pt'] = array(
 	'backforth-next' => 'Próximo ($1)',
 	'backforth-prev' => 'Anterior ($1)',
-),
+);
 
 /** Russian (Русский)
  * @author .:Ajvol:.
  */
-'ru' => array(
+$messages['ru'] = array(
 	'backforth-next' => 'След. ($1)',
 	'backforth-prev' => 'Пред. ($1)',
-),
+);
 
-'sah' => array(
+$messages['sah'] = array(
 	'backforth-next' => 'Аныгыскы ($1)',
 	'backforth-prev' => 'Иннинээҕи ($1)',
-),
+);
 
-'scn' => array(
+$messages['scn'] = array(
 	'backforth-next' => 'Succissivi ($1)',
 	'backforth-prev' => 'Pricidenti ($1)',
-),
+);
 
-'sk' => array(
+$messages['sk'] = array(
 	'backforth-next' => 'Ďalšie ($1)',
 	'backforth-prev' => 'Predošlé ($1)',
-),
+);
 
 /** ћирилица (ћирилица)
  * @author Sasa Stefanovic
  */
-'sr-ec' => array(
+$messages['sr-ec'] = array(
 	'backforth-next' => 'Следеће ($1)',
 	'backforth-prev' => 'Претходно ($1)',
-),
+);
 
-'stq' => array(
+$messages['stq'] = array(
 	'backforth-next' => 'Naiste ($1)',
 	'backforth-prev' => 'Foarige ($1)',
-),
+);
 
-'sv' => array(
+$messages['sv'] = array(
 	'backforth-next' => 'Nästa ($1)',
 	'backforth-prev' => 'Föregående ($1)',
-),
+);
 
 /** Volapük (Volapük)
  * @author Malafaya
  */
-'vo' => array(
+$messages['vo'] = array(
 	'backforth-next' => 'Sököl ($1)',
 	'backforth-prev' => 'Büik ($1)',
-),
+);
 
-'yue' => array(
+$messages['yue'] = array(
 	'backforth-next' => '下一篇 ($1)',
 	'backforth-prev' => '上一篇 ($1)',
-),
+);
 
-'zh-hans' => array(
+$messages['zh-hans'] = array(
 	'backforth-next' => '下一条 ($1)',
 	'backforth-prev' => '上一条 ($1)',
-),
+);
 
-'zh-hant' => array(
+$messages['zh-hant'] = array(
 	'backforth-next' => '下一條 ($1)',
 	'backforth-prev' => '上一條 ($1)',
-),
+);
 
-	);
-
-	/* Chinese defaults, fallback to zh-hans or zh-hant */
-	$messages['zh'] = $messages['zh-hans'];
-	$messages['zh-cn'] = $messages['zh-hans'];
-	$messages['zh-hk'] = $messages['zh-hant'];
-	$messages['zh-sg'] = $messages['zh-hans'];
-	$messages['zh-tw'] = $messages['zh-hant'];
-	/* Cantonese default, fallback to yue */
-	$messages['zh-yue'] = $messages['yue'];
-
-	return $messages;
-}
+$messages['zh'] = $messages['zh-hans'];
+$messages['zh-cn'] = $messages['zh-hans'];
+$messages['zh-hk'] = $messages['zh-hant'];
+$messages['zh-sg'] = $messages['zh-hans'];
+$messages['zh-tw'] = $messages['zh-hant'];
+$messages['zh-yue'] = $messages['yue'];
--- extensions/BackAndForth/BackAndForth.php
+++ extensions/BackAndForth/BackAndForth.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Extension adds "next" and "previous" alphabetic paging links to
  * the top of articles
@@ -7,28 +6,31 @@
  * @addtogroup Extensions
  * @author Rob Church <robchur@gmail.com>
  */
-if( defined( 'MEDIAWIKI' ) ) {
 
-	$wgAutoloadClasses['BackAndForth'] = dirname( __FILE__ ) . '/BackAndForth.class.php';
-	$wgExtensionFunctions[] = 'efBackAndForth';
-	$wgExtensionCredits['other'][] = array(
-		'name' => 'Back and Forth',
-		'author' => 'Rob Church',
-		'url' => 'http://www.mediawiki.org/wiki/Extension:Back-and-Forth',
-	);
-	
-	/**
-	 * Extension setup function
-	 */
-	function efBackAndForth() {
-		global $wgMessageCache, $wgHooks;
-		require_once( dirname( __FILE__ ) . '/BackAndForth.i18n.php' );
-		foreach( efBackAndForthMessages() as $lang => $messages )
-			$wgMessageCache->addMessages( $messages, $lang );
-		$wgHooks['ArticleViewHeader'][] = 'BackAndForth::viewHook';
-	}
-	
-} else {
+if( !defined( 'MEDIAWIKI' ) ) {
 	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
-	exit( 1 );
+	die( 1 );
+}
+
+$wgAutoloadClasses['BackAndForth'] = dirname( __FILE__ ) . '/BackAndForth.class.php';
+$wgExtensionFunctions[] = 'efBackAndForth';
+$wgExtensionCredits['other'][] = array(
+	'name' => 'Back and Forth',
+	'version' => '2008-01-08',
+	'author' => 'Rob Church',
+	'description' => 'Adds "Next" and "Previous" alphabetic paging links to the top of articles',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Back-and-Forth',
+);
+
+$wgExtensionMessagesFiles['BackAndForth'] = dirname(__FILE__) . '/BackAndForth.i18n.php';
+
+/**
+ * Extension setup function
+ */
+function efBackAndForth() {
+	global $wgHooks;
+
+	wfLoadExtensionMessages( 'BackAndForth' );
+
+	$wgHooks['ArticleViewHeader'][] = 'BackAndForth::viewHook';
 }
