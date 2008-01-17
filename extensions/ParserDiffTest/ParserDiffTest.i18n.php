b7477f2175368b11e22e196fd208f72bb8d2bd03
rotem <rotem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-17 14:34:51 +0000

    Adding Hebrew translation.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@29897 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ParserDiffTest/ParserDiffTest.i18n.php
+++ extensions/ParserDiffTest/ParserDiffTest.i18n.php
@@ -1,6 +1,6 @@
 <?php
 $messages = array(
-	'en' => array( 
+	'en' => array(
 		'parserdifftest' => 'Parser diff test',
 		'pdtest_no_target' => 'No target specified.',
 		'pdtest_page_missing' => 'The specified page was not found in the database.',
@@ -13,6 +13,19 @@ $messages = array(
 		'pdtest_diff' => 'Differences',
 		'pdtest_side_by_side' => 'Output comparison',
 		'pdt_comparing_page' => 'Comparing parser output from [[$1]]',
-	)
+	),
+	'he' => array(
+		'parserdifftest' => 'בדיקת השינויים במפענח',
+		'pdtest_no_target' => 'לא נבחר יעד.',
+		'pdtest_page_missing' => 'הדף המבוקש לא נמצא במסד הנתונים.',
+		'pdtest_no_changes' => 'לא נמצאו שינויים.',
+		'pdtest_time_report' => '<b>$1</b> לקח $2 שניות, <b>$3</b> לקח $4 שניות.',
+		'pdtest_title' => 'כותרת הדף:',
+		'pdtest_text' => 'טקסט לבדיקה:',
+		'pdtest_ok' => 'שליחה',
+		'pdtest_get_text' => 'שימוש בטקסט מהדף',
+		'pdtest_diff' => 'הבדלים',
+		'pdtest_side_by_side' => 'השוואת הפלט',
+		'pdt_comparing_page' => 'משווה את פלט המפענחים מהדף [[$1]]',
+	),
 );
-
