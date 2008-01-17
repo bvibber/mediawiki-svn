af9bc5fef3742546b336cc8fac12c585c5fac020
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-17 20:12:04 +0000

    * change ParserDiffTest.i18n.php format * add ParserDiffTest support to translate and add to WMF extensions group * add zea to LocalNamesEn.php in cldr
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@29901 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ParserDiffTest/ParserDiffTest.i18n.php
+++ extensions/ParserDiffTest/ParserDiffTest.i18n.php
@@ -1,31 +1,38 @@
 <?php
-$messages = array(
-	'en' => array(
-		'parserdifftest' => 'Parser diff test',
-		'pdtest_no_target' => 'No target specified.',
-		'pdtest_page_missing' => 'The specified page was not found in the database.',
-		'pdtest_no_changes' => 'No changes detected.',
-		'pdtest_time_report' => '<b>$1</b> took $2 seconds, <b>$3</b> took $4 seconds.',
-		'pdtest_title' => 'Context title:',
-		'pdtest_text' => 'Input text:',
-		'pdtest_ok' => 'OK',
-		'pdtest_get_text' => 'Get text from page',
-		'pdtest_diff' => 'Differences',
-		'pdtest_side_by_side' => 'Output comparison',
-		'pdt_comparing_page' => 'Comparing parser output from [[$1]]',
-	),
-	'he' => array(
-		'parserdifftest' => 'בדיקת השינויים במפענח',
-		'pdtest_no_target' => 'לא נבחר יעד.',
-		'pdtest_page_missing' => 'הדף המבוקש לא נמצא במסד הנתונים.',
-		'pdtest_no_changes' => 'לא נמצאו שינויים.',
-		'pdtest_time_report' => '<b>$1</b> לקח $2 שניות, <b>$3</b> לקח $4 שניות.',
-		'pdtest_title' => 'כותרת הדף:',
-		'pdtest_text' => 'טקסט לבדיקה:',
-		'pdtest_ok' => 'שליחה',
-		'pdtest_get_text' => 'שימוש בטקסט מהדף',
-		'pdtest_diff' => 'הבדלים',
-		'pdtest_side_by_side' => 'השוואת הפלט',
-		'pdt_comparing_page' => 'משווה את פלט המפענחים מהדף [[$1]]',
-	),
+/**
+ * Internationalisation file for extension ParserDiffTest.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'parserdifftest' => 'Parser diff test',
+	'pdtest_no_target' => 'No target specified.',
+	'pdtest_page_missing' => 'The specified page was not found in the database.',
+	'pdtest_no_changes' => 'No changes detected.',
+	'pdtest_time_report' => '<b>$1</b> took $2 seconds, <b>$3</b> took $4 seconds.',
+	'pdtest_title' => 'Context title:',
+	'pdtest_text' => 'Input text:',
+	'pdtest_ok' => 'OK',
+	'pdtest_get_text' => 'Get text from page',
+	'pdtest_diff' => 'Differences',
+	'pdtest_side_by_side' => 'Output comparison',
+	'pdt_comparing_page' => 'Comparing parser output from [[$1]]',
+),
+
+$messages['he'] = array(
+	'parserdifftest' => 'בדיקת השינויים במפענח',
+	'pdtest_no_target' => 'לא נבחר יעד.',
+	'pdtest_page_missing' => 'הדף המבוקש לא נמצא במסד הנתונים.',
+	'pdtest_no_changes' => 'לא נמצאו שינויים.',
+	'pdtest_time_report' => '<b>$1</b> לקח $2 שניות, <b>$3</b> לקח $4 שניות.',
+	'pdtest_title' => 'כותרת הדף:',
+	'pdtest_text' => 'טקסט לבדיקה:',
+	'pdtest_ok' => 'שליחה',
+	'pdtest_get_text' => 'שימוש בטקסט מהדף',
+	'pdtest_diff' => 'הבדלים',
+	'pdtest_side_by_side' => 'השוואת הפלט',
+	'pdt_comparing_page' => 'משווה את פלט המפענחים מהדף [[$1]]',
 );
