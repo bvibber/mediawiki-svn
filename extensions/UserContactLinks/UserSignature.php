35fc6ef5279fece55cf60179b3a89fdf95c574ed
gri6507 <gri6507@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-27 16:31:33 +0000

    Fixed a minor bug which would result in a E_NOTICE error message
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30184 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UserContactLinks/UserSignature.php
+++ extensions/UserContactLinks/UserSignature.php
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 global $wgHooks, $wgOut;
 $wgHooks['ParserAfterStrip'][]      = 'parseUserSignatures';
 $wgExtensionCredits['parserhook'][] = array(
-	'version'     => '0.4',
+	'version'     => '0.4.1',
 	'name'        => 'UserSignature',
 	'author'      => 'Paul Grinberg',
 	'email'       => 'gri6507 at yahoo dot com',
@@ -59,7 +59,7 @@ if (!function_exists('getUserIDFromUserText')) {
 		$userid = 0;
 
 		if (preg_match('/^\s*(.*?)\s*$/', $user, $matches))
-		$user = $matches[1];
+		    $user = $matches[1];
 
 		$u = User::newFromName($user);
 		if ($u) {
@@ -76,7 +76,15 @@ if (!function_exists('getUserIDFromUserText')) {
 				}
 				$dbr->freeResult($res);
 				if (!$userid) { // if not case insensitive userRealName, try case insensitive lastname
-					list ($first, $last) = preg_split('/\s+/', $user);
+					$first = "";
+					$last = "";
+					$fullname = array();
+					$fullname = preg_split('/\s+/', $user);
+					if (count($fullname) > 0)
+					    $first=$fullname[0];
+					if (count($fullname) > 1)
+					    $last=$fullname[1];
+
 					if ($last != '') {
 						$sql = "SELECT user_id FROM ". $dbr->tableName('user') ." WHERE UPPER(user_real_name) LIKE '%" . strtoupper($last) . "%'";
 						$res = $dbr->query( $sql, __METHOD__ );
