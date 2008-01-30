1379fb18403c85222d6f2c6e90c92517e54205d4
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-30 05:02:17 +0000

    * fix for reporting on title key rebuild when no pages are processed * command-line script to do offline rebuilds * split class to a separate file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30286 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/TitleKey/rebuildTitleKeys.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "../../maintenance/commandLine.inc";
+
+// In case we want to do offline initialization...
+if( !class_exists( 'TitleKey' ) ) {
+	require dirname( __FILE__ ) . '/TitleKey_body.php';
+}
+
+if( isset( $options['help'] ) ) {
+	echo "Rebuilds titlekey table entries for all pages in DB.\n";
+	echo "Usage:\n";
+	echo "  php extensions/TitleKey/rebuildTitleKeys.php [--start=<page_id>]\n";
+} else {
+	$start = intval( @$options['start'] );
+	echo "Rebuilding titlekey table...\n";
+	TitleKey::populateKeys( $start );
+}
+
+?>
