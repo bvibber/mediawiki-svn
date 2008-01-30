0f4587e8d167ee79958bef463de33b9a41a572f3
tlaqua <tlaqua@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-30 20:05:36 +0000

    * Adding support for multiple group portals
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30310 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GroupPortal/GroupPortal.php
+++ extensions/GroupPortal/GroupPortal.php
@@ -12,35 +12,38 @@ $wgHooks['ArticlePageDataBefore'][] = 'efGroupPortal_ArticlePageDataBefore';
 function efGroupPortal_ArticlePageDataBefore ( &$article, &$fields ) {
 	$mainTitle = Title::newMainPage();
 	$title = $article->getTitle();
-	if ( $title->getFullText() == $mainTitle->getFullText() ) {
-		$groupPortalText = wfMsgForContentNoTrans('groupportal');
-		#echo 'groupportal:' . $groupPortalText;
-		
-		preg_match_all('/^(.+)\|(.+)$/', $groupPortalText, $groupPortals, PREG_SET_ORDER );
-		
-		global $wgUser;
-		$groups = $wgUser->getGroups();
-		
-		$targetPortal = '';
-		
-		foreach ($groupPortals as $groupPortal) {
-			#echo '<br />Testing: ' . $groupPortal[1];
+	
+	global $wgRequest;
+	$action    = $wgRequest->getVal( 'action'    );
+	$redirect  = $wgRequest->getVal( 'redirect'  );	
+	
+	if ( (empty( $action ) || $action == 'view') && !isset( $redirect ) ) {
+		if ( $title->getFullText() == $mainTitle->getFullText() ) {
+			$groupPortalText = wfMsgForContentNoTrans('groupportal');
 			
-			foreach ($groups as $group) {
-				#echo '<br />group: ' . $group;
-			}
-			if ( in_array($groupPortal[1], $groups ) ) {
-				$targetPortal = $groupPortal[2];
-				#echo 'Redirecting to: ' . $groupPortal[2];
-			}
-		}
-		
-		if ( !empty( $targetPortal ) ) {
-			#echo 'trying to redirect!:<br />';
-			$target = Title::newFromText($targetPortal);
+			$groupPortals = spliti("\n", $groupPortalText);
 			
-			if( is_object( $target ) ) {
-				header('Location: ' . $target->getLocalURL() );
+			global $wgUser;
+			$groups = $wgUser->getGroups();
+			
+			$targetPortal = '';
+			foreach ($groupPortals as $groupPortal) {
+				$mcount = preg_match('/^(.+)\|(.+)$/', $groupPortal, $matches);
+				
+				if ( $mcount > 0 ) {
+					if ( in_array($matches[1], $groups ) || 
+						 ( $matches[1] == '*' && empty( $targetPortal ) ) ) {
+						$targetPortal = $matches[2];
+					}
+				}
+			}
+			
+			if ( !empty( $targetPortal ) ) {
+				$target = Title::newFromText($targetPortal);
+				
+				if( is_object( $target ) ) {
+					header('Location: ' . $target->getLocalURL() );
+				}
 			}
 		}
 	}
