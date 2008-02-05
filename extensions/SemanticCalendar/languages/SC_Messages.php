05bb37fdb770212b5eff622ad1660b08f21c3311
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-05 21:37:38 +0000

    New extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30590 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SemanticCalendar/languages/SC_Messages.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalization file for the Semantic Calendar extension
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	// user messages
+        'sc_previousmonth' => 'Previous month',
+        'sc_nextmonth' => 'Next month',
+	'sc_error_year2038' => 'Error: This system cannot handle dates after 2038, due to the [http://en.wikipedia.org/wiki/Year_2038_problem year 2038 problem]',
+	'sc_error_beforeyear' => 'Error: This system cannot handle dates before $1',
+);
