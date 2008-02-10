4f915ad0240dcdfdf67ae871772832df5a6dee87
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-10 23:16:32 +0000

    * Import NewUserMessage from http://www.mediawiki.org/w/index.php?title=Extension:NewUserMessage&oldid=146872. Made a few adjustments (added descriptionmsg, bumped version). * Add support for Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30813 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/NewUserMessage/NewUserMessage.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation file for extension NewUserMessage.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/* English
+ * @author [http://www.organicdesign.co.nz/User:Nad Nad]
+ */
+$messages['en'] = array(
+	'newusermessage-desc' => "Add a [[MediaWiki:NewUserMessage|message]] to newly created user's talk pages",
+);
+
+/* Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'newusermessage-desc' => 'Voegt een [[MediaWiki:NewUserMessage|bericht]] toe aan de overlegpagina van nieuwe gebruikers',
+);
