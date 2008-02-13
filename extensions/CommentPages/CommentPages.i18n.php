fad7f92d70eaeb79cbf790f7197628017d3c3ce2
skizzerz <skizzerz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-13 22:18:42 +0000

    Add CommentPages extension from http://code.zachhauri.com/svn/commenttabs/
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30923 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CommentPages/CommentPages.i18n.php
@@ -0,0 +1,9 @@
+<?php
+    $wgCommentsPagesMessages = array();
+
+    $wgCommentsPagesMessages['en'] = array(
+        'commenttab-editintro' => '',
+        'commenttab-preload' => '',
+        'nstab-comments' => 'Commentary',
+    );
+
