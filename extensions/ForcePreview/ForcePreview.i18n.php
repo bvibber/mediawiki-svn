256b8aac86d19c85d291df4a53571253faa47514
skizzerz <skizzerz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-13 21:26:00 +0000

    Adding two extensions (ForcePreview and RightFunctions)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@30918 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ForcePreview/ForcePreview.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for the ForcePreview extension
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/* English
+ @author Ryan Schmidt
+ */
+$messages['en'] = array(
+	'forcepreview' => 'Save page (use preview first)',
+	'forcepreview-desc' => 'Force preview for unprivelaged users',
+);
---
+++ extensions/RightFunctions/RightFunctions.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for the RightFunctions extension
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/* English
+ @author Ryan Schmidt
+ */
+$messages['en'] = array( 
+	'rightfunctions' => 'Permission-based parser functions',
+	'rightfunctions-restboth' => '$1 (from local page), $2 (from cascading sources)',
+	'rightfunctions-restcasc' => '$1 (from cascading sources)',
+);
