d0d6768f996432ad8d3d8abf55bae86c1f3b74f1
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-19 16:43:47 +0000

    New extension: Data Transfer
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31093 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/DataTransfer/languages/DT_Messages.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalization file for the Data Transfer extension
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	'viewxml' => 'View XML',
+	'dt_viewxml_docu' => 'Please select among the following categories and namespaces to view in XML format.',
+	'dt_viewxml_categories' => 'Categories',
+	'dt_viewxml_namespaces' => 'Namespaces',
+	'dt_viewxml_simplifiedformat' => 'Simplified format',
+	'dt_xml_namespace' => 'Namespace',
+	'dt_xml_page' => 'Page',
+	'dt_xml_field' => 'Field',
+	'dt_xml_name' => 'Name',
+	'dt_xml_title' => 'Title',
+	'dt_xml_freetext' => 'Free_Text',
+);
