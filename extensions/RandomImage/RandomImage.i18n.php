46e4dfd3d6e7b652da096b3fe7c92907c0385b2e
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-20 11:03:58 +0000

    Add description message for [[Special:Version]]. Used at Wikia.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31116 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/RandomImage/RandomImage.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation file for RandomImage extension.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'randomimage-desc' => 'Provides a random media picker using <tt>&lt;randomimage /&gt;</tt>',
+);
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'randomimage-desc' => 'Fügt das <tt>&lt;randomimage /&gt;</tt>-Tag zur Anzeige eines zufälligen Bildes hinzu',
+);
