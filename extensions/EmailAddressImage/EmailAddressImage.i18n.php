7a3104b625d36485716ddf858c8717eb8dc39874
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-25 18:35:59 +0000

    * importing EmailAddressImage from http://www.mediawiki.org/w/index.php?title=Extension:EmailAddressImage&oldid=164471 with some markup and path changes and added ExtensionCredits * add support for EmailAddressImage in Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31268 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/EmailAddressImage/EmailAddressImage.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation file for extension EmailAddressImage.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Maarten van Dantzich
+ */
+$messages['en'] = array(
+	'emailaddressimage-desc' => 'Adds <tt>&lt;email&gt;</tt> to display an email address as image',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'emailaddressimage-desc' => 'Voegt <tt>&lt;email&gt;</tt> toe om een e-mailadres als afbeelding weer te geven',
+);
