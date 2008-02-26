9931d31ddbafb920a6792552b93aab9a4cc86b06
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-26 13:55:14 +0000

    Add SubPageList3 extension, by James McCormack. Extension is designed to provide a wikitext tag to include a list of subpages of a certain page. Intended for use on Wikiversities and Wikibooks, as those wikis make extensive use of subpaging, and Special:Prefixindex is less efficient, and causes cache incoherency.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31300 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SubPageList3/SubPageList3.i18n.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ***** BEGIN LICENSE BLOCK *****
+ * This file is part of the SubPageList3 extension.
+ * Copyright (c) 2008 James McCormack
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SubPageList3; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * ***** END LICENSE BLOCK *****
+ *
+ * @version 1.02
+ * @link http://www.mediawiki.org/wiki/Extension:SubPageList3
+ * @licence GNU General Public Licence 2.0 or later
+ * @author McCormack, James
+ */
+ 
+/**
+ * Messages list.
+ */
+ 
+$messages = array();
+ 
+$messages['en'] = array(
+        'spl3-desc'         => 'Automatically creates a list of the subpages of a page.',
+        'spl3_debug'        => 'Unknown value for option $1.',
+        'spl3_nosubpages'   => '$1 has no subpages to list.',
+);
