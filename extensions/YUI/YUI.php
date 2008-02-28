5535a06fdeb4b12186ca0f8392167f859eba2e46
emil <emil@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-02-28 16:26:02 +0000

    added Aaron's and David's social tools (see http://www.mediawiki.org/wiki/Extension:SocialProfile)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31383 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/YUI/YUI.php
@@ -0,0 +1,9 @@
+<?php
+$wgExtensionFunctions[] = "wfYUI";
+
+function wfYUI() {
+        global $wgOut;
+	$wgOut->addScript("<script type=\"text/javascript\" src=\"/extensions/YUI/yui.js\"></script>\n");
+}
+
+?>
