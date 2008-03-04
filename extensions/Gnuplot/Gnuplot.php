50d4bfe9f23f1b8f4907a4985fe374d5476ffac9
tbleher <tbleher@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-03-04 10:09:30 +0000

    First pass through the code: * Use tabs instead of spaces * Global vars for configuration instead of a class * Some cleanups for easier readability * Security: Properly escape some paths before passing them to the shell and HTML
    Probably needs more fixes before it is ready for a public site.
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31538 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Gnuplot/Gnuplot.php
+++ extensions/Gnuplot/Gnuplot.php
@@ -15,88 +15,85 @@
 #   Code adapted from the timeline extension.
 #   Include this file in your LocalSettings.php:
 #     add 'include("extensions/Gnuplot.php")' to the end of the file.
-#   Set member gnuplotCommand of $wgGnuplotSettings to your gnuplot.
+#   Set $wgGnuplotCommand to your gnuplot.
 ####################
 
-class GnuplotSettings {
-  var $gnuplotCommand, $defaultTerminal, $defaultSize;
+$wgGnuplotCommand = '/usr/bin/gnuplot';
+$wgGnuplotDefaultTerminal = 'set terminal png';
+$wgGnuplotDefaultSize = 'set size 0.5,0.5';
 
-    function GnuplotSettings () {
-	$this->gnuplotCommand = '/usr/bin/gnuplot';
-	// Set some defaults here; they may be overwritten by the user.
-	$this->defaultTerminal = 'set terminal png';
-	$this->defaultSize = 'set size 0.5,0.5';
-    }
-};
-
-$wgGnuplotSettings = new GnuplotSettings;
 $wgExtensionFunctions[] = "wfGnuplotExtension";
 
 function wfGnuplotExtension() {
-    global $wgParser;
-    $wgParser->setHook( "gnuplot", "renderGnuplot" );
+	global $wgParser;
+	$wgParser->setHook( "gnuplot", "renderGnuplot" );
 }
 
 function renderGnuplot( $gnuplotsrc ) {
-    global $wgGnuplotSettings, $wgUploadDirectory, $wgUploadPath;
+	global $wgGnuplotDefaultTerminal, $wgGnuplotDefaultSize, $wgGnuplotCommand;
+	global $wgUploadDirectory, $wgUploadPath;
 
-    // filter out harmfull system commands to close a security hole
-    $replaces = array("`" => "", "system" => "", "shell" => "");
-    $gnuplotsrc = strtr($gnuplotsrc, $replaces);
+	// filter out harmfull system commands to close a security hole
+	$replaces = array("`" => "", "system" => "", "shell" => "");
+	$gnuplotsrc = strtr($gnuplotsrc, $replaces);
 
-    // create directory for storing the plot
-    $gnuplotDir = "/gnuplot/";
-    $dest = $wgUploadDirectory . $gnuplotDir;
-    if (!is_dir($dest)) {
-	mkdir($dest, 0777);
-	chmod($dest, 0777);
-    }
-
-    // get the name of the graph to be produced
-    $name = getOutputName ($gnuplotsrc);
-    $graphname = $dest . $name;
-    $fname = $graphname . ".tmp";
-
-    // write the default settings and the input code from wiki into a 
-    // temporary file to be executed by gnuplot, then execute the command
-    if ( ! (file_exists($fname) || file_exists($fname . '.err'))) { 
-	$handle = fopen($fname, 'w');
-	
-	// if terminal and size are not set in the gnuplot source we do it here
-	if (strpos($gnuplotsrc, 'set terminal ') === false) {
-	    fwrite($handle, $wgGnuplotSettings->defaultTerminal . "\n");
-	}
-	if (strpos($gnuplotsrc, 'set size ') === false) {
-	    fwrite($handle, $wgGnuplotSettings->defaultSize . "\n");
+	// create directory for storing the plot
+	$gnuplotDir = "/gnuplot/";
+	$dest = $wgUploadDirectory . $gnuplotDir;
+	if (!is_dir($dest)) {
+		mkdir($dest, 0777);
+		chmod($dest, 0777);
 	}
 
-	// Need to find each occurance of src:<FILE NAME> and replace it 
-	// with the complete file path
-	while (strpos($gnuplotsrc, 'src:') != false) {
-	    $srcStartPosition = strpos ($gnuplotsrc, 'src:') + strlen("src:");
-            $srcEndPosition = strpos ($gnuplotsrc, ' ', $srcStartPosition);
-            $tmpString = substr($gnuplotsrc, $srcStartPosition, $srcEndPosition-$srcStartPosition-1);
-	    $srcFileNamePath = getSourceDataPath($tmpString);
-	    $gnuplotsrc = str_replace("src:$tmpString",$srcFileNamePath,$gnuplotsrc);
-        }
+	// get the name of the graph to be produced
+	$name = getOutputName ($gnuplotsrc);
+	$graphname = $dest . $name;
+	$fname = $graphname . ".tmp";
 
-	fwrite($handle, "\nset output '" . $graphname . "'\n");
-	// Remove the 'set output' command from the source as we will set it.
-	$gnuplotsrc = deleteOutputLine($gnuplotsrc);
+	// write the default settings and the input code from wiki into a 
+	// temporary file to be executed by gnuplot, then execute the command
+	if ( ! (file_exists($fname) || file_exists($fname . '.err'))) { 
+		$handle = fopen($fname, 'x');
+		if( $handle === FALSE )
+			// not sure what to do here, this shouldn't happen
+			return '<p><b><img src="' . htmlspecialchars($wgUploadPath . $gnuplotDir . $name) .
+				'" alt="Gnuplot Plot"></b></p>';
 
-	fwrite($handle, $gnuplotsrc . "\n");
-	fclose($handle);
+		// if terminal and size are not set in the gnuplot source we do it here
+		if (strpos($gnuplotsrc, 'set terminal ') === false) {
+			fwrite($handle, $wgGnuplotDefaultTerminal . "\n");
+		}
+		if (strpos($gnuplotsrc, 'set size ') === false) {
+			fwrite($handle, $wgGnuplotDefaultSize . "\n");
+		}
 
-	$cmdlinePlot = wfEscapeShellArg($wgGnuplotSettings->gnuplotCommand)
-	    . ' ' . $fname;
-	shell_exec($cmdlinePlot);
-	
-	// some cleanup
-       	unlink($fname);
-    }
+		// Need to find each occurance of src:<FILE NAME> and replace it 
+		// with the complete file path
+		while (strpos($gnuplotsrc, 'src:') != false) {
+			$srcStartPosition = strpos ($gnuplotsrc, 'src:') + strlen("src:");
+			$srcEndPosition = strpos ($gnuplotsrc, ' ', $srcStartPosition);
+			$tmpString = substr($gnuplotsrc, $srcStartPosition, $srcEndPosition-$srcStartPosition-1);
+			$srcFileNamePath = getSourceDataPath($tmpString);
+			$gnuplotsrc = str_replace("src:$tmpString",$srcFileNamePath,$gnuplotsrc);
+		}
 
-    return "<p><b><img src=\"" . $wgUploadPath . $gnuplotDir . $name .
-	"\" alt=\"Gnuplot Plot\"></b></p>" ;
+		fwrite($handle, "\nset output '" . $graphname . "'\n");
+		// Remove the 'set output' command from the source as we will set it.
+		$gnuplotsrc = preg_replace( '/^set output.*$/m', '', $gnuplotsrc );
+
+		fwrite($handle, $gnuplotsrc . "\n");
+		fclose($handle);
+
+		$cmdlinePlot = wfEscapeShellArg($wgGnuplotCommand)
+			. ' ' . wfEscapeShellArg( $fname );
+		shell_exec($cmdlinePlot);
+
+		// some cleanup
+		unlink($fname);
+	}
+
+	return '<p><b><img src="' . htmlspecialchars($wgUploadPath . $gnuplotDir . $name) .
+		'" alt="Gnuplot Plot"></b></p>';
 }
 
 /*** 
@@ -108,25 +105,27 @@ function renderGnuplot( $gnuplotsrc ) {
  * Output  : the file name to be used 
  */
 function getOutputName ( $gnuplotsrc ) {
-    // determine the file format of the plot - default is png
-    $format = "png";
-    $termpos = strpos($gnuplotsrc, "set terminal ");
-    if ($termpos === true) {
-      list( , ,$format, ) = split(" ", substr($gnuplotsrc, $termpos), 4);
-    }
+	// determine the file format of the plot - default is png
+	$format = "png";
+	$termpos = strpos($gnuplotsrc, "set terminal ");
+	if ($termpos === true) {
+		list( , ,$format, ) = split(" ", substr($gnuplotsrc, $termpos), 4);
+	}
 
-    $output = "";
-    $strlength = strlen("set output ");
-    $pos = strpos ($gnuplotsrc, "set output");
-    if (! $pos ) {	// If there is no output file specified
-	$output = md5($gnuplotsrc) . "." . $format;
-    } else {		// If the output file is directly specified
-	$posEnd = strpos($gnuplotsrc, "\n", $pos);
-	$output = substr($gnuplotsrc, 
-			 $pos + $strlength + 1, 
-			 $posEnd - $pos - $strlength - 2);
-    }
-    return $output;
+	$output = "";
+	$strlength = strlen("set output ");
+	$pos = strpos ($gnuplotsrc, "set output");
+	if (! $pos ) {	// If there is no output file specified
+		$output = md5($gnuplotsrc) . "." . $format;
+	} else {		// If the output file is directly specified
+		$posEnd = strpos($gnuplotsrc, "\n", $pos);
+		$output = substr($gnuplotsrc, 
+			$pos + $strlength + 1, 
+			$posEnd - $pos - $strlength - 2);
+	}
+	// remove /, otherwise users may possibly write to random places in the FS
+	$output = str_replace( '/', '', $output ); 
+	return $output;
 }
 
 /***
@@ -136,31 +135,7 @@ function getOutputName ( $gnuplotsrc ) {
  * Output  : the filesystem path for the file
  */
 function getSourceDataPath ( $name ) {
-    $h = Image::newFromName($name);
-
-    if (!$h->exists())
-        return null;
-    else
-        return $h->getImagePath(); 
-}
-
-/*** 
- * Function: deleteOutputLine
- * Purpose : Deletes the line starting with 'set output' from the gnuplot 
- *           source. If it does not exist this function has no effect.
- * Input   : $gnuplotsrc - the gnuplot input code from wiki
- * Output  : the file name to be used 
- */
-function deleteOutputLine ( $gnuplotsrc ) {
-    $strlength = strlen("set output ");
-    $pos = strpos ($gnuplotsrc, "set output");
-
-    if ($pos) {
-	$posEnd = strpos ($gnuplotsrc, "\n", $pos);
-
-	$gnuplotsrc = substr($gnuplotsrc, 0, $pos) 
-	    . substr($gnuplotsrc, $posEnd + 1, strlen($gnuplotsrc) - 1);
-     }
-   return $gnuplotsrc;
+	$h = Image::newFromName($name);
+	return $h->exists() ? $h->getImagePath() : null;
 }
 
