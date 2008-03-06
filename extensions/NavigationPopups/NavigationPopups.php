d565fa551a4fe7cbb9ac27c83c248152f5ac955a
sanbeg <sanbeg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-03-06 16:56:38 +0000

    loader for navigation popups
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31621 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/NavigationPopups/NavigationPopups.php
@@ -0,0 +1,45 @@
+<?php
+
+$wgHooks['OutputPageParserOutput'][] = 'efInstallPopups';
+
+function efInstallPopups( &$outputPage, $parserOutput) 
+{
+  global $wgJsMimeType, $wgScriptPath, $wgArticlePath;
+  global $wgRequest;
+
+  if ($wgRequest->getVal('popup') == 'no') return true;
+
+  $thispath = $wgScriptPath . '/extensions/NavigationPopups';
+  
+  #we need article path to be of the from /APATH$1, to get the APATH we need.
+  if (preg_match(':/(.+)/\$1$:', $wgArticlePath, $m)) {
+    $apath = $m[1];
+  }
+  if (preg_match(':/(.+)$:', $wgScriptPath, $m)) {
+    $bpath = $m[1];
+  }
+  
+  #add the stylesheet
+  $outputPage->addLink( 
+		       array( 
+			     'rel' => 'stylesheet', 
+			     'type' => 'text/css', 
+			     'href' => $thispath . '/navpop.css' 
+			     ) 
+		       );
+  #add the javascript functions
+  $outputPage->addScript( 
+			 "<script type=\"{$wgJsMimeType}\" src=\"{$thispath}/popups.js\">" .
+			 "</script>\n" 
+			 );
+  #local overrides
+  $outputPage->addScript(
+			 "<script type=\"{$wgJsMimeType}\">".
+			 "function siteArticlePath(){ return '$apath'; }".
+			 "function siteBotInterfacePath(){ return '$bpath'; }".
+			 "popupImages=false;".
+			 "</script>");
+  return true;
+}
+
+?>
