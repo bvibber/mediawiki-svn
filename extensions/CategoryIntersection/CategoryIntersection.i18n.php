3f9081a8d302d4b5689003a607e04c8f36d61f43
grondin <grondin@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-03-06 23:01:21 +0000

    * Update internationalization messages for french language * Adding Category Intersection extension to Translate extension tools
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31648 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CategoryIntersection/CategoryIntersection.i18n.php
+++ extensions/CategoryIntersection/CategoryIntersection.i18n.php
@@ -5,11 +5,22 @@ $messages['en'] = array(
         'categoryintersection' => 'Category Intersection',
         'categoryintersection_doit' => 'List pages in all these categories',
         'categoryintersection_maxcategories' => 'The maximum allowed number of intersecting categories is $1.',
-        'categoryintersection_results' => 'The search returned $1 title(s).',
-        );
+	'categoryintersection_results' => 'The search returned $1 {{PLURAL:$1|title|titles}}.',
+);
+
 $messages['de'] = array(
         'categoryintersection' => 'Kategorien-Schnittmenge',
         'categoryintersection_doit' => 'Alle Seiten in dieser Schnittmenge anzeigen',
         'categoryintersection_maxcategories' => 'Die maximale Anzahl von Kategorien für Schnittmengen ist $1.',
         'categoryintersection_results' => 'Die Suche ergab $1 Treffer.',
         );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'categoryintersection'               => 'Intersection de la catégorie',
+	'categoryintersection_doit'          => 'Liste les pages dans toutes ces catégories',
+	'categoryintersection_maxcategories' => 'Le nombre maximum du nombre des catégories s’entrecoupant est de $1.',
+	'categoryintersection_results'       => 'La recherche a retourné $1 {{PLURAL:$1|titre|titres}}.',
+);
