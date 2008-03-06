e0f9c368666caa5d7ac4b28448373edb4c76f89b
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-03-06 23:21:12 +0000

    Housekeeping: fix indentation, EOL whitespace, file headers, add credits and L10n for extension description
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@31649 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CategoryIntersection/CategoryIntersection.i18n.php
+++ extensions/CategoryIntersection/CategoryIntersection.i18n.php
@@ -1,26 +1,40 @@
-<?PHP
+<?php
+/**
+ * Internationalisation file for the extension Category Intersection
+ *
+ * @author Magnus Manske
+ * @addtogroup Extensions
+ */
 
 $messages = array();
-$messages['en'] = array( 
-        'categoryintersection' => 'Category Intersection',
-        'categoryintersection_doit' => 'List pages in all these categories',
-        'categoryintersection_maxcategories' => 'The maximum allowed number of intersecting categories is $1.',
-	'categoryintersection_results' => 'The search returned $1 {{PLURAL:$1|title|titles}}.',
+
+/** English
+ * @author Magnus Manske
+ */
+$messages['en'] = array(
+	'categoryintersection'               => 'Category Intersection',
+	'categoryintersection-desc'          => 'Maintains a table with hash values for [[Special:CategoryIntersection|category intersections]] within a page',
+	'categoryintersection-doit'          => 'List pages in all these categories',
+	'categoryintersection-maxcategories' => 'The maximum allowed number of intersecting categories is $1.',
+	'categoryintersection-results'       => 'The search returned $1 {{PLURAL:$1|title|titles}}.',
 );
 
+/** German (Deutsch)
+ * @author Magnus Manske
+ */
 $messages['de'] = array(
-        'categoryintersection' => 'Kategorien-Schnittmenge',
-        'categoryintersection_doit' => 'Alle Seiten in dieser Schnittmenge anzeigen',
-        'categoryintersection_maxcategories' => 'Die maximale Anzahl von Kategorien für Schnittmengen ist $1.',
-        'categoryintersection_results' => 'Die Suche ergab $1 Treffer.',
-        );
+	'categoryintersection'               => 'Kategorien-Schnittmenge',
+	'categoryintersection-doit'          => 'Alle Seiten in dieser Schnittmenge anzeigen',
+	'categoryintersection-maxcategories' => 'Die maximale Anzahl von Kategorien für Schnittmengen ist $1.',
+	'categoryintersection-results'       => 'Die Suche ergab $1 Treffer.',
+);
 
 /** French (Français)
  * @author Grondin
  */
 $messages['fr'] = array(
 	'categoryintersection'               => 'Intersection de la catégorie',
-	'categoryintersection_doit'          => 'Liste les pages dans toutes ces catégories',
-	'categoryintersection_maxcategories' => 'Le nombre maximum du nombre des catégories s’entrecoupant est de $1.',
-	'categoryintersection_results'       => 'La recherche a retourné $1 {{PLURAL:$1|titre|titres}}.',
+	'categoryintersection-doit'          => 'Liste les pages dans toutes ces catégories',
+	'categoryintersection-maxcategories' => 'Le nombre maximum du nombre des catégories s’entrecoupant est de $1.',
+	'categoryintersection-results'       => 'La recherche a retourné $1 {{PLURAL:$1|titre|titres}}.',
 );
