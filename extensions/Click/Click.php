845c6ca5952d5ba14f9271d0c1345cff2ef56b44
minuteelectron <minuteelectron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-03-29 14:31:15 +0000

    Click 1.4 - fixed regex and accessibility issues re. title attribute (props. Danny B.)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@32565 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Click/Click.php
+++ extensions/Click/Click.php
@@ -3,14 +3,14 @@
 /**
  * Click Extension
  *
- * dds a parser function to display an image with a link that leads to a page other than the image description page.
+ * Adds a parser function to display an image with a link that leads to a page other than the image description page.
  *
  * @addtogroup Extensions
  *
  * @link http://www.mediawiki.org/wiki/Extension:Click
  *
  * @author MinuteElectron <minuteelectron@googlemail.com>
- * @copyright Copyright © 2008 MinuteElectron.
+ * @copyright Copyright © 2008 MinuteElectron and Danny B.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
@@ -21,9 +21,9 @@ if( !defined( 'MEDIAWIKI' ) ) die;
 $wgExtensionCredits[ 'parserhook' ][] = array(
 	'name'        => 'Click',
 	'description' => 'Adds a parser function to display an image with a link that leads to a page other than the image description page.',
-	'author'      => 'MinuteElectron',
+	'author'      => array( 'MinuteElectron', 'Danny B.' ),
 	'url'         => 'http://www.mediawiki.org/wiki/Extension:Click',
-	'version'     => '1.3',
+	'version'     => '1.4',
 );
 
 // Setup function.
@@ -35,14 +35,14 @@ $wgHooks[ 'LanguageGetMagic' ][] = 'efClickParserFunction_Magic';
 function efClickParserFunction_Setup() {
 	global $wgParser;
 	// Register parser function hook.
-	$wgParser->setFunctionHook( 'Click', 'efClickParserFunction_Render' );
+	$wgParser->setFunctionHook( 'click', 'efClickParserFunction_Render' );
 	// Return true so things don't break.
 	return true;
 }
 
 function efClickParserFunction_Magic( &$magicWords, $langCode ) {
 	// Register parser function magic word.
-	$magicWords[ 'Click' ] = array( 0, 'Click' );
+	$magicWords[ 'click' ] = array( 0, 'click' );
 	// Return true so things don't break.
 	return true;
 }
@@ -51,11 +51,11 @@ function efClickParserFunction_Render( &$parser, $target = '', $image = '', $wid
 
 	// Width and alt-text are interchangable.
 	// No need to escape here, automatically done by Xml class functions.
-	if( preg_match( '#^[0|1|2|3|4|5|6|7|8|9]*px$#', $widthalt ) ) {
+	if( preg_match( '#^[0-9]+px$#', $widthalt ) ) {
 		// First value width, second alt.
 		$width = $widthalt;
 		$alt = $altwidth;
-	} elseif( preg_match( '#^[0|1|2|3|4|5|6|7|8|9]*px$#', $altwidth ) ) {
+	} elseif( preg_match( '#^[0-9]+px$#', $altwidth ) ) {
 		// First value alt, second width.
 		$alt = $widthalt;
 		$width = $altwidth;
@@ -68,9 +68,15 @@ function efClickParserFunction_Render( &$parser, $target = '', $image = '', $wid
 	// Open hyperlink, default to a on-wiki page, but if it doesn't exist and
 	// is a valid external URL then use it.
 	$targettitle = Title::newFromText( $target );
+	// Link title attribute (full page name).
+	if( $targettitle->getNamespace() !== 0 ) {
+		$title = $targettitle->getNsText() . ':' . $targettitle->getText();
+	} else {
+		$title = $targettitle->getText();
+	}
 	if( is_object( $targettitle ) && $targettitle->exists() ) {
 		// Internal link, open hyperlink and register internal link.
-		$r = Xml::openElement( 'a', array( 'href' => $targettitle->getLocalUrl(), 'title' => $alt ) );
+		$r = Xml::openElement( 'a', array( 'href' => $targettitle->getLocalUrl(), 'title' => $title ) );
 		$parser->mOutput->addLink( $targettitle );
 	} else {
 		// Internal page doesn't exist, test if external.
@@ -84,11 +90,11 @@ function efClickParserFunction_Render( &$parser, $target = '', $image = '', $wid
 		}
 		if( $ext ) {
 			// External link, open hyperlink with escaped href and register external link.
-			$r = Xml::openElement( 'a', array( 'href' => htmlspecialchars( $target ), 'title' => $alt ) );
+			$r = Xml::openElement( 'a', array( 'href' => $target, 'title' => $target ) );
 			$parser->mOutput->addExternalLink( $target );
 		} elseif( is_object( $targettitle ) ) {
 				// Valid internal link after all (but to non-existant page), open hyperlink and register internal link.
-				$r = Xml::openElement( 'a', array( 'class' => 'new', 'href' => $targettitle->getLocalUrl( 'action=edit&redlink=1' ), 'title' => $alt ) );
+				$r = Xml::openElement( 'a', array( 'class' => 'new', 'href' => $targettitle->getLocalUrl( 'action=edit&redlink=1' ), 'title' => wfMsg( 'red-link-title', $title ) ) );
 				$parser->mOutput->addLink( $targettitle );
 		}
 	}
