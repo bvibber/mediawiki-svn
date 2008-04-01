855b1aabb3a970ee6c452ed49886c2d56da07ba0
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-01 14:04:58 +0000

    Special page display name tweak
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@32659 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Mibbit/Mibbit.i18n.php
+++ extensions/Mibbit/Mibbit.i18n.php
@@ -11,10 +11,10 @@ $messages = array();
  * @author MinuteElectron <minuteelectron@googlemail.com>
  */
 $messages[ 'en' ] = array(
-	'mibbit'        => 'Mibbit',
+	'mibbit'        => 'Mibbit Chat',
 	'mibbit-desc'   => 'Adds a [[Special:Mibbit|special page]] used to chat in real time with other wiki users',
 	'mibbit-header' => 'This special page allows you to collaborate in real time with other users of the wiki.
 To connect simply press the "Connect" button.
 If you are logged in your nickname will have been prefilled.
-Anonymous users are given an automatically generated one, but it can be	changed.',
+Anonymous users are given an automatically generated one, but it can be changed.',
 );
