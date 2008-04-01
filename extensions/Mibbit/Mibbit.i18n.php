4487ade24e9a7853952906c810d319fe372e3f09
minuteelectron <minuteelectron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-01 13:18:30 +0000

    Committing Mibbit extension 1.0, integrates the Mibbit web IRC client into a special page.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@32655 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Mibbit/Mibbit.i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Internationalisation file for Mibbit extension.
+ * 
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author MinuteElectron <minuteelectron@googlemail.com>
+ */
+$messages[ 'en' ] = array(
+	'mibbit'        => 'Mibbit',
+	'mibbit-desc'   => 'Adds a special page used to chat in real time with other wiki users.',
+	'mibbit-header' => 'This special page allows you to collaborate in real time
+		with other users of the wiki.  To connect simply press the "Connect"
+		button, if you are logged in your nickname will have been prefilled -
+		anonymous users are given an automatically generated one, but it can be
+		changed.',
+);
