7a943fd2b9421274ebe86c92fbaa93260076ca14
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-07 13:06:51 +0000

    Add LocalFileLink extension, by Moisa Doru
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@32917 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/LocalFileLink/LocalFileLink.php
@@ -0,0 +1,46 @@
+<?php
+# localFileLink MediaWiki Extention
+# Created by  Doru Moisa, Optaros Inc.
+#
+
+$wgExtensionCredits['other'][] = array(
+	'name' => 'LocalFileLink',
+	'author' => 'Moisa Doru (tmoisa@optaros.com), Optaros Inc.',
+	'version' => preg_replace('/^.* (\d\d\d\d-\d\d-\d\d) .*$/', '\1', '$LastChangedDate: 2008-04-07 21:51:06 +1000 (Mon, 07 Apr 2008) $'), #just the date of the last change
+	'description' => 'Allows the usage of local file links with FCKEditor',
+);
+
+function lflAddLocalFileLinks(&$out) {
+    
+    $text = $out->mBodytext;
+    
+    $search         = '/\[(file\:\/{2,5}[a-zA-Z]{0,1}[\:]{0,1}\S*)\s*([^\]]*)\]/';
+    $replace        = '<a href="$1" class="link-ftp">$2</a>';
+
+    $text = preg_replace($search, $replace, $text);
+    
+    $out->mBodytext = $text;
+
+    return true;
+} 
+
+function lflAjaxAddLocalFileLinks(&$parser, &$text) {
+    
+    $search         = '/\[(file\:\/{2,5}[a-zA-Z]{0,1}[\:]{0,1}\S*)\s*([^\]]*)\]/';
+    $replace        = '<a href="$1" class="link-ftp">$2</a>';
+
+    $text = preg_replace($search, $replace, $text);
+    
+    return true;
+} 
+
+
+
+if ( isset($_REQUEST['rs']) ) {
+    $wgHooks['ParserBeforeTidy'][] = 'lflAjaxAddLocalFileLinks';
+}
+else {
+    $wgHooks['BeforePageDisplay'][] = 'lflAddLocalFileLinks';
+}
+
+?>
