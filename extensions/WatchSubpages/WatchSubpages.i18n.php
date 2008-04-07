fb4b8fca32308f9e0c1043651f4f5ff88cc6fae9
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-07 10:04:12 +0000

    Tweak extension description
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@32910 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WatchSubpages/WatchSubpages.i18n.php
+++ extensions/WatchSubpages/WatchSubpages.i18n.php
@@ -12,6 +12,6 @@ $messages = array();
  * @author http://www.strategywiki.org/wiki/User:Prod
  */
 $messages['en'] = array(
-                'watchsubpages-desc' => 'Quickly [[Special:WatchSubpages|add all subpages]] of a guide to the users watchlist',
+                'watchsubpages-desc' => 'Quickly [[Special:WatchSubpages|add all subpages]] of a page to the watchlist',
                 'watchsubpages'      => 'Watch Guide Subpages',
 );
