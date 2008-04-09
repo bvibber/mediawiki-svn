758a26d9bd68f01b239bad80aeb4a4c2e723bcd0
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-09 18:13:12 +0000

    (bug 13520) New OnlineStatus extension: allow users to show their status on their user pages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@33039 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/OnlineStatus/OnlineStatus.i18n.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Internationalisation file for extension UserStatus.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'onlinestatus-desc'             => 'Add a preference to show if the user is currently present or not on the wiki',
+	'onlinestatus-levels'           => '* online
+* offline', // Do not translate this message
+	'onlinestatus-subtitle-offline' => 'This user is currently offline',
+	'onlinestatus-subtitle-online'  => 'This user is currently online',
+	'onlinestatus-toggles-desc'     => 'Your status:',
+	'onlinestatus-toggles-explain'  => 'This allows you to show to other users if you are actually online or not by viewing your user page.',
+	'onlinestatus-toggles-show'     => 'Show online status on my user page',
+	'onlinestatus-toggle-offline'   => 'Offline',
+	'onlinestatus-toggle-online'    => 'Online',
+);
+
+/** French (Français)
+ * @author IAlex
+ */
+$messages['fr'] = array(
+	'onlinestatus-desc'             => 'Ajoute une préférence pour montrer si l\'utilisateur est présent ou non',
+	'onlinestatus-subtitle-offline' => 'Cet utilisateur est actuellement hors ligne',
+	'onlinestatus-subtitle-online'  => 'Cet utilisateur est actuellement en ligne',
+	'onlinestatus-toggles-desc'     => 'Votre statut&nbsp;:',
+	'onlinestatus-toggles-explain'  => 'Ceci permet aux autres utilisateur de savoir si vous êtes actuellement présent en regardant votre page utilisateur.',
+	'onlinestatus-toggles-show'     => 'Montrer mon statut sur ma page utilisateur',
+	'onlinestatus-toggle-offline'   => 'Absent',
+	'onlinestatus-toggle-online'    => 'Présent',
+);
