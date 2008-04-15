9fde31bfbfe62dd8b46379acca00ccf8485b1442
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-15 05:45:14 +0000

    $wgRCTypeGroupedLogs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@33327 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Newuserlog/Newuserlog.php
+++ extensions/Newuserlog/Newuserlog.php
@@ -25,14 +25,14 @@ $wgExtensionMessagesFiles['Newuserlog'] = dirname(__FILE__) . '/Newuserlog.i18n.
 
 function wfNewuserlog() {
 	# Add a new log type
-	global $wgLogTypes, $wgLogNames, $wgLogHeaders, $wgLogActions;
+	global $wgLogTypes, $wgLogNames, $wgLogHeaders, $wgLogActions, $wgRCTypeGroupedLogs;
 	$wgLogTypes[]                      = 'newusers';
 	$wgLogNames['newusers']            = 'newuserlogpage';
 	$wgLogHeaders['newusers']          = 'newuserlogpagetext';
 	$wgLogActions['newusers/newusers'] = 'newuserlogentry';
 	$wgLogActions['newusers/create']   = 'newuserlog-create-entry';
 	$wgLogActions['newusers/create2']  = 'newuserlog-create2-entry';
-
+	$wgRCTypeGroupedLogs[] = 'newusers';
 	# Run this hook on new account creation
 	global $wgHooks;
 	$wgHooks['AddNewAccount'][] = 'wfNewuserlogHook';
