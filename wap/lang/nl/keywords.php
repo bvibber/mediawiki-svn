4a2df189650f538065016ce09cb19dd7d2b9d650
nhuffschmid <nhuffschmid@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-24 18:09:04 +0000

    support of dutch language
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@33863 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ wap/lang/nl/keywords.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * english -> dutch translations of wikipedia keywords
+ * $Date: 2008/04/24 15:41:03 $
+ */
+
+$keywords = Array(
+"Go" => "Artikel", // submit button for article request
+"Special:Export" => "Speciaal:Exporteren",
+"Special:Search" => "Speciaal:Zoeken",
+'Image:'	=> 'Afbeelding:',
+'http://upload.wikimedia.org/wikipedia/'	=> 'http://upload.wikimedia.org/wikipedia/',
+'left'		=> 'left',
+'center'	=> 'center',
+'right'		=> 'right',
+'thumb'		=> 'thumb',
+'px'		=> 'px',
+"###end###"=>"###end###");
+
+?>
---
+++ wap/lang/nl/translations.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * english -> dutch translation file for hawpedia
+ * $Date: 2008/04/24 15:41:03 $
+ */
+
+$translation = Array(
+"About" => "Over",
+"Back" => "Terug",
+"Call" => "Bel",
+"Contents" => "Inhoud",
+"Continue" => "Doorgaan",
+"Copyright" => "Auteursrecht",
+"external link" => "Externe link",
+"for" => "voor",
+"Found articles for:" => "Artikels gevonden voor:",
+"Home" => "Homepage",
+"Language" => "Taal",
+"MP3 conversion failed" => "MP3 conversie mislukt",
+"No wikipedia article found for:" => "Geen wikipedia artikel gevonden voor:",
+"Not available" => "Niet beschikbaar",
+"please press" => "AUB druk op",
+"Save" => "Bewaren",
+"Settings" => "Instellingen",
+"Spoken Wikipedia" => "Gesproken Wikipedia",
+"Wikipedia currently not available" => "Wikipedia momenteel niet beschikbaar",
+"wikipedia export error" => "wikipedia exporteerfout",
+"###end###"=>"###end###");
+
+?>
