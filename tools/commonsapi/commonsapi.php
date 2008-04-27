e72a7d516f9f9fef05327075eda38c9306ec6eeb
magnusmanske <magnusmanske@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-04-27 21:10:51 +0000

    Many fixes: * Moved license data to wiki page * Additional info template is recognized * Improved location detection (geo-coordinates) * More options * Bugfixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@33950 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/commonsapi/commonsapi.php
+++ tools/commonsapi/commonsapi.php
@@ -6,57 +6,82 @@
   RUns at http://tools.wikimedia.de/~magnus/commonsapi.php
 */
 
-// Version
-$api_version = '0.9' ;
 
-// License detail data
-$license_data = array (
-  'GFDL' => array (
-    'full_name' => 'GNU Free Documentation License',
-    'attach_full_license_text' => '1',
-    'attribute_author' => '1',
-    'keep_under_same_license' => '1',
-    'keep_under_similar_license' => '0',
-    'license_logo_url' => 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Heckert_GNU_white.svg/64px-Heckert_GNU_white.svg.png',
-    'license_info_url' => 'http://www.gnu.org/copyleft/fdl.html',
-    'license_text_url' => 'http://www.gnu.org/licenses/fdl.txt',
-  ) ,
-  'CC-BY-2.5' => array (
-    'full_name' => 'Creative Commons Attribution V2.5',
-    'attach_full_license_text' => '0',
-    'attribute_author' => '1',
-    'keep_under_same_license' => '0',
-    'keep_under_similar_license' => '0',
-    'license_logo_url' => 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/79/CC_some_rights_reserved.svg/90px-CC_some_rights_reserved.svg.png',
-    'license_info_url' => 'http://creativecommons.org/licenses/by/2.5/',
-    'license_text_url' => 'http://creativecommons.org/licenses/by/2.5/legalcode',
-  ) ,
-  'PD' => array (
-    'full_name' => 'Public domain',
-    'attach_full_license_text' => '0',
-    'attribute_author' => '0',
-    'keep_under_same_license' => '0',
-    'keep_under_similar_license' => '0',
-    'license_logo_url' => 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/62/PD-icon.svg/64px-PD-icon.svg.png',
-  ) ,
-) ;
+/* USEFUL TEST IMAGES
+	ChathamHDY0016.JPG	                has location
+	Sa-warthog.jpg                      has many languages, featured
+	Gesammelte_Werke_(Thoma)_1_307.jpg  non-{{Information}} template
+*/
 
-$license_data['Public domain'] = $license_data['PD'] ;
-$license_data['Author died more than 70 years ago public domain images'] = $license_data['PD'] ;
-$license_data['Author died more than 70 years ago public domain images']['full_name'] = 'Public domain (author died more than 70 years ago)' ;
 
-// Do not include these categories in the list
-$ignore_categories = array (
-  'Media with locations',
-) ;
 
+$api_version = '0.9' ; // Version
+$api_data_page_url = 'http://commons.wikimedia.org/w/index.php?title=MediaWiki:Commons_API&action=raw' ; // URL of license data page
+
+$license_data = array () ; // License detail data
+$ignore_categories = array () ; // Do not include these categories in the list
+
+
+
+# Scans the raw text from http://commons.wikimedia.org/w/index.php?title=MediaWiki:Commons_API
+# and uses it as data
+function read_from_api_page () {
+  global $testing , $license_data , $ignore_categories , $api_data_page_url ;
+
+  $text = file_get_contents ( $api_data_page_url ) ;
+
+  $license_data = array () ;
+  $ignore_categories = array () ;
+  $h = array () ;
+  for ( $a = 0 ; $a < 9 ; $a++ ) $h[$a] = '' ;
+  $lines = explode ( "\n" , $text ) ;
+  foreach ( $lines AS $l ) {
+    $l = trim ( $l ) ;
+    if ( $l == '' ) continue ; # Ignore blank lines
+    
+    if ( substr ( $l , 0 , 1 ) == '=' ) { # Heading
+      $cnt = 0 ;
+      while ( substr ( $l , 0 , 1 ) == '=' ) {
+        $l = substr ( $l , 1 , strlen ( $l ) - 2 ) ;
+        $cnt++ ;
+      }
+      $l = trim ( $l ) ;
+      $l = str_replace ( '_' , ' ' , $l ) ;
+      if ( $cnt == 2 ) $l = strtolower ( $l ) ;
+      $h[$cnt] = $l ;
+      for ( $a = $cnt+1 ; $a < 9 ; $a++ ) $h[$a] = '' ;
+      continue ;
+    }
+    
+    if ( $h[2] == '' ) continue ; # No section level 2, ignore
+    
+    if ( $h[2] == 'license data' ) {
+      if ( substr ( $l , 0 , 1 ) != ';' ) continue ; # Something else...
+      $l = substr ( $l , 1 ) ;
+      $l = explode ( ':' , $l , 2 ) ;
+      $key = array_shift ( $l ) ;
+      $value = array_shift ( $l ) ;
+      $key = str_replace ( ' ' , '_' , $key ) ;
+      if ( !isset ( $license_data[$h[3]] ) ) $license_data[$h[3]] = array () ;
+      
+      if ( $key == 'based_on' ) {
+        $license_data[$h[3]] = $license_data[$value] ;
+      } else $license_data[$h[3]][$key] = $value ;
+    } elseif ( $h[2] == 'ignore categories' ) {
+      if ( substr ( $l , 0 , 1 ) != '*' ) continue ;
+      $l = substr ( $l , 1 ) ;
+      $l = str_replace ( '_' , ' ' , $l ) ;
+      $ignore_categories[] = trim ( $l ) ;
+    }
+  }
+}
 
 
 
 // Function to convert text in attributes and between tags to XML by changing some charactes to entities
 function myurlencode ( $text ) {
   $text = str_replace ( '<a href="/w' , '<a href="http://commons.wikimedia.org/w' , $text ) ;
-	return str_replace ( array ( '&', '"', "'", '<', '>', "'" ), array ( '&amp;' , '&quot;', '&apos;' , '&lt;' , '&gt;', '&apos;' ), $text );
+	return str_replace ( array ( '&', '"', "'", '<', '>' ), array ( '&amp;' , '&quot;', '&apos;' , '&lt;' , '&gt;' ), $text );
 }
 
 
@@ -83,27 +108,109 @@ function wiki2html ( &$wiki ) {
   $wiki = $wiki['parse']['text']['*'] ;
 }
 
+function print_tag ( $tag , $text ) {
+  if ( !isset ( $text ) ) print "<$tag/>" ;
+  else print "<$tag>" . myurlencode ( $text ) . "</$tag>" ;
+}
 
-// Fake user agent
-ini_set('user_agent','Commons API;');
+function get_same_level_html ( $t ) {
+  $cnt = 0 ;
+  for ( $pos = 0 ; $pos < strlen ( $t ) ; $pos++ ) {
+    if ( $t[$pos] == '<' && $t[$pos+1] == '/' && $cnt == 0 ) return substr ( $t , 0 , $pos - 1 ) ;
+    
+    if ( $t[$pos] == '<' ) {
+      if ( $t[$pos+1] == '/' ) $cnt-- ; // Closing
+      else if ( $t[$pos+1] == '!' ) ; // Comment
+      else $cnt++ ; // Opening
+    } else if ( $t[$pos] == '>' ) {
+      if ( $t[$pos-1] == '/' ) $cnt-- ; // Self-closing
+    }
+    
+  }
+  return $t ;
+}
 
-/* USEFUL TEST IMAGES
-	ChathamHDY0016.JPG	                has location
-	Sa-warthog.jpg                      has many languages, featured
-	Gesammelte_Werke_(Thoma)_1_307.jpg  non-{{Information}} template
-*/
+function try_information_template ( $text ) {
+  global $titles ;
+  $matches = array () ;
+  preg_match_all ( '/span\s+id="field-[^"]+"/' , $text , &$matches ) ;
+  $matches = $matches[0] ;
+  if ( count ( $matches ) == 0 ) return false ;
 
-// Get parameters
-$testing = isset ( $_REQUEST['test'] ) ;
-$img = $_REQUEST['image'] ;
-$force_html = isset ( $_REQUEST['forcehtml'] ) ;
-$thumb_width = $_REQUEST['thumbwidth'] ;
-$thumb_height = $_REQUEST['thumbheight'] ;
-if ( !$thumb_height || !$thumb_width ) $thumb_height = 0 ;
-if ( !$thumb_width ) $thumb_width = 0 ;
+  $ret = false ;
+  foreach ( $matches AS $m ) {
+    $t = array_pop ( explode ( $m , $text , 2 ) ) ;
+    $title = array () ;
+    preg_match ( '/\s+title="[^"]*"/' , $t , &$title ) ;
+    $title = $title[0] ;
+    $title = array_pop ( explode ( 'title="' , $title , 2 ) ) ;
+    $title = substr ( $title , 0 , -1 ) ;
+    $title = urldecode ( $title ) ;
+    
+    $k = explode ( 'field-' , $m ) ;
+    $k = str_replace ( '"' , '' , $k[1] ) ;
+    $titles[$k] = $title ;
+    $ret = true ;
+  }
+  wiki2html ( $titles['description'] ) ;
+  wiki2html ( $titles['date'] ) ;
+  wiki2html ( $titles['author'] ) ;
+  wiki2html ( $titles['source'] ) ;
+  wiki2html ( $titles['permission'] ) ;
+  wiki2html ( $titles['otherversions'] ) ;
+  return $ret ;
+}
 
-// Die with explanation if no image given
-if ( !isset ( $img ) ) {
+function try_fileinfo_template ( $text ) {
+  global $titles ;
+  $matches = array () ;
+  preg_match_all ( '/th[^>]+id="fileinfotpl_[^"]+"/' , $text , &$matches ) ;
+  $matches = $matches[0] ;
+
+  if ( count ( $matches ) == 0 ) return false ;
+
+#  header('Content-type: text/plain; charset=utf-8');
+  $ret = false ;
+  $t2 = array () ;
+  foreach ( $matches AS $m ) {
+    $k = array_pop ( explode ( 'fileinfotpl_' , $m , 2 ) ) ;
+    $k = array_shift ( explode ( '"' , $k , 2 ) ) ;
+    
+    $t = array_pop ( explode ( $m , $text , 2 ) ) ;
+    $t = array_pop ( explode ( '<td' , $t , 2 ) ) ;
+    $t = array_pop ( explode ( '>' , $t , 2 ) ) ;
+    
+    $t = get_same_level_html ( $t ) ;
+    
+    if ( substr ( $t , 0 , 3 ) == '<b>' ) $t = substr ( $t , 3 ) ;
+    if ( substr ( $t , -4 , 4 ) == '</b>' ) $t = substr ( $t , 0 , -4 ) ;
+
+    if ( isset ( $t2[$k] ) ) {
+      if ( $t2[$k] != $t ) $t2[$k] .= "\n$t" ;
+    } else $t2[$k] = $t ;
+    $ret = true ;
+  }
+  $titles['description'] = array() ;
+  $titles['date'] = array() ;
+  $titles['author'] = array() ;
+  $titles['source'] = array() ;
+  $titles['permission'] = array() ;
+  
+  foreach ( $t2 AS $k => $v ) {
+    if ( $k == 'aut' ) $titles['author'][] = $v ;
+    if ( $k == 'desc' ) $titles['description'][] = $v ;
+    if ( $k == 'date' ) $titles['date'][] = $v ;
+    if ( $k == 'src' ) $titles['source'][] = $v ;
+    if ( $k == 'perm' ) $titles['permission'][] = $v ;
+  }
+  
+  foreach ( $titles AS $k => $v ) $titles[$k] = implode ( "\n" , $v ) ;
+#  exit;
+  return $ret ;
+}
+
+
+function die_with_form () {
 	print "<h1>Wikimedia Commons API</h1>
 <hr/>
 Usage : <b>commonsapi.php?image=IMAGENAME</b><br/>
@@ -112,13 +219,54 @@ Optional parameters:
 <ul>
 <li><b>&thumbwidth=XX</b> returns a URL to a thumbnail maximal XX pixel wide</li>
 <li><b>&thumbheight=YY</b> returns a URL to a thumbnail maximal YY pixel high (requires <b>thumbwidth</b>)</li>
-<li><b>&forcehtml</b> converts wiki text into HTML (slower)</li>
+<li><b>&languages=en|de|default</b> returns only the languages for the listed codes; separate with \"|\" (default: all languages)</li>
+<li><b>&forcehtml</b> converts wiki text into HTML (slower, but gets rid of annoying wiki markup)</li>
+<li><b>&versions</b> adds information about all former versions of the file</li>
+<li><b>&meta</b> adds meta information that is stored with the file (e.g., EXIF data)</li>
 </ul>
-Example : <a href='commonsapi.php?image=Sa-warthog.jpg&thumbwidth=150&thumbheight=150'>Sa-warthog.jpg</a><br/>
-<i>Note:</i> All returned attributes and texts are entity-encoded.
+Examples :
+<ul>
+<li><a href='commonsapi.php?image=Sa-warthog.jpg&thumbwidth=150&thumbheight=150&versions&meta'>Sa-warthog.jpg</a> with thumbnail, version, and meta information</li>
+<li><a href='commonsapi.php?image=Yarra_Panorama.jpg&forcehtml'>Yarra_Panorama.jpg</a> (contains geocoding, which resolves to a <tt>&lt;location&gt;</tt> tag)</li>
+</ul>
+<i>Note:</i> All returned attributes and texts are entity-encoded (<i><tt>\"'<>&</tt></i> are replaced with XML entities).<br/><br/>
+
+(You want to help making this API better? Data source for license information is <a href=\"http://commons.wikimedia.org/wiki/MediaWiki:Commons_API\">here</a>. Bugs and feature requests go to <a href=\"http://commons.wikimedia.org/wiki/User_talk:Magnus_Manske\">this guy</a>.)
 " ;
 	exit ;
 }
+
+
+
+
+# ___________________
+# MAIN PROGRAM
+
+
+// Fake user agent
+ini_set('user_agent','Commons API;');
+
+// Get parameters
+$testing = isset ( $_REQUEST['test'] ) ;
+$versions = isset ( $_REQUEST['versions'] ) ;
+$meta = isset ( $_REQUEST['meta'] ) ;
+$img = $_REQUEST['image'] ;
+$force_html = isset ( $_REQUEST['forcehtml'] ) ;
+$get_languages = $_REQUEST['languages'] ;
+$thumb_width = $_REQUEST['thumbwidth'] ;
+$thumb_height = $_REQUEST['thumbheight'] ;
+if ( !$thumb_height || !$thumb_width ) $thumb_height = 0 ;
+if ( !$thumb_width ) $thumb_width = 0 ;
+if ( !isset ( $get_languages ) ) $get_languages = '' ;
+
+if ( substr ( strtolower ( $img ) , 0 , 6 ) == 'image:' ) $img = substr ( $img , 6 ) ;
+
+// Read in license information
+read_from_api_page () ;
+
+
+// Die with explanation if no image given
+if ( !isset ( $img ) ) die_with_form () ;
 
 // Prepare and read rendered image page
 $img = str_replace ( ' ' , '_' , $img ) ;
@@ -127,7 +275,6 @@ srand(time()) ;
 $text = file_get_contents ( $url."?".rand() ) ;
 
 // get file data via "normal" API
-// &iiurlwidth=100
 $ii_url = "http://commons.wikimedia.org/w/api.php?format=php&action=query&prop=imageinfo&iilimit=500&iiprop=timestamp|user|url|size|sha1|metadata&titles=Image:" . $img ;
 if ( $thumb_width != 0 ) $ii_url .= '&iiurlwidth=' . $thumb_width ;
 if ( $thumb_height != 0 ) $ii_url .= '&iiurlheight=' . $thumb_height ;
@@ -158,29 +305,11 @@ if ( !isset ( $filedata[0]['size'] ) ) $filedata[0]['size'] = 0 ;
 
 
 // Info table
-$matches = array () ;
-preg_match_all ( '/span\s+id="field-[^"]+"/' , $text , &$matches ) ;
-$matches = $matches[0] ;
 $titles = array () ;
-foreach ( $matches AS $m ) {
-	$t = array_pop ( explode ( $m , $text , 2 ) ) ;
-	$title = array () ;
-	preg_match ( '/\s+title="[^"]*"/' , $t , &$title ) ;
-	$title = $title[0] ;
-	$title = array_pop ( explode ( 'title="' , $title , 2 ) ) ;
-	$title = substr ( $title , 0 , -1 ) ;
-	$title = urldecode ( $title ) ;
-	
-	$k = explode ( 'field-' , $m ) ;
-	$k = str_replace ( '"' , '' , $k[1] ) ;
-	$titles[$k] = $title ;
-}
-wiki2html ( $titles['description'] ) ;
-wiki2html ( $titles['date'] ) ;
-wiki2html ( $titles['author'] ) ;
-wiki2html ( $titles['source'] ) ;
-wiki2html ( $titles['permission'] ) ;
-wiki2html ( $titles['otherversions'] ) ;
+
+$found = false ;
+if ( !$found ) $found = try_information_template ( $text ) ;
+if ( !$found ) $found = try_fileinfo_template ( $text ) ;
 
 // Set default description
 $desc = array () ;
@@ -220,9 +349,18 @@ foreach ( $matches AS $m ) {
 	}
 	
 	if ( !isset ( $desc[$m] ) ) $desc[$m] = $t ;
-	else $desc[$m] .= "<br/>\n$t" ;
+	else if ( $desc[$m] != $t ) $desc[$m] .= "<br/>\n$t" ;
 }
 if ( !isset ( $desc['default'] ) && isset ( $desc['en'] ) ) $desc['default'] = $desc['en'] ;
+
+if ( $get_languages != '' ) {
+  $gl = explode ( '|' , $get_languages ) ;
+  $d2 = array () ;
+  foreach ( $gl AS $l ) {
+    if ( isset ( $desc[$l] ) ) $d2[$l] = $desc[$l] ;
+  }
+  $desc = $d2 ;
+}
 
 
 // Categories
@@ -275,6 +413,8 @@ foreach ( $cats AS $cat ) {
 
 // Location
 $location = '' ;
+$lat = '' ;
+$lon = '' ;
 $matches = array () ;
 preg_match_all ( '/<span\s+class="latitude">([0-9\.]+)<\/span>([^<]*)/' , $text , &$matches ) ;
 if ( count ( $matches ) == 3 ) {
@@ -283,27 +423,24 @@ if ( count ( $matches ) == 3 ) {
 	preg_match_all ( '/<span\s+class="longitude">([0-9\.]+)<\/span>([^<]*)/' , $text , &$matches ) ;
 	if ( count ( $matches ) == 3 ) {
 		$lon = $matches[1][0] . $matches[2][0] ;
-		if ( $lat != '' && $lon != '' )
-			$location = '<location><lat>' . $lat . '</lat><lon>' . $lon . '</lon></location>' ;
 	}
 }
+
+if ( $lat . $lon == '' ) {
+  $matches = array () ;
+  preg_match_all ( '/<span\s+class="geo"[^>]*>([\-0-9\.]+)\s*;\s*([\-0-9\.]+)<\/span>/' , $text , &$matches ) ;
+  if ( count ( $matches ) == 3 ) {
+    $lat = $matches[1][0] ;
+    $lon = $matches[2][0] ;
+  }
+}
+
+if ( $lat != '' && $lon != '' ) $location = '<location><lat>' . $lat . '</lat><lon>' . $lon . '</lon></location>' ;
 
 // Fixes
 $justbools = array ( 'featuredpicture' , 'qualityimage' ) ;
 foreach ( $justbools AS $b ) {
 	if ( isset ( $titles[$b] ) ) $titles[$b] = '1' ;
-}
-
-// Test mode output
-if ( $testing ) {
-	print "<pre>" ;
-	print_r ( $data ) ;
-/*	print_r ( $titles ) ;
-	print_r ( $desc ) ;
-	print_r ( $cats ) ;
-	print_r ( $licenses ) ;*/
-	print "</pre>" ;
-	exit ;
 }
 
 // Now print this!
@@ -313,16 +450,21 @@ print '<response version="' . $api_version . '">' ;
 
 
 print '<file>' ;
-print '<name>' . myurlencode ( $img ) . '</name>' ;
+print '<name>' . str_replace ( '_' , ' ' , myurlencode ( $img ) ) . '</name>' ;
 print '<title>' . myurlencode ( "Image:$img" ) . '</title>' ;
 print '<urls>' ;
 print '<file>' . $file_url . '</file>' ;
 print '<description>' . myurlencode ( $url ) . '</description>' ;
 if ( $thumb_url != '' ) print '<thumbnail>' . myurlencode ( $thumb_url ) . '</thumbnail>' ;
 print '</urls>' ;
-print '<size>' . myurlencode ( $filedata[0]['size'] ) . '</size>' ;
-print '<width>' . myurlencode ( $filedata[0]['width'] ) . '</width>' ;
-print '<height>' . myurlencode ( $filedata[0]['height'] ) . '</height>' ;
+
+print_tag ( 'size' , $filedata[0]['size'] ) ;
+print_tag ( 'width' , $filedata[0]['width'] ) ;
+print_tag ( 'height' , $filedata[0]['height'] ) ;
+print_tag ( 'uploader' , $filedata[0]['user'] ) ;
+print_tag ( 'upload_date' , $filedata[0]['timestamp'] ) ;
+print_tag ( 'sha1' , $filedata[0]['sha1'] ) ;
+
 print $location ;
 foreach ( $titles AS $k => $v ) {
   if ( $k == 'potd' ) {
@@ -334,7 +476,7 @@ foreach ( $titles AS $k => $v ) {
 print '</file>' ;
 
 
-if ( count ( $metadata ) > 0 ) {
+if ( count ( $metadata ) > 0 and $meta ) {
   print '<meta>' ;
   foreach ( $metadata AS $k => $v ) {
     $k = strtolower ( $k ) ;
@@ -375,18 +517,18 @@ foreach ( $licenses AS $l ) {
 }
 print '</licenses>' ;
 
-
-print '<versions>' ;
-foreach ( $filedata AS $fd ) {
-  print '<version>' ;
-  foreach ( $fd AS $k => $v ) {
-    if ( $k == 'metadata' ) continue ;
-    print '<' . $k . '>' . myurlencode ( $v ) . '</' . $k . '>' ;
+if ( $versions ) {
+  print '<versions>' ;
+  foreach ( $filedata AS $fd ) {
+    print '<version>' ;
+    foreach ( $fd AS $k => $v ) {
+      if ( $k == 'metadata' ) continue ;
+      print '<' . $k . '>' . myurlencode ( $v ) . '</' . $k . '>' ;
+    }
+    print '</version>' ;
   }
-  print '</version>' ;
+  print '</versions>' ;
 }
-print '</versions>' ;
-
 
 print '</response>' ;
 
