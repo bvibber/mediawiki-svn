35e7500216b875c7e2cb56781f357efba599460b
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-04 19:29:38 +0000

    minnor fix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34225 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MetavidWiki/skins/mv_embed/mv_embed.js
+++ extensions/MetavidWiki/skins/mv_embed/mv_embed.js
@@ -1944,14 +1944,14 @@ if (typeof DOMParser == "undefined") {
 	     /*
 	      * IE and non-firebug debug:
 	      */	     
-	     var log_elm = document.getElementById('mv_js_log');
+	     /*var log_elm = document.getElementById('mv_js_log');
 	     if(!log_elm){
 	     	document.write('<div style="position:absolute;z-index:50;top:0px;left:0px;right:0px;height:150px;"><textarea id="mv_js_log" cols="80" rows="6"></textarea></div>');
 	     	var log_elm = document.getElementById('mv_js_log');
 	     }
 	     if(log_elm){
 	     	log_elm.value+=string+"\n";
-	     }
+	     }*/
 	   }
 	}
 //}
