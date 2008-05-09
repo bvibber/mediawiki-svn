8555a8cfcdafa57519b9d0c2aa5af3f7c017c4d6
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-09 08:17:28 +0000

    Localisation updates German
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34498 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SkinPerPage/SkinPerPage.i18n.php
+++ extensions/SkinPerPage/SkinPerPage.i18n.php
@@ -11,6 +11,13 @@ $messages['en'] = array(
 	'skinperpage-desc' => 'Adds <tt>&lt;skin&gt;</tt> tag to use different skins per page',
 );
 
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'skinperpage-desc' => 'Ergänzt das <tt>&lt;skin&gt;</tt>-Tag, um pro Seite unterschiedliche Skins zu nutzen',
+);
+
 $messages['fa'] = array(
 	'skinperpage-desc' => 'برچسب <tt>&lt;skin&gt;</tt> را اضافه می‌کند تا بتوان در هر صفحه از پوسته‌ای متفاوت استفاده کرد',
 );
