b818d73440829a54cd4363d35bf82e0dfe50dd83
skizzerz <skizzerz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-11 16:29:40 +0000

    Adding SimpleAntiSpam extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34618 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SimpleAntiSpam/SimpleAntiSpam.i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+$messages = array();
+
+/** English
+ * @author Ryan Schmidt
+ */
+$messages['en'] = array(
+ 'simpleantispam' => 'Your edit triggered the anti-spam mechanism',
+ 'simpleantispam-label' => 'Anti-spam check. Do <strong>NOT</strong> fill this in!',
+ 'simpleantispam-desc' => 'Adds a simple spam/bot check to forms',
+ );
