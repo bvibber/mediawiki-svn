b379b918d558c6e33bc44141ff1f0b858cc001e0
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-14 09:11:38 +0000

    Localisation updates for extension messages from Betawiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34786 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SimpleAntiSpam/SimpleAntiSpam.i18n.php
+++ extensions/SimpleAntiSpam/SimpleAntiSpam.i18n.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Internationalisation file for extension SimpleAntiSpam.
+ *
+ * @addtogroup Extensions
+ */
 
 $messages = array();
 
@@ -11,3 +16,73 @@ $messages['en'] = array(
 Do '''NOT''' fill this in!",
 	'simpleantispam-desc'  => 'Adds a simple spam/bot check to forms',
 );
+
+/** French (Français)
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'simpleantispam'       => 'Votre modification a déclenché le mécanisme anti-spam',
+	'simpleantispam-label' => "Vérification anti-spam.
+Ne '''RIEN''' inscrire dedans !",
+	'simpleantispam-desc'  => 'Ajoute au formulaire la vérification d’un bot spammeur',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'simpleantispam'       => 'Är Ännerung huet den anti-spam Mekanismus ausgeléist',
+	'simpleantispam-label' => "Anti-Spam Kontroll.
+Fëllt '''näischt''' hei ënnendrënner aus!",
+	'simpleantispam-desc'  => 'Setzt eng einfach Spam/Bot Kontroll a Formulairen derbäi',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'simpleantispam'       => 'Uw bewerking heeft het antispammechanisme in werking gesteld',
+	'simpleantispam-label' => "Antispamcontrole.
+Vul dit veld '''NIET''' in!",
+	'simpleantispam-desc'  => 'Voegt een eenvoudige spam/botcontrole toe aan formuleren',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ */
+$messages['no'] = array(
+	'simpleantispam'       => 'Redigeringen din utløste antispammekanismen',
+	'simpleantispam-label' => "Antispamsjekk.
+'''IKKE''' fyll inn dette feltet!",
+	'simpleantispam-desc'  => 'Legger til en enkel spam-/robot-sjekk på skjemaer',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'simpleantispam-label' => "Antispamová kontrola.
+'''NEVYPĹŇAJTE''' nasledovné!",
+	'simpleantispam-desc'  => 'Pridáva k formulárom jednoduchú ochranu prosti botom/spamu.',
+);
+
+/** Swedish (Svenska)
+ * @author M.M.S.
+ */
+$messages['sv'] = array(
+	'simpleantispam'       => 'Din redigering utlöste anti-spam mekanismen',
+	'simpleantispam-label' => "Anti-spam koll.
+Fyll '''INTE''' i det här!",
+	'simpleantispam-desc'  => 'Lägger till en enkel spam/robot koll till formulär',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Vinhtantran
+ */
+$messages['vi'] = array(
+	'simpleantispam'       => 'Sửa đổi của bạn đã kích hoạt cơ chế chống spam',
+	'simpleantispam-label' => "Kiểm tra chống spam.
+Xin '''ĐỪNG''' điền gì vào!",
+	'simpleantispam-desc'  => 'Thêm một kiểm tra spam/bot vào mẫu',
+);
+
