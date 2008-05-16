3097d6b73b5617eee75022bd39155860eeddbcef
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-16 20:56:39 +0000

    spamRegex: *standard coding style *author's last name & MediaWiki.org page url to extension credits
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34939 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SpamRegex/SpamRegexCore.php
+++ extensions/SpamRegex/SpamRegexCore.php
@@ -6,29 +6,29 @@
 
 global $wgHooks;
 /* initialize hook, FilterEdit is too far in code */
-$wgHooks['AlternateEdit'][] = 'wfGetSpamRegex' ;
-$wgHooks['EditFilter'][] = 'wfGetSummarySpamRegex' ;
+$wgHooks['AlternateEdit'][] = 'wfGetSpamRegex';
+$wgHooks['EditFilter'][] = 'wfGetSummarySpamRegex';
 
 function wfGetSpamRegex () {
 	global $wgSpamRegex;
 	if (!wfSimplifiedRegexCheckSharedDB())
-		return true ;
+		return true;
 
 	/* get here only the phrases for blocking in textbox */
-	$phrases = wfFetchSpamRegexData (SPAMREGEX_TEXTBOX) ;
-	("" != $phrases) ? $wgSpamRegex =  "/".$phrases."/i" : $wgSpamRegex = false ;
-	return true ;
+	$phrases = wfFetchSpamRegexData (SPAMREGEX_TEXTBOX);
+	("" != $phrases) ? $wgSpamRegex =  "/".$phrases."/i" : $wgSpamRegex = false;
+	return true;
 }
 
 function wfGetSummarySpamRegex ($editpage) {
 	global $wgOut;
 	if (!wfSimplifiedRegexCheckSharedDB())
-		return true ;
+		return true;
 
-	$matches = array() ;
+	$matches = array();
 	/* here we get only the phrases for blocking in summaries... */
-	$phrases = wfFetchSpamRegexData (SPAMREGEX_SUMMARY) ;
-	("" != $phrases) ? $m_phrases =  "/".$phrases."/i" : $m_phrases = false ;
+	$phrases = wfFetchSpamRegexData (SPAMREGEX_SUMMARY);
+	("" != $phrases) ? $m_phrases =  "/".$phrases."/i" : $m_phrases = false;
 	if ( $m_phrases && ($editpage->summary != '') && preg_match( $m_phrases, $editpage->summary, $matches ) ) {
 		/*	...so let's rock with our custom spamPage to indicate that
 			(since some phrases can be safely in the text and not in a summary
@@ -42,45 +42,45 @@ function wfGetSummarySpamRegex ($editpage) {
 		if ( $matches[0] ) {
 			$wgOut->addWikiText( wfMsg( 'spamprotectionmatch', "<nowiki>{$matches[0]}</nowiki>" ) );
 		}
-		$wgOut->addWikiText ( wfMsg ('spamregex_summary') ) ;
+		$wgOut->addWikiText ( wfMsg ('spamregex_summary') );
 
 		$wgOut->returnToMain( false );
 
-		return false ;
+		return false;
 	}
-	return true ;
+	return true;
 }
 
 function wfFetchSpamRegexData ($mode) {
-	global $wgMemc, $wgUser, $wgSpamRegex, $wgSharedDB ;
+	global $wgMemc, $wgUser, $wgSpamRegex, $wgSharedDB;
 
 	$phrases = "" ;
 	$first = true ;
 
 	/* first, check if regex string is already stored in memcache */
-	( $mode == SPAMREGEX_SUMMARY ) ? $key_clause = ":Summary" : $key_clause = ":Textbox" ;
-	$key = "$wgSharedDB:spamRegexCore:spamRegex" . $key_clause ;
-	$cached = $wgMemc->get ($key) ;
+	( $mode == SPAMREGEX_SUMMARY ) ? $key_clause = ":Summary" : $key_clause = ":Textbox";
+	$key = "$wgSharedDB:spamRegexCore:spamRegex" . $key_clause;
+	$cached = $wgMemc->get($key);
 	if ( !$cached ) {
 		/* fetch data from db, concatenate into one string, then fill cache */
-		( $mode == SPAMREGEX_SUMMARY ) ? $clause = " WHERE spam_summary = 1" : $clause = " WHERE spam_textbox = 1" ;
-		$dbr =& wfGetDB( DB_SLAVE ) ;
-		$query = "SELECT spam_text FROM " . wfSpamRegexGetTable() . $clause ;
-		$res = $dbr->query ($query) ;
+		( $mode == SPAMREGEX_SUMMARY ) ? $clause = " WHERE spam_summary = 1" : $clause = " WHERE spam_textbox = 1";
+		$dbr =& wfGetDB( DB_SLAVE );
+		$query = "SELECT spam_text FROM " . wfSpamRegexGetTable() . $clause;
+		$res = $dbr->query($query);
 		while ( $row = $dbr->fetchObject( $res ) ) {
-			$concat = $row->spam_text ;
+			$concat = $row->spam_text;
 			if (!$first) {
-				$phrases .= "|".$concat ;
+				$phrases .= "|".$concat;
 			} else {
-				$phrases .= $concat ;
-				$first = false ;
+				$phrases .= $concat;
+				$first = false;
 			}
 		}
-		$wgMemc->set ($key, $phrases, 0) ;
-		$dbr->freeResult ($res) ;
+		$wgMemc->set($key, $phrases, 0);
+		$dbr->freeResult($res);
 	} else {
 		/* take from cache */
-		$phrases = $cached ;
+		$phrases = $cached;
 	}
-	return $phrases ;
+	return $phrases;
 }
