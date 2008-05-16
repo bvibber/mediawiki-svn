542c0dacecedf5f43c47004d4a49f767ec806c3d
nhuffschmid <nhuffschmid@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-16 18:52:51 +0000

    redesign of buggy/incomprehensible function: remove_section()
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34930 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wap/hawpedia.php
+++ wap/hawpedia.php
@@ -12,43 +12,43 @@ function start_hawpedia_session()
 {
 	$sessionDeck = new HAW_deck();
 	$sessionDeck->enable_session();
-	ini_set('session.use_cookies', 0);    // do not use cookies
-	ini_set('session.use_trans_sid', 1);  // use transient sid support
+  ini_set('session.use_cookies', 0);    // do not use cookies
+  ini_set('session.use_trans_sid', 1);  // use transient sid support
 
-	if ($sessionDeck->ml == HAW_HTML) {
-		// remove form entry - see http://bugs.php.net/bug.php?id=13472
-		ini_set('url_rewriter.tags', 'a=href');
-	}
+  if ($sessionDeck->ml == HAW_HTML) {
+  	// remove form entry - see http://bugs.php.net/bug.php?id=13472
+    ini_set('url_rewriter.tags', 'a=href');
+  }
 
-	session_start();
-
-	determine_settings();
+  session_start();
+  
+  determine_settings();
 }
 
 function set_deck_properties(&$deck)
 {
 	$deck->set_charset("UTF-8");
-	$deck->set_width(HAWIKI_DISP_WIDTH);
-	$deck->set_height(HAWIKI_DISP_HEIGHT);
-	$deck->set_disp_bgcolor(HAWIKI_DISP_BGCOLOR);
-	$deck->use_simulator(HAWIKI_SKIN);
+  $deck->set_width(HAWIKI_DISP_WIDTH);
+  $deck->set_height(HAWIKI_DISP_HEIGHT);
+  $deck->set_disp_bgcolor(HAWIKI_DISP_BGCOLOR);
+  $deck->use_simulator(HAWIKI_SKIN);
 }
 
 function hawpedia_error($error_msg)
 {
-	$error_deck = new HAW_deck(HAWIKI_TITLE);
-	set_deck_properties($error_deck);
-	$error_text = new HAW_text($error_msg);
-	$error_deck->add_text($error_text);
-
-	$rule = new HAW_rule();
-	$error_deck->add_rule($rule);
-
-	$homelink = new HAW_link(hawtra("Home"), "index.php");
-	$error_deck->add_link($homelink);
-
-	$error_deck->create_page();
-	exit();
+  $error_deck = new HAW_deck(HAWIKI_TITLE);
+  set_deck_properties($error_deck);
+  $error_text = new HAW_text($error_msg);
+  $error_deck->add_text($error_text);
+  
+  $rule = new HAW_rule();
+  $error_deck->add_rule($rule);
+  
+  $homelink = new HAW_link(hawtra("Home"), "index.php");
+  $error_deck->add_link($homelink);
+  
+  $error_deck->create_page();
+  exit();
 }
 
 function validate_language($lang) {
@@ -126,19 +126,19 @@ function determine_settings()
 
 function hawtra($text)
 {
-	// translate given text	
+  // translate given text	
 
-	$translationFile = "lang/" . $_SESSION['language'] . "/translations.php";
-
-	if (!file_exists($translationFile))
-		return($text); // no translation possible
-
-	require($translationFile);
-
-	if (isset($translation[$text]))
-		return $translation[$text];
-	else
-		return $text; // no translation available
+  $translationFile = "lang/" . $_SESSION['language'] . "/translations.php";
+  
+  if (!file_exists($translationFile))
+    return($text); // no translation possible
+     
+  require($translationFile);
+  
+  if (isset($translation[$text]))
+    return $translation[$text];
+  else
+    return $text; // no translation available
 }
 
 function hawpptra($input)
@@ -160,20 +160,20 @@ function hawpptra($input)
 
 function translate_wikipedia_keyword($keyword) {
 
-	// translate language-specific wikipedia keyword
+  // translate language-specific wikipedia keyword
+  
+  if ($_SESSION['language'] == 'en')
+    return $keyword; // no translation needed
 
-	if ($_SESSION['language'] == 'en')
-		return $keyword; // no translation needed
-
-	$languageFile = "lang/" . $_SESSION['language'] . "/keywords.php";
-	if (!file_exists($languageFile))
-		die("file not found: " . $filename);
-	require($languageFile);
+  $languageFile = "lang/" . $_SESSION['language'] . "/keywords.php";
+  if (!file_exists($languageFile))
+    die("file not found: " . $filename);
+  require($languageFile);
 
 	if (!isset($keywords[$keyword]))
-		die("unknown keyword: " . $keyword);
-
-	return $keywords[$keyword];
+    die("unknown keyword: " . $keyword);
+    
+  return $keywords[$keyword];
 }
 
 function export_wikipedia($searchTerm)
@@ -181,9 +181,9 @@ function export_wikipedia($searchTerm)
 	$result = array();
 
 	$export_keyword = translate_wikipedia_keyword('Special:Export');
-
+	
 	$searchTerm = str_replace(" ", "_", $searchTerm); // blanks must become underscores
-
+	
 	// get wikipedia xml file
 	$ch = curl_init();
 	$url = "http://" . $_SESSION['language'] . ".wikipedia.org/wiki/" . $export_keyword . "/" . $searchTerm;
@@ -192,19 +192,19 @@ function export_wikipedia($searchTerm)
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$curlResultString = curl_exec($ch);
 	if (!is_string($curlResultString))
-		hawpedia_error(hawtra("Wikipedia currently not available")); // exits internally
+	  hawpedia_error(hawtra("Wikipedia currently not available")); // exits internally
 
 	curl_close($ch);
 
 	// determine page title
 	if (!preg_match("%<title>(.*)</title>%", $curlResultString, $matches))
-		return false; // search term not found
-
+	  return false; // search term not found
+	  
 	$result['title'] = $matches[1];
-
+	
 	// determine wiki text
 	if (!preg_match("/(<text [^>]*>)/", $curlResultString, $matches))
-		hawpedia_error(hawtra("wikipedia export error")); // exits internally
+	  hawpedia_error(hawtra("wikipedia export error")); // exits internally
 	$textStart = strpos($curlResultString, $matches[1]) + strlen($matches[1]);
 	$textEnd = strpos($curlResultString, "</text>");
 	$result['wikitext'] = substr($curlResultString, $textStart, $textEnd - $textStart);
@@ -232,11 +232,11 @@ function uri_exists($uri)
 function search_articles($article)
 {
 	// search related articles (after export has failed)
-
+	
 	$result = array();
 
 	$search_keyword = translate_wikipedia_keyword('Special:Search');
-
+	
 	$article = str_replace(" ", "_", $article); // blanks must become underscores
 
 	// get wikipedia search result (in html format)
@@ -247,76 +247,76 @@ function search_articles($article)
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$curlResultString = curl_exec($ch);
 	if (!is_string($curlResultString))
-		hawpedia_error(hawtra("Wikipedia currently not available")); // exits internally
+	  hawpedia_error(hawtra("Wikipedia currently not available")); // exits internally
 
 	curl_close($ch);
 
-	// extract article links from html
-	preg_match_all("%1em;\"><a href=\"/wiki/([^?\"]*)%", $curlResultString, $matches);
-
-	for ($i=0; $i < count($matches[1]); $i++) {
+  // extract article links from html
+  preg_match_all("%1em;\"><a href=\"/wiki/([^?\"]*)%", $curlResultString, $matches);
+  
+  for ($i=0; $i < count($matches[1]); $i++) {
 		// iterate over found articles (no category links!)
-		if (!strstr($matches[1][$i], ":"))
-			$result[] = $matches[1][$i];
+    if (!strstr($matches[1][$i], ":"))
+      $result[] = $matches[1][$i];
+      
+    if (count($result) >= 10)
+      break; // consider not more than 10 links 
+  }
 
-		if (count($result) >= 10)
-			break; // consider not more than 10 links 
-	}
-
-	if (count($result) == 0)
-		return 0; // nothing found
-	else
-		return $result;
+  if (count($result) == 0)
+    return 0; // nothing found
+  else
+    return $result;
 }
 
 function show_related_articles($articles, $searchterm)
 {
-	$search_deck = new HAW_deck(HAWIKI_TITLE);
-	set_deck_properties($search_deck);
+  $search_deck = new HAW_deck(HAWIKI_TITLE);
+  set_deck_properties($search_deck);
+  
+  // tell what this deck is about
+  $intro = new HAW_text(hawtra("Found articles for:") . " " . $searchterm);
+  $search_deck->add_text($intro);
 
-	// tell what this deck is about
-	$intro = new HAW_text(hawtra("Found articles for:") . " " . $searchterm);
-	$search_deck->add_text($intro);
+  // separate intro from link section 
+  $rule = new HAW_rule();
+  $search_deck->add_rule($rule);
+    
+  // create one link for each article 
+  foreach ($articles as $article) {
+    $article_link = new HAW_link(urldecode($article), "transcode.php?go=" . $article);
+    $search_deck->add_link($article_link);
+  }
+  
+  // add home link
+  $search_deck->add_rule($rule);
+  $homelink = new HAW_link(hawtra("Home"), "index.php");
+  $search_deck->add_link($homelink);
 
-	// separate intro from link section 
-	$rule = new HAW_rule();
-	$search_deck->add_rule($rule);
-
-	// create one link for each article 
-	foreach ($articles as $article) {
-		$article_link = new HAW_link(urldecode($article), "transcode.php?go=" . $article);
-		$search_deck->add_link($article_link);
-	}
-
-	// add home link
-	$search_deck->add_rule($rule);
-	$homelink = new HAW_link(hawtra("Home"), "index.php");
-	$search_deck->add_link($homelink);
-
-	$search_deck->create_page();
-	exit();
+  $search_deck->create_page();
+  exit();
 }
 
 function extract_chapter($wikitext, $chapter)
 {
 	if (!preg_match("/\n(==+)(\s?" . $chapter . "\s?==+)/", $wikitext, $matches))
-		return("invalid chapter"); // should never happen
-
+	  return("invalid chapter"); // should never happen
+	  
 	$chapterStart = strpos($wikitext, $matches[1] . $matches[2]);	
 
-	// search end of chapter
-	$chapterEnd = $chapterStart + strlen($chapter);
-	do {
-		// number of '=' characters must match exactly
-		$chapterEnd = strpos($wikitext, "\n" . $matches[1], $chapterEnd + 1);
-	}
-	while (($chapterEnd !== false) && (substr($wikitext, $chapterEnd + 1 + strlen($matches[1]), 1) == "="));
-
-	if ($chapterEnd !== false)
-		$wikitext = substr($wikitext, $chapterStart, $chapterEnd - $chapterStart);
-	else
-		$wikitext = substr($wikitext, $chapterStart);
-
+  // search end of chapter
+  $chapterEnd = $chapterStart + strlen($chapter);
+  do {
+  	// number of '=' characters must match exactly
+    $chapterEnd = strpos($wikitext, "\n" . $matches[1], $chapterEnd + 1);
+  }
+  while (($chapterEnd !== false) && (substr($wikitext, $chapterEnd + 1 + strlen($matches[1]), 1) == "="));
+  
+  if ($chapterEnd !== false)
+    $wikitext = substr($wikitext, $chapterStart, $chapterEnd - $chapterStart);
+  else
+    $wikitext = substr($wikitext, $chapterStart);
+  
 	return($wikitext);
 }
 
@@ -325,56 +325,51 @@ function remove_section($wikitext, $startStr, $endStr)
 	// remove all text within startStr and endStr (incl. limiters)
 	// preg_replace can cause problems as described here: http://bugs.php.net/bug.php?id=24460
 
-	while (true) {
-		// do for all sections in $wikitext ...
+  $index = 0;   // current position in wikitext
+  $counter = 0; // for nested sections
 
-		$secStart = strpos($wikitext, $startStr);	
-		if ($secStart === false)
-			break;
+  while (true) {
+  
+    // Guard against PHP whingeing with strpos start position out of range
+    if ((strlen($wikitext) < ($index + strlen($startStr))) ||
+        (strlen($wikitext) < ($index + strlen($startStr))))
+      break;
 
-		// consider nested limiters
-		$nest_count = 1;
-		$search_pos = $secStart + strlen($startStr);
-		$secEnd = false;
-		while (true) {
-			// Guard against PHP whinging with strpos start position out of range
-			if( $search_pos > strlen( $wikitext ) )
-				break;
-			if( $search_pos + strlen( $startStr ) > strlen( $wikitext ) )
-				break;
-			$start_pos = strpos($wikitext, $startStr, $search_pos);
-			$end_pos   = strpos($wikitext, $endStr,   $search_pos + strlen($startStr));
+    $startIndex = strpos($wikitext, $startStr, $index);  	
+    $endIndex   = strpos($wikitext, $endStr,   $index);
+    
+    if (($startIndex === false) && ($counter == 0))
+	    break; // we're done
+      	
+    if ($endIndex === false)
+	    break; // missing end - invalid syntax!
+	    
+	  if (($startIndex !== false) && ($startIndex < $endIndex)) {
+	  	// (sub-)section starts here
+	  	$index = $startIndex + strlen($startStr);
+	  	$counter++;
 
-			if (($start_pos !== false) && ($start_pos < $end_pos)) {
-				// nested section found
-				$search_pos = $start_pos + strlen($startStr);
-				$nest_count++;
-				continue;	
-			}
-
-			if ($end_pos !== false) {
-				$nest_count--;
-				if ($nest_count == 0) {
-					// end of section found
-					$secEnd = $end_pos;
-					break;
-				}
-				else {
-					// continue after end of nested section
-					$search_pos = $end_pos + strlen($endStr);
-				}
-			}
-			else
-				break; // invalid section syntax!
-			};
-
-		//remove section
-		if (($secStart !== false) && ($secEnd !== false)) {
-			$wikitext = substr($wikitext, 0, $secStart) . substr($wikitext, $secEnd + strlen($endStr));
-		}
-		else
-		break;
-	};
+	  	if ($counter == 1) {
+        // section starts here
+        $secStart = $startIndex;
+      }
+    }
+	  else {
+	  	// (sub-)section ends here
+	  	$index = $endIndex + strlen($endStr);
+	  	$counter--;
+	  	
+	  	if ($counter < 0)
+	  	  break; // endStr without startStr - invalid syntax!
+	  	
+	  	if ($counter == 0) {
+        // remove section
+        $wikitext = substr($wikitext, 0, $secStart) .
+                    substr($wikitext, $endIndex + strlen($endStr));
+        $index = $secStart;
+      }
+    }
+	}; // end of while loop
 
 	return($wikitext);
 }
@@ -401,13 +396,13 @@ function remove_controls($wikitext)
 function links2text($wikitext)
 {
 	// make [[wikilinks]] to wikilinks
-	$wikitext = preg_replace('/\[\[([^:\]]*\|)?([^:\]]*)\]\]/','${2}', $wikitext);
-
+  $wikitext = preg_replace('/\[\[([^:\]]*\|)?([^:\]]*)\]\]/','${2}', $wikitext);
+	
 	// disable detection of http links
-	$wikitext = preg_replace('/http/','h t t p ', $wikitext);
-	//$wikitext = preg_replace('@\[?http://\S*(.*?)\]?@','${1}', $wikitext);
-	//$wikitext = preg_replace('@\[?http://(\S*)\]?@','${1}', $wikitext);
-
+  $wikitext = preg_replace('/http/','h t t p ', $wikitext);
+  //$wikitext = preg_replace('@\[?http://\S*(.*?)\]?@','${1}', $wikitext);
+  //$wikitext = preg_replace('@\[?http://(\S*)\]?@','${1}', $wikitext);
+	
 	//echo $wikitext;
 	return($wikitext);	
 }
@@ -416,37 +411,37 @@ function split_wikitext($wikitext, $segLength)
 {
 	$result = array(); // init empty array
 
-	while(true) {
+  while(true) {
+  	
+	  $seg = substr($wikitext, 0, $segLength); // determine maximum segment
+	  
+	  if (strlen($seg) < $segLength) {
+	    // end of text
+  	  $result[] = $seg; // add last array element
+	    break;  	
+    }
+    
+	  $crPos = strrpos($seg, "\n"); // find previous new line
 
-		$seg = substr($wikitext, 0, $segLength); // determine maximum segment
+    if ($crPos === false) {
+      // no newline found in segment, find next new line
+  	  $crPos = strpos($wikitext, "\n", $segLength);
+    }
+      
+    if (($crPos === false) || ($crPos == 0)) {
+      // no newline in whole text
+      $crPos = strlen($wikitext); // consider whole text
+    }
 
-		if (strlen($seg) < $segLength) {
-			// end of text
-			$result[] = $seg; // add last array element
-			break;  	
-		}
-
-		$crPos = strrpos($seg, "\n"); // find previous new line
-
-		if ($crPos === false) {
-			// no newline found in segment, find next new line
-			$crPos = strpos($wikitext, "\n", $segLength);
-		}
-
-		if (($crPos === false) || ($crPos == 0)) {
-			// no newline in whole text
-			$crPos = strlen($wikitext); // consider whole text
-		}
-
-		$seg = substr($wikitext, 0, $crPos + 1); // determine segment
-		$result[] = $seg; // add array element
-		$wikitext = substr($wikitext, $crPos + 1); // continue with new line
-
-		if ($crPos == 0)
-			exit;
-	}
-
-	return $result;
+ 	  $seg = substr($wikitext, 0, $crPos + 1); // determine segment
+	  $result[] = $seg; // add array element
+	  $wikitext = substr($wikitext, $crPos + 1); // continue with new line
+	  
+	  if ($crPos == 0)
+	    exit;
+  }
+  
+  return $result;
 }
 
 function save_url()
@@ -461,5 +456,5 @@ function save_url()
 	  fclose($fp);
   }
 }
-
+		
 ?>
