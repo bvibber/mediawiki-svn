003c13acab154eda7bdbbe47befcfcf4727a77f5
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-17 14:07:37 +0000

    Imagetabs: *standard coding style *proper copyright symbol as per http://en.wikipedia.org/wiki/Copyright_symbol
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@34979 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Imagetabs/imagetabs.php
+++ extensions/Imagetabs/imagetabs.php
@@ -6,7 +6,7 @@
  * (at your option) any later version.
  *
  * @author Roan Kattouw <roan.kattouw@home.nl>
- * @copyright Copyright (C) 2007 Roan Kattouw 
+ * @copyright Copyright © 2007 Roan Kattouw 
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
  *
  * An extension that shows interwiki tabs above Image: pages
@@ -19,26 +19,22 @@
 $wgExtensionFunctions[] = 'createImageTabs_setup';
 $wgExtensionCredits['other'][] = array(
 	'name' => 'Imagetabs',
-	'author' => 'Joe Beaudoin Jr. and Roan Kattouw',
+	'author' => array('Joe Beaudoin Jr.', 'Roan Kattouw'),
 	'description' => 'Adds tabs with interwiki links above Image: pages',
 	'version' => '1.0',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:Imagetabs'
 );
 
-function createImageTabs_setup()
-{
+function createImageTabs_setup() {
 	global $wgHooks;
 	$wgHooks['SkinTemplateContentActions'][] = 'createImageTabs_hook';
 }
 
-function createImageTabs_hook(&$content_actions)
-{
+function createImageTabs_hook(&$content_actions) {
 	global $wgEnableInterwikiImageTabs, $wgInterwikiImageTabs, $wgTitle, $wgLocalInterwiki;
-	if($wgEnableInterwikiImageTabs && $wgTitle->getNamespace() == NS_IMAGE)
-	{
+	if($wgEnableInterwikiImageTabs && $wgTitle->getNamespace() == NS_IMAGE) {
 		$i = 0;
-		foreach($wgInterwikiImageTabs as $prefix => $caption)
-		{
+		foreach($wgInterwikiImageTabs as $prefix => $caption) {
 			// Go to prefix:Image:title. Image: is automatically translated if necessary.
 			$titleObj = Title::newFromText($prefix . ":Image:" . $wgTitle->getText());
 			// Check that we don't link to ourselves
