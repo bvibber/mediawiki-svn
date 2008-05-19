26e7a9183097353daff6e6dd678808c180742267
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-05-19 21:22:34 +0000

    * article -> page * email -> e-mail
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@35057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EmailArticle/EmailArticle.i18n.php
+++ extensions/EmailArticle/EmailArticle.i18n.php
@@ -11,22 +11,22 @@ $messages = array();
  * @author Nad
  */
 $messages['en'] = array(
-	'emailarticle'        => 'Email article',
-	'ea-desc'             => 'Send rendered HTML article to an email address or list of addresses using [http://phpmailer.sourceforge.net phpmailer].',
-	'ea-heading'          => "=== Emailing [[$1]] article ===",
-	'ea-noarticle'        => "Please specify an article to send, for example [[Special:EmailArticle/Main Page]].",
-	'ea-norecipients'     => "No valid email addresses found!",
+	'emailarticle'        => 'E-mail page',
+	'ea-desc'             => 'Send rendered HTML page to an e-mail address or list of addresses using [http://phpmailer.sourceforge.net phpmailer].',
+	'ea-heading'          => "=== E-mailing [[$1]] page ===",
+	'ea-noarticle'        => "Please specify a page to send, for example [[Special:EmailArticle/Main Page]].",
+	'ea-norecipients'     => "No valid e-mail addresses found!",
 	'ea-listrecipients'   => "=== List of $1 {{PLURAL:$1|recipient|recipients}} ===",
 	'ea-error'            => "'''Error sending [[$1]]:''' ''$2''",
-	'ea-sent'             => "Article [[$1]] sent successfully to '''$2''' {{PLURAL:$2|recipient|recipients}} by [[User:$3|$3]].",
+	'ea-sent'             => "Page [[$1]] sent successfully to '''$2''' {{PLURAL:$2|recipient|recipients}} by [[User:$3|$3]].",
 	'ea-selectrecipients' => 'Select recipients',
 	'ea-compose'          => 'Compose content',
-	'ea-selectlist'       => "Additional recipients as article titles or email addresses
+	'ea-selectlist'       => "Additional recipients as page titles or e-mail addresses
 *''separate items with , ; * \\n
 *''list can contain templates and parser-functions''",
 	'ea-show'             => 'Show recipients',
 	'ea-send'             => 'Send!',
-	'ea-subject'          => 'Enter a subject line for the email',
+	'ea-subject'          => 'Enter a subject line for the e-mail',
 	'ea-header'           => 'Prepend content with optional message (wikitext)',
 	'ea-selectcss'        => 'Select a CSS stylesheet',
 );
