04279dd88fbf6e5e512909c2080151dc1e47c22e
rotem <rotem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-02 13:47:56 +0000

    Tab0 extension by Agbad, slightly edited.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@35712 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Tab0/Tab0.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for extension Tab0.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'tab0-desc' => 'Adds "0" tab, linking to the edit page of the first section of the page',
+);
+
+$messages['he'] = array(
+	'tab0-desc' => 'הוספת לשונית "0" המקשרת לעריכת הפסקה הראשונה בדף',
+);
