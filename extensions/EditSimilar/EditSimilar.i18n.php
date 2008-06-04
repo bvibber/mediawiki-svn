726f7be96fc8bf9ef21ecf8c5eceb94f9e31dd5c
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-04 18:53:31 +0000

    EditSimilar: *standard coding style *remove some unused globals *tweak extension credits *add Finnish i18n
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@35873 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EditSimilar/EditSimilar.i18n.php
+++ extensions/EditSimilar/EditSimilar.i18n.php
@@ -1,36 +1,61 @@
 <?php
 /**
- * Internationalisation file for extension EditSimilar
+ * Internationalisation file for the EditSimilar extension
  *
- * @addtogroup Extensions
+ * @ingroup Extensions
  */
 
 $messages = array();
 
+/** English
+ * @author Bartek Łapiński
+ * @author Łukasz 'TOR' Garczewski
+ */
 $messages['en'] = array(
 	'editsimilar-thanks'                         => 'Thank you for your contribution.
-Check out these related pages: $1.' ,
+Check out these related pages: $1.',
 	'editsimilar-thanks-singleresult'            => 'Thank you for your contribution.
-Check out this related page: $1.' ,
+Check out this related page: $1.',
 	'editsimilar-thanks-notsimilar'              => 'Thank you for your contribution.
 These pages could also use your help: $1.',
 	'editsimilar-thanks-notsimilar-singleresult' => 'Thank you for your contribution.
 This page could also use your help: $1.',
-	'editsimilar-thankyou'                       => 'Thank you for your contribution, $1!' ,
-	'editsimilar-link-disable'                   => 'set preferences' ,
-	'tog-edit-similar'                           => 'Enable similar pages suggestions' ,
+	'editsimilar-thankyou'                       => 'Thank you for your contribution, $1!',
+	'editsimilar-link-disable'                   => 'set preferences',
+	'tog-edit-similar'                           => 'Enable similar pages suggestions',
 );
 
+/** Finnish (Suomi)
+ * @author Jack Phoenix
+ */
+$messages['fi'] = array(
+	'editsimilar-thanks'                         => 'Kiitos muokkauksestasi.
+Katso nämä aiheeseen liittyvät sivut: $1.',
+	'editsimilar-thanks-singleresult'            => 'Kiitos muokkauksestasi.
+Katso tämä aiheeseen liittyvä sivu: $1.',
+	'editsimilar-thanks-notsimilar'              => 'Kiitos muokkauksestasi.
+Nämä sivut voisivat myös kaivata apuasi: $1.',
+	'editsimilar-thanks-notsimilar-singleresult' => 'Kiitos muokkauksestasi.
+Tämä sivu voisi myös kaivata apuasi: $1.',
+	'editsimilar-thankyou'                       => 'Kiitos muokkauksestasi, $1!',
+	'editsimilar-link-disable'                   => 'aseta asetukset',
+	'tog-edit-similar'                           => 'Ota käyttöön samanlaisten sivujen ehdotukset',
+);
+
+/** Polish (Polski)
+ * @author Bartek Łapiński
+ * @author Łukasz 'TOR' Garczewski
+ */
 $messages['pl'] = array(
 	'editsimilar-thanks'                         => 'Dzięki za edycję!
-Zerknij też na podobne artykuły: $1.' ,
+Zerknij też na podobne artykuły: $1.',
 	'editsimilar-thanks-singleresult'            => 'Dzięki za edycję!
-Zerknij też na ten podobny artykuł: $1.' ,
+Zerknij też na ten podobny artykuł: $1.',
 	'editsimilar-thanks-notsimilar'              => 'Dzięki za edycję!
 Te artykuły również mogą potrzebować twojej pomocy: $1.',
 	'editsimilar-thanks-notsimilar-singleresult' => 'Dzięki za edycję!
 Ten artykuł również może potrzebować twojej pomocy: $1.',
-	'editsimilar-thankyou'                       => 'Dzięki za Twoją edycję, $1!' ,
-	'editsimilar-link-disable'                   => 'zmień ustawienia' ,
-	'tog-edit-similar'                           => 'Włącz sugestie edycji podobnych artykułów' ,
+	'editsimilar-thankyou'                       => 'Dzięki za Twoją edycję, $1!',
+	'editsimilar-link-disable'                   => 'zmień ustawienia',
+	'tog-edit-similar'                           => 'Włącz sugestie edycji podobnych artykułów',
 );
