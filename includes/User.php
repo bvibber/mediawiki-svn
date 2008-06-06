bvibber/mediawiki-svn

commit 9994021df58bfe7180aa64685e8e43036564fe55

(bug 12859) Deprecate $wgRateLimitsExcludedGroups in favor of adding the 'noratelimit' user right. Doing it properly this time, so old functionality is still intact.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@35908 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/User.php
@@ -1102,7 +1102,13 @@ class User {
 	 */
 	public function isPingLimitable() {
 		global $wgRateLimitsExcludedGroups;
-		return array_intersect($this->getEffectiveGroups(), $wgRateLimitsExcludedGroups) == array();
+		if ( array_intersect($this->getEffectiveGroups(), $wgRateLimitsExcludedGroups) == array() || 
+			!$this->isAllowed('noratelimit') ) {
+				return true;
+			}
+		else {
+			return false;
+		}
 	}
 
 	/**

RELEASE-NOTES
@@ -52,6 +52,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * $wgActiveUserEditCount sets the number of edits that must be performed over
   a certain number of days to be considered active
 * $wgActiveUserDays is that number of days
+* $wgRateLimitsExcludedGroups has been deprecated in favor of 
+  $wgGroupPermissions[]['noratelimit']. The former still works, however.
 
 === New features in 1.13 ===
 
@@ -335,6 +337,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 14386) Fix subpage namespace oddity when moving a talk page
 * (bug 11771) Signup form now not shown if in read-only mode.
 * (bug 10080) Users can now modify an existing block without unblocking first.
+* (bug 12859) $wgRateLimitsExcludedGroups has been deprecated in favor of
+  $wgGroupPermissions[]['noratelimit']. 
 
 === API changes in 1.13 ===
 

includes/DefaultSettings.php
@@ -1140,10 +1140,12 @@ $wgGroupPermissions['sysop']['markbotedits']     = true;
 $wgGroupPermissions['sysop']['suppressredirect'] = true;
 $wgGroupPermissions['sysop']['apihighlimits']    = true;
 $wgGroupPermissions['sysop']['browsearchive']    = true;
+$wgGroupPermissions['sysop']['noratelimit']      = true;
 #$wgGroupPermissions['sysop']['mergehistory']     = true;
 
 // Permission to change users' group assignments
 $wgGroupPermissions['bureaucrat']['userrights']  = true;
+$wgGroupPermissions['bureaucrat']['noratelimit'] = true;
 // Permission to change users' groups assignments across wikis
 #$wgGroupPermissions['bureaucrat']['userrights-interwiki'] = true;
 
@@ -2848,8 +2850,14 @@ $wgRateLimitLog = null;
 
 /**
  * Array of groups which should never trigger the rate limiter
+ * 
+ * @deprecated as of 1.13.0, the preferred method is using
+ *  $wgGroupPermissions[]['noratelimit']. However, this will still
+ *  work if desired.
+ *  
+ *  $wgRateLimitsExcludedGroups = array( 'sysop', 'bureaucrat' );
  */
-$wgRateLimitsExcludedGroups = array( 'sysop', 'bureaucrat' );
+
 
 /**
  * On Special:Unusedimages, consider images "used", if they are put

commit 14d3cdab5075522ccc7baa1070b471011d23edd3

* (bug 14424) Fix regression in $wgRateLimitsExcludedGroups support
Fixes logic error introduced in r35908.



git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/phase3@35970 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/User.php
@@ -1102,13 +1102,11 @@ class User {
 	 */
 	public function isPingLimitable() {
 		global $wgRateLimitsExcludedGroups;
-		if ( array_intersect($this->getEffectiveGroups(), $wgRateLimitsExcludedGroups) == array() || 
-			!$this->isAllowed('noratelimit') ) {
-				return true;
-			}
-		else {
+		if( array_intersect( $this->getEffectiveGroups(), $wgRateLimitsExcludedGroups ) ) {
+			// Deprecated, but kept for backwards-compatibility config
 			return false;
 		}
+		return !$this->isAllowed('noratelimit');
 	}
 
 	/**