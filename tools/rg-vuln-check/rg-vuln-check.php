86cc06072bcfc5a93ff96bebd2a9b36f491e7ece
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-14 09:43:12 +0000

    Scanner for classic register_globals vulnerabilities. Also detects some kinds of plain XSS vulnerability.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36281 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/rg-vuln-check/rg-vuln-check.php
@@ -0,0 +1,209 @@
+#!/usr/bin/env php
+<?php
+
+if ( php_sapi_name() != 'cli' ) {
+	echo "This script must be run on the command line\n";
+	exit( 1 );
+}
+
+$options = array();
+
+if ( in_array( '-v', $argv ) ) {
+	$options['verbose'] = true;
+	$argv = array_diff( $argv, array( '-v' ) );
+}
+if ( in_array( '--opcodes', $argv ) ) {
+	$options['opcodes'] = true;
+	$argv = array_diff( $argv, array( '--opcodes' ) );
+}
+
+if ( count( $argv ) <= 1 ) {
+	echo "Usage: php {$argv[0]} [-v] [--opcodes] <filename> [<filename> ...]\n";
+	exit( 1 );
+}
+
+$confFile = dirname( __FILE__ ) . '/conf.php';
+if ( !file_exists( $confFile ) ) {
+	echo "Configuration file not found\n";
+	echo "Copy conf.php.sample to conf.php, and change the settings to suit your installation.\n";
+	exit( 1 );
+}
+
+$cvc = new ClassicVulnerabilityCheck( $options );
+$cvc->readConf( $confFile );
+
+array_shift( $argv );
+$good = true;
+foreach ( $argv as $file ) {
+	$good = $good && $cvc->check( $file );
+}
+
+exit( $good ? 0 : 1 );
+
+class ClassicVulnerabilityCheck {
+	/**
+	 * Set this to the base URL where all the scripts to be tested are kept. It 
+	 * should be configured with register_globals, display_errors and open_basedir 
+	 * enabled.
+	 */
+	var $jailDir = '/home/tstarling/src/mediawiki';
+
+	/**
+	 * Set this to the local directory corresponding to the URL
+	 */
+	var $jailUrl = 'http://shimmer/jail';
+
+	/*
+	 * Be verbose
+	 */
+	var $verbose = false;
+
+	/**
+	 * Dump parsekit output
+	 */
+	var $opcodes = false;
+
+	function __construct( $options ) {
+		foreach ( $options as $name => $value ) {
+			$this->$name = $value;
+		}
+	}
+
+	function readConf( $filename ) {
+		$jailDir = $this->jailDir;
+		$jailUrl = $this->jailUrl;
+		include( $filename );
+		$this->jailDir = $jailDir;
+		$this->jailUrl = $jailUrl;
+	}
+
+	function compile( $filename ) {
+		// This call needs to be in its own function, otherwise it segfaults
+		return array( $parseInfo, $errors );
+	}
+
+	function check( $filename ) {
+		$errors = false;
+		$parseInfo = parsekit_compile_file( $filename, $errors );
+
+		if ( $errors ) {
+			echo "Errors encountered:\n";
+			print_r( $errors );
+			return false;
+		}
+
+		if ( $this->opcodes ) {
+			var_dump( $parseInfo );
+		}
+
+		$globals = array_keys( $this->getGlobalsFromParseInfo( $parseInfo ) );
+
+		if ( !$globals ) {
+			if ( $this->verbose ) {
+				print "$filename: SECURE (No globals referenced)\n";
+			}
+			return true;
+		}
+
+		if ( $this->verbose ) {
+			print "Globals referenced: " . implode( ', ', $globals ) . "\n";
+		}
+
+		$filename = realpath( $filename );
+		if ( substr( $filename, 0, strlen( $this->jailDir ) + 1 ) !== $this->jailDir . '/' ) {
+			echo "The file specified is not in the jail directory\n";
+			return false;
+		}
+
+		$ret = true;
+
+		// Need to do each global separately because some globals will 
+		// generate a fatal when set to a string, masking the vulnerability
+		foreach ( $globals as $i => $global ) {
+			$url = $this->jailUrl . substr( $filename, strlen( $this->jailDir ) ) . '?';
+			$url .= urlencode( $global ) . '=' . urlencode( "/TEST_GLOBAL_$i<>" );
+			if ( $this->verbose ) {
+				echo "Fetching $url\n";
+			}
+
+			$curl = curl_init( $url );
+			curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+			$html = curl_exec( $curl );
+			$lines = explode( "\n", $html );
+
+			if ( $this->verbose ) {
+				echo str_repeat( '-', 76 );
+				echo "\n$html\n";
+				echo str_repeat( '-', 76 ) . "\n";
+			}
+			
+			if ( strpos( $html, "TEST_GLOBAL_$i<>" ) ) {
+				echo "$filename XSS VULNERABILITY \$$global\n";
+				$ret = false;
+			}
+			foreach ( $lines as $line ) {
+				if ( preg_match( "/TEST_GLOBAL_$i.*failed to open stream/", $line ) ) {
+					echo "$filename INCLUSION VULNERABILITY \$$global\n";
+					$ret = false;
+				}
+			}
+		}
+		if ( $ret ) {
+			if ( $this->verbose ) {
+				echo "$filename SECURE\n";
+			}
+		}
+		return $ret;
+	}
+
+	function getGlobalsFromFunction( $opArray ) {
+		$globals = array();
+		foreach ( $opArray as $opLine ) {
+			if ( $opLine['opcode_name'] == 'ZEND_FETCH_W' 
+				&& $opLine['op1']['type_name'] == 'IS_CONST' ) 
+			{
+				$globals[$opLine['op1']['constant']] = true;
+			}
+		}
+		return $globals;
+	}
+
+	function getGlobalsFromTop( $opArray ) {
+		// Start with ZEND_FETCH_W calls
+		$globals = $this->getGlobalsFromFunction( $opArray );
+		// Now add local variable references
+		foreach ( $opArray as $opLine ) {
+			if ( isset( $opLine['op1']['varname'] ) ) {
+				$globals[$opLine['op1']['varname']] = true;
+			}
+			if ( isset( $opLine['op2']['varname'] ) ) {
+				$globals[$opLine['op2']['varname']] = true;
+			}
+		}
+		return $globals;
+	}
+
+	function getGlobalsFromParseInfo( $parseInfo ) {
+		// Get globals referenced in the file scope
+		$globals = $this->getGlobalsFromTop( $parseInfo['opcodes'] );
+
+		// Get globals referenced in global functions
+		if ( isset( $parseInfo['function_table'] ) ) {
+			foreach ( $parseInfo['function_table'] as $function ) {
+				$globals += $this->getGlobalsFromFunction( $function['opcodes'] );
+			}
+		}
+
+		// Get globals from class member functions
+		if ( isset( $parseInfo['class_table'] ) ) {
+			foreach ( $parseInfo['class_table'] as $class ) {
+				if ( isset( $class['function_table'] ) ) {
+					foreach ( $class['function_table'] as $function ) {
+						$globals += $this->getGlobalsFromFunction( $function['opcodes'] );
+					}
+				}
+			}
+		}
+		return $globals;
+	}
+}
