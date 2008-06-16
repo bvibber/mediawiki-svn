2545a15f406440119570c179d33207d9dc3cbd12
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-16 14:58:52 +0000

    No point in allowing external images in an ImageMap if we don't allow external images period.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36336 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ImageMap/ImageMap_body.php
+++ extensions/ImageMap/ImageMap_body.php
@@ -133,7 +133,7 @@ class ImageMap {
 				if (is_null($title))
 					return self::error('imagemap_invalid_title', $lineNum);
 				$alt = $title->getFullText();
-			} elseif ( in_array( substr( $link , 1 , strpos($link, '//' )+1 ) , $wgUrlProtocols ) || in_array( substr( $link , 1 , strpos($link, ':' ) ) , $wgUrlProtocols ) ) {
+			} elseif ( in_array( substr( $link , 1 , strpos($link, '//' )+1 ) , $wgUrlProtocols ) || in_array( substr( $link , 1 , strpos($link, ':' ) ) , $wgUrlProtocols ) && $wgAllowExternalImages ) {
 				if ( preg_match( '/^ \[  ([^\s]*+)  \s  ([^\]]*+)  \] \w* $ /x', $link, $m ) ) {
 					$title = $m[1];
 					$alt = trim( $m[2] );
