75c39028b52331d9d901b32ae41b807528c2cff2
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-16 19:34:18 +0000

    Localisation updates German
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/InterwikiList/InterwikiList.i18n.php
+++ extensions/InterwikiList/InterwikiList.i18n.php
@@ -5,8 +5,18 @@
 $messages = array();
 
 $messages['en'] = array (
-                'interwikilist'				=> 'Interwiki list',
-                'interwikilist-desc'		=> 'Adds a [[Special:Interwikilist|special page]] to view available interwiki links',
-                'interwikilist-linkname'	=> 'Interwiki link',
-                'interwikilist-target'		=> 'Target URL',
+                'interwikilist'          => 'Interwiki list',
+                'interwikilist-desc'     => 'Adds a [[Special:Interwikilist|special page]] to view available interwiki links',
+                'interwikilist-linkname' => 'Interwiki link',
+                'interwikilist-target'   => 'Target URL',
+);
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array (
+                'interwikilist'          => 'Interwiki-Liste',
+                'interwikilist-desc'     => 'Ergänzt eine [[Special:Interwikilist|Spezialseite]] zur Anzeige verfügbarer Interwiki-Links',
+                'interwikilist-linkname' => 'Interwiki-Link',
+                'interwikilist-target'   => 'Ziel-URL',
 );
