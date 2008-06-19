b21d2b98b8ffd85f9ab21911714fe2ead79eed58
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-19 16:15:04 +0000

    update about description, add background comments
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36467 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UniversalEditButton/UniversalEditButton.php
+++ extensions/UniversalEditButton/UniversalEditButton.php
@@ -1,13 +1,40 @@
 <?php
 
-// For experimental browser widget :)
+/**
+ * Adds a hidden <link> to the edit page into the HTML page headers
+ * to support the Universal Edit Button extension for Firefox.
+ *
+ * That extension detects the presence of the link and adds a clickable
+ * edit button into the URL bar -- in a consistent place, with a consistent
+ * icon for any wiki that supports it.
+ *
+ * For more background information, see: http://universaleditbutton.org/
+ *
+ *
+ * Copyright (C) 2008 Brion Vibber.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
 $wgExtensionCredits['other'][] = array(
 	'name'           => 'UniversalEditButton',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:UniversalEditButton',
 	'svn-date'       => '$LastChangedDate$',
 	'svn-revision'   => '$LastChangedRevision$',
-	'description'    => 'For experimental browser widget :)',
+	'description'    => 'Adds HTML header link to support Universal Edit Button browser extension.',
 	'author'         => 'Brion Vibber',
 );
 
