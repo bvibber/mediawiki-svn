c075565aa22c31885603edfb9d8a74490acd253a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-23 20:52:49 +0000

    Localisation updates. Remove trailing period for consistency.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36582 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UniversalEditButton/UniversalEditButton.i18n.php
+++ extensions/UniversalEditButton/UniversalEditButton.i18n.php
@@ -8,14 +8,14 @@
 $messages = array();
 
 $messages['en'] = array(
-	'universaleditbutton-desc' => 'Adds HTML header <code>&lt;link&gt;</code> to support Universal Edit Button browser extension.',
+	'universaleditbutton-desc' => 'Adds HTML header <code>&lt;link&gt;</code> to support Universal Edit Button browser extension',
 );
 
 /** Czech (Česky)
  * @author Danny B.
  */
 $messages['cs'] = array(
-	'universaleditbutton-desc' => 'Přidává do hlavičky <code>&lt;link&gt;</code> pro podporu rozšíření Universal Edit Button v prohlížečích.',
+	'universaleditbutton-desc' => 'Přidává do hlavičky <code>&lt;link&gt;</code> pro podporu rozšíření Universal Edit Button v prohlížečích',
 );
 
 /** German (Deutsch)
@@ -29,12 +29,12 @@ $messages['de'] = array(
  * @author Rotem Liss
  */
 $messages['he'] = array(
-	'universaleditbutton-desc' => 'הוספת כותרת ה־HTML <code>&lt;link&gt;</code> כדי לתמוך בהרחבת הדפדפן Universal Edit Button.',
+	'universaleditbutton-desc' => 'הוספת כותרת ה־HTML <code>&lt;link&gt;</code> כדי לתמוך בהרחבת הדפדפן Universal Edit Button',
 );
 
 /** Slovak (Slovenčina)
  * @author Danny B.
  */
 $messages['sk'] = array(
-	'universaleditbutton-desc' => 'Pridává do hlavičky <code>&lt;link&gt;</code> pre podporu rozšírenia Universal Edit Button v prehliadačach.',
+	'universaleditbutton-desc' => 'Pridává do hlavičky <code>&lt;link&gt;</code> pre podporu rozšírenia Universal Edit Button v prehliadačach',
 );
