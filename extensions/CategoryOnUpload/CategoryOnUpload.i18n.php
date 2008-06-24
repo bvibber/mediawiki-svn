f93030683358e689235d644638e77523fe91908c
minuteelectron <minuteelectron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-24 17:59:55 +0000

    New extension CategoryOnUpload.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36614 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CategoryOnUpload/CategoryOnUpload.i18n.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalisation file for CategoryOnUpload extension.
+ * 
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author MinuteElectron <minuteelectron@googlemail.com>
+ */
+$messages[ 'en' ] = array(
+	'categoryonupload-desc'  =>  'Prompts a user to select a category when uploading a file.',
+	'categoryonupload-label' => 'Category:',
+
+	'categoryonupload-none'  => '-', # Do not translate or duplicate this message to other languages.
+);
