0a98378ae8a318ec50c3be06577d7808670d00e2
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-24 19:35:49 +0000

    I was bored today, so I wrote an extension to handle bug 14592 (and a long-standing request to disallow userpage moving). Introduces the 'move-userpage' right for overriding (if you want to allow sysops, etc to move). Enjoy.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36616 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/NoMoveUserpages/NoMoveUserpages.i18n.php
@@ -0,0 +1,10 @@
+<?php
+
+// Internationalization file for Extension:NoMoveUserpages
+
+$messages = array();
+
+$messages['en'] = array (
+                'nomoveuserpages-desc'  => 'Blocks users from moving userpages unless they have the "move-userpages" right',
+                'nomoveuserpages-error' => 'Moving of user pages has been disabled',
+);
