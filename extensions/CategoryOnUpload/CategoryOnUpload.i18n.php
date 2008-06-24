29632158678355ec23979b6fb079ab914a0da4b9
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-24 20:31:36 +0000

    Localisation updates German
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36622 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CategoryOnUpload/CategoryOnUpload.i18n.php
+++ extensions/CategoryOnUpload/CategoryOnUpload.i18n.php
@@ -11,9 +11,17 @@ $messages = array();
 /** English
  * @author MinuteElectron <minuteelectron@googlemail.com>
  */
-$messages[ 'en' ] = array(
-	'categoryonupload-desc'  =>  'Prompts a user to select a category when uploading a file.',
+$messages['en'] = array(
+	'categoryonupload-desc'  => 'Prompts a user to select a category when uploading a file',
 	'categoryonupload-label' => 'Category:',
 
 	'categoryonupload-none'  => '-', # Do not translate or duplicate this message to other languages.
+);
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'categoryonupload-desc'  => 'Fragt einen Benutzer beim Hochladen von Dateien nach einer Kategorie',
+	'categoryonupload-label' => 'Kategorie:',
 );
--- extensions/NoMoveUserpages/NoMoveUserpages.i18n.php
+++ extensions/NoMoveUserpages/NoMoveUserpages.i18n.php
@@ -7,4 +7,13 @@ $messages = array();
 $messages['en'] = array (
                 'nomoveuserpages-desc'  => 'Blocks users from moving userpages unless they have the "move-userpages" right',
                 'nomoveuserpages-error' => 'Moving of user pages has been disabled',
+		'right-move-userpages'  => 'Move user pages',
+);
+
+/** German (Deutsch)
+ * @author Raimond Spekking
+ */
+$messages['de'] = array (
+                'nomoveuserpages-desc'  => "Schränkt die Verschiebung von Benutzerseiten auf Benutzer mit dem ''move-userpages''-Recht ein",
+                'nomoveuserpages-error' => 'Das Verschieben von Benutzerseiten wurde deaktiviert',
 );
