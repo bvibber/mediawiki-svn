dcf12221f1866b452650d7c2214a488505b7107f
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-06-30 23:26:59 +0000

    Two new extensions from Wikia: *RandomUsersWithAvatars - a parser hook to show randomly chosen user avatars. Cannot be used without SocialProfile extension. *WikiTextLoggedInOut - defines two new parser hooks to show different output depending on the users' login state. Can be used to support SocialProfile or as a stand-alone extension.
    Original code by Wikia's New York Office, some improvements by me.
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36826 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/RandomUsersWithAvatars/RandomUsersWithAvatars.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalization file for the RandomUsersWithAvatars extension.
+ *
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Wikia, Inc.
+ */
+$messages['en'] = array(
+	'random-users-avatars-title' => 'Meet the Community',
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix
+ */
+$messages['fi'] = array(
+	'random-users-avatars-title' => 'Tapaa yhteisö',
+);
