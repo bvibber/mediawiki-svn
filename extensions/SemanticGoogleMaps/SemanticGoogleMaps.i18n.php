f0cce8fb41ce0e57d4f6f3aefef7811be5bc8d1d
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-07 19:42:45 +0000

    Semantic Google Maps extension added to SVN
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@37280 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SemanticGoogleMaps/SemanticGoogleMaps.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalization file for the Semantic Google Maps extension
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	// user messages
+	'semanticgooglemaps_lookupcoordinates' => 'Look up coordinates',
+);
+
