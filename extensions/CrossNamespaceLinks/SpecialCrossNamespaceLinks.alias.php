91b3d70bcfd54f173270cf1ff91c72756406d8ff
jhsoby <jhsoby@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-09 16:34:59 +0000

    ... and one file that wasn't added
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@37405 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CrossNamespaceLinks/SpecialCrossNamespaceLinks.alias.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Aliases for Special:CrossNamespaceLinks
+ *
+ * @addtogroup Extensions
+ */
+
+$aliases = array();
+
+/** English
+ * @author Jon Harald Søby
+ */
+$aliases['en'] = array(
+    'CrossNamespaceLinks'            => array('CrossNamespaceLinks')
+);
+
+/** Norwegian (Norsk (bokmål))
+ * @author Jon Harald Søby
+ */
+$aliases['no'] = array(
+    'CrossNamespaceLinks'            => array('Lenker_mellom_navnerom')
+);
