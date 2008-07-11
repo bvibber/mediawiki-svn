a1d48959bfaaaf3131dfcbf2e0debd2f17bd27d1
stipe <stipe@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-11 04:04:32 +0000

    basic flowplayer javascript implemented
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/MetavidWiki-exp@37547 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- MetavidWiki/skins/mv_embed/mv_flashEmbed.js
+++ MetavidWiki/skins/mv_embed/mv_flashEmbed.js
@@ -5,14 +5,17 @@ var flashEmbed = {
     	var controls_html ='';
     	js_log('embedObj control is: '+this.controls);
 		if(this.controls){
-			controls_html+= this.getControlsHtml('stop');			
+			controls_html+= this.getControlsHtml('play_head') +					
+						this.getControlsHtml('play_or_pause') + 
+						this.getControlsHtml('stop') +
+   						this.getControlsHtml('info_span');
 		}
         setTimeout('document.getElementById(\''+this.id+'\').postEmbedJS()', 150);
 		return this.wrapEmebedContainer( this.getEmbedObj() )+ controls_html;
     },
     getEmbedObj:function(){
     	if(!this.duration)this.duration=30;
-        return '<div id="FlowPlayerAnnotationHolder"></div>'+"\n";
+        return '<div id="FlowPlayerAnnotationHolder_'+this.pid+'"></div>'+"\n";
     },
     postEmbedJS : function()
     {
@@ -24,9 +27,84 @@ var flashEmbed = {
     },
     doFlashEmbed : function()
     {
-        new flashembed("FlowPlayerAnnotationHolder",
-        { src: mv_embed_path + 'FlowPlayerDark.swf', width: this.width, height: this.height, id: this.pid },
+        var clip = flashembed('FlowPlayerAnnotationHolder_'+this.pid,
+        { src: mv_embed_path + 'FlowPlayerDark.swf', width: this.width, height: this.height, id: this.pid},
         { config: { autoPlay: true, showStopButton: false, showPlayButton: false,
            videoFile: this.media_element.selected_source.uri } });
+    },
+    /* js hooks/controls */
+    play : function(){
+    	if(this.thumbnail_disp)
+        {
+	    	//call the parent
+    		this.parent_play();
+    	}else{
+            this.getPluginEmbed().DoPlay();
+			this.paused=false;
+    	}
+    },
+    pause : function(){
+		this.getPluginEmbed().Pause();
     }
+}
+
+function locateFlashEmbed(clip)
+{
+    for(var i in global_ogg_list)
+    {
+        var embed = document.getElementById(global_ogg_list[i]);
+        if(embed.media_element.selected_source.uri.match(clip.fileName))
+        {
+            js_log('found flash embed');
+            return embed;
+        }
+    }
+}
+
+/* flowplayer callbacks */
+function onFlowPlayerReady()
+{
+    js_log('onFlowPlayerReady');
+}
+
+function onClipDone(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Clip Done...");
+}
+
+function onLoadBegin(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Loading Begun...");
+}
+
+function onPlay(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Playing...");
+}
+
+function onStop(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Stopped...");
+}
+
+function onPause(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Paused...");
+}
+
+function onResume(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Resumed...");
+}
+
+function onStartBuffering(clip)
+{
+    var embed = locateFlashEmbed(clip);
+    embed.setStatus("Buffering Started...");
 }
