8f23fd256a62cfa7b98ef1badc09c6dbe10818e0
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-12 19:36:12 +0000

    New extension: error handler
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@37599 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ErrorHandler/ErrorHandler.i18n.php
@@ -0,0 +1,54 @@
+<?php
+
+//if( !defined( 'MEDIAWIKI' ) ) die();
+
+$messages = array();
+
+$messages['en'] = array(
+	'errorhandler-desc'                  => 'Error handler for MediaWiki',
+	'errorhandler-errors'                => 'Errors:',
+	'errorhandler-error-fatal'           => 'Fatal error',
+	'errorhandler-error-warning'         => 'Warning',
+	'errorhandler-error-parse'           => 'Parser error',
+	'errorhandler-error-notice'          => 'Notice',
+	'errorhandler-error-core-error'      => 'Core error',
+	'errorhandler-error-core-warning'    => 'Core warning',
+	'errorhandler-error-compile-error'   => 'Compile error',
+	'errorhandler-error-compile-warning' => 'Compile warning',
+	'errorhandler-error-user-error'      => 'User error',
+	'errorhandler-error-user-warning'    => 'User warning',
+	'errorhandler-error-user-notice'     => 'User notice',
+	'errorhandler-error-strict'          => 'Stricts standarts',
+	'errorhandler-error-recoverable'     => 'Catchable fatal error',
+	'errorhandler-msg-text'              => '$1 : $2 in $3 (line $4)',
+	'errorhandler-msg-html'              => '<b>$1</b> : <i>$2</i> in <b>$3</b> (line <b>$4</b>)',
+	'errorhandler-trace'                 => 'trace:',
+	'errorhandler-trace-line'            => '$1 (line $2): $3',
+	'errorhandler-trace-line-internal'   => '[internal function]: $1',
+);
+
+/** French (Français)
+ * @author IAlex
+ */
+$messages['fr'] = array(
+	'errorhandler-desc'                  => "Gestionnaire d'erreurs pour MediaWiki",
+	'errorhandler-errors'                => 'Erreurs :',
+	'errorhandler-error-fatal'           => 'Erreur fatale',
+	'errorhandler-error-warning'         => 'Avertissement',
+	'errorhandler-error-parse'           => 'Erreur de parse',
+	'errorhandler-error-notice'          => 'Notice',
+	'errorhandler-error-core-error'      => 'Erreur du coeur',
+	'errorhandler-error-core-warning'    => 'Avertissement du coeur',
+	'errorhandler-error-compile-error'   => 'Erreur de compliation',
+	'errorhandler-error-compile-warning' => 'Avertissement de compilation',
+	'errorhandler-error-user-error'      => 'Erreur (utilisateur)',
+	'errorhandler-error-user-warning'    => 'Avertissement (utilisateur)',
+	'errorhandler-error-user-notice'     => 'Notice (utilisateur)',
+	'errorhandler-error-strict'          => 'Standarts stricts',
+	'errorhandler-error-recoverable'     => 'Erreur fatale attrapable',
+	'errorhandler-msg-text'              => '$1 : $2 dans $3 (ligne $4)',
+	'errorhandler-msg-html'              => '<b>$1</b> : <i>$2</i> dans <b>$3</b> (ligne <b>$4</b>)',
+	'errorhandler-trace'                 => 'trace :',
+	'errorhandler-trace-line'            => '$1 (ligne $2): $3',
+	'errorhandler-trace-line-internal'   => '[fonction interne] : $1',
+);
