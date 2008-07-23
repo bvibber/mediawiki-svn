f9aa13ca30ce65f900addcbe85c6c7aa53c443c8
jeluf <jeluf@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-23 21:20:05 +0000

    GPL headers
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@37977 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SlippyMap/SlippyMap.php
+++ extensions/SlippyMap/SlippyMap.php
@@ -15,6 +15,28 @@
 # 
 # This file should be placed in the mediawiki 'extensions' directory
 # ...and then it needs to be 'included' within LocalSettings.php
+#
+##################################################################################
+#
+# Copyright 2008 Harry Wood, Jens Frank, Grant Slater, Raymond Spekking and others
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# @addtogroup Extensions
+#
+
 
 if( defined( 'MEDIAWIKI' ) ) {
 	$wgExtensionFunctions[] = 'wfslippymap';
