25da7f9e1f9fd723796a22c5bbdd09d2f43b1513
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-26 02:59:07 +0000

    Basic implementation of a <substall> tag.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38054 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SubstAll/SubstAll.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array ( 
+	'substall-desc' => 'Creates a <nowiki><substall></nowiki> tag that expands all templates to wikisyntax.',
+	'substall-hook' => 'substall', // If this isn't localized, it defaults to en.
+);
