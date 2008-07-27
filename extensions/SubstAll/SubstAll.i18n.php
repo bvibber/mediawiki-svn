a3679e4add512fec70334c79d3f36de4be6d0727
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-27 10:37:01 +0000

    * fix trailing whitespace in SubstAll * add header to i18n file for SubstAll * add SubstAll to Translate * add special page alias localisation for SpamRegex to Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38086 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SubstAll/SubstAll.i18n.php
+++ extensions/SubstAll/SubstAll.i18n.php
@@ -1,8 +1,16 @@
 <?php
+/**
+ * Internationalisation file for extension SubstAll.
+ *
+ * @ingroup Extensions
+ */
 
 $messages = array();
 
-$messages['en'] = array ( 
+/** English
+ * @author Chad Horohoe
+ */
+$messages['en'] = array(
 	'substall-desc' => 'Creates a <nowiki><substall></nowiki> tag that expands all templates to wikisyntax.',
 	'substall-hook' => 'substall', // If this isn't localized, it defaults to en.
 );
--- extensions/SubstAll/SubstAll.php
+++ extensions/SubstAll/SubstAll.php
@@ -2,7 +2,7 @@
 /**
  * Extension:SubstAll - Fully expands all contained templates to final wikisyntax,
  * similar to the ExpandTemplates extension by Tim Starling.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
