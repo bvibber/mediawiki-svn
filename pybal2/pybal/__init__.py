81e8b42dcded8ef301aaea58edd89e0d996070e8
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-28 12:33:48 +0000

    Maybe not needed after all
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38131 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- pybal2/pybal/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-pybal.__init__.py
-Copyright (C) 2006 by Mark Bergsma <mark@nedworks.org>
-
-The pybal package contains all PyBal modules
-"""
-
-__all__ = [ 'ipvs', 'monitor', 'pybal', 'util', 'monitors' ]
