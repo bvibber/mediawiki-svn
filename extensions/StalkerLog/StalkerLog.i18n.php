68266691d345fbe570940bec2fa6e6a4f8a3da4b
jhsoby <jhsoby@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-30 17:56:47 +0000

    Adding StalkerLog to Translate, change wording of some messages in StalkerLog
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38243 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/StalkerLog/StalkerLog.i18n.php
+++ extensions/StalkerLog/StalkerLog.i18n.php
@@ -5,9 +5,9 @@
 $messages = array();
 
 $messages['en'] = array(
-				'stalkerlog-desc'       => 'Adds an entry to [[Special:Log]] on user login/logout',
+				'stalkerlog-desc'       => 'Adds an entry to [[Special:Log]] when someone logs in or out',
 				'stalkerlog-log-login'  => 'logged in.',
 				'stalkerlog-log-logout' => 'logged out.',
 				'stalkerlog-log-type'   => 'User authentication log',
-				'stalkerlog-log-text'   => 'Below is a log of user login/logout actions',
+				'stalkerlog-log-text'   => 'This is a log of user logins and logouts.',
 );
