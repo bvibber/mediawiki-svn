5ea8f9936221157d530bdbc268e01e5d9840115d
jhsoby <jhsoby@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-30 19:40:53 +0000

    Changing one message in StalkerLog per siebrand's request
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38246 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/StalkerLog/StalkerLog.i18n.php
+++ extensions/StalkerLog/StalkerLog.i18n.php
@@ -5,7 +5,7 @@
 $messages = array();
 
 $messages['en'] = array(
-				'stalkerlog-desc'       => 'Adds an entry to [[Special:Log]] when someone logs in or out',
+				'stalkerlog-desc'       => 'Adds [[Special:Log/stalkerlog|a log]] of user logins and logouts.',
 				'stalkerlog-log-login'  => 'logged in.',
 				'stalkerlog-log-logout' => 'logged out.',
 				'stalkerlog-log-type'   => 'User authentication log',
