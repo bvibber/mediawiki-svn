5107a359a7c6096300974ce08d07e5843f015dcb
jhsoby <jhsoby@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-30 19:48:36 +0000

    Remove trailing period per siebrand. XD
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38248 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/StalkerLog/StalkerLog.i18n.php
+++ extensions/StalkerLog/StalkerLog.i18n.php
@@ -5,7 +5,7 @@
 $messages = array();
 
 $messages['en'] = array(
-				'stalkerlog-desc'       => 'Adds [[Special:Log/stalkerlog|a log]] of user logins and logouts.',
+				'stalkerlog-desc'       => 'Adds [[Special:Log/stalkerlog|a log]] of user logins and logouts',
 				'stalkerlog-log-login'  => 'logged in.',
 				'stalkerlog-log-logout' => 'logged out.',
 				'stalkerlog-log-type'   => 'User authentication log',
