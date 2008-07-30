79bd06a803f2d1009584c5f6f52f95044a0e1c96
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-07-30 16:01:22 +0000

    Add StalkerLog extension I wrote up for work awhile back (but we never ended up using). Despite the name, it's targeted at corporate wikis for office productivity monitoring, etc.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38240 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/StalkerLog/StalkerLog.i18n.php
@@ -0,0 +1,13 @@
+<?php
+
+// Internationalization file for Extension:StalkerLog
+
+$messages = array();
+
+$messages['en'] = array(
+				'stalkerlog-desc'       => 'Adds an entry to [[Special:Log]] on user login/logout',
+				'stalkerlog-log-login'  => 'logged in.',
+				'stalkerlog-log-logout' => 'logged out.',
+				'stalkerlog-log-type'   => 'User authentication log',
+				'stalkerlog-log-text'   => 'Below is a log of user login/logout actions',
+);
