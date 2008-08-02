7db13a6997acb639241f55885f15c598aefd2e47
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-02 20:26:52 +0000

    Google AdSense extension using hook SkinBuildSidebar
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38452 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/GoogleAdSense/GoogleAdSense.class.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * Class file for the GoogleAdSense extension
+ *
+ * @addtogroup Extensions
+ * @author Siebrand Mazeland
+ * @license MIT
+ */
+class GoogleAdSense {
+
+	static function GoogleAdSenseInSidebar( $skin, &$bar ) {
+		global $wgGoogleAdSenseWidth, $wgGoogleAdSenseID,
+			$wgGoogleAdSenseHeight, $wgGoogleAdSenseClient,
+			$wgGoogleAdSenseSlot, $wgGoogleAdSenseSrc;
+
+		// Return $bar unchanged if not all values have been set.
+		// FIXME: signal incorrect configuration nicely?
+		if( $wgGoogleAdSenseClient == 'none' || $wgGoogleAdSenseSlot == 'none' || $wgGoogleAdSenseID == 'none' )
+			return $bar;
+
+		wfLoadExtensionMessages( 'GoogleAdSense' );
+
+		$bar['googleadsense'] = "<script type=\"text/javascript\"><!--
+google_ad_client = \"$wgGoogleAdSenseClient\";
+/* $wgGoogleAdSenseID */
+google_ad_slot = \"$wgGoogleAdSenseSlot\";
+google_ad_width = $wgGoogleAdSenseWidth;
+google_ad_height = $wgGoogleAdSenseHeight;
+//-->
+</script>
+<script type=\"text/javascript\"
+src=\"$wgGoogleAdSenseSrc\">
+</script>";
+
+		return $bar;
+	}
+}
---
+++ extensions/GoogleAdSense/GoogleAdSense.i18n.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Internationalisation file for extension GoogleAdSense
+ *
+ * @addtogroup Extensions
+ * @license MIT
+ */
+
+$messages = array();
+
+/** English
+ * @author Siebrand Mazeland
+ */
+$messages['en'] = array(
+	'googleadsense'      => 'Google AdSense', # do not translate or duplicate this message to other languages
+	'googleadsense-desc' => 'Adds [http://www.google.com/adsense Google AdSense] to the sidebar',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand Mazeland
+ */
+$messages['nl'] = array(
+	'googleadsense-desc' => 'Voegt [http://www.google.com/adsense?hl=nl Google AdSense] to the sidebar',
+);
