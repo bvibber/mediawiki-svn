9efe0c6c9f024a136707c765d167147ad434295f
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-03 23:38:13 +0000

    partiality working flvServer
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/MetavidWiki-exp@38521 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ MetavidWiki/skins/mv_embed/flvServer/mvFlvServer.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * mv_flvServer.php 
+ *
+ * @date Jul 25, 2008
+ * 
+ * this should enable serving segments of flv files
+ * 
+ * called with: mv_flvServer.php?file=myClip.flv&t=0:01:00/0:02:00
+ * 
+ *   
+ */
+define('BASE_LOCAL_CLIP_PATH', '/var/www/flv_clips');
+
+define('WEB_CLIP_PATH', '/flv_cips');
+define('FLASH_VIDEO_CONTENT_TYPE', 'video/x-flv');
+
+define('AUDIO_FRAME_INTERVAL', 3);
+//what to append to the flv file name to store serialized meta tags:
+
+
+if(!function_exists('filter_input')){
+	die('you version of php lacks <b>filter_input()</b> function</br>');
+}
+$file_req = filter_input(INPUT_GET, 'file', FILTER_SANITIZE_STRING);
+$time_req = filter_input(INPUT_GET, 't', FILTER_SANITIZE_STRING);
+
+//additional filtering to avoid directory traversing:
+$file_req = str_replace(array('../','./'), '', $file_req);
+if($file_req=='')die('error: missing file name');
+$file_loc =BASE_LOCAL_CLIP_PATH.'/'.$file_req;
+if(!is_file($file_loc))die('error: requested file not found');
+
+//if 't' is empty no seeking nessesary: 
+if($time_req==''){
+	output_full_file($file_loc);
+}else{	
+	//@@todo support more time request formats
+	if(strpos( $time_req,'/')!==false){
+		list($start_time_ntp, $end_time_ntp)=explode('/',$time_req);
+		$start_sec 	= ntp2seconds($start_time_ntp);
+		$end_sec 	= ntp2seconds($end_time_ntp);
+	}else{
+		$start_sec 	= ntp2seconds($time_req);
+		$end_sec	= null;
+		if($start_sec==0)output_full_file($file_loc);
+	}
+	if($start_sec > $end_sec)die('error: requested invalid time range');	
+
+	//print "DO: $start_sec $end_sec \n";
+	require_once('MvFlv.php');
+	//open up the metavid Flv object: 
+	$flv = new MvFLV();
+	try {
+		$flv->open( $file_loc );
+	} catch (Exception $e) {
+		die("<pre>The following exception was detected while trying to open a FLV file:\n" . $e->getMessage() . "</pre>");
+	}
+	//header('Content-type: '.FLASH_VIDEO_CONTENT_TYPE);
+	$flv->playTimeReq($start_sec, $end_sec);
+	//print_r($flv->computeMetaData() );
+	$flv->close();
+}
+
+
+function output_full_file($file_loc){
+	header('Content-Type: '.FLASH_VIDEO_CONTENT_TYPE);
+	while (ob_get_level() > 0) {
+   		ob_end_flush();
+	} //turn off output buffering	
+	@readfile($file_loc);
+	die();
+}
+
+function ntp2seconds($str_time){
+	$time_ary = explode(':', $str_time);	
+	$hours=$min=$sec=0;
+	if(count($time_ary)==3){
+		$hours=$time_ary[0];
+		$min=$time_ary[1];
+		$sec=$time_ary[2];
+	}else if(count($time_ary)==2){
+		$min=$time_ary[0];
+		$sec=$time_ary[1];
+	}else if(count($time_ary)==1){
+		$sec=$time_ary[0];
+	}	
+	return ($hours*3600) + ($min*60) + $sec;
+}
+?>
