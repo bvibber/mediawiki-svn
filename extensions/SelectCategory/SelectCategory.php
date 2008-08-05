85af0455cd07137aaf12d40348dc1ec9e5cc1953
leon <leon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-05 19:04:27 +0000

    - Set our hook functions immediately instead of setting them from the fnSelectCategory()   entry function. This is to avoid some weird race condition someone experienced where   the hooks where called before fnSelectCategory(), so they weren't set yet. (Patch by   Lambert Lum) - Avoid an array indexing error (Patch by Lambert Lum)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38652 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SelectCategory/SelectCategory.php
+++ extensions/SelectCategory/SelectCategory.php
@@ -14,6 +14,9 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die();
 }
 
+## Load the file containing the hook functions:
+require_once( 'SelectCategoryFunctions.php' );
+
 ## Options:
 # $wgSelectCategoryNamespaces - list of namespaces in which this extension should be active
 if( !isset( $wgSelectCategoryNamespaces	) ) $wgSelectCategoryNamespaces = array(
@@ -70,24 +73,19 @@ $wgExtensionCredits['parserhook'][] = array(
 
 $dir = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['SelectCategory'] = $dir . 'SelectCategory.i18n.php';
-$wgExtensionFunctions[]	= 'fnSelectCategory';
 
 ## Set Hook:
-function fnSelectCategory() {
-	global $wgHooks;
+global $wgHooks;
 
-	## Showing the boxes
-	# Hook when starting editing:
-	$wgHooks['EditPage::showEditForm:initial'][] = array( 'fnSelectCategoryShowHook', false );
-	# Hook for the upload page:
-	$wgHooks['UploadForm:initial'][] = array( 'fnSelectCategoryShowHook', true );
+## Showing the boxes
+# Hook when starting editing:
+$wgHooks['EditPage::showEditForm:initial'][] = array( 'fnSelectCategoryShowHook', false );
+# Hook for the upload page:
+$wgHooks['UploadForm:initial'][] = array( 'fnSelectCategoryShowHook', true );
 
-	## Saving the data
-	# Hook when saving page:
-	$wgHooks['EditPage::attemptSave'][] = array( 'fnSelectCategorySaveHook', false );
-	# Hook when saving the upload:
-	$wgHooks['UploadForm:BeforeProcessing'][] = array( 'fnSelectCategorySaveHook', true );
-}
+## Saving the data
+# Hook when saving page:
+$wgHooks['EditPage::attemptSave'][] = array( 'fnSelectCategorySaveHook', false );
+# Hook when saving the upload:
+$wgHooks['UploadForm:BeforeProcessing'][] = array( 'fnSelectCategorySaveHook', true );
 
-## Load the file containing the hook functions:
-require_once( 'SelectCategoryFunctions.php' );
