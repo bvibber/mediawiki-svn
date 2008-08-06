065d94f387af592c3f5c3540469b2f7b7d9e8042
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-06 11:58:19 +0000

    swexec: fix indentation; need Solaris project support for stable toolserver
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@38706 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- switchboard/swexec.c
+++ switchboard/swexec.c
@@ -1,3 +1,5 @@
+/* Copyright 2008 The Apache Software Foundation. */
+/* Copyright 2008 River Tarnell <river@wikimedia.org */
 /* Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -16,35 +18,33 @@
 
 /*
  * suexec.c -- "Wrapper" support program for suEXEC behaviour for Apache
- *
- ***********************************************************************
- *
- * NOTE! : DO NOT edit this code!!!  Unless you know what you are doing,
- *         editing this code might open up your system in unexpected
- *         ways to would-be crackers.  Every precaution has been taken
- *         to make this code as safe as possible; alter it at your own
- *         risk.
- *
- ***********************************************************************
- *
- *
  */
+
+#if defined(__sun) && defined(__svr4__)
+/* Solaris */
+# define USE_PROJECTS
+#endif
 
 #define SB_SUEXEC_UMASK 022
 
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <errno.h>
+#include	<sys/param.h>
+#include	<sys/stat.h>
+#include	<sys/types.h>
+#include	<string.h>
+#include	<time.h>
+#include	<unistd.h>
+#include	<stdio.h>
+#include	<stdarg.h>
+#include	<stdlib.h>
+#include	<errno.h>
 
-#include <pwd.h>
-#include <grp.h>
+#ifdef USE_PROJECTS
+# include	<sys/task.h>
+# include	<project.h>
+#endif
+
+#include	<pwd.h>
+#include	<grp.h>
 
 #if defined(PATH_MAX)
 #define SB_MAXPATH PATH_MAX
@@ -66,9 +66,9 @@ static const char *const safe_env_lst[] =
 };
 
 
-static void err_output(int is_error, const char *fmt, va_list ap)
+static void
+err_output(int is_error, const char *fmt, va_list ap)
 {
-#ifdef SB_LOG_EXEC
     time_t timevar;
     struct tm *lt;
 
@@ -95,262 +95,274 @@ static void err_output(int is_error, const char *fmt, va_list ap)
     vfprintf(log, fmt, ap);
 
     fflush(log);
-#endif /* SB_LOG_EXEC */
-    return;
 }
 
-static void log_err(const char *fmt,...)
+static void
+log_err(const char *fmt,...)
 {
-#ifdef SB_LOG_EXEC
     va_list ap;
 
     va_start(ap, fmt);
     err_output(1, fmt, ap); /* 1 == is_error */
     va_end(ap);
-#endif /* SB_LOG_EXEC */
-    return;
 }
 
-static void log_no_err(const char *fmt,...)
+static void
+log_no_err(const char *fmt,...)
 {
-#ifdef SB_LOG_EXEC
     va_list ap;
 
     va_start(ap, fmt);
     err_output(0, fmt, ap); /* 0 == !is_error */
     va_end(ap);
-#endif /* SB_LOG_EXEC */
-    return;
 }
 
-static void clean_env(void)
+static void
+clean_env(void)
 {
-    char envbuf[512];
-    char **cleanenv;
-    char **ep;
-    int cidx = 0;
-    int idx;
+	char envbuf[512];
+	char **cleanenv;
+	char **ep;
+	int cidx = 0;
+	int idx;
 
-    /* While cleaning the environment, the environment should be clean.
-     * (e.g. malloc() may get the name of a file for writing debugging info.
-     * Bad news if MALLOC_DEBUG_FILE is set to /etc/passwd.  Sprintf() may be
-     * susceptible to bad locale settings....)
-     * (from PR 2790)
-     */
-    char **envp = environ;
-    char *empty_ptr = NULL;
+	/* While cleaning the environment, the environment should be clean.
+	 * (e.g. malloc() may get the name of a file for writing debugging info.
+	 * Bad news if MALLOC_DEBUG_FILE is set to /etc/passwd.  Sprintf() may be
+	 * susceptible to bad locale settings....)
+	 * (from PR 2790)
+	 */
+	char **envp = environ;
+	char *empty_ptr = NULL;
 
-    environ = &empty_ptr; /* VERY safe environment */
+	environ = &empty_ptr; /* VERY safe environment */
 
-    if ((cleanenv = (char **) calloc(SB_ENVBUF, sizeof(char *))) == NULL) {
-        log_err("failed to malloc memory for environment\n");
-        exit(120);
-    }
+	if ((cleanenv = (char **) calloc(SB_ENVBUF, sizeof(char *))) == NULL) {
+		log_err("failed to malloc memory for environment\n");
+		exit(120);
+	}
 
-    sprintf(envbuf, "PATH=%s", SB_SAFE_PATH);
-    cleanenv[cidx] = strdup(envbuf);
-    cidx++;
-    sprintf(envbuf, "PHP_FCGI_MAX_REQUESTS=5000");
-    cleanenv[cidx] = strdup(envbuf);
-    cidx++;
+	sprintf(envbuf, "PATH=%s", SB_SAFE_PATH);
+	cleanenv[cidx] = strdup(envbuf);
+	cidx++;
+	sprintf(envbuf, "PHP_FCGI_MAX_REQUESTS=5000");
+	cleanenv[cidx] = strdup(envbuf);
+	cidx++;
 
-    for (ep = envp; *ep && cidx < SB_ENVBUF-1; ep++) {
-        for (idx = 0; safe_env_lst[idx]; idx++) {
-            if (!strncmp(*ep, safe_env_lst[idx],
-                         strlen(safe_env_lst[idx]))) {
-                cleanenv[cidx] = *ep;
-                cidx++;
-                break;
-            }
-        }
-    }
+	for (ep = envp; *ep && cidx < SB_ENVBUF-1; ep++) {
+		for (idx = 0; safe_env_lst[idx]; idx++) {
+			if (!strncmp(*ep, safe_env_lst[idx],
+					strlen(safe_env_lst[idx]))) {
+				cleanenv[cidx] = *ep;
+				cidx++;
+				break;
+			}
+		}
+	}
 
-    cleanenv[cidx] = NULL;
+	cleanenv[cidx] = NULL;
 
-    environ = cleanenv;
+	environ = cleanenv;
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    int userdir = 0;        /* ~userdir flag             */
-    uid_t uid;              /* user information          */
-    gid_t gid;              /* target group placeholder  */
-    char *target_uname;     /* target user name          */
-    char *target_gname;     /* target group name         */
-    char *target_homedir;   /* target home directory     */
-    char *actual_uname;     /* actual user name          */
-    char *actual_gname;     /* actual group name         */
-    char *prog;             /* name of this program      */
-    struct passwd *pw;      /* password entry holder     */
-    struct group *gr;       /* group entry holder        */
+	int userdir = 0;        /* ~userdir flag             */
+	uid_t uid;              /* user information          */
+	gid_t gid;              /* target group placeholder  */
+	char *target_uname;     /* target user name          */
+	char *target_gname;     /* target group name         */
+	char *target_homedir;   /* target home directory     */
+	char *actual_uname;     /* actual user name          */
+	char *actual_gname;     /* actual group name         */
+	char *prog;             /* name of this program      */
+	struct passwd *pw;      /* password entry holder     */
+	struct group *gr;       /* group entry holder        */
+#ifdef USE_PROJECTS
+	struct project proj;
+	char nssbuf[PROJECT_BUFSZ];
+#endif
 
-    /*
-     * Start with a "clean" environment
-     */
-    clean_env();
+	/*
+	 * Start with a "clean" environment
+	 */
+	clean_env();
 
-    prog = argv[0];
-    /*
-     * Check existence/validity of the UID of the user
-     * running this program.  Error out if invalid.
-     */
-    uid = getuid();
-    if ((pw = getpwuid(uid)) == NULL) {
-        log_err("crit: invalid uid: (%ld)\n", uid);
-        exit(102);
-    }
+	prog = argv[0];
+	/*
+	 * Check existence/validity of the UID of the user
+	 * running this program.  Error out if invalid.
+	 */
+	uid = getuid();
+	if ((pw = getpwuid(uid)) == NULL) {
+		log_err("crit: invalid uid: (%ld)\n", (long) uid);
+		exit(102);
+	}
 
-    /*
-     * If there are a proper number of arguments, set
-     * all of them to variables.  Otherwise, error out.
-     */
-    if (argc < 3) {
-        log_err("too few arguments\n");
-        exit(101);
-    }
-    target_uname = argv[1];
-    target_gname = argv[2];
+	/*
+	 * If there are a proper number of arguments, set
+	 * all of them to variables.  Otherwise, error out.
+	 */
+	if (argc < 3) {
+		log_err("too few arguments\n");
+		exit(101);
+	}
+	target_uname = argv[1];
+	target_gname = argv[2];
 
-    /*
-     * Check to see if the user running this program
-     * is the user allowed to do so as defined in
-     * suexec.h.  If not the allowed user, error out.
-     */
-    if (strcmp(SB_USER, pw->pw_name)) {
-        log_err("user mismatch (%s instead of %s)\n", pw->pw_name, SB_USER);
-        exit(103);
-    }
+	/*
+	 * Check to see if the user running this program
+	 * is the user allowed to do so as defined in
+	 * suexec.h.  If not the allowed user, error out.
+	 */
+	if (strcmp(SB_USER, pw->pw_name)) {
+		log_err("user mismatch (%s instead of %s)\n", pw->pw_name, SB_USER);
+		exit(103);
+	}
 
-    /*
-     * Error out if the target username is invalid.
-     */
-    if (strspn(target_uname, "1234567890") != strlen(target_uname)) {
-        if ((pw = getpwnam(target_uname)) == NULL) {
-            log_err("invalid target user name: (%s)\n", target_uname);
-            exit(105);
-        }
-    }
-    else {
-        if ((pw = getpwuid(atoi(target_uname))) == NULL) {
-            log_err("invalid target user id: (%s)\n", target_uname);
-            exit(121);
-        }
-    }
+	/*
+	* Error out if the target username is invalid.
+	*/
+	if (strspn(target_uname, "1234567890") != strlen(target_uname)) {
+		if ((pw = getpwnam(target_uname)) == NULL) {
+			log_err("invalid target user name: (%s)\n", target_uname);
+			exit(105);
+		}
+	} else {
+		if ((pw = getpwuid(atoi(target_uname))) == NULL) {
+			log_err("invalid target user id: (%s)\n", target_uname);
+			exit(121);
+		}
+	}
 
-    /*
-     * Error out if the target group name is invalid.
-     */
-    if (strspn(target_gname, "1234567890") != strlen(target_gname)) {
-        if ((gr = getgrnam(target_gname)) == NULL) {
-            log_err("invalid target group name: (%s)\n", target_gname);
-            exit(106);
-        }
-    }
-    else {
-        if ((gr = getgrgid(atoi(target_gname))) == NULL) {
-            log_err("invalid target group id: (%s)\n", target_gname);
-            exit(106);
-        }
-    }
-    gid = gr->gr_gid;
-    actual_gname = strdup(gr->gr_name);
+	/*
+	 * Error out if the target group name is invalid.
+	 */
+	if (strspn(target_gname, "1234567890") != strlen(target_gname)) {
+		if ((gr = getgrnam(target_gname)) == NULL) {
+			log_err("invalid target group name: (%s)\n", target_gname);
+			exit(106);
+		}
+	} else {
+		if ((gr = getgrgid(atoi(target_gname))) == NULL) {
+			log_err("invalid target group id: (%s)\n", target_gname);
+			exit(106);
+		}
+	}
+	gid = gr->gr_gid;
+	actual_gname = strdup(gr->gr_name);
 
-    /*
-     * Save these for later since initgroups will hose the struct
-     */
-    uid = pw->pw_uid;
-    actual_uname = strdup(pw->pw_name);
-    target_homedir = strdup(pw->pw_dir);
+	/*
+	 * Save these for later since initgroups will hose the struct
+	 */
+	uid = pw->pw_uid;
+	actual_uname = strdup(pw->pw_name);
+	target_homedir = strdup(pw->pw_dir);
 
-    /*
-     * Log the transaction here to be sure we have an open log
-     * before we setuid().
-     */
-    log_no_err("uid: (%s/%s) gid: (%s/%s) cmd: %s\n",
-               target_uname, actual_uname,
-               target_gname, actual_gname,
-               PHP_BIN);
+	/*
+	 * Log the transaction here to be sure we have an open log
+	 * before we setuid().
+	 */
+	log_no_err("uid: (%s/%s) gid: (%s/%s) cmd: %s\n",
+		target_uname, actual_uname,
+		target_gname, actual_gname,
+		PHP_BIN);
 
-    /*
-     * Error out if attempt is made to execute as root or as
-     * a UID less than SB_UID_MIN.  Tsk tsk.
-     */
-    if ((uid == 0) || (uid < SB_UID_MIN)) {
-        log_err("cannot run as forbidden uid (%d/%s)\n", uid, PHP_BIN);
-        exit(107);
-    }
+	/*
+	 * Error out if attempt is made to execute as root or as
+	 * a UID less than SB_UID_MIN.  Tsk tsk.
+	 */
+	if ((uid == 0) || (uid < SB_UID_MIN)) {
+		log_err("cannot run as forbidden uid (%d/%s)\n", uid, PHP_BIN);
+		exit(107);
+	}
 
-    /*
-     * Error out if attempt is made to execute as root group
-     * or as a GID less than SB_GID_MIN.  Tsk tsk.
-     */
-    if ((gid == 0) || (gid < SB_GID_MIN)) {
-        log_err("cannot run as forbidden gid (%d/%s)\n", gid, PHP_BIN);
-        exit(108);
-    }
+	/*
+	 * Error out if attempt is made to execute as root group
+	 * or as a GID less than SB_GID_MIN.  Tsk tsk.
+	 */
+	if ((gid == 0) || (gid < SB_GID_MIN)) {
+		log_err("cannot run as forbidden gid (%d/%s)\n", gid, PHP_BIN);
+		exit(108);
+	}
 
-    /*
-     * Change UID/GID here so that the following tests work over NFS.
-     *
-     * Initialize the group access list for the target user,
-     * and setgid() to the target group. If unsuccessful, error out.
-     */
-    if (((setgid(gid)) != 0) || (initgroups(actual_uname, gid) != 0)) {
-        log_err("failed to setgid (%ld: %s)\n", gid, PHP_BIN);
-        exit(109);
-    }
+#ifdef USE_PROJECTS
+	if (getdefaultproj(actual_uname, &proj, nssbuf, sizeof(nssbuf)) == NULL) {
+		log_err("failed to get default project (%s: %s)",
+				actual_uname, strerror(errno));
+		exit(113);
+	}
 
-    /*
-     * setuid() to the target user.  Error out on fail.
-     */
-    if ((setuid(uid)) != 0) {
-        log_err("failed to setuid (%ld: %s)\n", uid, PHP_BIN);
-        exit(110);
-    }
+	if (setproject(proj.pj_name, actual_uname, TASK_FINAL) != 0) {
+		log_err("failed to set project (%s: %s)",
+				actual_uname, strerror(errno));
+		exit(114);
+	}
+#endif
+		
+	/*
+	 * Change UID/GID here so that the following tests work over NFS.
+	 *
+	 * Initialize the group access list for the target user,
+	 * and setgid() to the target group. If unsuccessful, error out.
+	 */
+	if (((setgid(gid)) != 0) || (initgroups(actual_uname, gid) != 0)) {
+		log_err("failed to setgid (%ld: %s)\n", (long) gid, PHP_BIN);
+		exit(109);
+	}
 
-    if (chdir(target_homedir) != 0) {
-        log_err("cannot chdir to home directory (%s)\n", target_homedir);
-        exit(112);
-    }
+	/*
+	 * setuid() to the target user.  Error out on fail.
+	 */
+	if ((setuid(uid)) != 0) {
+		log_err("failed to setuid (%ld: %s)\n", (long) uid, PHP_BIN);
+		exit(110);
+	}
+
+	if (chdir(target_homedir) != 0) {
+		log_err("cannot chdir to home directory (%s)\n", target_homedir);
+		exit(112);
+	}
 
 #ifdef SB_SUEXEC_UMASK
-    /*
-     * umask() uses inverse logic; bits are CLEAR for allowed access.
-     */
-    if ((~SB_SUEXEC_UMASK) & 0022) {
-        log_err("notice: SB_SUEXEC_UMASK of %03o allows "
-                "write permission to group and/or other\n", SB_SUEXEC_UMASK);
-    }
-    umask(SB_SUEXEC_UMASK);
+	/*
+	 * umask() uses inverse logic; bits are CLEAR for allowed access.
+	 */
+	if ((~SB_SUEXEC_UMASK) & 0022) {
+		log_err("notice: SB_SUEXEC_UMASK of %03o allows "
+			"write permission to group and/or other\n", SB_SUEXEC_UMASK);
+	}
+	umask(SB_SUEXEC_UMASK);
 #endif /* SB_SUEXEC_UMASK */
 
-    /*
-     * Be sure to close the log file so the CGI can't
-     * mess with it.  If the exec fails, it will be reopened
-     * automatically when log_err is called.  Note that the log
-     * might not actually be open if SB_LOG_EXEC isn't defined.
-     * However, the "log" cell isn't ifdef'd so let's be defensive
-     * and assume someone might have done something with it
-     * outside an ifdef'd SB_LOG_EXEC block.
-     */
-    if (log != NULL) {
-        fclose(log);
-        log = NULL;
-    }
+	/*
+	 * Be sure to close the log file so the CGI can't
+	 * mess with it.  If the exec fails, it will be reopened
+	 * automatically when log_err is called.  Note that the log
+	 * might not actually be open if SB_LOG_EXEC isn't defined.
+	 * However, the "log" call isn't ifdef'd so let's be defensive
+	 * and assume someone might have done something with it
+	 * outside an ifdef'd SB_LOG_EXEC block.
+	 */
+	if (log != NULL) {
+		fclose(log);
+		log = NULL;
+	}
 
-    /*
-     * Execute the command, replacing our image with its own.
-     */
-    execl(PHP_BIN, PHP_BIN, NULL);
+	/*
+	 * Execute the command, replacing our image with its own.
+	 */
+	execl(PHP_BIN, PHP_BIN, NULL);
 
-    /*
-     * (I can't help myself...sorry.)
-     *
-     * Uh oh.  Still here.  Where's the kaboom?  There was supposed to be an
-     * EARTH-shattering kaboom!
-     *
-     * Oh well, log the failure and error out.
-     */
-    log_err("(%d)%s: exec failed (%s)\n", errno, strerror(errno), PHP_BIN);
-    exit(255);
+	/*
+	 * (I can't help myself...sorry.)
+	 *
+	 * Uh oh.  Still here.  Where's the kaboom?  There was supposed to be an
+	 * EARTH-shattering kaboom!
+	 *
+	 * Oh well, log the failure and error out.
+	 */
+	log_err("(%d)%s: exec failed (%s)\n", errno, strerror(errno), PHP_BIN);
+	exit(255);
 }
