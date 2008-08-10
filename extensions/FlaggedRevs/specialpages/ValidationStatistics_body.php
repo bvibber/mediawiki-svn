d94f4ab762a7462cfb9b1c1e81a78ca90f1dccde
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-10 22:47:43 +0000

    backport some fixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/REL1_13@39107 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FlaggedRevs/specialpages/ValidationStatistics_body.php
+++ extensions/FlaggedRevs/specialpages/ValidationStatistics_body.php
@@ -32,7 +32,7 @@ class ValidationStatistics extends UnlistedSpecialPage
 		$wgOut->addWikiText( wfMsg('validationstatistics-table') );
 		$wgOut->addHTML( "<table class='wikitable flaggedrevs_stats_table'>\n" );
 		$wgOut->addHTML( "<tr>\n" );
-		$msgs = array("ns","total","stable","latest","synced"); // our headings
+		$msgs = array("ns","total","stable","latest","synced","old"); // our headings
 		foreach( $msgs as $msg ) {
 			$wgOut->addHTML( "<th>".wfMsg("validationstatistics-$msg")."</th>" );
 		}
@@ -43,9 +43,11 @@ class ValidationStatistics extends UnlistedSpecialPage
 			$NsText = $wgContLang->getFormattedNsText( $row->namespace );
 			$NsText = $NsText ? $NsText : wfMsgHTML('blanknamespace');
 			
-			$percRev = sprintf( '%4.2f', 100*intval($row->reviewed)/intval($row->total) );
-			$percLatest = sprintf( '%4.2f', 100*intval($row->synced)/intval($row->total) );
-			$percSynced = sprintf( '%4.2f', 100*intval($row->synced)/intval($row->reviewed) );
+			$percRev = @sprintf( '%4.2f', 100*intval($row->reviewed)/intval($row->total) );
+			$percLatest = @sprintf( '%4.2f', 100*intval($row->synced)/intval($row->total) );
+			$percSynced = @sprintf( '%4.2f', 100*intval($row->synced)/intval($row->reviewed) );
+			$outdated = intval($row->reviewed) - intval($row->synced);
+			$outdated = max( 0, $outdated ); // lag between queries
 			
 			$wgOut->addHTML( "<tr align='center'>" );
 			$wgOut->addHTML( "<td>$NsText</td>" );
@@ -53,6 +55,7 @@ class ValidationStatistics extends UnlistedSpecialPage
 			$wgOut->addHTML( "<td>{$row->reviewed} <i>($percRev%)</i></td>" );
 			$wgOut->addHTML( "<td>{$row->synced} <i>($percLatest%)</i></td>" );
 			$wgOut->addHTML( "<td>$percSynced%</td>" );
+			$wgOut->addHTML( "<td>".$outdated."</td>" );
 			$wgOut->addHTML( "</tr>" );
 		}
 		$wgOut->addHTML( "</table>" );
@@ -64,10 +67,18 @@ class ValidationStatistics extends UnlistedSpecialPage
 		$keySQL = wfMemcKey( 'flaggedrevs', 'statsUpdating' );
 		// If a cache update is needed, do so asynchronously.
 		// Don't trigger query while another is running.
-		if( !$dbCache->get( $key ) && !$dbCache->get( $keySQL ) ) {
+		if( $dbCache->get( $key ) ) {
+			wfDebugLog( 'ValidationStatistic', __METHOD__ . " skipping, got data" );
+		} elseif( $dbCache->get( $keySQL ) ) {
+			wfDebugLog( 'ValidationStatistic', __METHOD__ . " skipping, in progress" );
+		} else {
 			$ext = strpos( $_SERVER['SCRIPT_NAME'], 'index.php5' ) === false ? 'php' : 'php5';
 			$path = wfEscapeShellArg( dirname(__FILE__).'/../maintenance/updateStats.php' );
-			exec( "$ext $path > /dev/null &" );
+			$wiki = wfEscapeShellArg( wfWikiId() );
+			$devNull = wfIsWindows() ? "NUL" : "/dev/null";
+			$commandLine = "$ext $path --wiki=$wiki > $devNull &";
+			wfDebugLog( 'ValidationStatistic', __METHOD__ . " executing: $commandLine" );
+			exec( $commandLine );
 		}
 	}
 	
