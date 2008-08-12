d519e219dafa177a0fcdbf223a688a36c7d5711f
krimpet <krimpet@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-12 07:11:36 +0000

    Many fixes to the Lua extension, including: - Streamlined many parts of the code - Fixed the debug hook so it properly intercepts infinite loops and such - Documented the code fairly thoroughly
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39211 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Lua/Lua.hooks.php
+++ extensions/Lua/Lua.hooks.php
@@ -10,6 +10,7 @@
  */
 
 class LuaHooks {
+	/** ParserFirstCallInit hook */
 	public static function parserInit() {
 		global $wgParser;
 		$wgParser->setHook( 'lua', 'LuaHooks::renderTag' );
@@ -17,24 +18,30 @@ class LuaHooks {
 		return true;
 	}
 
+	/** LanguageGetMagic hook */
 	public static function magic(&$magicWords, $langCode) {
 		$magicWords['luaexpr'] = array(0, 'luaexpr');
 		return true;
 	}
 
+	/** ParserBeforeTidy hook */
 	public static function beforeTidy(&$parser, &$text) {
 		global $wgLua;
 		if (isset($wgLua)) {
-			$wgLua->kill();
+			$wgLua->destroy();
 		}
 		return TRUE;
 	}
 
+	/** Parser hook for the <lua> tag */
 	public static function renderTag($input, $args, &$parser) {
 		global $wgLua;
+		# Create a new LuaWrapper if needed
 		if (!isset($wgLua))
-			$wgLua = LuaWrapper::create();
+			$wgLua = new LuaWrapper;
 
+		# Process the tag's arguments into a chunk of Lua code
+		# that initializes them in the Lua sandbox
 		$arglist = '';
 		foreach ($args as $key => $value)
 			$arglist .= (preg_replace('/\W/', '', $key) . '=\'' .
@@ -42,28 +49,33 @@ class LuaHooks {
 				     '\';');
 		if ($arglist) {
 			try {
-				$wgLua->run($arglist);
+				$wgLua->wrap($arglist);
 			} catch (LuaError $e) {
 				return $e->getMessage();
 			}
 		}
 
+		# Execute this Lua chunk, and send the results through the 
+		# Parser.
 		try {
-			return $parser->recursiveTagParse($wgLua->run($input));
+			return $parser->recursiveTagParse($wgLua->wrap($input));
 		} catch (LuaError $e) {
 			return $e->getMessage();
 		}
 	}
 	
+	/** Parser function hook for the #luaexpr function*/
 	public static function renderExpr(&$parser, $param1 = FALSE) {
 		global $wgLua;
+		# Create a new LuaWrapper if needed
 		if (!isset($wgLua))
 			$wgLua = LuaWrapper::create();
 		
+		# Execute this Lua chunk, wrapped in io.write().
 		if ($param1 == FALSE)
 			return '';
 		try {
-			return $wgLua->run("io.write($param1)");
+			return $wgLua->wrap("io.write($param1)");
 		} catch (LuaError $e) {
 			return $e->getMessage();
 		}
