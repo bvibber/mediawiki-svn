40209055ceacc344121e474f1d1bd399181acb51
mfarag <mfarag@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-12 04:38:22 +0000

    Add special page aliases for ar and arz
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39204 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/NewsChannel/NewsChannel.alias.php
+++ extensions/NewsChannel/NewsChannel.alias.php
@@ -12,3 +12,17 @@ $aliases = array();
 $aliases['en'] = array(
 	'NewsChannel'    => array( 'NewsChannel' ),
 );
+
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$aliases['ar'] = array(
+	'NewsChannel'   => array( 'قناة_أخبار' ),
+);
+
+ /** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$aliases['arz'] = array(
+	'NewsChannel'   => array( 'قناة_أخبار' ),
+);
