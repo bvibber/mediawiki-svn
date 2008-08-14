8178d7165ed74c8d18baadb827d8e291c02b8051
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-14 08:52:41 +0000

    A few updates for 1.13. Just for the record before I delete this file.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39332 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- release-tools/make-release.sh
+++ release-tools/make-release.sh
@@ -33,12 +33,24 @@ case "$1" in
 	fi
 esac
 
+[ -e "$dir" ] || mkdir "$dir"
+
+if [ -e /usr/bin/md5sum ];then
+	MD5SUM=/usr/bin/md5sum
+	SHA1SUM=/usr/bin/sha1sum
+else
+	MD5SUM=`which md5`
+	SHA1SUM=`which sha1`
+fi
+
 package=mediawiki-$ver
 
-svn co http://svn.wikimedia.org/svnroot/mediawiki/$branch/phase3 $package
+echo "Exporting..." >&2
+svn export -q http://svn.wikimedia.org/svnroot/mediawiki/$branch/phase3 $package
+echo "Done." >&2
 
 # Unix package
-tar cvf - --exclude .svn --exclude testsuite --exclude mediawiki-large.xcf --exclude mediawiki-largesquare.xcf $package | gzip -9 > $package.tar.gz
+tar cf - --exclude testsuite --exclude mediawiki-large.xcf --exclude mediawiki-largesquare.xcf $package | gzip -9 > "$dir/$package.tar.gz"
 outfiles="$package.tar.gz"
 
 # Patch
@@ -50,9 +62,13 @@ if [ "$prev" ]; then
   outfiles="$outfiles $package.patch"
 fi
 
+uploadfiles=
 for f in $outfiles; do
-  gpg --detach-sign $f
+  gpg --detach-sign $dir/$f
+  uploadfiles="$uploadfiles $dir/$f $dir/$f.sig"
 done
+
+tar -cf upload.tar $uploadfiles
 
 echo ""
 echo "Full release notes:"
@@ -73,16 +89,23 @@ for f in $outfiles; do
 done
 echo ""
 
+echo
+echo Public keys:
+echo "https://secure.wikimedia.org/keys.html"
+echo
+
+pushd $dir >/dev/null
 echo ""
 echo "SHA-1 checksums:"
 for f in $outfiles; do
-  sha1 $f
+  $SHA1SUM $f
 done
 echo ""
 
 echo ""
 echo "MD-5 checksums:"
 for f in $outfiles; do
-  md5 $f
+  $MD5SUM $f
 done
 echo ""
+popd > /dev/null
