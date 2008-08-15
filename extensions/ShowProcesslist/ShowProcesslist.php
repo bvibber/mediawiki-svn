6bebbf249d9e70003c4226acde06c60b35f3fb2d
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-15 20:01:48 +0000

    * remove dependency on ExtensionFunctions.php * delayed message loading * add $wgExtensionAliasesFiles and support in Translate * method renamed for consistency with other extensions * remove EOL whitespace
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39430 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ShowProcesslist/ShowProcesslist.php
+++ extensions/ShowProcesslist/ShowProcesslist.php
@@ -9,15 +9,14 @@ $wgExtensionCredits['specialpage'][] = array(
 	'name'           => 'ShowProcesslist',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:Show_Process_List',
 	'author'         => 'Brion VIBBER',
-	'svn-date' => '$LastChangedDate$',
-	'svn-revision' => '$LastChangedRevision$',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
 	'description'    => 'Display the output of SHOW FULL PROCESSLIST',
 	'descriptionmsg' => 'showprocesslist-desc',
 );
 
-if ( !function_exists( 'extAddSpecialPage' ) ) {
-	require( dirname(__FILE__) . '/../ExtensionFunctions.php' );
-}
-extAddSpecialPage( dirname(__FILE__) . '/ShowProcesslist_body.php', 'ShowProcesslist', 'ShowProcesslistPage' );
 $dir = dirname(__FILE__) . '/';
-$wgExtensionMessagesFiles['showprocesslist'] = $dir . 'ShowProcesslist.i18n.php';
+$wgExtensionMessagesFiles['ShowProcesslist'] = $dir . 'ShowProcesslist.i18n.php';
+$wgExtensionAliasesFiles['ShowProcesslist'] = $dir . 'ShowProcesslist.alias.php';
+$wgAutoloadClasses['SpecialShowProcesslist'] = $dir . 'ShowProcesslist_body.php';
+$wgSpecialPages['ShowProcesslist'] = 'SpecialShowProcesslist';
