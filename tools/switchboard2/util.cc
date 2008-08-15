81efc565f198f3418290ff5a20d9b4c4df17b995
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-15 06:20:50 +0000

    missing file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39387 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/switchboard2/util.cc
@@ -0,0 +1,60 @@
+/* Copyright (c) 2008 River Tarnell <river@wikimedia.org>. */
+/*
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely. This software is provided 'as-is', without any express or implied
+ * warranty.
+ */
+/* $Id$ */
+
+#include	<sys/types.h>
+#include	<sys/time.h>
+#include	<unistd.h>
+
+#include	<cerrno>
+
+#include	"util.h"
+
+ssize_t
+timed_read(int fd, void *buf, std::size_t nbytes, int timeout)
+{
+	if (timeout > -1) {
+		struct timeval tv;
+		tv.tv_sec = timeout;
+		tv.tv_usec = 0;
+		fd_set set;
+		FD_ZERO(&set);
+		FD_SET(fd, &set);
+		int i = select(fd + 1, &set, NULL, NULL, &tv);
+		if (i == -1)
+			return -1;
+		if (i == 0) {
+			errno = ETIMEDOUT;
+			return -1;
+		}
+	}
+
+	return read(fd, buf, nbytes);
+}
+
+ssize_t
+timed_write(int fd, void const *buf, std::size_t nbytes, int timeout)
+{
+	if (timeout > -1) {
+		struct timeval tv;
+		tv.tv_sec = timeout;
+		tv.tv_usec = 0;
+		fd_set set;
+		FD_ZERO(&set);
+		FD_SET(fd, &set);
+		int i = select(fd + 1, NULL, &set, NULL, &tv);
+		if (i == -1)
+			return -1;
+		if (i == 0) {
+			errno = ETIMEDOUT;
+			return -1;
+		}
+	}
+
+	return write(fd, buf, nbytes);
+}
