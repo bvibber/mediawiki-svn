82d49fd25291b7c4a2b7e0c932f122d2e2a14c0b
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-15 21:04:54 +0000

    * remove dependency on ExtensionFunctions.php * add $wgExtensionAliasesFiles and support in Translate * methods renamed for consistency with other extensions * delayed message loading * removed EOL whitespace * removed some commented out code ($wgExtensionFunctions)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39440 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SignDocument/SignDocument.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$aliases = array();
+
+$aliases['en'] = array(
+	'SignDocument' => array( 'SignDocument' ),
+	'CreateSignDocument' => array( 'CreateSignDocument' ),
+);
