e9d2e142099efe6e865b5bdeecf643734180878e
mfarag <mfarag@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-23 23:56:17 +0000

    * Add aliases for ar and arz * Update links
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@39879 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/gis/Geo.alias.php
+++ extensions/gis/Geo.alias.php
@@ -8,4 +8,16 @@ $aliases = array();
 
 $aliases['en'] = array(
 	'Geo' => array( 'Geo' ),
+
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$aliases['ar'] = array(
+	'Geo' => array( 'جيو' ),
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$aliases['arz'] = array(
+	'Geo' => array( 'جيو' ),
 );
