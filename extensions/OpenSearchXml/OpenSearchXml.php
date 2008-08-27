5848fffcf8b75445ff2b149639f9d86d6175ae6c
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-27 19:56:07 +0000

    Add extension for the OpenSearch suggest XML variant supported by IE 8 beta 2. Includes preliminary versions of some text & image extraction code which will be generalized to other search interfaces pending improved support:
    * OpenSearch suggest JSON interface provides for text extracts, but I believe Firefox doesn't support it currently
    * Image extractions could be used nicely for on-site search, and we'll want it for future geo-type searches.
    * Improved text extraction could be used to update the abstract extension and various other list-like interfaces.
    
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40100 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/OpenSearchXml/OpenSearchXml.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Copyright (C) 2008 Brion Vibber <brion@wikimedia.org>
+ * http://www.mediawiki.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+$wgAPIModules['opensearch'] = 'ApiOpenSearchXml';
+$wgAutoloadClasses['ApiOpenSearchXml'] =
+	dirname(__FILE__) . '/ApiOpenSearchXml.php';
+
+$wgHooks['OpenSearchUrls'][] = 'efOpenSearchXmlUrls';
+
+$wgOpenSearchAdvertiseXml = true;
+
+function efOpenSearchXmlUrls( &$urls ) {
+	global $wgEnableAPI, $wgOpenSearchAdvertiseXml;
+	if( $wgEnableAPI && $wgOpenSearchAdvertiseXml ) {
+		$urls[] = array(
+			'type' => 'application/x-suggestions+xml',
+			'method' => 'get',
+			'template' => efOpenSearchXmlTemplate() );
+
+	}
+	return true;
+}
+
+function efOpenSearchXmlTemplate() {
+	global $wgServer, $wgScriptPath;
+	$ns = implode( ',', SearchEngine::defaultNamespaces() );
+	if(!$ns) $ns = "0";
+	return $wgServer . $wgScriptPath . '/api.php?action=opensearch&format=xml&search={searchTerms}&namespace='.$ns;
+}
