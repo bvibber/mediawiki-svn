f3b73785519f8583c99d8eca543a3c7b38e31833
vasilievvv <vasilievvv@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-29 21:10:31 +0000

    Add CloseWikis extension for better wikis closure without $wgReadOnly and with proper UI for stewards.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40246 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CloseWikis/CloseWikis.i18n.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Internationalization for CloseWikis extension.
+ */
+
+$messages = array();
+
+/** 
+ * English
+ * @author Victor Vasiliev
+ */
+$messages['en'] = array(
+	'closewikis-desc'           => 'Allows to close wiki sites',
+	'closewikis-closed'         => '$1',
+	'closewikis-closed-default' => 'This wiki is closed',
+	'closewikis-page'           => 'Close wiki',
+
+	'closewikis-page-close' => 'Close wiki', 
+	'closewikis-page-close-wiki' => 'Wiki:', 
+	'closewikis-page-close-dreason' => 'Reason (displayed):', 
+	'closewikis-page-close-reason' => 'Reason (logged):',
+	'closewikis-page-close-submit' => 'Close', 
+	'closewikis-page-close-success' => 'Wiki successfully closed',
+	'closewikis-page-reopen' => 'Reopen wiki',
+	'closewikis-page-reopen-wiki' => 'Wiki:', 
+	'closewikis-page-reopen-reason' => 'Reason:',
+	'closewikis-page-reopen-submit' => 'Reopen', 
+	'closewikis-page-reopen-success' => 'Wiki successfully reopen',
+	'closewikis-page-err-nowiki' => 'Invalid wiki specified',
+	'closewikis-page-err-closed' => 'Wiki is already closed',
+	'closewikis-page-err-opened' => 'Wiki is not closed',
+
+	'closewikis-log'         => 'Wikis closure log',
+	'closewikis-log-header'  => 'Here is a log of all wiki closures and reopenings made by steward',
+	'closewikis-log-close'   => 'closed $2',
+	'closewikis-log-reopen'  => 'reopened $2',
+	'right-editclosedwikis'  => 'Edit closed wikis',
+);
---
+++ extensions/CloseWikis/CloseWikis.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ * Copyright (C) 2008 Victor Vasiliev <vasilvv@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) )
+	die();
+
+$wgExtensionCredits['other'][] = array(
+	'name'           => 'CloseWikis',
+	'author'         => 'Victor Vasiliev',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'description'    => 'Allows to close wiki sites',
+	'descriptionmsg' => 'closewikis-desc',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:CloseWikis',
+);
+
+$dir = dirname( __FILE__ );
+$wgExtensionMessagesFiles['CloseWikis'] =  "$dir/CloseWikis.i18n.php";
+$wgHooks['getUserPermissionsErrors'][] = "CloseWikis::userCan";
+
+$wgGroupPermissions['steward']['closewikis'] = true;
+$wgAvailableRights[] = 'closewikis';
+// To be promoted globally
+$wgAvailableRights[] = 'editclosedwikis';
+
+$wgAutoloadClasses['SpecialCloseWiki'] = "$dir/CloseWikis.page.php";
+$wgSpecialPages['CloseWiki'] = 'SpecialCloseWiki';
+
+$wgCloseWikisDatabase = 'closedwikis';
+
+$wgLogTypes[]                     = 'closewiki';
+$wgLogNames['closewiki']          = 'closewikis-log';
+$wgLogHeaders['closewiki']        = 'closewikis-log-header';
+$wgLogActions['closewiki/close']  = 'closewikis-log-close';
+$wgLogActions['closewiki/reopen'] = 'closewikis-log-reopen';
+
+class CloseWikis {
+	static function getSlaveDB() {
+		global $wgCloseWikisDatabase;
+		return wfGetDB( DB_SLAVE, 'closewikis', $wgCloseWikisDatabase );
+	}
+
+	static function getMasterDB() {
+		global $wgCloseWikisDatabase;
+		return wfGetDB( DB_MASTER, 'closewikis', $wgCloseWikisDatabase );
+	}
+
+	static function getList() {
+		global $wgMemc;
+		$cached = $wgMemc->get( 'closedwikis' );
+		if( is_array( $cached ) ) 
+			return $cached;
+		$list = array();
+		$dbr = self::getSlaveDB();
+		$result = $dbr->select( 'closedwikis', '*', false, __METHOD__ );
+		foreach( $result as $row ) {
+			$list[$row->cw_wiki] = $row->cw_reason;
+		}
+		$dbr->freeResult( $result );
+		$wgMemc->set( 'closedwikis', $list );
+		return $list;
+	}
+
+	static function getUnclosedList() {
+		global $wgLocalDatabases;
+		return array_diff( $wgLocalDatabases, array_keys( self::getList() ) );
+	}
+
+	static function userCan( &$title, &$user, $action, &$result ) {
+		$list = self::getList();
+		if( isset( $list[wfWikiID()] ) && !$user->isAllowed( 'editclosedwikis' ) ) {
+			wfLoadExtensionMessages( 'CloseWikis' );
+			$reason = $list[wfWikiID()];
+			$result[] = $reason ?
+				array( 'closewikis-closed', $reason ) :
+				array( 'closewikis-closed-default' );
+			return false;
+		}
+		return true;
+	}
+}
