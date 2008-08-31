e854c24af08ec8e14143915104b9f2d931ace453
skizzerz <skizzerz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-31 20:26:43 +0000

    * committing new extension External image whitelist. (dir/files CamelCase so the path doesn't have spaces - which breaks some systems). Still in beta, not well-tested
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40282 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ExternalImageWhitelist/ExternalImageWhitelist.i18n.php
@@ -0,0 +1,14 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'external_image_whitelist' => " #<pre>Leave this line exactly as it is
+#Put regular expression fragments (just the part that goes between the //) below
+#These will be matched with the URLs of external (hotlinked) images
+#Those that match will be displayed as images, otherwise only a link to the image will be shown
+#Lines beginning with # are treated as comments
+
+#Put all regex fragments ABOVE this line. Leave this line exactly as it is</pre>";
+	'external-image-whitelist' => 'Only allow external (hotlinked) images to be displayed when they match a regex on an on-wiki whitelist';
+);
