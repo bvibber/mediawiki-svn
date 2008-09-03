9bbabb0dafce508deaf48d1bdf29c2262f368712
jdpond <jdpond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-09-03 06:41:07 +0000

    Added FR and IT
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40362 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/DiscussionThreading/DiscussionThreading.i18n.php
+++ extensions/DiscussionThreading/DiscussionThreading.i18n.php
@@ -25,8 +25,20 @@ $wgDiscussionThreadMessages['de'] = array(
         'threadnewsection' => 'neu',
         'threadnewsectionhint' => "Neuen Eintrag erstellen"
 );
+$wgDiscussionThreadMessages['fr'] = array(
+        'replysection' => 'r&eacute;pondre',
+        'replysectionhint' => "R&eacute;pondre à ce commentaire",
+        'threadnewsection' => 'nouveau',
+        'threadnewsectionhint' => "Commencer une nouvelle discussion"
+);
+$wgDiscussionThreadMessages['it'] = array(
+        'replysection' => 'rispondi',
+        'replysectionhint' => "Rispondi a questo messaggio",
+        'threadnewsection' => 'nuovo',
+        'threadnewsectionhint' => "Avvia una nuova discussione"
+);
 /* Need to add 
-	af,br,bs,ca,cs,cy,et,eu,fi,fr,ga,gl,he,hr,hsb,id,is,it,ja,kk-kz,kk-tr,kk-cn,kk,lv,nl,
+	af,br,bs,ca,cs,cy,et,eu,fi,ga,gl,he,hr,hsb,id,is,ja,kk-kz,kk-tr,kk-cn,kk,lv,nl,
 	no,nn,oc,pt,pt-br,ro,ru,sk,sl,sq,uk,wa,zh-cn,zh-tw,zh-yue,zh-hk,zh-sg
 
 	Would do this by adding a new $wgDiscussionThreadMessages array example:
