b9b04e5962a9bda6a5f92a59d9fa0f45bef92db5
mark <mark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-09-16 22:21:36 +0000

    wikimedia-task-search (1.01) hardy; urgency=low
      * Add init script
      * Add logrotate script
      * Create lsearch user account, which does not exist in NIS
      * Remove dependency on 'daemon'
      * OpenJDK 6 is too buggy for indexer usage, so be less restrictive in JDKs
    
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40939 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ debs/wikimedia-task-search/debian/init.d
@@ -0,0 +1,69 @@
+#!/bin/sh
+# chkconfig: 2345 84 16
+# description: MediaWiki Lucene Search daemon
+# vim: autoindent
+
+SERVICE_NAME="Lucene Search daemon"
+BINDIR=/a/search/lucene-search
+LOGDIR=/a/search/log
+
+. /lib/lsb/init-functions
+. /etc/default/rcS
+
+export PATH=/usr/bin:/usr/local/sbin:$PATH
+pid=/var/run/lsearchd.pid
+
+case "$1" in
+	start)
+		# Check if running
+		if [ -s $pid ] && kill -0 $(cat /var/run/lsearchd.pid) >/dev/null 2>&1; then
+                	log_progress_msg "Already running"
+                	log_end_msg 0
+                	exit 0
+	        fi
+		log_daemon_msg "Starting $SERVICE_NAME"
+
+		# Increase FD limit
+		ulimit -n 8192
+		# Run the daemon
+		if start-stop-daemon --start --quiet --background --user lsearch --chuid lsearch --pidfile $pid --make-pidfile --exec /usr/bin/java -- -Xmx1500m -Djava.rmi.server.codebase=file://$BINDIR/LuceneSearch.jar -Djava.rmi.server.hostname=$HOSTNAME -jar $BINDIR/LuceneSearch.jar
+		then
+			rc=0
+			sleep 1
+			if ! kill -0 $(cat /var/run/lsearchd.pid) >/dev/null 2>&1; then
+	                    log_failure_msg "$SERVICE_NAME failed to start"
+			    rc=1
+	                fi
+		fi
+
+		if [ $rc -eq 0 ]; then
+	                log_end_msg 0
+	        else
+        	        log_end_msg 1
+                	rm -f /var/run/lsearchd.pid
+	        fi
+		;;
+	stop)
+		log_daemon_msg "Stopping $SERVICE_NAME"
+		start-stop-daemon --stop --quiet --oknodo --pidfile $pid
+		log_end_msg $?
+		rm -f /var/run/lsearchd.pid
+		echo
+		;;
+	restart)
+		$0 stop
+		sleep 5s
+		$0 start
+		;;
+	status)
+		if [ -s $pid ] && kill -0 $(cat /var/run/lsearchd.pid) >/dev/null 2>&1; then
+			echo "$SERVICE_NAME is running"
+		else
+			echo "$SERVICE_NAME is not running"
+		fi
+		;;
+	*)
+		echo "Usage: $0 {start|stop|status|restart}"
+		exit 1
+		;;
+esac
