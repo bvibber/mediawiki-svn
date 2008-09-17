415e2234d2c9977e30c3ed33e9e228c208df3286
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-09-17 09:19:28 +0000

    avoid some invalid diff links
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@40952 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/CodeRevisionView.php
+++ extensions/CodeReview/CodeRevisionView.php
@@ -106,16 +106,18 @@ class CodeRevisionView extends CodeView {
 		$desc = wfMsgHtml( 'code-rev-modified-'.strtolower( $action ) );
 		$encPath = htmlspecialchars( $path );
 		$viewvc = $this->mRepo->getViewVcBase();
+		$diff = wfMsgHtml('code-rev-diff');
 		if( $viewvc ) {
 			$rev = $this->mRev->getId();
 			$prev = $rev - 1;
 			$safePath = wfUrlEncode( $path );
 			$link = $this->mSkin->makeExternalLink( "$viewvc$safePath?view=markup&pathrev=$rev", $encPath );
-			$diff = $this->mSkin->makeExternalLink( "$viewvc$safePath?&pathrev=$rev&r1=$prev&r2=$rev", 
-				wfMsgHtml('code-rev-diff') );
+			if( $action !== 'A' ) {
+				$diff = $this->mSkin->makeExternalLink( "$viewvc$safePath?&pathrev=$rev&r1=$prev&r2=$rev", 
+					wfMsgHtml('code-rev-diff') );
+			}
 		} else {
 			$link = $encPath;
-			$diff = wfMsgHtml('code-rev-diff');
 		}
 		return "<li>$link ($desc) ($diff)</li>\n";
 	}
