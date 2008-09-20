cb0d7250b4ea97198079e3508cad163dd8fc9941
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-09-20 14:24:36 +0000

    add UserWelcome extension - a cute little parser hook to show some social info to the user when SocialProfile extension (v. 1.3) is installed on the wiki. original code by Wikia, Inc. and some cleanup & Finnish translations by me
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@41070 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UserWelcome/UserWelcome.i18n.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Internationalization file for UserWelcome extension.
+ *
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Wikia, Inc.
+ */
+$messages['en'] = array(
+	'mp-userlevels-link' => 'User Levels', // This is the help page...e.g. English default is Help:User Levels -- you can define a new help page with this msg.
+	'mp-welcome-upload' => 'Upload',
+	'mp-welcome-edit' => 'Edit',
+	'mp-welcome-points' => '$1 points',
+	'mp-welcome-needed-points' => 'To advance to <b><a href="$1">$2</a></b> earn <i>$3</i> more points!',
+	'mp-welcome-logged-in' => 'Hello $1',
+	'mp-requests-title' => 'Requests',
+	'mp-requests-message' => 'You have the following requests.',
+	'mp-request-new-message' => 'new message',
+	'mp-request-new-award' => '$1 new {{PLURAL:$1|award|awards}}',
+	'mp-request-new-gift' => '$1 new {{PLURAL:$1|gift|gifts}}',
+	'mp-request-new-foe' => '$1 new {{PLURAL:$1|foe|foes}}',
+	'mp-request-new-friend' => '$1 new {{PLURAL:$1|friend|friends}}',
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix
+ */
+$messages['fi'] = array(
+	'mp-userlevels-link' => 'Käyttäjätasot',
+	'mp-welcome-upload' => 'Tallenna',
+	'mp-welcome-edit' => 'Muokkaa',
+	'mp-welcome-points' => '$1 pistettä',
+	'mp-welcome-needed-points' => 'Päästäksesi tasolle <b><a href="$1">$2</a></b>, ansaitse <i>$3</i> pistettä lisää!',
+	'mp-welcome-logged-in' => 'Hei $1',
+	'mp-requests-title' => 'Pyynnöt',
+	'mp-requests-message' => 'Sinulla on seuraavat pyynnöt.',
+	'mp-request-new-message' => 'uusi viesti',
+	'mp-request-new-award' => '$1 {{PLURAL:$1|uusi palkinto|uutta palkintoa}}',
+	'mp-request-new-gift' => '$1 {{PLURAL:$1|uusi lahja|uutta lahjaa}}',
+	'mp-request-new-foe' => '$1 {{PLURAL:$1|uusi vihollinen|uutta vihollista}}',
+	'mp-request-new-friend' => '$1 {{PLURAL:$1|uusi ystävä|uutta ystävää}}',
+);
