0079ff7ec761db8928092878040180c9ae32fd55
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-09-21 15:15:46 +0000

    * findSettings.php now works for extensions (--ext option) * Updated extensions settings
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@41117 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Configure/findSettings.php
+++ extensions/Configure/findSettings.php
@@ -26,10 +26,11 @@ function printHelp(){
 	echo "Script that find settings that aren't configurable by the extension.\n";
 	echo "\n";
 	echo "Usage:\n";
-	echo "  php findSettings.php [--help|--from-doc [--alpha]]\n";
+	echo "  php findSettings.php [--help|--ext|--from-doc [--alpha]]\n";
 	echo "\n";
 	echo "options:\n";
 	echo "--help: display this screen\n";
+	echo "--ext: search for extensions settings\n";
 	echo "--from-doc: compare with settings from mediawiki.org instead settings\n";
 	echo "            from this extension\n";
 	echo "--alpha: get the alphabetical list of settings\n";
@@ -53,38 +54,79 @@ function printArray( $msg, $arr, $sort = true ) {
 if( isset( $options['help'] ) )
 	printHelp();
 
-// Get our settings defs
-if( isset( $options['from-doc'] ) ){
-	if( isset( $options['alpha'] ) ){
-		$page = "Manual:Configuration_settings_(alphabetical)";
-	} else {
-		$page = "Manual:Configuration_settings";
+$coreSettings = ConfigurationSettings::singleton( CONF_SETTINGS_CORE );
+if( isset( $options['ext'] ) ){
+	$exts = ConfigurationSettings::singleton( CONF_SETTINGS_EXT )->getAllExtensionsObjects();
+	$ingnoreList = array(
+		# Core
+		'wgTitle', 'wgArticle', 'wgContLang', 'wgLang', 'wgOut', 'wgParser', 'wgMessageCache',
+		# Extensions
+		'wgCaptcha', 'wgConfirmEditIP',
+		'wgCitationCache', 'wgCitationCounter', 'wgCitationRunning',
+		'wgCSS',
+		'wgErrorHandlerErrors', 'wgErrorHandlerOutputDone',
+		'wgExtParserFunctions',
+		'wgTitleBlacklist',
+	);
+	foreach( $exts as $ext ){
+		if( !$ext->isInstalled() )
+			continue;
+		$file = file_get_contents( $ext->getFile() );
+		$name = $ext->getName();
+		$m = array();
+		preg_match_all( '/\$(wg[A-Za-z0-9]+)\s*\=/', $file, $m );
+		$definedSettings = array_unique( $m[1] );
+		$allSettings = array_keys( $ext->getSettings() );
+		
+		$remain = array_diff( $definedSettings, $allSettings );
+		$obsolete = array_diff( $allSettings, $definedSettings );
+		$missing = array();
+		foreach( $remain as $setting ){
+			if( !$coreSettings->isSettingAvailable( $setting ) && !in_array( $setting, $ingnoreList ) )
+				$missing[] = $setting;
+		}
+		if( count( $missing ) == 0 && count( $obsolete ) == 0 ) {
+			#echo "Extension $name ok\n";
+		} else {
+			echo "Extension $name:\n";
+			printArray( '  missing', $missing );
+			printArray( '  obsolete', $obsolete );
+		}
 	}
-	$cont = Http::get( "http://www.mediawiki.org/w/index.php?title={$page}&action=raw" );
-	$m = array();
-	preg_match_all( '/\[\[[Mm]anual:\$(wg[A-Za-z0-9]+)\|/', $cont, $m );
-	$allSettings = array_unique( $m[1] );
 } else {
-	$allSettings = array_keys( ConfigurationSettings::singleton( CONF_SETTINGS_CORE )->getAllSettings() );
+	// Get our settings defs
+	if( isset( $options['from-doc'] ) ){
+		if( isset( $options['alpha'] ) ){
+			$page = "Manual:Configuration_settings_(alphabetical)";
+		} else {
+			$page = "Manual:Configuration_settings";
+		}
+		$cont = Http::get( "http://www.mediawiki.org/w/index.php?title={$page}&action=raw" );
+		$m = array();
+		preg_match_all( '/\[\[[Mm]anual:\$(wg[A-Za-z0-9]+)\|/', $cont, $m );
+		$allSettings = array_unique( $m[1] );
+	} else {
+		$allSettings = array_keys( $coreSettings->getAllSettings() );
+	}
+	
+	// Now we'll need to open DefaultSettings.php
+	$m = array();
+	$defaultSettings = file_get_contents( "$IP/includes/DefaultSettings.php" );
+	preg_match_all( '/\$(wg[A-Za-z0-9]+)\s*\=/', $defaultSettings, $m );
+	$definedSettings = array_unique( $m[1] );
+	
+	$missing = array_diff( $definedSettings, $allSettings );
+	$remain = array_diff( $allSettings, $definedSettings );
+	$obsolete = array();
+	foreach( $remain as $setting ){
+		if( $coreSettings->isSettingAvailable( $setting ) )
+			$obsolete[] = $setting;
+	}
+	
+	// let's show the results:
+	printArray('missing', $missing );
+	printArray('obsolete', $obsolete );
+	 
+	if( count( $missing ) == 0 && count( $obsolete ) == 0 ) 
+		echo "Looks good!\n";
 }
-
-// Now we'll need to open DefaultSettings.php
-$m = array();
-$defaultSettings = file_get_contents( "$IP/includes/DefaultSettings.php" );
-preg_match_all( '/\$(wg[A-Za-z0-9]+)\s*\=/', $defaultSettings, $m );
-$definedSettings = array_unique( $m[1] );
-
-$missing = array_diff( $definedSettings, $allSettings );
-$remain = array_diff( $allSettings, $definedSettings );
-$obsolete = array();
-foreach( $remain as $setting ){
-	if( ConfigurationSettings::singleton( CONF_SETTINGS_CORE )->isSettingAvailable( $setting ) )
-		$obsolete[] = $setting;
-}
-
-// let's show the results:
-printArray('missing', $missing );
-printArray('obsolete', $obsolete );
- 
-if( count( $missing ) == 0 && count( $obsolete ) == 0 ) 
-	echo "Looks good!\n";
