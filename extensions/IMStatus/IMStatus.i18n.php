306379463ed7f2d3330dcc25ca703eb433174cd5
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-05 20:00:58 +0000

    Import IMStatus as download from http://www.patheticcockroach.com/mpam4/files/IMStatus_latest.zip on 2008-10-05 21:53:51 CEST
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@41707 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/IMStatus/IMStatus.i18n.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Internationalisation file for IM Status extension
+ *
+ * @ingroup Extensions
+ */
+ 
+$messages = array();
+ 
+$messages['en'] = array(
+        'imstatus_desc' => 'Adds tags to show various IM online status (AIM, Google Talk, ICQ, MSN/Live Messenger, Skype, Xfire, Yahoo)',
+        'imstatus_syntax' => 'Syntax',
+        'imstatus_default' => 'Default',
+        'imstatus_example' => 'Example',
+        'imstatus_possible_val' => 'Possible values',
+        'imstatus_max' => 'max',
+        'imstatus_min' => 'min',
+        'imstatus_or' => 'or',
+        'imstatus_style' => 'style of the status indicator',
+        'imstatus_action' => 'action when the button is clicked',
+        'imstatus_details_saa' => 'For more details about all the styles and actions, see $1.',
+        'imstatus_your_name' => 'your $1 name',
+ 
+        'imstatus_aim_presence' => '$1 shows your status with a link that will launch AIM (provided the user has it installed) to send you an IM.',
+        'imstatus_aim_api' => '$1 shows your status with a link that will launch a <b>browser</b>, javascript version of AIM to send you an IM.',
+ 
+        'imstatus_gtalk_code' => 'your google talk code',
+        'imstatus_gtalk_get_code' => 'your google talk code: get it at $1.',
+        'imstatus_gtalk_height' => 'height of the box, in pixel.',
+        'imstatus_gtalk_width' => 'width of the box, in pixel.',
+ 
+        'imstatus_icq_id' => 'your ICQ id',
+        'imstatus_icq_style' => 'a number ranging from 0 to 26 (yes, there are 27 available styles...).',
+ 
+        'imstatus_live_code' => 'your Live Messenger website id',
+        'imstatus_live_get_code' => 'your Live Messenger website id: <strong>this is not your e-mail address</strong>, you need to generate one in 
+<a href="$1">your live messenger options</a>. The id you need to provide is the numbers and letters between "$2" and "$3".',
+ 
+        'imstatus_skype_nbstyle' => 'NB: if you choose a style which is also an action, your action choice will be overridden by the action matching your chosen style.',
+ 
+        'imstatus_xfire_size' => 'the button size, from $1 (biggest) to $2 (smallest).',
+ 
+        'imstatus_yahoo_style' => 'the button style, from $1 (smallest) to $2 (biggest), $3 and $4 are for voicemail.',
+);
+$messages['de'] = array(
+        'imstatus_desc' => 'Fügt Tags hinzu um den Online-Status verschiedener Instant-Messenger anzuzeigen (AIM, Google Talk, ICQ, MSN/Live Messenger, Skype, Xfire, Yahoo)',
+        'imstatus_syntax' => 'Syntax',
+        'imstatus_default' => 'Standard',
+        'imstatus_example' => 'Beispiel',
+        'imstatus_possible_val' => 'Mögliche Werte',
+        'imstatus_max' => 'max',
+        'imstatus_min' => 'min',
+        'imstatus_or' => 'oder',
+        'imstatus_style' => 'Stil der Status-Anzeige',
+        'imstatus_action' => 'Aktion beim klicken des Buttons',
+        'imstatus_details_saa' => 'Für mehr Details zu den Stilen und Aktionen, siehe $1.',
+        'imstatus_your_name' => 'dein $1 Name',
+ 
+        'imstatus_aim_presence' => '$1 zeigt deinen Status mit einem Link der AIM startet (wenn er es installiert hat) um dir eine Nachricht zu senden.',
+        'imstatus_aim_api' => '$1 zeigt deinen Status mit einem Link der eine <b>Browser</b>, JavaScript Version von AIM startet um die eine nachricht zu senden.',
+ 
+        'imstatus_gtalk_code' => 'dein Google-Talk Code',
+        'imstatus_gtalk_get_code' => 'dein Google-Talk Code kriegst du bei $1.',
+        'imstatus_gtalk_height' => 'Höhe der Box in Pixel.',
+        'imstatus_gtalk_width' => 'Breite der Box in Pixel.',
+ 
+        'imstatus_icq_id' => 'deine ICQ-UIN',
+        'imstatus_icq_style' => 'eine Zahl zwischen 0 und 26 (ja, es gibt 27 verschiedene Styles...).',
+ 
+        'imstatus_live_code' => 'deine Live Messenger Website Id',
+        'imstatus_live_get_code' => 'deine Live Messenger Website Id: <strong>das ist nicht deine E-Mail-Adresse</strong>, du musst die eine in <a href="$1">deinen Live Messenger Optionen</a> generieren. Die Id die du benötigst sind die Zahlen und Buchstaben zwischen "$2" und "$3".',
+ 
+        'imstatus_skype_nbstyle' => 'NB: wenn du einen Style aussuchst der auch eine Aktion beinhaltet wird deine Aktions-Auswahl durch die Aktions des Styles ersetzt.',
+ 
+        'imstatus_xfire_size' => 'die Größe des Buttons, von $1 (größte) bis $2 (kleinste).',
+ 
+        'imstatus_yahoo_style' => 'der Stil des Buttons, von $1 (kleinste) bis $2 (größte), $3 und $4 sind für Voicemail.',
+);
+?>
