049c5b4267578c1a5c301bcc593f09e03157ef30
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-08 08:30:32 +0000

    Improve messages, update formatting
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@41834 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PlayerStatsGrabber/PlayerStatsGrabber.i18n.php
+++ extensions/PlayerStatsGrabber/PlayerStatsGrabber.i18n.php
@@ -1,37 +1,35 @@
 <?php
 $messages = array();
 
-$messages['en'] = array( 
-        'playerstatsgrabber' => 'Player Stats Survey',
-		'ps_take_video_survey'=> 'Video Playback Survey',
-		'ps_survey_description'=> 
-			'This Survey will help wikipedia improve support for rich media. Please answer the following questions:',
-		'ps_could_play'=>'When you clicked "play", did the video playback perfectly with audio?',
-		'ps_play_yes'			=>'Yes, the clip played perfectly',
-		'ps_play_no'			=>'No, had issues or did not play',
+$messages['en'] = array(
+        'playerstatsgrabber' => 'Player stats survey',
+	'ps_take_video_survey'=> 'Video playback survey',
+	'ps_survey_description'=> 'This survey will help improve support for rich media.
+Please answer the following questions:',
+	'ps_could_play'=>'When you clicked "play", did the video playback perfectly with audio?',
+	'ps_play_yes' =>'Yes, the clip played perfectly',
+	'ps_play_no' =>'No, there were issues or it did not play',
 
-		'ps_problem_checkoff'	=> 'Please mark any of the following problems if they occurred:',
-		
-		'ps_no_video'			=> 'No Video played back at all',
-		'ps_jumpy_playback'		=>' Jumping playback (the video played then paused then played)',
-		'ps_no_sound'			=> 'No Audio, (My Computer Volume is ON but I hear no audio for this video)',
-		'ps_bad_sync'			=> 'Audio was out of sync with the video',
+	'ps_problem_checkoff' => 'Please mark any of the following problems if they occurred:',
 
-		'ps_problems_desc'		=> 'Please describe any additional issues bellow (optional):',
+	'ps_no_video' => 'No video played back at all',
+	'ps_jumpy_playback' =>' Jumping playback (the video played,, then paused then played)',
+	'ps_no_sound' => 'No audio, (my computer volume is on, but I hear no audio for this video)',
+	'ps_bad_sync' => 'Audio was out of sync with the video',
 
-		'ps_would_install'		=>'As a Wikipedia user, would you install an additional plug-in to view
-videos on Wikipedia?',
-		'ps_no_install'			=>'No, I would not install additional plug-in',
-		'ps_yes_install'		=>'Yes, I would install an additional plug-in',
+	'ps_problems_desc' => 'Please describe any additional issues below (optional):',
 
-		'ps_would_switch'		=>'You appear to be running Internet Explore. As a Wikipedia user, would you consider installing a different browser to improve your media experience on Wikipedia?',					
-		'ps_yes_switch'			=>'Yes I would switch browsers',
-		'ps_no_switch'			=>'No I would switch browsers',
-		
-		'ps_your_email'			=>'your email address (optional) ',
-		'ps_submit_survey'		=>'Submit Survey',
-		'ps_privacy'			=>'Privacy link or note goes here',
-		'ps_thanks'				=>'Thank you for Participating in this Video Survey'
+	'ps_would_install' =>'Would you install an additional plug-in to view videos on this wiki?',
+	'ps_no_install' =>'No, I would not install an additional plug-in',
+	'ps_yes_install' =>'Yes, I would install an additional plug-in',
+
+	'ps_would_switch' =>'You appear to be running Internet Explorer.
+Would you consider installing a different browser to improve your media experience on this wiki?',
+	'ps_yes_switch' =>'Yes, I would switch browsers',
+	'ps_no_switch' =>'No, I would switch browsers',
+
+	'ps_your_email' =>'Your email address (optional)',
+	'ps_submit_survey' =>'Submit survey',
+	'ps_privacy' =>'Privacy link or note goes here',
+	'ps_thanks' =>'Thank you for participating in this video survey',
 );
-        
-?>
