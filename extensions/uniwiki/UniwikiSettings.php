7966d5021d4f4b17986b9a5ac6716db4048a30df
mej <mej@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-17 16:28:20 +0000

    added UniwikiSettings.php for default UNIWIKI config
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42172 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/uniwiki/UniwikiSettings.php
@@ -0,0 +1,18 @@
+<?php
+
+# default settings for UNIWIKI extension package
+$wgLogo = "$wgScriptPath/extensions/uniwiki/uniwiki.png"; 
+$uw = "$IP/extensions/uniwiki"; 
+require_once("$uw/CssHooks/CssHooks.php"); 
+require_once("$uw/Javascript/Javascript.php"); 
+require_once("$uw/MooTools12core/MooTools12core.php"); 
+require_once("$uw/AutoCreateCategoryPages/AutoCreateCategoryPages.php"); 
+require_once("$uw/GenericEditPage/GenericEditPage.php"); 
+require_once("$uw/CatBoxAtTop/CatBoxAtTop.php"); 
+require_once("$uw/Layouts/Layouts.php"); 
+require_once("$uw/Authors/Authors.php"); 
+require_once("$uw/CustomToolbar/CustomToolbar.php"); 
+require_once("$uw/CreatePage/CreatePage.php"); 
+require_once("$uw/FormatChanges/FormatChanges.php"); 
+require_once("$uw/FormatSearch/FormatSearch.php");
+require_once("$uw/FormatHistory/FormatHistory.php");
