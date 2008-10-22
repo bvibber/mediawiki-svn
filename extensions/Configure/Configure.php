9a0910f6e31254b9eda84c23fd9afbb0616ec6c8
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-22 17:09:57 +0000

    Added Special:Configure, Special:Extensions and Special:ViewConfig to "Wiki data and tools" group on Special:SpecialPages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42338 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Configure/Configure.php
+++ extensions/Configure/Configure.php
@@ -17,7 +17,7 @@ $wgExtensionCredits['specialpage'][] = array(
 	'url' => 'http://www.mediawiki.org/wiki/Extension:Configure',
 	'description' => 'Allow authorised users to configure the wiki by a web-based interface',
 	'descriptionmsg' => 'configure-desc',
-	'version' => '0.8.6',
+	'version' => '0.8.7',
 );
 
 ## Configuration part
@@ -163,6 +163,13 @@ define( 'CONF_SETTINGS_BOTH', 3 );
 
 ## Helper for configuration settings
 $wgAutoloadClasses['ConfigurationSettings'] = $dir . 'Configure.settings.php';
+
+## Groups
+if( isset( $wgSpecialPageGroups ) && is_array( $wgSpecialPageGroups ) ){
+ 	$wgSpecialPageGroups['Configure'] = 'wiki';
+ 	$wgSpecialPageGroups['Extensions'] = 'wiki';
+ 	$wgSpecialPageGroups['ViewConfig'] = 'wiki';
+}
 
 ## Diff stuff
 $wgAutoloadClasses['ConfigurationDiff'] = $dir . 'Configure.diff.php';
