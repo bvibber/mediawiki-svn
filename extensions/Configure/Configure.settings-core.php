9dad356e00171a71203c111590b54e0fa83e45df
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-23 19:06:47 +0000

    Added $wgNamespacesToBeSearchedProject
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42389 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Configure/Configure.settings-core.php
+++ extensions/Configure/Configure.settings-core.php
@@ -238,6 +238,7 @@ $settings = array(
 			'wgNamespaceProtection' => 'array',
 			'wgNamespaceRobotPolicies' => 'array',
 			'wgNamespacesToBeSearchedDefault' => 'array',
+			'wgNamespacesToBeSearchedProject' => 'array',
 			'wgNamespacesWithSubpages' => 'array',
 			'wgNoFollowNsExceptions' => 'array',
 			'wgNonincludableNamespaces' => 'array',
@@ -754,6 +755,7 @@ $arrayDefs = array(
 	'wgNamespaceProtection' => 'ns-array',
 	'wgNamespaceRobotPolicies' => 'ns-text',
 	'wgNamespacesToBeSearchedDefault' => 'ns-bool',
+	'wgNamespacesToBeSearchedProject' => 'ns-bool',
 	'wgNamespacesWithSubpages' => 'ns-bool',
 	'wgNoFollowNsExceptions' => 'ns-text',
 	'wgNonincludableNamespaces' => 'ns-simple',
@@ -1187,6 +1189,7 @@ $settingsVersion = array(
 	'wgEdititis' => array( array( '1.14alpha', '>=' ) ),
 	'wgMaxAnimatedGifArea' => array( array( '1.14alpha', '>=' ) ),
 	'wgMaxShellTime' => array( array( '1.14alpha', '>=' ) ),
+	'wgNamespacesToBeSearchedProject' => array( array( '1.14alpha', '>=' ) ),
 ## Obsolete
 	'wgProfileSampleRate' => array( array( '1.8alpha', '<' ) ),
 	'wgProfilerType' => array( array( '1.8alpha', '<' ) ),
