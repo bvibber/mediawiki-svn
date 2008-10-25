539c13a4af2f787113f97ed803e61e53ef4b3572
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-25 21:31:31 +0000

    ...and that needs i18n as well.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42580 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/NssMySQLAuth/NssMySQLAuth.i18n.php
+++ extensions/NssMySQLAuth/NssMySQLAuth.i18n.php
@@ -12,6 +12,8 @@ $messages['en'] = array(
 	'nss-save-changes'	=> 'Save changes',
 	'nss-create-account-header'	=> 'Create new account',
 	'nss-create-account'	=> 'Create account',
+	'nss-welcome-mail'	=> 'An account with username $1 and password $2 has been created for you.',
+	'nss-welcome-mail-subject' => 'Account creation',
 	
 	'nss-db-error' => 'Error reading from authentication database'
 );
