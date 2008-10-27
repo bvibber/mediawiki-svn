4b8370e58febe597e9cad5e0869771eb19586504
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-27 22:09:44 +0000

    Update logentry-parserhook-desc per r42690
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42692 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LogEntry/LogEntry.i18n.php
+++ extensions/LogEntry/LogEntry.i18n.php
@@ -13,7 +13,7 @@ $messages = array();
  */
 $messages['en'] = array(
 	'logentry-append' => 'Append',
-	'logentry-parserhook-desc' => 'This tag extension provides a form for appending to log pages',
+	'logentry-parserhook-desc' => 'This tag extension provides a form for appending/prepending to log pages',
 	'logentry-specialpage-desc' => 'This tag extension provides processing for appending to log pages',
 	'logentry' => 'LogEntry',
 	'logentry-invalidpage' => 'Invalid page',
