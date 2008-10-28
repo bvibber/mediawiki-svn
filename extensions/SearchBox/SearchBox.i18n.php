765f61ad6078a862255b86c8a54c6bc5d94ebf20
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-10-28 18:53:53 +0000

    Initial import.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@42742 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SearchBox/SearchBox.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Inernationalization for SearchBox extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'searchbox-desc' => 'Allow inclusion of search forms',
+	'searchbox-search' => 'Search',
+	'searchbox-go' => 'Go',
+	'searchbox-namespaces' => 'Namespaces:'
+);
