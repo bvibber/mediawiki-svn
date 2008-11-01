b5ce1d328104756589c3ef5d2290b2412fc07640
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-01 21:03:58 +0000

    Update for uniwiki/AutoCreateCategoryPages: * standard i18n, change message IDs * use wfMsgForContent in edit summaries and page creation * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43026 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/uniwiki/AutoCreateCategoryPages/AutoCreateCategoryPages.i18n.php
+++ extensions/uniwiki/AutoCreateCategoryPages/AutoCreateCategoryPages.i18n.php
@@ -1,28 +1,35 @@
 <?php
-/* vim: noet ts=4 sw=4
- * http://www.mediawiki.org/wiki/Extension:Uniwiki_Auto_Create_Category_Pages
- * http://www.gnu.org/licenses/gpl-3.0.txt */
+/**
+ * Internationalisation for Uniwiki/AutoCreateCategoryPages extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
-if (!defined("MEDIAWIKI"))
-	die();
+$messages = array();
 
-$wgAutoCreateCategoryPagesMessages = array();
-$wgAutoCreateCategoryPagesMessages['en'] = array(
-	'accp_stub'      => "This is a category page.  It lists all of the pages in category \"$1\" as well as all subcategories of category \"$1\" if any exist.",
-	'accp_createdby' => "Created automatically by the AutoCreateCategoryPage extension."
+/** English */
+$messages['en'] = array(
+	'autocreatecategorypages-desc' => 'Create missing category pages automatically on save',
+	'autocreatecategorypages-stub' => 'This is a category page.
+It lists all of the pages in category "$1" as well as all subcategories of category "$1" if any exist.',
+	'autocreatecategorypages-createdby' => 'Created automatically by the AutoCreateCategoryPages extension.'
 );
 
-$wgAutoCreateCategoryPagesMessages['es'] = array(
-	'accp_stub'      => "Esta es una pÃ¡gina de categorÃ­as.  AquÃ­ se enumeran todas las pÃ¡ginas en la categorÃ­a \"$1\" asÃ­ como todas las subcategorÃ­as en la categorÃ­a \"$1\" si las hubiere.",
-	'accp_createdby' => "Creado automÃ¡ticamente por la extensiÃ³n AutoCreateCategoryPe."
+$messages['es'] = array(
+	'autocreatecategorypages-stub' => 'Esta es una página de categorías.
+Aqx se enumeran todas las páginas en la categoría "$1" asx como todas las subcategorías en la categoría "$1" si las hubiere.',
+	'autocreatecategorypages-createdby' => 'Creado automáticamente por la extensión AutoCreateCategoryPages.'
 );
 
-$wgAutoCreateCategoryPagesMessages['de'] = array(
-	'accp_stub'      => "Dies ist eine Kategorie Übersicht. Es listet alle Seiten in der Kategorie \"$1\" sowie alle Unterkategorien der Kategorie \"$1\" wenn sie existiert.",
-	'accp_createdby' => "Erstellt automatisch von der AutoCreateCategoryPage Erweiterung"
+$messages['de'] = array(
+	'autocreatecategorypages-stub' => 'Dies ist eine Kategorie Übersicht.
+Es listet alle Seiten in der Kategorie "$1" sowie alle Unterkategorien der Kategorie "$1" wenn sie existiert.',
+	'autocreatecategorypages-createdby' => 'Erstellt automatisch von der AutoCreateCategoryPages Erweiterung'
 );
 
-$wgAutoCreateCategoryPagesMessages['pt-br'] = array(
-	'accp_stub'      => "Esta Ã© uma pÃ¡gina de categoria.  Ela lista todas as pÃ¡ginas da categoria \"$1\", bem como todas as subcategorias da categoria \"$1\", se existirem.",
-	'accp_createdby' => "Gerada automaticamente pela extensÃ£o AutoCreateCategoryPag."
+$messages['pt-br'] = array(
+	'autocreatecategorypages-stub' => 'Esta Ã© uma página de categoria.
+Ela lista todas as páginas da categoria "$1", bem como todas as subcategorias da categoria "$1", se existirem.',
+	'autocreatecategorypages-createdby' => 'Gerada automaticamente pela extensxo AutoCreateCategoryPages.'
 );
--- extensions/uniwiki/AutoCreateCategoryPages/AutoCreateCategoryPages.php
+++ extensions/uniwiki/AutoCreateCategoryPages/AutoCreateCategoryPages.php
@@ -3,32 +3,27 @@
  * http://www.mediawiki.org/wiki/Extension:Uniwiki_Auto_Create_Category_Pages
  * http://www.gnu.org/licenses/gpl-3.0.txt */
 
-if (!defined('MEDIAWIKI'))
+if ( !defined( 'MEDIAWIKI' ) )
 	die();
 
 /* ---- CREDITS ---- */
 $wgExtensionCredits['other'][] = array(
-	'name'        => "AutoCreateCategoryPages",
-	'author'      => "Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)",
-	'description' => "Create stub Category pages automatically"
+	'name'           => 'AutoCreateCategoryPages',
+	'author'         => 'Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)',
+	'description'    => 'Create stub Category pages automatically',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:Uniwiki/AutoCreateCategoryPages',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'descriptionmsg' => 'autocreatecategorypages-desc',
 );
 
-/* ---- INTERNATIONALIZATION ---- */
-
-require_once ("AutoCreateCategoryPages.i18n.php");
-$wgExtensionFunctions[] = "AutoCreateCategoryPages_i18n";
-
-function AutoCreateCategoryPages_i18n() {
-	// add this extension's messages to the message cache
-	global $wgMessageCache, $wgAutoCreateCategoryPagesMessages;
-	foreach ($wgAutoCreateCategoryPagesMessages as $lang => $messages)
-		$wgMessageCache->addMessages ($messages, $lang);
-}
+$wgExtensionMessagesFiles['AutoCreateCategoryPages'] = dirname( __FILE__ ) . '/AutoCreateCategoryPages.i18n.php';
 
 /* ---- HOOKS ---- */
 $wgHooks['ArticleSaveComplete'][] = "UW_AutoCreateCategoryPages_Save";
-function UW_AutoCreateCategoryPages_Save (&$article, &$user, &$text, &$summary, &$minoredit,
-	&$watchthis, &$sectionanchor, &$flags, $revision) {
+
+function UW_AutoCreateCategoryPages_Save ( &$article, &$user, &$text, &$summary, &$minoredit,
+	&$watchthis, &$sectionanchor, &$flags, $revision ) {
 	global $wgDBprefix;
 
 	/* after the page is saved, get all the categories
@@ -37,38 +32,38 @@ function UW_AutoCreateCategoryPages_Save (&$article, &$user, &$text, &$summary, 
 
 	// extract the categories on this page
 	$regex = "/\[\[category:(.+?)(?:\|.*)?\]\]/i";
-	preg_match_all ($regex, $text, $matches);
+	preg_match_all ( $regex, $text, $matches );
 
 	// array of the categories on the page (in db form)
 	$on_page = array();
-	foreach ($matches[1] as $cat)
-		$on_page[] = Title::newFromText ($cat)->getDBkey();
+	foreach ( $matches[1] as $cat )
+		$on_page[] = Title::newFromText ( $cat )->getDBkey();
 
 	// array of the categories in the db
-	$db = wfGetDB (DB_MASTER);
-	$results = $db->resultObject ($db->query(
+	$db = wfGetDB ( DB_MASTER );
+	$results = $db->resultObject ( $db->query(
 		"select distinct page_title from {$wgDBprefix}page " .
-		"where page_namespace = '".NS_CATEGORY."'"));
+		"where page_namespace = '" . NS_CATEGORY . "'" ) );
 
 	$in_db = array();
-	while ($r = $results->next())
+	while ( $r = $results->next() )
 		$in_db[] = $r->page_title;
 
 	/* loop through the categories in the page and
 	 * see if they already exist as a category page */
-	foreach ($on_page as $db_key) {
-		if (!in_array( $db_key, $in_db)) {
+	foreach ( $on_page as $db_key ) {
+		if ( !in_array( $db_key, $in_db ) ) {
 
 			// if it doesn't exist, then create it here
-			$page_title = Title::newFromDBkey ($db_key)->getText();
-			$stub = wfMsg ("accp_stub", $page_title);
-			$summary = wfMsg ("accp_createdby");
-			$article = new Article (Title::newFromDBkey("Category:$db_key"));
+			$page_title = Title::newFromDBkey ( $db_key )->getText();
+			$stub = wfMsgForContent ( 'autocreatecategorypages-stub', $page_title );
+			$summary = wfMsgForContent ( 'autocreatecategorypages-createdby' );
+			$article = new Article ( Title::newFromDBkey( "Category:$db_key" ) );
 
 			try {
-				$article->doEdit ($stub, $summary, EDIT_NEW & EDIT_SUPPRESS_RC);
+				$article->doEdit ( $stub, $summary, EDIT_NEW & EDIT_SUPPRESS_RC );
 
-			} catch (MWException $e) {
+			} catch ( MWException $e ) {
 				/* fail silently...
 				 * todo: what can go wrong here? */
 			}
