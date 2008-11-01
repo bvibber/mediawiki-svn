3539ade528193a73406d6f5c2d1df47955947763
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-01 18:29:39 +0000

    cleanup a little
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43010 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionReporting/ContributionReporting.php
+++ extensions/ContributionReporting/ContributionReporting.php
@@ -2,51 +2,62 @@
 
 # Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
 if (!defined('MEDIAWIKI')) {
-        echo <<<EOT
+	echo <<<EOT
 To install my extension, put the following line in LocalSettings.php:
 require_once( "\$IP/extensions/ContributionReporting/ContributionReporting.php" );
 EOT;
-        exit( 1 );
+	exit( 1 );
 }
- 
+
+// Override these with appropriate DB settings for the CiviCRM database...
+$wgContributionReportingDBserver = $wgDBserver;
+$wgContributionReportingDBuser = $wgDBuser;
+$wgContributionReportingDBpassword = $wgDBpassword;
+$wgContributionReportingDBname = $wgDBname;
+
 $dir = dirname(__FILE__) . '/';
- 
-$wgHooks['LanguageGetSpecialPageAliases'][] = 'contributionReportingLocalizedPageName'; # Add any aliases for the special page.
+
+// Add any aliases for the special page.
+$wgHooks['LanguageGetSpecialPageAliases'][] = 'contributionReportingLocalizedPageName';
 
 $wgExtensionMessagesFiles['ContributionHistory'] = $dir . 'ContributionHistory.i18n.php';
-$wgAutoloadClasses['ContributionHistory'] = $dir . 'ContributionHistory_body.php'; # Tell MediaWiki to load the extension body.
-$wgSpecialPages['ContributionHistory'] = 'ContributionHistory'; # Let MediaWiki know about your new special page.
+$wgAutoloadClasses['ContributionHistory'] = $dir . 'ContributionHistory_body.php';
+$wgSpecialPages['ContributionHistory'] = 'ContributionHistory';
 
 $wgExtensionMessagesFiles['ContributionTotal'] = $dir . 'ContributionTotal.i18n.php';
-$wgAutoloadClasses['ContributionTotal'] = $dir . 'ContributionTotal_body.php'; # Tell MediaWiki to load the extension body.
-$wgSpecialPages['ContributionTotal'] = 'ContributionTotal'; # Let MediaWiki know about your new special page.
+$wgAutoloadClasses['ContributionTotal'] = $dir . 'ContributionTotal_body.php';
+$wgSpecialPages['ContributionTotal'] = 'ContributionTotal';
 
 function contributionReportingLocalizedPageName(&$specialPageArray, $code) {
-  # The localized title of the special page is among the messages of the extension:
-  wfLoadExtensionMessages('ContributionHistory');
-  $text = wfMsg('contributionhistory');
- 
-  # Convert from title in text form to DBKey and put it into the alias array:
-  $title = Title::newFromText($text);
-  $specialPageArray['ContributionHistory'][] = $title->getDBKey();
-
-  wfLoadExtensionMessages('ContributionTotal');
-  $text = wfMsg('contributiontotal');
-  $title = Title::newFromText($text);
-  $specialPageArray['ContributionTotal'][] = $title->getDBKey();
- 
-  return true;
+	# The localized title of the special page is among the messages of the extension:
+	wfLoadExtensionMessages('ContributionHistory');
+	$text = wfMsg('contributionhistory');
+	
+	# Convert from title in text form to DBKey and put it into the alias array:
+	$title = Title::newFromText($text);
+	$specialPageArray['ContributionHistory'][] = $title->getDBKey();
+	
+	wfLoadExtensionMessages('ContributionTotal');
+	$text = wfMsg('contributiontotal');
+	$title = Title::newFromText($text);
+	$specialPageArray['ContributionTotal'][] = $title->getDBKey();
+	
+	return true;
 }
 
 function contributionReportingConnection() {
-  global $wgContributionReportingDBserver, $wgContributionReportingDBname;
-  global $wgContributionReportingDBuser, $wgContributionReportingDBpassword;
-
-  static $db;
-
-  if (!$db) {
-    $db = new DatabaseMysql($wgContributionReportingDBserver, $wgContributionReportingDBuser, $wgContributionReportingDBpassword, $wgContributionReportingDBname);
-  }
-  
-  return $db;
+	global $wgContributionReportingDBserver, $wgContributionReportingDBname;
+	global $wgContributionReportingDBuser, $wgContributionReportingDBpassword;
+	
+	static $db;
+	
+	if (!$db) {
+		$db = new DatabaseMysql(
+			$wgContributionReportingDBserver,
+			$wgContributionReportingDBuser,
+			$wgContributionReportingDBpassword,
+			$wgContributionReportingDBname );
+	}
+	
+	return $db;
 }
