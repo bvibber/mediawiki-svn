577fe9228e39680c1562b457ed9c91bdc394eaf7
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-01 21:58:17 +0000

    Update for uniwiki/CssHooks: * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43036 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/uniwiki/CssHooks/CssHooks.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation for Uniwiki/CssHooks extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'csshooks-desc' => 'Adds CSS to the HTML output of pages for better styling',
+);
--- extensions/uniwiki/CssHooks/CssHooks.php
+++ extensions/uniwiki/CssHooks/CssHooks.php
@@ -3,7 +3,7 @@
  * http://www.mediawiki.org/wiki/Extension:Uniwiki_CSS_Hooks
  * http://www.gnu.org/licenses/gpl-3.0.txt */
 
-if (!defined("MEDIAWIKI"))
+if ( !defined( "MEDIAWIKI" ) )
 	die();
 
 /* ---- CREDITS ---- */
@@ -12,36 +12,40 @@ $wgExtensionCredits['other'][] = array(
 	'name'        => "Uniwiki CSS Hooks",
 	'author'      => "Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)",
 	'description' => "Add some CSS hooks to the HTML output of articles, for better styling"
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:Uniwiki_CSS_Hooks',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'descriptionmsg' => 'csshooks-desc',
 );
 
 /* ---- HOOKS ---- */
-
 $wgHooks['OutputPageBeforeHTML'][] = 'UW_CssHooks_AddHooks';
-function UW_CssHooks_AddHooks (&$out, &$text) {
+
+function UW_CssHooks_AddHooks ( &$out, &$text ) {
 	global $wgRequest;
 
 	// break the page into sections via their <h2>s
-	$sections = preg_split ("/(<a name=\".+?\"><\/a><h2>.+?<\/h2>)/",
-		$text, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$sections = preg_split ( "/(<a name=\".+?\"><\/a><h2>.+?<\/h2>)/",
+		$text, -1, PREG_SPLIT_DELIM_CAPTURE );
 
 	// remove the first empty section
-	if ($sections[0] == "")
-		array_shift ($sections);
+	if ( $sections[0] == "" )
+		array_shift ( $sections );
 
 	$index = 0;
 	$output = '';
 	$div_open = false;
-	for ($i=0; $i<count ($sections); $i++) {
+	for ( $i = 0; $i < count ( $sections ); $i++ ) {
 
 		/* is this block of text a header? (check for mw-headline
 		 * to only include actual section headers, and dodge toc) */
-		if (substr ($sections[$i], 0, 7) == "<a name") {
-			if (strstr ($sections[$i], "<span class=\"mw-headline\">") !== false) {
+		if ( substr ( $sections[$i], 0, 7 ) == "<a name" ) {
+			if ( strstr ( $sections[$i], "<span class=\"mw-headline\">" ) !== false ) {
 				$index++;
 
 				/* close current section div, if one exists,
 				 * and always open a new one with hooks */
-				if ($div_open) $output .= "</div>";
+				if ( $div_open ) $output .= "</div>";
 				$output .= "<div class=\"uw-section sect-$index\">";
 				$div_open = true;
 			}
@@ -51,7 +55,7 @@ function UW_CssHooks_AddHooks (&$out, &$text) {
 		$output .= $sections[$i];
 
 		// close the last section, if one is open
-		if (($i==count ($sections)-1) && $div_open)
+		if ( ( $i == count ( $sections ) - 1 ) && $div_open )
 			$output .= "</div>";
 	}
 
