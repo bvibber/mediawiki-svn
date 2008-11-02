b62bde53ad4359acb5b8932c6673eb2761a0da24
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-02 02:01:04 +0000

    Update for uniwiki/FormatSearch: * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43061 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/uniwiki/FormatSearch/FormatSearch.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation for Uniwiki/FormatSearch extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'formatsearch-desc' => 'Changes to clean up the search results page',
+);
--- extensions/uniwiki/FormatSearch/FormatSearch.php
+++ extensions/uniwiki/FormatSearch/FormatSearch.php
@@ -2,23 +2,27 @@
 /* vim: noet ts=4 sw=4
  * http://www.gnu.org/licenses/gpl-3.0.txt */
 
-if (!defined("MEDIAWIKI"))
+if ( !defined( "MEDIAWIKI" ) )
 	die();
 
-/* ---- CREDITS ---- */
-
 $wgExtensionCredits['other'][] = array(
-	'name'        => "Uniwiki Format Search",
-	'author'      => "Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)",
-	'description' => "Minor changes to clean up the search results page"
+	'name'           => 'FormatSearch',
+	'author'         => 'Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)',
+	'description'    => 'Changes to clean up the search results page',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:Uniwiki_Format_Search',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'descriptionmsg' => 'formatsearch-desc',
 );
 
-/* ---- HOOKS ---- */
+$wgExtensionMessagesFiles['FormatSearch'] = dirname( __FILE__ ) . '/FormatSearch.i18n.php';
 
+/* ---- HOOKS ---- */
 $wgHooks['BeforePageDisplay'][] = "UW_FormatSearch_CSS";
-function UW_FormatSearch_CSS (&$out) {
+
+function UW_FormatSearch_CSS ( &$out ) {
 	global $wgScriptPath;
 	$href = "$wgScriptPath/extensions/uniwiki/FormatSearch/style.css";
-	$out->addScript ("<link rel='stylesheet' href='$href' />");
+	$out->addScript ( "<link rel='stylesheet' href='$href' />" );
 	return true;
 }
