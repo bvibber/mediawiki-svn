bc5d37f5d9b759e2d5e8107c9b95ce81df6b767f
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-02 01:45:53 +0000

    Actually register the i18n file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43058 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/uniwiki/Javascript/Javascript.php
+++ extensions/uniwiki/Javascript/Javascript.php
@@ -7,7 +7,7 @@ if ( !defined( 'MEDIAWIKI' ) )
 	die();
 
 $wgExtensionCredits['other'][] = array(
-	'name'           => 'Uniwiki Javascript',
+	'name'           => 'Javascript',
 	'author'         => 'Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)',
 	'description'    => 'Adds uniwiki.js to each page containing Javascript code shared between Uniwiki extensions',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:Uniwiki_Javascript',
@@ -15,6 +15,8 @@ $wgExtensionCredits['other'][] = array(
 	'svn-revision'   => '$LastChangedRevision$',
 	'descriptionmsg' => 'javascript-desc',
 );
+
+$wgExtensionMessagesFiles['Javascript'] = dirname( __FILE__ ) . '/Javascript.i18n.php';
 
 $wgHooks['BeforePageDisplay'][] = 'UW_Javascript_addJS';
 function UW_Javascript_addJS( $out ) {
