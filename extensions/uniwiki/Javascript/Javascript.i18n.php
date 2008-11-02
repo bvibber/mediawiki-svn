b7e114affd93ddd0b90ed8e3c5b7a28d90b1a965
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-02 01:28:26 +0000

    Update for uniwiki/Javascript: * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/uniwiki/Javascript/Javascript.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation for Uniwiki/Javascript extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'javascript-desc' => 'Adds uniwiki.js to each page containing Javascript code shared between Uniwiki extensions',
+);
--- extensions/uniwiki/Javascript/Javascript.php
+++ extensions/uniwiki/Javascript/Javascript.php
@@ -3,19 +3,23 @@
  * http://www.mediawiki.org/wiki/Extension:Uniwiki_Javascript
  * http://www.gnu.org/licenses/gpl-3.0.txt */
 
-if (!defined('MEDIAWIKI'))
+if ( !defined( 'MEDIAWIKI' ) )
 	die();
 
 $wgExtensionCredits['other'][] = array(
-	'name'        => "Uniwiki Javascript",
-	'author'      => "Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)",
-	'description' => "Adds uniwiki.js to each page (which currently just provides lightweight i18n support in Javascript), and serves as a placeholder for future Javascript code shared between Uniwiki extensions"
+	'name'           => 'Uniwiki Javascript',
+	'author'         => 'Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)',
+	'description'    => 'Adds uniwiki.js to each page containing Javascript code shared between Uniwiki extensions',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:Uniwiki_Javascript',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'descriptionmsg' => 'javascript-desc',
 );
 
-$wgHooks['BeforePageDisplay'][] = "UW_Javascript_addJS";
-function UW_Javascript_addJS($out) {
+$wgHooks['BeforePageDisplay'][] = 'UW_Javascript_addJS';
+function UW_Javascript_addJS( $out ) {
 	global $wgScriptPath;
 	$src = "$wgScriptPath/extensions/uniwiki/Javascript/uniwiki.js";
-	$out->addScript ("<script type='text/javascript' src='$src'></script>");
+	$out->addScript ( "<script type='text/javascript' src='$src'></script>" );
 	return true;
 }
