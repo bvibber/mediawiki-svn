af7eb7dd3ae2d8d3ac3528a93a460b21853d1aa1
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-02 01:51:46 +0000

    Update for uniwiki/GenericEditPage: * standard i18n * put global $wgCategoryPage in a message * grouped all hook registrations * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43059 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/uniwiki/GenericEditPage/GenericEditPage.i18n.php
+++ extensions/uniwiki/GenericEditPage/GenericEditPage.i18n.php
@@ -1,88 +1,96 @@
 <?php
-/* vim: noet ts=4 sw=4
- * http://www.mediawiki.org/wiki/Extension:Uniwiki_Generic_Edit_Page
- * http://www.gnu.org/licenses/gpl-3.0.txt */
+/**
+ * Internationalisation for Uniwiki/GenericEditPage extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
-if (!defined("MEDIAWIKI"))
-	die();
+$messages = array();
 
-$wgGenericEditPageMessages = array();
-$wgGenericEditPageMessages['en'] = array(
-	'gep_emailsubject'          => "[$1] Category suggestion: $2",
-	'gep_emailbody'             => "User '$1' suggested category '$2' for site '$3'.\n",
-	'gep_emailfailure'          => "Sorry, your suggestion could not be processed.",
-	'gep_emailsuccess'          => "Thanks for suggesting the category: $1.",
-	'gep_categories'            => "Categories",
-	'gep_addcategory'           => "Add a Category",
-	'gep_addcategorybutton'     => "Add",
-	'gep_suggestcategory'       => "Suggest a Category",
-	'gep_suggestcategorybutton' => "Submit",
-	'gep_sections'              => "Sections",
-	'gep_sectionnotdisabled'    => "This section cannot be disabled",
-	'gep_addsection'            => "Add a Section",
-	'gep_addsectionbutton'      => "Add",
-	'gep_classicmode'           => "Classic Mode",
-	'gep_genericmode'           => "Generic Mode",
-	'gep_nosectioninstructions' => "There are no sections on this page. Add some using the tools on the right.",
-	'gep_nocategories'          => "Please select at least one category before saving"
+/** English */
+$messages['en'] = array(
+	'gep-desc' => 'Suppliments the edit page to be more user friendly',
+	'gep-emailsubject' => '[$1] Category suggestion: $2',
+	'gep-emailbody' => 'User "$1" suggested category "$2" for site "$3".',
+	'gep-emailfailure' => 'Sorry, your suggestion could not be processed.',
+	'gep-emailsuccess' => 'Thanks for suggesting the category: $1.',
+	'gep-categories' => 'Categories',
+	'gep-addcategory' => 'Add a category',
+	'gep-addcategorybutton' => 'Add',
+	'gep-suggestcategory' => 'Suggest a category',
+	'gep-suggestcategorybutton' => 'Submit',
+	'gep-sections' => 'Sections',
+	'gep-sectionnotdisabled' => 'This section cannot be disabled',
+	'gep-addsection' => 'Add a section',
+	'gep-addsectionbutton' => 'Add',
+	'gep-classicmode' => 'Classic mode',
+	'gep-genericmode' => 'Generic mode',
+	'gep-nosectioninstructions' => 'There are no sections on this page.
+Add some using the tools on the right.',
+	'gep-nocategories' => 'Please select at least one category before saving',
+	'gep-categorypage' => 'MediaWiki:Editpagetags',
 );
 
-$wgGenericEditPageMessages['es'] = array(
-	'gep_emailsubject'          => "Sugerencia de categoría de [$1]: $2",
-	'gep_emailbody'             => "El usuario '$1' sugirió la categoría '$2' para el sitio '$3'.\n",
-	'gep_emailfailure'          => "No fue posible procesar su sugerencia.",
-	'gep_emailsuccess'          => "Gracias por sugerir la categoría: $1.",
-	'gep_categories'            => "Categorías",
-	'gep_addcategory'           => "Agregar una Categoría",
-	'gep_addcategorybutton'     => "Agregar",
-	'gep_suggestcategory'       => "Sugerir una Categoría",
-	'gep_suggestcategorybutton' => "Enviar",
-	'gep_sections'              => "Secciones",
-	'gep_sectionnotdisabled'    => "Esta sección no se puede desactivar",
-	'gep_addsection'            => "Agregar una Sección",
-	'gep_addsectionbutton'      => "Agregar",
-	'gep_classicmode'           => "Modo Clásico",
-	'gep_genericmode'           => "Modo Genérico",
-	'gep_nosectioninstructions' => "No hay secciones en esta página. Agrega algunas secciones utilizando las herramientas a la derecha.",
-	'gep_nocategories'          => "Selecciona por lo menos una categoría antes de guardar"
+$messages['es'] = array(
+	'gep-emailsubject' => 'Sugerencia de categoría de [$1]: $2',
+	'gep-emailbody' => 'El usuario "$1" sugirió la categoría "$2" para el sitio "$3".',
+	'gep-emailfailure' => 'No fue posible procesar su sugerencia.',
+	'gep-emailsuccess' => 'Gracias por sugerir la categoría: $1.',
+	'gep-categories' => 'Categorías',
+	'gep-addcategory' => 'Agregar una Categoría',
+	'gep-addcategorybutton' => 'Agregar',
+	'gep-suggestcategory' => 'Sugerir una Categoría',
+	'gep-suggestcategorybutton' => 'Enviar',
+	'gep-sections' => 'Secciones',
+	'gep-sectionnotdisabled' => 'Esta sección no se puede desactivar',
+	'gep-addsection' => 'Agregar una Sección',
+	'gep-addsectionbutton' => 'Agregar',
+	'gep-classicmode' => 'Modo Clásico',
+	'gep-genericmode' => 'Modo Genérico',
+	'gep-nosectioninstructions' => 'No hay secciones en esta página.
+Agrega algunas secciones utilizando las herramientas a la derecha.',
+	'gep-nocategories' => 'Selecciona por lo menos una categoría antes de guardar',
 );
 
-$wgGenericEditPageMessages['de'] = array(
-	'gep_emailsubject'          => "[$1] Vorschlag Kategorie: $2",
-	'gep_emailbody'             => "User '$1' hat die Kategorie '$2' für die Seite '$3'. ausgewählt\n",
-	'gep_emailfailure'          => "Leider, konnte Deine Änderung nicht verarbeitet werden.",
-	'gep_emailsuccess'          => "Danke für den Vorschlag der Kategorie: $1.",
-	'gep_categories'            => "Kategorien",
-	'gep_addcategory'           => "Eine Kategorie einfügen",
-	'gep_addcategorybutton'     => "Einfügen",
-	'gep_suggestcategory'       => "Eine Kategorie vorschlagen",
-	'gep_suggestcategorybutton' => "Senden",
-	'gep_sections'              => "Abschnitte",
-	'gep_sectionnotdisabled'    => "Dieser Abschnitt kann nicht aufgehoben werden",
-	'gep_addsection'            => "Einen Abschnitt einfügen",
-	'gep_addsectionbutton'      => "Einfügen",
-	'gep_classicmode'           => "Normaler Modus",
-	'gep_genericmode'           => "Genereller Modus",
-	'gep_nosectioninstructions' => "Diese Seite hat keine Abschnitte. Gib ein paar ein mit den Werkzeugen an der rechten Seite.",
-	'gep_nocategories'          => "Bitte vor Abspeichern, mindestens eine Kategorie aussuchen"
+$messages['de'] = array(
+	'gep-emailsubject' => '[$1] Vorschlag Kategorie: $2',
+	'gep-emailbody' => 'User "$1" hat die Kategorie "$2" für die Seite "$3". ausgewählt',
+	'gep-emailfailure' => 'Leider, konnte Deine Änderung nicht verarbeitet werden.',
+	'gep-emailsuccess' => 'Danke für den Vorschlag der Kategorie: $1.',
+	'gep-categories' => 'Kategorien',
+	'gep-addcategory' => 'Eine Kategorie einfügen',
+	'gep-addcategorybutton' => 'Einfügen',
+	'gep-suggestcategory' => 'Eine Kategorie vorschlagen',
+	'gep-suggestcategorybutton' => 'Senden',
+	'gep-sections' => 'Abschnitte',
+	'gep-sectionnotdisabled' => 'Dieser Abschnitt kann nicht aufgehoben werden',
+	'gep-addsection' => 'Einen Abschnitt einfügen',
+	'gep-addsectionbutton' => 'Einfügen',
+	'gep-classicmode' => 'Normaler Modus',
+	'gep-genericmode' => 'Genereller Modus',
+	'gep-nosectioninstructions' => 'Diese Seite hat keine Abschnitte.
+Gib ein paar ein mit den Werkzeugen an der rechten Seite.',
+	'gep-nocategories' => 'Bitte vor Abspeichern, mindestens eine Kategorie aussuchen',
 );
 
-$wgGenericEditPageMessages['pt-br'] = array(
-	'gep_emailsubject'          => "Sugestão de categoria de [$1] : $2",
-	'gep_emailbody'             => "O usuário '$1' sugeriu a categoria '$2' para o site '$3'.\n",
-	'gep_emailfailure'          => "Não foi possível processar a sua sugestão.",
-	'gep_emailsuccess'          => "Obrigado por sugerir essa categoria: $1.",
-	'gep_categories'            => "Categorias",
-	'gep_addcategory'           => "Adicionar uma Categoria",
-	'gep_addcategorybutton'     => "Adicionar",
-	'gep_suggestcategory'       => "Sugerir uma Categoria",
-	'gep_suggestcategorybutton' => "Encaminhar",
-	'gep_sections'              => "Seções",
-	'gep_sectionnotdisabled'    => "Esta seção não pode ser desativada",
-	'gep_addsection'            => "Adicionar uma Seção",
-	'gep_addsectionbutton'      => "Acrescentar",
-	'gep_classicmode'           => "Modo Clássico",
-	'gep_genericmode'           => "Modo Genérico",
-	'gep_nosectioninstructions' => "Não há seções nesta página. Use as ferramentas à direita para incluir algumas seções.",
-	'gep_nocategories'          => "Selecione, no mínimo, uma categoria antes de salvar"
+$messages['pt-br'] = array(
+	'gep-emailsubject' => 'Sugestão de categoria de [$1] : $2',
+	'gep-emailbody' => 'O usuário "$1" sugeriu a categoria "$2" para o site "$3".',
+	'gep-emailfailure' => 'Não foi possível processar a sua sugestão.',
+	'gep-emailsuccess' => 'Obrigado por sugerir essa categoria: $1.',
+	'gep-categories' => 'Categorias',
+	'gep-addcategory' => 'Adicionar uma Categoria',
+	'gep-addcategorybutton' => 'Adicionar',
+	'gep-suggestcategory' => 'Sugerir uma Categoria',
+	'gep-suggestcategorybutton' => 'Encaminhar',
+	'gep-sections' => 'Seções',
+	'gep-sectionnotdisabled' => 'Esta seção não pode ser desativada',
+	'gep-addsection' => 'Adicionar uma Seção',
+	'gep-addsectionbutton' => 'Acrescentar',
+	'gep-classicmode' => 'Modo Clássico',
+	'gep-genericmode' => 'Modo Genérico',
+	'gep-nosectioninstructions' => 'Não há seções nesta página.
+Use as ferramentas à direita para incluir algumas seções.',
+	'gep-nocategories' => 'Selecione, no mínimo, uma categoria antes de salvar',
 );
