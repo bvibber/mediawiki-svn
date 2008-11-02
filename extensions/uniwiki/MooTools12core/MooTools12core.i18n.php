f61c326e0a4547dbb55ffcbed50ff6e8c8c97abf
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-02 00:26:54 +0000

    Update for uniwiki/MooTools12core: * complete extension credits * update indentation, remove trailing whitespace, stylize.php
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43051 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/uniwiki/MooTools12core/MooTools12core.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation for Uniwiki/MooTools12core extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'mootools12core-desc' => 'Adds mootools-1.2-core-yc.js to each page',
+);
--- extensions/uniwiki/MooTools12core/MooTools12core.php
+++ extensions/uniwiki/MooTools12core/MooTools12core.php
@@ -3,20 +3,26 @@
  * http://www.mediawiki.org/wiki/Extension:MooTools_1.2_Core
  * http://www.gnu.org/licenses/gpl-3.0.txt */
 
-if (!defined('MEDIAWIKI'))
+if ( !defined( 'MEDIAWIKI' ) )
 	die();
 
 $wgExtensionCredits['other'][] = array(
-	'name'        => "MooTools12core",
-	'author'      => "Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)",
-	'description' => "Adds mootools-1.2-core-yc.js to each page"
+	'name'           => 'MooTools12core',
+	'author'         => 'Merrick Schaefer, Mark Johnston, Evan Wheeler and Adam Mckaig (at UNICEF)',
+	'description'    => 'Adds mootools-1.2-core-yc.js to each page',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:MooTools_1.2_Core',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'descriptionmsg' => 'mootools12core-desc',
 );
 
+$wgExtensionMessagesFiles['MooTools12core'] = dirname( __FILE__ ) . '/MooTools12core.i18n.php';
 
-$wgHooks['BeforePageDisplay'][] = "UW_MooTools12core_addJS";
-function UW_MooTools12core_addJS($out) {
+$wgHooks['BeforePageDisplay'][] = 'UW_MooTools12core_addJS';
+
+function UW_MooTools12core_addJS( $out ) {
 	global $wgScriptPath;
 	$src = "$wgScriptPath/extensions/uniwiki/MooTools12core/mootools-1.2-core-yc.js";
-	$out->addScript("<script type='text/javascript' src='$src'></script>");
+	$out->addScript( "<script type='text/javascript' src='$src'></script>" );
 	return true;
 }
