defc9701e88d4b4d2a7d3d17785413ccd549e5b1
straussd <straussd@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-03 19:52:36 +0000

    Clean up duplicated code from the merge
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43139 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionTracking/ContributionTracking_body.php
+++ extensions/ContributionTracking/ContributionTracking_body.php
@@ -74,7 +74,7 @@ class ContributionTracking extends SpecialPage {
     
     // Normalized amount
     $repost['amount'] = $values['amount'];
-    if ($values['amountGiven']) {
+    if ( $values['amountGiven'] ) {
       $repost['amount'] = $values['amountGiven'];
     }
     
@@ -84,69 +84,14 @@ class ContributionTracking extends SpecialPage {
     // Output the repost form
     $output = '<form method="post" name="contributiontracking" action="' . $action . '">';
 
-    foreach ($repost as $key => $value) {
+    foreach ( $repost as $key => $value ) {
       $output .= '<input type="hidden" name="' . htmlspecialchars($key) . '" value="' . htmlspecialchars($value) . '" />';
     }
     
-    // Offer a button to post the form if the user has Javascript support
+    // Offer a button to post the form if the user has no Javascript support
     $output .= '<noscript>';
     $output .= '<input type="submit" value="Continue" />';
     $output .= '</noscript>';
-
-		$this->setHeaders();
-
-		$db = contributionTrackingConnection();
-
-		$tracked_contribution = array(
-		'note' => $wgRequest->getText( 'comment', NULL ),
-		'referrer' => $wgRequest->getText( 'referrer', NULL ),
-		'anonymous' => ( $wgRequest->getCheck( 'comment-option' ) ? 0 : 1 ),
-		'utm_source' => $wgRequest->getText( 'utm_source', NULL ),
-		'utm_medium' => $wgRequest->getText( 'utm_medium', NULL ),
-		'utm_campaign' => $wgRequest->getText( 'utm_campaign', NULL ),
-		'optout' => ( $wgRequest->getCheck( 'email' ) ? 0 : 1 ),
-		'language' => $wgRequest->getText( 'language', NULL ),
-		);
-
-		// Make all empty strings NULL
-		foreach ( $tracked_contribution as $key => $value ) {
-			if ( $value === '' ) {
-				$tracked_contribution[$key] = NULL;
-			}
-		}
-
-		// Store the contribution data
-		$db->insert( 'contribution_tracking', $tracked_contribution );
-
-		$contribution_tracking_id = $db->insertId();
-
-		$values = $wgRequest->getValues();
-
-		// Set the action and tracking ID fields
-		$action = 'http://wikimediafoundation.org/';
-		$tracking = '';
-		if ( $values['gateway'] == 'paypal' ) {
-			$action = 'https://www.paypal.com/cgi-bin/webscr';
-			$tracking = '<input type="hidden" name="on0" value="contribution_tracking_id" />';
-		}
-		else if ( $values['gateway'] == 'moneybookers' ) {
-			$action = 'https://www.moneybookers.com/app/payment.pl';
-			$tracking = '<input type="hidden" name="merchant_fields" value="os0" />';
-		}
-		$tracking .= '<input type="hidden" name="os0" value="' . $contribution_tracking_id . '" />';
-
-		// Output the repost form
-		$output = '<form method="post" name="contributiontracking" action="' . $action . '">';
-
-		foreach ( $values as $key => $value ) {
-			$output .= '<input type="hidden" name="' . htmlspecialchars( $key ) . '" value="' . htmlspecialchars( $value ) . '" />';
-		}
-		$output .= $tracking;
-
-		// Offer a button to post the form if the user has Javascript support
-		$output .= '<noscript>';
-		$output .= '<input type="submit" value="Continue" />';
-		$output .= '</noscript>';
 
 		$output .= '</form>';
 
