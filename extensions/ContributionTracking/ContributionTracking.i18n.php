da40a871b6df0a61cfd6327c51b0f2ef3de930ed
rotem <rotem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-03 21:15:11 +0000

    * Localization update for he. * Removing obsolete message noticetranslate.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43153 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionTracking/ContributionTracking.i18n.php
+++ extensions/ContributionTracking/ContributionTracking.i18n.php
@@ -21,3 +21,17 @@ $messages['en'] = array(
 	'contrib-tracking-submitting' => 'Submitting to payment processor...',
 	'contrib-tracking-continue' => 'Click the button to complete your donation at PayPal.',
 );
+
+/** Hebrew (עברית)
+ * @author Rotem Liss
+ */
+$messages['he'] = array(
+	'contributiontracking-desc' => '[[Special:ContributionTracking|מעקב תרומות]] להתרמה לקרן ויקימדיה',
+	'contributiontracking' => 'מעקב תרומות',
+
+	'contrib-tracking-error' => 'שגיאה',
+	'contrib-tracking-error-text' => 'שליחת פורום בלתי תקינה',
+
+	'contrib-tracking-submitting' => 'נשלח למעבד התרומות...',
+	'contrib-tracking-continue' => 'נא לחצו על הכפתור כדי להשלים את תרומתכם באתר PayPal.',
+);
