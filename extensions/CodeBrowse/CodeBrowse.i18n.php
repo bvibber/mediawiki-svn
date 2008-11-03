574f1548dbf4ec973a86f64233c00ebe3ec05d5b
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-03 08:36:57 +0000

    Update header
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43114 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeBrowse/CodeBrowse.i18n.php
+++ extensions/CodeBrowse/CodeBrowse.i18n.php
@@ -1,19 +1,26 @@
 <?php
+/**
+ * Internationalisation file for extension CategoryTree.
+ *
+ * @addtogroup Extensions
+ * @author Bryan Tong Minh
+ */
 
 $messages = array();
 
+/** English
+ * @author Bryan Tong Minh
+ */
 $messages['en'] = array(
-	'code-log'	=> 'log',	// Might want to move this to CodeReview
-	
-	'codebrowse'	=> 'CodeBrowse',
-	'codebrowse-not-found' =>	'Could not find file $1.',
+	'code-log' => 'log', // Might want to move this to CodeReview
+
+	'codebrowse' => 'CodeBrowse',
+	'codebrowse-not-found' => 'Could not find file $1.',
 	'codebrowse-dir-listing' => 'Directory index of $1',
-	'codebrowse-name'	=> 'Name',
-	'codebrowse-last-modifier'	=> 'Author',
-	'codebrowse-revision'	=> 'Revision',
-	'codebrowse-lastchange'	=> 'Last modified date',
-	'codebrowse-size' 	=> 'Size',
+	'codebrowse-name' => 'Name',
+	'codebrowse-last-modifier' => 'Author',
+	'codebrowse-revision' => 'Revision',
+	'codebrowse-lastchange' => 'Last modified date',
+	'codebrowse-size' => 'Size',
 	'codebrowse-binary-file'	=> 'Binary files will not be shown.'
 );
-
-	
