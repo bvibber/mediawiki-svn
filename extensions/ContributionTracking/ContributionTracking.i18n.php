41d2e9710f7bc4fa90c6fdc9f396494750084228
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-03 21:05:16 +0000

    Add ContributionTracking to translatewiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@43150 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionTracking/ContributionTracking.i18n.php
+++ extensions/ContributionTracking/ContributionTracking.i18n.php
@@ -14,11 +14,10 @@ $messages = array();
 $messages['en'] = array(
 	'contributiontracking-desc' => '[[Special:ContributionTracking|Contribution tracking]] for the Wikimedia fundraiser',
 	'contributiontracking' => 'Contribution tracking',
-	
+
 	'contrib-tracking-error' => 'Error',
 	'contrib-tracking-error-text' => 'Invalid form submission',
-	
-    'contrib-tracking-submitting' => 'Submitting to payment processor...',
-    'contrib-tracking-continue' => 'Click the button to complete your donation at PayPal.',
-    
+
+	'contrib-tracking-submitting' => 'Submitting to payment processor...',
+	'contrib-tracking-continue' => 'Click the button to complete your donation at PayPal.',
 );
