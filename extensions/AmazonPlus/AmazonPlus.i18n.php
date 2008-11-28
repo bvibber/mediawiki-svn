eac453c7617d293d3e36e6a8373d03d2d1fb2f29
skizzerz <skizzerz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-11-28 16:45:52 +0000

    * committing new extension AmazonPlus
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@44017 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AmazonPlus/AmazonPlus.i18n.php
@@ -0,0 +1,28 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'amazonplus-desc' => 'A highly customizable extension to display Amazon information',
+	'amazonplus-nores' => '<span class="error">Error: No results found!</span>',
+	'amazonplus-noidres' => '<span class="error">Error: Could not find a product ID!</span>',
+	'amazonplus-fgcerr' => '<span class="error">Error: Could not retrieve data from Amazon!</span>',
+	'amazonplus-slserr' => '<span class="error">Error: Could not parse data from Amazon!</span>',
+	'amazonplus-used' => 'used',
+	'amazonplus-german' => 'german',
+	'amazonplus-french' => 'french',
+	'amazonplus-japanese' => 'japanese',
+	'amazonplus-english' => '',
+	'amazonplus-amazon' => 'amazon price',
+	'amazonplus-new' => 'new',
+	'amazonplus-status' => '($1)',
+	'amazonplus-status-sep' => ', ',
+	'amazonplus-none' => 'No copies of this item are up for sale.',
+	'amazonplus-currency' => '$3$1 $2$4',
+	'amazonplus-cp-none' => 'None',
+	'amazonplus-cp-usd' => 'USD',
+	'amazonplus-cp-cad' => 'CAD',
+	'amazonplus-cp-gbp' => 'GBP',
+	'amazonplus-cp-eur' => 'EUR',
+	'amazonplus-cp-jpy' => 'JPY',
+);
