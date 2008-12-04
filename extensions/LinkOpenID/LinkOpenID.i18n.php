e46fdd7322b8e5c4dcea1699bf6a0488dcd691c7
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-12-04 11:42:07 +0000

    * update extension credits * fix typo in message * update whitespace and indentation
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@44222 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LinkOpenID/LinkOpenID.i18n.php
+++ extensions/LinkOpenID/LinkOpenID.i18n.php
@@ -6,37 +6,37 @@
  * @copyright 2008 Michael Holzt
  * @license GNU General Public License 2.0
  */
- 
+
 $messages = array();
- 
+
 /** English
  * @author Michael Holzt <kju@fqdn.org>
  */
 $messages['en'] = array(
-  # for Special:Version
-  'linkopenid-desc' => 'Allow users to link their account to an external OpenID',
- 
-  # for Special:Preferences
-  'linkopenid-prefs' => 'OpenID',
-  'linkopenid-prefstext-pre' => 'If you have a OpenID from an external provider you can specify it here. This allows you to use your userpage as a OpenID as well.',
-  'linkopenid-prefstext-openid' => 'Your OpenID:',
-  'linkopenid-prefstext-v1url' => 'Server-URL for OpenID Version 1.1:',
-  'linkopenid-prefstext-v2url' => 'Server-URL for OpenID Version 2:'
+	# for Special:Version
+	'linkopenid-desc' => 'Allow users to link their account to an external OpenID',
+
+	# for Special:Preferences
+	'linkopenid-prefs' => 'OpenID',
+	'linkopenid-prefstext-pre' => 'If you have a OpenID from an external provider you can specify it here.
+This allows you to use your userpage as an OpenID as well.',
+	'linkopenid-prefstext-openid' => 'Your OpenID:',
+	'linkopenid-prefstext-v1url' => 'Server-URL for OpenID Version 1.1:',
+	'linkopenid-prefstext-v2url' => 'Server-URL for OpenID Version 2:'
 );
- 
+
 /** German
  * @author Michael Holzt <kju@fqdn.org>
  */
- 
 $messages['de'] = array(
-  # for Special:Version
-  'linkopenid-desc' => 'Erlaubt Benutzern eine externe OpenID ihrem Account zuzuordnen',
- 
-  # for Special:Preferences
-  'linkopenid-prefs' => 'OpenID',
-  'linkopenid-prefstext-pre' => 'Wenn Sie eine OpenID bei einem externen Anbieter besitzen, können Sie diese hier angeben. Dies ermöglicht Ihnen die alternative Nutzung Ihrer Benutzerseite als OpenID.',
-  'linkopenid-prefstext-openid' => 'Ihre OpenID:',
-  'linkopenid-prefstext-v1url' => 'Server-URL für OpenID Version 1.1:',
-  'linkopenid-prefstext-v2url' => 'Server-URL für OpenID Version 2:'
+	# for Special:Version
+	'linkopenid-desc' => 'Erlaubt Benutzern eine externe OpenID ihrem Account zuzuordnen',
+
+	# for Special:Preferences
+	'linkopenid-prefs' => 'OpenID',
+	'linkopenid-prefstext-pre' => 'Wenn Sie eine OpenID bei einem externen Anbieter besitzen, können Sie diese hier angeben.
+Dies ermöglicht Ihnen die alternative Nutzung Ihrer Benutzerseite als OpenID.',
+	'linkopenid-prefstext-openid' => 'Ihre OpenID:',
+	'linkopenid-prefstext-v1url' => 'Server-URL für OpenID Version 1.1:',
+	'linkopenid-prefstext-v2url' => 'Server-URL für OpenID Version 2:'
 );
-?>
--- extensions/LinkOpenID/LinkOpenID.php
+++ extensions/LinkOpenID/LinkOpenID.php
@@ -6,122 +6,113 @@
  * @copyright 2008 Michael Holzt
  * @license GNU General Public License 2.0
  */
- 
+
 if ( !defined('MEDIAWIKI')) {
-  echo('This file is an extension for the MediaWiki software and cannot be used standalone.\n');
-  die(1);
+	echo('This file is an extension for the MediaWiki software and cannot be used standalone.\n');
+	die(1);
 }
- 
+
 $wgExtensionCredits['other'][] = array(
-  'name' => 'LinkOpenID',
-  'author' => 'Michael Holzt',
-  'description' => 'allow users to link their account to an external OpenID',
-  'descriptionmsg' => 'linkopenid-desc'
+	'name'           => 'LinkOpenID',
+	'author'         => 'Michael Holzt',
+	'description'    => 'allow users to link their account to an external OpenID',
+	'descriptionmsg' => 'linkopenid-desc',
+	'svn-date'       => '$LastChangedDate$',
+	'svn-revision'   => '$LastChangedRevision$',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:LinkOpenID',
 );
- 
+
 $wgHooks['ArticleViewHeader'][] = 'wfLinkOpenIDViewHeader';
 $wgHooks['InitPreferencesForm'][] = 'wfLinkOpenIDInitPrefs';
 $wgHooks['RenderPreferencesForm'][] = 'wfLinkOpenIDRenderPrefs';
 $wgHooks['SavePreferences'][] = 'wfLinkOpenIDSavePrefs';
 $wgHooks['ResetPreferences'][] = 'wfLinkOpenIDResetprefs';
- 
+
 $dir = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['LinkOpenID'] = $dir . 'LinkOpenID.i18n.php';
- 
-/* --- */
- 
+
 function wfLinkOpenIDViewHeader($article)
 {
-  global $wgOut;
- 
-  /* We only care about the main page of any user */
-  $nt = $article->getTitle();
-  if ( $nt && $nt->getNamespace() == NS_USER &&
-    strpos($nt->getText(), '/') === false ) {
- 
-    $user = User::newFromName($nt->getText());
-    if ( $user && $user->getID() != 0) {
- 
-      $openid = $user->getOption('wflinkopenid_openid');
- 
-      if ( $openid != '' ) {
-        $v1url = $user->getOption('wflinkopenid_v1url');
-        $v2url = $user->getOption('wflinkopenid_v2url');
- 
-        if ( $v1url != '' ) {
-          $wgOut->addLink( array('rel' => 'openid.server', 'href' => $v1url) );
-          $wgOut->addLink( array('rel' => 'openid.delegate', 'href' => $openid) );
-        }
- 
-        if ( $v2url != '' ) {
-          $wgOut->addLink( array('rel' => 'openid2.provider', 'href' => $v2url) );
-          $wgOut->addLink( array('rel' => 'openid2.local_id', 'href' => $openid) );
-        }
-      }
-    }
-  }
- 
-  return TRUE;
-}        
- 
-/* --- */
- 
+	global $wgOut;
+
+	/* We only care about the main page of any user */
+	$nt = $article->getTitle();
+	if ( $nt && $nt->getNamespace() == NS_USER &&
+	strpos($nt->getText(), '/') === false ) {
+
+		$user = User::newFromName($nt->getText());
+		if ( $user && $user->getID() != 0) {
+
+			$openid = $user->getOption('wflinkopenid_openid');
+
+			if ( $openid != '' ) {
+				$v1url = $user->getOption('wflinkopenid_v1url');
+				$v2url = $user->getOption('wflinkopenid_v2url');
+
+				if ( $v1url != '' ) {
+					$wgOut->addLink( array('rel' => 'openid.server', 'href' => $v1url) );
+					$wgOut->addLink( array('rel' => 'openid.delegate', 'href' => $openid) );
+				}
+
+				if ( $v2url != '' ) {
+					$wgOut->addLink( array('rel' => 'openid2.provider', 'href' => $v2url) );
+					$wgOut->addLink( array('rel' => 'openid2.local_id', 'href' => $openid) );
+				}
+			}
+		}
+	}
+
+	return TRUE;
+}
+
 function wfLinkOpenIDInitPrefs($prefs, $request) {
-  $prefs->wfLinkOpenID = Array();
-  $prefs->wfLinkOpenID['openid'] = $request->getVal('wflinkopenid_openid');
-  $prefs->wfLinkOpenID['v1url'] = $request->getVal('wflinkopenid_v1url');
-  $prefs->wfLinkOpenID['v2url'] = $request->getVal('wflinkopenid_v2url');
-  return TRUE; 
+	$prefs->wfLinkOpenID = Array();
+	$prefs->wfLinkOpenID['openid'] = $request->getVal('wflinkopenid_openid');
+	$prefs->wfLinkOpenID['v1url'] = $request->getVal('wflinkopenid_v1url');
+	$prefs->wfLinkOpenID['v2url'] = $request->getVal('wflinkopenid_v2url');
+	return TRUE;
 }
- 
-/* --- */
- 
+
 function wfLinkOpenIDRenderPrefs($prefs, $out) {
-  wfLoadExtensionMessages( 'LinkOpenID' );
- 
-  $out->addHTML( "<fieldset><legend>" . 
-                 wfMsgHtml( 'linkopenid-prefs' ) .
-                 "</legend>");
- 
-  $out->addWikiMsg( 'linkopenid-prefstext-pre' );
- 
-  $out->addHTML(
-    "<table>" .
- 
-    "<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-openid' ) . "</td><td>" .
-    "<input type='text' name='wflinkopenid_openid' size='60' value='" . 
-    htmlentities($prefs->wfLinkOpenID['openid']) . "'></td></tr>" .
- 
-    "<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-v1url' ) . "</td><td>" .
-    "<input type='text' name='wflinkopenid_v1url' size='60' value='" . 
-    htmlentities($prefs->wfLinkOpenID['v1url']) . "'></td></tr>" .
- 
-    "<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-v2url' ) . "</td><td>" .
-    "<input type='text' name='wflinkopenid_v2url' size='60' value='" . 
-    htmlentities($prefs->wfLinkOpenID['v2url']) . "'></td></tr>" .
-    "</table></fieldset>"
-  );
- 
-  return TRUE; 
+	wfLoadExtensionMessages( 'LinkOpenID' );
+
+	$out->addHTML( "<fieldset><legend>" .
+		wfMsgHtml( 'linkopenid-prefs' ) .
+		"</legend>");
+
+	$out->addWikiMsg( 'linkopenid-prefstext-pre' );
+
+	$out->addHTML(
+		"<table>" .
+
+			"<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-openid' ) . "</td><td>" .
+			"<input type='text' name='wflinkopenid_openid' size='60' value='" .
+			htmlentities($prefs->wfLinkOpenID['openid']) . "'></td></tr>" .
+
+			"<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-v1url' ) . "</td><td>" .
+			"<input type='text' name='wflinkopenid_v1url' size='60' value='" .
+			htmlentities($prefs->wfLinkOpenID['v1url']) . "'></td></tr>" .
+
+			"<tr><td>" . wfMsgHtml( 'linkopenid-prefstext-v2url' ) . "</td><td>" .
+			"<input type='text' name='wflinkopenid_v2url' size='60' value='" .
+			htmlentities($prefs->wfLinkOpenID['v2url']) . "'></td></tr>" .
+		"</table></fieldset>"
+	);
+
+	return TRUE;
 }
- 
-/* --- */
- 
+
 function wfLinkOpenIDSavePrefs($form, $user, &$message) {
-  $user->setOption('wflinkopenid_openid', $form->wfLinkOpenID['openid'] );
-  $user->setOption('wflinkopenid_v1url', $form->wfLinkOpenID['v1url'] );
-  $user->setOption('wflinkopenid_v2url', $form->wfLinkOpenID['v2url'] );
-  return TRUE;
+	$user->setOption('wflinkopenid_openid', $form->wfLinkOpenID['openid'] );
+	$user->setOption('wflinkopenid_v1url', $form->wfLinkOpenID['v1url'] );
+	$user->setOption('wflinkopenid_v2url', $form->wfLinkOpenID['v2url'] );
+	return TRUE;
 }
- 
-/* --- */
- 
+
 function wfLinkOpenIDResetPrefs($prefs, $user) {
-  $prefs->wfLinkOpenID = Array();
-  $prefs->wfLinkOpenID['openid'] = $user->getOption('wflinkopenid_openid');
-  $prefs->wfLinkOpenID['v1url'] = $user->getOption('wflinkopenid_v1url');
-  $prefs->wfLinkOpenID['v2url'] = $user->getOption('wflinkopenid_v2url');
-  return TRUE; 
+	$prefs->wfLinkOpenID = Array();
+	$prefs->wfLinkOpenID['openid'] = $user->getOption('wflinkopenid_openid');
+	$prefs->wfLinkOpenID['v1url'] = $user->getOption('wflinkopenid_v1url');
+	$prefs->wfLinkOpenID['v2url'] = $user->getOption('wflinkopenid_v2url');
+	return TRUE;
 }
- 
-?>
