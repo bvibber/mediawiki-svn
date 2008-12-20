688894e52f7f30f0e8cf5c8c79ca114c369668ac
pinky <pinky@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-12-20 01:18:28 +0000

    Adding new extension GlobalUserrights. Extends UserrightsPage to provide a special page for managing user rights across multiple wikis
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@44821 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/GlobalUserrights/GlobalUserrights.alias.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Aliases for GlobalUserrights
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'GlobalUserrights' => array( 'GlobalUserRights' ),
+);
+
+/** German (Deutsch) */
+$aliases['de'] = array(
+	'GlobalUserrights' => array( 'Globalebenutzerrechte' )
+);
---
+++ extensions/GlobalUserrights/GlobalUserrights.i18n.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Internationalisation file for GlobalUserrights
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Nathaniel Herman
+ */
+$messages['en'] = array(
+	'globaluserrights' => 'Global User Rights Management',
+	'gur-rightslog-name' => 'Global rights log',
+	'gur-rightslog-header' => 'This is a log of changes to global rights.',
+	'gur-rightslog-entry' => 'changed global group membership for $1 from $2 to $3'
+);
+
+/** German (Deutsch) */
+$messages['de'] = array(
+	'globaluserrights' => 'Globale Benutzerrechtewaltung'
+);
