3a641c57f0bc7c40ce82e690f4fbd1b859ea30eb
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-12-26 19:48:14 +0000

    Remove one of double 'only'. Thanks to ChrisiPK for noticing.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45059 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/regexBlock/regexBlock.i18n.php
+++ extensions/regexBlock/regexBlock.i18n.php
@@ -21,7 +21,7 @@ If you believe this is in error, please $1',
 	'regexblock-reason-regex'         => 'This username is prevented from editing due to vandalism or other disruption by a user with a similar name.
 Please create an alternate user name or $1 about the problem',
 	'regexblock-help'                 => 'Use the form below to block write access from a specific IP address or username.
-This should be done only only to prevent vandalism, and in accordance with policy.
+This should be done only to prevent vandalism, and in accordance with policy.
 \'\'This page will allow you to block even non-existing users, and will also block users with names similar to given, i.e. "Test" will be blocked along with "Test 2" etc.
 You can also block full IP addresses, meaning that no one logging from them will be able to edit pages.
 Note: partial IP addresses will be treated by usernames in determining blocking.
