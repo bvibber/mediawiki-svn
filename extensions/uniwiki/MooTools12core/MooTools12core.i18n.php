5f34630a9ecb7376c14009be9693626dbd8d4205
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-12-27 12:33:19 +0000

    Provide a little more information in 'mootools12core-desc'. Per Nikerabbit.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45085 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/uniwiki/MooTools12core/MooTools12core.i18n.php
+++ extensions/uniwiki/MooTools12core/MooTools12core.i18n.php
@@ -10,7 +10,7 @@ $messages = array();
 
 /** English */
 $messages['en'] = array(
-	'mootools12core-desc' => 'Adds mootools-1.2-core-yc.js to each page',
+	'mootools12core-desc' => 'Adds [http://mootools.net mootools-1.2-core-yc.js], a compact JavaScript framework, to each page',
 );
 
 /** Message documentation (Message documentation)
@@ -25,49 +25,49 @@ Do not translate or change links.',
  * @author DCLXVI
  */
 $messages['bg'] = array(
-	'mootools12core-desc' => 'Добавя mootools-1.2-core-yc.js към всяка страница',
+	'mootools12core-desc' => 'Добавя [http://mootools.net mootools-1.2-core-yc.js] към всяка страница',
 );
 
 /** Czech (Česky)
  * @author Matěj Grabovský
  */
 $messages['cs'] = array(
-	'mootools12core-desc' => 'Přidává mootools-1.2-core-yc.js na každou stránku',
+	'mootools12core-desc' => 'Přidává [http://mootools.net mootools-1.2-core-yc.js] na každou stránku',
 );
 
 /** German (Deutsch)
  * @author Melancholie
  */
 $messages['de'] = array(
-	'mootools12core-desc' => 'Fügt mootools-1.2-core-yc.js in jede Seite ein',
+	'mootools12core-desc' => 'Fügt [http://mootools.net mootools-1.2-core-yc.js] in jede Seite ein',
 );
 
 /** Lower Sorbian (Dolnoserbski)
  * @author Michawiki
  */
 $messages['dsb'] = array(
-	'mootools12core-desc' => 'Pśidawa mootools-1.2-core-yc.js kuždemu bokoju',
+	'mootools12core-desc' => 'Pśidawa [http://mootools.net mootools-1.2-core-yc.js] kuždemu bokoju',
 );
 
 /** Esperanto (Esperanto)
  * @author Yekrats
  */
 $messages['eo'] = array(
-	'mootools12core-desc' => 'Aldonas ilojn mootools-1.2-core-yc.js al ĉiu paĝo',
+	'mootools12core-desc' => 'Aldonas ilojn [http://mootools.net mootools-1.2-core-yc.js] al ĉiu paĝo',
 );
 
 /** Spanish (Español)
  * @author Sanbec
  */
 $messages['es'] = array(
-	'mootools12core-desc' => 'Añade mootools-1.2-core-yc.js a cada página',
+	'mootools12core-desc' => 'Añade [http://mootools.net mootools-1.2-core-yc.js] a cada página',
 );
 
 /** Finnish (Suomi)
  * @author Mobe
  */
 $messages['fi'] = array(
-	'mootools12core-desc' => 'Lisää koodin mootools-1.2-core-yc.js jokaiselle sivulle.',
+	'mootools12core-desc' => 'Lisää koodin [http://mootools.net mootools-1.2-core-yc.js] jokaiselle sivulle.',
 );
 
 /** French (Français)
@@ -75,139 +75,139 @@ $messages['fi'] = array(
  * @author Sherbrooke
  */
 $messages['fr'] = array(
-	'mootools12core-desc' => 'Ajouter mootools-1.2-core-yc.js à chaque page',
+	'mootools12core-desc' => 'Ajouter [http://mootools.net mootools-1.2-core-yc.js] à chaque page',
 );
 
 /** Galician (Galego)
  * @author Toliño
  */
 $messages['gl'] = array(
-	'mootools12core-desc' => 'Engade mootools-1.2-core-yc.js a cada páxina',
+	'mootools12core-desc' => 'Engade [http://mootools.net mootools-1.2-core-yc.js] a cada páxina',
 );
 
 /** Hebrew (עברית)
  * @author Rotemliss
  */
 $messages['he'] = array(
-	'mootools12core-desc' => 'הוספת mootools-1.2-core-yc.js לכל דף',
+	'mootools12core-desc' => 'הוספת [http://mootools.net mootools-1.2-core-yc.js] לכל דף',
 );
 
 /** Upper Sorbian (Hornjoserbsce)
  * @author Michawiki
  */
 $messages['hsb'] = array(
-	'mootools12core-desc' => 'Přidawa mootools-1.2-core-yc.js kóždej stronje',
+	'mootools12core-desc' => 'Přidawa [http://mootools.net mootools-1.2-core-yc.js] kóždej stronje',
 );
 
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
 $messages['ia'] = array(
-	'mootools12core-desc' => 'Adde mootools-1.2-core-yc.js a cata pagina',
+	'mootools12core-desc' => 'Adde [http://mootools.net mootools-1.2-core-yc.js] a cata pagina',
 );
 
 /** Japanese (日本語)
  * @author Mizusumashi
  */
 $messages['ja'] = array(
-	'mootools12core-desc' => 'mootools-1.2-core-yc.jsをそれぞれのページに加える',
+	'mootools12core-desc' => '[http://mootools.net mootools-1.2-core-yc.js]をそれぞれのページに加える',
 );
 
 /** Javanese (Basa Jawa)
  * @author Pras
  */
 $messages['jv'] = array(
-	'mootools12core-desc' => 'Tambahaké mootools-1.2-core-yc.js ing saben kaca',
+	'mootools12core-desc' => 'Tambahaké [http://mootools.net mootools-1.2-core-yc.js] ing saben kaca',
 );
 
 /** Khmer (ភាសាខ្មែរ)
  * @author Thearith
  */
 $messages['km'] = array(
-	'mootools12core-desc' => 'បន្ថែម mootools-1.2-core-yc.js ទៅ​ទំព័រ​នីមួយៗ',
+	'mootools12core-desc' => 'បន្ថែម [http://mootools.net mootools-1.2-core-yc.js] ទៅ​ទំព័រ​នីមួយៗ',
 );
 
 /** Ripoarisch (Ripoarisch)
  * @author Purodha
  */
 $messages['ksh'] = array(
-	'mootools12core-desc' => 'Deit <i lang="en">mootools-1.2-core-yc.js</i> op jede Sigg.',
+	'mootools12core-desc' => 'Deit <i lang="en">[http://mootools.net mootools-1.2-core-yc.js]</i> op jede Sigg.',
 );
 
 /** Luxembourgish (Lëtzebuergesch)
  * @author Robby
  */
 $messages['lb'] = array(
-	'mootools12core-desc' => 'mootools-1.2-core-yc.js op all Säit derbäisetzen',
+	'mootools12core-desc' => '[http://mootools.net mootools-1.2-core-yc.js] op all Säit derbäisetzen',
 );
 
 /** Nedersaksisch (Nedersaksisch)
  * @author Servien
  */
 $messages['nds-nl'] = array(
-	'mootools12core-desc' => 'Voeg mootools-1.2-core-yc.js toe an elke pagina',
+	'mootools12core-desc' => 'Voeg [http://mootools.net mootools-1.2-core-yc.js] toe an elke pagina',
 );
 
 /** Dutch (Nederlands)
  * @author Siebrand
  */
 $messages['nl'] = array(
-	'mootools12core-desc' => 'Voegt mootools-1.2-core-yc.js toe aan iedere pagina',
+	'mootools12core-desc' => 'Voegt [http://mootools.net mootools-1.2-core-yc.js] toe aan iedere pagina',
 );
 
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
  * @author Jon Harald Søby
  */
 $messages['no'] = array(
-	'mootools12core-desc' => 'Legger til mootools-1.2-core-yc.js på hver side',
+	'mootools12core-desc' => 'Legger til [http://mootools.net mootools-1.2-core-yc.js] på hver side',
 );
 
 /** Occitan (Occitan)
  * @author Cedric31
  */
 $messages['oc'] = array(
-	'mootools12core-desc' => 'Apondre mootools-1.2-core-yc.js a cada pagina',
+	'mootools12core-desc' => 'Apondre [http://mootools.net mootools-1.2-core-yc.js] a cada pagina',
 );
 
 /** Portuguese (Português)
  * @author Waldir
  */
 $messages['pt'] = array(
-	'mootools12core-desc' => 'Adiciona mootools-1.2-core-yc.js a todas as páginas',
+	'mootools12core-desc' => 'Adiciona [http://mootools.net mootools-1.2-core-yc.js] a todas as páginas',
 );
 
 /** Romanian (Română)
  * @author KlaudiuMihaila
  */
 $messages['ro'] = array(
-	'mootools12core-desc' => 'Adaugă mootools-1.2-core-yc.js la fiecare pagină',
+	'mootools12core-desc' => 'Adaugă [http://mootools.net mootools-1.2-core-yc.js] la fiecare pagină',
 );
 
 /** Tarandíne (Tarandíne)
  * @author Joetaras
  */
 $messages['roa-tara'] = array(
-	'mootools12core-desc' => "Aggiunge 'nu mootools-1.2-core-yc.js a ogne pàgene",
+	'mootools12core-desc' => "Aggiunge 'nu [http://mootools.net mootools-1.2-core-yc.js] a ogne pàgene",
 );
 
 /** Russian (Русский)
  * @author Александр Сигачёв
  */
 $messages['ru'] = array(
-	'mootools12core-desc' => 'Добавляет на каждую страницу mootools-1.2-core-yc.js',
+	'mootools12core-desc' => 'Добавляет на каждую страницу [http://mootools.net mootools-1.2-core-yc.js]',
 );
 
 /** Slovak (Slovenčina)
  * @author Helix84
  */
 $messages['sk'] = array(
-	'mootools12core-desc' => 'Pridáva mootools-1.2-core-yc.js na každú stránku',
+	'mootools12core-desc' => 'Pridáva [http://mootools.net mootools-1.2-core-yc.js] na každú stránku',
 );
 
 /** Swedish (Svenska)
  * @author Najami
  */
 $messages['sv'] = array(
-	'mootools12core-desc' => 'Lägger till mootools-1.2-core-yc.js på varje sida',
+	'mootools12core-desc' => 'Lägger till [http://mootools.net mootools-1.2-core-yc.js] på varje sida',
 );
 
