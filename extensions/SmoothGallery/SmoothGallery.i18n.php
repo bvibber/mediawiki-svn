37f192cbe9fbaf9de21f7044c816f6fde09df81d
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-12-28 13:17:36 +0000

    Adding PLURAL to messages: smoothgallery-invalid-images smoothgallery-no-images
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45122 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SmoothGallery/SmoothGallery.i18n.php
+++ extensions/SmoothGallery/SmoothGallery.i18n.php
@@ -20,9 +20,9 @@ Allows most options of SmoothGallery',
 	'smoothgallery-not-found'           => 'No images were added into the gallery.
 Please add at least one image.',
 	'smoothgallery-no-images'           => 'No images were found in this gallery.
-Make sure all images requested exist.
+Make sure {{PLURAL:$2|the image|all images}} requested exist.
 The following images were not found: $1',
-	'smoothgallery-invalid-images'      => 'The following requested images were of an invalid type: $1',
+	'smoothgallery-invalid-images'      => 'The following requested {{PLURAL:$2|image was|images were}} of an invalid type: $1',
 	'smoothgallery-unexpected-error'    => 'There was an unexpected error.
 Please file a bug report.',
 	'smoothgallery-javascript-disabled' => 'Javascript is required to view this gallery properly.',
