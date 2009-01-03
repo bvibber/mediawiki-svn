b54e17be22168fe3af896cc54eadff86233280eb
btongminh <btongminh@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-03 15:41:32 +0000

    Creating branch for new uploading architecture from r45369
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-upload@45371 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/languages/messages/MessagesKri.php
@@ -0,0 +1,390 @@
+<?php
+/** Krio (Krio)
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Jose77
+ * @author Meno25
+ * @author Protostar
+ */
+
+$messages = array(
+# User preference toggles
+'tog-justify'         => 'johstifai di paragraf-dehm',
+'tog-hideminor'       => 'Aid di smohl-smohl chenj-dehm na di list ov tin-dehm we dehm johs chenj',
+'tog-extendwatchlist' => 'Luk di big watchlist we geht ohltin we dehm dohn chenj',
+
+# Dates
+'sun'           => 'Sohn',
+'mon'           => 'Mohn',
+'tue'           => 'Tyu',
+'wed'           => 'Wehn',
+'thu'           => 'Toh',
+'fri'           => 'Frai',
+'sat'           => 'Sat',
+'january'       => 'Janyuari',
+'february'      => 'Fehbruari',
+'march'         => 'Mach',
+'april'         => 'Epril',
+'may_long'      => 'Mey',
+'june'          => 'Jun',
+'july'          => 'Julai',
+'august'        => 'Ohgohst',
+'september'     => 'Sehptehmba',
+'october'       => 'Ohktoba',
+'november'      => 'Novehmba',
+'december'      => 'Disehmba',
+'january-gen'   => 'Janyuari',
+'february-gen'  => 'Fehbuari',
+'march-gen'     => 'Mach',
+'april-gen'     => 'Epril',
+'may-gen'       => 'Mey',
+'june-gen'      => 'Jun',
+'july-gen'      => 'Julai',
+'august-gen'    => 'Ohgohst',
+'september-gen' => 'Sehptehmba',
+'october-gen'   => 'Ohktoba',
+'november-gen'  => 'Novehmba',
+'december-gen'  => 'Disehmba',
+'jan'           => 'Jan',
+'feb'           => 'Fehb',
+'mar'           => 'Mach',
+'apr'           => 'Apr',
+'may'           => 'Mey',
+'jun'           => 'Jun',
+'jul'           => 'Jul',
+'aug'           => 'Ohg',
+'sep'           => 'Seh',
+'oct'           => 'Ohk',
+'nov'           => 'Nov',
+'dec'           => 'Dis',
+
+# Categories related messages
+'pagecategories'        => '{{PLURAL:$1|Katigori|Katigori-dehm}}',
+'category_header'       => 'Pej-dehm we de na katigori "$1"',
+'subcategories'         => 'Sohbkatigori-dehm',
+'category-media-header' => 'Midya we de na katigori "$1"',
+'category-empty'        => "''Dis katigori-ya noh geht noh pej o midya foh naw.''",
+'hidden-categories'     => '{{PLURAL:$1|Katigori we dehm dohn ayd|Katigori-dehm we dehm dohn ayd}}',
+
+'qbedit' => 'Chenj',
+'mytalk' => 'Mi Tohk',
+
+'returnto'         => 'Go bak tu $1.',
+'tagline'          => 'Dis kohmoh {{SITENAME}}',
+'help'             => 'Ehp mi',
+'search'           => 'Luk foh am',
+'searchbutton'     => 'Luk foh am',
+'go'               => 'Go to am',
+'searcharticle'    => 'Go de',
+'history'          => 'Pej-im istri',
+'history_short'    => 'Istri',
+'updatedmarker'    => 'wetin dehm dohn ohpdet since wehn ah las kam',
+'info_short'       => 'Infohmeshohn',
+'printableversion' => 'Vazhon foh Print',
+'permalink'        => 'Pamanehnt link',
+'print'            => 'Print am',
+'edit'             => 'Chenj',
+'create'           => 'Mek sohmtin',
+'editthispage'     => 'Edit dis pej-ya',
+'create-this-page' => 'Mek dis pej-ya',
+'delete'           => 'Ires',
+'deletethispage'   => 'Ires dis pej-ya',
+'protectthispage'  => 'Protekt dis pej-ya',
+'talkpagelinktext' => 'Tohk',
+'specialpage'      => 'Spehshal Pej',
+'personaltools'    => 'Mi-yon tul-dehm',
+'postcomment'      => 'Lef yu-yon opinyon',
+'articlepage'      => 'Luk wetin de insai',
+'talk'             => 'Diskohshohn',
+'views'            => 'We-dehm foh luk dis pej-ya',
+'toolbox'          => 'Tulbohks',
+'userpage'         => 'Luk dis pohsin-im yuza-pej',
+'jumpto'           => 'Jomp go:',
+'jumptonavigation' => 'navigeshohn',
+'jumptosearch'     => 'luk foh am',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite'            => 'Boht {{SITENAME}}',
+'aboutpage'            => 'Project:Boht',
+'copyright'            => 'Dis kontehnt-im prais noh pas $1 .',
+'copyrightpagename'    => '{{wehbsait-im nem}} kohpirait',
+'disclaimers'          => 'Disklema-dehm',
+'disclaimerpage'       => 'Project:Disklema foh ohltin',
+'edithelp'             => 'Advais foh we yu wan ehdit',
+'edithelppage'         => 'Help:Ehditin',
+'faq'                  => 'Kweshtyohn-dehm we ohlman de aks',
+'faqpage'              => 'Project:Kwehstyohn-dehm we ohlman de aks',
+'helppage'             => 'Help:Kohntehnt-dehm',
+'mainpage'             => 'Men Pej',
+'mainpage-description' => 'Men Pej',
+'policy-url'           => 'Project:Pohlisi',
+'portal'               => 'Mitin-ples foh yuza-dehm',
+'portal-url'           => 'Project:Mitin-ples foh yuza-dehm',
+'privacy'              => 'Pohlisi foh Praivesi',
+'privacypage'          => 'Project:Pohlisi foh Praivesi',
+
+'badaccess'        => 'Yu noh geh pamishohn foh luk dis',
+'badaccess-group0' => 'Yu noh geht pamishohn foh du wetin yu johs chuz',
+'badaccess-groups' => 'Na di pipul-dehm we dohn johyn {{PLURAL:$2|the group|one of the groups}}: $1 nohmoh ebul du wetin yu johs chuz.',
+
+'versionrequired'     => 'Yu nid MediaWiki Vazhohn $1',
+'versionrequiredtext' => 'Yu nid MediaWiki Vazhohn $1 foh yuz dis pej-ya.
+Luk [[Special:Version|version page]].',
+
+'ok'                   => 'OK',
+'retrievedfrom'        => 'Dis kohmoh "$1"',
+'youhavenewmessages'   => 'Yu dohn geht $1 ($2).',
+'editsection'          => 'chenj',
+'editsection-brackets' => '[$1]',
+'editold'              => 'chenj',
+'viewsourceold'        => 'luk di source',
+'editsectionhint'      => 'chenj dis sekshohn: $1',
+'toc'                  => 'Wetin de insai',
+'showtoc'              => 'sho am',
+'hidetoc'              => 'aid am',
+'thisisdeleted'        => 'Luk wetin i bi naw o tohn am bak tu wetin i bin bi bifo $1?',
+'viewdeleted'          => 'Luk am $1?',
+'restorelink'          => '{{PLURAL:$1|wan chenj we dehm dohn ires|$1 chenj-dehm we dehm dohn ires}}',
+'feedlinks'            => 'Feed:',
+'site-rss-feed'        => '$1 RSS Feed',
+'site-atom-feed'       => '$1 Atom Feed',
+
+# Short words for each namespace, by default used in the namespace tab in monobook
+'nstab-image'     => 'Fail',
+'nstab-mediawiki' => 'Mehsej',
+'nstab-template'  => 'Tehmplet',
+'nstab-help'      => 'Advais pej',
+'nstab-category'  => 'Katigohri',
+
+# Main script and global functions
+'nosuchaction'      => 'Da kain akshohn noh de',
+'nosuchactiontext'  => 'di wiki noh de ohndastand di akshon we di URL tehl am foh du',
+'nosuchspecialpage' => 'Da kain spehshal pej noh de',
+
+# General errors
+'viewsource' => 'Luk di source',
+
+# Login and logout pages
+'nav-login-createaccount' => ' Kam insai di sait/ Mek yu-yon yuza',
+'loginprompt'             => 'Yu go geht foh lait yu cookies bifo yu go ebul foh kam insai {{SITENAME}}.',
+'userlogin'               => 'Kam insai di sait / Mek yu-yon yuza',
+'logout'                  => 'Kohmoh na di sait',
+'userlogout'              => 'Kohmoh na di sait',
+'notloggedin'             => 'Yu noh kam insai di sait yet',
+'nologin'                 => 'Yu noh geht yuza login? $1.',
+'nologinlink'             => 'Mek yu-yon yuza login',
+'createaccount'           => 'I dohn',
+'gotaccount'              => 'Yu dohn mek yu yuza login? $1.',
+'gotaccountlink'          => 'Kam insai di sait',
+'createaccountmail'       => 'wit e-mel',
+'badretype'               => 'Di tu paswohd-dehm noh mach.',
+'userexists'              => 'Ohda pohsin dohn tek da yuzanem de.
+Yu go geht foh pik ohda wan.',
+'youremail'               => 'E-mail:',
+'username'                => 'Yuzanem:',
+
+# Edit pages
+'watchthis'                        => 'Wach dis pej-ya',
+'savearticle'                      => 'Sev dis pej-ya',
+'showpreview'                      => 'Luk di privyu',
+'showlivepreview'                  => 'Luk di laiv privyu',
+'showdiff'                         => 'Sho wetin dohn chenj',
+'anoneditwarning'                  => "'''Wet O:''' Yu noh log-in yet.
+So dehm go sev yu IP address na di pej-im chenj istri",
+'missingsummary'                   => "'''Mehmba se:''' yu noh mek chenj sohmari.
+If yu pres Sev bak, dehm go sev yu chenj lehk so.",
+'missingcommenttext'               => 'Rait yu opinyohn ohnda yah.',
+'permissionserrorstext-withaction' => 'Yu noh go ebul fo $2, bicoh {{PLURAL:$1|reason|reasons}}:',
+'recreate-deleted-warn'            => "'''Ebo!!: Yu de mek bak wan pej we dehm bin dohn ires.'''
+
+Yu sabi if yu foh wok pan dis pej-ya.
+Di log we dehm kip foh dis pej-in dilishohn de ya:",
+'deleted-notice'                   => 'Dehm dohn ires dis pej-ya.
+Di log we dehm kip foh dis pej-in dilishohn de ya:',
+'deletelog-fulllog'                => 'Luk di wan-ol lohg',
+'edit-hook-aborted'                => 'Wan Hook dohn pul wetin yu chenj.
+I noh eksplen wetin-mek.',
+'edit-gone-missing'                => 'Wi noh ebul updet dis pej-ya.
+Sohmtehm dehm dohn ires am.',
+'edit-conflict'                    => 'Chenj dehm tin-dehm ya we noh gri wit dehm kohmpin.',
+'edit-no-change'                   => 'Wi no luk wetin yu chenj, bicoh yu no chenj wetin rait de.',
+
+# History pages
+'last'       => 'las',
+'page_first' => 'fohs',
+'page_last'  => 'las',
+
+# Diffs
+'editundo' => 'chenj am bak tu wetin i bin bi bifo',
+
+# Search results
+'prevn'              => 'Go bak $1',
+'nextn'              => 'nehks wan $1',
+'searchhelp-url'     => 'Help:Kohntehnt-dehm',
+'powersearch'        => 'Luk foh sohmtin fain-wan',
+'powersearch-legend' => 'Luk foh sohmtin fain-wan',
+'powersearch-ns'     => 'Luk foh sohmtin na namespaces:',
+'powersearch-redir'  => 'List de sehn yu kam yah',
+'powersearch-field'  => 'Luk foh',
+'search-external'    => 'Go fehn am ohdasai',
+'searchdisabled'     => '{{websait-im nem}} Di shach no de wok.
+We yu de wet foh mek im kam bak yu kin yuz Google yon.
+Mehmba se sohmtehm dehm indeks-dehm noh ohp-tu-det {{SITENAME}}.',
+
+# Preferences page
+'prefsnologin'      => 'Yu noh kam insai di sait yet',
+'searchresultshead' => 'Luk foh am',
+
+# Upload
+'upload'          => 'Ohplod fail',
+'uploadbtn'       => 'Ohplod fail',
+'uploadnologin'   => 'Yu noh kam insai di sait yet',
+'watchthisupload' => 'Wach dis pej-ya',
+
+# File description page
+'filehist-deleteone' => 'ires',
+'filehist-datetime'  => 'Det/Taim',
+'filehist-thumb'     => 'Tohmnel',
+'imagelinks'         => 'Link-dehm',
+'linkstoimage'       => 'Dehm {{PLURAL:$1|pej de link|pej-dehm ya de link}} go dis fail:',
+'linkstoimage-more'  => 'I pahs wan $1 {{PLURAL:$1|pej we de link|pej-dem we delink}} go dis fail-ya.
+Dis list geht di{{PLURAL:$1|fohs pej link|fohs $1 pej link-dehm}} foh dis fail nohmoh.
+Di [[Special:WhatLinksHere/$2|ful list]] de yah.',
+'nolinkstoimage'     => 'Dis fail-ya noh geht noh pej we de link go tu am.',
+
+# File deletion
+'filedelete-submit' => 'Ires',
+
+'brokenredirects-edit'   => '(chenj)',
+'brokenredirects-delete' => '(ires)',
+
+# Miscellaneous special pages
+'movethispage' => 'Muf dis pej-ya',
+
+# Book sources
+'booksources-go' => 'Go to am',
+
+# Special:AllPages
+'alphaindexline' => '$1 tu $2',
+
+# Special:LinkSearch
+'linksearch-ok' => 'Luk foh am',
+
+# Watchlist
+'watchlist'         => 'Mi list foh wach',
+'mywatchlist'       => 'Mi list foh wach',
+'watchlistfor'      => "(foh '''$1''')",
+'nowatchlist'       => 'Yu noh geht natin na yu list foh wach.',
+'watchlistanontext' => '$1 Foh luk o chenj wetin de na yu list foh wach',
+'watchnologin'      => 'Yu noh kam insai di sait yet',
+'watch'             => 'Wach',
+'watchthispage'     => 'Wach dis pej-ya',
+'unwatch'           => 'Lehf foh wach dis pej-ya',
+'unwatchthispage'   => 'Lehf foh wach',
+'notanarticle'      => 'Dis pej-ya noh de tehl yu wetin de insai',
+'notvisiblerev'     => 'Dehm dohn ires di rivizhon',
+
+# Displayed when you click the "watch" button and it is in the process of watching
+'watching'   => 'Ah de wach...',
+'unwatching' => 'Ah de lehf foh wach...',
+
+# Delete
+'deletepage'    => 'Ires dis pej-ya',
+'confirm'       => 'Kohnfam',
+'excontent'     => "Nah dis i bin geht/ de tohk boht: '$1'",
+'delete-legend' => 'Ires',
+'dellogpage'    => 'Di list ov wetin dehn dohn ires',
+
+# Restrictions (nouns)
+'restriction-edit'   => 'Chenj',
+'restriction-create' => 'Mek sohmtin',
+
+# Undelete
+'undelete-search-submit' => 'Luk foh am',
+
+'sp-contributions-submit' => 'Luk foh am',
+
+# What links here
+'whatlinkshere' => 'Wetin de link kam yah',
+
+# Block/unblock
+'ipblocklist-submit' => 'Luk foh am',
+'contribslink'       => 'kohntribyushohn-dehm',
+
+# Move page
+'movenologin' => 'Yu noh kam insai di sait yet',
+'move-watch'  => 'Wach dis pej-ya',
+
+# Thumbnails
+'thumbnail-more'  => 'Mek i big',
+'filemissing'     => 'Di fail noh de',
+'thumbnail_error' => 'Nah prohblehm foh mek dis tohmbnel: $1',
+
+# Tooltip help for the actions
+'tooltip-pt-mytalk'               => 'Mi tohk pej',
+'tooltip-pt-mycontris'            => 'List ov wetin ah dohn kohntribyut',
+'tooltip-pt-login'                => 'I go behteh if yu log-in, boht if yu noh du am i noh bad.',
+'tooltip-pt-anonlogin'            => 'I go behteh if yu log-in, boht if yu noh du am i noh bad.',
+'tooltip-pt-logout'               => 'Kohmoh na di sait',
+'tooltip-ca-talk'                 => 'Diskohshohn boht wetin de na di kohntehnt pej',
+'tooltip-ca-edit'                 => 'Yu ebul foh chenj dis pej-ya
+Boht prehs di privyu bohtin befo yu sev am',
+'tooltip-ca-addsection'           => 'Lehf yu-yon opinyon na dis diskohshohn.',
+'tooltip-ca-viewsource'           => 'Yu noh go ebul chenj di pej-ya.
+Na im source nohmoh yu go ebul luk.',
+'tooltip-ca-history'              => 'Dehm wan-dehm-ya na  di pej-im ol vazhon-dem.',
+'tooltip-ca-protect'              => 'Protekt dis pej-ya',
+'tooltip-ca-delete'               => 'Ires dis pej-ya',
+'tooltip-ca-undelete'             => 'Bring bak di chenj-dehm we dehm bin mek na dis pej-ya bifo dehm ires am.',
+'tooltip-ca-move'                 => 'Muf dis pej-ya',
+'tooltip-ca-watch'                => 'Put dis pej na yu list foh wach',
+'tooltip-ca-unwatch'              => 'Pul dis pej komoh nah yu list foh wach',
+'tooltip-search'                  => 'Luk nah dis wehbsait {{SITENAME}}',
+'tooltip-n-mainpage'              => 'Go si di Men Pej',
+'tooltip-n-portal'                => 'Infohmashohn boht di projehkt, wetin yu ebul du, usai foh fehn ohltin',
+'tooltip-n-currentevents'         => 'Luk foh infohmashon boht wetin de apin naunaw',
+'tooltip-n-recentchanges'         => 'List ov wetin dehm dohn chenj risehntli na di wiki.',
+'tooltip-n-randompage'            => 'Go tu ehni pej',
+'tooltip-n-help'                  => 'Di ples foh lan.',
+'tooltip-t-whatlinkshere'         => 'Pej we geht ohl di wiki pej-dehm we de link kam yah',
+'tooltip-t-recentchangeslinked'   => 'Chenj-dehm we dehm dohn mek na di pej-dem we de link kohmoh na dis pej-ya',
+'tooltip-feed-rss'                => 'RSS feed foh dis pej-ya',
+'tooltip-feed-atom'               => 'Atom feed foh dis pej-ya',
+'tooltip-t-contributions'         => 'dis yuza-im kohntribyushohn list',
+'tooltip-t-emailuser'             => 'Sehn e-mail gi dis yuza',
+'tooltip-t-upload'                => 'Ohplod fail-dehm',
+'tooltip-t-specialpages'          => 'Spehshal pej list',
+'tooltip-t-print'                 => 'Vazhon foh print',
+'tooltip-t-permalink'             => 'Pamanehnt link go dis vazhon',
+'tooltip-save'                    => 'Sev wetin yu dohn chenj',
+'tooltip-preview'                 => 'Luk wetin you dohn chenj, Luk am bifo yu sev!',
+'tooltip-diff'                    => 'Luk us chenj-dehm yu mek nah di tehks.',
+'tooltip-compareselectedversions' => 'Luk wetin difrehnt pan dehm tu vazhon-dehm we yu dohn chuz',
+'tooltip-watch'                   => 'Put dis pej-ya na yu list foh wach',
+
+# Special:NewFiles
+'ilsubmit' => 'Luk foh am',
+
+# 'all' in various places, this might be different for inflected languages
+'namespacesall' => 'ohl',
+'monthsall'     => 'ohl',
+
+# E-mail address confirmation
+'confirmemail'         => 'Konfam yu e-mail adrehs',
+'confirmemail_noemail' => 'Di email adrehs we yu pu na [[Special:Preferences|user preferences]] noh de.',
+
+# Multipage image navigation
+'imgmultigo' => 'Go to am!',
+
+# Table pager
+'table_pager_limit_submit' => 'Go to am',
+
+# Special:FileDuplicateSearch
+'fileduplicatesearch-submit' => 'Luk foh am',
+
+# Special:SpecialPages
+'specialpages' => 'Spehshal pej-dehm',
+
+);
---
+++ phase3/languages/messages/MessagesSa.php
@@ -0,0 +1,266 @@
+<?php
+/** Sanskrit (संस्कृत)
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Kaustubh
+ * @author Mahitgar
+ */
+
+$fallback = 'hi';
+
+$digitTransformTable = array(
+	'0' => '०', # &#x0966;
+	'1' => '१', # &#x0967;
+	'2' => '२', # &#x0968;
+	'3' => '३', # &#x0969;
+	'4' => '४', # &#x096a;
+	'5' => '५', # &#x096b;
+	'6' => '६', # &#x096c;
+	'7' => '७', # &#x096d;
+	'8' => '८', # &#x096e;
+	'9' => '९', # &#x096f;
+);
+
+$linkPrefixExtension = false;
+
+$namespaceNames = array(
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Special',
+	NS_MAIN	            => '',
+	NS_TALK	            => 'संभाषणं',
+	NS_USER             => 'योजकः',
+	NS_USER_TALK        => 'योजकसंभाषणं',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK     => '$1संभाषणं',
+	NS_FILE             => 'चित्रं',
+	NS_FILE_TALK        => 'चित्रसंभाषणं',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_talk',
+	NS_TEMPLATE         => 'Template',
+	NS_TEMPLATE_TALK    => 'Template_talk',
+	NS_HELP             => 'उपकारः',
+	NS_HELP_TALK        => 'उपकारसंभाषणं',
+	NS_CATEGORY         => 'वर्गः',
+	NS_CATEGORY_TALK    => 'वर्गसंभाषणं',
+);
+
+$messages = array(
+'underline-always' => 'सदा',
+
+# Dates
+'sunday'    => 'विश्रामवासरे',
+'monday'    => 'सोमवासरे',
+'tuesday'   => 'मंगलवासरे',
+'wednesday' => 'बुधवासरे',
+'thursday'  => 'गुरुवासरे',
+'friday'    => 'शुक्रवासरे',
+'saturday'  => 'शनिवासरे',
+'sun'       => 'विश्राम',
+'mon'       => 'सोम',
+'tue'       => 'मंगल',
+'wed'       => 'बुध',
+'thu'       => 'गुरु',
+'fri'       => 'शुक्र',
+'sat'       => 'शनि',
+'january'   => 'पौषमाघे',
+'february'  => 'फाल्गुने',
+'march'     => 'फाल्गुनचैत्रे',
+'april'     => 'मधुमासे',
+'may_long'  => 'वैशाखज्येष्ठे',
+'june'      => 'ज्येष्ठाषाढके',
+'july'      => 'आषाढश्रावणे',
+'august'    => 'नभस्ये',
+'september' => 'भाद्रपदाश्विने',
+'october'   => 'अश्विनकार्तिके',
+'november'  => 'कार्तिकमार्गशीर्षे',
+'december'  => 'मार्गशीर्षपौषे',
+
+# Categories related messages
+'listingcontinuesabbrev' => 'आगामि.',
+
+'about'         => 'विषये',
+'newwindow'     => '(उद्घट् नविन पृष्ठ)',
+'cancel'        => 'अपकर्ष',
+'qbfind'        => 'शोध',
+'qbedit'        => 'संपादयति',
+'qbpageoptions' => 'इदम्‌ पृष्ठ',
+'navigation'    => 'सुचालन',
+'and'           => '&#32;एवम्',
+
+'help'             => 'सहायता',
+'search'           => 'शोध',
+'searchbutton'     => 'शोध',
+'go'               => 'गच्छति',
+'searcharticle'    => 'गच्छति',
+'history'          => 'पृष्ठस्य इतिहास',
+'history_short'    => 'इतिहास',
+'printableversion' => 'मुद्रणस्य पाठ',
+'permalink'        => 'स्थायी निबन्धन',
+'newpage'          => 'नविन पृष्ठ',
+'talkpagelinktext' => 'संवाद',
+'specialpage'      => 'विशेष पृष्ठ',
+'personaltools'    => 'वैयक्तिक साधन',
+'views'            => 'दृश्य',
+'toolbox'          => 'साधनपेटी',
+'jumptonavigation' => 'सुचालन',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite'            => '{{SITENAME}}विषये',
+'aboutpage'            => 'Project:विषये',
+'copyrightpage'        => '{{ns:project}}:प्रताधिकार',
+'currentevents'        => 'सद्य घटना',
+'disclaimers'          => 'स्वाम्यत्यागं',
+'edithelp'             => 'संपादनार्थं सहायता',
+'faq'                  => 'अतिप्रश्नपृष्ट',
+'helppage'             => 'Help:सहाय्य',
+'mainpage'             => 'मुखपृष्ठं',
+'mainpage-description' => 'मुखपृष्ठं',
+'privacy'              => 'गोपनीयविषये नीति',
+
+'newmessageslink' => 'नूतन संदेश',
+'editsection'     => 'संपादयति',
+
+# Short words for each namespace, by default used in the namespace tab in monobook
+'nstab-main'     => 'पृष्ठ',
+'nstab-image'    => 'संचिका',
+'nstab-template' => 'बिंबधर',
+'nstab-category' => 'वर्ग',
+
+# General errors
+'error'      => 'विभ्रम',
+'viewsource' => 'स्रोत पश्यति',
+
+# Login and logout pages
+'yourpassword'            => 'सङ्केतशब्द:',
+'login'                   => 'प्रवेश करोसि',
+'nav-login-createaccount' => 'प्रवेश करोसि/ सृज् उपयोजकसंज्ञा',
+'logout'                  => 'बहिर्गच्छति',
+'userlogout'              => 'बहिर्गच्छति',
+'createaccount'           => 'सृज उपयोजकसंज्ञा',
+'gotaccountlink'          => 'प्रवेश करोसि',
+'loginsuccesstitle'       => 'सुस्वागतम्‌ प्रवेश यशस्वी अस्ति',
+
+# Edit pages
+'watchthis'   => 'इदं पृष्ठ निरीक्षा',
+'savearticle' => 'पृष्ठ त्रायते',
+'preview'     => 'प्रारूप प्रेक्षा',
+'showpreview' => 'प्रारूप प्रेक्षा',
+'newarticle'  => '(नविन)',
+
+# History pages
+'currentrevisionlink' => 'सद्य आवृत्ती',
+'cur'                 => 'अद्य',
+'last'                => 'पूर्वतन',
+'page_first'          => 'प्रथम्‌',
+'page_last'           => 'अन्तिम',
+
+# Diffs
+'editundo' => 'पूर्ववत करोसि',
+
+# Search results
+'nextn'       => 'आगामि$1',
+'powersearch' => 'परिणत शोध',
+
+# Recent changes
+'recentchanges'   => 'नवीनतम परिवर्तन',
+'rcshowhideanons' => 'अनामिक योजकस्य परिवर्त $1',
+'hide'            => 'प्रछद्',
+'show'            => 'दर्शयति',
+
+# Recent changes linked
+'recentchangeslinked' => 'सम्भन्दिन् परिवर्त',
+
+# Upload
+'upload' => 'भारं न्यस्यति सञ्चिका',
+
+# Random page
+'randompage' => 'अविशिष्ट पृष्ठ',
+
+# Statistics
+'statistics' => 'सांख्यिकी',
+
+# Miscellaneous special pages
+'longpages'    => 'दीर्घ पृष्ठ',
+'newpages'     => 'नूतन पृष्ठ',
+'ancientpages' => 'प्राचीनतम् पृष्ठा',
+'movethispage' => 'इदं पृष्ठस्य स्थानांतर',
+
+# Book sources
+'booksources-go' => 'प्रस्थानम्',
+
+# Special:AllPages
+'allpages'       => 'सर्व पृष्ठ',
+'alphaindexline' => 'इतः $1 यावत् $2',
+'allarticles'    => 'सर्व लेखा',
+'allpagessubmit' => 'गच्छति',
+
+# Special:Categories
+'categories' => 'वर्ग',
+
+# E-mail user
+'emailsubject' => 'विषयः',
+'emailmessage' => 'सन्देशः',
+
+# Watchlist
+'watchthispage' => 'प्रतिरक्षति इदं पृष्ठ',
+
+# Displayed when you click the "watch" button and it is in the process of watching
+'watching' => 'निरिक्षा',
+
+# Delete
+'actioncomplete' => 'कार्य समापनम्',
+
+# Protect
+'protectcomment'          => 'प्रतिक्रीया:',
+'protect-level-sysop'     => 'केवल प्रबंधक',
+'protect-summary-cascade' => 'निःश्रेणि',
+'restriction-type'        => 'अनुमति:',
+
+# Namespace form on various pages
+'namespace' => 'नामविश्व:',
+
+# What links here
+'whatlinkshere' => 'किम्‌ पृष्ठ सम्बद्धं करोति',
+
+# Block/unblock
+'blocklink' => 'निषेध',
+
+# Namespace 8 related
+'allmessages'     => 'व्यवस्था सन्देशानि',
+'allmessagesname' => 'नाम',
+
+# Tooltip help for the actions
+'tooltip-search'          => '{{SITENAME}} अन्वेषणं करोति',
+'tooltip-n-mainpage'      => 'मुखपृष्ठ अभ्यागम',
+'tooltip-n-recentchanges' => 'नविनतम परिवर्तन सूची',
+'tooltip-n-randompage'    => 'अविशीष्ट लेख',
+'tooltip-save'            => 'त्रायते',
+
+# Skin names
+'skinname-standard'    => 'पूर्व',
+'skinname-nostalgia'   => 'पुराण',
+'skinname-cologneblue' => 'नील',
+'skinname-monobook'    => 'पुस्तक',
+'skinname-myskin'      => 'मे चर्मन्',
+'skinname-chick'       => 'Chick',
+
+# Special:NewFiles
+'newimages' => 'नूतन संचिका दालन',
+
+# 'all' in various places, this might be different for inflected languages
+'watchlistall2' => 'सर्व',
+'namespacesall' => 'सर्व',
+'monthsall'     => 'सर्व',
+
+# Auto-summaries
+'autosumm-new' => 'नवीन पृष्ठं: $1',
+
+# Special:Version
+'version' => 'आवृत्ति', # Not used as normal message but as header for the special page itself
+
+# Special:SpecialPages
+'specialpages' => 'विशेष पृष्ठ',
+
+);
