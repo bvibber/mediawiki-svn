4fb88fd55b0303cb348c7c6557efb7631e72cb67
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-05 20:43:33 +0000

    * make 'review_blocked' less rude * minor tweaks to a few other messages (no localisation updates needed)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45426 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Review/Review.i18n.php
+++ extensions/Review/Review.i18n.php
@@ -3,28 +3,29 @@
  * Internationalisation file for Review extension.
  *
  * @addtogroup Extensions
-*/
+ */
 
 $messages = array();
 
 $messages['en'] = array(
 	'review_sidebar_title' => "Review",
-	'review-desc' => 'The resurrected [[Special:Review|validation]] feature.',
+	'review-desc' => 'The resurrected [[Special:Review|validation]] feature',
 	'review_topics' => "# The following is the list of topics and ranges for the review extension.
 # Each topic is defined by a line of five values, separated by ':' (all other lines are ignored).
-# The order is: numerical key (>0), name, max range (>1; 2=yes/no), meaning of lowest value (e.g., 'bad'), meaning of highest value (e.g., 'good').
+# The order is: numerical key (>0), name, max range (>1; 2=yes/no), meaning of lowest value (f.e. 'bad'), meaning of highest value (f.e. 'good').
 1:Quality:5:bad:good
 2:Ready for 1.0:2:no:yes",
 	'review_topic_page' => "Review topics",
 	'review_sidebar_explanation' => "The leftmost checkbox always means 'no opinion'.",
 	'review_save' => "Store review",
 	'review_your_review' => "Your review of this page/revision:",
-	'review_sidebar_you_have_other_reviews_for_this_article' => "You have already reviewed other versions of this page. Your other ratings will be merged into this one where you selected 'no opinion'.",
+	'review_sidebar_you_have_other_reviews_for_this_article' => "You have already reviewed other versions of this page.
+Your other ratings will be merged into this one where you selected 'no opinion'.",
 	'review_page_link' => "review statistics",
 	'review_sidebar_final' => "For review data of this page view, see its $1",
 	'review_for_page' => "Review for page \"$1\"",
 	'review_for_user' => "Review for user \"$1\"",
-	'review_error' => "Something's wrong!",
+	'review_error' => "Something is wrong!",
 	'review_no_reviews_for_page' => "There are currently no reviews for \"$1\".",
 	'review_total_statistics' => "Total",
 	'review_statistics_left_corner' => "Revision",
@@ -41,7 +42,7 @@ $messages['en'] = array(
 	'review_has_been_stored' => "<span id='review_has_been_stored'>Your review has been stored!</span>",
 	'revision_review_this_page_version_link' => "Review this version of the page.",
 	'review_page_review' => "Review of page \"$1\"",
-	'review_blocked' => "You're blocked, go away.",
+	'review_blocked' => "You are blocked and cannot participate in reviews.",
 	'review_wrong_namespace' => "Pages in this namespace cannot be reviewed!",
 	'review_topic' => "Topic",
 	'review_no_opinion' => "No opinion",
