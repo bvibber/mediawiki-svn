ea85f3bb38097ddf5678d6f7fc3cf37b1ab11443
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-07 18:05:45 +0000

    * Use SpecialPage_initList to add the new special pages instead of using an extension function * Put hooks in a class, moved in an new file and autoload it * Added support for special page aliases and add it to Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45508 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/OpenID/OpenID.alias.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English
+ * @author IAlex
+ */
+$aliases['en'] = array(
+	'OpenIDLogin'  => array( 'OpenIDLogin' ),
+	'OpenIDFinish' => array( 'OpenIDFinish' ),
+	'OpenIDConvert' => array( 'OpenIDConvert' ),
+	'OpenIDServer' => array( 'OpenIDServer' ),
+	'OpenIDXRDS' => array( 'OpenIDXRDS' ),
+);
