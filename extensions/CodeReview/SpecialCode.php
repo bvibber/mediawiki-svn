0ef3724cc3e9c03fa5e260bdac9d0a2b08cd062f
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-14 14:29:53 +0000

    Bug 17011 Autolink URLs in CodeReview SVN commit messages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45727 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/SpecialCode.php
+++ extensions/CodeReview/SpecialCode.php
@@ -171,12 +171,21 @@ class CodeCommentLinker {
 	}
 
 	function link( $text ) {
+		# Catch links like http://www.mediawiki.org/wiki/Special:Code/MediaWiki/44245#c829
+		# Ended by space or brackets (like those pesky <br/> tags)
+		$text = preg_replace_callback( '/(\b)('.wfUrlProtocols().')([^ <>]+)(\b)/', array( $this, 'generalLink' ), $text );
 		$text = preg_replace_callback( '/\br(\d+)\b/', array( $this, 'messageRevLink' ), $text );
 		$text = preg_replace_callback( '/\bbug #?(\d+)\b/i', array( $this, 'messageBugLink' ), $text );
 		return $text;
 	}
+	
+	function generalLink( $arr ) {
+		$url = $arr[2] . $arr[3];
+		// Re-add the surrounding space/punctuation
+		return $arr[1] . $this->makeExternalLink( $url, $url ). $arr[4];
+	}
 
-	function messageBugLink( $arr ){
+	function messageBugLink( $arr ) {
 		$text = $arr[0];
 		$bugNo = intval( $arr[1] );
 		$url = $this->mRepo->getBugPath( $bugNo );
