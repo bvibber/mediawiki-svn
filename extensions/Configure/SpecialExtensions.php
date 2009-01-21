2677c7b7192699fbce52439293d947cfb03e20a5
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-21 20:56:08 +0000

    removed now useless check
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@45975 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Configure/SpecialExtensions.php
+++ extensions/Configure/SpecialExtensions.php
@@ -96,7 +96,7 @@ class SpecialExtensions extends ConfigurationPage {
 			if( !$ext->isInstalled() ) continue; // must exist
 			$settings = $ext->getSettings();
 			foreach ( $settings as $setting => $type ) {
-				if ( !isset( $GLOBALS[$setting] ) && !isset( $this->conf[$setting] ) && file_exists( $ext->getFile() ) ) {
+				if ( !isset( $this->conf[$setting] ) && file_exists( $ext->getFile() ) ) {
 					if ( !$globalDone ) {
 						extract( $GLOBALS, EXTR_REFS );
 						global $wgHooks;
