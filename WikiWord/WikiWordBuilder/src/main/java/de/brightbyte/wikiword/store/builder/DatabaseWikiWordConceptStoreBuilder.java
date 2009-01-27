f9a9befbae9671f7b998ca409d5f9cfce28f0641
daniel <daniel@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-27 16:09:27 +0000

    smaller chunks when self-joining link-table. does this help?
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@46350 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiWord/WikiWordBuilder/src/main/java/de/brightbyte/wikiword/store/builder/DatabaseWikiWordConceptStoreBuilder.java
+++ WikiWord/WikiWordBuilder/src/main/java/de/brightbyte/wikiword/store/builder/DatabaseWikiWordConceptStoreBuilder.java
@@ -410,7 +410,7 @@ public abstract class DatabaseWikiWordConceptStoreBuilder<T extends WikiWordConc
 		" join "+linkTable.getSQLName()+" as B on A.anchor = B.target AND B.anchor = A.target ";
 		String suffix = " on duplicate key update bilink = bilink + values(bilink)"; 
 
-		return executeChunkedUpdate("finishGlobalConcepts", "similarities:bilink", sql, suffix, linkTable, "A.anchor");
+		return executeChunkedUpdate("finishGlobalConcepts", "similarities:bilink", sql, suffix, linkTable, "A.anchor", 5);
 	}
 
 	//----------------------------------------------------------------------------------
