fcc48768bc17353c03dfc30f5e5030cbfc976f43
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-01-30 00:42:39 +0000

    removed debug line
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@46572 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MetavidWiki/maintenance/metavid2mvWiki.inc.php
+++ extensions/MetavidWiki/maintenance/metavid2mvWiki.inc.php
@@ -723,8 +723,8 @@ function do_people_insert( $doInterestLookup = false, $forcePerson = '', $force 
 						}
 						$max = 0;
 						$mapk = null;
-						print_r($matches);
-						die;
+						//print_r($matches);
+						//die;
 						foreach ( $point as $k => $v ) {
 							if ( $v > $max ) {
 								$mapk = $matches[1][$k];
