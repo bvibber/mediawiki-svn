37921dc5b6308f3a03d2a3f1a6d0dc02775de467
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-02-09 23:07:24 +0000

    Testing things out.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47064 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/WikipediaStatistics/LocalSettings.php
@@ -0,0 +1,8 @@
+<?php
+
+$localSettings = array(
+	'host' => null,
+	'dbname' => null,
+	'username' => null,
+	'password' => null,
+);
--- tools/WikipediaStatistics/index.php
+++ tools/WikipediaStatistics/index.php
@@ -5,9 +5,19 @@
 <body>
 	<h1>Wikipedia Statistics</h1>
 	<?php
-	/*
-	 * Wikipedia Statistics
-	 */
+
+	require_once 'LocalSettings.php';
+
+	$dbh = new PDO(
+		'mysql:host=' . $localSettings['host'] . ';' .
+		'dbname=' . $localSettings['dbname'],
+		$localSettings['username'],
+		$localSettings['password']
+	);
+	foreach( $dbh->query( 'SELECT * FROM user_groups' ) as $row ){
+		echo implode( ' / ', $row ) . '<br />';
+	}
+	$dbh = null;
 
 	?>
 </body>
