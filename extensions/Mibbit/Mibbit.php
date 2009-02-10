ff9aa91eaea4118ce23412181d45fa775442edc6
minuteelectron <minuteelectron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-02-10 19:01:16 +0000

    * Mibbit: Code cleanup, remove excess whitespace and an obsolete configuration parameters.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47106 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Mibbit/Mibbit.php
+++ extensions/Mibbit/Mibbit.php
@@ -10,27 +10,27 @@
  * @link http://www.mediawiki.org/wiki/Extension:Mibbit
  *
  * @author MinuteElectron <minuteelectron@googlemail.com>
- * @copyright Copyright © 2008 MinuteElectron.
+ * @copyright Copyright © 2008 - 2009 Robert Leverington.
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
 // If this is run directly from the web die as this is not a valid entry point.
-if( !defined( 'MEDIAWIKI' ) ) die( 'Invalid entry point.' );
+if( !defined( 'MEDIAWIKI' ) ) die 'Invalid entry point.';
 
 // Extension credits.
 $wgExtensionCredits[ 'specialpage' ][] = array(
 	'name'           => 'Mibbit',
 	'description'    => 'Adds a special page used to chat in real time with other wiki users.',
 	'descriptionmsg' => 'mibbit-desc',
-	'author'         => 'MinuteElectron',
+	'author'         => 'Robert Leverington',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:Mibbit',
-	'version'        => '1.2',
+	'version'        => '1.3.0',
 );
 
 // Register special page.
-$dir = dirname(__FILE__) . '/';
-$wgSpecialPages[ 'Mibbit' ]    = 'Mibbit';
-$wgSpecialPageGroups['Mibbit'] = 'wiki';
+$dir = dirname( __FILE__ ) . '/';
+$wgSpecialPages[ 'Mibbit' ] = 'Mibbit';
+$wgSpecialPageGroups[ 'Mibbit' ] = 'wiki';
 $wgAutoloadClasses[ 'Mibbit' ] = $dir . 'Mibbit_body.php';
 
 // Extension messages.
@@ -40,8 +40,9 @@ $wgExtensionMessagesFiles[ 'Mibbit' ] =  $dir . 'Mibbit.i18n.php';
 $wgMibbitServer          = '';
 $wgMibbitChannel         = '';
 $wgMibbitExtraParameters = array( 'noServerMotd' => 'true' );
-$wgMibbitAllowAnonymous  = true;
+
+// Default permissions.
+$wgAvailableRights[] = 'mibbit';
 $wgGroupPermissions[ '*'     ][ 'mibbit' ] = false;
 $wgGroupPermissions[ 'user'  ][ 'mibbit' ] = true;
 $wgGroupPermissions[ 'sysop' ][ 'mibbit' ] = true;
-$wgAvailableRights[] = 'mibbit';
