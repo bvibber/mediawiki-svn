f2d413c59424bca5962bcdf05cd6cd7af1176261
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-02-18 08:21:26 +0000

    * Support use of release-notes tag * Tweak heuristics
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47432 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/CodeReleaseNotes.php
+++ extensions/CodeReview/CodeReleaseNotes.php
@@ -67,7 +67,8 @@ class CodeReleaseNotes extends CodeView {
 			$where .= ' OR cr_path = '.$dbr->addQuotes($this->mPath).')';
 		}
 		# Select commits within this range...
-		$res = $dbr->select( 'code_rev', array('cr_message','cr_author','cr_id'),
+		$res = $dbr->select( array('code_rev','code_tags'),
+			array('cr_message','cr_author','cr_id','ct_tag AS rnotes'),
 			array( 
 				'cr_repo_id' => $this->mRepo->getId(), // this repo
 				"cr_status NOT IN('reverted','deferred','fixme')", // not reverted/deferred/fixme
@@ -75,14 +76,17 @@ class CodeReleaseNotes extends CodeView {
 				$where // in range
 			),
 			__METHOD__,
-			array( 'ORDER BY' => 'cr_id DESC' )
+			array( 'ORDER BY' => 'cr_id DESC' ),
+			array( 'code_tags' => array('LEFT JOIN', # Tagged for release notes?
+				'ct_repo_id = cr_repo_id AND ct_rev_id = cr_id AND ct_tag = "release-notes"')
+			)
 		);
 		$wgOut->addHTML( '<ul>' );
 		# Output any relevant seeming commits...
 		while( $row = $dbr->fetchObject( $res ) ) {
 			$summary = htmlspecialchars($row->cr_message);
-			# Add this commit summary if needed
-			if( $this->isRelevant( $summary ) ) {
+			# Add this commit summary if needed.
+			if( $row->rnotes || $this->isRelevant($summary) ) {
 				# Keep it short if possible...
 				$summary = $this->shortenSummary( $summary );
 				# Anything left? (this can happen with some heuristics)
@@ -100,8 +104,8 @@ class CodeReleaseNotes extends CodeView {
 	}
 	
 	private function shortenSummary( $summary, $first = true ) {
-		# Asterixs often used as point-by-point bullets
-		if( strpos($summary,'*') !== false ) {
+		# Astericks often used for point-by-point bullets
+		if( preg_match('/(^|\n) ?\*/', $summary) ) {
 			$blurbs = explode('*',$summary);
 		# Double newlines separate importance generally
 		} else if( strpos($summary,"\n\n") !== false ) {
@@ -132,7 +136,7 @@ class CodeReleaseNotes extends CodeView {
 	}
 	
 	// Quick relevance tests (these *should* be over-inclusive a little if anything)
-	private function isRelevant( $summary ) {
+	private function isRelevant( $summary, $whole = true ) {
 		# Fixed a bug? Mentioned a config var?
 		if( preg_match( '/\b(bug #?(\d+)|\$[we]g[0-9a-z]{3,50})\b/i', $summary ) )
 			return true;
@@ -141,13 +145,16 @@ class CodeReleaseNotes extends CodeView {
 		if( mb_strlen($summary) < 40 || $words <= 5 )
 			return false;
 		# All caps words (like "BREAKING CHANGE"/magic words)? 
-		# Literals like "'autoconfirmed'"/'"user contributions"'?
-		if( preg_match( '/\b([A-Z]{8,30}|[\'"]\w+[\'"])\b/', $summary ) )
+		if( preg_match( '/\b[A-Z]{7,30}\b/', $summary ) )
 			return true;
 		# Random keywords
 		if( preg_match( '/\b(wiki|HTML\d|CSS\d|UTF-?8|(Apache|PHP|CGI|Java|Perl|Python|\w+SQL) ?\d?\.?\d?)\b/i', $summary ) )
 			return true;
-		# Longish summary :)
-		return ( $words > 35 );
+		# Are we looking at the whole summary or an aspect of it?
+		if( $whole ) {
+			return preg_match('/(^|\n) ?\*/',$summary); # List of items?
+		} else {
+			return true;
+		}
 	}
 }
