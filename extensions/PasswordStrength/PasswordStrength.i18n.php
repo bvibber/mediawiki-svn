c2e306ec44023310e09edd6ada44adc4e15e74ca
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-02-20 17:52:47 +0000

    * Add extension PasswordStrength to Translatewiki * Rebuild i18n file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47566 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PasswordStrength/PasswordStrength.i18n.php
+++ extensions/PasswordStrength/PasswordStrength.i18n.php
@@ -6,9 +6,10 @@ $messages = array();
 /**
  * English
  */
-$messages['en'] = array( 'passwordstr-desc'           => 'Perform additional security checks on passwords with regular expressions',
-						 'passwordstr-regex-hit'      => 'Your password did not match the complexity requirements.',
-						 'passwordstr-needmore-ints'  => 'You need at least {{PLURAL:$1|one number|$1 numbers}} (0-9)',
-						 'passwordstr-needmore-upper' => 'You need at least $1 upper cased {{PLURAL:$1|letter|letters}} (A-Z)',
-						 'passwordstr-needmore-lower' => 'You need at least $1 lower cased {{PLURAL:$1|letter|letters}} (a-z)',
-					    );
+$messages['en'] = array(
+	'passwordstr-desc' => 'Perform additional security checks on passwords with regular expressions',
+	'passwordstr-regex-hit' => 'Your password did not match the complexity requirements.',
+	'passwordstr-needmore-ints' => 'You need at least {{PLURAL:$1|one number|$1 numbers}} (0-9)',
+	'passwordstr-needmore-upper' => 'You need at least $1 upper cased {{PLURAL:$1|letter|letters}} (A-Z)',
+	'passwordstr-needmore-lower' => 'You need at least $1 lower cased {{PLURAL:$1|letter|letters}} (a-z)',
+);
