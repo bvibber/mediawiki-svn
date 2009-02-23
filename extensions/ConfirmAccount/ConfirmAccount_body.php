bvibber/mediawiki-svn

commit 09bd0b32a0587397e3d977c343577437310c4d52

Replace hard coded pipe separators with locale based
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47054 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/ConfirmAccount/ConfirmAccount_body.php
@@ -15,9 +15,9 @@ class ConfirmAccountsPage extends SpecialPage
         SpecialPage::SpecialPage('ConfirmAccounts','confirmaccount');
     }
 
-    function execute( $par ) {
-        global $wgRequest, $wgOut, $wgUser, $wgAccountRequestTypes;
-        
+	function execute( $par ) {
+		global $wgRequest, $wgOut, $wgUser, $wgAccountRequestTypes, $wgLang;
+
 		if( !$wgUser->isAllowed( 'confirmaccount' ) ) {
 			$wgOut->permissionRequired( 'confirmaccount' );
 			return;
@@ -91,22 +91,40 @@ class ConfirmAccountsPage extends SpecialPage
 			$listLink = wfMsgHtml( 'confirmaccount-showopen' );
 		}
 		if( $this->acrID || !$this->showHeld ) {
-			$listLink .= ' | '.$this->skin->makeKnownLinkObj( $titleObj, 
-				wfMsgHtml( 'confirmaccount-showheld' ), wfArrayToCGI( array( 'wpShowHeld' => 1 ) ) );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				$this->skin->makeKnownLinkObj( $titleObj, 
+					wfMsgHtml( 'confirmaccount-showheld' ), wfArrayToCGI( array( 'wpShowHeld' => 1 ) ) )
+			) );
 		} else {
-			$listLink .= ' | '.wfMsgHtml( 'confirmaccount-showheld' );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				wfMsgHtml( 'confirmaccount-showheld' )
+			) );
 		}
 		if( $this->acrID || !$this->showRejects ) {
-			$listLink .= ' | '.$this->skin->makeKnownLinkObj( $titleObj, wfMsgHtml( 'confirmaccount-showrej' ),
-				wfArrayToCGI( array( 'wpShowRejects' => 1 ) ) );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				$this->skin->makeKnownLinkObj( $titleObj, wfMsgHtml( 'confirmaccount-showrej' ),
+					wfArrayToCGI( array( 'wpShowRejects' => 1 ) ) )
+			) );
 		} else {
-			$listLink .= ' | '.wfMsgHtml( 'confirmaccount-showrej' );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				wfMsgHtml( 'confirmaccount-showrej' )
+			) );
 		}
 		if( $this->acrID || !$this->showStale ) {
-			$listLink .= ' | '.$this->skin->makeKnownLinkObj( $titleObj, wfMsgHtml( 'confirmaccount-showexp' ),
-				wfArrayToCGI( array( 'wpShowStale' => 1 ) ) );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				$this->skin->makeKnownLinkObj( $titleObj, wfMsgHtml( 'confirmaccount-showexp' ),
+					wfArrayToCGI( array( 'wpShowStale' => 1 ) ) )
+			) );
 		} else {
-			$listLink .= ' | '.wfMsgHtml( 'confirmaccount-showexp' );
+			$listLink = $wgLang->pipeList( array(
+				$listLink,
+				wfMsgHtml( 'confirmaccount-showexp' )
+			) );
 		}
 		
 		# Say what queue we are in...
@@ -133,7 +151,7 @@ class ConfirmAccountsPage extends SpecialPage
 	}
 	
 	function showQueues() {
-		global $wgOut, $wgAccountRequestTypes;
+		global $wgOut, $wgAccountRequestTypes, $wgLang;
 		
 		$wgOut->addWikiText( wfMsg('confirmaccount-maintext') );
 		
@@ -169,7 +187,9 @@ class ConfirmAccountsPage extends SpecialPage
 				__METHOD__ );
 			$rejects .= " [$count]";
 				
-			$wgOut->addHTML( "<li><i>".wfMsgHtml("confirmaccount-type-$i")."</i> ($open | $held | $rejects | $stale)</li>" );
+			$wgOut->addHTML( "<li><i>".wfMsgHtml("confirmaccount-type-$i")."</i> ($open" .
+				$wgLang->pipeList( array( $held, $rejects, $stale ) ) .
+				")</li>" );
 		}
 		$wgOut->addHTML( '</ul>' );
 	}

extensions/CategoryTree/CategoryTreeFunctions.php
@@ -550,7 +550,7 @@ class CategoryTree {
 
 			$wikiLink = $special->getLocalURL( 'target=' . $t->getPartialURL() . '&' . $this->getOptionsAsUrlParameters() );
 
-			if ( $s !== '' ) $s .= ' | ';
+			if ( $s !== '' ) $s .= wfMsgExt( 'pipe-separator' , 'escapenoentities' );
 
 			$s .= Xml::openElement( 'span', array( 'class' => 'CategoryTreeItem' ) );
 			$s .= Xml::openElement( 'a', array( 'class' => 'CategoryTreeLabel', 'href' => $wikiLink ) ) . $label . Xml::closeElement( 'a' );

extensions/CleanChanges/CleanChanges_body.php
@@ -473,7 +473,8 @@ class NCL extends EnhancedChangesList {
 		}
 
 		if( $items ) {
-			$data = array( "wgUserInfo$rci" => '(' . implode( ' | ', $items ) . ')' );
+			global $wgLang;
+			$data = array( "wgUserInfo$rci" => '(' . $wgLang->pipeList( $items ) . ')' );
 
 			return array($tl, $data);
 		} else {

extensions/CodeBrowse/CodeBrowseRepoListView.php
@@ -13,20 +13,20 @@ class CodeBrowseRepoListView extends CodeBrowseView {
 		
 		$text = '';
 		foreach( $repos as $repo ){
+			global $wgLang;
+
 			$name = $repo->getName();
 			$text .= "* '''[[Special:CodeBrowse/$name|$name]]''' (";
-			$text .= "[[Special:Code/$name|".wfMsgHtml( 'code-log' )."]]";
-			$text .= " | [[Special:Code/$name/comments|".wfMsgHtml( 'code-notes' )."]]";
-			$text .= " | [[Special:Code/$name/tag|".wfMsgHtml( 'code-tags' )."]]";
-			$text .= " | [[Special:Code/$name/author|".wfMsgHtml( 'code-authors' )."]]";
+			$links[] = "[[Special:Code/$name|" . wfMsgHtml( 'code-log' ) . "]]";
+			$links[] = "[[Special:Code/$name/comments|" . wfMsgHtml( 'code-notes' ) . "]]";
+			$links[] = "[[Special:Code/$name/tag|" . wfMsgHtml( 'code-tags' ) . "]]";
+			$links[] = "[[Special:Code/$name/author|" . wfMsgHtml( 'code-authors' ) . "]]";
+			$text .= $wgLang->pipeList( $links );
 			$text .= ")\n";
 		}
 		
 		return $text;
-		
 	}
-	
-	
 }
 
 class CodeRepoListView {

extensions/CodeReview/CodeRepoListView.php
@@ -19,13 +19,15 @@ class CodeRepoListView {
 	}
 	
 	public static function getNavItem( $name ) {
+		global $wgLang;
+
 		$text = "'''[[Special:Code/$name|$name]]''' (";
-		$text .= "[[Special:Code/$name/comments|".wfMsgHtml( 'code-notes' )."]]";
-		$text .= " | [[Special:Code/$name/status|".wfMsgHtml( 'code-status' )."]]";
-		$text .= " | [[Special:Code/$name/tag|".wfMsgHtml( 'code-tags' )."]]";
-		$text .= " | [[Special:Code/$name/author|".wfMsgHtml( 'code-authors' )."]]";
+		$links[] = "[[Special:Code/$name/comments|" . wfMsgHtml( 'code-notes' ) . "]]";
+		$links[] = "[[Special:Code/$name/status|" . wfMsgHtml( 'code-status' ) . "]]";
+		$links[] = "[[Special:Code/$name/tag|" . wfMsgHtml( 'code-tags' ) . "]]";
+		$links[] = "[[Special:Code/$name/author|" . wfMsgHtml( 'code-authors' ) . "]]";
+		$text .= $wgLang->pipeList( $links );
 		$text .= ")";
 		return $text;
 	}
 }
-

extensions/CodeReview/CodeRevisionView.php
@@ -113,6 +113,8 @@ class CodeRevisionView extends CodeView {
 	}
 	
 	protected function navigationLinks() {
+		global $wgLang;
+
 		$rev = $this->mRev->getId();
 		$prev = $this->mRev->getPrevious();
 		$next = $this->mRev->getNext();
@@ -139,7 +141,7 @@ class CodeRevisionView extends CodeView {
 			$links[] = $this->mSkin->link( $nextTarget, "r$next" ) . '&nbsp;&gt;';
 		}
 
-		return implode( ' | ', $links );
+		return $wgLang->pipeList( $links );
 	}
 
 	protected function checkPostings() {

extensions/ContributionReporting/ContributionHistory_body.php
@@ -5,7 +5,7 @@ class ContributionHistory extends SpecialPage {
 	}
 
 	function execute( $language = NULL ) {
-		global $wgRequest, $wgOut, $wgTitle;
+		global $wgRequest, $wgOut, $wgTitle, $wgLang;
 
 		if ( !preg_match( '/^[a-z-]+$/', $language ) ) {
 			$language = 'en';
@@ -75,7 +75,7 @@ class ContributionHistory extends SpecialPage {
 		);
 		$pagingDiv = Xml::openElement( 'div',
 				array( 'align' => 'right', 'style' => 'padding-bottom:20px' ) ) .
-			implode( " | ", $pagingLinks ) .
+			$wgLang->pipeList( $pagingLinks ) .
 			Xml::closeElement( 'div' );
 		$output .= $pagingDiv;
 		

extensions/DynamicPageList/DynamicPageList2.php
@@ -3879,7 +3879,7 @@ class DPL2 {
 				if($article->mUserLink != '')	$rBody .= ' . . [[User:' . $article->mUser .'|'.$article->mUser.']]';
 				if($article->mContributor != '')$rBody .= ' . . [[User:' . $article->mContributor .'|'.$article->mContributor." $article->mContrib]]";
 							
-				if( !empty($article->mCategoryLinks) )	$rBody .= ' . . <SMALL>' . wfMsg('categories') . ': ' . implode(' | ', $article->mCategoryLinks) . '</SMALL>';
+				if( !empty($article->mCategoryLinks) )	$rBody .= ' . . <small>' . wfMsg('categories') . ': ' . $wgLang->pipeList( $article->mCategoryLinks ) . '</small>';
 			}
 			
 			// add included contents
@@ -4115,7 +4115,4 @@ class DPL2Logger {
 		sort($paramoptions);
 		return $this->escapeMsg( $msgid, $paramvar, htmlspecialchars( $val ), ExtDynamicPageList2::$options[$paramvar]['default'], implode(' | ', $paramoptions ));
 	}
-
 }
-
-?>

commit 469405989d6fbddac78a864c67b5e54d5506b378

Fix link pipe breakage from r47054
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@47732 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/ConfirmAccount/ConfirmAccount_body.php
@@ -187,9 +187,8 @@ class ConfirmAccountsPage extends SpecialPage
 				__METHOD__ );
 			$rejects .= " [$count]";
 				
-			$wgOut->addHTML( "<li><i>".wfMsgHtml("confirmaccount-type-$i")."</i> ($open" .
-				$wgLang->pipeList( array( $held, $rejects, $stale ) ) .
-				")</li>" );
+			$wgOut->addHTML( "<li><i>".wfMsgHtml("confirmaccount-type-$i")."</i> (" .
+				$wgLang->pipeList( array( $open, $held, $rejects, $stale ) ) . ")</li>" );
 		}
 		$wgOut->addHTML( '</ul>' );
 	}