457b999d006944804aa4ff25e97e09d3760fc37e
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-03-10 12:10:46 +0000

    Pipe Escape extension version 0.1.0, of author David M. Sledge, added verbatim from source at http://www.mediawiki.org/wiki/Extension:Pipe_Escape
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@48262 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PipeEscape/PipeEscape.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @package MediaWiki
+ * @subpackage Extensions
+ *
+ * @link http://www.mediawiki.org/wiki/Extension:Pipe_Escape Documentation
+ *
+ * @author David M. Sledge
+ * @copyright Copyright © 2008 David M. Sledge
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0
+ *     or later
+ * @version 0.1.0
+ *     initial creation.
+ *
+ * @todo
+ */
+
+if ( !defined( "MEDIAWIKI" ) )
+{
+    die( "This file is a MediaWiki extension, it is not a valid entry point" );
+}
+
+$wgHooks[ 'ParserFirstCallInit' ][] = "ExtPipeEsc::setup";
+$wgExtensionCredits[ 'parserhook' ][] = array(
+    "author"      => "David M. Sledge",
+    "name"        => "Pipe Escape",
+    "version"     => ExtPipeEsc::VERSION,
+    "description" => "Parser function for when you want a pipe character " .
+        "to be just a pipe character",
+    "url"         => "http://www.mediawiki.org/wiki/Extension:Pipe_Escape",
+);
+
+$wgHooks[ 'LanguageGetMagic' ][]  = 'ExtPipeEsc::languageGetMagic';
+
+class ExtPipeEsc
+{
+    const VERSION = "0.1.0";
+    private static $parserFunctions = array(
+        '!' => 'pipeChar',
+    );
+
+    public static function setup( &$parser )
+    {
+        // register each hook
+        foreach( self::$parserFunctions as $hook => $function )
+            $parser->setFunctionHook( $hook,
+                array( __CLASS__, $function ), SFH_OBJECT_ARGS );
+
+        return true;
+    }
+
+    public static function languageGetMagic( &$magicWords, $langCode )
+    {
+        $magicWords[ '!' ] = array( 0, '!' );
+
+        return true;
+    }
+
+    public static function pipeChar( &$parser, $frame, $args )
+    {
+        $output = array_shift( $args );
+
+        // no parameters means we're done.  spit out an empty string
+        if ( !isset( $output ) )
+            return '';
+
+        // expand the first argument
+        $output = $frame->expand( $output );
+
+        // get the rest of the arguments, expand each one, prefix each expansion
+        // with a pipe character, and append it to the output string.
+        for ( $arg = array_shift( $args ); isset( $arg );
+              $arg = array_shift( $args ) )
+        {
+            $output .= '|' . $frame->expand( $arg );
+        }
+
+        //return '<pre><nowiki>'. trim( $output ) . '</nowiki></pre>';
+        return trim( $output );
+    }
+}
+
