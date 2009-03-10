7d89056548d95ea363b824e97cab8856e98a736b
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-03-10 12:12:30 +0000

    Version 0.1.1 - some i18n support added.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@48263 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PipeEscape/PipeEscape.i18n.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Internationalisation file for extension PipeEscape.
+ *
+ * @addtogroup Extensions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * @author David M. Sledge
+ * @author Purodha Blissenbach, http://ksh.wikipedia.org/wiki/User:Purodha
+ * @copyright Copyright � 2008 David M. Sledge
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$messages = array();
+
+/** English (English)
+ * @author David M. Sledge
+ * @author Purodha
+ */
+$messages['en'] = array(
+	# general messages
+	'pipeescape-desc'	=> 'Parser function <code>#!</code> for when you want a pipe character to be just a pipe character',
+);
+
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'pipeescape-desc'	=> 'Short description of the [[:mw:Extension:Pipe_Escape|Pipe_Escape]] extension.
+Shown in [[Special:Version]].
+Do not translate link targets, or function names.',
+);
+
+/** German (Deutsch)
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'pipeescape-desc' => '!!FUZZY!!Parserfunktion <code>#!</code> ',
+);
+
+/** German (formal address) (Deutsch (Sie-Form))
+ */
+$messages['de-formal'] = array(
+);
+
+/** Haitian (Kreyòl ayisyen)
+ */
+$messages['ht'] = array(
+);
+
+/** Hungarian (Magyar)
+ */
+$messages['hu'] = array(
+);
+
+/** Interlingua (Interlingua)
+ */
+$messages['ia'] = array(
+);
+
+/** Ido (Ido)
+ */
+$messages['io'] = array(
+);
+
+/** Icelandic (Íslenska)
+ */
+$messages['is'] = array(
+);
+
+/** Italian (Italiano)
+ */
+$messages['it'] = array(
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'pipeescape-desc' => '!!FUZZY!!Brengk de Paaserfunxjuhn <code>#!</code> en et Wiki, die der sangkraaschte Shtresh unmollesteet dorsch loht.',
+);
+
+/** Latin (Latina)
+ * @author purodha
+ */
+$messages['la'] = array(
+	'pipeescape-desc' => '!!FUZZY!!Functio parseris nominae <code>#!</code> adita in vici, protegens litera | qui haec separator parametorum non interpretatur.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ */
+$messages['lb'] = array(
+);
+
+/** Low German (Plattdüütsch)
+ */
+$messages['nds'] = array(
+);
+
+/** Nedersaksisch (Nedersaksisch)
+ */
+$messages['nds-nl'] = array(
+);
+
