f582617b6451f9f7315f14eaf7675bc340620036
river <river@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-03-19 19:53:21 +0000

    moved to svn.toolserver.org
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@48610 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/tsutils/config.mk
+++
@@ -1,7 +0,0 @@
-#LINT		= lint
-#CC		= suncc
-#CFLAGS		= -m64 -O -g -errwarn -xc99=all
-#LINTFLAGS	= -axsm -u -errtags=yes -s -Xc99=%none -Xarch=amd64 -errsecurity=core -erroff=E_INCONS_ARG_DECL2
-
-CC		= gcc
-CFLAGS		= -W -Wall -Werror
--- tools/tsutils/mk/rules.mk
+++
@@ -1,9 +0,0 @@
-include ../config.mk
-
-ifeq ($(LIBTSUTILS),YES)
-INCLUDES	+= -I../libtsutils
-LIBS		+= -L../libtsutils -ltsutils
-endif
-
-.c.o:
-	$(CC) $(CPPFLAGS) $(INCLUDES) $(CFLAGS) -c $<
