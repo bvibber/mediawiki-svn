9fe10c0b55a5611a3cff355287f219878f11af01
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-03-27 20:54:03 +0000

    committing my work on AjaxLogin extension. original code by Inez Korczyński for Wikia, some tweaks by me to make it work on vanilla MWs (tested with 1.14alpha) and message translations by various wikians. see README file for details, caveats etc.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@48935 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AjaxLogin/AjaxLogin.i18n.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Internationalization file for AjaxLogin extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Inez Korczyński <korczynski@gmail.com>
+ */
+$messages['en'] = array(
+	'ajaxLogin1' => 'To complete your log in, you must enter a new password.  This will take you away from this edit page and you may lose your current edit.',
+	'ajaxLogin2' => 'Are you sure? You may lose your edits if you leave this page now.',
+	'ajaxlogin-create' => 'Create an account',
+);
+
+/** German (Deutsch)
+ * @author Tim 'Avatar' Bartel
+ */
+$messages['de'] = array(
+	'ajaxLogin1' => 'Du musst ein neues Passwort eingeben um die Anmeldung durchzuführen. Dies geschieht auf einer andere Seite, so dass du deine aktuellen Änderungen auf dieser Seite möglicherweise verlieren wirst.',
+	'ajaxLogin2' => 'Bist du sicher? Möglicherweise gehen deine aktuellen Änderungen verloren, wenn du diese Seite verlässt.',
+);
+
+/** Spanish (Español)
+ * @author Juan David Ruiz
+ */
+$messages['es'] = array(
+	'ajaxLogin1' => 'Para completar tu registro, debes escribir una nueva contraseña. Esto hará que salgas de esta página de edición y podrías perder tu edición actual.',
+	'ajaxLogin2' => '¿Estas seguro? Podrías perder tus ediciones si abandonas esta página ahora.',
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['fi'] = array(
+	'ajaxLogin1' => 'Suorittaaksesi sisäänkirjautumisesi loppuun, sinun tulee antaa uusi salasana. Tämä vie sinut pois tältä muokkaussivulta ja saatat menettää nykyisen muokkauksesi.',
+	'ajaxLogin2' => 'Oletko varma? Saatat menettää muokkauksesi jos lähdet tältä sivulta nyt.',
+	'ajaxlogin-create' => 'Luo käyttäjätunnus',
+);
+
+/** French (Français)
+ * @author Polytherion
+ */
+$messages['fr'] = array(
+	'ajaxLogin1' => 'Pour compléter votre connexion, vous devez saisir un nouveau mot de passe. Cela vous fera quitter cette page et vous pourriez perdre votre modification.',
+	'ajaxLogin2' => 'Êtes-vous certain ? Vous pourriez perdre vos modifications si vous quittez cette page maintenant.',
+);
+
+/** Irish (Gaeilge)
+ * @author Panu Petteri Höglund
+ */
+$messages['ga'] = array(
+	'ajaxLogin2' => 'An bhfuil tú cinnte? Má imíonn tú ón leathanach seo anois, is féidir go gcaillfear na hathruithe a rinne tú.',
+);
+
+/** Hungarian (Magyar)
+ * @author Blogadmin
+ */
+$messages['hu'] = array(
+	'ajaxLogin1' => 'A bejelentkezés befejezéséhez meg kell adnod a jelszavadat. Ez elirányít a szerkesztőablakból, úgyhogy ezek a szerkesztéseid el fognak veszni. Ha ezt nem szeretnéd, másold ki őket a szövegmezőből!',
+	'ajaxLogin2' => 'Biztos vagy benne? Az oldal elhagyásával a szerkesztéseid elvesznek.',
+);
+
+/** Japanese (日本語)
+ * @author Tommy6
+ */
+$messages['ja'] = array(
+	'ajaxLogin1' => 'ログインを完了するには新しいパスワードを入力する必要があります。これにより、現在編集中のページから離れ、保存されていない内容は失われることになります。',
+	'ajaxLogin2' => '本当によろしいでしょうか？今このページを離れると保存されていない編集は失われることになります。',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Brunoy Anastasiya Seryozhenko
+ */
+$messages['pt-br'] = array(
+	'ajaxLogin1' => 'Para completar seu login, Você deve colocar uma nova password. Isto o impedirá de editar esta página e você perderá esta edição.',
+	'ajaxLogin2' => 'Tem certeza? Você perderá as suas edições se abandonar a página agora.',
+);
+
+/** Romanian (Română)
+ * @author Pixi
+ */
+$messages['ro'] = array(
+	'ajaxLogin1' => 'Pentru a te autentifica, trebuie să introduci o parolă nouă. Aceasta înseamnă că vei părăsi pagina de editare şi vei pierde editările efectuate până acum.',
+	'ajaxLogin2' => 'Eşti sigur? Vei pierde editările efectuate dacă părăseşti această pagină acum.',
+);
+
+/** Russian (Русский)
+ * @author Exlex
+ */
+$messages['ru'] = array(
+	'ajaxLogin1' => 'Чтобы закончить вход, необходимо ввести новый пароль. При этом Вы покинете страницу редактирования и это может привести к потере внесённых изменений.',
+	'ajaxLogin2' => 'Вы уверены? Внесённые изменения могут быть утрачены, если сейчас покинуть эту страницу.',
+);
+
+/** Swedish (Svenska)
+ * @author Grillmannen
+ */
+$messages['sv'] = array(
+	'ajaxLogin1' => 'För att komplettera din inloggning måste du skriva in ett nytt lösenord. Detta kommer att föra dig bort från den här redigeringssidan och du kan komma att förlora din nuvarande redigering.',
+	'ajaxLogin2' => 'Är du säker? Du kan förlora din redigering om du lämnar den här sidan nu.',
+);
+
+/** Chinese (中文)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh'] = array (
+	'ajaxLogin1' => '为完成登入动作，請輸入新的密碼。这动作可能会使你跳离编輯頁面，可能会損失編輯結果。',
+);
+
+/** Chinese (PRC)‬ (‪中文(中国大陆)‬)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-cn'] = array (
+	'ajaxLogin1' => '为完成登入动作，请输入新的密码。这动作可能会使你跳离编辑页面，可能会损失编辑结果。',
+	'ajaxLogin2' => '这动作可能会使你跳离编辑页面，可能会损失编辑结果。确定要离开吗？',
+);
+
+/** Simplified Chinese (‪中文(简化字)‬)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-hans'] = array (
+	'ajaxLogin2' => '这动作可能会使你跳离编辑页面，可能会损失编辑结果。确定要离开吗？',
+);
+
+/** Traditional Chinese (‪中文(傳統字)‬)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-hant'] = array (
+	'ajaxLogin2' => '這動作可能會使你跳離編輯頁面，可能會損失編輯結果。確定要離開嗎？',
+);
+
+/** Hong Kong Chinese‬ (‪中文(香港)‬)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-hk'] = array (
+	'ajaxLogin1' => '為完成登入動作，請輸入新的密碼。這動作可能會使你跳離編輯頁面，可能會損失編輯結果。',
+	'ajaxLogin2' => '這動作可能會使你跳離編輯頁面，可能會損失編輯結果。確定要離開嗎？',
+);
+
+/** Chinese (Singapore) (中文 (新加坡))
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-sg'] = array (
+	'ajaxLogin1' => '为完成登入动作，请输入新的密码。这动作可能会使你跳离编辑页面，可能会损失编辑结果。',
+);
+
+/** Taiwan Chinese (‪中文(台灣)‬)
+ * @author 許瑜真 (Yuchen Hsu/KaurJmeb)
+ */
+$messages['zh-tw'] = array (
+	'ajaxLogin1' => '為完成登入動作，請輸入新的密碼。這動作可能會使你跳離編輯頁面，可能會損失編輯結果。',
+	'ajaxLogin2' => '這動作可能會使你跳離編輯頁面，可能會損失編輯結果。確定要離開嗎？',
+);
