b917e191556963fd384fbcfab358502d133008f7
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-04-13 15:13:25 +0000

    * article -> page * remove period from description * tweak 'wikiarticlefeeds_combined_description'
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@49444 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiArticleFeeds/WikiArticleFeeds.i18n.php
+++ extensions/WikiArticleFeeds/WikiArticleFeeds.i18n.php
@@ -9,6 +9,6 @@
 $messages = array();
 
 $messages['en'] = array(
-	'wikiarticlefeeds-desc' => 'Produces feeds generated from MediaWiki articles.',
-	'wikiarticlefeeds_combined_description' => 'This is a combination feed, containing items from several feed sources.',
+	'wikiarticlefeeds-desc' => 'Produces feeds generated from MediaWiki pages',
+	'wikiarticlefeeds_combined_description' => 'This is a combined feed, containing items from several feed sources.',
 );
