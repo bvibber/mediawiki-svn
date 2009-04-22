99269e3f55ebe91f73ef5097a95f929249bbcaf4
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-04-22 03:18:34 +0000

    Reimplement convertUserOptions to run as a batch on lots of 50 users. Tested locally on a dump of elwikiversity and it took about 10-15 seconds for 666 users.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/preferences-work@49726 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/maintenance/convertUserOptions.php
+++ phase3/maintenance/convertUserOptions.php
@@ -2,13 +2,40 @@
 require( './commandLine.inc' );
 
 // Do each user sequentially, since accounts can't be deleted
-$i = 1;
 
-$u = User::newFromId( $i );
+print "Beginning batch conversion of user options.\n";
 
-while( $u->loadFromDatabase() ) {
-	$u->saveSettings();
+$id = 0;
+$dbw = wfGetDB( DB_MASTER );
+$conversionCount = 0;
+
+while ($id !== null) {
+	$idCond = 'user_id>'.$dbw->addQuotes( $id );
+	$optCond = "user_options!=".$dbw->addQuotes( '' ); // For compatibility
+	$res = $dbw->select( 'user', '*',
+			array( $optCond, $idCond ), __METHOD__,
+			array( 'LIMIT' => 50, 'FOR UPDATE' ) );
+	$id = convertOptionBatch( $res, $dbw );
+	$dbw->commit();
 	
-	++$i;
-	$u = User::newFromId( $i );
+	wfWaitForSlaves( 1 );
+	
+	if ($id)
+		print "--Converted to ID $id\n";
+}
+print "Conversion done. Converted $conversionCount user records.\n";
+
+function convertOptionBatch( $res, $dbw ) {
+	$id = null;
+	while ($row = $dbw->fetchObject( $res ) ) {
+		global $conversionCount;
+		$conversionCount++;
+		
+		$u = User::newFromRow( $row );
+		
+		$u->saveSettings();
+		$id = $row->user_id;
+	}
+	
+	return $id;
 }
