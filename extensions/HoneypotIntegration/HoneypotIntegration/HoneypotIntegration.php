2a1b1087de92be6ddcf7b45b8d02c5617e41718e
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-04-24 00:45:33 +0000

    Merge HoneypotIntegration from trunk to preferences-work to prevent conflicts
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/preferences-work@49788 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/HoneypotIntegration/HoneypotIntegration/HoneypotIntegration.php
@@ -0,0 +1,44 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) )
+	die( "This is a MediaWiki extension definition file; it is not a valid entry point." );
+
+/**#@+
+ * Provides integration with Project Honey Pot for MediaWiki sites.
+ * Requires
+ * @addtogroup Extensions
+ *
+ *
+ * @author Andrew Garrett <andrew@werdn.us>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$dir = dirname( __FILE__ );
+$wgExtensionCredits['other'][] = array(
+	'name'           => 'HoneypotIntegration',
+	'author'         => 'Andrew Garrett',
+	'description'    => 'Provides integration with Project Honey Pot for MediaWiki sites',
+	'descriptionmsg' => 'honeypot-desc',
+);
+
+$wgExtensionMessagesFiles['HoneypotIntegration'] =  "$dir/HoneypotIntegration.i18n.php";
+$wgAutoloadClasses[ 'HoneypotIntegration' ] = "$dir/HoneypotIntegration.class.php";
+
+#$wgHooks['AbuseFilter-filterAction'][] = 'HoneypotIntegration::onAbuseFilterFilterAction';
+#$wgHooks['AbuseFilter-builder'][] = 'HoneypotIntegration::onAbuseFilterBuilder';
+$wgHooks['EditPage::showEditForm:fields'][] = 'HoneypotIntegration::onShowEditForm';
+$wgHooks['GetUserPermissionsErrorsExpensive'][] =
+	'HoneypotIntegration::onGetUserPermissionsErrorsExpensive';
+
+$wgHoneypotURLs = array( 'http://www.google.com' );
+$wgHoneypotTemplates = array(
+	'<a href="honeypoturl"><!-- randomtext --></a>',
+);
+
+$wgHoneypotAutoLoad = false;
+
+$wgHoneypotDataFile = false;
+
+if ( !extension_loaded( 'fss' ) ) {
+	die( "FastStringSearch is required for Project Honeypot Integration" );
+}
