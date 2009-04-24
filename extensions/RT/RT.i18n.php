bvibber/mediawiki-svn

commit d04a081263ef21c8af2952642cc40db923107e6a

* minor updates to RT messages * add support for RT to Translate
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@49836 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/RT/RT.i18n.php
@@ -20,10 +20,15 @@ $messages['en'] = array(
 	'rt-desc'         => 'Fancy interface to RT (Request Tracker)',
 	'rt-inactive'     => 'The RT extension is not active',
 	'rt-badquery'     => 'The RT extension encountered an error when talking to the RT database',
-	'rt-badlimit'     => 'Invalid LIMIT (l) arg: must be a number. You tried: <b>$1</b>',
-	'rt-badorderby'   => 'Invalid ORDER BY (ob) arg: must be a standard field (see documentation). You tried: <b>$1</b>',
-	'rt-badstatus'    => 'Invalid status (s) arg: must be a standard field (see documentation). You tried: <b>$1</b>',
-	'rt-badqueue'     => 'Invalid queue (q) arg: must be a simple word. You tried: <b>$1</b>',
-	'rt-badowner'     => 'Invalid owner (o) arg: must be a valud username. You tried: <b>$1</b>',
+	'rt-badlimit'     => "Invalid LIMIT (l) arg: must be a number.
+You tried: '''\$1'''",
+	'rt-badorderby'   => "Invalid ORDER BY (ob) arg: must be a standard field (see documentation).
+You tried: '''\$1'''",
+	'rt-badstatus'    => "Invalid status (s) arg: must be a standard field (see documentation).
+You tried: '''\$1'''",
+	'rt-badqueue'     => "Invalid queue (q) arg: must be a simple word.
+You tried: '''\$1'''",
+	'rt-badowner'     => "Invalid owner (o) arg: must be a valud username.
+You tried: '''\$1'''',
 	'rt-nomatches'    => 'No matching RT tickets were found',
 );

commit 2bc3dc820e076681bf9cf1602224492d90612fa7

Fix syntax error introduced in r49836
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@49837 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/RT/RT.i18n.php
@@ -29,6 +29,6 @@ You tried: '''\$1'''",
 	'rt-badqueue'     => "Invalid queue (q) arg: must be a simple word.
 You tried: '''\$1'''",
 	'rt-badowner'     => "Invalid owner (o) arg: must be a valud username.
-You tried: '''\$1'''',
+You tried: '''\$1'''",
 	'rt-nomatches'    => 'No matching RT tickets were found',
 );