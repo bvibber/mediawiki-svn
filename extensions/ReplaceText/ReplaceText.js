fd1eff7ad5764c5dd858e07a51be8b19640da242
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-04-27 18:34:07 +0000

    Fixed so that inverting selections doesn't affect move-pages option checkboxes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@49968 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ReplaceText/ReplaceText.js
+++ extensions/ReplaceText/ReplaceText.js
@@ -2,7 +2,8 @@ function invertSelections() {
 	form = document.getElementById('choose_pages');
 	num_elements = form.elements.length;
 	for (i = 0; i < num_elements; i++) {
-		if (form.elements[i].type == "checkbox") {
+		cur_element = form.elements[i];
+		if (cur_element.type == "checkbox" && cur_element.id != 'create-redirect' && cur_element.id != 'watch-pages') {
 			if (form.elements[i].checked == true)
 				form.elements[i].checked = false;
 			else
