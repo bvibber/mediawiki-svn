aecaacc54cc401084209a8e599d5db26dfbec5f1
jan <jan@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-02 06:15:20 +0000

    Import files of the TSPoll extensions
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50116 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/TSPoll/TSPoll.i18n.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @ingroup Extensions
+ */
+
+$messages = array();
+$messages['en'] = array(
+    'descript_msg' => 'Include the [http://www.toolserver.org/~jan/poll/index.php Toolserver-Poll-Skript] as a HTML-tag',
+);
+$messages['de'] = array(
+    'descript_msg' => 'Bindet das [http://www.toolserver.org/~jan/poll/index.php Toolserver-Poll-Skript] als ein HTML-Tag ein',
+);
---
+++ extensions/TSPoll/TSPoll.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Toolserver Poll - Include the Toolserver-Poll-Skript(http://toolserver.org/~jan/poll/index.php) 
+ *
+ * To activate this extension, add the following into your LocalSettings.php file:
+ * require_once("$IP/extensions/TSPoll/TSPoll.php");
+ *
+ * @ingroup Extensions
+ * @author Jan Luca <jan@toolserver.org>
+ * @version 1.0 Dev
+ * @link http://www.mediawiki.org/wiki/User:Jan_Luca/Extension:TSPoll Documentation
+ * @license http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported or later
+ */
+ 
+/**
+ * Protect against register_globals vulnerabilities.
+ * This line must be present before any global variable is referenced.
+ */
+ 
+//Abbrechen des Skriptes, wenn es nicht in Mediawiki eingebunden ist
+if( !defined( 'MEDIAWIKI' ) ) {
+	echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+	die( -1 );
+}
+ 
+// Extension credits that will show up on Special:Version    
+$wgExtensionCredits['parserhook'][] = array(
+	'name'          => 'TSPoll',
+	'version'       => '1.0 Dev',
+	'author'        => 'Jan Luca', 
+	'url'           => 'http://www.mediawiki.org/wiki/User:Jan_Luca/Extension:TSPoll',
+	'descriptionmsg'=> 'descript_msg'
+);
+
+//Avoid unstubbing $wgParser on setHook() too early on modern (1.12+) MW versions, as per r35980
+if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
+	$wgHooks['ParserFirstCallInit'][] = 'efTSPollSetup';
+} else { // Otherwise do things the old fashioned way
+	$wgExtensionFunctions[] = 'efTSPollSetup';
+}
+
+$wgExtensionMessagesFiles['TSPoll'] = dirname( __FILE__ ) . '/TSPoll.i18n.php';
+include_once(dirname( __FILE__ ) . '/http.func.php');
+ 
+function efTSPollSetup() {
+	global $wgParser;
+	$wgParser->setHook( 'TSPoll', 'efTSPollRender' );
+  $wgParser->setHook( 'tspoll', 'efTSPollRender' );
+  return true;
+}
+
+function efTSPollRender( $input, $args, $parser ) {
+	foreach( $args as $name => $value ) {
+    if ($name == "id") {
+		    $id = htmlspecialchars( $value );
+        break;
+    }
+    else {
+        continue;
+    }
+  }
+  
+  $http = new http_w("toolserver.org","/~jan/poll/main.php?page=wiki_output&id=".$id.""); 
+  $get_server = $http->get("");
+  if($get_server != "") {
+	    return $get_server;
+  }
+  else {
+      return "Fehler beim Holen!";
+  }
+}
