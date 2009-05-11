50fb212e30fca338b21a5e685518c94f0da4aa19
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-11 19:04:11 +0000

    * Style fixes: whitespace, indentation, etc * Remove some un-needed variables and globals * TODO/FIXME: If $action == 'submit', in the submit() method, $action could never possibly be 'create' as well, so those checks can't happen. Also, don't use $_POST directly, that's what $wgRequest is for.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50495 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Poll/Poll_body.php
+++ extensions/Poll/Poll_body.php
@@ -1,12 +1,12 @@
 <?php
 
 class Poll extends SpecialPage {
-	function __construct() {
+	
+	public function __construct() {
 		parent::__construct( 'Poll' );
-		wfLoadExtensionMessages( 'Poll' );
 	}
 
-	function execute( $par ) {
+	public function execute( $par ) {
 		global $wgRequest, $wgOut;
 
 		$this->setHeaders();
@@ -22,91 +22,82 @@ class Poll extends SpecialPage {
 		if ( $action == "vote" ) {
 			$this->vote();
 		}
-    
-    if ( $action == "submit" ) {
-      $this->submit();
-    }
+
+    	if ( $action == "submit" ) {
+      		$this->submit();
+    	}
 	}
-  
-  public function create() {
-      global $wgRequest, $wgOut, $wgUser;
-      
-      $wgOut->setPagetitle( wfMsg( 'poll-title-create' ) );
-      
-      $controll_create_right = $wgUser->isAllowed( 'poll-create' );
-      $controll_create_blocked = $wgUser->isBlocked();
-      if ( $controll_create_right != true ) {
-          $wgOut->addWikiMsg( 'poll-create-right-error' );
-      }
-      elseif ( $controll_create_blocked == true ) {
-          $wgOut->addWikiMsg( 'poll-create-block-error' );
-      }
-      else {
-          $wgOut->addHtml( Xml::openElement( 'form', array('method'=> 'post', 'action' => 'index.php?title=Special:Poll&action=submit' ) ) );
-          $wgOut->addHtml( Xml::openElement( 'table' ) );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-question' ).':</td><td>'.Xml::input('question').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 1:</td><td>'.Xml::input('poll_alternative_1').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 2:</td><td>'.Xml::input('poll_alternative_2').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 3:</td><td>'.Xml::input('poll_alternative_3').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 4:</td><td>'.Xml::input('poll_alternative_4').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 5:</td><td>'.Xml::input('poll_alternative_5').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 6:</td><td>'.Xml::input('poll_alternative_6').'</td></tr>' );
-          $wgOut->addHtml( '<tr><td>'.Xml::submitButton(wfMsg( 'poll-submit' )).''.Xml::hidden('action', 'create').'</td></tr>' );
-          $wgOut->addHtml( Xml::closeElement( 'table' ) );
-          $wgOut->addHtml( Xml::closeElement( 'form' ) );
-      }
-  }
-  
-   public function vote() {
-      global $wgRequest, $wgOut, $wgUser;
-      
-      $wgOut->setPagetitle( wfMsg( 'poll-title-vote' ) );
-      
-      $controll_create_right = $wgUser->isAllowed( 'poll-vote' );
-      $controll_create_blocked = $wgUser->isBlocked();
-      if ( $controll_create_right != true ) {
-          $wgOut->addWikiMsg( 'poll-vote-right-error' );
-      }
-      elseif ( $controll_create_blocked == true ) {
-          $wgOut->addWikiMsg( 'poll-vote-block-error' );
-      }
-      else {
-          
-      }
-  }
-  
-  public function submit() {
-      global $wgRequest, $wgOut, $wgUser;
-	  
-	  $type = htmlenttitles($_POST['action']);
-	  
-	  if($type == 'create') {
-	    $controll_create_right = $wgUser->isAllowed( 'poll-create' );
-        $controll_create_blocked = $wgUser->isBlocked();
-        if ( $controll_create_right != true ) {
-            $wgOut->addWikiMsg( 'poll-create-right-error' );
-        }
-        elseif ( $controll_create_blocked == true ) {
-            $wgOut->addWikiMsg( 'poll-create-block-error' );
-        }
+	public function create() {
+		global $wgRequest, $wgOut, $wgUser;
+
+		$wgOut->setPagetitle( wfMsg( 'poll-title-create' ) );
+
+		if ( !$wgUser->isAllowed( 'poll-create' ) ) {
+			$wgOut->addWikiMsg( 'poll-create-right-error' );
+		}
+		elseif ( $wgUser->isBlocked() ) {
+			$wgOut->addWikiMsg( 'poll-create-block-error' );
+		}
 		else {
-		  $dbw = wfGetDB( DB_MASTER );
-		  $question = $_POST['question'];
-		  $alternative_1 = $_POST['poll_alternative_1'];
-	      $alternative_2 = $_POST['poll_alternative_2'];
-		  $alternative_3 = ($_POST['poll_alternative_3'] != "")? $_POST['poll_alternative_3'] : "";
-		  $alternative_4 = ($_POST['poll_alternative_4'] != "")? $_POST['poll_alternative_4'] : "";
-		  $alternative_5 = ($_POST['poll_alternative_5'] != "")? $_POST['poll_alternative_5'] : "";
-		  $alternative_6 = ($_POST['poll_alternative_6'] != "")? $_POST['poll_alternative_6'] : "";
-		  
-		  if($question != "" && $alternative_1 != "" && $alternative_2 != "") {
-            $dbw->insert( 'poll', array( 'question' => $question, 'alternative_1' => $alternative_1, 'alternative_2' => $alternative_2,
-			'alternative_3' => $alternative_3, 'alternative_4' => $alternative_4, 'alternative_5' => $alternative_5,
-			'alternative_6' => $alternative_6 ) );
-		  }
-		  else {
-		      $wgOut->addWikiMsg( 'poll-create-fields-error' );
-		  }
-	  }
-  }
+			$wgOut->addHtml( Xml::openElement( 'form', array('method'=> 'post', 'action' => 'index.php?title=Special:Poll&action=submit' ) ) );
+			$wgOut->addHtml( Xml::openElement( 'table' ) );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-question' ).':</td><td>'.Xml::input('question').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 1:</td><td>'.Xml::input('poll_alternative_1').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 2:</td><td>'.Xml::input('poll_alternative_2').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 3:</td><td>'.Xml::input('poll_alternative_3').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 4:</td><td>'.Xml::input('poll_alternative_4').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 5:</td><td>'.Xml::input('poll_alternative_5').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.wfMsg( 'poll-alternative' ).' 6:</td><td>'.Xml::input('poll_alternative_6').'</td></tr>' );
+			$wgOut->addHtml( '<tr><td>'.Xml::submitButton(wfMsg( 'poll-submit' )).''.Xml::hidden('action', 'create').'</td></tr>' );
+			$wgOut->addHtml( Xml::closeElement( 'table' ) );
+			$wgOut->addHtml( Xml::closeElement( 'form' ) );
+		}
+	}
+   
+	public function vote() {
+		global $wgOut, $wgUser;
+
+		$wgOut->setPagetitle( wfMsg( 'poll-title-vote' ) );
+
+		if ( !$wgUser->isAllowed( 'poll-vote' ); ) {
+			$wgOut->addWikiMsg( 'poll-vote-right-error' );
+		}
+		elseif ( $wgUser-isBlocked() ) {
+			$wgOut->addWikiMsg( 'poll-vote-block-error' );
+		}
+		else {
+		}
+	}
+
+	public function submit() {
+		global $wgRequest, $wgOut, $wgUser;
+
+		$type = htmlenttitles($_POST['action']);
+		
+		if ( $type == 'create' && !$wgUser->isAllowed( 'poll-create' ) ) {
+			$wgOut->addWikiMsg( 'poll-create-right-error' );
+		}
+		elseif ( $type == 'create' && $wgUser->isBlocked() ) {
+			$wgOut->addWikiMsg( 'poll-create-block-error' );
+		}
+		else {
+			$dbw = wfGetDB( DB_MASTER );
+			$question = $_POST['question'];
+			$alternative_1 = $_POST['poll_alternative_1'];
+		    $alternative_2 = $_POST['poll_alternative_2'];
+			$alternative_3 = ($_POST['poll_alternative_3'] != "")? $_POST['poll_alternative_3'] : "";
+			$alternative_4 = ($_POST['poll_alternative_4'] != "")? $_POST['poll_alternative_4'] : "";
+			$alternative_5 = ($_POST['poll_alternative_5'] != "")? $_POST['poll_alternative_5'] : "";
+			$alternative_6 = ($_POST['poll_alternative_6'] != "")? $_POST['poll_alternative_6'] : "";
+			
+			if($question != "" && $alternative_1 != "" && $alternative_2 != "") {
+            	$dbw->insert( 'poll', array( 'question' => $question, 'alternative_1' => $alternative_1, 'alternative_2' => $alternative_2,
+				'alternative_3' => $alternative_3, 'alternative_4' => $alternative_4, 'alternative_5' => $alternative_5,
+				'alternative_6' => $alternative_6 ) );
+			}
+			else {
+				$wgOut->addWikiMsg( 'poll-create-fields-error' );
+			}
+		}
+	}
 }
