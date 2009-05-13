2d149debee833d58720a65493b5767e4f7338597
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-13 14:42:26 +0000

    New extension: AdminLinks
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50543 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/AdminLinks/AdminLinks.i18n.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Internationalization file for Admin Links
+ *
+ * @file
+ * @ingroup Language
+ * @ingroup I18n
+ */
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	'adminlinks' => 'Admin links',
+	'adminlinks_documentation' => '$1 documentation',
+	'adminlinks_edit' => 'Edit $1',
+	'adminlinks_general' => 'General',
+	'adminlinks_sidebar' => 'sidebar',
+	'adminlinks_cssfile' => 'CSS file',
+	'adminlinks_mainpagename' => 'name of main page',
+	'adminlinks_users' => 'Users',
+	'adminlinks_createuser' => 'Create a user',
+	'adminlinks_browsesearch' => 'Browsing and searching',
+	'adminlinks_importexport' => 'Importing and exporting',
+);
+
---
+++ extensions/AdminLinks/AdminLinks.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * A special page holding special convenience links for sysops
+ *
+ * @author Yaron Koren
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+// credits
+$wgExtensionCredits['specialpage'][] = array(
+	'name' => 'Admin Links',
+	'version' => '0.1',
+	'author' => 'Yaron Koren',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:Admin_Links',
+	'description' => 'A special page that holds helpful links for administrators',
+	'descriptionmsg'  => 'adminlinks-desc',
+);
+
+$wgAdminLinksIP = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['AdminLinks'] = $wgAdminLinksIP . 'AdminLinks.i18n.php';
+$wgSpecialPages['AdminLinks'] = array('AdminLinks', 'adminlinks');
+$wgHooks['PersonalUrls'][] = 'AdminLinks::addURLToUserLinks';
+$wgAvailableRights[] = 'adminlinks';
+// by default, sysops see the link to this page
+$wgGroupPermissions['sysop']['adminlinks'] = true;
+$wgAutoloadClasses['AdminLinks']
+	= $wgAutoloadClasses['ALTree']
+	= $wgAutoloadClasses['ALSection']
+	= $wgAutoloadClasses['ALRow']
+	= $wgAutoloadClasses['ALItem']
+	= "$wgAdminLinksIP/AdminLinks_body.php";
