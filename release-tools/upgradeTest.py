55b4ccbe3fd078ee003f6ccdedc7c1759a1b38b0
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-14 08:34:18 +0000

    Mostly rewrote upgradeTest.py. * OOP, used subprocess not os.system * Added separate configuration files so that you don't have to edit the source all the time. * Generate schema dumps and produce a diff sequence between adjacent upgrades, thus highlighting schema changes that did not have upgrade patches.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50578 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- release-tools/upgradeTest.py
+++ release-tools/upgradeTest.py
@@ -1,124 +1,283 @@
-import sys, os
+import sys, os, stat, re
+from ConfigParser import SafeConfigParser
+from subprocess import *
+from xml.dom.minidom import *
+from xml.dom import *
+
+# Configure by creating local.conf in the release-tools directory and overriding 
+# the entries in default.conf
 
 class UpgradeError(Exception): pass
 
-def runCommand(command):
-	print command
-	sys.stdout.flush() # misordering of output observed without this
-	retval = os.system(command)
-	if retval:
-		raise UpgradeError("Nonzero return code (%d)! Aborting." % retval)
-	return retval
+def versionCompare( v1, v2 ):
+	parts1 = v1.split( '.' )
+	parts2 = v2.split( '.' )
+	numParts = max((len(parts1), len(parts2)))
+	for i in range(numParts):
+		if i >= len(parts1):
+			part1 = 0
+		else:
+			part1 = int(parts1[i])
+		if i >= len(parts2):
+			part2 = 0
+		else:
+			part2 = int(parts2[i])
+
+		if part1 < part2:
+			return -1
+		if part1 > part2:
+			return 1
+	return 0
+
+class UpgradeTest:
+	def __init__( self ):
+		self.conf = {}
+		self.versionConf = {}
+		self.baseDir = os.path.dirname( os.path.abspath( sys.argv[0] ) )
+		self.currentVersion = None
+
+	def run( self ):
+		# Configure
+		settings = ['php', 'svnroot', 'dbUser', 'dbPassword', 'runUpdate', 
+				'runRebuildMessages', 'runOldUpdate']
+		booleans = ['runUpdate', 'runRebuildMessages', 'runOldUpdate']
+		confParser = SafeConfigParser()
+		confParser.read( [ self.baseDir + '/default.conf', self.baseDir + '/local.conf' ] )
+		for key in settings:
+			if key in booleans:
+				value = confParser.getboolean( 'main', key )
+			else:
+				value = confParser.get( 'main', key )
+			self.conf[key] = value
+
+		for version in confParser.sections():
+			self.versionConf[version] = {}
+			for key in settings:
+				if confParser.has_option( version, key ):
+					if key in booleans:
+						value = confParser.getboolean( version, key )
+					else:
+						value = confParser.get( version, key )
+					self.versionConf[version][key] = value
 
 
-def shellEscape(param):
-	"""Escape a string parameter, or set of strings, for the shell."""
-	if isinstance(param, basestring):
-		return "'" + param.replace("'", "'\\''") + "'"
-	elif param is None:
-		# A blank string might actually be needed; None means we can leave it out
-		return ""
-	else:
-		return tuple([shellEscape(x) for x in param])
+		# Initialise file structure
+		self.dumpFile( 'mysql-client.conf', """
+[client]
+user = %s
+password = %s
+""" % (self.conf['dbUser'], self.conf['dbPassword']) )
 
-def readFile(filename):
-	file = open(filename, "r")
-	text = file.read()
-	file.close()
-	return text
+		os.chmod(self.baseDir + '/mysql-client.conf', stat.S_IRUSR | stat.S_IWUSR)
 
-def dumpFile(filename, text):
-	"""Dump a string to a file."""
-	print "Writing to %s" % filename
-	file = open(filename, "wt")
-	file.write(text)
-	file.close()
+		if not os.path.exists( self.baseDir + '/schemas' ):
+			os.mkdir( self.baseDir + '/schemas' )
+		for path in os.listdir( self.baseDir + '/schemas' ):
+			os.unlink( self.baseDir + '/schemas/' + path )
 
-def writeTemplate(template, output, data):
-	filled = readFile(template) % data
-	dumpFile(output, filled)
+		# Execute tests
+		if len(sys.argv) <= 1:
+			sys.stderr.write( "Usage: python upgradeTest.py <version>" )
+			sys.exit( 1 )
+
+		targetVersion = sys.argv[1];
+
+		versions = self.getAllVersions()
+		prevVersion = None
+		for version in versions:
+			self.currentVersion = version
+			if versionCompare( version, targetVersion ) >= 0:
+				break
+			
+			self.testUpgrade( version, targetVersion )
+			self.dumpSchema( version )
+			if prevVersion != None:
+				self.compareSchemas( prevVersion, version )
+			prevVersion = version
+
+	def getConf( self, key ):
+		v = self.currentVersion
+		if v != None and ( v in self.versionConf ) and ( key in self.versionConf[v] ):
+			return self.versionConf[v][key]
+		elif key in self.conf:
+			return self.conf[key]
+		else:
+			return None
+
+	def getAllVersions( self ):
+		print "Getting version list"
+		versions = []
+		proc = Popen( ['svn', 'ls', '--xml', self.conf['svnroot'] + '/branches'], stdout=PIPE )
+		xml = proc.communicate()[0]
+		if proc.returncode:
+			raise UpgradeError( 'svn ls returned exit status ' + proc.returncode )
+
+		doc = parseString(xml)
+		entries = doc.getElementsByTagName( 'entry' )
+		for entry in entries:
+			name = entry.firstChild
+			while name != None and ( name.nodeType != Node.ELEMENT_NODE or name.tagName != 'name' ):
+				name = name.nextSibling
+			if name == None or name.firstChild.nodeType != Node.TEXT_NODE:
+				raise UpgradeError( 'Invalid response from svn ls' )
+			nameText = name.firstChild.data
+			if re.match( '^REL[0-9]+_[0-9]+$', nameText ):
+				version = self.branchToVersion( 'branches/' + nameText )
+				if versionCompare( version, '1.2' ) >= 0:
+					versions.append( version )
+		versions.sort( versionCompare )
+		print "OK: " + ', '.join( versions )
+		return versions
+
+	def versionToDB( self, version ):
+		return 'uptest' + version.replace('.', '_')
+
+	def versionToBranch( self, version ):
+		return 'branches/REL' + version.replace( '.', '_' )
+
+	def branchToVersion( self, branch ):
+		return branch.replace( 'branches/REL', '').replace( '_', '.' )
+
+	def dumpSchema( self, version ):
+		self.runCommand(
+			[
+				'mysqldump', '--defaults-file=' + self.baseDir + '/mysql-client.conf', 
+				'--no-data', self.versionToDB( version )
+			], 
+			stdout = open( self.baseDir + '/schemas/' + version + '.sql', 'w' ))
+
+	def compareSchemas( self, v1, v2 ):
+		self.runCommand(
+			[ 
+				'diff', '-u', 
+				v1 + '.sql', 
+				v2 + '.sql'
+			],
+			cwd = self.baseDir + '/schemas',
+			stdout = open( self.baseDir + '/schemas/diffs', 'a' ),
+			)
+
+	def runCommand( self, args, **options ):
+		print ' '.join( args )
+		sys.stdout.flush() # misordering of output observed without this
+		retval = Popen(args, stderr = sys.stderr, **options).wait()
+		if retval and args[0] != 'diff':
+			raise UpgradeError("Nonzero return code (%d)! Aborting." % retval)
+		return retval
+
+	def readFile( self, filename ):
+		file = open( self.baseDir + '/' + filename, "r" )
+		text = file.read()
+		file.close()
+		return text
+
+	def dumpFile( self, filename, text ):
+		"""Dump a string to a file."""
+		print "Writing to %s" % filename
+		file = open( self.baseDir + '/' + filename, "wt" )
+		file.write(text)
+		file.close()
+
+	def writeTemplate( self, template, output, data ):
+		filled = self.readFile(template) % data
+		self.dumpFile(output, filled)
+
+	def sqlStatement( self, statement ):
+		self.runCommand(
+			[ 'mysql', '--defaults-file=' + self.baseDir + '/mysql-client.conf', '-e', statement ],
+			stdout = sys.stdout)
+
+	def sqlFile( self, dbname, filename ):
+		self.runCommand(
+			[ 'mysql', '--defaults-file=' + self.baseDir + '/mysql-client.conf', dbname ],
+			stdin = open( self.baseDir + '/' + filename, 'r' ),
+			stdout = sys.stdout)
+
+	def makeDatabase( self, dbname ):
+		self.sqlStatement("DROP DATABASE IF EXISTS %s; CREATE DATABASE %s;" % (dbname, dbname))
+
+	def dropDatabase( self, dbname ):
+		self.sqlStatement("DROP DATABASE %s;" % dbname)
+
+	def runPhp( self, dir, filename, *moreArgs, **kw):
+		args = self.getConf( 'php' ).split( ' ' )
+		args.append( filename )
+		if len( moreArgs ):
+			args.extend( moreArgs )
+		fullDir = self.baseDir + '/' + dir
+		if not os.path.exists( fullDir + '/' + filename ):
+			raise UpgradeError( "File not found: " + fullDir + '/' + filename )
+
+		self.runCommand( args, cwd = fullDir, stdout = sys.stdout, **kw)
+
+	def makeConfig( self, version, dbname ):
+		data = {
+			"dbName": dbname,
+			"dbUser": self.getConf( 'dbUser' ),
+			"dbPassword" : self.getConf( 'dbPassword' ) }
+		branch = self.versionToBranch( version )
+		self.writeTemplate("LocalSettings.in", branch + "/phase3/LocalSettings.php", data)
+		self.writeTemplate("AdminSettings.in", branch + "/phase3/AdminSettings.php", data)
+
+	def clearConfig( self, version ):
+		branch = self.versionToBranch( version )
+		os.remove(self.baseDir + '/' + branch + "/phase3/LocalSettings.php")
+		os.remove(self.baseDir + '/' + branch + "/phase3/AdminSettings.php")
+
+	def runUpdater( self, version, dbname ):
+		self.makeConfig( version, dbname )
+		branch = self.versionToBranch( version )
+		self.currentVersion = version
+		if ( self.getConf( 'runUpdate' ) ):
+			self.runPhp(
+				branch + "/phase3", 
+				"maintenance/update.php", "--quick")
+
+		if ( self.getConf( 'runRebuildMessages' ) ):
+			self.runPhp(
+				branch + "/phase3", 
+				"maintenance/rebuildMessages.php", 
+				stdin = open( self.baseDir + '/' + "rebuildMessages.in" ) )
+
+	def svnCheckout( self, version ):
+		branch = self.versionToBranch( version )
+		if os.path.exists( self.baseDir + '/' + branch ):
+			self.runCommand(
+				[ 'svn', 'up', '-q'], 
+				cwd = self.baseDir + '/' + branch + '/phase3',
+				stdout = sys.stdout )
+		else:
+			self.runCommand(
+				[ 
+					'svn', 'co', '-q', self.getConf( 'svnroot' ) + '/' + branch + '/phase3',
+					branch + '/phase3'
+				],
+				cwd = self.baseDir,
+				stdout = sys.stdout )
 
 
+	def testUpgrade(self, sourceVersion, destVersion):
+		"""Try building an empty database for the given version, then upgrading."""
+		print "\n\nTesting " + sourceVersion + "\n--------------------------------------------------------"
+		self.svnCheckout(destVersion)
+		self.svnCheckout(sourceVersion)
+		
+		# Initialise database
+		dbname = self.versionToDB( sourceVersion )
+		sourceBranch = self.versionToBranch( sourceVersion )
+		destBranch = self.versionToBranch( destVersion )
+		self.makeDatabase( dbname )
+		self.sqlFile( dbname, sourceBranch + "/phase3/maintenance/tables.sql" )
+		if os.path.exists( self.baseDir + '/' + sourceBranch + "/phase3/maintenance/indexes.sql"):
+			self.sqlFile(dbname, sourceBranch + "/phase3/maintenance/indexes.sql")
+		
+		# Normalise some content in it
+		if self.getConf( 'runOldUpdate' ):
+			self.runUpdater(sourceVersion, dbname)
+		
+		# Try the upgrade
+		self.runUpdater(destVersion, dbname)
 
 
-def sqlStatement(statement):
-	# hack!
-	runCommand("echo %s | mysql -u root" % shellEscape(statement))
-
-def sqlFile(dbname, filename):
-	runCommand("mysql -u root %s <%s" % shellEscape((dbname, filename)))
-
-def makeDatabase(dbname):
-	sqlStatement("DROP DATABASE IF EXISTS %s; CREATE DATABASE %s;" % (dbname, dbname))
-
-def dropDatabase(dbname):
-	sqlStatement("DROP DATABASE %s;" % dbname)
-
-def runPhp(dir, filename, options=""):
-	runCommand("cd %s && php %s %s" % (
-		shellEscape(dir),
-		shellEscape(filename),
-		options))
-
-def makeConfig(dbname, branch):
-	data = {
-		"dbname": dbname,
-		"dbuser": "root",
-		"dbpassword" : ""}
-	writeTemplate("LocalSettings.in", branch + "/phase3/LocalSettings.php", data)
-	writeTemplate("AdminSettings.in", branch + "/phase3/AdminSettings.php", data)
-
-def clearConfig(branch):
-	os.remove(branch + "/phase3/LocalSettings.php")
-	os.remove(branch + "/phase3/AdminSettings.php")
-
-def runUpdater(dbname, branch):
-	makeConfig(dbname, branch)
-	if branch == "branches/REL1_2" or branch == "branches/REL1_3":
-		# These old versions didn't have update.php.
-		# Run rebuildMessages directly
-		runPhp(branch + "/phase3", "maintenance/rebuildMessages.php", "<../../../rebuildMessages.in")
-	else:
-		runPhp(branch + "/phase3", "maintenance/update.php", "--quick")
-	#clearConfig("trunk")
-
-
-def svnCheckout(branch):
-	if os.path.exists(branch):
-		runCommand("cd %s/phase3 && svn up -q" % branch)
-	else:
-		runCommand("svn co -q http://svn.wikimedia.org/svnroot/mediawiki/%s/phase3 %s/phase3" % (branch, branch))
-
-
-def testUpgrade(dbname, branch):
-	"""Try building an empty database for the given version, then upgrading."""
-	print "\n\nTesting " + branch + "\n--------------------------------------------------------"
-	svnCheckout("trunk")
-	svnCheckout(branch)
-	
-	# Initialise database
-	makeDatabase(dbname)
-	sqlFile(dbname, branch + "/phase3/maintenance/tables.sql")
-	if os.path.exists(branch + "/phase3/maintenance/indexes.sql"):
-		sqlFile(dbname, branch + "/phase3/maintenance/indexes.sql")
-	
-	# Normalise some content in it
-	runUpdater(dbname, branch)
-	
-	# Try the upgrade
-	runUpdater(dbname, "trunk")
-	
-	#dropDatabase(dbname)
-
-testUpgrade("uptest113", "branches/REL1_13")
-testUpgrade("uptest112", "branches/REL1_12")
-testUpgrade("uptest111", "branches/REL1_11")
-testUpgrade("uptest110", "branches/REL1_10")
-testUpgrade("uptest19", "branches/REL1_9")
-testUpgrade("uptest18", "branches/REL1_8")
-testUpgrade("uptest17", "branches/REL1_7")
-testUpgrade("uptest16", "branches/REL1_6")
-testUpgrade("uptest15", "branches/REL1_5")
-testUpgrade("uptest14", "branches/REL1_4")
-testUpgrade("uptest13", "branches/REL1_3")
-testUpgrade("uptest12", "branches/REL1_2")
+tester = UpgradeTest()
+tester.run()
