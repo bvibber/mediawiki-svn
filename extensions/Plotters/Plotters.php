c4c7ab544396e8c9905b2c34c3d9c3052c30011a
laner <laner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-15 21:00:43 +0000

    Upping version to 0.2a
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50646 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Plotters/Plotters.php
+++ extensions/Plotters/Plotters.php
@@ -27,7 +27,7 @@ if ( !defined( 'MEDIAWIKI' ) )
 $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'Plotter parser extension',
-	'version'        => '0.1b',
+	'version'        => '0.2a',
 	'author'         => 'Ryan Lane',
 	'description'    => 'Allows users to create client side graphs and plots',
 	'descriptionmsg' => 'plotters-desc',
