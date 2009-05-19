554f25dc26fb5d6c758fdb9dc00444382f21b1bf
thedevilonline <thedevilonline@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-19 18:47:44 +0000

    * Initial version of an extension to timely update localisation messages in Mediawiki, using the new 'MessageNotInMwNs'-hook
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50786 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/LocalisationUpdate/LocalisationUpdate.update.php
@@ -0,0 +1,50 @@
+<?php
+function myLog($log){
+	if ( isset( $_SERVER ) && array_key_exists( 'REQUEST_METHOD', $_SERVER ) ) {
+		print($log."<br />");
+	} else {
+		print($log."\n");
+	}
+}
+
+$verbose = false;
+
+if(($argc > 0 && $argv[1] == "verbose") OR (isset($_GET['verbose'])))
+	$verbose = true;
+	
+$mtime = microtime();
+$mtime = explode(" ",$mtime);
+$mtime = $mtime[1] + $mtime[0];
+$starttime = $mtime; 
+
+define("MEDIAWIKI",true);
+
+$IP = strval( getenv('MW_INSTALL_PATH') ) !== ''
+	? getenv('MW_INSTALL_PATH')
+	: realpath( dirname( __FILE__ )."/../../" );
+
+if ( file_exists( $IP."/StartProfiler.php" ) ) {
+	require_once( $IP."/StartProfiler.php" );
+} else {
+	require_once( $IP."/includes/ProfilerStub.php" );
+}
+
+require_once( $IP."/includes/AutoLoader.php" );
+require_once( $IP."/includes/Defines.php" );
+require_once( $IP."/LocalSettings.php" );
+
+require_once( $IP."/includes/Setup.php" );
+require_once( $IP."/install-utils.inc" );
+
+
+if(is_callable("updateMessages"))
+	updateMessages($verbose);
+else
+	myLog("Error: LocalisationUpdate extension is not (correctly) included in LocalSettings.php");
+
+$mtime = microtime();
+$mtime = explode(" ",$mtime);
+$mtime = $mtime[1] + $mtime[0];
+$endtime = $mtime;
+$totaltime = ($endtime - $starttime);
+myLog("All done in ".$totaltime." seconds"); 
---
+++ extensions/LocalisationUpdate/README_FIRST.txt
@@ -0,0 +1,5 @@
+To install this extension first add the required new tables to your database by using install.sql.
+
+Include LocalisationUpdate/LocalisationUpdate.php in your LocalSettings.php
+
+Whenever you want to run an update, run LocalisationUpdate/LocalisationUpdate.update.php in your browser, on the commandline, trough a cronjob or whatever method you wish
