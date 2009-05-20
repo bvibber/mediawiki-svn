28eeb3d65bb78c5549157dc5563f4def70a9d4b5
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-05-20 08:57:33 +0000

    Useless but pretty extension. Thanks go to Splarka for testing.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@50816 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UserOptionStats/UserOptionStats.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages of UserOptionStats extension.
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$aliases = array();
+
+/** English
+ * @author Nike
+ */
+$aliases['en'] = array(
+	'UserOptionStats'          => array( 'UserOptionStats' ),
+);
---
+++ extensions/UserOptionStats/UserOptionStats.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations of UserOptionStats extension.
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$messages = array();
+
+/** English
+ * @author Nike
+ */
+$messages['en'] = array(
+	'useroptionstats' => 'User option statistics',
+	'uos-warn' => 'PHPlot is not installed!',
+	'uos-choose' => 'Choose one of the following user options: $1',
+	'uos-unknown' => '*Default',
+	'uos-other' => '*Other',
+	'uos-title' => 'Distribution of values of user option $1',
+);
