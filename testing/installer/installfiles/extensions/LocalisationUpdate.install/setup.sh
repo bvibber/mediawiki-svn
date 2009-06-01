1fd4c2fe5530c22ff634b8ffb12cfdb7cec6c925
kim <kim@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-01 17:43:09 +0000

    New setup.sh script from gerardm
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51300 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- testing/installer/installfiles/extensions/LocalisationUpdate.install/setup.sh
+++ testing/installer/installfiles/extensions/LocalisationUpdate.install/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-$MYSQL_COMMAND $DATABASE_NAME < $DESTINATION_DIR/$NAME/install.sql
+$MYSQL_COMMAND $DATABASE_NAME < $DESTINATION_DIR/$NAME/schema.sql
+
+php $DESTINATION_DIR/../maintenance/update.php
