5e862685a3133f0e90b27cd09142f900f29d1b3f
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-02 15:29:03 +0000

    Tag for version 0.1.1
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@51357 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ AdminLinks/REL_0_1_1/AdminLinks.alias.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'AdminLinks' => array( 'AdminLinks' ),
+);
+
+/** Finnish (Suomi) */
+$aliases['fi'] = array(
+	'AdminLinks' => array( 'Ylläpitolinkit' ),
+);
+
+/** Interlingua (Interlingua) */
+$aliases['ia'] = array(
+	'AdminLinks' => array( 'Ligamines pro administratores' ),
+);
+
+/** Japanese (日本語) */
+$aliases['ja'] = array(
+	'AdminLinks' => array( '管理者用リンク集' ),
+);
+
+/** Maltese (Malti) */
+$aliases['mt'] = array(
+	'AdminLinks' => array( 'ĦoloqAmmin' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'AdminLinks' => array( 'Beheerdersverwijzingen' ),
+);
+
