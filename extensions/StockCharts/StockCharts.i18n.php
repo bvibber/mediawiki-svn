69c8852d20c21f8517b16714fededc023bd15577
rfong <rfong@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-04 22:27:41 +0000

    The StockCharts extension allows users to include interactive financial charts within an article. Charts cover stocks prices, but also economic data such as the Consumer Price Index and interest rates, along with prices for commodities such as oil, and currency exchange rates. Charts are flash-powered and users can pan and zoom without a page refresh. Data is provided by Thomson / Reuters and Xignite, with a 15-20 minute delay.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51478 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/StockCharts/StockCharts.i18n.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalisation file for StockCharts extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$wgStockChartsMessages = array();
+
+$wgStockChartsMessages['en'] = array(
+	'stockchart_missingticker' => 'To embed a stock chart, you need to at least specify a ticker, like this: &lt;stockchart ticker="AAPL"/&gt;',
+);
+
+?>
