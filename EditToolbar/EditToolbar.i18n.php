e39d2f845c9b64ba2928769e245bf0ff988669a3
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-04 17:35:47 +0000

    Changed the name of the toolbar extension to EditToolbar and modified all uses of the name to match. Divided internal and external link icons. Introduced internationalization.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@51472 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ EditToolbar/EditToolbar.i18n.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Internationalisation for Usability Initiative Editing Toolbar extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Trevor Parscal
+ */
+$messages['en'] = array(
+	'edittoolbar' => 'Editing Toolbar',
+	'edittoolbar-desc' => 'Edit page toolbar with enhanced usability',
+	'edittoolbar-preference' => 'Enable enhanced editing toolbar',
+	'edittoolbar-format-bold' => 'Bold',
+	'edittoolbar-format-bold-example' => 'Bold text',
+	'edittoolbar-format-italic' => 'Italic',
+	'edittoolbar-format-italic-example' => 'Italic text',
+	'edittoolbar-insert-ilink' => 'Internal link',
+	'edittoolbar-insert-ilink-example' => 'Link title',
+	'edittoolbar-insert-xlink' => 'External link (remember http:// prefix)',
+	'edittoolbar-insert-xlink-example' => 'http://www.example.com link title',
+	'edittoolbar-insert-image' => 'Embedded file',
+	'edittoolbar-insert-image-example' => 'File:Example.jpg',
+	'edittoolbar-insert-reference' => 'Insert a reference',
+	'edittoolbar-insert-reference-example' => 'Insert footnote text here',
+);
