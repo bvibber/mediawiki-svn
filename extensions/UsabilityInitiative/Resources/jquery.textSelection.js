fdf1231c7e8d5cadb4ff3edaec007b902df5607f
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-05 22:30:42 +0000

    Refactored the javascript code extensively, ported code from edit.css into the new jquery plugin format. Added more features...
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51520 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/Resources/jquery.textSelection.js
+++ extensions/UsabilityInitiative/Resources/jquery.textSelection.js
@@ -1,54 +1,82 @@
 /*
- * Functions getSelection and replaceSelection based on:
- * 		(c) 2006 Alex Brem <alex@0xab.cd> - http://blog.0xab.cd
- * All other code:
- * 		(c) 2009 Wikimedia Foundation (GPLv2) - http://www.wikimedia.org
+ * Ported from skins/common/edit.js by Trevor Parscal
+ * (c) 2009 Wikimedia Foundation (GPLv2) - http://www.wikimedia.org
  */
 (function($) {
     $.fn.extend({
-		getSelection: function() {
+		encapsulateSelection: function( pre, peri, post ) {
             var e = this.jquery ? this[0] : this;
-			if ( 'selectionStart' in e ) {
-				/* Mozilla / DOM 3.0 */
-				var l = e.selectionEnd - e.selectionStart;
-				return { start: e.selectionStart, end: e.selectionEnd, length: l, text: e.value.substr( e.selectionStart, l ) };
-			} else if ( document.selection ) {
-				/* Internet Explorer */
+			/**
+			 * CLEAN THIS UP PLEASE!
+			 */
+			var selText;
+			var isSample = false;
+			if (document.selection  && document.selection.createRange) { // IE/Opera
+		
+				//save window scroll position
+				if (document.documentElement && document.documentElement.scrollTop)
+					var winScroll = document.documentElement.scrollTop
+				else if (document.body)
+					var winScroll = document.body.scrollTop;
+				//get current selection
 				e.focus();
-				var r = document.selection.createRange();
-				if (r == null) {
-					return { start: 0, end: e.value.length, length: 0, text: null }
+				var range = document.selection.createRange();
+				selText = range.text;
+				//insert tags
+				checkSelectedText();
+				range.text = pre + selText + post;
+				//mark sample text as selected
+				if (isSample && range.moveStart) {
+					if (window.opera)
+						post = post.replace(/\n/g,'');
+					range.moveStart('character', - post.length - selText.length);
+					range.moveEnd('character', - post.length);
 				}
-				var re = e.createTextRange();
-				var rc = re.duplicate();
-				re.moveToBookmark( r.getBookmark() );
-				rc.setEndPoint( 'EndToStart', re );
-				return { start: rc.text.length, end: rc.text.length + r.text.length, length: r.text.length, text: r.text };
-			} else {
-				/* Browser not supported */
-				return { start: 0, end: e.value.length, length: 0, text: null };
-			}
-        },
-		replaceSelection: function() {
-            var e = this.jquery ? this[0] : this;
-			var text = arguments[0] || '';
-			if ( 'selectionStart' in e ) {
-				/* Mozilla / DOM 3.0 */
-				e.value = e.value.substr( 0, e.selectionStart ) + text + e.value.substr( e.selectionEnd, e.value.length );
-			} else if ( document.selection ) {
-				/* Internet Explorer */
+				range.select();
+				//restore window scroll position
+				if (document.documentElement && document.documentElement.scrollTop)
+					document.documentElement.scrollTop = winScroll
+				else if (document.body)
+					document.body.scrollTop = winScroll;
+		
+			} else if (e.selectionStart || e.selectionStart == '0') { // Mozilla
+		
+				//save textarea scroll position
+				var textScroll = e.scrollTop;
+				//get current selection
 				e.focus();
-				document.selection.createRange().text = text;
-			} else {
-				/* Browser not supported  */
-				e.value += text;
+				var startPos = e.selectionStart;
+				var endPos = e.selectionEnd;
+				selText = e.value.substring(startPos, endPos);
+				//insert tags
+				checkSelectedText();
+				e.value = e.value.substring(0, startPos)
+					+ pre + selText + post
+					+ e.value.substring(endPos, e.value.length);
+				//set new selection
+				if (isSample) {
+					e.selectionStart = startPos + pre.length;
+					e.selectionEnd = startPos + pre.length + selText.length;
+				} else {
+					e.selectionStart = startPos + pre.length + selText.length + post.length;
+					e.selectionEnd = e.selectionStart;
+				}
+				//restore textarea scroll position
+				e.scrollTop = textScroll;
 			}
-			return this;
-        },
-		encapsulateSelection: function( pre, post, insert ) {
-			var obj = $( this );
-			obj.replaceSelection( ( pre || '' ) + ( obj.getSelection().text || insert ) + ( post || pre || '' ) );
-			return this;
-        }
+			// Checks if the selected text is the same as the insert text
+			function checkSelectedText(){
+				if (!selText) {
+					selText = peri;
+					isSample = true;
+				} else if (selText.charAt(selText.length - 1) == ' ') { //exclude ending space char
+					selText = selText.substring(0, selText.length - 1);
+					post += ' '
+				}
+			}
+			/**
+			 * /CLEAN THIS UP PLEASE!
+			 */
+		}
     });
 })(jQuery);
