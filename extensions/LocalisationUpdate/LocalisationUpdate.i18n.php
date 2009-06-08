b3b1d3909fbe01289046dd1ddeb8ee84d48a5fc1
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-08 23:23:01 +0000

    Localisation update: qqq, de, ksh.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51619 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LocalisationUpdate/LocalisationUpdate.i18n.php
+++ extensions/LocalisationUpdate/LocalisationUpdate.i18n.php
@@ -15,14 +15,46 @@ $messages['en'] = array(
 	'localisationupdate-desc' => 'Keeps the localised messages as up to date as possible',
 );
 
+/** Message Documentation
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'localisationupdate-desc' => 'This is a short description of the extension.
+It is shown on [[SPECIAL:Version]].
+Do not translate tag names, or link targets.',
+);
+
+/** German
+ * @author Purodha
+ */
+$messages['de'] = array(
+	'localisationupdate-desc' => 'Lokalisierte Texte und Nachrichten möglichst aktuell halten',
+);
+
+/** Ripuarian
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'localisationupdate-desc' => 'Texte un Nohreeschte vum Wiki esu joot wi müjjelich om neueste Shtand halde',
+);
+
+/** Cantonese
+ * @author Tom Maaswinkel
+ */
 $messages['yue'] = array(
 	'localisationupdate-desc' => '將本地化嘅信息保持最新',
 );
 
+/** Chinese (simplified)
+ * @author Tom Maaswinkel
+ */
 $messages['zh-hans'] = array(
 	'localisationupdate-desc' => '将本地化的信息保持最新',
 );
 
+/** Chinese (Tranditional)
+ * @author Tom Maaswinkel
+ */
 $messages['zh-hant'] = array(
 	'localisationupdate-desc' => '將本地化的信息保持最新',
 );
