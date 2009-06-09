7fcf06fd781250c4a743cefa3e7e906b3a22ac78
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-09 20:19:43 +0000

    Adjusted language in warning message.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51660 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/EditWarning/EditWarning.i18n.php
+++ extensions/UsabilityInitiative/EditWarning/EditWarning.i18n.php
@@ -14,5 +14,5 @@ $messages = array();
 $messages['en'] = array(
 	'editwarning' => 'Edit warning',
 	'editwarning-desc' => 'Show a warning when a user tries to leave the edit form without saving',
-	'editwarning-warning' => 'You have unsaved changes. Click OK to leave this page and destroy your changes, or Cancel to stay here and keep them.',
+	'editwarning-warning' => 'You have unsaved changes. Click OK to leave this page and abandon your changes, or Cancel to stay here and keep them.',
 );
