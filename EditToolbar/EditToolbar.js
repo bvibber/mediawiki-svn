585e57395c9ee4c4d9a1741c912f4573fcf7a355
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-09 20:23:33 +0000

    Moved message code which emulates loadGM and gM functions to a central place.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@51661 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditToolbar/EditToolbar.js
+++ EditToolbar/EditToolbar.js
@@ -1,46 +1,6 @@
 /* JavaScript for EditToolbar extension */
 
 /**
- * This is designed to be directly compatible with (and is essentially taken
- * directly from) the mv_embed code for bringing internationalized messages into
- * the JavaScript space. As such, if we get to the point of merging that stuff
- * into the main branch this code will be uneeded and probably cause issues.
- */
-// Creates global message object if not already in existence
-if ( !gMsg ) var gMsg = {};
-/**
- * Caches a list of messages for later retieval
- * @param {Object} msgSet Hash of key:value pairs of messages to cache
- */
-function loadGM( msgSet ){
-	for ( var i in msgSet ){
-		gMsg[ i ] = msgSet[i];
-	}
-}
-/**
- * Retieves a message from the global message cache, performing on-the-fly
- * replacements using MediaWiki message syntax ($1, $2, etc.)
- * @param {String} key Name of message as it is in MediaWiki
- * @param {Array} args Array of replacment arguments
- */
-function gM( key, args ) {
-	var ms = '';	
-	if ( key in gMsg ) {
-		ms = gMsg[ key ];
-		if ( typeof args == 'object' || typeof args == 'array' ) {				 
-			for ( var v in args ){
-				var rep = '\$'+ ( parseInt(v) + 1 );
-				ms = ms.replace( rep, args[v]);
-			}			 
-		} else if ( typeof args =='string' || typeof args =='number' ) {
-			ms = ms.replace( /\$1/, args );
-		}
-		return ms;
-	} else {
-		return '[' + key + ']';
-	}
-}
-/**
  * This is the toolbar plugin, which can be used like
  * $( 'div#edittoolbar' ).toolbar( '#wpTextbox1', tools );
  * Where tools is an array of objects which describe each tool (see below for
