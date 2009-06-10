d51f9a6cb8e033ccd27668420be3e015edbfa21e
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-10 22:41:53 +0000

    EditToolbar: Add a Special characters tab. The actual characters need to be added by the wiki in a JS file like Common.js; to import from an old-style charinsert object, use 	editToolbarConfiguration['specialchars'].pages = $().parseCharinsert( charinsert );
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51714 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
+++ extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -18,7 +18,7 @@
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 1;
+$wgEditToolbarStyleVersion = 2;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
