986a5c05bbe6323b6ad5b11b6fdfcbb0e1b10032
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-11 19:38:39 +0000

    UsabilityInitiative: Committing work on PrefStats extension to track skin and toolbar usage. Code is WIP and should not be used
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51753 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UsabilityInitiative/PrefStats/PrefStats.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for Usability Initiative EditWarning extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Roan Kattouw
+ */
+$messages['en'] = array(
+	'prefstats' => 'Preference statistics',
+	'prefstats-desc' => 'Track statistics about how many users have certain preferences enabled',
+);
---
+++ extensions/UsabilityInitiative/PrefStats/PrefStats.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Usability Initiative PrefStats extension
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * This file contains the include file for the EditWarning portion of the
+ * UsabilityInitiative extension of MediaWiki.
+ *
+ * Usage: This file is included automatically by ../UsabilityInitiative.php
+ *
+ * @author Roan Kattouw <roan.kattouw@gmail.com>
+ * @license GPL v2 or later
+ * @version 0.1.1
+ */
+
+/* Configuration */
+
+//.Set to false to disable tracking
+$wgPrefStatsEnable = true;
+
+// array('prefname' => 'value')
+// value can't be the default value
+// Tracking multiple values of the same preference is not possible
+$wgPrefStatsTrackPrefs = array();
+
+/* Setup */
+
+// Credits
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'PrefStats',
+	'author' => 'Roan Kattouw',
+	'version' => '0.1.1',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:UsabilityInitiative',
+	'descriptionmsg' => 'prefstats-desc',
+);
+
+// Adds Autoload Classes
+$wgAutoloadClasses['PrefStatsHooks'] =
+	dirname( __FILE__ ) . '/PrefStats.hooks.php';
+
+// Adds Internationalized Messages
+$wgExtensionMessagesFiles['PrefStats'] =
+	dirname( __FILE__ ) . '/PrefStats.i18n.php';
+
+// Registers Hooks
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'PrefStatsHooks::schema';
+$wgHooks['UserSaveOptions'][] = 'PrefStatsHooks::save';
