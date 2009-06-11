bvibber/mediawiki-svn

commit 6f6b098b6d547aaed53e36f09c10f1a57157933f

possible XSS
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@36340 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

wiki2xml/php/mediawiki_converter.php
@@ -42,7 +42,7 @@ class MediaWikiConverter {
 		}
 		$xml = '<article' ;
 		if ( $title != "" ) {
-			$xml .= " title='{$title}'" ;
+			$xml .= ' title="' . htmlspecialchars( $title ) '"' ;
 			$content_provider->add_article ( urldecode ( $ot ) ) ;
 		}
 		$xml .= '>' ;

commit 654d829aa1d6c004bca540d16eb050a621acda55

Fix breaking typo introduced by tstarling in r36340, noticed by Jan Rous
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@51756 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

wiki2xml/php/mediawiki_converter.php
@@ -42,7 +42,7 @@ class MediaWikiConverter {
 		}
 		$xml = '<article' ;
 		if ( $title != "" ) {
-			$xml .= ' title="' . htmlspecialchars( $title ) '"' ;
+			$xml .= ' title="' . htmlspecialchars( $title ) . '"' ;
 			$content_provider->add_article ( urldecode ( $ot ) ) ;
 		}
 		$xml .= '>' ;