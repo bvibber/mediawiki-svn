09d6584604bee7b2a91168eb96f3247cfe701523
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-13 20:17:19 +0000

    Localisation updates for extension messages from translatewiki.net (2009-06-13 19:49 UTC)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@51838 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.i18n.php
+++ UsabilityInitiative.i18n.php
@@ -15,3 +15,19 @@ $messages['en'] = array(
 	'usabilityinitiative' => 'Usability Initiative',
 	'usabilityinitiative-desc' => 'Features developed by the Wikipedia Usability Initiative to enhance the usability of MediaWiki',
 );
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'usabilityinitiative' => 'Зручны пачатак',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'usabilityinitiative' => 'Bruikbaarheidsinitiatief',
+	'usabilityinitiative-desc' => 'Functionaliteit die door het Wikipedia Bruikbaarheidsinitiatief is ontwikkeld ter verbetering van de bruikbaarheid van MediaWiki',
+);
+
