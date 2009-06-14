c5987e0704d64f1efac0cb222157ce9ce45557a1
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-14 14:40:31 +0000

    Localisation updates for extension messages from translatewiki.net (2009-06-14 14:35 UTC)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@51851 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrefStats/PrefStats.i18n.php
+++ PrefStats/PrefStats.i18n.php
@@ -16,6 +16,31 @@ $messages['en'] = array(
 	'prefstats-desc' => 'Track statistics about how many users have certain preferences enabled',
 );
 
+/** French (Français)
+ * @author Crochet.david
+ * @author IAlex
+ */
+$messages['fr'] = array(
+	'prefstats' => 'Préférence des statistiques',
+	'prefstats-desc' => "Statistiques sur le nombre d'utilisateurs ayant certaines préférences activées",
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'prefstats' => 'Statistika nastajenjow',
+	'prefstats-desc' => 'Statistika wo tym, kelko wužiwarjow je wěste nastajenja aktiwizowało',
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'prefstats' => 'Shtatistike övver Enshtellunge',
+	'prefstats-desc' => 'Määt Shtatistike doh drövver, wi vill Metmaacher beshtemmpte Enshtellunge för sesch jemaat han.',
+);
+
 /** Dutch (Nederlands)
  * @author Siebrand
  */
