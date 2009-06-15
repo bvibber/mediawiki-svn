0f1fc3fddb3f78523d23ac516207007f89045a53
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-15 07:49:02 +0000

    Localisation updates for extension messages from translatewiki.net (2009-06-15 07:42 UTC)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@51865 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrefStats/PrefStats.i18n.php
+++ PrefStats/PrefStats.i18n.php
@@ -16,6 +16,14 @@ $messages['en'] = array(
 	'prefstats-desc' => 'Track statistics about how many users have certain preferences enabled',
 );
 
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'prefstats' => 'Статыстыка прывілеяў',
+	'prefstats-desc' => 'Ствараць статыстыку аб тым, як многа ўдзельнікаў дакладна карыстаюцца прывілеямі',
+);
+
 /** French (Français)
  * @author Crochet.david
  * @author IAlex
@@ -33,6 +41,14 @@ $messages['hsb'] = array(
 	'prefstats-desc' => 'Statistika wo tym, kelko wužiwarjow je wěste nastajenja aktiwizowało',
 );
 
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'prefstats' => '個人設定の統計',
+	'prefstats-desc' => 'どの程度の数の利用者が個人設定のある項目を有効にしているかの追跡統計',
+);
+
 /** Ripoarisch (Ripoarisch)
  * @author Purodha
  */
@@ -47,5 +63,13 @@ $messages['ksh'] = array(
 $messages['nl'] = array(
 	'prefstats' => 'Voorkeurenstatistieken',
 	'prefstats-desc' => 'Statistieken bijhouden over hoeveel gebruikers bepaalde voorkeuren hebben ingeschakeld',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'prefstats' => 'Statistece de preferenze',
+	'prefstats-desc' => 'Traccie le statisteche sus a quanda utinde onne certe preferenze abbilitate',
 );
 
