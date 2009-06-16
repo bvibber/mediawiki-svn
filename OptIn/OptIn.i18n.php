68660d20308e42320ae8741ec02f93230931a2a9
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-16 21:21:13 +0000

    OptIn: Another tweak per Siebrand
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@52005 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- OptIn/OptIn.i18n.php
+++ OptIn/OptIn.i18n.php
@@ -15,7 +15,7 @@ $messages['en'] = array(
 	'optin' => 'OptIn',
 	'optin-desc' => 'Allow users to opt-in to the Usability Initiative\'s usability enhancements',
 	'optin-title' => 'Usability Intitiative enhancements',
-	'optin-needlogin' => '<div class="plainlinks">You need to [$1 log in] to opt-in to the Usability Initiative\'s usability enhancements.</div>',
+	'optin-needlogin' => 'You need to [$1 log in] to opt-in to the Usability Initiative\'s usability enhancements.',
 	'optin-intro' => 'The Wikipedia Usability Initiative has developed a new skin and a new edit toolbar intended to enhance the usability of Wikipedia. These enhancements have not been enabled for all users yet, but you can opt-in to them by clicking the button below.',
 	'optin-success-in' => 'You have successfully opted in to the Usability Initiative\'s usability enhancements. You can opt back out at any time by clicking the button below.',
 	'optin-success-out' => 'You have successfully opted out of the Usability Initiative\'s usability enhancements. You can opt back in at any time by clicking the button below.',
