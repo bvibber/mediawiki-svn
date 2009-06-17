387777f302527a1cb1ddaa925f94c1bd6bafebbb
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-17 01:52:16 +0000

    added remoteMwEmbed.js for supporting remote usage of mwEmbed for wikis without the new-upload branch cleaned up mvClipEdit.js
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-upload@52016 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js2/editPage.js
+++ phase3/js2/editPage.js
@@ -25,6 +25,7 @@ mwAddOnloadHook( function(){
 var mwEditPageHelper = {	
 	init:function(){
 		var _this = this;
+		alert('runing mwEditPageHelper');
 		//add the add-media-wizard button
 		$j('#toolbar').append('<img style="cursor:pointer" id="btn-add-media-wiz" src="' + mv_skin_img_path + 'Button_add_media.png">');
 		$j('#btn-add-media-wiz').addMediaWiz( 
