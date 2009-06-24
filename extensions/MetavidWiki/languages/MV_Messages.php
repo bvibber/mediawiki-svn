0d487c6ca4859de7466893faab2c88b225d8fba7
purodha <purodha@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-24 07:40:46 +0000

    Added missing space to message.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@52350 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MetavidWiki/languages/MV_Messages.php
+++ extensions/MetavidWiki/languages/MV_Messages.php
@@ -399,7 +399,7 @@ Jump to stream view: $2',
 	'mv_error_mvd_not_found' => 'Error mvd not found',
 
 	# sequence text:
-	'mv_edit_sequence' => 'Editing sequence:$1',
+	'mv_edit_sequence' => 'Editing sequence: $1',
 	'mv_sequence_player_title' => 'sequence player',
 	'mv_resource_not_found' => "Resource '''$1''' not found",
 	'mv_resource_not_supported' => "Resource '''$1''' type not supported",
