d381af9dec019da1ab28bac15c8ada752ea4a184
rarohde <rarohde@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-06-26 07:07:00 +0000

    New extension: Drop-in CSS/JS compressor that captures output from action=raw and sends it to either JSMin or the YUI CSS compressor.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@52436 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Minify/Minify.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Internationalisation for extension Minify
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Robert Rohde
+ */
+$messages['en'] = array(
+	'minify-desc' => 'Shrinks CSS and Javascript output from action=raw.',
+);
+
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'stringfunctions-desc' => 'Short description of the extension. Shown in [[Special:Version]].',
+);
