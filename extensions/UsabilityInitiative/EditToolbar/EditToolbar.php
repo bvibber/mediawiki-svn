544ee482b1887bd1e32569412768954da24cc25d
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-01 23:04:05 +0000

    Updated version of script and style resources
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@52661 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
+++ extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -18,7 +18,7 @@
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 7;
+$wgEditToolbarStyleVersion = 8;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
