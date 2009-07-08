749d071f43d1c09b8df272110877a2aac97750c8
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-08 19:04:31 +0000

    Add 'collab' to the special-case list (*.wikimedia.org sites which need to use 'wikipedia' sitename on secure)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@52936 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/bookmarklets/secure-switch.js
+++ tools/bookmarklets/secure-switch.js
@@ -1,4 +1,5 @@
 function swapUrl(base) {
+	var specials = "meta|commons|office|internal|collab";
 	if (base[4] == 's') {
 		base = base.replace(
 			/^https:\/\/secure\.wikimedia\.org\/(.*?)\/(.*?)\/(.*)$/,
@@ -7,14 +8,14 @@ function swapUrl(base) {
 			"http://mediawiki.wikipedia",
 			"http://www.mediawiki");
 		base = base.replace(
-			/http:\/\/(meta|commons|office|internal)\.wikipedia/,
+			new RegExp("^http:\/\/(" + specials + ")\.wikipedia"),
 			"http://$1.wikimedia");
 	} else {
 		base = base.replace(
 			"http://www.mediawiki",
 			"http://mediawiki.wikipedia");
 		base = base.replace(
-			/http:\/\/(meta|commons|office|internal)\.wikimedia/,
+			new RegExp("^http:\/\/(" + specials + ")\.wikimedia"),
 			"http://$1.wikipedia");
 		base = base.replace(
 			/^http:\/\/(.*?)\.(.*?)\.org\/(.*)$/,
