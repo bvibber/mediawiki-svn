6f122098f4c8f2c53c24d198c4564d8671c58a6d
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-08 23:23:51 +0000

    Added support for allowing an existing onbeforeunload hook to be evaluated and returned if needed - thus superceding our own.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@52950 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditWarning/EditWarning.js
+++ EditWarning/EditWarning.js
@@ -1,19 +1,43 @@
 /* JavaScript for EditWarning extension */
 
 $( document ).ready( function() {
+	// Checks if the skin is not vector
 	if( skin != 'vector' )
+		// Exits
 		return;
-	
+	// Gets the original values of some form elements
 	$( '#wpTextbox1, #wpSummary' ).each( function() {
 		$(this).data( 'origtext', $(this).val() );
 	});
-	if( !( 'onbeforeunload' in window ) )
-		window.onbeforeunload = function() {
-			if( $( '#wpTextbox1' ).data( 'origtext' ) != $( '#wpTextbox1' ).val() ||
-					$( '#wpSummary' ).data( 'origtext' ) != $( '#wpSummary' ).val() )
-				return gM( 'editwarning-warning' );
-	};
+	
+	fallbackWindowOnBeforeUnload = window.onbeforeunload;
+	window.onbeforeunload = function() {
+		var fallbackResult = null;
+		// Checks if someone already set on onbeforunload hook
+		if ( fallbackWindowOnBeforeUnload ) {
+			// Gets the result of their onbeforeunload hook
+			fallbackResult = fallbackWindowOnBeforeUnload();
+		}
+		// Checks if their onbeforeunload hook returned something
+		if ( fallbackResult !== null ) {
+			// Exits here, returning their message
+			return fallbackResult;
+		}
+		// Checks if the current values of some form elements are the same as
+		// the original values
+		if(
+			$( '#wpTextbox1' ).data( 'origtext' ) != $( '#wpTextbox1' ).val() ||
+			$( '#wpSummary' ).data( 'origtext' ) != $( '#wpSummary' ).val()
+		) {
+			// Returns our message
+			return gM( 'editwarning-warning' );
+		}
+	}
+	// Adds form submission handler
 	$( 'form' ).submit( function() {
-		window.onbeforeunload = function() {};
+		// Restores whatever previous onbeforeload hook existed
+		window.onbeforeunload = fallbackWindowOnBeforeUnload;
 	});
 });
+//Global storage of fallback for onbeforeunload hook
+var fallbackWindowOnBeforeUnload = null;
