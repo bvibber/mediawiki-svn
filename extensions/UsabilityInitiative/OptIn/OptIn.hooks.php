a92eccd729e574913b7a3085d493da3e8a3eb831
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-09 20:44:28 +0000

    Added global configs to set if the personal tools link for opting in and out should be always or never visible.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53008 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/OptIn/OptIn.hooks.php
+++ extensions/UsabilityInitiative/OptIn/OptIn.hooks.php
@@ -11,6 +11,7 @@ class OptInHooks {
 	/* Static Functions */
 	public static function schema() {
 		global $wgExtNewTables;
+		
 		$wgExtNewTables[] = array(
 			'optin_survey',
 			dirname( __FILE__ ) . '/OptIn.sql'
@@ -20,20 +21,37 @@ class OptInHooks {
 	
 	public static function personalUrls( &$personal_urls, &$title ) {
 		global $wgUser;
+		global $wgOptInAlwaysShowPersonalLink, $wgOptInNeverShowPersonalLink;
 		
-		//if ( SpecialOptIn::isOptedIn( $wgUser ) ) {
+		// Checks if...
+		if (
+			// This should be shown because...
+			(
+				// The user is opted in
+				SpecialOptIn::isOptedIn( $wgUser ) ||
+				// Or the link should always be shown
+				$wgOptInAlwaysShowPersonalLink
+			) &&
+			// And the link is allowed to be shown
+			!$wgOptInNeverShowPersonalLink
+		) {
+			// Loads opt-in messages
 			wfLoadExtensionMessages( 'OptIn' );
+			// Inserts a link into personal tools
 			$personal_urls = array_merge(
 				array(
 					'acaibeta' => array(
-						'text' => SpecialOptIn::isOptedIn( $wgUser ) ? wfMsg( 'optin-leave' ) : wfMsg( 'optin-try' ),
-						'href' => SpecialPage::getTitleFor( 'UsabilityInitiativeOptIn' )->getLocalUrl()
-							. '/' . $title->getFullText(),
+						'text' => SpecialOptIn::isOptedIn( $wgUser ) ?
+									wfMsg( 'optin-leave' ) :
+									wfMsg( 'optin-try' ),
+						'href' => SpecialPage::getTitleFor(
+							'UsabilityInitiativeOptIn'
+						)->getLocalUrl() . '/' . $title->getFullText(),
 					)
 				),
 				$personal_urls
 			);
-		//}
+		}
 		return true;
 	}
 }
