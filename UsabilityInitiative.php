9e274fef968621b784ea61081e36ce7c023ba6db
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-10 12:16:20 +0000

    Revert r52564, arbitrary script inclusion vulnerability.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@53045 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.php
+++ UsabilityInitiative.php
@@ -22,19 +22,6 @@
 // Set this to true to simply override the stock toolbar for everyone
 $wgUsabilityInitiativeCoesxistWithMvEmbed = false;
 
-// Configures which modules should be enabled
-if (
-	!isset( $wgUsabilityInitiativeModules ) ||
-	!is_array( $wgUsabilityInitiativeModules )
-) {
-	$wgUsabilityInitiativeModules = array(
-		'EditToolbar',
-		'EditWarning',
-		'PrefStats',
-		'OptIn'
-	);
-}
-
 /* Setup */
 
 // Sets Credits
@@ -55,10 +42,11 @@ $wgAutoloadClasses['UsabilityInitiativeHooks'] =
 $wgExtensionMessagesFiles['UsabilityInitiative'] =
 	dirname( __FILE__ ) . "/UsabilityInitiative.i18n.php";
 
-// Includes modules
-foreach ( $wgUsabilityInitiativeModules as $module ) {
-	require_once( dirname( __FILE__ ) . "/{$module}/{$module}.php" );
-}
+// Includes sub-extensions
+require_once( dirname( __FILE__ ) . "/EditToolbar/EditToolbar.php" );
+require_once( dirname( __FILE__ ) . "/EditWarning/EditWarning.php" );
+require_once( dirname( __FILE__ ) . "/PrefStats/PrefStats.php" );
+require_once( dirname( __FILE__ ) . "/OptIn/OptIn.php" );
 
 // Registers Hooks
 $wgHooks['AjaxAddScript'][] = 'UsabilityInitiativeHooks::addJs';
