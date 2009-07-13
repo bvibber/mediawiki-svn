bvibber/mediawiki-svn

commit ff95ec0488863c7f237aca7bb30b2c975ceeb52b

Fix regression from r49061: Restore sequence of paramters. Do not shift parameters with different content. This breaks every customization in the local MediaWiki namespace.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@49076 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/FlaggedRevs/specialpages/StableVersions_body.php
@@ -56,14 +56,14 @@ class StableVersions extends UnlistedSpecialPage
 	public function formatRow( $row ) {
 		global $wgLang, $wgUser;
 
-		$time = $wgLang->timeanddate( wfTimestamp(TS_MW, $row->rev_timestamp), true );
-		$fdatim = $wgLang->timeanddate( wfTimestamp(TS_MW, $row->fr_timestamp), true );
-		$fdate = $wgLang->date( wfTimestamp(TS_MW, $row->fr_timestamp), true );
-		$ftime = $wgLang->time( wfTimestamp(TS_MW, $row->fr_timestamp), true );
+		$fdatim = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
+		$fdate = $wgLang->date( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
+		$ftime = $wgLang->time( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
 		$review = wfMsgExt( 'stableversions-review', array( 'parseinline', 'replaceafter' ),
-			$fdatim, $fdate, $ftime,
-			$this->skin->userLink( $row->fr_user, $row->user_name ) .
-			' ' . $this->skin->userToolLinks( $row->fr_user, $row->user_name ) );
+			$fdatim,
+ 			$this->skin->userLink( $row->fr_user, $row->user_name ) .
+ 			' ' . $this->skin->userToolLinks( $row->fr_user, $row->user_name ),
+			$fdate, $ftime );
 
 		$lev = ( $row->fr_quality >=1 ) ? wfMsgHtml('hist-quality') : wfMsgHtml('hist-stable');
 		$link = $this->skin->makeKnownLinkObj( $this->page, $time,

commit 1613352cb9399d1a13996aab3ba8aadd0727455a

Follow up to r53153: wasn't just a typo...also the wrong var...added the correct rev_timestamp again. This was broken from r49076.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53154 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/FlaggedRevs/specialpages/StableVersions_body.php
@@ -56,7 +56,7 @@ class StableVersions extends UnlistedSpecialPage
 
 	public function formatRow( $row ) {
 		global $wgLang, $wgUser;
-
+		$rdatim = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->rev_timestamp ), true );
 		$fdatim = $wgLang->timeanddate( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
 		$fdate = $wgLang->date( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
 		$ftime = $wgLang->time( wfTimestamp( TS_MW, $row->fr_timestamp ), true );
@@ -64,12 +64,10 @@ class StableVersions extends UnlistedSpecialPage
 			$fdatim,
  			$this->skin->userLink( $row->fr_user, $row->user_name ) .
  			' ' . $this->skin->userToolLinks( $row->fr_user, $row->user_name ),
-			$fdate, $ftime, $row->user_name );
-
+			$fdate, $ftime, $row->user_name
+		);
 		$lev = ( $row->fr_quality >=1 ) ? wfMsgHtml('hist-quality') : wfMsgHtml('hist-stable');
-		$link = $this->skin->makeKnownLinkObj( $this->page, $ftime,
-			'stableid='.$row->fr_rev_id );
-
+		$link = $this->skin->makeKnownLinkObj( $this->page, $rdatim, 'stableid='.$row->fr_rev_id );
 		return '<li>'.$link.' ('.$review.') <strong>['.$lev.']</strong></li>';
 	}
 }