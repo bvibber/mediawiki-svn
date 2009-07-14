6a512a58999992c2fefa0268718f900b5f90c174
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-14 17:26:48 +0000

    Fill out that special exceptions list a bit more. :)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53244 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/bookmarklets/secure-switch.js
+++ tools/bookmarklets/secure-switch.js
@@ -1,19 +1,59 @@
 function swapUrl(base) {
-	var specials = "meta|commons|office|internal|collab|usability";
+	/* Hardcore special cases... use "virtual" hostnames for some flipping. */
+	var specialCases = [
+		["www.mediawiki.org", "mediawiki.wikipedia.org"],
+		["wikimediafoundation.org", "foundation.wikipedia.org"]];
+	/* Sites we need to swap in 'wikipedia' in place of 'wikimedia': */
+	var specialWikis = [
+		'advisory',
+		'auditcom',
+		'board',
+		'chair',
+		'chapcom',
+		'collab',
+		'comcom',
+		'commons',
+		'exec',
+		'grants',
+		'incubator',
+		'internal',
+		'langcom',
+		'meta',
+		'office',
+		'otrs-wiki',
+		'quality',
+		'searchcom',
+		'spcom',
+		'species',
+		'usability',
+		'wikimania2005',
+		'wikimania2006',
+		'wikimania2007',
+		'wikimania2008',
+		'wikimania2009',
+		'wikimania2010',
+		'wikimaniateam',
+		'wikimania'];
+	var specials = specialWikis.join('|');
+	
 	if (base[4] == 's') {
 		base = base.replace(
 			/^https:\/\/secure\.wikimedia\.org\/(.*?)\/(.*?)\/(.*)$/,
 			"http://$2.$1.org/$3");
-		base = base.replace(
-			"http://mediawiki.wikipedia",
-			"http://www.mediawiki");
+		for (var i = 0; i < specialCases.length; i++) {
+			base = base.replace(
+				"http://" + specialCases[i][1],
+				"http://" + specialCases[i][0]);
+		}
 		base = base.replace(
 			new RegExp("^http:\/\/(" + specials + ")\.wikipedia"),
 			"http://$1.wikimedia");
 	} else {
-		base = base.replace(
-			"http://www.mediawiki",
-			"http://mediawiki.wikipedia");
+		for (var i = 0; i < specialCases.length; i++) {
+			base = base.replace(
+				"http://" + specialCases[i][0],
+				"http://" + specialCases[i][1]);
+		}
 		base = base.replace(
 			new RegExp("^http:\/\/(" + specials + ")\.wikimedia"),
 			"http://$1.wikipedia");
