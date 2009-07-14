a81a76b1f11063d2007ec8490f397abdd8c863e0
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-14 20:12:42 +0000

    Added initial files for FundraiserPortal extension.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53257 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FundraiserPortal/FundraiserPortal.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * FundraiserPortal extension
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * This file contains the main include file for the FundraiserPortal
+ * extension of MediaWiki.
+ *
+ * Usage: Add the following line to your LocalSettings.php file
+ * require_once( "$IP/extensions/FundraiserPortal/FundraiserPortal.php" );
+ *
+ * @author Trevor Parscal <tparscal@wikimedia.org>
+ * Allow "or a later version" here?
+ * @license GPL v2
+ * @version 0.1.1
+ */
+
+/* Configuration */
+
+$wgFundraiserPortalShow = true;
+$wgFundraiserPortalURL = 'http://wikimediafoundation.org/wiki/Donate/Now/en';
+
+/* Setup */
+
+// Sets Credits
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'FundraiserPortal',
+	'author' => 'Trevor Parscal',
+	'version' => '0.1.1',
+	'url' => 'http://www.mediawiki.org/wiki/FundraiserPortal',
+	'descriptionmsg' => 'fundraiserportal-desc',
+);
+
+// Adds Autoload Classes
+$wgAutoloadClasses['FundraiserPortalHooks'] =
+	dirname( __FILE__ ) . "/FundraiserPortal.hooks.php";
+
+// Adds Internationalized Messages
+$wgExtensionMessagesFiles['FundraiserPortal'] =
+	dirname( __FILE__ ) . "/FundraiserPortal.i18n.php";
+
+// Registers Hooks
+$wgHooks['SkinBuildSidebar'][] = 'FundraiserPortalHooks::buildSidebar';
