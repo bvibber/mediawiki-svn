1cf1def0946782f9c87efc5029ab821ac9641d2a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-17 23:13:47 +0000

    Localisation updates for extension messages from translatewiki.net (2009-07-17 22:50 UTC)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/FundraiserPortal@53429 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- FundraiserPortal.i18n.php
+++ FundraiserPortal.i18n.php
@@ -24,3 +24,170 @@ $messages['en'] = array(
 	'fundraiserportal-tourmaline-message' => 'Every donation helps us to keep Wikipedia free for everyone.',
 	'fundraiserportal-tourmaline-close' => 'Hide this from now on',
 );
+
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'fundraiserportal-desc' => 'This is a short description of this extension.
+It is shown in [[Special:Version]].
+Do not translate link targets, and tag names.',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'fundraiserportal' => 'Партал ахвяраваньняў',
+	'fundraiserportal-desc' => 'Дадае партал «калі ласка, ахвяруйце» у верхняй частцы панэлі.',
+);
+
+/** German (Deutsch) */
+$messages['de'] = array(
+	'fundraiserportal' => 'Sponsoren-Portal',
+	'fundraiserportal-desc' => 'Fügt am Kopf der Seitenleiste einen Link auf das „Bitte spenden“-Portal hinzu',
+);
+
+/** Spanish (Español)
+ * @author Crazymadlover
+ */
+$messages['es'] = array(
+	'fundraiserportal' => 'Portal de recaudador de fondos',
+);
+
+/** Finnish (Suomi)
+ * @author Crt
+ */
+$messages['fi'] = array(
+	'fundraiserportal' => 'Keräysportaali',
+	'fundraiserportal-desc' => 'Lisää lahjoitusportaalin sivupalkin yläriviin.',
+);
+
+/** French (Français)
+ * @author Crochet.david
+ */
+$messages['fr'] = array(
+	'fundraiserportal' => 'Portail de dons',
+	'fundraiserportal-desc' => 'Ajoute un portail pour « Faire un don » vers le haut de la barre latérale.',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'fundraiserportal' => 'Portal de recadación de fondos',
+	'fundraiserportal-desc' => 'Engade un portal "por favor, doa" no canto superior da barra lateral.',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'fundraiserportal' => 'Fundraiser-Portal',
+	'fundraiserportal-desc' => 'Fiegt e „Bitte Spände“-Portal obe in d Syteleischte yy',
+);
+
+/** Hiligaynon (Ilonggo)
+ * @author Tagimata
+ */
+$messages['hil'] = array(
+	'fundraiserportal' => 'Alamotay Portal',
+	'fundraiserportal-desc' => 'Mga pasayod "palihog amot" portal ay ara sa ibabaw sang sidebar.',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'fundraiserportal' => 'Portal za darjenske kampanje',
+	'fundraiserportal-desc' => 'Přidawa portal za "darjenske akcije" horjeka na pobóčnej lajsće.',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'fundraiserportal' => 'Portal pro collecta de fundos',
+	'fundraiserportal-desc' => 'Adde un portal "per favor dona" in alto del barra lateral.',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Bennylin
+ */
+$messages['id'] = array(
+	'fundraiserportal' => 'Portal Penggalangan Dana',
+	'fundraiserportal-desc' => 'Menambahkan portal "menyumbang" di bagian atas kotak samping.',
+);
+
+/** Italian (Italiano)
+ * @author Darth Kule
+ */
+$messages['it'] = array(
+	'fundraiserportal' => 'Portale raccolta fondi',
+	'fundraiserportal-desc' => 'Aggiunge un portale "Fai una donazione" nella parte superiore della barra laterale.',
+);
+
+/** Japanese (日本語)
+ * @author Aotake
+ */
+$messages['ja'] = array(
+	'fundraiserportal' => '資金調達ポータル',
+	'fundraiserportal-desc' => '「寄付をお願いします」ポータルをサイドバー上部に追加する',
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'fundraiserportal' => 'Pooz för Jeld ze sammelle',
+	'fundraiserportal-desc' => 'Deiht ene „Donn spende!“ Ennjangs-Lengk bovve aan de Sigg dobei.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'fundraiserportal-desc' => 'Setzt e "Maacht en Don"-Portal uewen op d\'Sidebar derbäi.',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'fundraiserportal' => 'Fondswervingsportaal',
+	'fundraiserportal-desc' => 'Voegt een "Doneren"-portaal toe bovenaan de menubalk',
+);
+
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'fundraiserportal' => 'Portal de dons',
+	'fundraiserportal-desc' => 'Apond un portal per « Far un don » cap al naut de la barra laterala.',
+);
+
+/** Russian (Русский)
+ * @author Ferrer
+ */
+$messages['ru'] = array(
+	'fundraiserportal' => 'Портал пожертвований',
+	'fundraiserportal-desc' => 'Добавляет «просьбу пожертвовать» наверх боковой панели.',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'fundraiserportal' => 'Portál fundraisingu',
+	'fundraiserportal-desc' => 'Pridáva portál „Prosím, prispejte“ na vrch bočného panelu.',
+);
+
+/** Turkish (Türkçe)
+ * @author Joseph
+ */
+$messages['tr'] = array(
+	'fundraiserportal' => 'Fonsağlayıcı Portalı',
+	'fundraiserportal-desc' => 'Kenar çubuğunun en üstüne bir "lütfen bağış yapın" portalı ekler.',
+);
+
