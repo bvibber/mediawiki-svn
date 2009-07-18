1b40008665f416176e72d363dc9fd9453df4613d
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-18 16:40:29 +0000

    (bug 19665) new extension: cooperation statstics
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53449 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CooprationStatistics/CooperationStatistics.alias.php
@@ -0,0 +1,14 @@
+﻿<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$aliases = array();
+
+/** English
+ *
+ */
+$aliases['en'] = array(
+	'CooperationStatistics' => array( 'CooperationStatistics' ),
+);
---
+++ extensions/CooprationStatistics/CooperationStatistics.i18n.php
@@ -0,0 +1,37 @@
+﻿<?php
+/**
+ * Internationalisation file for extension cooperationstatistics.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'cooperationstatistics'				=> 'Cooperation statistics',
+	'cooperationstatistics-desc'		=> 'Show [[Special:CooperationStatistics|cooperation statistics on main namespace]].',
+	'cooperationstatistics-text'		=> 'Show cooperation statistics on main namespace. (From [[MediaWiki:cooperationstatistics-limit-few-revisors|{{MediaWiki:cooperationstatistics-limit-few-revisors}}]] to [[MediaWiki:cooperationstatistics-limit-many-revisors|{{MediaWiki:cooperationstatistics-limit-many-revisors}}+]] revisors)' .
+											'<br>See also the [[Special:MostRevisors|\'\'\'pages with the most revisors\'\'\']] and [[Special:MostRevisions|with the most revisions]].',	
+	'cooperationstatistics-tablearticle'	=> 'Article count',
+	'cooperationstatistics-tablevalue'		=> 'Number of editors',
+	'cooperationstatistics-users'			=> 'editors',
+	'cooperationstatistics-articles'			=> '$1 articles',
+	'cooperationstatistics-nbusers'	=> 'have $1 editors',
+	'cooperationstatistics-nblessusers'	=> 'have $1 or less editors',
+	'cooperationstatistics-nbmoreusers'	=> 'have $1 or more editors',
+	'cooperationstatistics-legendmore'	=> ' or more editors.',
+	'cooperationstatistics-limit-few-revisors'	=> '1',
+	'cooperationstatistics-limit-many-revisors' => '5',
+);
+
+/** Message documentation (Message documentation)
+ * @author Fryed-peach
+ * @author Jon Harald SÃ¸by
+ * @author Lejonel
+ * @author Purodha
+ * @author Siebrand
+ */
+$messages['qqq'] = array(
+	'cooperationstatistics' => '{{Identical|Most Revisors}}',
+	'cooperationstatistics-desc' => 'Short description of the extension, shown on [[Special:Version]].',
+);
