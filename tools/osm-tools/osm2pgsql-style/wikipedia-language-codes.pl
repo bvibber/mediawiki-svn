effbd2c4258b924c00582d8975494e81f5e594bd
avar <avar@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-18 19:00:48 +0000

    A directory for the tools I'm writing to import OSM on Cassini
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53452 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ tools/osm-tools/osm2pgsql-style/wikipedia-language-codes.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+wikipedia-language-codes - Get the list of language codes currently used on Wikipedia from Special:SiteMatrix
+
+=head1 SYNOPSIS
+
+    # Spew out a list of Wikipedia language codes and corresponding languages 
+    wikipedia-language-codes
+
+=head1 OPTIONS
+
+=over
+
+=item -h, --help
+
+Print a usage message listing all available options
+
+=item --url
+
+The URL to the SiteMatrix,
+L<http://en.wikipedia.org/wiki/Special:SiteMatrix> by default.
+
+=head1 AUTHOR
+
+E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avarab@gmail.com>
+
+=cut
+
+use strict;
+use warnings;
+
+use WWW::Mechanize;
+use HTML::TableParser::Grid;
+use Encode qw(encode decode);
+use YAML::Syck qw(Dump);
+
+use Getopt::Long;
+use Pod::Usage ();
+
+#
+# Get command line options
+#
+
+Getopt::Long::Parser->new(
+	config => [ qw< bundling no_ignore_case no_require_order > ],
+)->getoptions(
+	'h|help' => \my $help,
+	'url=s' => \(my $url = 'http://en.wikipedia.org/wiki/Special:SiteMatrix'),
+) or help();
+
+help() if $help;
+
+#
+# main
+#
+
+my %matrix = parse_sitematrix();
+
+print Dump \%matrix;
+
+exit 0;
+
+sub parse_sitematrix
+{
+    my $content = get_sitematrix();
+
+    my $parser = HTML::TableParser::Grid->new($content);
+
+    my %lang;
+
+    for my $n (0 .. $parser->num_rows - 1) {
+        my %row;
+        @row{qw(language code)} = $parser->row($n);
+
+        # Mark this as UTF-8
+        for my $key (keys %row) {
+            $row{$key} = encode('utf8', $row{$key});
+        }
+
+        next if $row{language} eq 'Total';
+
+        $lang{$row{code}} = $row{language};
+    }
+
+    return %lang;
+}
+
+sub get_sitematrix
+{
+    my $mech = WWW::Mechanize->new(
+        agent => $0,
+    );
+
+    $mech->get($url);
+
+    unless ($mech->success)
+    {
+        die "Can't get $url";
+    }
+
+    my $content = $mech->content;
+
+    return $content;
+}
+
+sub help
+{
+    require Pod::Usage;
+    my %arg = @_;
+
+    Pod::Usage::pod2usage(
+        -verbose => $arg{ verbose },
+        -exitval => $arg{ exitval } || 0,
+    );
+}
