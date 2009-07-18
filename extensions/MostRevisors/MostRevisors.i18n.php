2903ce2707e5433bd162fcb6daf7129f272d366d
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-18 16:24:01 +0000

    (bug 19632) Commit extension MostRevisors.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53448 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/MostRevisors/MostRevisors.i18n.php
@@ -0,0 +1,31 @@
+﻿<?php
+/**
+ * Internationalisation file for extension mostrevisors.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'mostrevisors-header'     => "'''This page lists the {{PLURAL:$1|page|$1 pages}} with most revisors on the wiki.'''",
+	'mostrevisors-limitlinks' => 'Show up to $1 pages',
+	'mostrevisors-namespace'  => 'Namespace:',
+	'mostrevisors-none'       => 'No entries were found.',
+	'mostrevisors-ns-header'  => "'''This page lists the {{PLURAL:$1|page|$1 pages}} with most revisors in the $2 namespace.'''",
+	'mostrevisors-showing'    => 'Listing {{PLURAL:$1|page|$1 pages}}:',
+	'mostrevisors-submit'     => 'Go',
+	'mostrevisors-showredir'  => 'Show redirect pages',
+	'mostrevisors-hideredir'  => 'Hide redirect pages',
+	'mostrevisors'                => 'Pages with the most revisors',
+	'mostrevisors-desc'           => 'List [[Special:MostRevisors|pages with the most revisors]]',
+	'mostrevisors-viewcontributors' => 'View main contributors',
+	'mostrevisors-text'           => 'Show Pages with the most revisors, starting from [[MediaWiki:mostrevisors-limit-few-revisors|{{MediaWiki:mostrevisors-limit-few-revisors}} revisors]]. See also [[Special:MostRevisions]].',	
+	'mostrevisors-users'          => 'editors',
+	'mostrevisors-limit-few-revisors' => '1',
+);
+
+$messages['qqq'] = array(
+	'mostrevisors' => '{{Identical|Most Revisors}}',
+	'mostrevisors-desc' => 'Short description of the extension, shown on [[Special:Version]].',
+);
