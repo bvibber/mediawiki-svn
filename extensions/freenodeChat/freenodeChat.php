714c6f69911fa7fc338d2831f9c5cb7b2db969fb
roberthl <roberthl@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-23 12:55:32 +0000

    freenodeChat: * Various updates and cleanup.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53673 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/freenodeChat/freenodeChat.php
+++ extensions/freenodeChat/freenodeChat.php
@@ -20,7 +20,7 @@ $wgExtensionCredits[ 'specialpage' ][] = array(
 	'name'           => 'Freenode Chat',
 	'description'    => 'Adds a special page used to chat in real time with other wiki users (using freenode web IRC client).',
 	'descriptionmsg' => 'freenodechat-desc',
-	'version'        => '0.1.1',
+	'version'        => '1.0.0',
 	'author'         => array( 'Robert Leverington', 'Marco 27' )
 );
 
@@ -35,8 +35,12 @@ $wgAutoloadClasses[ 'FreenodeChat' ] = $dir . 'freenodeChat_body.php';
 $wgExtensionMessagesFiles[ 'FreenodeChat' ] =  $dir . 'freenodeChat.i18n.php';
 $wgExtensionAliasesFiles[ 'FreenodeChat' ] =  $dir . 'freenodeChat.alias.php';
 
+// Default configuration.
+$wgFreenodeChatChannel         = '#mediawiki-i18n';
+$wgFreenodeChatExtraParameters = array();
+
 // Permissions.
 $wgAvailableRights[] = 'freenodechat';
-$wgGroupPermissions[ '*'     ][ 'freenodechat' ] = true;
+$wgGroupPermissions[ '*'     ][ 'freenodechat' ] = false;
 $wgGroupPermissions[ 'user'  ][ 'freenodechat' ] = true;
 $wgGroupPermissions[ 'sysop' ][ 'freenodechat' ] = true;
--- extensions/freenodeChat/freenodeChat_body.php
+++ extensions/freenodeChat/freenodeChat_body.php
@@ -12,7 +12,7 @@ class FreenodeChat extends SpecialPage {
 
 	function execute( $par ) {
 		global $wgOut, $wgUser;
-		global $wgFreenodeChannel;
+		global $wgFreenodeChatChannel, $wgFreenodeChatExtraParameters;
 
 		// Preperation.
 		wfLoadExtensionMessages( 'FreenodeChat' );
@@ -22,13 +22,12 @@ class FreenodeChat extends SpecialPage {
 		$wgOut->addWikiMsg( 'freenodechat-header' );
 
 		// Prepare query string to pass to widget.
-		$queryAssoc = array(
-			'channels' => $wgFreenodeChannel
-		);
-
-        if ( $wgUser->IsLoggedIn() ) $queryAssoc[ 'nick' ] = str_replace( ' ', '_', $wgUser->getName() );
-
-        foreach ( $queryAssoc as $parameter => $value ) {
+		$queryAssoc = array( 'channels' => $wgFreenodeChatChannel );
+		if ( $wgUser->IsLoggedIn() ) $queryAssoc[ 'nick' ] = str_replace( ' ', '_', $wgUser->getName() );
+		if ( $wgFreenodeChatExtraParameters ) {
+			$queryAssoc = array_merge( $queryAssoc, $wgFreenodeChatExtraParameters );
+		}
+		foreach ( $queryAssoc as $parameter => $value ) {
 			$query[] = $parameter . '=' . urlencode( $value );
 		}
 		$queryString = implode( '&', $query );
@@ -37,7 +36,24 @@ class FreenodeChat extends SpecialPage {
 		$wgOut->addHTML( Xml::openElement( 'iframe', array(
 			'width'     => '1000',
 			'height'    => '500',
+			'scrolling' => 'no',
+			'border'    => '0',
+			'onLoad'    => 'freenodeChatExpand( this )',
 			'src'       => 'http://webchat.freenode.net/?' . $queryString
 		) ) . Xml::closeElement( 'iframe' ) );
+
+		// Hack to make the chat area a reasonable size.
+		$wgOut->addHTML( Xml::tags( 'script',
+			array( 'type' => 'text/javascript' ),
+'/* <![CDATA[ */
+function freenodeChatExpand( elem ) {
+	height = elem.height;
+	width  = elem.width;
+	elem.height = screen.height - 500;
+	elem.width  = screen.width  - 250;
+}
+/* ]]> */'
+			) );
+
 	}
 }
