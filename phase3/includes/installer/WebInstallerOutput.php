d0c024b63bb9ef606e9e9b422d7fde455945e807
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-26 17:23:21 +0000

    Add closing to <meta>
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-installer@53783 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/installer/WebInstallerOutput.php
+++ phase3/includes/installer/WebInstallerOutput.php
@@ -77,7 +77,7 @@ class WebInstallerOutput {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
-	<meta name="robots" content="noindex, nofollow">
+	<meta name="robots" content="noindex, nofollow" />
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<title>MediaWiki <?php echo( htmlspecialchars( $wgVersion ) ); ?> Installation</title>
 	<link rel="stylesheet" type="text/css" href="../skins/monobook/main.css"/>
@@ -144,7 +144,7 @@ class WebInstallerOutput {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<meta name="robots" content="noindex, nofollow">
+	<meta name="robots" content="noindex, nofollow" />
 	<title>MediaWiki <?php echo( htmlspecialchars( $wgVersion ) ); ?> Installation</title>
 	<link rel="stylesheet" type="text/css" href="../skins/monobook/main.css"/>
 	<link rel="stylesheet" type="text/css" href="../skins/common/config.css"/>
