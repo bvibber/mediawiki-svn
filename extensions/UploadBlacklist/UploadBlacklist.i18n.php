140d536b6965ba6c813b751e5192f30ee00ec99b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-27 19:51:53 +0000

    * Add extension credits * Add i18n file * Remove trailing whitespaces * Add extension to translatewiki.net
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@53834 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UploadBlacklist/UploadBlacklist.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation file for the User Merge and Delete Extension.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+$messages['en'] = array(
+	'uploadblacklist-desc' => 'Adds an upload blacklist functionality',
+);
+
+/** Message documentation (Message documentation)
+ * @author Raymond
+ */
+$messages['qqq'] = array(
+	'uploadblacklist-desc' => '{{desc}}',
+);
+
