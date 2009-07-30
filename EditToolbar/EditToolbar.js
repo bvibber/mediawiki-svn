0bea5f1866fc03ce049d53d9c4cf1fd3efecda36
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-30 21:55:53 +0000

    EditToolbar: Switch some Hebrew chars around per Simetrical
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@54050 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditToolbar/EditToolbar.js
+++ EditToolbar/EditToolbar.js
@@ -629,10 +629,10 @@ var editToolbarConfiguration = {
 				'characters': generateSpecialChars( [ "\u05d0",
 					"\u05d1", "\u05d2", "\u05d3", "\u05d4",
 					"\u05d5", "\u05d6", "\u05d7", "\u05d8",
-					"\u05d9", "\u05da", "\u05db", "\u05dc",
-					"\u05dd", "\u05de", "\u05df", "\u05e0",
-					"\u05e1", "\u05e2", "\u05e3", "\u05e4",
-					"\u05e5", "\u05e6", "\u05e7", "\u05e8",
+					"\u05d9", "\u05db", "\u05da", "\u05dc",
+					"\u05de", "\u05dd", "\u05e0", "\u05df",
+					"\u05e1", "\u05e2", "\u05e4", "\u05e3",
+					"\u05e6", "\u05e5", "\u05e7", "\u05e8",
 					"\u05e9", "\u05ea", "\u05f3", "\u05f4",
 					"\u05f0", "\u05f1", "\u05f2" ] )
 			}
