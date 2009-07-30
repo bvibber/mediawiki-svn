ec1e3c386218227a7944b8f46b16619e8f755734
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-30 01:06:20 +0000

    Tag for version 0.2
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@53998 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticMaps/REL_0_2/YahooMaps/SM_YahooMapsFormInput.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+* Form input hook that adds an Yahoo! Maps map format to Semantic Forms
+ *
+ * @file SM_YahooMapsFormInput.php
+ * @ingroup SemanticMaps
+ * 
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class SMYahooMapsFormInput extends SMFormInput {
+	
+	/**
+	 * @see SMFormInput::setFormInputSettings()
+	 *
+	 */
+	protected function setFormInputSettings() {
+		global $egMapsYahooMapsZoom;
+		
+		$this->elementNamePrefix = 'map_yahoo';
+		$this->showAddresFunction = 'showYAddress';		
+
+		$this->earthZoom = 17;
+		$this->defaultZoom = $egMapsYahooMapsZoom;			
+	}	
+	
+	/**
+	 * @see SMFormInput::doMapServiceLoad()
+	 *
+	 */
+	protected function doMapServiceLoad() {
+		global $egYahooMapsOnThisPage;
+		
+		if (empty($egYahooMapsOnThisPage)) {
+			$egYahooMapsOnThisPage = 0;
+			MapsYahooMaps::addYMapDependencies($this->formOutput);
+		}
+		$egYahooMapsOnThisPage++;			
+		
+		$this->elementNr = $egYahooMapsOnThisPage;
+	}	
+	
+	/**
+	 * @see SMFormInput::addSpecificFormInputHTML()
+	 *
+	 */
+	protected function addSpecificFormInputHTML() {
+		global $wgJsMimeType;
+		
+		$type = MapsYahooMaps::getYMapType($this->type);
+		
+		$controlItems = MapsYahooMaps::createControlsString($this->controls);		
+		
+		$width = $this->width . 'px';
+		$height = $this->height . 'px';		
+		
+		$this->formOutput .="
+		<div id='".$this->mapName."' style='width: $width; height: $height;'></div>  
+		
+		<script type='$wgJsMimeType'>/*<![CDATA[*/
+		addLoadEvent(makeFormInputYahooMap('".$this->mapName."', '".$this->coordsFieldName."', ".$this->centre_lat.", ".$this->centre_lon.", ".$this->zoom.", ".$this->marker_lat.", ".$this->marker_lon.", $type, [$controlItems], ".$this->autozoom."));
+		/*]]>*/</script>";		
+	}
+	
+	/**
+	 * @see SMFormInput::manageGeocoding()
+	 *
+	 */
+	protected function manageGeocoding() {
+		global $egYahooMapsKey;
+		$this->enableGeocoding = strlen(trim($egYahooMapsKey)) > 0;
+		if ($this->enableGeocoding) MapsYahooMaps::addYMapDependencies($this->formOutput);			
+	}
+
+	
+}
