0b9406e007602aec5015ea6a31aa6028a0bac32f
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-30 17:51:56 +0000

    Tag for version 0.2.1
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@54032 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticMaps/REL_0_2_1/GoogleMaps/SM_GoogleMapsFormInput.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * A class that holds static helper functions and extension hooks for the Google Maps service
+ *
+ * @file SM_GoogleMapsFormInput.php
+ * @ingroup SemanticMaps
+ * 
+ * @author Robert Buzink
+ * @author Yaron Koren
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class SMGoogleMapsFormInput extends SMFormInput {
+
+	/**
+	 * @see SMFormInput::setFormInputSettings()
+	 *
+	 */
+	protected function setFormInputSettings() {
+		global $egMapsGoogleMapsZoom;
+		
+		$this->elementNamePrefix = 'map_google';
+		$this->showAddresFunction = 'showGAddress';	
+
+		$this->earthZoom = 1;
+		$this->defaultZoom = $egMapsGoogleMapsZoom;
+	}
+	
+
+	/**
+	 * @see SMFormInput::doMapServiceLoad()
+	 *
+	 */
+	protected function doMapServiceLoad() {
+		global $egGoogleMapsOnThisPage;
+
+		if (empty($egGoogleMapsOnThisPage)) {
+			$egGoogleMapsOnThisPage = 0;
+			MapsGoogleMaps::addGMapDependencies($this->formOutput);
+		}
+		
+		$egGoogleMapsOnThisPage++;	
+		
+		$this->elementNr = $egGoogleMapsOnThisPage;
+	}
+	
+	/**
+	 * @see SMFormInput::addSpecificFormInputHTML()
+	 *
+	 */
+	protected function addSpecificFormInputHTML() {
+		global $wgJsMimeType;
+		
+		$enableEarth = $this->mapProperties['earth'] == 'on' || $this->mapProperties['earth'] == 'yes';
+		$earth = $enableEarth ? 'true' : 'false';
+		
+		$this->type = MapsGoogleMaps::getGMapType($this->type, $enableEarth);
+		$control = MapsGoogleMaps::getGControlType($this->controls);		
+		
+		$this->formOutput .= "
+		<div id='".$this->mapName."' class='".$this->class."'></div>
+	
+		<script type='$wgJsMimeType'>/*<![CDATA[*/
+		addLoadEvent(makeFormInputGoogleMap('".$this->mapName."', '".$this->coordsFieldName."', ".$this->width.", ".$this->height.", ".$this->centre_lat.", ".$this->centre_lon.", ".$this->zoom.", ".$this->marker_lat.", ".$this->marker_lon.", ".$this->type.", new $control(), ".$this->autozoom.", $earth));
+		window.unload = GUnload;
+		/*]]>*/</script>";
+	}
+	
+	/**
+	 * @see SMFormInput::manageGeocoding()
+	 *
+	 */
+	protected function manageGeocoding() {
+		global $egGoogleMapsKey;
+		$this->enableGeocoding = strlen(trim($egGoogleMapsKey)) > 0;
+		if ($this->enableGeocoding) MapsGoogleMaps::addGMapDependencies($this->formOutput);		
+	}	
+	
+}
