569af8aead4d8b52ace2a5ce06719b416cc9a81b
roberthl <roberthl@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-30 22:38:28 +0000

    WebChat, freenodeChat, Mibbit: * Create WebChat extension, a widget-agnostic version of the freenodeChat and Mibbit extensions. * Mark the freenodeChat and Mibbit extensions as obsolete.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54055 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/WebChat/WebChat.i18n.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Internationalisation file for Mibbit extension.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Robert Leverington <robert@rhl.me.uk>
+ */
+$messages['en'] = array(
+	'webchat'       => 'Web chat',
+	'webchat-desc'   => 'Adds a [[Special:WebChat|special page]] used to chat in real time with other wiki users',
+	'webchat-header' => 'This special page allows you to collaborate in real time with other users of the wiki.
+To connect simply press the "Connect" button.
+If you are logged in your nickname will have been prefilled.
+Anonymous users are given an automatically generated one, but it can be changed.',
+	'right-webchat'  => 'Allowed to use web chat',
+);
