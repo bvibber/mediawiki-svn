96a2215bf29a5fb5f7e958ffa346ce7767e5c426
roberthl <roberthl@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-31 09:55:23 +0000

    MultiBoilerplate: * Integrate patch by Al Maghi, adds a special page listing boilerplates.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54083 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MultiBoilerplate/MultiBoilerplate.i18n.php
+++ extensions/MultiBoilerplate/MultiBoilerplate.i18n.php
@@ -17,6 +17,15 @@ $messages['en'] = array(
 	'multiboilerplate-label'  => '',
 	'multiboilerplate-legend' => 'Select boilerplate',
 	'multiboilerplate-submit' => 'Load',
+	'boilerplates'            => 'Custom preload on creation',
+	'boilerplates-name'       => 'Boilerplates',
+	'multiboilerplate-special-title'                   => 'Custom templates preloader',
+	'multiboilerplate-special-pagetext'                => 'Those templates can be preloaded on page creation. (<span class=plainlinks>[{{fullurl:This page does not exist|action=edit}} demo]</span>)',
+	'multiboilerplate-special-define-on-localsettings' => 'Boilerplates are defined on LocalSettings.php in array <code>&#36;wgMultiBoilerplateOptions</code>. Set <code>&#36;wgMultiBoilerplateOptions = false;</code> to use [[MediaWiki:Multiboilerplate]] instead.',
+	'multiboilerplate-special-define-on-interface'     => 'Boilerplates are defined on interface page: [[MediaWiki:Multiboilerplate]].',
+	'multiboilerplate-label'                           => '',
+	'multiboilerplate-legend'                          => 'Select boilerplate',
+	'multiboilerplate-submit'                          => 'Load',
 );
 
 /** Message documentation (Message documentation)
@@ -30,6 +39,7 @@ $messages['qqq'] = array(
 	'multiboilerplate-submit' => 'The text displayed on the button that loads the boilerplate into the edit box.
 
 {{Identical|Load}}',
+	'boilerplates' => 'The description on Special:SpecialPages',
 );
 
 /** Afrikaans (Afrikaans)
