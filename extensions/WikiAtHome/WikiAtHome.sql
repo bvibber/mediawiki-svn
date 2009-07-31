6dc06a03a8722adccafadbb5cff6a5b527625697
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-07-31 09:36:29 +0000

    updated schema
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54080 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiAtHome/WikiAtHome.sql
+++ extensions/WikiAtHome/WikiAtHome.sql
@@ -2,9 +2,11 @@
 -- Table structure for table `wah_jobqueue`
 --
 
+DROP TABLE IF EXISTS `wah_jobqueue`;
 CREATE TABLE IF NOT EXISTS `wah_jobqueue` (
   `job_id` int(12) unsigned NOT NULL auto_increment,
   `job_set_id` int(12) unsigned NOT NULL,
+  `job_order_id` int(5) NOT NULL,
   `job_last_assigned_time` int(14) default NULL,
   `job_last_assigned_user_id` int(11) unsigned default NULL,
   `job_done_time` int(14) default NULL,
@@ -12,9 +14,9 @@ CREATE TABLE IF NOT EXISTS `wah_jobqueue` (
   `job_assign_count` int(4) unsigned NOT NULL default '0',
   `job_json` blob NOT NULL,
   PRIMARY KEY  (`job_id`),
-  KEY `job_set_id` (`job_set_id`),
-  KEY `job_last_assigned_user_id` (`job_last_assigned_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `job_last_assigned_user_id` (`job_last_assigned_user_id`),
+  KEY `job_set_id` (`job_set_id`,`job_order_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -22,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `wah_jobqueue` (
 -- Table structure for table `wah_jobset`
 --
 
+DROP TABLE IF EXISTS `wah_jobset`;
 CREATE TABLE IF NOT EXISTS `wah_jobset` (
   `set_id` int(10) unsigned NOT NULL auto_increment,
   `set_namespace` int(11) default NULL,
@@ -35,4 +38,4 @@ CREATE TABLE IF NOT EXISTS `wah_jobset` (
   KEY `set_namespace` (`set_namespace`,`set_title`),
   KEY `set_done_time` (`set_done_time`),
   KEY `set_client_count` (`set_client_count`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
