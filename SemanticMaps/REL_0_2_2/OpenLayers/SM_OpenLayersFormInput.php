b0ce21f84effcde1c0257d8e22f3b1a17b2ec93e
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-01 18:13:48 +0000

    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@54166 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticMaps/REL_0_2_2/OpenLayers/SM_OpenLayersFormInput.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Form input hook that adds an Open Layers map format to Semantic Forms
+ *
+ * @file SM_OpenLayersFormInput.php
+ * @ingroup SemanticMaps
+ * 
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class SMOpenLayersFormInput extends SMFormInput {
+	
+	/**
+	 * @see SMFormInput::setFormInputSettings()
+	 *
+	 */
+	protected function setFormInputSettings() {
+		global $egMapsOpenLayersZoom;
+		
+		$this->elementNamePrefix = 'open_layer';
+		$this->showAddresFunction = 'showOLAddress';	
+
+		$this->earthZoom = 1;
+		$this->defaultZoom = $egMapsOpenLayersZoom;		
+	}	
+	
+	/**
+	 * @see SMFormInput::doMapServiceLoad()
+	 *
+	 */
+	protected function doMapServiceLoad() {
+		global $egOpenLayersOnThisPage;
+		
+		MapsOpenLayers::addOLDependencies($this->formOutput);
+		$egOpenLayersOnThisPage++;	
+
+		$this->elementNr = $egOpenLayersOnThisPage;
+	}	
+	
+	/**
+	 * @see SMFormInput::addSpecificFormInputHTML()
+	 *
+	 */
+	protected function addSpecificFormInputHTML() {
+		global $wgJsMimeType;
+		
+		$controlItems = MapsOpenLayers::createControlsString($this->controls);
+		
+		$layerItems = MapsOpenLayers::createLayersStringAndLoadDependencies($this->formOutput, $this->mapProperties['layers']);	
+		
+		$width = $this->width . 'px';
+		$height = $this->height . 'px';			
+		
+		$this->formOutput .="
+		<div id='".$this->mapName."' style='width: $width; height: $height; background-color: #cccccc;'></div>  
+		
+		<script type='$wgJsMimeType'>/*<![CDATA[*/
+		addLoadEvent(makeFormInputOpenLayer('".$this->mapName."', '".$this->coordsFieldName."', ".$this->centre_lat.", ".$this->centre_lon.", ".$this->zoom.", ".$this->marker_lat.", ".$this->marker_lon.", [$layerItems], [$controlItems]));
+		/*]]>*/</script>";			
+	}
+	
+	/**
+	 * @see SMFormInput::manageGeocoding()
+	 *
+	 */
+	protected function manageGeocoding() {	
+		$this->enableGeocoding = false;
+	}
+	
+}
