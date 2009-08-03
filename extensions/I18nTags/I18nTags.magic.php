c40080d562de83048d4eebf325ef241df950ac65
nikerabbit <nikerabbit@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-03 13:03:36 +0000

    * Localisation updates from translatewiki.net (testing magic words export)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54276 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/I18nTags/I18nTags.magic.php
+++ extensions/I18nTags/I18nTags.magic.php
@@ -5,3 +5,11 @@ $magicWords = array();
 $magicWords['en'] = array(
 	'languagename' => array( 0, 'languagename' ),
 );
+
+$magicWords['ar'] = array(
+	'languagename'   => array( '0', 'اسم_لغة', 'اسم_اللغة', 'languagename' ),
+);
+
+$magicWords['ksh'] = array(
+	'languagename'   => array( '0', 'ShproocheName', 'languagename' ),
+);
