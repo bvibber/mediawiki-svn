a9c73fde04150c76bc6175279c929e4e32fea155
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-05 22:44:49 +0000

    Merge fatal error fix from trunk r52879
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@54493 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/Skin.php
+++ includes/Skin.php
@@ -1439,7 +1439,8 @@ END;
 			return $out;
 		}
 		// Allow for site and per-namespace customization of copyright notice.
-		wfRunHooks( 'SkinCopyrightFooter', array( $wgArticle->getTitle(), $type, &$msg, &$link ) );
+		if( isset($wgArticle) )
+			wfRunHooks( 'SkinCopyrightFooter', array( $wgArticle->getTitle(), $type, &$msg, &$link ) );
 		
 		$out .= wfMsgForContent( $msg, $link );
 		return $out;
