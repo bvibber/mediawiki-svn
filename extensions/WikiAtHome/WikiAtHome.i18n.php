d40e2f557a8dd90f89fad44a1b36625f4689d733
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-06 23:41:27 +0000

    Update formatting
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54551 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiAtHome/WikiAtHome.i18n.php
+++ extensions/WikiAtHome/WikiAtHome.i18n.php
@@ -12,26 +12,27 @@ $messages = array();
  * @author Purodha 	http://ksh.wikipedia.org/wiki/User:Purodha
  */
 $messages['en'] = array(
-	'specialwikiathome'	=> 'Wiki@Home',
-	'wah-desc'			=> 'Enables distributing transcoding video jobs to clients using firefogg',
-	'wah-user-desc'		=> 'Wiki@Home enables community members to donate spare cpu cycles to help with resource intensive operations',
-	'wah-short-audio'	=> '$1 sound file, $2',
-	'wah-short-video'	=> '$1 video file, $2',
-	'wah-short-general'	=> '$1 media file, $2',
+	'specialwikiathome' => 'Wiki@Home',
+	'wah-desc' => 'Enables distributing transcoding video jobs to clients using firefogg',
+	'wah-user-desc' => 'Wiki@Home enables community members to donate spare cpu cycles to help with resource intensive operations',
+	'wah-short-audio' => '$1 sound file, $2',
+	'wah-short-video' => '$1 video file, $2',
+	'wah-short-general' => '$1 media file, $2',
 
-	'wah-long-audio'       	=> '($1 sound file, length $2, $3)',
-	'wah-long-video'       	=> '($1 video file, length $2, $4×$5 pixels, $3)',
-	'wah-long-multiplexed' 	=> '(multiplexed audio/video file, $1, length $2, $4×$5 pixels, $3 overall)',
-	'wah-long-general'     	=> '(media file, length $2, $3)',
-	'wah-long-error'       	=> '(ffmpeg could not read this file: $1)',
+	'wah-long-audio' => '($1 sound file, length $2, $3)',
+	'wah-long-video' => '($1 video file, length $2, $4×$5 pixels, $3)',
+	'wah-long-multiplexed' => '(multiplexed audio/video file, $1, length $2, $4×$5 pixels, $3 overall)',
+	'wah-long-general' => '(media file, length $2, $3)',
+	'wah-long-error' => '(ffmpeg could not read this file: $1)',
 
 	'wah-transcode-working' => 'This video is being processed, please try again later',
-	'wah-transcode-helpout' => 'The clip is $1 percent done. You can help transcode this video by visiting [[Special:WikiAtHome|Wiki@Home]]',
+	'wah-transcode-helpout' => 'The clip is $1 percent done.
+You can help transcode this video by visiting [[Special:WikiAtHome|Wiki@Home]]',
 
-	'wah-transcode-fail'	=> 'This file failed to transcode.',
+	'wah-transcode-fail' => 'This file failed to transcode.',
 
-	'wah-javascript-off'	=> 'You must have JavaScript enabled to participate in Wiki@Home',
-	'wah-loading'		=> 'loading Wiki@Home interface <blink>...</blink>'
+	'wah-javascript-off' => 'You must have JavaScript enabled to participate in Wiki@Home',
+	'wah-loading' => 'loading Wiki@Home interface <blink>...</blink>'
 );
 
 /** Message documentation (Message documentation)
