2dcd351f2074c1055fa6fddf1bd43093a3f71aa8
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-07 20:18:05 +0000

    Merge r53427, r53344 from trunk... * fix for tooltips in vector * plainlinks class to common styles, so now included in Vector
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@54598 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1501,7 +1501,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '230';
+$wgStyleVersion = '233x';
 
 
 # Server-side caching:
