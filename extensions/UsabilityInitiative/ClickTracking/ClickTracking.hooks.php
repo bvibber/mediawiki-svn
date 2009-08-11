0fbae962d2d275ca9c9af2d7057fa16a26920232
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-11 22:45:26 +0000

    Created backend for click tracking
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54821 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/ClickTracking/ClickTracking.hooks.php
+++ extensions/UsabilityInitiative/ClickTracking/ClickTracking.hooks.php
@@ -1,1 +1,54 @@
 <?php
+
+/**
+ * Hooks for Usability Initiative ClickTracking extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+class ClickTrackingHooks {
+
+	/* Static Functions */
+	public static function schema() {
+		global $wgExtNewTables, $wgExtNewFields;
+		
+		$wgExtNewTables[] = array(
+			'click_tracking',
+			dirname( __FILE__ ) . '/ClickTracking.sql'
+		);
+		
+		return true;
+	}
+
+	/**
+	 * Track particular event
+	 * @param $is_logged_in whether or not the user is logged in
+	 * @param $namespace namespace the user is editing
+	 * @param $event_id event type
+	 * @param $contribs contributions the user has made (or NULL if user not logged in)
+	 * @return true if the event was stored in the DB
+	 */
+	public static function trackEvent($is_logged_in, $namespace, $event_id, $contribs=-1){
+		
+		$dbw = wfGetDB( DB_MASTER );
+		if ($contribs < 0) { //meaning the user is not logged in
+			$contribs = "NULL"; 
+		}
+		
+		$dbw->begin();
+		// Builds insert information
+		$data = array(
+			'is_logged_in' => $is_logged_in,
+			'namespace' => (int) $namespace,
+			'event_id' => (int) $event_id,
+			'user_contribs' => $contribs
+		);
+		
+		$db_status = $dbw->insert('click_tracking', $data, __METHOD__);
+		$dbw->commit();
+		return $db_status;
+	}
+	
+	
+}
--- extensions/UsabilityInitiative/ClickTracking/ClickTracking.php
+++ extensions/UsabilityInitiative/ClickTracking/ClickTracking.php
@@ -1,1 +1,44 @@
 <?php
+/**
+ * Usability Initiative Click Tracking extension
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * This file contains the include file for the Click Tracking portion of the
+ * UsabilityInitiative extension of MediaWiki.
+ *
+ * Usage: Include the following line in your LocalSettings.php
+ * require_once( "$IP/extensions/UsabilityInitiative/ClickTracking/ClickTracking.php" );
+ *
+ * @author Nimish Gautam <ngautam@wikimedia.org>
+ * @license GPL v2 or later
+ * @version 0.1.1
+ */
+
+/* Configuration */
+
+$wgClickTrackStyleVersion = 1;
+
+//functions should check if this is set before logging clicktrack events
+$wgClickTrackEnabled = true;
+
+// Credits
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'Click Tracking',
+	'author' => 'Nimish Gautam',
+	'version' => '0.1.1',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:UsabilityInitiative',
+	'descriptionmsg' => 'clicktracking-desc',
+);
+
+// Includes parent extension
+require_once( dirname( dirname( __FILE__ ) ) . "/UsabilityInitiative.php" );
+
+// Adds Autoload Classes
+$wgAutoloadClasses['ClickTrackingHooks'] =
+	dirname( __FILE__ ) . '/ClickTracking.hooks.php';
+
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'ClickTrackingHooks::schema';
+
