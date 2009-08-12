80df54b4e834fdbd08cc99f8088035a0c9afb0c5
tomasz <tomasz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-12 22:12:34 +0000

    adding timestamps
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54895 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionTracking/ContributionTracking_body.php
+++ extensions/ContributionTracking/ContributionTracking_body.php
@@ -26,6 +26,8 @@ class ContributionTracking extends UnlistedSpecialPage {
 		}
 		
 		$db = contributionTrackingConnection();
+
+		$ts = wfTimeStamp( 'TS_DB' );
 		
 		$tracked_contribution = array(
 			'note' => $wgRequest->getText('comment', NULL),
@@ -36,6 +38,7 @@ class ContributionTracking extends UnlistedSpecialPage {
 			'utm_campaign' => $wgRequest->getText('utm_campaign', NULL),
 			'optout' => ($wgRequest->getCheck('email', 0) ? 0 : 1),
 			'language' => $wgRequest->getText('language', NULL),
+			'ts' => $ts,
 		);
 		
 		// Make all empty strings NULL
