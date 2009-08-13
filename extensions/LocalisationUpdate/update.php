cfdcbe2956a7b6ad496a5fd024cf1c0c3f159978
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-13 18:48:20 +0000

    this isn't the dark ages; let microtime() do the floatization for us
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@54959 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LocalisationUpdate/update.php
+++ extensions/LocalisationUpdate/update.php
@@ -11,10 +11,7 @@ $verbose = false;
 if ( $argc > 1 && $argv[0] == "verbose" )
 	$verbose = true;
 
-$mtime = microtime();
-$mtime = explode( " ", $mtime );
-$mtime = $mtime[1] + $mtime[0];
-$starttime = $mtime;
+$starttime = microtime( true );
 
 // Prevent the script from timing out
 set_time_limit( 0 );
@@ -22,9 +19,6 @@ ini_set( "max_execution_time", 0 );
 
 LocalisationUpdate::updateMessages( $verbose );
 
-$mtime = microtime();
-$mtime = explode( " ", $mtime );
-$mtime = $mtime[1] + $mtime[0];
-$endtime = $mtime;
+$endtime = microtime( true );
 $totaltime = ( $endtime - $starttime );
 print "All done in " . $totaltime . " seconds\n";
