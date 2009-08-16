91f32e9c57e6a56b1c99427f281abb99279afb5e
tbleher <tbleher@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-16 09:52:10 +0000

    Add initial version of Commentbox extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@55132 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Commentbox/Commentbox.i18n.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Internationalisation file for extension Commentbox.
+ *
+ * @addtogroup Extensions
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+$messages = array();
+
+$messages['en'] = array(
+	'commentbox-desc' => 'Adds a commentbox to certain pages',
+	'commentbox-prefill' => '',
+	'commentbox-intro' => '== Add a comment... ==
+You have a comment on this page? Add it here or <span class="plainlinks">[{{fullurl:{{FULLPAGENAME}}|action=edit}} edit the page directly]</span>.',
+	'commentbox-savebutton' => 'Save comment',
+	'commentbox-name' => 'Name:',
+	'commentbox-name-explanation' => '<small>(Tip: If you [[Special:Userlogin|log in]], you won\'t have to fill in your name here manually)</small>',
+	'commentbox-log' => 'New Comments',
+	'commentbox-first-comment-heading' => '== Comments ==',
+	'commentbox-regex' => '/\n==\s*Comments\s*==\s*\n/i', # should match the above heading
+	'commentbox-errorpage-title' => 'Error while creating comment',
+	'commentbox-error-page-nonexistent' => 'This page does not exist!',
+	'commentbox-error-namespace' => 'Comments are not allowed in this namespace!',
+	'commentbox-error-empty-comment' => 'Empty comments are not allowed!',
+);
+
+$messages['de'] = array(
+	'commentbox-desc' => 'Fügt in bestimmte Seiten ein Kommentarfeld ein',
+	'commentbox-prefill' => '',
+	'commentbox-intro' => '== Kommentar hinzufügen... ==
+Du hast einen Kommentar zu dieser Seite? Trag ihn hier ein oder <span class="plainlinks">[{{fullurl:{{FULLPAGENAME}}|action=edit}} bearbeite die Seite direkt]</span>.',
+	'commentbox-savebutton' => 'Kommentar speichern',
+	'commentbox-name' => 'Name:',
+	'commentbox-name-explanation' => '<small>(Tipp: Wenn Du Dich [[Spezial:Anmelden|anmeldest]], musst Du nicht mehr hier Deinen Namen angeben)</small>',
+	'commentbox-log' => 'Neuer Kommentar',
+	'commentbox-first-comment-heading' => '== Kommentare ==',
+	'commentbox-regex' => '/\n==\s*Kommentare\s*==\s*\n/i',
+	'commentbox-errorpage-title' => 'Fehler bei der Erzeugung des Kommentars',
+	'commentbox-error-page-nonexistent' => 'Die Seite existiert nicht!',
+	'commentbox-error-namespace' => 'Kommentare sind in diesem Namensraum nicht erlaubt!',
+	'commentbox-error-empty-comment' => 'Leere Kommentare sind nicht erlaubt!',
+);
+
