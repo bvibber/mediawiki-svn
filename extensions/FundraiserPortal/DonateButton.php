4d0d21d9dc2505b59d7c8824bcd104f1d16048bb
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-17 23:24:08 +0000

    Merge FundraiserPortal-multi branch back in.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@55212 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FundraiserPortal/DonateButton.php
@@ -0,0 +1,101 @@
+<?php
+
+class DonateButton extends UnlistedSpecialPage {
+	/* Members */
+
+	private $mSharedMaxAge = 600;
+	private $mMaxAge = 600;
+	
+	var $templates = array( 'Ruby', 'RubyText', 'Tourmaline', 'Sapphire' );
+
+	/* Functions */ 
+	
+	function __construct() {
+		parent::__construct( 'DonateButton' );
+	}
+
+	function execute () {
+		global $wgOut;
+		$wgOUt->disable();
+		$this->sendHeaders();
+		$js = $this->getJsOutput();
+	}
+
+	public function sharedMaxAge() {
+		return $this->mSharedMaxAge();
+	}   
+
+	public function maxAge() {
+		return $this->mMaxAge();
+	}   
+
+	// Set the caches 
+	private function sendHeaders() {
+		$smaxage = $this->sharedMaxAge();
+		$maxage = $this->maxAge();
+		$public = ( session_id() == '' );
+
+		header( "Content-type: text/javascript; charset=utf-8" );
+		if ( $public ) { 
+			header( "Cache-Control: public, s-maxage=$smaxage, max-age=$maxage" );
+		} else {
+			header( "Cache-Control: private, s-maxage=0, max-age=$maxage" );
+		}
+	}
+
+	public function getJsOutput() {
+		global $wgFundraiserPortalTemplates;
+	
+		foreach( $wgFundraiserPortalTemplates as $template => $weight ) {
+			$buttons[$template] = $this->getButtonText( $template );
+		}
+
+		$encButtons = json_encode( $buttons );
+
+		return $this->getScriptFunctions() .
+			'wgFundraiserPortalButtons=' . $encButtons . ";\n" .
+			"wgFundraiserPortal=wgFundraiserPortalButtons[wgDonateButton];\n";
+	}
+
+	public function getScriptFunctions() {
+		return $this->fetchTemplate( 'donateScripts.js' );
+	}
+
+	public function getButtonText( $template ) {
+		global $wgFundraiserImageUrl,$wgFundraiserPortalURL;
+
+		wfLoadExtensionMessages( 'FundraiserPortal' );
+
+		// Add our tracking identifiet
+		$buttonUrl = $wgFundraiserPortalURL . "&utm_source=$template";
+
+		// Switch statement of horror
+		if( in_array( $template, $this->templates ) ) {
+			$text = $this->fetchTemplate( "$template.tmpl" );
+			
+			$text = strtr( $text, array(
+				'{{{imageUrl}}}' => $wgFundraiserImageUrl,
+				'{{{buttonUrl}}}' => $buttonUrl ));
+			
+			// Note these are raw; no HTML translation or anything...
+			$text = preg_replace_callback( '/\{\{msg:(.*?)\}\}/',
+				array( $this, 'templateMessageCallback' ),
+				$text );
+			
+			return $text;
+		}
+		return false;
+	}
+	
+	function templateMessageCallback( $matches ) {
+		return wfMsg( $matches[1] );
+	}
+	
+	/**
+	 * Read one of this extension's resource files...
+	 */
+	protected function fetchTemplate( $filename ) {
+		$basedir = dirname( __FILE__ );
+		return file_get_contents( "$basedir/Templates/$filename" );
+	}
+}
--- extensions/FundraiserPortal/FundraiserPortal.php
+++ extensions/FundraiserPortal/FundraiserPortal.php
@@ -19,10 +19,32 @@
 
 /* Configuration */
 
+// on/off Switch
 $wgFundraiserPortalShow = true;
-$wgFundraiserPortalURL = 'http://wikimediafoundation.org/wiki/Donate/Now/en';
+
+// Set this to the base target of any button 
+$wgFundraiserPortalURL = 'http://wikimediafoundation.org/wiki/Donate/Now/en?utm_medium=sidebar&utm_campaign=spontaneous_donation';
+
+// Set this to the location the extensions images
+$wgFundraiserImageUrl = $wgScriptPath . '/extensions/FundraiserPortal/images';
+
 // Allowable templates: Plain, Ruby, RubyText, Sapphire, Tourmaline
-$wgFundraiserPortalTemplate = 'Tourmaline';
+$wgFundraiserPortalTemplates = array( 
+				'Ruby' => 25,
+				'Tourmaline' => 25,
+				'RubyText' => 25,
+				'Sapphire' => 25,
+				);
+
+// Set this to the public path where your js is pulled from
+$wgFundraiserPortalPath = 'http://192.168.250.128/sandbox';
+
+// Set this to the systme path location that the button js file will be written to
+// Must be reachable by the address in $wgNoticeProjectPath
+$wgFundraiserPortalDirectory = '/var/www/sandbox';
+
+// Only running this on wikipedia for now
+$wgFundraiserPortalProject = 'wikipedia';
 
 /* Setup */
 
@@ -30,19 +52,60 @@ $wgFundraiserPortalTemplate = 'Tourmaline';
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'FundraiserPortal',
-	'author' => 'Trevor Parscal',
-	'version' => '0.1.1',
+	'author' => 'Trevor Parscal, Tomasz Finc',
+	'version' => '0.2.0',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:FundraiserPortal',
 	'descriptionmsg' => 'fundraiserportal-desc',
 );
 
-// Adds Autoload Classes
-$wgAutoloadClasses['FundraiserPortalHooks'] =
-	dirname( __FILE__ ) . "/FundraiserPortal.hooks.php";
+
+// Load some classes
+$wgAutoloadClasses['DonateButton'] = dirname( __FILE__ ) . '/' . 'DonateButton.php';
 
 // Adds Internationalized Messages
 $wgExtensionMessagesFiles['FundraiserPortal'] =
 	dirname( __FILE__ ) . "/FundraiserPortal.i18n.php";
 
-// Registers Hooks
-$wgHooks['SkinBuildSidebar'][] = 'FundraiserPortalHooks::buildSidebar';
+$wgExtensionFunctions[] = 'efFundraiserPortalSetup';
+
+
+// Register hooks
+function efFundraiserPortalSetup() {
+	global $wgHooks;
+
+	$wgHooks['BeforePageDisplay'][] = 'efFundraiserPortalLoader';
+	//$wgHooks['SkinBuildSidebar'][] = 'efFundraiserPortalNoticeDisplay';
+}
+
+// Load the js that will choose the button client side
+function efFundraiserPortalLoader( $out, $skin ) {
+	global $wgOut, $wgLang;
+	global $wgJsMimeType, $wgStyleVersion;
+	global $wgFundraiserPortalShow, $wgFundraiserPortalProject, $wgFundraiserPortalPath;
+	
+	// Only proceed if we are configured to show the portal
+	if ( !$wgFundraiserPortalShow ) {
+		return true;
+	}
+
+	// Pull in our loader
+	$lang = $wgLang->getCode();
+	$fundraiserLoader = "$wgFundraiserPortalProject/$lang/fundraiserportal.js";
+	$encFundraiserLoader = htmlspecialchars( "$wgFundraiserPortalPath/$fundraiserLoader" );
+	$wgOut->addInlineScript( "var wgFundraiserPortal='';");
+	$wgOut->addScript( "<script type=\"{$wgJsMimeType}\" src=\"$encFundraiserLoader?$wgStyleVersion\"></script>\n" );
+	$loader = file_get_contents( dirname( __FILE__ ) . '/Templates/loader.js' );
+	$wgOut->addInlineScript( $loader );
+
+	return true;
+}
+
+// Finally display it if we got content
+function efFundraiserPortalNoticeDisplay( $skin, &$bar ) {
+	$loader = 
+		"<script type='text/javascript'>" .
+		"if (wgFundraiserPortal != '') { var document.writeln(wgFundraiserPortal);" .
+		"</script>";
+	$bar['Donate'] = $loader;
+	return true;
+}
