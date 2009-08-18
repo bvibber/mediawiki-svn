330e8e535d76e91d1866b136c4459fcf6e38e885
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-18 00:08:21 +0000

    and.... gratuitously different 'body' class for Vector compat
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@55218 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FundraiserPortal/Templates/loader.js
+++ extensions/FundraiserPortal/Templates/loader.js
@@ -31,7 +31,7 @@ addOnloadHook(function() {
 			div.className = "portlet";
 			if (fundraiserPortalToggleState)
 				div.className += ' collapsed';
-			div.innerHTML = '<div class="pBody">' + wgFundraiserPortal + '</div>';
+			div.innerHTML = '<div class="body pBody">' + wgFundraiserPortal + '</div>';
 			if (logo.nextSibling == null) {
 				logo.parentNode.appendChild(div);
 			} else {
