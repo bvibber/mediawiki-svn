14289b62ab25a053e10a1e135f4f711fd789a23f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-18 01:16:46 +0000

    Tag for version 0.3.1
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@55224 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Maps/REL_0_3_1/Geocoders/Maps_YahooGeocoder.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Yahoo! Geocoding Service info: http://developer.yahoo.com/geo/geoplanet/
+ *
+ * @file Maps_YahooGeocoder.php
+ * @ingroup Maps
+ *
+ * @author Jeroen De Dauw
+ * 
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class MapsYahooGeocoder extends MapsBaseGeocoder {
+	
+	/**
+	 * @see MapsBaseGeocoder::geocode()
+	 *
+	 * @param unknown_type $address
+	 */
+	public static function geocode($address) {
+		global $egYahooMapsKey;
+
+		// In case the Yahoo! Maps API key is not set, return false
+		if (empty($egYahooMapsKey)) return false;
+
+		// Create the request url
+		$requestURL = "http://where.yahooapis.com/v1/places.q('".urlencode($address)."')?appid=".urlencode($egYahooMapsKey)."&format=xml"; 
+
+		//Set up a CURL request, telling it not to spit back headers, and to throw out a user agent.
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $requestURL);
+		curl_setopt($ch, CURLOPT_HEADER, 0); //Change this to a 1 to return headers
+		curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"]);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		 
+		$result = curl_exec($ch);
+		curl_close($ch);
+	
+		$lon = self::getXmlElementValue($result, "longitude");
+		$lat = self::getXmlElementValue($result, "latitude");
+
+		// In case one of the values is not found, return false
+		if (!$lon || !$lat) return false;
+
+		return array(
+					'lat' => $lat,
+					'lon' => $lon
+					);	
+	}	
+	
+	/**
+	 * Gets the contents of the first XML tag with the provided name,
+	 * returns false when no matching element is found.
+	 *
+	 * @param string $xml
+	 * @param string $tagName
+	 * @return unknown
+	 */
+	private static function getXmlElementValue($xml, $tagName) {
+		$match = array();
+		preg_match("/<$tagName>(.*?)<\/$tagName>/", $xml, $match);
+		return count($match) > 1 ? $match[1] : false;
+	}
+	
+}
