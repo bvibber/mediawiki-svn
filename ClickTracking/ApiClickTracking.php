40c38269a85a50a30f97249fafa0f419c532ad04
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-21 20:28:08 +0000

    follow-up to r55425: *convert spaces to tabs *tweak doxygen comments *coding style tweaks
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@55430 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ClickTracking/ApiClickTracking.php
+++ ClickTracking/ApiClickTracking.php
@@ -1,86 +1,82 @@
 <?php
-
 /**
  * Extend the API for click tracking
- * 
- * @author nimishgautam
  *
+ * @file
+ * @ingroup API
  */
 class ApiClickTracking extends ApiBase {
 
 	/**
-	 * runs when the api is called with "clicktracking", takes in "eventid" and an edit token given to the user, "token"
+	 * runs when the API is called with "clicktracking", takes in "eventid" and an edit token given to the user, "token"
 	 * @see includes/api/ApiBase#execute()
 	 */
 	public function execute(){
 		global $wgUser, $wgTitle;
-		
+
 		$params = $this->extractRequestParams();
 		$this->validateParams( $params );
 		$eventid_to_lookup = $params['eventid'];
 		$session_id = $params['token'];
-		
-		//Event ID lookup table
-		$event_id = ClickTrackingHooks::getEventIDFromName(urldecode($eventid_to_lookup));
-		
+
+		// Event ID lookup table
+		$event_id = ClickTrackingHooks::getEventIDFromName( urldecode( $eventid_to_lookup ) );
+
 		$is_logged_in = $wgUser->isLoggedIn();
-		
-		
+
 		ClickTrackingHooks::trackEvent(
-		  $session_id,  //randomly generated session ID
-		  $is_logged_in, 						 //is the user logged in?
-		  $wgTitle->getNamespace(), 			 //what namespace are they editing?
-		  $event_id,							 //event ID passed in
-		  ( $is_logged_in ? $wgUser->getEditCount() : 0 ), //total edit count or 0 if anonymous
-		  ( $is_logged_in ? 
-		  				( ClickTrackingHooks::getEditCountSince( time() - $wgClickTrackContribTimeValue) ) 
-		       			: 0)   					 //contributions since whatever the time value is, or 0 if anonymous
-		  );
+			$session_id,  // randomly generated session ID
+			$is_logged_in, 						 // is the user logged in?
+			$wgTitle->getNamespace(), 			 // what namespace are they editing?
+			$event_id,							 // event ID passed in
+			( $is_logged_in ? $wgUser->getEditCount() : 0 ), // total edit count or 0 if anonymous
+			( $is_logged_in ?
+		  		( ClickTrackingHooks::getEditCountSince( time() - $wgClickTrackContribTimeValue ) ) 
+				: 0
+			) // contributions since whatever the time value is, or 0 if anonymous
+		);
 	}
 
 	/**
-	 * required parameter check
+	 * Required parameter check
 	 * @param $params params extracted from the POST
-	 * @return unknown_type
 	 */
  	protected function validateParams( $params ) {
-        $required = array( 'eventid', 'token');
-        foreach( $required as $arg ) {
-            if ( !isset( $params[$arg] ) ) {
-                $this->dieUsageMsg( array( 'missingparam', $arg ) );
-            }
-        }
-    }
-    
-    
+		$required = array( 'eventid', 'token' );
+		foreach( $required as $arg ) {
+			if ( !isset( $params[$arg] ) ) {
+				$this->dieUsageMsg( array( 'missingparam', $arg ) );
+			}
+		}
+	}
 
-    public function getParamDescription() {
-        return array(
-            'eventid' => 'string of eventID',
-        	'token'  => 'unique edit ID for this edit session'
-        );
-    }
-    
+	public function getParamDescription() {
+		return array(
+			'eventid' => 'string of eventID',
+			'token'  => 'unique edit ID for this edit session'
+		);
+	}
+
 	public function getDescription() {
-        return array(
-            'Track user clicks on Javascript items.' );
-    }
+		return array(
+			'Track user clicks on JavaScript items.'
+		);
+	}
 
 	public function getAllowedParams() {
-        return array(
-            'eventid' => array(
-                ApiBase::PARAM_TYPE => 'string'
-            ),
-            'token' => array(
-                ApiBase::PARAM_TYPE => 'string'
-            )
-            );
-    }
-    
-    //TODO: create a more useful 'version number'
-    public function getVersion() {
-        return __CLASS__ . ': $Id: $';
-    }
-	
-	
+		return array(
+			'eventid' => array(
+				ApiBase::PARAM_TYPE => 'string'
+			),
+			'token' => array(
+				ApiBase::PARAM_TYPE => 'string'
+			)
+		);
+	}
+
+	// TODO: create a more useful 'version number'
+	public function getVersion() {
+		return __CLASS__ . ': $Id: $';
+	}
+
 }
--- ClickTracking/ClickTracking.hooks.php
+++ ClickTracking/ClickTracking.hooks.php
@@ -10,7 +10,7 @@
 class ClickTrackingHooks {
 
 	/* Static Functions */
-	
+
 	/* initializations */
 
 	/* 3 tables for click tracking */
@@ -26,7 +26,7 @@ class ClickTrackingHooks {
 			'user_daily_contribs',
 			dirname( __FILE__ ) . '/UserDailyContribs.sql'
 		);
-		
+
 		$wgExtNewTables[] = array(
 			'click_tracking_events',
 			dirname( __FILE__ ) . '/ClickTrackingEvents.sql'
@@ -35,146 +35,144 @@ class ClickTrackingHooks {
 		return true;
 	}
 
-	
 	/**
-	 * adds javascript
+	 * Adds JavaScript
 	 */
 	public static function addJS(){
 		UsabilityInitiativeHooks::initialize();
-		UsabilityInitiativeHooks::addScript('ClickTracking/ClickTracking.js');
+		UsabilityInitiativeHooks::addScript( 'ClickTracking/ClickTracking.js' );
 		UsabilityInitiativeHooks::addVariables(
 			array( 'wgTrackingToken' => ClickTrackingHooks::get_session_id() )
 		);
 		return true;
 	}
-	
-	
+
 	/**
 	 * Gets the session ID...we just want a unique random ID for the page load
 	 * @return session ID
 	 */
 	public static function get_session_id(){
-			global $wgUser;
-			return wfGenerateToken(array($wgUser->getName(), time()));
+		global $wgUser;
+		return wfGenerateToken( array( $wgUser->getName(), time() ) );
 	}
-	
-     /**
+
+	/**
 	 * Get the number of revisions a user has made since a given time
 	 * @param $ts beginning timestamp
 	 * @return number of revsions this user has made
 	 */
-	public static function getEditCountSince($ts){
+	public static function getEditCountSince( $ts ){
 		global $wgUser;
-		
-		//convert to just the day
-		$time = gmdate('Ymd', wfTimestamp(TS_UNIX,$ts));
-		
+
+		// convert to just the day
+		$time = gmdate( 'Ymd', wfTimestamp( TS_UNIX, $ts ) );
+
 		$dbr = wfGetDB( DB_SLAVE );
-		 
-		$edits = $dbr->selectField( 
-        'user_daily_contribs', 
-        'SUM(contribs)',
-        array( 
-            'user_id' => $wgUser->getId(),
-            "day < '$time'"
-        	), 
-        __METHOD__
-    	);
-    
-    	//user hasn't made any edits in whatever amount of time
-    	if( $edits == null ){
-    		$edits = 0;
-    	}
-    		
-    	return $edits;
-    	
+
+		$edits = $dbr->selectField(
+			'user_daily_contribs',
+			'SUM(contribs)',
+			array(
+				'user_id' => $wgUser->getId(),
+				"day < '$time'"
+			),
+			__METHOD__
+		);
+
+		// user hasn't made any edits in whatever amount of time
+		if( $edits == null ){
+			$edits = 0;
+		}
+
+		return $edits;
 	}
-	
+
 	/**
 	 * Stores a new contribution
-	 * @return unknown_type
+	 * @return true
 	 */
 	public static function storeNewContrib(){
 		global $wgUser;
-		$today = gmdate('Ymd',time());
+		$today = gmdate( 'Ymd', time() );
 		$dbw = wfGetDB( DB_MASTER );
 		$sql = 
 		"INSERT INTO user_daily_contribs (user_id,day,contribs) VALUES ({$wgUser->getId()},$today,1) ON DUPLICATE KEY UPDATE contribs=contribs+1;";
 		$dbw->query($sql, __METHOD__);
 		return true;
 	}
-	
-	/**
-	 * get event ID from name
-	 * @param $event_name
-	 * @return $event ID
-	 */
-	public static function getEventIDFromName($event_name){
-		$dbr = wfGetDB( DB_SLAVE );
-		
-		$id_num = $dbr->selectField( 
-        'click_tracking_events', 
-        'id',
-        array( 
-            'event_name' => $event_name
-        	), 
-        __METHOD__
-    	);
-    	
-    	// if this entry doesn't exist...
-    	// this will be incredibly rare as the whole database will only have a few hundred entries in it at most
-    	// and getting DB_MASTER up top would be wasteful 
-    	
-    	if($id_num === false){
-    		$dbw = wfGetDB( DB_MASTER );
-    		$dbw->insert('click_tracking_events', 
-    					array('event_name' => (string) $event_name), 
-    						__METHOD__);
-    						
-        //should be inserted now...if not, we return zero later
 
-    		$id_num = $dbr->selectField( 
-        		'click_tracking_events', 
-        		'id',
-        		array( 
-            		'event_name' => $event_name
-        		), 
-        		__METHOD__
-    			);
-    	}
-    	
-    	if($id_num === false){ return 0;}
-    	return $id_num;
+	/**
+	 * Get event ID from name
+	 * @param $event_name String: name of the event to get
+	 * @return integer
+	 */
+	public static function getEventIDFromName( $event_name ){
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$id_num = $dbr->selectField(
+			'click_tracking_events',
+			'id',
+			array( 
+				'event_name' => $event_name
+			), 
+			__METHOD__
+		);
+
+		// if this entry doesn't exist...
+		// this will be incredibly rare as the whole database will only have a few hundred entries in it at most
+		// and getting DB_MASTER up top would be wasteful
+		if( $id_num === false ){
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->insert(
+				'click_tracking_events',
+				array( 'event_name' => (string) $event_name ),
+				__METHOD__
+			);
+
+			// should be inserted now...if not, we return zero later
+			$id_num = $dbr->selectField(
+				'click_tracking_events',
+				'id',
+				array(
+					'event_name' => $event_name
+				), 
+				__METHOD__
+			);
+		}
+
+		if( $id_num === false ){
+			return 0;
+		}
+
+		return $id_num;
 	}
-	
+
 	/**
 	 * Track particular event
-	 * @param $session_id unique session id for this editing sesion
-	 * @param $is_logged_in whether or not the user is logged in
-	 * @param $namespace namespace the user is editing
-	 * @param $event_id event type
-	 * @param $contribs contributions the user has made (or NULL if user not logged in)
-	 * @param $contribs_in_timespan number of contributions user has made in a given timespan
+	 * @param $session_id String: unique session id for this editing sesion
+	 * @param $is_logged_in Boolean: whether or not the user is logged in
+	 * @param $namespace Integer: namespace the user is editing
+	 * @param $event_id Integer: event type
+	 * @param $contribs Integer: contributions the user has made (or NULL if user not logged in)
+	 * @param $contribs_in_timespan Integer: number of contributions user has made in a given timespan
 	 * @return true if the event was stored in the DB
 	 */
-	public static function trackEvent( $session_id, $is_logged_in, $namespace, $event_id, $contribs=0, $contribs_in_timespan=0 ){
-		
+	public static function trackEvent( $session_id, $is_logged_in, $namespace, $event_id, $contribs = 0, $contribs_in_timespan = 0 ){
 		$dbw = wfGetDB( DB_MASTER );
 
 		$dbw->begin();
-		
+
 		// Builds insert information
 		$data = array(
 			'session_id' => (string) $session_id,
 			'is_logged_in' => (bool) $is_logged_in,
 			'user_total_contribs' => ( $is_logged_in ? (int) $contribs : null ),
-		    'user_contribs_span' => ( $is_logged_in ? (int) $contribs_in_timespan : null ),
-		    'namespace' => (int) $namespace,
+			'user_contribs_span' => ( $is_logged_in ? (int) $contribs_in_timespan : null ),
+			'namespace' => (int) $namespace,
 			'event_id' => (int) $event_id
-
 		);
 
-		$db_status = $dbw->insert('click_tracking', $data, __METHOD__);
+		$db_status = $dbw->insert( 'click_tracking', $data, __METHOD__ );
 		$dbw->commit();
 		return $db_status;
 	}
