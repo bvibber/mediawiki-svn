d6390fbe391bab72b4419a543bd89903e6ad1caf
shinjiman <shinjiman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-22 10:00:04 +0000

    Localisation updates Cantonese and Chinese for extension messages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@55472 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ClickTracking/ClickTracking.i18n.php
+++ ClickTracking/ClickTracking.i18n.php
@@ -24,3 +24,27 @@ $messages['vec'] = array(
 	'clicktracking-desc' => 'Traciamento dei click, par traciare i eventi che no provoca mia un refresh de la pagina.',
 );
 
+/** Cantonese
+ * @author Shinjiman
+ */
+$messages['yue'] = array(
+	'clicktracking' => '可用性倡議撳追蹤',
+	'clicktracking-desc' => '撳追蹤，響唔使重載版嘅情況之下追蹤撳'
+);
+
+/** Simplified Chinese
+ * @author Shinjiman
+ */
+$messages['zh-hans'] = array(
+	'clicktracking' => '可用性倡议点击追踪',
+	'clicktracking-desc' => '点击追踪，不在重载页面的情况中用来追踪点击'
+);
+
+/** Traditional Chinese
+ * @author Shinjiman
+ */
+$messages['zh-hant'] = array(
+	'clicktracking' => '可用性倡議點擊追蹤',
+	'clicktracking-desc' => '點擊追蹤，不在重載頁面的情況中用來追蹤點擊'
+);
+
