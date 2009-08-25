8c3a9883779bd5a5abcf764ba812dd728be8d1be
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-08-25 18:43:33 +0000

    Tag for version 0.3.3
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@55580 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticMaps/REL_0_3_3/GoogleMaps/SM_GoogleMapsFormInput.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * A class that holds static helper functions and extension hooks for the Google Maps service
+ *
+ * @file SM_GoogleMapsFormInput.php
+ * @ingroup SemanticMaps
+ * 
+ * @author Robert Buzink
+ * @author Yaron Koren
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class SMGoogleMapsFormInput extends SMFormInput {
+
+	public $serviceName = MapsGoogleMaps::SERVICE_NAME;
+	
+	/**
+	 * @see MapsMapFeature::setMapSettings()
+	 *
+	 */
+	protected function setMapSettings() {
+		global $egMapsGoogleMapsZoom, $egMapsGoogleMapsPrefix;
+		
+		$this->elementNamePrefix = $egMapsGoogleMapsPrefix;
+		$this->showAddresFunction = 'showGAddress';	
+
+		$this->earthZoom = 1;
+		
+		$this->defaultParams = MapsGoogleMapsUtils::getDefaultParams();
+        $this->defaultZoom = $egMapsGoogleMapsZoom;		
+	}
+	
+
+	/**
+	 * @see MapsMapFeature::doMapServiceLoad()
+	 *
+	 */
+	protected function doMapServiceLoad() {
+		global $egGoogleMapsOnThisPage;
+
+		if (empty($egGoogleMapsOnThisPage)) {
+			$egGoogleMapsOnThisPage = 0;
+			MapsGoogleMapsUtils::addGMapDependencies($this->output);
+		}
+		
+		$egGoogleMapsOnThisPage++;	
+		
+		$this->elementNr = $egGoogleMapsOnThisPage;
+	}
+	
+	/**
+	 * @see MapsMapFeature::addSpecificFormInputHTML()
+	 *
+	 */
+	protected function addSpecificMapHTML() {
+		global $wgJsMimeType;
+		
+		$enableEarth = MapsGoogleMapsUtils::getEarthValue($this->earth);
+		
+		$this->autozoom = MapsGoogleMapsUtils::getAutozoomJSValue($this->autozoom);
+		
+		$this->type = MapsGoogleMapsUtils::getGMapType($this->type, true);
+		
+		$this->controls = MapsGoogleMapsUtils::createControlsString($this->controls);		
+		
+		$this->types = explode(",", $this->types);
+		
+		$typesString = MapsGoogleMapsUtils::createTypesString($this->types, $enableEarth);
+		
+		$this->output .= "
+		<div id='".$this->mapName."' class='".$this->class."'></div>
+	
+		<script type='$wgJsMimeType'>/*<![CDATA[*/
+		addLoadEvent(makeFormInputGoogleMap('$this->mapName', '$this->coordsFieldName', $this->width, $this->height, $this->centre_lat, $this->centre_lon, $this->zoom, $this->type, [$typesString], [$this->controls], $this->autozoom, $this->marker_lat, $this->marker_lon));
+		window.unload = GUnload;
+		/*]]>*/</script>";
+	}
+	
+	/**
+	 * @see SMFormInput::manageGeocoding()
+	 *
+	 */
+	protected function manageGeocoding() {
+		global $egGoogleMapsKey;
+		$this->enableGeocoding = strlen(trim($egGoogleMapsKey)) > 0;
+		if ($this->enableGeocoding) MapsGoogleMapsUtils::addGMapDependencies($this->output);		
+	}	
+	
+}
---
+++ SemanticMaps/REL_0_3_3/YahooMaps/SM_YahooMapsFormInput.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+* Form input hook that adds an Yahoo! Maps map format to Semantic Forms
+ *
+ * @file SM_YahooMapsFormInput.php
+ * @ingroup SemanticMaps
+ * 
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class SMYahooMapsFormInput extends SMFormInput {
+	
+	public $serviceName = MapsYahooMaps::SERVICE_NAME;	
+	
+	/**
+	 * @see MapsMapFeature::setMapSettings()
+	 *
+	 */
+	protected function setMapSettings() {
+		global $egMapsYahooMapsZoom, $egMapsYahooMapsPrefix;
+		
+		$this->elementNamePrefix = $egMapsYahooMapsPrefix;
+		$this->showAddresFunction = 'showYAddress';		
+
+		$this->earthZoom = 17;	
+
+		$this->defaultParams = MapsYahooMapsUtils::getDefaultParams();
+        $this->defaultZoom = $egMapsYahooMapsZoom;		
+	}	
+	
+	/**
+	 * @see MapsMapFeature::doMapServiceLoad()
+	 *
+	 */
+	protected function doMapServiceLoad() {
+		global $egYahooMapsOnThisPage;
+		
+		if (empty($egYahooMapsOnThisPage)) {
+			$egYahooMapsOnThisPage = 0;
+			MapsYahooMapsUtils::addYMapDependencies($this->output);
+		}
+		$egYahooMapsOnThisPage++;			
+		
+		$this->elementNr = $egYahooMapsOnThisPage;
+	}	
+	
+	/**
+	 * @see MapsMapFeature::addSpecificMapHTML()
+	 *
+	 */
+	protected function addSpecificMapHTML() {
+		global $wgJsMimeType;
+		
+		$type = MapsYahooMapsUtils::getYMapType($this->type, true);
+		
+		$this->autozoom = MapsYahooMapsUtils::getAutozoomJSValue($this->autozoom);
+		
+		$controlItems = MapsYahooMapsUtils::createControlsString($this->controls);		
+		
+		MapsUtils::makePxValue($this->width);
+		MapsUtils::makePxValue($this->height);	
+		
+		$this->types = explode(",", $this->types);
+		
+		$typesString = MapsYahooMapsUtils::createTypesString($this->types);			
+		
+		$this->output .="
+		<div id='".$this->mapName."' style='width: $this->width; height: $this->height;'></div>  
+		
+		<script type='$wgJsMimeType'>/*<![CDATA[*/
+		addLoadEvent(makeFormInputYahooMap('$this->mapName', '$this->coordsFieldName', $this->centre_lat, $this->centre_lon, $this->zoom, $type, [$typesString], [$controlItems], $this->autozoom, $this->marker_lat, $this->centre_lon));
+		/*]]>*/</script>";		
+	}
+	
+	/**
+	 * @see SMFormInput::manageGeocoding()
+	 *
+	 */
+	protected function manageGeocoding() {
+		global $egYahooMapsKey;
+		$this->enableGeocoding = strlen(trim($egYahooMapsKey)) > 0;
+		if ($this->enableGeocoding) MapsYahooMapsUtils::addYMapDependencies($this->output);			
+	}
+
+	
+}
