36f3fcce9a84fbbe54407abb66045481868293f3
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-03 18:06:57 +0000

    Solves r20485 by explicitly listing javascript files to be combined since order is important, and we were previously just getting luck that alphabetical order was acceptable.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@55781 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/js/plugins.combined.js
+++ extensions/UsabilityInitiative/js/plugins.combined.js
@@ -1010,82 +1010,6 @@ scrollToCaretPosition: function( pos ) {
 }
 
 } ); } )( jQuery );/**
- * Extend the RegExp object with an escaping function
- * From http://simonwillison.net/2006/Jan/20/escape/
- */
-RegExp.escape = function( s ) { return s.replace(/([.*+?^${}()|\/\\[\]])/g, '\\$1'); };
-
-/**
- * Dialog Module for wikiEditor
- */
-( function( $ ) { $.wikiEditor.modules.dialogs = {
-
-/**
- * API accessible functions
- */
-api: {
-	addDialog: function( context, data ) {
-		$.wikiEditor.modules.dialogs.fn.create( context, { 'modules': data } )
-	},
-	openDialog: function( context, data ) {
-		if ( data.dialog in $.wikiEditor.modules.dialogs.modules ) {
-			$( '#' + $.wikiEditor.modules.dialogs.modules[data.dialog].id ).dialog( 'open' );
-		}
-	},
-	closeDialog: function( context, data ) {
-		if ( data.dialog in $.wikiEditor.modules.dialogs.modules ) {
-			$( '#' + $.wikiEditor.modules.dialogs.modules[data.dialog].id ).dialog( 'close' );
-		}
-	}
-},
-/**
- * Internally used functions
- */
-fn: {
-	/**
-	 * Creates a dialog module within a wikiEditor
-	 * 
-	 * @param {Object} context Context object of editor to create module in
-	 * @param {Object} config Configuration object to create module from
-	 */
-	create: function( context, config ) {
-		// Add modules
-		for ( module in config ) {
-			$.wikiEditor.modules.dialogs.modules[module] = config[module];
-		}
-		// Build out modules immediately
-		for ( module in $.wikiEditor.modules.dialogs.modules ) {
-			var module = $.wikiEditor.modules.dialogs.modules[module];
-			// Only create the dialog if it doesn't exist yet
-			if ( $( '#' + module.id ).size() == 0 ) {
-				var configuration = module.dialog;
-				// Add some stuff to configuration
-				configuration.bgiframe = true;
-				configuration.autoOpen = false;
-				configuration.modal = true;
-				configuration.title = $.wikiEditor.autoMsg( module, 'title' );
-				// Transform messages in keys
-				// Stupid JS won't let us do stuff like
-				// foo = { gM ('bar'): baz }
-				for ( msg in configuration.buttons ) {
-					configuration.buttons[gM( msg )] = configuration.buttons[msg];
-					delete configuration.buttons[msg];
-				}
-				// Create the dialog <div>
-				$( '<div /> ' )
-					.attr( 'id', module.id )
-					.html( module.html )
-					.data( 'context', context )
-					.appendTo( $( 'body' ) )
-					.each( module.init )
-					.dialog( configuration );
-			}
-		}
-	}
-},
-'modules': {}
-
-}; } ) ( jQuery );/**
  * This plugin provides a way to build a user interface around a textarea. You
  * can build the UI from a confguration..
  * 	$j( 'div#edittoolbar' ).wikiEditor(
@@ -1246,226 +1170,80 @@ context.$textarea.scrollToCaretPosition( 0 );
 return $(this).data( 'context', context );
 
 };})(jQuery);/**
- * TOC Module for wikiEditor
+ * Extend the RegExp object with an escaping function
+ * From http://simonwillison.net/2006/Jan/20/escape/
  */
-( function( $ ) { $.wikiEditor.modules.toc = {
+RegExp.escape = function( s ) { return s.replace(/([.*+?^${}()|\/\\[\]])/g, '\\$1'); };
+
+/**
+ * Dialog Module for wikiEditor
+ */
+( function( $ ) { $.wikiEditor.modules.dialogs = {
 
 /**
  * API accessible functions
  */
 api: {
-	//
+	addDialog: function( context, data ) {
+		$.wikiEditor.modules.dialogs.fn.create( context, { 'modules': data } )
+	},
+	openDialog: function( context, data ) {
+		if ( data.dialog in $.wikiEditor.modules.dialogs.modules ) {
+			$( '#' + $.wikiEditor.modules.dialogs.modules[data.dialog].id ).dialog( 'open' );
+		}
+	},
+	closeDialog: function( context, data ) {
+		if ( data.dialog in $.wikiEditor.modules.dialogs.modules ) {
+			$( '#' + $.wikiEditor.modules.dialogs.modules[data.dialog].id ).dialog( 'close' );
+		}
+	}
 },
 /**
  * Internally used functions
  */
 fn: {
 	/**
-	 * Creates a table of contents module within a wikiEditor
+	 * Creates a dialog module within a wikiEditor
 	 * 
 	 * @param {Object} context Context object of editor to create module in
 	 * @param {Object} config Configuration object to create module from
 	 */
 	create: function( context, config ) {
-		if ( '$toc' in context.modules ) {
-			return;
+		// Add modules
+		for ( module in config ) {
+			$.wikiEditor.modules.dialogs.modules[module] = config[module];
 		}
-		context.modules.$toc = $( '<div></div>' )
-			.addClass( 'wikiEditor-ui-toc' )
-			.attr( 'id', 'wikiEditor-ui-toc' );
-		$.wikiEditor.modules.toc.fn.build( context, config );
-		context.$ui.find( '.wikiEditor-ui-bottom' )
-			.append( context.modules.$toc );
-		context.modules.$toc.height(
-			context.$ui.find( '.wikiEditor-ui-bottom' ).height()
-		);
-		// Make some css modifications to make room for the toc on the right...
-		// Perhaps this could be configurable?
-		context.modules.$toc
-			.css( 'width', '12em' )
-			.css( 'marginTop', -( context.$ui.find( '.wikiEditor-ui-bottom' ).height() ) );
-		context.$ui.find( '.wikiEditor-ui-text' )
-			.css( ( $( 'body.rtl' ).size() ? 'marginLeft' : 'marginRight' ), '12em' );
-		// Add the TOC to the document
-		$.wikiEditor.modules.toc.fn.build( context );
-		$.wikiEditor.modules.toc.fn.update( context );
-		context.$textarea
-			.bind( 'keyup encapsulateSelection',
-				function( event ) {
-					var context = $(this).data( 'context' );
-					$(this).eachAsync( {
-						bulk: 0,
-						loop: function() {
-							$.wikiEditor.modules.toc.fn.build( context );
-							$.wikiEditor.modules.toc.fn.update( context );
-						}
-					} );
+		// Build out modules immediately
+		for ( module in $.wikiEditor.modules.dialogs.modules ) {
+			var module = $.wikiEditor.modules.dialogs.modules[module];
+			// Only create the dialog if it doesn't exist yet
+			if ( $( '#' + module.id ).size() == 0 ) {
+				var configuration = module.dialog;
+				// Add some stuff to configuration
+				configuration.bgiframe = true;
+				configuration.autoOpen = false;
+				configuration.modal = true;
+				configuration.title = $.wikiEditor.autoMsg( module, 'title' );
+				// Transform messages in keys
+				// Stupid JS won't let us do stuff like
+				// foo = { gM ('bar'): baz }
+				for ( msg in configuration.buttons ) {
+					configuration.buttons[gM( msg )] = configuration.buttons[msg];
+					delete configuration.buttons[msg];
 				}
-			)
-			.bind( 'mouseup scrollToPosition',
-				function( event ) {
-					var context = $(this).data( 'context' );
-					$(this).eachAsync( {
-						bulk: 0,
-						loop: function() {
-							$.wikiEditor.modules.toc.fn.update( context );
-						}
-					} );
-				}
-			);
-	},
-	/**
-	 * Highlight the section the cursor is currently within
-	 * 
-	 * @param {Object} context
-	 */
-	update: function( context ) {
-		context.modules.$toc.find( 'a' ).removeClass( 'currentSelection' );
-		var position = context.$textarea.getCaretPosition();
-		var section = 0;
-		if ( context.data.outline.length > 0 ) {
-			// If the caret is before the first heading, you must be in section
-			// 0, and there is no need to look any farther - otherwise check
-			// that the caret is before each section, and when it's not, we now
-			// know what section it is in
-			if ( !( position < context.data.outline[0].position - 1 ) ) {
-				while (
-					section < context.data.outline.length && context.data.outline[section].position - 1 < position
-				) {
-					section++;
-				}
-				section = Math.max( 0, section );
+				// Create the dialog <div>
+				$( '<div /> ' )
+					.attr( 'id', module.id )
+					.html( module.html )
+					.data( 'context', context )
+					.appendTo( $( 'body' ) )
+					.each( module.init )
+					.dialog( configuration );
 			}
-			context.modules.$toc.find( 'a.section-' + section ).addClass( 'currentSelection' );
 		}
-	},
-	/**
-	 * Builds table of contents
-	 * 
-	 * @param {Object} context
-	 */
-	build: function( context ) {
-		/**
-		 * Builds a structured outline from flat outline
-		 * 
-		 * @param {Object} outline Array of objects with level fields
-		 */
-		function buildStructure( outline, offset, level ) {
-			if ( offset == undefined ) offset = 0;
-			if ( level == undefined ) level = 1;
-			var sections = [];
-			for ( var i = offset; i < outline.length; i++ ) {
-				if ( outline[i].nLevel == level ) {
-					var sub = buildStructure( outline, i + 1, level + 1 );
-					if ( sub.length ) {
-						outline[i].sections = sub;
-					}
-					sections[sections.length] = outline[i];
-				} else if ( outline[i].nLevel < level ) {
-					break;
-				}
-			}
-			return sections;
-		}
-		/**
-		 * Bulds unordered list HTML object from structured outline
-		 * 
-		 * @param {Object} structure Structured outline
-		 */
-		function buildList( structure ) {
-			var list = $( '<ul></ul>' );
-			for ( i in structure ) {
-				var item = $( '<li></li>' )
-					.append(
-						$( '<a></a>' )
-							.attr( 'href', '#' )
-							.addClass( 'section-' + structure[i].index )
-							.data( 'textbox', context.$textarea )
-							.data( 'position', structure[i].position )
-							.click( function( event ) {
-								$(this).data( 'textbox' ).scrollToCaretPosition( $(this).data( 'position' ) );
-								event.preventDefault();
-							} )
-							.text( structure[i].text )
-					);
-				if ( structure[i].sections !== undefined ) {
-					item.append( buildList( structure[i].sections ) );
-				}
-				list.append( item );
-			}
-			return list;
-		}
-		// Build outline from wikitext
-		var outline = [];
-		var wikitext = '\n' + context.$textarea.val() + '\n';
-		var headings = wikitext.match( /\n={1,5}.*={1,5}(?=\n)/g );
-		var offset = 0;
-		headings = $.makeArray( headings );
-		for ( var h = 0; h < headings.length; h++ ) {
-			text = headings[h];
-			// Get position of first occurence
-			var position = wikitext.indexOf( text, offset );
-			// Update offset to avoid stumbling on duplicate headings
-			if ( position > offset ) {
-				offset = position;
-			} else if ( position == -1 ) {
-				// Not sure this is possible, or what should happen
-				continue;
-			}
-			// Trim off whitespace
-			text = $.trim( text );
-			// Detect the starting and ending heading levels
-			var startLevel = 0;
-			for ( var c = 0; c < text.length; c++ ) {
-				if ( text.charAt( c ) == '=' ) {
-					startLevel++;
-				} else {
-					break;
-				}
-			}
-			var endLevel = 0;
-			for ( var c = text.length - 1; c >= 0; c-- ) {
-				if ( text.charAt( c ) == '=' ) {
-					endLevel++;
-				} else {
-					break;
-				}
-			}
-			// Use the lowest number of =s as the actual level
-			var level = Math.min( startLevel, endLevel );
-			text = $.trim( text.substr( level, text.length - ( level * 2 ) ) );
-			// Add the heading data to the outline
-			outline[h] = { 'text': text, 'position': position, 'level': level, 'index': h + 1 };
-		}
-		// Normalize heading levels for list creation
-		// This is based on Linker::generateTOC() so, it should behave like the
-		// TOC on rendered articles does - which is considdered to be correct
-		// at this point in time.
-		var lastLevel = 0;
-		var nLevel = 0;
-		for ( var i = 0; i < outline.length; i++ ) {
-			if ( outline[i].level > lastLevel ) {
-				nLevel++;
-			}
-			else if ( outline[i].level < nLevel ) {
-				nLevel -= Math.max( 1, lastLevel - outline[i].level );
-			}
-			if ( nLevel <= 0 ) {
-				nLevel = 1;
-			}
-			outline[i].nLevel = nLevel;
-			lastLevel = outline[i].level;
-		}
-		// Recursively build the structure and add special item for
-		// section 0, if needed
-		var structure = buildStructure( outline );
-		if ( $( 'input[name=wpSection]' ).val() == '' )
-			structure.unshift( { 'text': wgPageName.replace(/_/g, ' '), 'level': 1, 'index': 0, 'position': 0 } );
-		context.modules.$toc.html( buildList( structure ) );
-		// Cache the outline for later use
-		context.data.outline = outline;
 	}
-}
+},
+'modules': {}
 
 }; } ) ( jQuery );/**
  * Toolbar module for wikiEditor
@@ -1925,6 +1703,12 @@ fn : {
 						$section.show();
 						$(this).addClass( 'current' );
 					}
+					
+					//click tracking
+					if($.trackAction != undefined){
+						$.trackAction($section.attr('rel') + '.' + ( show ? 'show': 'hide' )  );
+					}
+					
 					$.cookie(
 						'wikiEditor-' + $(this).data( 'context' ).instance + '-toolbar-section',
 						show ? $section.attr( 'rel' ) : null
@@ -2022,3 +1806,226 @@ fn : {
 }
 
 }; } )( jQuery );
+/**
+ * TOC Module for wikiEditor
+ */
+( function( $ ) { $.wikiEditor.modules.toc = {
+
+/**
+ * API accessible functions
+ */
+api: {
+	//
+},
+/**
+ * Internally used functions
+ */
+fn: {
+	/**
+	 * Creates a table of contents module within a wikiEditor
+	 * 
+	 * @param {Object} context Context object of editor to create module in
+	 * @param {Object} config Configuration object to create module from
+	 */
+	create: function( context, config ) {
+		if ( '$toc' in context.modules ) {
+			return;
+		}
+		context.modules.$toc = $( '<div></div>' )
+			.addClass( 'wikiEditor-ui-toc' )
+			.attr( 'id', 'wikiEditor-ui-toc' );
+		$.wikiEditor.modules.toc.fn.build( context, config );
+		context.$ui.find( '.wikiEditor-ui-bottom' )
+			.append( context.modules.$toc );
+		context.modules.$toc.height(
+			context.$ui.find( '.wikiEditor-ui-bottom' ).height()
+		);
+		// Make some css modifications to make room for the toc on the right...
+		// Perhaps this could be configurable?
+		context.modules.$toc
+			.css( 'width', '12em' )
+			.css( 'marginTop', -( context.$ui.find( '.wikiEditor-ui-bottom' ).height() ) );
+		context.$ui.find( '.wikiEditor-ui-text' )
+			.css( ( $( 'body.rtl' ).size() ? 'marginLeft' : 'marginRight' ), '12em' );
+		// Add the TOC to the document
+		$.wikiEditor.modules.toc.fn.build( context );
+		$.wikiEditor.modules.toc.fn.update( context );
+		context.$textarea
+			.bind( 'keyup encapsulateSelection',
+				function( event ) {
+					var context = $(this).data( 'context' );
+					$(this).eachAsync( {
+						bulk: 0,
+						loop: function() {
+							$.wikiEditor.modules.toc.fn.build( context );
+							$.wikiEditor.modules.toc.fn.update( context );
+						}
+					} );
+				}
+			)
+			.bind( 'mouseup scrollToPosition',
+				function( event ) {
+					var context = $(this).data( 'context' );
+					$(this).eachAsync( {
+						bulk: 0,
+						loop: function() {
+							$.wikiEditor.modules.toc.fn.update( context );
+						}
+					} );
+				}
+			);
+	},
+	/**
+	 * Highlight the section the cursor is currently within
+	 * 
+	 * @param {Object} context
+	 */
+	update: function( context ) {
+		context.modules.$toc.find( 'a' ).removeClass( 'currentSelection' );
+		var position = context.$textarea.getCaretPosition();
+		var section = 0;
+		if ( context.data.outline.length > 0 ) {
+			// If the caret is before the first heading, you must be in section
+			// 0, and there is no need to look any farther - otherwise check
+			// that the caret is before each section, and when it's not, we now
+			// know what section it is in
+			if ( !( position < context.data.outline[0].position - 1 ) ) {
+				while (
+					section < context.data.outline.length && context.data.outline[section].position - 1 < position
+				) {
+					section++;
+				}
+				section = Math.max( 0, section );
+			}
+			context.modules.$toc.find( 'a.section-' + section ).addClass( 'currentSelection' );
+		}
+	},
+	/**
+	 * Builds table of contents
+	 * 
+	 * @param {Object} context
+	 */
+	build: function( context ) {
+		/**
+		 * Builds a structured outline from flat outline
+		 * 
+		 * @param {Object} outline Array of objects with level fields
+		 */
+		function buildStructure( outline, offset, level ) {
+			if ( offset == undefined ) offset = 0;
+			if ( level == undefined ) level = 1;
+			var sections = [];
+			for ( var i = offset; i < outline.length; i++ ) {
+				if ( outline[i].nLevel == level ) {
+					var sub = buildStructure( outline, i + 1, level + 1 );
+					if ( sub.length ) {
+						outline[i].sections = sub;
+					}
+					sections[sections.length] = outline[i];
+				} else if ( outline[i].nLevel < level ) {
+					break;
+				}
+			}
+			return sections;
+		}
+		/**
+		 * Bulds unordered list HTML object from structured outline
+		 * 
+		 * @param {Object} structure Structured outline
+		 */
+		function buildList( structure ) {
+			var list = $( '<ul></ul>' );
+			for ( i in structure ) {
+				var item = $( '<li></li>' )
+					.append(
+						$( '<a></a>' )
+							.attr( 'href', '#' )
+							.addClass( 'section-' + structure[i].index )
+							.data( 'textbox', context.$textarea )
+							.data( 'position', structure[i].position )
+							.click( function( event ) {
+								$(this).data( 'textbox' ).scrollToCaretPosition( $(this).data( 'position' ) );
+								event.preventDefault();
+							} )
+							.text( structure[i].text )
+					);
+				if ( structure[i].sections !== undefined ) {
+					item.append( buildList( structure[i].sections ) );
+				}
+				list.append( item );
+			}
+			return list;
+		}
+		// Build outline from wikitext
+		var outline = [];
+		var wikitext = '\n' + context.$textarea.val() + '\n';
+		var headings = wikitext.match( /\n={1,5}.*={1,5}(?=\n)/g );
+		var offset = 0;
+		headings = $.makeArray( headings );
+		for ( var h = 0; h < headings.length; h++ ) {
+			text = headings[h];
+			// Get position of first occurence
+			var position = wikitext.indexOf( text, offset );
+			// Update offset to avoid stumbling on duplicate headings
+			if ( position > offset ) {
+				offset = position;
+			} else if ( position == -1 ) {
+				// Not sure this is possible, or what should happen
+				continue;
+			}
+			// Trim off whitespace
+			text = $.trim( text );
+			// Detect the starting and ending heading levels
+			var startLevel = 0;
+			for ( var c = 0; c < text.length; c++ ) {
+				if ( text.charAt( c ) == '=' ) {
+					startLevel++;
+				} else {
+					break;
+				}
+			}
+			var endLevel = 0;
+			for ( var c = text.length - 1; c >= 0; c-- ) {
+				if ( text.charAt( c ) == '=' ) {
+					endLevel++;
+				} else {
+					break;
+				}
+			}
+			// Use the lowest number of =s as the actual level
+			var level = Math.min( startLevel, endLevel );
+			text = $.trim( text.substr( level, text.length - ( level * 2 ) ) );
+			// Add the heading data to the outline
+			outline[h] = { 'text': text, 'position': position, 'level': level, 'index': h + 1 };
+		}
+		// Normalize heading levels for list creation
+		// This is based on Linker::generateTOC() so, it should behave like the
+		// TOC on rendered articles does - which is considdered to be correct
+		// at this point in time.
+		var lastLevel = 0;
+		var nLevel = 0;
+		for ( var i = 0; i < outline.length; i++ ) {
+			if ( outline[i].level > lastLevel ) {
+				nLevel++;
+			}
+			else if ( outline[i].level < nLevel ) {
+				nLevel -= Math.max( 1, lastLevel - outline[i].level );
+			}
+			if ( nLevel <= 0 ) {
+				nLevel = 1;
+			}
+			outline[i].nLevel = nLevel;
+			lastLevel = outline[i].level;
+		}
+		// Recursively build the structure and add special item for
+		// section 0, if needed
+		var structure = buildStructure( outline );
+		if ( $( 'input[name=wpSection]' ).val() == '' )
+			structure.unshift( { 'text': wgPageName.replace(/_/g, ' '), 'level': 1, 'index': 0, 'position': 0 } );
+		context.modules.$toc.html( buildList( structure ) );
+		// Cache the outline for later use
+		context.data.outline = outline;
+	}
+}
+
+}; } ) ( jQuery );
