d1022a107c335f0c532ada55037ad6d31dcff594
emiller <emiller@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-04 12:53:55 +0000

    * Support v2 of Static Maps API (v1 is deprecated) * Fix links generated by Special:GoogleMapsKML
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@55815 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GoogleMaps/export/GoogleMapsImgExporter.php
+++ extensions/GoogleMaps/export/GoogleMapsImgExporter.php
@@ -2,50 +2,48 @@
 
 class GoogleMapsImgExporter extends GoogleMapsExporter {
     var $mApiKey;
-    var $mPathCount;
     var $mLanguageCode;
 
     function GoogleMapsImgExporter($pApiKey, $pLanguageCode) {
         $this->mApiKey = $pApiKey;
-        $this->mPathCount = 0;
         $this->mLanguageCode = $pLanguageCode;
     }
 
     function addHeader($o) {
-        $width  = $o['width']  > 512 ? 512 : $o['width'];
-        $height = $o['height'] > 512 ? 512 : $o['height'];
+        $width  = $o['width']  > 640 ? 640 : $o['width'];
+        $height = $o['height'] > 640 ? 640 : $o['height'];
         $this->mOutput .= '<img ';
         $this->mOutput .= "alt=\"Map\" ";
         $this->mOutput .= "height=\"{$height}\" width=\"{$width}\" ";
-        $this->mOutput .= 'src="http://maps.google.com/staticmap';
+        $this->mOutput .= 'src="http://maps.google.com/maps/api/staticmap';
         $this->mOutput .= "?center={$o['lat']}%2C{$o['lon']}";
         $this->mOutput .= "&zoom={$o['zoom']}";
         $this->mOutput .= "&size={$width}x{$height}";
         $this->mOutput .= "&key={$this->mApiKey}";
-        $this->mOutput .= "&hl={$this->mLanguageCode}";
-        $this->mOutput .= "&markers=";
+        $this->mOutput .= "&language={$this->mLanguageCode}";
+        $this->mOutput .= "&sensor=false";
+        if ($o['type'] == 'terrain' || $o['type'] == 'satellite' || $o['type'] == 'hybrid') {
+            $this->mOutput .= "&maptype={$o['type']}";
+        }
     }
 
     function addMarker($pLat, $pLon, $pIcon, $pTitle, $pTitleLink, $pCaption, $pMaxContent, $pIsLine) 
     {
-        if ($this->mPathCount == 0) {
-            $label = '';
-            if ($pIcon) {
-                $label = strtolower(substr($pIcon, 0, 1));
-            }
-            $this->mOutput .= urlencode("{$pLat},{$pLon},red{$label}|");
+        $this->mOutput .= "&markers=".urlencode("size:mid|color:red");
+        if ($pIcon) {
+            $this->mOutput .= urlencode("|label:" . strtoupper(substr($pIcon, 0, 1)));
         }
+        $this->mOutput .= urlencode("|{$pLat},{$pLon}");
     }
 
     function addPolyline( $pPolyline, $pLineColor, $pLineOpacity, $pStroke, $pFillColor, $pFillOpacity )
     {
-        if ($this->mPathCount == 0) { # limit 1
-            $this->mPathCount++;
-            $this->mOutput .= "&path=";
-            $this->mOutput .= urlencode("rgba:0x{$pLineColor}{$pLineOpacity},weight:{$pStroke}");
-            foreach ($pPolyline as $p) {
-                $this->mOutput .= urlencode("|{$p['lat']},{$p['lon']}");
-            }
+        $this->mOutput .= "&path=".urlencode("color:0x{$pLineColor}{$pLineOpacity}|weight:{$pStroke}");
+        if ( isset( $pFillColor ) ) {
+            $this->mOutput .= urlencode("|fillcolor:0x{$pFillColor}{$pFillOpacity}");
+        }
+        foreach ($pPolyline as $p) {
+            $this->mOutput .= urlencode("|{$p['lat']},{$p['lon']}");
         }
     }
 
