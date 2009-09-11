f1ae88c6b285640877918e3aebae451e474ed96f
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-11 18:52:00 +0000

    Bump style versions, recombine JS&CSS for my recent commit spree
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56204 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -21,16 +21,16 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/suggestions.css', 'version' => 5 ),
 				array( 'src' => 'css/wikiEditor.css', 'version' => 4 ),
 				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 5 ),
-				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 1 ),
+				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 2 ),
 				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 5 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 7 ),
+				array( 'src' => 'css/combined.css', 'version' => 8 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 7 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 8 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2' ),
 			),
 		)
@@ -61,18 +61,18 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.cookie.js', 'version' => 3 ),
 				array( 'src' => 'js/plugins/jquery.namespaceSelect.js', 'version' => 0 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 2 ),
-				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 9 ),
+				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 10 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 4 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 11 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 1 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 6 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 2 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 7 ),
 				array( 'src' => 'js/js2/jquery-ui-1.7.2.js', 'version' => '1.7.2x' ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 17 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 18 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 17 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 18 ),
 			),
 		),
 	);
