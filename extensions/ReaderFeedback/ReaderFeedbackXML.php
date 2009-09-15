e818defd2df00dcec5d8bfad86e2e9c513d10971
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-15 23:34:55 +0000

    Fix fatal: getNamespaceMenu() not copied over during split
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56401 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ReaderFeedback/ReaderFeedbackXML.php
+++ extensions/ReaderFeedback/ReaderFeedbackXML.php
@@ -2,6 +2,44 @@
 
 class ReaderFeedbackXML {
 	/**
+	 * Get a selector of rateable namespaces
+	 * @param int $selected, namespace selected
+	 * @param $all Mixed: Value of an item denoting all namespaces, or null to omit
+	 * @returns string
+	 */
+	public static function getNamespaceMenu( $selected=null, $all=null ) {
+		global $wgContLang, $wgFeedbackNamespaces;
+		$s = "<label for='namespace'>" . wfMsgHtml('namespace') . "</label>";
+		if( $selected !== '' ) {
+			if( is_null( $selected ) ) {
+				# No namespace selected; let exact match work without hitting Main
+				$selected = '';
+			} else {
+				# Let input be numeric strings without breaking the empty match.
+				$selected = intval($selected);
+			}
+		}
+		$s .= "\n<select id='namespace' name='namespace' class='namespaceselector'>\n";
+		$arr = $wgContLang->getFormattedNamespaces();
+		if( !is_null($all) ) {
+			$arr = array( $all => wfMsg( 'namespacesall' ) ) + $arr; // should be first
+		}
+		foreach( $arr as $index => $name ) {
+			# Content pages only (except 'all')
+			if( $index !== $all && !in_array($index, $wgFeedbackNamespaces) ) {
+				continue;
+			}
+			$name = $index !== 0 ? $name : wfMsg('blanknamespace');
+			if( $index === $selected ) {
+				$s .= "\t" . Xml::element("option", array("value" => $index, "selected" => "selected"), $name) . "\n";
+			} else {
+				$s .= "\t" . Xml::element("option", array("value" => $index), $name) . "\n";
+			}
+		}
+		$s .= "</select>\n";
+		return $s;
+	}
+	/**
 	 * Get tag dropdown select
 	 * @param int $selected, selected level
 	 * @returns string
