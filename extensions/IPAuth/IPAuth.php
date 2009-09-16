7471f49a7fb005636eb39465242270aabe811b91
daniel <daniel@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-16 12:50:55 +0000

    license statement for IPAuth
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56411 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/IPAuth/IPAuth.php
+++ extensions/IPAuth/IPAuth.php
@@ -1,4 +1,36 @@
 <?php 
+/*
+ Copyright (c) 2009, Wikimedia Deutschland (Daniel Kinzler)
+  All rights reserved.
+ 
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of Wikimedia Deutschland nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
+ 
+  THIS SOFTWARE IS PROVIDED BY WIKIMEDIA DEUTSCHLAND ''AS IS'' AND ANY
+  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL WIKIMEDIA DEUTSCHLAND BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ NOTE: This software is not released as a product. It was written primarily for
+ Wikimedia Deutschland's own use, and is made public as is, in the hope it may
+ be useful. Wikimedia Deutschland may at any time discontinue developing or
+ supporting this software. There is no guarantee any new versions or even fixes
+ for security issues will be released.
+*/
 
 if( !defined( 'MEDIAWIKI' ) ) {
 	echo( "IPAuth.\n" );
@@ -8,7 +40,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['other'][] = array( 
 	'path' => __FILE__,
 	'name' => 'IPAuth', 
-	'author' => 'Daniel Kinzler', 
+	'author' => 'Daniel Kinzler for Wikimedia Deutschland', 
 	'url' => 'http://mediawiki.org/wiki/Extension:IPAuth',
 	'description' => 'Automatic login from fixed IPs',
 );
