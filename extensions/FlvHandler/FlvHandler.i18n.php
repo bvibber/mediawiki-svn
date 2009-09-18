6dd60b6c7f180b3a664cbd49de333c86c5d28521
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-18 14:04:27 +0000

    Update description
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56599 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FlvHandler/FlvHandler.i18n.php
+++ extensions/FlvHandler/FlvHandler.i18n.php
@@ -11,6 +11,6 @@ $messages = array();
  * @author Adam Nielsen <malvineous@shikadi.net>
  */
 $messages['en'] = array(
-	'flvhandler_desc'   => 'Allow Flash Video (.flv) files to be used in standard image tags (e.g. <nowiki>[[Image:Movie.flv]]</nowiki>)',
+	'flvhandler_desc'   => 'Allow Flash Video (.flv) files to be added as standard files (e.g. <nowiki>[[File:Movie.flv]]</nowiki>)',
 	'flv-long-desc'     => '(Flash video, $1 × $2 pixels, file size: $3)'
 );
