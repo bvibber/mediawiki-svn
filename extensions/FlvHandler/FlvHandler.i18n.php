b9a942f7e29e5d42a673b91ddc78159c1eed7ed5
malvineous <malvineous@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-18 08:06:08 +0000

    [Extension:FlvHandler] Import new extension into SVN
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56586 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FlvHandler/FlvHandler.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Internationalisation file for FlvHandler extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Adam Nielsen <malvineous@shikadi.net>
+ */
+$messages['en'] = array(
+	'flvhandler_desc'   => 'Allow Flash Video (.flv) files to be used in standard image tags (e.g. <nowiki>[[Image:Movie.flv]]</nowiki>)',
+	'flv-long-desc'     => '(Flash video, $1 × $2 pixels, file size: $3)'
+);
+
+?>
