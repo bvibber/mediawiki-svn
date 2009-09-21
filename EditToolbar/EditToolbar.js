2fdf180a17fd25c27c63d5f35f57f7d1c63c24a4
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-21 18:05:10 +0000

    EditToolbar: (bug 20715) Search&Replace messes up in Opera
    This was caused by Opera treating newlines as one character in $textarea.val().indexOf() , but as two characters in $textarea.setSelection(). Worked around by an ugly temporary hack that detects whether we're on a broken browser and replacing \n with \r\n in $textarea.val() . Will redo properly after discussing with Trevor.
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@56723 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditToolbar/EditToolbar.js
+++ EditToolbar/EditToolbar.js
@@ -1179,6 +1179,40 @@ js2AddOnloadHook( function() {
 		dialog: {
 			buttons: {
 				'edittoolbar-tool-replace-button': function() {
+					function fixOperaBrokenness( s ) {
+						// This function works around Opera's
+						// broken newline handling in textareas.
+						// .val() has \n while selection functions
+						// treat newlines as \r\n
+						
+						if ( typeof $j.isOperaBroken == 'undefined' ) {
+							// Create a textarea inside a div
+							// with zero area, to hide it properly
+							var div = $j( '<div />' )
+								.height( 0 )
+								.width( 0 )
+								.insertBefore( $textarea );
+							var textarea = $j( '<textarea></textarea' )
+								.height( 0 )
+								.appendTo( div )
+								.val( "foo\r\nbar" );
+							
+							// Try to search&replace bar --> BAR
+							var index = textarea.val().indexOf( 'bar' );
+							textarea.select();
+							textarea.setSelection( index, index + 3 );
+							textarea.encapsulateSelection( '', 'BAR', '', false, true );
+							if ( textarea.val().substr( -1 ) == 'R' )
+								$j.isOperaBroken = false;
+							else
+								$j.isOperaBroken = true; 
+							div.remove();
+						}
+						if ( $j.isOperaBroken )
+							s = s.replace( /\n/g, "\r\n" );
+						return s;
+					}
+
 					var searchStr = $j( '#edittoolbar-replace-search' ).val();
 					var replaceStr = $j( '#edittoolbar-replace-replace' ).val();
 					var flags = '';
@@ -1193,13 +1227,14 @@ js2AddOnloadHook( function() {
 					}
 					var regex = new RegExp( searchStr, flags );
 					var $textarea = $j(this).data( 'context' ).$textarea;
-					var matches = $textarea.val().match( regex );
+					var text = fixOperaBrokenness( $textarea.val() );
+					var matches = text.match( regex );
 					if ( !matches ) {
 						alert( gM( 'edittoolbar-tool-replace-nomatch' ) );
 					} else {
 						var start, end;
 						for ( var i = 0; i < matches.length; i++ ) {
-							start = $textarea.val().indexOf( matches[i] );
+							start = text.indexOf( matches[i] );
 							end = start + matches[i].length;
 							$textarea.setSelection( start, end );
 							$textarea.encapsulateSelection( '', replaceStr, '', false, true );
