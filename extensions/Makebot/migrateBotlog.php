ceb88f4f64072ff470745ce3221eae27ddaee9da
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-24 22:59:17 +0000

    merged r56904
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@56905 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Makebot/migrateBotlog.php
+++ extensions/Makebot/migrateBotlog.php
@@ -27,28 +27,36 @@ class migrateBotlog extends Maintenance {
 		// Determining what groups the account was in before the change
 		// would be difficult and unnecessary 99.9% of the time, so we just
 		// assume the account was in no other groups
-		$params = array( 'grant' => "\nbot",
-			'revoke' => "bot\n"
-		);
+		$params = array( 'grant' => "\nbot", 'revoke' => "bot\n" );
 
 		$logrows = $dbw->select( 'logging',
 			array( 'log_id', 'log_action' ),
-			array( 'log_type' => 'makebot' ),
+			array( 'log_type' => 'makebot',
+				'log_action' => array('grant','revoke') ), // sanity check
 			__METHOD__
 		);
 		$count = $logrows->numRows();	
 		$this->output( "Updating $count entries in the logging table\n" );
+		
+		$batch = 0;
 		foreach ( $logrows as $row ) {
 			$dbw->update( 'logging',
-				array( 'log_action' => 'rights', 'log_type'=>'rights', 'log_params' => $params[$row->log_action] ),
+				array( 'log_action' => 'rights', 'log_type'=>'rights',
+					'log_params' => $params[$row->log_action] ),
 				array( 'log_id' => $row->log_id ),
 				__METHOD__
 			);
+			$batch++;
+			if( $batch == 100 ) {
+				wfWaitForSlaves( 5 );
+				$batch = 0;
+			}
 		}
 
 		$rcrows = $dbw->select( 'recentchanges',
 			array( 'rc_id', 'rc_log_action' ),
-			array( 'rc_log_type' => 'makebot' ),
+			array( 'rc_log_type' => 'makebot',
+				'rc_log_action' => array('grant','revoke') ), // sanity check
 			__METHOD__
 		);
 		$count = $rcrows->numRows();	
@@ -59,7 +67,8 @@ class migrateBotlog extends Maintenance {
 				array( 'rc_id' => $row->rc_id ),
 				__METHOD__
 			);
-		}	
+		}
+		$this->output( "Done!\n" );
 	}
 }
 
