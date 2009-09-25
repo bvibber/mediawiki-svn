cad6148c1f936abe79b12fe79e5a4bb44fac917b
rarohde <rarohde@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-25 18:24:42 +0000

    New Extension: FilePageMasking
    Allows file description page URLs to be automatically rewritten by Mediawiki so that they don't appear to end in a file extension.  This avoids confusing systems and search engines that use file extensions on URLs to determine behavior.
    
    By default, it converts ".xxx" to "_xxx" for calls to file description pages.
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56925 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/FilePageMasking/FilePageMasking.i18n.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Internationalisation file for FilePageMasking extension.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'file_masking_desc'              => 'Rewrites ".xxx" into "_xxx" in image description page links',
+);
