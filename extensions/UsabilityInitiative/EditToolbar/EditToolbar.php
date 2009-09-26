230144696cc8822a8b96f7ceb20100ab4fc7126a
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-26 11:23:07 +0000

    EditToolbar: (bug 20605) Also trigger Insert/Replace button when Enter key is pressed on the dialog. This doesn't work in all browsers: Chrome, for one, doesn't seem to catch keypress events at all when the dialog or one of its non-form field children is selected.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56948 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
+++ extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -19,7 +19,7 @@
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 37;
+$wgEditToolbarStyleVersion = 38;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
