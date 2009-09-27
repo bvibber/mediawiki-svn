5292fc6614d04cab8f25c79b3b610a3cafdd838c
tomasz <tomasz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-27 03:53:30 +0000

    fixing to follow up with r56977
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@56978 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionReporting/ContributionTrackingStatistics_body.php
+++ extensions/ContributionReporting/ContributionTrackingStatistics_body.php
@@ -63,7 +63,7 @@ class SpecialContributionTrackingStatistics extends SpecialPage {
 				if ( ! in_array($template[0], $wgAllowedTemplates ) )
 					continue;
 				// Pull together templates, clicks, donations, conversion rate
-				$conversion_rate = ( $template[2] == 0 ) ? 0 : ( $template[2] / $template[1] ) * 100; 
+				$conversion_rate = ( $template[1] == 0 ) ? 0 : ( $template[2] / $template[1] ) * 100; 
 				$amount = ( $template[3] == 0 ) ? 0 : $template[3];
 
 				$htmlOut .= Xml::tags( 'tr', null,
@@ -135,7 +135,7 @@ class SpecialContributionTrackingStatistics extends SpecialPage {
 				if ( ! in_array($template[0], $wgAllowedTemplates ) )
 					continue;
 				// Pull together templates, clicks, donations, conversion rate
-				$conversion_rate = ( $template[2] == 0 ) ? 0 : $template[2] / $template[1] * 100; 
+				$conversion_rate = ( $template[1] == 0 ) ? 0 : $template[2] / $template[1] * 100; 
 				$amount = ( $template[3] == 0 ) ? 0 : $template[3];
 				
 				$htmlOut .= Xml::tags( 'tr', null,
