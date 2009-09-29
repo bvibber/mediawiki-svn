a819619c71914bea4057e76234e8e3eecd5d7162
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-09-29 17:53:22 +0000

    Merge r57040 & r57041 mysql 4 compat fixes from trunk
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@57066 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UserDailyContribs/UserDailyContribs.hooks.php
+++ extensions/UsabilityInitiative/UserDailyContribs/UserDailyContribs.hooks.php
@@ -29,18 +29,15 @@ class UserDailyContribsHooks {
 		global $wgUser;
 		$today = gmdate( 'Ymd', time() );
 		$dbw = wfGetDB( DB_MASTER );
-		/*
-		//there seems no way to set contribs to contribs+1 in a reasonably fast manner in this framework
-		try{
-			$dbw->insert("user_daily_contribs", array("user_id" => $wgUser->getId(), "day" => $today, "contribs" => 1), __METHOD__); 
-		}
-		catch(Exception $e){
-			$dbw->update( "user_daily_contribs", array( "contribs" => "contribs+1"), array("user_id" => $wgUser->getId(), "day" => $today), __METHOD__);
-		}
-		*/
-		$sql = 
-		"INSERT INTO user_daily_contribs (user_id,day,contribs) VALUES ({$wgUser->getId()},$today,1) ON DUPLICATE KEY UPDATE contribs=contribs+1;";
+		
+		//normal $db->update doesn't support SQL variables yet
+		$sql = "UPDATE user_daily_contribs SET contribs=contribs+1 WHERE day = $today AND user_id = {$wgUser->getId()}";
 		$dbw->query($sql, __METHOD__);
+		
+		if($dbw->affectedRows() == 0){
+			$dbw->insert("user_daily_contribs", array("user_id" => $wgUser->getId(), "day" => $today, "contribs" => 1), __METHOD__);	
+		}
+	
 		return true;
 	}
 	
