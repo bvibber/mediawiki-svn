73d989834d7214e494b1bb505b225a490d99dc1d
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-01 01:07:21 +0000

    Tag for version 0.9.1
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@57181 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ ExternalData/REL_0_9_1/ExternalData.i18n.magic.php
@@ -0,0 +1,32 @@
+<?php
+
+$magicWords = array();
+
+$magicWords['en'] = array(
+	'get_external_data'  => array ( 0, 'get_external_data' ),
+	'get_ldap_data'      => array ( 0, 'get_ldap_data' ),
+	'get_db_data'        => array ( 0, 'get_db_data' ),
+	'external_value'     => array ( 0, 'external_value' ),
+	'for_external_table' => array ( 0, 'for_external_table' ),
+);
+
+$magicWords['ar'] = array(
+	'external_value'       => array( '0', 'قيمة_خارجية', 'external_value' ),
+	'for_external_table'   => array( '0', 'لجدول_خارجي', 'for_external_table' ),
+);
+
+$magicWords['nl'] = array(
+	'get_external_data'    => array( '0', 'externe_gegevens_ophalen', 'get_external_data' ),
+	'get_ldap_data'        => array( '0', 'ldap_gegevens_ophalen', 'get_ldap_data' ),
+	'get_db_data'          => array( '0', 'db_gegevens_ophalen', 'get_db_data' ),
+	'external_value'       => array( '0', 'externe_waarde', 'external_value' ),
+	'for_external_table'   => array( '0', 'voor_externe_tabel', 'for_external_table' ),
+);
+
+$magicWords['ru'] = array(
+	'get_external_data'    => array( '0', 'получить_внешние_данные', 'get_external_data' ),
+	'get_ldap_data'        => array( '0', 'получить_данные_ldap', 'get_ldap_data' ),
+	'get_db_data'          => array( '0', 'получить_данные_db', 'get_db_data' ),
+	'external_value'       => array( '0', 'внешнее_значение', 'external_value' ),
+	'for_external_table'   => array( '0', 'для_внешней_таблицы', 'for_external_table' ),
+);
