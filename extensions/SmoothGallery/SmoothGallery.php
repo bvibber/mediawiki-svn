01d212238051a72b04864c6bebfd41472f3569d0
laner <laner@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-03 23:02:59 +0000

    Fix backwards compatability with MediaWiki less than 1.16alpha
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@57348 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SmoothGallery/SmoothGallery.php
+++ extensions/SmoothGallery/SmoothGallery.php
@@ -35,7 +35,7 @@ if ( !defined( 'MEDIAWIKI' ) )
 $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'SmoothGallery parser extension',
-	'version'        => '1.1f',
+	'version'        => '1.1g',
 	'author'         => 'Ryan Lane',
 	'description'    => 'Allows users to create galleries with images that have been uploaded. Allows most options of SmoothGallery',
 	'descriptionmsg' => 'smoothgallery-desc',
@@ -48,7 +48,11 @@ $wgHooks['OutputPageParserOutput'][] = 'smoothGalleryParserOutput';
 
 $dir = dirname( __FILE__ ) . '/';
 $wgExtensionMessagesFiles['SmoothGallery'] = $dir . 'SmoothGallery.i18n.php';
-$wgExtensionMessagesFiles['SmoothGalleryMagic'] = $dir . 'SmoothGallery.i18n.magic.php';
+if( version_compare( $wgVersion, '1.16alpha', '>=' ) ) {
+	$wgExtensionMessagesFiles['SmoothGalleryMagic'] = $dir . 'SmoothGallery.i18n.magic.php';
+} else {
+	$wgHooks['LanguageGetMagic'][] = 'smoothGalleryLanguageGetMagic';
+}
 $wgAutoloadClasses['SmoothGallery'] = $dir . 'SmoothGalleryClass.php';
 $wgAutoloadClasses['SmoothGalleryParser'] = $dir . 'SmoothGalleryParser.php';
 
@@ -140,5 +144,14 @@ function smoothGalleryParserOutput( &$outputPage, &$parserOutput )  {
 	if ( !empty( $parserOutput->mSmoothGallerySetTag ) ) {
 		SmoothGallery::setGallerySetHeaders( $outputPage );
 	}
+	return true;
+}
+
+/**
+ * We ignore langCode - parser function names can be translated but
+ * we are not using this feature
+ */
+function smoothGalleryLanguageGetMagic( &$magicWords, $langCode ) {
+	$magicWords['sgallery']  = array( 0, 'sgallery' );
 	return true;
 }
