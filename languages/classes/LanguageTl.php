5734b80c7e036249a1c72ab4e9d1c51eb7584410
brion <brion@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-07 17:20:25 +0000

    Cleanup merge -- some files got doubled. :P
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@57467 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- languages/classes/LanguageTl.php
+++ languages/classes/LanguageTl.php
@@ -14,19 +14,3 @@ class LanguageTl extends Language {
 		return ($count <= 1) ? $forms[0] : $forms[1];
 	}
 }
-<?php
-/**
- *
- * @ingroup Language
- */
-class LanguageTl extends Language {
-	/**
-	 * Use singular form for zero
-	 */
-	function convertPlural( $count, $forms ) {
-		if ( !count($forms) ) { return ''; }
-		$forms = $this->preConvertPlural( $forms, 2 );
-
-		return ($count <= 1) ? $forms[0] : $forms[1];
-	}
-}
