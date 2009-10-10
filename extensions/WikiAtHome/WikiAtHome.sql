206f757706a1415d7f5e89c735a110ea0e2c2c0c
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-10 01:00:11 +0000

    db schema updates
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@57599 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiAtHome/WikiAtHome.sql
+++ extensions/WikiAtHome/WikiAtHome.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `wah_jobqueue` (
   PRIMARY KEY  (`job_id`),
   KEY `job_last_assigned_user_id` (`job_last_assigned_user_id`),
   KEY `job_set_id` (`job_set_id`,`job_order_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `wah_jobset` (
   `set_title` varchar(255) default NULL,
   `set_jobs_count` int(11) unsigned NOT NULL,
   `set_encodekey` varchar(40) default NULL,
+  `set_job_type` enum('transcode','flatten') NOT NULL,
   `set_creation_time` int(14) NOT NULL,
   `set_done_time` int(14) default NULL,
   `set_failed` tinyint(1) default NULL,
@@ -38,5 +39,6 @@ CREATE TABLE IF NOT EXISTS `wah_jobset` (
   PRIMARY KEY  (`set_id`),
   KEY `set_namespace` (`set_namespace`,`set_title`),
   KEY `set_done_time` (`set_done_time`),
-  KEY `set_client_count` (`set_client_count`)
+  KEY `set_client_count` (`set_client_count`),
+  KEY `set_job_type` (`set_job_type`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
