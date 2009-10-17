8d972bb4ec5cd54c6784653b4759c4348fee068a
aaron <aaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-17 04:57:16 +0000

    merged r57850: review checkbox behavior fix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@57851 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/FlaggedRevs/FlaggedRevs.class.php
+++ extensions/FlaggedRevs/FlaggedRevs.class.php
@@ -911,8 +911,7 @@ class FlaggedRevs {
 	}
 	
 	/**
-	* Clears cache for a page when merges are done.
-	* We may have lost the stable revision to another page.
+	* Resets links for a page when changed (other than edits)
 	*/
 	public static function articleLinksUpdate( $article ) {
 		global $wgUser, $wgParser;
@@ -930,7 +929,7 @@ class FlaggedRevs {
 	}
 
 	/**
-	* Clears cache for a page when revisiondelete/undelete is used
+	* Resets links for a page when changed (other than edits)
 	*/
 	public static function titleLinksUpdate( $title ) {
 		return self::articleLinksUpdate( new Article($title) );
