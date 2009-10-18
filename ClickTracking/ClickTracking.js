16db34b0d9cb1238e23d2773db43b13b036006ae
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-18 13:31:28 +0000

    ClickTracking: *coding style cleanup *marked SpecialPage's constructor and execute func as public *fixed some message names in SpecialClickTracking.php *capitalized SQL keywords
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@57865 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ClickTracking/ClickTracking.js
+++ ClickTracking/ClickTracking.js
@@ -1,6 +1,6 @@
 (function($) {
 
-	if(!wgClickTrackingIsThrottled){
+	if( !wgClickTrackingIsThrottled ) {
 		// creates 'track action' function to call the clicktracking API and send the ID
 		$.trackAction = function ( id ) {
 			$j.post( wgScriptPath + '/api.php', { 'action': 'clicktracking', 'eventid': id, 'token': wgTrackingToken } );
