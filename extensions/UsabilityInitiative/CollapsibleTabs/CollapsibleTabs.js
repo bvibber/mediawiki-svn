4671e9f26624125cc402657c833b0d766cc9f93c
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-19 20:30:43 +0000

    Adding animation to the collapsible tabs. Tested in FF3.5 and Safari 4 on mac
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@57912 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/CollapsibleTabs/CollapsibleTabs.js
+++ extensions/UsabilityInitiative/CollapsibleTabs/CollapsibleTabs.js
@@ -1,6 +1,6 @@
 js2AddOnloadHook( function() {
 	
-	/* ANIMATION NOT READY FOR PRIME TIME 
+	//Overloading the moveToCollapsed function to animate the transition 
 	$j.collapsibleTabs.moveToCollapsed = function( ele ) {
 		$moving = $j(ele);
 		$j($moving.data('collapsibleTabsSettings').expandedContainer).data('collapsibleTabsSettings').shifting = true;
@@ -9,15 +9,16 @@ js2AddOnloadHook( function() {
 		target = $moving.data('collapsibleTabsSettings').collapsedContainer;
 		// $moving.hide(500);
 		$moving.css("position", "relative").css('right',0);
-		$moving.animate({width: '0px'},"normal",function(){
+		$moving.animate({width: '1px'},"normal",function(){
 			//$j(this).remove().prependTo(target).data('collapsibleTabsSettings', data).show();
+			$j(this).hide();
 			$j(this).remove().prependTo(target).data('collapsibleTabsSettings', data);
-			$j(this).css("position", "static").css('right','auto').css("width", "auto");
+			$j(this).attr('style', '');
 			$j($j(ele).data('collapsibleTabsSettings').expandedContainer).data('collapsibleTabsSettings').shifting = false;
 		});
-		//$moving.remove().prependTo(target).data('collapsibleTabsSettings', data);
 	};
 	
+	// Overloading the moveToExpanded function to animate the transition
 	$j.collapsibleTabs.moveToExpanded = function( ele ) {
 		$moving = $j(ele);
 		$j($moving.data('collapsibleTabsSettings').expandedContainer).data('collapsibleTabsSettings').shifting = true;
@@ -25,22 +26,26 @@ js2AddOnloadHook( function() {
 		// remove this element from where it's at and put it in the dropdown menu
 		target = $moving.data('collapsibleTabsSettings').prevElement;
 		expandedWidth = $moving.data('collapsibleTabsSettings').expandedWidth;
-		$moving.css("position", "relative").css('right',0).css('width','0px');
-		$moving.remove().insertAfter(target).data('collapsibleTabsSettings', data)
+		$moving.css("position", "relative").css('left',0).css('width','1px');
+		$moving.remove().css('width','1px').insertAfter(target).data('collapsibleTabsSettings', data)
 			.animate({width: expandedWidth+"px"}, "normal", function(){
-			//$j(this).remove().prependTo(target).data('collapsibleTabsSettings', data).show();
-			$j(this).css("position", "static").css('right','auto');
+			$j(this).attr('style', '');
 			$j($moving.data('collapsibleTabsSettings').expandedContainer).data('collapsibleTabsSettings').shifting = false;
 
 		});
 	};
-	*/
-	if($j("#p-cactions li").length==0) $j("#p-cactions").removeClass("emptyPortlet").addClass("filledPortlet");
-	$j('#p-views ul').collapsibleTabs().bind("beforeTabCollapse", function(){
-		$j("#p-cactions").removeClass("emptyPortlet").addClass("filledPortlet");
+	
+	// Bind callback functions to animate our drop down menu in and out
+	// and then call the collapsibleTabs function on the menu 
+	$j('#p-views ul').bind("beforeTabCollapse", function(){
+		if($j('#p-cactions').css('display')=='none')
+		$j("#p-cactions").addClass("filledPortlet").removeClass("emptyPortlet")
+			.find('h5').css('width','1px').animate({'width':'26px'}, 390);
 	}).bind("beforeTabExpand", function(){
-		if($j("#p-cactions li.collapsible").length==1) 
-			$j("#p-cactions").removeClass("filledPortlet").addClass("emptyPortlet");
-	});
+		if($j('#p-cactions li').length==1)
+		$j("#p-cactions h5").animate({'width':'1px'},370, function(){
+			$j(this).attr('style','').parent().addClass("emptyPortlet").removeClass("filledPortlet");
+		});
+	}).collapsibleTabs();
 	
 });
