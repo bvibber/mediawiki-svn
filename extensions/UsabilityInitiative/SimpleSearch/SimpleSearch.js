67a9d779a759f5279eb610a05f438998eb1c697d
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-24 19:41:59 +0000

    SimpleSearch: Fix indentation, docs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58095 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/SimpleSearch/SimpleSearch.js
+++ extensions/UsabilityInitiative/SimpleSearch/SimpleSearch.js
@@ -41,79 +41,84 @@ js2AddOnloadHook( function() {
 				$j(this).parent().find( 'label' ).show();
 			}
 		});
-		$j( '#searchInput, #searchInput2, #powerSearchText, #searchText' ).suggestions( {
-			fetch: function( query ) {
-				var $this = $j(this);
-				var request = $j.ajax( {
-					url: wgScriptPath + '/api.php',
-					data: {
-						'action': 'opensearch',
-						'search': query,
-						'namespace': 0,
-						'suggest': ''
-					},
-					dataType: 'json',
-					success: function( data ) {
-						$this.suggestions( 'suggestions', data[1] );
-					}
-				});
-				$j(this).data( 'request', request );
-			},
-			cancel: function () {
-				var request = $j(this).data( 'request' );
-				// If the delay setting has caused the fetch to have not even happend yet, the request object will
-				// have never been set
-				if ( request && typeof request.abort == 'function' ) {
-					request.abort();
-					$j(this).removeData( 'request' );
-				}
-			},
-			result: {
-				select: function( $textbox ) {
-					$textbox.closest( 'form' ).submit();
-				}
-			},
-			delay: 120
-		} );
-		$j( '#searchInput' ).suggestions( {
-			special: {
-				render: function( query ) {
-					var perfectMatch = false;
-					$j(this).closest( '.suggestions' ).find( '.suggestions-results div' ).each( function() {
-						if ( $j(this).data( 'text' ) == query ) {
-							perfectMatch = true;
-						}
-					} );
-					if ( perfectMatch ) {
-						if ( $j(this).children().size() == 0  ) {
-							$j(this).show();
-							$label = $j( '<div />' )
-								.addClass( 'special-label' )
-								.text( gM( 'simplesearch-containing' ) )
-								.appendTo( $j(this) );
-							$query = $j( '<div />' )
-								.addClass( 'special-query' )
-								.text( query )
-								.appendTo( $j(this) );
-							$query.autoEllipse();
-						} else {
-							$j(this).find( '.special-query' )
-								.empty()
-								.text( query )
-								.autoEllipse();
-						}
-					} else {
-						$j(this).hide();
-						$j(this).empty();
-					}
+	$j( '#searchInput, #searchInput2, #powerSearchText, #searchText' ).suggestions( {
+		fetch: function( query ) {
+			var $this = $j(this);
+			var request = $j.ajax( {
+				url: wgScriptPath + '/api.php',
+				data: {
+					'action': 'opensearch',
+					'search': query,
+					'namespace': 0,
+					'suggest': ''
 				},
-				select: function( $textbox ) {
-					$textbox.closest( 'form' ).append(
-						$j( '<input />' ).attr( { 'type': 'hidden', 'name': 'fulltext', 'value': 1 } )
-					);
-					$textbox.closest( 'form' ).submit();
+				dataType: 'json',
+				success: function( data ) {
+					$this.suggestions( 'suggestions', data[1] );
+				}
+			});
+			$j(this).data( 'request', request );
+		},
+		cancel: function () {
+			var request = $j(this).data( 'request' );
+			// If the delay setting has caused the fetch to have not even happend yet, the request object will
+			// have never been set
+			if ( request && typeof request.abort == 'function' ) {
+				request.abort();
+				$j(this).removeData( 'request' );
+			}
+		},
+		result: {
+			select: function( $textbox ) {
+				$textbox.closest( 'form' ).submit();
+			}
+		},
+		delay: 120
+	} );
+	$j( '#searchInput' ).suggestions( {
+		result: {
+			select: function( $textbox ) {
+				$textbox.closest( 'form' ).submit();
+			}
+		},
+		special: {
+			render: function( query ) {
+				var perfectMatch = false;
+				$j(this).closest( '.suggestions' ).find( '.suggestions-results div' ).each( function() {
+					if ( $j(this).data( 'text' ) == query ) {
+						perfectMatch = true;
+					}
+				} );
+				if ( perfectMatch ) {
+					if ( $j(this).children().size() == 0  ) {
+						$j(this).show();
+						$label = $j( '<div />' )
+							.addClass( 'special-label' )
+							.text( gM( 'simplesearch-containing' ) )
+							.appendTo( $j(this) );
+						$query = $j( '<div />' )
+							.addClass( 'special-query' )
+							.text( query )
+							.appendTo( $j(this) );
+						$query.autoEllipse();
+					} else {
+						$j(this).find( '.special-query' )
+							.empty()
+							.text( query )
+							.autoEllipse();
+					}
+				} else {
+					$j(this).hide();
+					$j(this).empty();
 				}
 			},
-			$region: $j( '#simpleSearch')
-		} );
+			select: function( $textbox ) {
+				$textbox.closest( 'form' ).append(
+					$j( '<input />' ).attr( { 'type': 'hidden', 'name': 'fulltext', 'value': 1 } )
+				);
+				$textbox.closest( 'form' ).submit();
+			}
+		},
+		$region: $j( '#simpleSearch' )
+	} );
 });
