35bbbdaf726d76d90d6f983c6d190f33d474d60c
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-26 22:48:04 +0000

    Correcting bug #21293 introduced in r57982 - removing a trailing comma that was causing problems in IE
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58170 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
+++ extensions/UsabilityInitiative/EditToolbar/EditToolbar.php
@@ -13,13 +13,13 @@
  *
  * @author Trevor Parscal <tparscal@wikimedia.org>, Roan Kattouw <roan.kattouw@gmail.com>
  * @license GPL v2 or later
- * @version 0.1.2
+ * @version 0.1.3
  */
 
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 62;
+$wgEditToolbarStyleVersion = 63;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
@@ -48,7 +48,7 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'EditToolbar',
 	'author' => 'Trevor Parscal',
-	'version' => '0.1.2',
+	'version' => '0.1.3',
 	'url' => 'http://www.mediawiki.org/wiki/Extension:UsabilityInitiative',
 	'descriptionmsg' => 'edittoolbar-desc',
 );
