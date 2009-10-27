d600b2ece612607e41a1eb988622d053bb39c175
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-27 22:31:25 +0000

    Allowing the advanced toolbar of the wikieditor to gracefully handle a browser resize. Icons now bump to the next line when width is limited, and continue to be show, while the textarea and navigable TOC move down to allow the toolbar the extra space it needs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@58229 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- js/plugins/jquery.wikiEditor.toolbar.js
+++ js/plugins/jquery.wikiEditor.toolbar.js
@@ -470,11 +470,14 @@ fn : {
 						if ( show ) {
 							$section.fadeIn( 'fast' );
 							$sections.animate( { 'height': $section.outerHeight() }, $section.outerHeight() * 2, function() { 
-								$(this).css('overflow', 'visible'); } );
+								$(this).css('overflow', 'visible').css('height', 'auto'); 
+							} );
 							$(this).addClass( 'current' );
 						} else {
-							$sections.animate( { 'height': 0 }, $section.outerHeight() * 2, function() { 
-								$(this).css('overflow', 'visible'); } );
+							$sections.css('height', $section.outerHeight() )
+								.animate( { 'height': 0 }, $section.outerHeight() * 2, function() { 
+									$(this).css('overflow', 'visible'); 
+								} );
 						}
 						// Click tracking
 						if($.trackAction != undefined){
