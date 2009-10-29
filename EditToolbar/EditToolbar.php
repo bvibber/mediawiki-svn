81f0d07d0c695a7439950e97bd7df130ec4dd982
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-10-29 12:41:03 +0000

    EditToolbar: (bug 21343) Treat regexes as multiline in search&replace, so ^ and $ match the start and end of lines instead of the entire textbox contents. I hear this breaks older browsers, but if those browsers are so old they're not supported by wikiEditor anyway, we can get away with it
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@58310 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- EditToolbar/EditToolbar.php
+++ EditToolbar/EditToolbar.php
@@ -19,7 +19,7 @@
 /* Configuration */
 
 // Bump the version number every time you change any of the .css/.js files
-$wgEditToolbarStyleVersion = 65;
+$wgEditToolbarStyleVersion = 66;
 
 // Set this to true to simply override the stock toolbar for everyone
 $wgEditToolbarGlobalEnable = false;
