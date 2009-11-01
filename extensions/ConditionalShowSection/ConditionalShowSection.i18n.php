876320b1288cb1a9a0df3e8efc2460fd961c86f6
jdpond <jdpond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-01 00:46:37 +0000

    The <cshow> tag in conditionalshowsection-desc choked Special:Version, so removed.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58397 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ConditionalShowSection/ConditionalShowSection.i18n.php
+++ extensions/ConditionalShowSection/ConditionalShowSection.i18n.php
@@ -12,7 +12,7 @@ $messages = array();
  * @author Jean-Lou Dupont
  */
 $messages['en'] = array(
-	'conditionalshowsection-desc' => 'Adding the <cshow> tag to conditionally show/hide a section of wikitext',
+	'conditionalshowsection-desc' => 'Adding the cshow tag to conditionally show/hide a section of wikitext',
 );
 
 /** Message documentation (Message documentation)
@@ -26,119 +26,119 @@ $messages['qqq'] = array(
  * @author EugeneZelenko
  */
 $messages['be-tarask'] = array(
-	'conditionalshowsection-desc' => 'Дадае тэг <cshow> для ўмоўнага паказу/хаваньня часткі вікі-тэксту',
+	'conditionalshowsection-desc' => 'Дадае тэг cshow для ўмоўнага паказу/хаваньня часткі вікі-тэксту',
 );
 
 /** Breton (Brezhoneg)
  * @author Fulup
  */
 $messages['br'] = array(
-	'conditionalshowsection-desc' => "Oc'h ouzhpennañ ar valizenn the <cshow> da ziskouez/kuzhat ur rann wikitestenn",
+	'conditionalshowsection-desc' => "Oc'h ouzhpennañ ar valizenn the cshow da ziskouez/kuzhat ur rann wikitestenn",
 );
 
 /** Bosnian (Bosanski)
  * @author CERminator
  */
 $messages['bs'] = array(
-	'conditionalshowsection-desc' => 'Dodaje oznaku <cshow> za uslovno prikazivanje/sakrivanje sekcija wikiteksta',
+	'conditionalshowsection-desc' => 'Dodaje oznaku cshow za uslovno prikazivanje/sakrivanje sekcija wikiteksta',
 );
 
 /** Catalan (Català)
  * @author Ssola
  */
 $messages['ca'] = array(
-	'conditionalshowsection-desc' => "Afegint l'etiqueta <cshow> per mostrar o amagar condicionadament una secció de Viquitext.",
+	'conditionalshowsection-desc' => "Afegint l'etiqueta cshow per mostrar o amagar condicionadament una secció de Viquitext.",
 );
 
 /** German (Deutsch)
  * @author PasO
  */
 $messages['de'] = array(
-	'conditionalshowsection-desc' => 'Ergänzt das <cshow>-Tag zur bedingten Anzeige eines Wikitext-Abschnitts',
+	'conditionalshowsection-desc' => 'Ergänzt das cshow-Tag zur bedingten Anzeige eines Wikitext-Abschnitts',
 );
 
 /** Lower Sorbian (Dolnoserbski)
  * @author Michawiki
  */
 $messages['dsb'] = array(
-	'conditionalshowsection-desc' => 'Toflicku <cshow> pśidaś, aby se wótrězk wikiteksta pokazał/schował',
+	'conditionalshowsection-desc' => 'Toflicku cshow pśidaś, aby se wótrězk wikiteksta pokazał/schował',
 );
 
 /** Greek (Ελληνικά)
  * @author Omnipaedista
  */
 $messages['el'] = array(
-	'conditionalshowsection-desc' => 'Προσθέτει την ετικέτα <cshow> για να προβάλλει/αποκρύψει υπό όρους τμήμα του βικικειμένου',
+	'conditionalshowsection-desc' => 'Προσθέτει την ετικέτα cshow για να προβάλλει/αποκρύψει υπό όρους τμήμα του βικικειμένου',
 );
 
 /** Spanish (Español)
  * @author Crazymadlover
  */
 $messages['es'] = array(
-	'conditionalshowsection-desc' => 'Agregando la etiqueta <cshow> para mostrar/ocultar condicionalmente una sección de wikitexto',
+	'conditionalshowsection-desc' => 'Agregando la etiqueta cshow para mostrar/ocultar condicionalmente una sección de wikitexto',
 );
 
 /** French (Français)
  * @author PieRRoMaN
  */
 $messages['fr'] = array(
-	'conditionalshowsection-desc' => 'Ajout de la balise <cshow> pour afficher ou masquer conditionnellement une section de wikitexte',
+	'conditionalshowsection-desc' => 'Ajout de la balise cshow pour afficher ou masquer conditionnellement une section de wikitexte',
 );
 
 /** Galician (Galego)
  * @author Toliño
  */
 $messages['gl'] = array(
-	'conditionalshowsection-desc' => 'Engade a etiqueta <cshow> para mostrar ou agochar condicionalmente unha sección de texto wiki',
+	'conditionalshowsection-desc' => 'Engade a etiqueta cshow para mostrar ou agochar condicionalmente unha sección de texto wiki',
 );
 
 /** Swiss German (Alemannisch)
  * @author Als-Holder
  */
 $messages['gsw'] = array(
-	'conditionalshowsection-desc' => 'Fiegt s <cshow>-Tag yy go je no Bedingig e Abschnitt vum Wikitext zeige/verstecke',
+	'conditionalshowsection-desc' => 'Fiegt s cshow-Tag yy go je no Bedingig e Abschnitt vum Wikitext zeige/verstecke',
 );
 
 /** Hebrew (עברית)
  * @author Rotemliss
  */
 $messages['he'] = array(
-	'conditionalshowsection-desc' => 'הוספת התגית <cshow> כדי להציג/להסתיר, לפי תנאים, פרק בטקסט ויקי',
+	'conditionalshowsection-desc' => 'הוספת התגית cshow כדי להציג/להסתיר, לפי תנאים, פרק בטקסט ויקי',
 );
 
 /** Upper Sorbian (Hornjoserbsce)
  * @author Michawiki
  */
 $messages['hsb'] = array(
-	'conditionalshowsection-desc' => 'Tafličku <cshow> přidać, zo by so wotrězk wikiteksta pokazał/schował',
+	'conditionalshowsection-desc' => 'Tafličku cshow přidać, zo by so wotrězk wikiteksta pokazał/schował',
 );
 
 /** Hungarian (Magyar)
  * @author Glanthor Reviol
  */
 $messages['hu'] = array(
-	'conditionalshowsection-desc' => '<cshow> tag a wikiszöveg egy szakaszának feltételes megjelenítéséhez/elrejtéséhez',
+	'conditionalshowsection-desc' => 'cshow tag a wikiszöveg egy szakaszának feltételes megjelenítéséhez/elrejtéséhez',
 );
 
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
 $messages['ia'] = array(
-	'conditionalshowsection-desc' => 'Adde le etiquetta <cshow> pro monstrar/celar conditionalmente un section de wikitexto',
+	'conditionalshowsection-desc' => 'Adde le etiquetta cshow pro monstrar/celar conditionalmente un section de wikitexto',
 );
 
 /** Indonesian (Bahasa Indonesia)
  * @author Irwangatot
  */
 $messages['id'] = array(
-	'conditionalshowsection-desc' => 'Menambahkan penanda <cshow> untuk kondisi tampil/sembunyi seksi dari tekswiki',
+	'conditionalshowsection-desc' => 'Menambahkan penanda cshow untuk kondisi tampil/sembunyi seksi dari tekswiki',
 );
 
 /** Japanese (日本語)
  * @author Fryed-peach
  */
 $messages['ja'] = array(
-	'conditionalshowsection-desc' => 'ある部分のウィキテキストの表示・非表示を条件によって切り替えられる <cshow> タグを追加する',
+	'conditionalshowsection-desc' => 'ある部分のウィキテキストの表示・非表示を条件によって切り替えられる cshow タグを追加する',
 );
 
 /** Ripoarisch (Ripoarisch)
@@ -152,83 +152,83 @@ $messages['ksh'] = array(
  * @author Robby
  */
 $messages['lb'] = array(
-	'conditionalshowsection-desc' => 'Den Tag <cshow> derbäisetzen fir en Abschnitt vum Wikitext ënner gewësse Konditionen ze weisen oder ze verstoppen',
+	'conditionalshowsection-desc' => 'Den Tag cshow derbäisetzen fir en Abschnitt vum Wikitext ënner gewësse Konditionen ze weisen oder ze verstoppen',
 );
 
 /** Macedonian (Македонски)
  * @author Bjankuloski06
  */
 $messages['mk'] = array(
-	'conditionalshowsection-desc' => 'Додавање наознаката <cshow> за условен приказ/сокривање на дел од вики-текст',
+	'conditionalshowsection-desc' => 'Додавање наознаката cshow за условен приказ/сокривање на дел од вики-текст',
 );
 
 /** Dutch (Nederlands)
  * @author Siebrand
  */
 $messages['nl'] = array(
-	'conditionalshowsection-desc' => 'Voegt de tag <code><nowiki><cshow></nowiki></code> toe om conditioneel een paragraaf wikitekst weer te geven of te verbergen',
+	'conditionalshowsection-desc' => 'Voegt de tag <code><nowiki>cshow</nowiki></code> toe om conditioneel een paragraaf wikitekst weer te geven of te verbergen',
 );
 
 /** Occitan (Occitan)
  * @author Cedric31
  */
 $messages['oc'] = array(
-	'conditionalshowsection-desc' => 'Apondon de la balisa <cshow> per afichar o amagar condicionalament una seccion de wikitèxte',
+	'conditionalshowsection-desc' => 'Apondon de la balisa cshow per afichar o amagar condicionalament una seccion de wikitèxte',
 );
 
 /** Polish (Polski)
  * @author Sp5uhe
  */
 $messages['pl'] = array(
-	'conditionalshowsection-desc' => 'Dodaje znacznik <cshow> warunkowego pokazywania lub ukrywania fragmentu tekstu wiki',
+	'conditionalshowsection-desc' => 'Dodaje znacznik cshow warunkowego pokazywania lub ukrywania fragmentu tekstu wiki',
 );
 
 /** Piedmontese (Piemontèis)
  * @author Dragonòt
  */
 $messages['pms'] = array(
-	'conditionalshowsection-desc' => 'Gionté ël tag <cshow> per mosté/stërmé condissionalment na session ëd test wiki',
+	'conditionalshowsection-desc' => 'Gionté ël tag cshow per mosté/stërmé condissionalment na session ëd test wiki',
 );
 
 /** Portuguese (Português)
  * @author Indech
  */
 $messages['pt'] = array(
-	'conditionalshowsection-desc' => 'Adicionando a marcação <cshow> para mostrar/ocultar condicionalmente uma seção de wikitexto',
+	'conditionalshowsection-desc' => 'Adicionando a marcação cshow para mostrar/ocultar condicionalmente uma seção de wikitexto',
 );
 
 /** Tarandíne (Tarandíne)
  * @author Joetaras
  */
 $messages['roa-tara'] = array(
-	'conditionalshowsection-desc' => "Aggiunge 'u tag <cshow> pe fa vedè/scunnè condizionalmende 'na sezione de teste de Uicchi",
+	'conditionalshowsection-desc' => "Aggiunge 'u tag cshow pe fa vedè/scunnè condizionalmende 'na sezione de teste de Uicchi",
 );
 
 /** Russian (Русский)
  * @author Александр Сигачёв
  */
 $messages['ru'] = array(
-	'conditionalshowsection-desc' => 'Добавляет тег <cshow> для условного показа или сокрытия раздела викитекста.',
+	'conditionalshowsection-desc' => 'Добавляет тег cshow для условного показа или сокрытия раздела викитекста.',
 );
 
 /** Slovak (Slovenčina)
  * @author Helix84
  */
 $messages['sk'] = array(
-	'conditionalshowsection-desc' => 'Pridáva značku <cshow> na podmienečné zobrazenie/skrytie sekcie wikitextu',
+	'conditionalshowsection-desc' => 'Pridáva značku cshow na podmienečné zobrazenie/skrytie sekcie wikitextu',
 );
 
 /** Veps (Vepsan kel')
  * @author Игорь Бродский
  */
 $messages['vep'] = array(
-	'conditionalshowsection-desc' => 'Ližadab <cshow>-virgan, miše ozutada/peitta wikitekstan palad',
+	'conditionalshowsection-desc' => 'Ližadab cshow-virgan, miše ozutada/peitta wikitekstan palad',
 );
 
 /** Vietnamese (Tiếng Việt)
  * @author Minh Nguyen
  */
 $messages['vi'] = array(
-	'conditionalshowsection-desc' => 'Thêm thẻ <cshow> để hiện/ẩn đoạn mã wiki tùy theo điều kiện',
+	'conditionalshowsection-desc' => 'Thêm thẻ cshow để hiện/ẩn đoạn mã wiki tùy theo điều kiện',
 );
 
