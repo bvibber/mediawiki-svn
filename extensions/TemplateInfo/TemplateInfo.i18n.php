668e22f38ac44322670f534e98d9b17b160a0229
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-03 20:31:18 +0000

    Add TemplateInfo to translatewiki Tweak i18n file a bit
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58497 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TemplateInfo/TemplateInfo.i18n.php
+++ extensions/TemplateInfo/TemplateInfo.i18n.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Messages file for the InputBox extension
+ * Messages file for the TemplateInfo extension
  *
  * @addtogroup Extensions
  */
@@ -14,14 +14,12 @@
 $messages = array();
 
 $messages['en'] = array(
-    'templateinfo-desc'    => 'Supports templates defining their data structure via XML markup.',
-    'templateinfo-header'  => 'The XML definition for this template is:',
+    'templateinfo-desc' => 'Supports templates defining their data structure via XML markup',
+    'templateinfo-header' => 'The XML definition for this template is:',
 );
 
 /** Message documentation (Message documentation)
- * @author Meno25
- * @author SPQRobin
- * @author Siebrand
+ * @author Yaron Koren 
  */
 $messages['qqq'] = array(
     'templateinfo-desc' => 'Short description of the Template Info extension, shown on [[Special:Version]].',
