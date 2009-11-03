091a1812318f1dcc9910bb6e44dde7cbfcb3705a
sergeychernyshev <sergeychernyshev@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-03 03:41:14 +0000

    Release 0.8.8: HTML is inserted as is now, thanks to Joshua C. Lerner.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@58460 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Widgets/REL_0_8_8/Widgets.i18n.magic.php
@@ -0,0 +1,19 @@
+<?php
+
+$magicWords = array();
+
+$magicWords['en'] = array(
+	'widget' => array( 0, 'widget' ),
+);
+
+$magicWords['ar'] = array(
+	'widget'   => array( '0', 'ودجة', 'widget' ),
+);
+
+$magicWords['mr'] = array(
+	'widget'   => array( '0', 'विसाधन', 'widget' ),
+);
+
+$magicWords['ru'] = array(
+	'widget'   => array( '0', 'виджет', 'widget' ),
+);
