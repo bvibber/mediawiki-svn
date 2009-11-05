efda51de0cd989444da570ece8c37c3d9a9d8a98
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-05 16:36:30 +0000

    Moving XML validation from API querying to parsing
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58587 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TemplateInfo/TemplateInfo.hooks.php
+++ extensions/TemplateInfo/TemplateInfo.hooks.php
@@ -19,6 +19,39 @@ class TemplateInfoHooks {
 	return true;
     }
 
+    public static function validateXML( $xml ) {
+	$xmlDTD =<<<END
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE template [
+<!ELEMENT template (description?,params?,data*)>
+<!ELEMENT params (param|group)*>
+<!ELEMENT param (label?,description?,options?,type?,data*)>
+<!ATTLIST param id ID #REQUIRED>
+<!ELEMENT group (label?,description?,param*,data*)>
+<!ELEMENT label (#PCDATA|msg)*>
+<!ELEMENT description (#PCDATA|msg)*>
+<!ELEMENT options (option*)>
+<!ELEMENT option (#PCDATA|msg)*>
+<!ELEMENT type (field*)>
+<!ATTLIST type name CDATA #REQUIRED>
+<!ELEMENT field EMPTY>
+<!ATTLIST field name CDATA #REQUIRED>
+<!ATTLIST field value CDATA #REQUIRED>
+<!ELEMENT msg (#PCDATA)>
+<!ATTLIST msg lang CDATA #REQUIRED>
+<!ELEMENT data (field*)>
+<!ATTLIST data app CDATA #REQUIRED>
+]>
+
+END;
+	// we are using the SimpleXML library to do the XML validation
+	// for now - this may change later
+	// hide parsing warnings
+	libxml_use_internal_errors(true);
+	$xml_success = simplexml_load_string($xmlDTD . $xml);
+	return $xml_success;
+    }
+
     // Render the displayed XML, if any
     public static function render( $input, $args, $parser, $frame ) {
 	// if this call is contained in a transcluded page or template,
@@ -28,7 +61,10 @@ class TemplateInfoHooks {
 	
 	// Store XML in the page_props table
 	// TODO: Do processing here, like parse to an array
-	$parser->getOutput()->setProperty( 'templateinfo', $input );
+ 	if ( TemplateInfoHooks::validateXML( $input ) )
+		$parser->getOutput()->setProperty( 'templateinfo', $input );
+	else
+		$parser->getOutput()->setProperty( 'templateinfo', "Error: Invalid XML" );
 
 	// Return output
 	$text = "<p>" . wfMsg( 'templateinfo-header' ) . "</p>\n";
