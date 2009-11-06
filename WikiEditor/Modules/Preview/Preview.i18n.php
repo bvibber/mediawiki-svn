7f169f295dac39405e2ff1c998e9ac1002f511a2
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-06 00:27:48 +0000

    ... and some more
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@58626 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/Modules/Preview/Preview.i18n.php
+++ WikiEditor/Modules/Preview/Preview.i18n.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Internationalisation for Usability Initiative SideBySidePreview extension
+ * Internationalisation for the Preview module of the Usability Initiative wikiEditor extension
  *
  * @file
  * @ingroup Extensions
@@ -12,8 +12,6 @@ $messages = array();
  * @author Roan Kattouw
  */
 $messages['en'] = array(
-	'sidebysidepreview' => 'Side-by-side preview',
-	'wikieditor-preview-desc' => 'Allows users to quickly switch between edit and preview mode on the same page',
 	'wikieditor-preview-preference' => 'Enable side-by-side preview',
 	'wikieditor-preview-tab-edit' => 'Edit',
 	'wikieditor-preview-tab-preview' => 'Preview',
@@ -24,7 +22,6 @@ $messages['en'] = array(
  * @author Roan Kattouw
  */
 $messages['qqq'] = array(
-	'wikieditor-preview-desc' => '{{desc}}',
 	'wikieditor-preview-preference' => "Option in [[Special:Preferences]], tab ''{{int:prefs-editing}}''",
 	'wikieditor-preview-tab-edit' => 'Caption of the tab containing the edit box',
 	'wikieditor-preview-tab-preview' => 'Caption of the tab containing the preview',
@@ -35,8 +32,6 @@ $messages['qqq'] = array(
  * @author Michawiki
  */
 $messages['dsb'] = array(
-	'sidebysidepreview' => 'Pśeglěd bok pó boku',
-	'wikieditor-preview-desc' => 'Wužywarjam zmóžniś, malsnje mjazy modusoma "wobźěłaś" a "pśeglěd" na samskem boku pśešaltowaś',
 	'wikieditor-preview-preference' => 'Pśeglěd bok pó boku zmóžniś',
 	'wikieditor-preview-tab-edit' => 'Wobźěłaś',
 	'wikieditor-preview-tab-preview' => 'Pśeglěd',
@@ -47,8 +42,6 @@ $messages['dsb'] = array(
  * @author Yekrats
  */
 $messages['eo'] = array(
-	'sidebysidepreview' => 'Kolumneca antaŭvido',
-	'wikieditor-preview-desc' => 'Permesas al uzantoj rapide ŝanĝi inter redakta kaj antaŭvida modusoj en la sama paĝo',
 	'wikieditor-preview-preference' => 'Ŝalti kolumnecan antaŭvidon',
 	'wikieditor-preview-tab-edit' => 'Redakta',
 	'wikieditor-preview-tab-preview' => 'Antaŭvida',
@@ -59,7 +52,6 @@ $messages['eo'] = array(
  * @author Toliño
  */
 $messages['gl'] = array(
-	'wikieditor-preview-desc' => 'Permite aos usuarios cambiar rapidamente entre os modos de edición e vista previa na mesma páxina',
 	'wikieditor-preview-tab-edit' => 'Editar',
 	'wikieditor-preview-tab-preview' => 'Vista previa',
 	'wikieditor-preview-loading' => 'Cargando...',
@@ -69,8 +61,6 @@ $messages['gl'] = array(
  * @author Als-Holder
  */
 $messages['gsw'] = array(
-	'sidebysidepreview' => 'Gegeiniberstelligs-Vorschau',
-	'wikieditor-preview-desc' => 'Benutzer erlaube, ass si schnäll zwisch em Bearbeitgsfänschter un dr Vorschau chenne hii un här gumpe',
 	'wikieditor-preview-preference' => 'Gegeiniberstelligs-Vorschau meglig mache',
 	'wikieditor-preview-tab-edit' => 'Bearbeite',
 	'wikieditor-preview-tab-preview' => 'Vorschau',
@@ -81,8 +71,6 @@ $messages['gsw'] = array(
  * @author Michawiki
  */
 $messages['hsb'] = array(
-	'sidebysidepreview' => 'Přehlad bok po boku',
-	'wikieditor-preview-desc' => 'Wužiwarjam zmóžnić, spěšnje mjez modusomaj "wobdźěłać" a "přehlad" na samsnej stronje přepinać',
 	'wikieditor-preview-preference' => 'Přehlad bok po boku zmóžnić',
 	'wikieditor-preview-tab-edit' => 'Wobdźěłać',
 	'wikieditor-preview-tab-preview' => 'Přehlad',
@@ -93,7 +81,6 @@ $messages['hsb'] = array(
  * @author Dani
  */
 $messages['hu'] = array(
-	'wikieditor-preview-desc' => 'Lehetővé teszi a felhasználók számára, hogy gyorsan váltsanak a szerkesztő- és az előnézet mód között, ugyanazon a lapon.',
 	'wikieditor-preview-tab-edit' => 'Szerkesztés',
 	'wikieditor-preview-tab-preview' => 'Előnézet',
 	'wikieditor-preview-loading' => 'Betöltés…',
@@ -103,8 +90,6 @@ $messages['hu'] = array(
  * @author IvanLanin
  */
 $messages['id'] = array(
-	'sidebysidepreview' => 'Pratayang bersisian',
-	'wikieditor-preview-desc' => 'Memungkinkan pengguna untuk dengan cepat beralih antara modus penyuntingan dan pratayang di halaman yang sama',
 	'wikieditor-preview-preference' => 'Aktifkan pratayang bersisian',
 	'wikieditor-preview-tab-edit' => 'Sunting',
 	'wikieditor-preview-tab-preview' => 'Pratayang',
@@ -115,8 +100,6 @@ $messages['id'] = array(
  * @author Praveenp
  */
 $messages['ml'] = array(
-	'sidebysidepreview' => 'പ്രിവ്യൂ അരികിൽ',
-	'wikieditor-preview-desc' => 'തിരുത്തലും പ്രിവ്യൂവും ഒരേ താളിൽ തന്നെ അതിവേഗം മാറിക്കാണുവാൻ ഉപയോക്താക്കളെ അനുവദിക്കുന്നു',
 	'wikieditor-preview-preference' => 'അരികിലായുള്ള പ്രിവ്യൂ സജ്ജമാക്കുക',
 	'wikieditor-preview-tab-edit' => 'തിരുത്തുക',
 	'wikieditor-preview-tab-preview' => 'പ്രിവ്യൂ',
@@ -127,7 +110,6 @@ $messages['ml'] = array(
  * @author Prima klasy4na
  */
 $messages['uk'] = array(
-	'wikieditor-preview-desc' => 'Дозволяє користувачу швидко перемикатися між режимами редагування та перегляду на одній сторінці',
 	'wikieditor-preview-tab-edit' => 'Редагувати',
 	'wikieditor-preview-tab-preview' => 'Попередній перегляд',
 	'wikieditor-preview-loading' => 'Завантаження...',
--- WikiEditor/Modules/Toc/Toc.js
+++ WikiEditor/Modules/Toc/Toc.js
@@ -1,4 +1,4 @@
-/* JavaScript for NavigableTOC extension */
+/* JavaScript for WikiEditor Toc module */
 
 js2AddOnloadHook( function() {
 	$j( 'textarea#wpTextbox1' ).wikiEditor( 'addModule', 'toc' );
