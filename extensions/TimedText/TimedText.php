b8ebb36513fc77dab515e81ad744048529b61f73
ialex <ialex@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-08 12:15:22 +0000

    * removed ?> * use tabs for indentation, not spaces * added 'path' item to $wgExtensionCredits, removed 'status' (not used) and commented out 'url' (linked to example.com) * use $wgRequest->getVal( 'action' ) to get the "action" parameter rather than getting it from "global $action;"
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58761 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TimedText/TimedText.php
+++ extensions/TimedText/TimedText.php
@@ -1,29 +1,28 @@
 <?php
 if( !defined( 'MEDIAWIKI' ) ) {
-    echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
-    die( 1 );
+	echo( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
+	die( 1 );
 }
  
-$wgExtensionCredits['other'][] = array( 
-    'name' => 'Add TimedText editing', 
-    'status' => 'experimental',
-    'author' => 'MediaBot',
-    'version' => '0.1',
-    'url' => 'http://www.example.com',
-    'description' => 'load custom javascript on TimedText edit pages.',
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'Add TimedText editing',
+	'author' => 'MediaBot',
+	'version' => '0.1',
+#	'url' => 'http://www.example.com',
+	'description' => 'load custom javascript on TimedText edit pages.',
 );
- 
+
 $wgHooks['EditPageBeforeEditToolbar'][] = 'wfAddTimedTextJs';
 
 function wfAddTimedTextJs( &$toolabar ) {
-  global $wgOut, $wgTitle, $action, $wgScriptPath;
-  $name = $wgTitle->getPrefixedDBKey();
-  if($action == 'edit' && substr($name, 0, 10) == 'TimedText:')
-  {
-    //$wgOut->addScriptClass( 'mvTimedTextEdit' );
-    $wgOut->addScriptFile( $wgScriptPath. '/js2/mwEmbed/libTimedText/mvTimeTextEdit.js' );
-  }
-  return true;
+	global $wgOut, $wgTitle, $wgRequest, $wgScriptPath;
+	$name = $wgTitle->getPrefixedDBKey();
+	$action = $wgRequest->getVal( 'action' );
+	if( $action == 'edit' && substr( $name, 0, 10 ) == 'TimedText:' ) {
+		//$wgOut->addScriptClass( 'mvTimedTextEdit' );
+		$wgOut->addScriptFile( $wgScriptPath. '/js2/mwEmbed/libTimedText/mvTimeTextEdit.js' );
+	}
+	return true;
 }
 
-?>
