ef01afb0f2412f3986d996383eaf9e67566dc5c6
kipcool <kipcool@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-09 20:54:48 +0000

    * faster script (removed a superflouous sql query) * also rearranged a bit the page structure and removed some debug comments
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@58834 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikidata/util/stats.php
+++ extensions/Wikidata/util/stats.php
@@ -30,31 +30,20 @@ function stopwatch() {
 
 $start = stopwatch();
 
-echo"<center>
-<h1>Number of Expressions per language</h1>
-<hr width=950 size=1 noshade><br />
-";
-
-$expressions_r = mysql_query( "SELECT  COUNT(*) FROM uw_expression WHERE expression_id IN (SELECT DISTINCT expression_id FROM uw_syntrans WHERE remove_transaction_id IS NULL)" );
-$expressions_a = mysql_fetch_row( $expressions_r );
-$expressions = $expressions_a[0];
-
+// finds the number of DMs.
 $defined_meanings_r = mysql_query( "SELECT  COUNT(DISTINCT defined_meaning_id) FROM uw_syntrans WHERE remove_transaction_id IS NULL" );
 $defined_meanings_a = mysql_fetch_row( $defined_meanings_r );
 $defined_meanings = $defined_meanings_a[0];
-echo"<br />\n";
-echo"Total <b>$defined_meanings</b> DefinedMeanings in database, linking together <b>$expressions</b> Expressions. Broken down per language:\n";
-echo"</br>\n";
-echo"<hr>\n";
+// The number of expressions is computed below as the sum of the number of expressions in each language, instead of a SQL query.
 
+// finds the list of languages with their English names
 $result = mysql_query( "SELECT *
 FROM language_names 
 where name_language_id = 85
 " ) or die ( "error " . mysql_error() );
 
 while ( $row = mysql_fetch_array( $result, MYSQL_NUM ) ) {
-// echo $row[0]." - ".$row[1]." - ".$row[2]."<br />";
-$lang[$row[0]] = $row[2];
+  $lang[$row[0]] = $row[2];
 }
 
 
@@ -74,20 +63,39 @@ group by language_id
 order by tot desc
  " ) or die ( "error " . mysql_error() );
 
-echo ' 
-<table cellpadding=0 width=950><tr><td width=200><b>Language</b></td><td align=right><b>Expressions</b></td><td width=30></td><td></td></tr>';
 $width = 500;
 $limit = 500;
 $max = 0;
+$total = 0 ;
 while ( $row = mysql_fetch_array( $result, MYSQL_NUM ) ) {
-if ( $max < $row[1] )$max = $row[1];
-$wi = ceil( ( ( $row[1] / $max ) * $width ) );
-$per = ceil( ( ( $row[1] / $max ) * 100 ) );
-if ( $row[1] > $limit )echo "<tr><td >" . $lang[$row[0]] . '</td><td align="right">' . $row[1] . "</td><td width=30></td><td><img src=sc1.png width=\"$wi\" height=20> $per %</td></tr>";
-else $tx .= $lang[$row[0]] . " (" . $row[1] . "), ";
-// $ar[$row[0]].=$row[1]."	".$row[2]."\n";
-// filewrite("out/".$row[0].".txt",$row[1]."	".$row[2]);
+  if ( $max < $row[1] )$max = $row[1];
+  $wi = ceil( ( ( $row[1] / $max ) * $width ) );
+  $per = ceil( ( ( $row[1] / $max ) * 100 ) );
+  $total += $row[1] ;
+  if ( $row[1] > $limit ) {
+    $tableLang .= "<tr><td >" . $lang[$row[0]] . '</td><td align="right">' . $row[1] . "</td><td width=30></td><td><img src=sc1.png width=\"$wi\" height=20> $per %</td></tr>";
+  } else {
+    $tx .= $lang[$row[0]] . " (" . $row[1] . "), ";
+  }
 }
+
+// now printing the table
+
+echo "<center>
+<h1>Number of Expressions per language</h1>
+<hr width=950 size=1 noshade><br />
+";
+
+echo "<p>\n";
+echo "Total <b>$defined_meanings</b> DefinedMeanings in database, linking together <b>$total</b> Expressions. Broken down per language:\n";
+echo "</p>\n";
+echo "<hr>\n";
+
+echo ' 
+<table cellpadding=0 width=950><tr><td width=200><b>Language</b></td><td align=right><b>Expressions</b></td><td width=30></td><td></td></tr>';
+
+echo "$tableLang" ;
+
 echo "
 <tr><td colspan=4>
 <div align=justify>
@@ -97,16 +105,6 @@ $tx
 </div>
 </td>
 </table><center>";
-/*
-for($i=0;$i<250;$i++){
-if(strlen($ar[$i])>20)filewrite("out/".$lang[$i].".txt",$ar[$i]);
-
-}
-*/
-// //////////////////////////////////////////////////////
-
-
-// echo "<pre>".$ar[85]."</pre>";
 
 echo "
 <br />
