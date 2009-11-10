48ba40e2b22b6d86e0d1ef98a900b29e5ee86ca0
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-10 21:18:48 +0000

    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@58878 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ SemanticMaps/REL_0_4_1/GoogleMaps/SM_GoogleMaps.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This groupe contains all Google Maps related files of the Semantic Maps extension.
+ * 
+ * @defgroup SMGoogleMaps Google Maps
+ * @ingroup SemanticMaps
+ */
+
+/**
+ * This file holds the general information for the Google Maps service.
+ *
+ * @file SM_GoogleMaps.php
+ * @ingroup SMGoogleMaps
+ *
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+$egMapsServices['googlemaps']['qp'] = array('class' => 'SMGoogleMapsQP', 'file' => 'SemanticMaps/GoogleMaps/SM_GoogleMapsQP.php', 'local' => false);
+$egMapsServices['googlemaps']['fi'] = array('class' => 'SMGoogleMapsFormInput', 'file' => 'SemanticMaps/GoogleMaps/SM_GoogleMapsFormInput.php', 'local' => false);
