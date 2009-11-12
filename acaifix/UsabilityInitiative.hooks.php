9c850dc63e78ae9c6113b678a0950a4de17589f1
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-12 21:43:00 +0000

    usability: Merge r58973, r58974 from trunk into acaifix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/usability@58975 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- acaifix/UsabilityInitiative.hooks.php
+++ acaifix/UsabilityInitiative.hooks.php
@@ -67,15 +67,15 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 6 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 17 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 12 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 21 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 22 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 8 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 27 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 63 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 64 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 63 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 64 ),
 			),
 		),
 	);
