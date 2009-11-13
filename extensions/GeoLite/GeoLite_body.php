2fa0dcbcf93a473bea5a930001015e5d50b88a02
tomasz <tomasz@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-13 22:01:45 +0000

    picking up r59030
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@59034 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/GeoLite/GeoLite_body.php
+++ extensions/GeoLite/GeoLite_body.php
@@ -6,31 +6,33 @@
  * @ingroup Extensions
  */
 
-// Special page GeoLite
-
 class SpecialGeoLite extends UnlistedSpecialPage {
 
-        /* Functions */
-
-        public function __construct() {
-                // Initialize special page
-                parent::__construct( 'GeoLite' );
-        }
-
+	// Initialize special page
+	public function __construct() {
+		parent::__construct( 'GeoLite' );
+	}
+	
 	public function execute( $sub ) {
-		global $wgOut, $wgRequest, $wgLandingPageBase, $wgChaptersPageBase, $wgChapterLandingPages;
-
+		global $wgOut, $wgRequest;
+		global $wgLandingPageBase, $wgChaptersPageBase, $wgChapterLandingPages, $wgLandingPageDefaultTarget;
+		
 		$lang = ( preg_match( '/^[A-Za-z-]+$/', $wgRequest->getVal( 'lang' ) ) ) ? $wgRequest->getVal( 'lang' ) : 'en' ;
 		$utm_source = $wgRequest->getVal( 'utm_source' );
 		$utm_medium = $wgRequest->getVal( 'utm_medium' );
 		$utm_campaign = $wgRequest->getVal( 'utm_campaign' );
 		$referrer = $wgRequest->getHeader( 'referer' );
-
+		$target = $wgRequest->getVal( 'target', null );
+		if ( !$target ) {
+			$target = $wgLandingPageDefaultTarget;
+		}
+		
 		$tracking = '?' . wfArrayToCGI( array( 
 			'utm_source' => "$utm_source",
 			'utm_medium' => "$utm_medium",
-		        'utm_campaign' => "$utm_campaign",
+			'utm_campaign' => "$utm_campaign",
 			'referrer' => "$referrer",
+			'target' => "$target",
 		) );
 		
 		$ip = ( $wgRequest->getVal( 'ip') ) ? $wgRequest->getVal( 'ip' ) : wfGetIP();
@@ -38,14 +40,12 @@ class SpecialGeoLite extends UnlistedSpecialPage {
 		if ( IP::isValid( $ip ) ) {
 			$country = geoip_country_code_by_name( $ip );
 			if ( is_string ( $country ) && array_key_exists( $country, $wgChapterLandingPages ) ) {
-			    $wgOut->redirect( $wgChaptersPageBase . "/" . $wgChapterLandingPages[ $country ] . $tracking );
-			} else { // Valid IP but no chapter page
-				$wgOut->redirect( $wgLandingPageBase . "/" . $lang . $tracking );
+				$wgOut->redirect( $wgChaptersPageBase . '/' . $wgChapterLandingPages[$country] . $tracking );
+				return;
 			}
-		} else { // No ip found so do the best we can 
-			$wgOut->redirect( $wgLandingPageBase . "/" . $lang . $tracking );
 		}
+		// No valid IP or chapter page - let's just go for the general one
+		$wgOut->redirect( $wgLandingPageBase . $target . '/' . $lang . $tracking );
 	}
 
 }
-
