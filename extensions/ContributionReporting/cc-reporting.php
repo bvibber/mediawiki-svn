3f6ca397ae4e150d23e59919eb68213a83feb54e
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-17 22:29:02 +0000

    comments
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59184 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ContributionReporting/cc-reporting.php
+++ extensions/ContributionReporting/cc-reporting.php
@@ -1,4 +1,6 @@
 <?php
+
+//handle request
 if(isset ($_REQUEST['load'])){
 	handleLoad();
 }
@@ -6,9 +8,11 @@ else if(isset ($_REQUEST['submit'])){
 	handleSubmit();
 }
 
+//handle load
 function handleLoad(){
 	$cc_id = randomCookie();
 	
+	//already have a cookie? use it, else set randomly generated one
 	if(isset($_COOKIE['cc-cookie'])){
 		$cc_id = $_COOKIE['cc-cookie'];
 	}
@@ -17,22 +21,28 @@ function handleLoad(){
 		setcookie('cc-cookie', $cc_id, time() + (60 * 60 * 24 * 300), '/', "payments.wikimedia.org");
 	}
 	
+	//add into DB
 	$cc_id = addslashes($cc_id);
 	$utm_src = isset($_REQUEST['utm_src']) ? 
 			   addslashes($_REQUEST['utm_src']) : "unknown";
 	$sql = "INSERT IGNORE INTO cc-track (cookie_id, utm_src, contribs) VALUES ('$cc_id','$utm_src', 0)";
 }
 
+//handle submit
 function handleSubmit(){
+	
+	//for people with cookies turned off, track them via "NULL" cookie
 	$cc_id = "NULL";
 	if(isset($_COOKIE['cc-cookie'])){
 		$cc_id = $_COOKIE['cc-cookie'];
 	}
 	
+	//update in DB
 	$cc_id = addslashes($cc_id);
 	$sql = "UPDATE cc-track SET contribs=contribs+1 WHERE cookie_id = '$cc_id'";
 }
 
+//random cookie
 function randomCookie(){
 	return md5(mt_rand() + time());
 }
