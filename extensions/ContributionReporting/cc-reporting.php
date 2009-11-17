55cae4f865830f0fc9361fce728b969dca6cd414
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-17 00:29:26 +0000

    Added cc-reporting stub
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59161 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ContributionReporting/cc-reporting.php
@@ -0,0 +1,34 @@
+<?php
+if(isset ($_REQUEST['load'])){
+	handleLoad();
+}
+else if(isset ($_REQUEST['submit'])){
+	handleSubmit();
+}
+
+function handleLoad(){
+	$cc_id = randomCookie();
+	
+	if(!isset($_COOKIE['cc-cookie'])){
+		$cc_id = $_REQUEST['cc-cookie'];
+	}
+	else{
+		//set cookie to expire in maybe about a year or so... 300 days
+		setcookie('cc-cookie', $cc_id, time() + (60 * 60 * 24 * 300), '/', "payments.wikimedia.org");
+	}
+	
+	//if not exist insert into DB
+}
+
+function handleSubmit(){
+	$cc_id = "NULL";
+	if(isset($_COOKIE['cc-cookie'])){
+		$cc_id = $_COOKIE['cc-cookie'];
+	}
+	
+	//donations++
+}
+
+function randomCookie(){
+	return md5(mt_rand() + time());
+}
