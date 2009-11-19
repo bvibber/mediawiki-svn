4c37d6740adee8f501f94adb1cabc915d7e8d28a
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-19 10:17:54 +0000

    Fix r59228: somehow it did increment the right style versions and recombine other things, but didn't recombine the plugins files. Also incrementing a style version for r59221
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59238 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -15,13 +15,13 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/Highlight/Highlight.js', 'version' => 1 ),
 			array( 'src' => 'Modules/Preview/Preview.js', 'version' => 1 ),
 			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 1 ),
-			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 5 ),
+			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 6 ),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 3 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 4 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 3 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 4 ),
 		),
 	);
 	static $modules = array(
