f6ecdc7bb217b0cec9f5075e3f709a5e68f878c7
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-20 12:43:34 +0000

    wmf-deployment: Merge r59253 (ClickTracking bugfix) from trunk
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@59284 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/ClickTracking/ClickTracking.js
+++ extensions/UsabilityInitiative/ClickTracking/ClickTracking.js
@@ -10,10 +10,15 @@
 			$( '#p-logo a, #p-navigation a, #p-tb a' ).click( function() {
 				var id = 'leftnav-' + skin + '-' +
 					( $(this).attr( 'id' ) || $(this).parent().attr( 'id' ) );
-				window.location =  wgScriptPath +
-					'/api.php?action=clicktracking&eventid=' + id + '&token=' +
-					wgTrackingToken +
-					'&redirectto=' + escape( $(this).attr( 'href' ) );
+				var href = $(this).attr( 'href' );
+				// Don't attach to javascript: URLs and the like,
+				// only to local URLs (start with a /), http:// ,
+				// https:// and same-protocol URLs (start with //)
+				if ( href[0] == '/' || href.match( /^https?:\/\// ) )
+					window.location =  wgScriptPath +
+						'/api.php?action=clicktracking&eventid=' +
+						id + '&token=' + wgTrackingToken +
+						'&redirectto=' + escape( href );
 			});
 		});
 	}
