5f076f11a5ad5a80f5b9ee71013eee1e3d8d7808
freakolowsky <freakolowsky@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-26 14:08:52 +0000

    Simple sendmail 2 wiki redirection extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59449 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/SendmailToWiki/SendmailToWiki.i18n.php
@@ -0,0 +1,35 @@
+<?php
+$messages = array();
+ 
+/* *** English *** */
+$messages['en'] = array( 
+	'sendmailtowiki' => 'SendmailToWiki',
+	'sendmailtowiki-desc' => "Post wiki content trough dedicated dynamic email address.",
+	'prefs-sendmailtowiki' => "Posting content with email",
+	'sendmailtowiki-inemail' => "Dedicated email:",
+	'sendmailtowiki-inpin' => "PIN:",
+	'prefs-help-sendmailtowiki_pin' => "Blank field for PIN number disables posting content to wiki with your account.",
+	'sendmailtowiki-err-pinlength' => "PIN must contain exactly 5 numbers!",
+	'sendmailtowiki-err-wrongprefix' => "Wrong wiki account prefix. Contact your administrator!",
+	'sendmailtowiki-err-invalidaccount' => "Invalid account. Check the email address you're sending to.",
+	'sendmailtowiki-err-invalidsender' => "Invalid sender. Check the email address you're sending from.",
+	'sendmailtowiki-err-invalidpin' => "Invalid PIN. Access Denied.",
+	'sendmailtowiki-err-onlyplain' => "Because of potential misinterpretatons only text/plain messages are accepted.",
+);
+ 
+/* *** German (Deutsch) *** */
+$messages['sl'] = array(
+	'sendmailtowiki' => 'SendmailToWiki',
+	'sendmailtowiki-desc' => 'Pošiljanje wiki vsebine preko namenskega dinamičnega email naslova.',
+	'prefs-sendmailtowiki' => "Pošiljanje vsebine prek emaila",
+	'sendmailtowiki-inemail' => "Namenski email:",
+	'sendmailtowiki-inpin' => "PIN:",
+	'prefs-help-sendmailtowiki_pin' => "Prazno polje za PIN številko onemogoči pošiljanje vsebine na wiki prek vašega računa.",
+	'sendmailtowiki-err-pinlength' => "PIN mora vsebovati točno 5 številk!",
+	'sendmailtowiki-err-wrongprefix' => "Napačna wiki predpona. Kontaktirajte vašega administratorja!",
+	'sendmailtowiki-err-invalidaccount' => "Nepravilen račun. Preverite email naslov na katerega pošiljate.",
+	'sendmailtowiki-err-invalidsender' => "Nepravilen pošiljatelj. Preverite email naslov iz katerega pošiljate.",
+	'sendmailtowiki-err-invalidpin' => "Nepravilen PIN. Dostop zavrnjen.",
+	'sendmailtowiki-err-onlyplain' => "Zaradi potencialnega nepravilnega interpretiranja so podprta samo text/plain sporočila.",
+);
+?>
