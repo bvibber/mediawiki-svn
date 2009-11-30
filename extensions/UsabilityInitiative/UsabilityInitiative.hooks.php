1d797d0eed96aa665766b674f56ec2cbf73db2c9
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-11-30 19:36:08 +0000

    Moving padding on toc elements onto the span to fix auto ellipse
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59595 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -22,16 +22,16 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/wikiEditor.css', 'version' => 7 ),
 				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 7 ),
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 2 ),
-				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 22 ),
+				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 23 ),
 				array( 'src' => 'css/wikiEditor.preview.css', 'version' => 1 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 29 ),
+				array( 'src' => 'css/combined.css', 'version' => 30 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 29 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 30 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
