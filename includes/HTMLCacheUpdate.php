569741c0d4cbd056c23174e4255315673ddb4c33
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-04 01:58:01 +0000

    Temporary hack to reduce the job queue size. Should be replaced by r59718 once the job queue is back down to zero.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@59719 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/HTMLCacheUpdate.php
+++ includes/HTMLCacheUpdate.php
@@ -226,6 +226,12 @@ class HTMLCacheUpdateJob extends Job {
 	}
 
 	public function run() {
+		# LIVE PATCH TS -- broken indexes
+		if ( $this->table == 'categorylinks' ) {
+			return true;
+		}
+		### END PATCH
+		
 		$update = new HTMLCacheUpdate( $this->title, $this->table, $this->start, $this->end );
 		$update->doUpdate();
 		return true;
