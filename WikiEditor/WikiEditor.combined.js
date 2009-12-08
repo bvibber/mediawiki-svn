79c01ca6a3569d18d1d9423602aa6276078b04b3
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-08 13:59:40 +0000

    Followup to r59825: forgot to recombine, minify WikiEditor JS
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@59826 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/WikiEditor.combined.js
+++ WikiEditor/WikiEditor.combined.js
@@ -1693,3 +1693,15 @@ mw.addOnloadHook( function() {
 
 } } );
 
+/* JavaScript for WikiEditor Template Editor module */
+
+mw.addOnloadHook( function() {
+	// Check preferences for highlight
+	if ( !wgWikiEditorPreferences || !( wgWikiEditorPreferences.templateEditor && wgWikiEditorPreferences.templateEditor.enable ) ) {
+		return true;
+	}
+	// Add the highlight module
+	if ( $j.wikiEditor ) {
+		$j( 'textarea#wpTextbox1' ).wikiEditor( 'addModule', 'templateEditor' );
+	}
+});
