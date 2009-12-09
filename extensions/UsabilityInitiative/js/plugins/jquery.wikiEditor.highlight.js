e54212c290141c97790874db191366aac2f3c89e
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-09 22:45:05 +0000

    templateEditor plugin, more fleshed out
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59912 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.highlight.js
+++ extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.highlight.js
@@ -11,7 +11,7 @@ api: {
  * Internally used event handlers
  */
 evt: {
-	change: function( event ) {
+	change: function( context, event ) {
 		/*
 		 * Triggered on any of the following events, with the intent on detecting if something was added, deleted or
 		 * replaced due to user action.
@@ -34,8 +34,13 @@ evt: {
 		 * 			;	Definition
 		 * 			:	Definition
 		 */
+			if(event.data.scope == 'do_not_trigger'){
+				$.wikiEditor.modules.highlight.fn.scan(context, "");
+				$.wikiEditor.modules.highlight.fn.mark(context, "", "");
+			}
 	}
 },
+
 /**
  * Internally used functions
  */
@@ -62,17 +67,73 @@ fn: {
 	strip: function( context, division ) {
 		return $( '<div />' ).html( division.html().replace( /\<br[^\>]*\>/g, "\n" ) ).text();
 	},
+	tokenArray : [],
 	scan: function( context, division ) {
 		/*
 		 * We need to look over some text and find interesting areas, then return the positions of those areas as tokens
 		 */
-		return []; // array of tokens?
+		token = function(offset, label){
+			this.offset = offset;
+			this.label = label;
+		}
+		
+		this.tokenArray = new Array();
+		var text = context.fn.getContents();
+		for ( module in $.wikiEditor.modules ) {
+			if ( 'exp' in $.wikiEditor.modules[module] ) {
+			   for(var i = 0; i < $.wikiEditor.modules[module].exp.length; i++){
+					var regex = $.wikiEditor.modules[module].exp[i].regex;
+					var label = $.wikiEditor.modules[module].exp[i].label;
+					var markAfter = false;
+					if(typeof($.wikiEditor.modules[module].exp[i].markAfter) != 'undefined'){
+						markAfter = true;
+					}
+					match = text.match(regex);
+					var oldOffset = 0;
+					while(match != null){
+						var markOffset = 0;
+						if(markAfter){
+							markOffset += match[0].length;
+						}
+						this.tokenArray.push(new token(match.index + oldOffset + markOffset, label));
+						oldOffset += (match.index + 1);
+						newSubstring = text.substring(oldOffset);
+						match = newSubstring.match(regex);
+					}
+			   }
+			}
+		}
+		
+		return this.tokenArray; // array of tokens
 	},
+	markers: [],
 	mark: function( context, division, tokens ) {
 		/*
 		 * We need to markup some text based on some tokens
 		 */
-	}
+		var rawText = context.fn.getContents();
+		
+		
+		//get all markers
+		for ( module in $.wikiEditor.modules ) {
+			if ( 'evt' in $.wikiEditor.modules[module]  && 'mark' in $.wikiEditor.modules[module].evt) {
+				$.wikiEditor.modules[module].evt.mark();
+			}
+		}
+		markedText = "";
+		var previousIndex = 0;
+		for(var currentIndex in this.markers){ //MAKE SURE THERE IS AT LEAST 1, or else this will delete EVERYTHING!
+			markedText+= rawText.substring(previousIndex, currentIndex);
+			
+			for(var i = 0 ; i < this.markers[currentIndex].length; i++){
+				markedText += this.markers[currentIndex][i];
+			}
+			
+			previousIndex = currentIndex;
+		}
+		markedText.replace(/\n/g, '<br\>');
+		context.fn.setContents({contents:markedText});
+	}//endmark
 }
 
 }; })( jQuery );
