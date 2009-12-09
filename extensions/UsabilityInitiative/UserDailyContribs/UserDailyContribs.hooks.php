b635210b78bc09947ef23a207d62e48a1e794c53
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-09 18:48:25 +0000

    switched "update" to use Database function
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@59895 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UserDailyContribs/UserDailyContribs.hooks.php
+++ extensions/UsabilityInitiative/UserDailyContribs/UserDailyContribs.hooks.php
@@ -39,9 +39,9 @@ class UserDailyContribsHooks {
 		$today = gmdate( 'Ymd', time() );
 		$dbw = wfGetDB( DB_MASTER );
 		
-		//normal $db->update doesn't support SQL variables yet
-		$sql = "UPDATE user_daily_contribs SET contribs=contribs+1 WHERE day = $today AND user_id = {$wgUser->getId()}";
-		$dbw->query($sql, __METHOD__);
+		$dbw->update( 'user_daily_contribs', array( 'contribs=contribs+1' ), 
+    				   array( 'day' => $today, 'user_id' => $wgUser->getId() ),
+  					   __METHOD__ );
 		
 		if($dbw->affectedRows() == 0){
 			$dbw->insert("user_daily_contribs", array("user_id" => $wgUser->getId(), "day" => $today, "contribs" => 1), __METHOD__);	
