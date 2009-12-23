122bb46bc1b47dc1a020a9c53a6238eaa194e23a
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-23 15:05:27 +0000

    Followup to r60136: hide all preferences when 'global' => false is set
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@60317 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector/Vector.hooks.php
+++ Vector/Vector.hooks.php
@@ -136,7 +136,9 @@ class VectorHooks {
 		global $wgVectorModules;
 		
 		foreach ( $wgVectorModules as $module => $enable ) {
-			if ( isset( self::$modules[$module]['i18n'], self::$modules[$module]['preferences'] ) ) {
+			if ( ( $enable['global'] || $enable['user'] ) &&
+					isset( self::$modules[$module]['i18n'] ) &&
+					isset( self::$modules[$module]['preferences'] ) ) {
 				wfLoadExtensionMessages( self::$modules[$module]['i18n'] );
 				foreach ( self::$modules[$module]['preferences'] as $key => $preference ) {
 					if ( $key == 'enable' && !$enable['user'] ) {
