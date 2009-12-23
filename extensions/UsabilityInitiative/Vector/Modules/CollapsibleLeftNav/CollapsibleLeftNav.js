eaa50310c636a2b75f471143e80ee2ed7d45d076
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-23 13:39:49 +0000

    UsabilityInitiative: Whitespace cleanup for r60299. Also add newlines at the end of .css/.js files where they were missing to make the combined files look nicer and prevent obscure combining bugs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@60313 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/Vector/Modules/CollapsibleLeftNav/CollapsibleLeftNav.js
+++ extensions/UsabilityInitiative/Vector/Modules/CollapsibleLeftNav/CollapsibleLeftNav.js
@@ -5,21 +5,22 @@
 /* $wgVectorUseCollapsibleLeftNav = true; */
 
 mw.addOnloadHook( function() {
-	if( !wgVectorUseCollapsibleLeftNav ) return;
-	$j( "#panel" ).addClass( "collapsible-nav" );
-	$j( "#panel > div.portal" ).toggleClass( "collapsed" );
-	$j( "#panel > div.portal:first" )
-		.toggleClass( "expanded" )
-		.toggleClass( "collapsed" )
-		.find( "div.body" )
-		.slideToggle( "fast" );
+	if( !wgVectorUseCollapsibleLeftNav )
+		return;
+	$j( '#panel' ).addClass( 'collapsible-nav' );
+	$j( '#panel > div.portal' ).toggleClass( 'collapsed' );
+	$j( '#panel > div.portal:first' )
+		.toggleClass( 'expanded' )
+		.toggleClass( 'collapsed' )
+		.find( 'div.body' )
+		.slideToggle( 'fast' );
 	// Toggle the selected menu's class and expand or collapse the menu
-	$j( "#panel > div.portal > h5" ).click( function() {
+	$j( '#panel > div.portal > h5' ).click( function() {
 		$j( this )
 			.parent()
-			.toggleClass("expanded")
-			.toggleClass("collapsed")
-			.find("div.body")
-			.slideToggle("fast");
+			.toggleClass( 'expanded' )
+			.toggleClass( 'collapsed' )
+			.find( 'div.body' )
+			.slideToggle( 'fast' );
 	} );
 } );
