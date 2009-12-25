1331a5bb41b6b8d5e35e1784900129487db8b1a2
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2009-12-25 22:40:18 +0000

    Tag for version 0.5.1.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@60405 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Maps/REL_0_5_1/Maps_Mapper.php
@@ -0,0 +1,110 @@
+<?php
+
+/** 
+ * A class that holds static helper functions for common functionality that is not map-spesific.
+ *
+ * @file Maps_Mapper.php
+ * @ingroup Maps
+ *
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+final class MapsMapper {
+	
+	/**
+	 * Array holding the parameters that are not spesific to a mapping service, 
+	 * their aliases, criteria and default value.
+	 *
+	 * @var array
+	 */
+	private static $mainParams;
+
+	public static function initializeMainParams() {
+		global $egMapsSizeRestrictions, $egMapsMapWidth, $egMapsMapHeight;
+
+		self::$mainParams = array
+			(
+			'zoom' => array(
+				'type' => 'integer',
+				'criteria' => array(
+					'in_range' => array(0, 15)
+					)
+				),
+			'width' => array(
+				'type' => 'integer',
+				'criteria' => array(
+					'in_range' => $egMapsSizeRestrictions['width']
+					),
+				'default' => $egMapsMapWidth		
+				),
+			'height' => array(
+				'type' => 'integer',
+				'criteria' => array(
+					'in_range' => $egMapsSizeRestrictions['height']
+					),
+				'default' => $egMapsMapHeight
+				),
+			);
+	}
+
+	/**
+	 * Returns the main parameters array.
+	 * 
+	 * @return array
+	 */
+	public static function getMainParams() {
+		return self::$mainParams;
+	}	
+	
+	/**
+	 * Returns a valid service. When an invalid service is provided, the default one will be returned.
+	 * Aliases are also chancged into the main service names @see MapsMapper::getMainServiceName().
+	 *
+	 * @param string $service
+	 * @param string $feature
+	 * 
+	 * @return string
+	 */
+	public static function getValidService($service, $feature) {
+		global $egMapsAvailableServices, $egMapsDefaultService, $egMapsDefaultServices, $egMapsServices;
+		
+		$service = self::getMainServiceName($service);
+		
+		$shouldChange = ! array_key_exists($service, $egMapsServices);
+		if (! $shouldChange) $shouldChange = ! array_key_exists($feature, $egMapsServices[$service]);
+		
+		if ($shouldChange) {
+			$service = array_key_exists($feature, $egMapsDefaultServices) ? $egMapsDefaultServices[$feature] : $egMapsDefaultService;
+		}
+		
+		if(! in_array($service, $egMapsAvailableServices)) $service = $egMapsDefaultService;
+		
+		return $service;
+	}
+	
+	/**
+	 * Checks if the service name is an alias for an actual service,
+	 * and changes it into the main service name if this is the case.
+	 *
+	 * @param string $service
+	 * @return string
+	 */
+	private static function getMainServiceName($service) {
+		global $egMapsServices;
+		
+		if (! array_key_exists($service, $egMapsServices)) {
+			foreach ($egMapsServices as $serviceName => $serviceInfo) {
+				if (in_array($service, $serviceInfo['aliases'])) {
+					 $service = $serviceName;
+					 break;
+				}
+			}
+		}
+		
+		return $service;
+	}	
+}
---
+++ Maps/REL_0_5_1/ParserFunctions/DisplayPoint/Maps_BasePointMap.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * File holding class MapsBasePointMap.
+ * 
+ * @file Maps_BasePointMap.php
+ * @ingroup Maps
+ * 
+ * @author Jeroen De Dauw
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Abstract class MapsBasePointMap provides the scafolding for classes handling display_point(s)
+ * and display_address(es) calls for a spesific mapping service. It inherits from MapsMapFeature and therefore
+ * forces inheriting classes to implement sereveral methods.
+ *
+ * @ingroup Maps
+ *
+ * @author Jeroen De Dauw
+ */
+abstract class MapsBasePointMap extends MapsMapFeature implements iDisplayFunction {
+	
+	private $markerData = array();
+	protected $markerStringFormat = '';
+	protected $markerString;
+	
+	/**
+	 * Handles the request from the parser hook by doing the work that's common for all
+	 * mapping services, calling the specific methods and finally returning the resulting output.
+	 *
+	 * @param unknown_type $parser
+	 * @param array $params
+	 * 
+	 * @return html
+	 */
+	public final function displayMap(&$parser, array $params) {
+		$this->setMapSettings();
+		
+		$this->featureParameters = MapsDisplayPoint::$parameters;
+		
+		if (parent::manageMapProperties($params, __CLASS__)) {
+			$this->doMapServiceLoad();
+	
+			$this->setMapName();
+			
+			$this->setCoordinates($parser);	
+	
+			$this->createMarkerString();
+			
+			$this->setZoom();
+			
+			$this->setCentre();
+			
+			$this->doParsing($parser);
+			
+			$this->doEscaping();
+			
+			$this->addSpecificMapHTML();			
+		}	
+		
+		return $this->output . $this->errorList;
+	}
+	
+	/**
+	 * Sets the zoom level to the provided value. When no zoom is provided, set
+	 * it to the default when there is only one location, or the best fitting soom when
+	 * there are multiple locations.
+	 *
+	 */
+	private function setZoom() {
+		if (strlen($this->zoom) < 1) {
+			if (count($this->markerData) > 1) {
+		        $this->zoom = 'null';
+		    }
+		    else {
+		        $this->zoom = $this->defaultZoom;
+		    }
+		}				
+	}	
+	
+	/**
+	 * Fills the $markerData array with the locations and their meta data.
+	 *
+	 * @param unknown_type $parser
+	 */
+	private function setCoordinates($parser) {
+		$this->coordinates = explode(';', $this->coordinates);		
+		
+		foreach($this->coordinates as $coordinates) {
+			$args = explode('~', $coordinates);
+			
+			$args[0] = str_replace('″', '"', $args[0]);
+			$args[0] = str_replace('′', "'", $args[0]);			
+			
+			$markerData = MapsUtils::getLatLon($args[0]);
+			
+			if (count($args) > 1) {
+				$markerData['title'] = $parser->recursiveTagParse( $args[1] );
+				
+				if (count($args) > 2) {
+					$markerData['label'] = $parser->recursiveTagParse( $args[2] );
+					
+					if (count($args) > 3) {
+						$markerData['icon'] = $args[3];
+					}					
+				}
+			}
+
+			$this->markerData[] = $markerData;
+		}
+	}
+	
+	/**
+	 * Creates a JS string with the marker data.
+	 * 
+	 * @return unknown_type
+	 */
+	private function createMarkerString() {
+		$markerItems = array();
+		
+		foreach ($this->markerData as $markerData) {
+			$lat = $markerData['lat'];
+			$lon = $markerData['lon'];		
+			
+			$title = array_key_exists('title', $markerData) ? $markerData['title'] : $this->title;
+			$label = array_key_exists('label', $markerData) ? $markerData['label'] : $this->label;
+
+			$title = str_replace("'", "\'", $title);
+			$label = str_replace("'", "\'", $label);	
+
+			$icon = array_key_exists('icon', $markerData) ? $markerData['icon'] : '';
+			
+			$markerItems[] = str_replace(	array('lon', 'lat', 'title', 'label', 'icon'), 
+											array($lon, $lat, $title, $label, $icon), 
+											$this->markerStringFormat
+											);
+		}
+		
+		$this->markerString = implode(',', $markerItems);		
+	}
+
+	/**
+	 * Sets the $centre_lat and $centre_lon fields.
+	 * Note: this needs to be done AFTRE the maker coordinates are set.
+	 */
+	private function setCentre() {
+		if (empty($this->centre)) {
+			if (count($this->markerData) == 1) {
+				// If centre is not set and there is exactelly one marker, use it's coordinates.
+				$this->centre_lat = $this->markerData[0]['lat'];
+				$this->centre_lon = $this->markerData[0]['lon'];
+			}
+			elseif (count($this->markerData) > 1) {
+				// If centre is not set and there are multiple markers, set the values to null,
+				// to be auto determined by the JS of the mapping API.
+				$this->centre_lat = 'null';
+				$this->centre_lon = 'null';
+			}
+			else {
+				// If centre is not set and there are no markers, use the default latitude and longitutde.
+				$this->setCentreDefaults();
+			}
+		}
+		else { // If a centre value is set, geocode when needed and use it.
+			$this->centre = MapsGeocodeUtils::attemptToGeocode($this->centre, $this->geoservice, $this->serviceName);
+			
+			// If the centre is not false, it will be a valid coordinate, which can be used to set the latitude and longitutde.
+			if ($this->centre) {
+				$this->centre = MapsUtils::getLatLon($this->centre);
+				$this->centre_lat = $this->centre['lat'];
+				$this->centre_lon = $this->centre['lon'];				
+			}
+			else { // If it's false, the coordinate was invalid, or geocoding failed. Either way, the default's should be used.
+				$this->setCentreDefaults();
+			}
+		}
+	}
+	
+	/**
+	 * Sets the centre latitude and longitutde to the defaults.
+	 */
+	private function setCentreDefaults() {
+		global $egMapsMapLat, $egMapsMapLon;
+		$this->centre_lat = $egMapsMapLat;
+		$this->centre_lon = $egMapsMapLon;		
+	}
+
+	/**
+	 * Parse the wiki text in the title and label values.
+	 * 
+	 * @param unknown_type $parser
+	 */
+	private function doParsing(&$parser) {
+		$this->title = $parser->recursiveTagParse( $this->title );
+		$this->label = $parser->recursiveTagParse( $this->label );
+	}
+	
+	/**
+	 * Escape the title and label text
+	 */
+	private function doEscaping() {
+		$this->title = str_replace("'", "\'", $this->title);
+		$this->label = str_replace("'", "\'", $this->label);		
+	}
+	
+}
