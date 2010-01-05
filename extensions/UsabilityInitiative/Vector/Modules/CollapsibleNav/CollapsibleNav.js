15a193078bafb0629cb099b613999c82f261b370
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-05 19:03:56 +0000

    Made some tweaks to the collapsibleNav code to simplify the DOM manipulations and avoid animation on page load. Bumped combined script versions.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@60681 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/Vector/Modules/CollapsibleNav/CollapsibleNav.js
+++ extensions/UsabilityInitiative/Vector/Modules/CollapsibleNav/CollapsibleNav.js
@@ -2,18 +2,19 @@
 /* First draft and will be changing greatly */
 
 mw.addOnloadHook( function() {
-	if( !wgVectorEnabledModules.collapsibleleftnav )
+	if( !wgVectorEnabledModules.collapsibleleftnav ) {
 		return true;
+	}
 	$j( '#panel' ).addClass( 'collapsible-nav' );
-	$j( '#panel > div.portal' ).toggleClass( 'collapsed' );
 	$j( '#panel > div.portal:first' )
-		.toggleClass( 'expanded' )
-		.toggleClass( 'collapsed' )
+		.addClass( 'expanded' )
 		.find( 'div.body' )
-		.slideToggle( 'fast' );
+		.show();
+	$j( '#panel > div.portal:not(:first)' )
+		.addClass( 'collapsed' );
 	// Toggle the selected menu's class and expand or collapse the menu
 	$j( '#panel > div.portal > h5' ).click( function() {
-		$j( this )
+		$j(this)
 			.parent()
 			.toggleClass( 'expanded' )
 			.toggleClass( 'collapsed' )
