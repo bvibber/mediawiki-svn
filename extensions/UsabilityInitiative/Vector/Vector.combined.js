02435b80bdf8d09dfb0db9c4e95fe0206878cfa7
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-05 19:51:09 +0000

    Fixed some broken built files. Also added cookies to CollapsibleNav.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@60683 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/Vector/Vector.combined.js
+++ extensions/UsabilityInitiative/Vector/Vector.combined.js
@@ -2,18 +2,31 @@
 /* First draft and will be changing greatly */
 
 mw.addOnloadHook( function() {
-	if( !wgVectorEnabledModules.collapsibleleftnav )
+	if( !wgVectorEnabledModules.collapsibleleftnav ) {
 		return true;
+	}
 	$j( '#panel' ).addClass( 'collapsible-nav' );
-	$j( '#panel > div.portal' ).toggleClass( 'collapsed' );
+	// Always show the first portal
 	$j( '#panel > div.portal:first' )
-		.toggleClass( 'expanded' )
-		.toggleClass( 'collapsed' )
+		.addClass( 'expanded' )
 		.find( 'div.body' )
-		.slideToggle( 'fast' );
+		.show();
+	// Remember which portals to hide and show
+	$j( '#panel > div.portal:not(:first)' )
+		.each( function() {
+			if ( $j.cookie( 'vector-nav-' + $j(this).attr( 'id' ) ) == 'true' ) {
+				$j(this)
+					.addClass( 'expanded' )
+					.find( 'div.body' )
+					.show();
+			} else {
+				$j(this).addClass( 'collapsed' );
+			}
+		} );
 	// Toggle the selected menu's class and expand or collapse the menu
 	$j( '#panel > div.portal > h5' ).click( function() {
-		$j( this )
+		$j.cookie( 'vector-nav-' + $j(this).parent().attr( 'id' ), $j(this).parent().is( '.collapsed' ) );
+		$j(this)
 			.parent()
 			.toggleClass( 'expanded' )
 			.toggleClass( 'collapsed' )
