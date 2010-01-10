94a96cd5ed2ffb5ca2cc4518302e5530c3c25ef6
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-10 01:25:41 +0000

    WikiTextLoggedInOut: fixes, fixes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@60882 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiTextLoggedInOut/WikiTextLoggedInOut.i18n.php
+++ extensions/WikiTextLoggedInOut/WikiTextLoggedInOut.i18n.php
@@ -1,8 +1,14 @@
 <?php
-#Internationalization file fo Wiki Text Logged In Out extenstion
+/**
+ * Internationalization file for WikiTextLoggedInOut extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 $messages = array();
 
+/** English */
 $messages['en'] = array(
 	'wikitextloggedinout-desc' => 'Two parser hooks, <tt>&lt;loggedin&gt;</tt> and <tt>&lt;loggedout&gt;</tt> to show different text depending on the users\' login state',
 );
