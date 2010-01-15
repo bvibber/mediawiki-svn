8e100eb7c83c240a2262624179c1775cf814a449
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-15 04:07:57 +0000

    Syntax error
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61077 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/alertbot/irc/config.tpl.php
+++ tools/alertbot/irc/config.tpl.php
@@ -7,7 +7,7 @@
 	$password=" ";               // nickserv password
 	$bulksms_from="abc@example.com"; // bulksms.co.uk "From" email address
 	$bulksms_auth=" ";           // bulksms.co.uk auth password
-	$url="http://example.com/warning.php" // URL to the Web UI
+	$url="http://example.com/warning.php"; // URL to the Web UI
 	$ttw=5*60;                   // seconds between warning display in IRC and SMS sending
 	$smsinterval=20*60           //seconds between sending of SMS, to avoid flood
 
