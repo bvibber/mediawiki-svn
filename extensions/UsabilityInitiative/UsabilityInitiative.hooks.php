b222fc89301e668dccf8cedea2e00e8651db1cf9
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-15 22:29:51 +0000

    Fixed some IE issues, but getContents is still causing parser issues.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61103 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -84,7 +84,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.namespaceSelect.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 22 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 44 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 45 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 12 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 41 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 10 ),
@@ -94,10 +94,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 1 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 142 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 143 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 142 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 143 ),
 			),
 		),
 	);
