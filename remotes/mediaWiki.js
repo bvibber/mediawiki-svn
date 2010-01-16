a00725aa4554e9e7ca073acac6c17a29f64ef099
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-16 18:07:06 +0000

    removed old this.pe
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work/phase3/js/mwEmbed@61141 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r75';
+var mwRemoteVersion = 'r76';
 var mwUseScriptLoader = true;
 
 // Setup up request Params: 
