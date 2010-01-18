adcbfaecd04a7ed280befa9231cc4ac82ce18ea3
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-18 02:12:33 +0000

    UsabilityInitiative: Whitespace cleanup in FooterCleanup
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61194 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector/Modules/FooterCleanup/FooterCleanup.js
+++ Vector/Modules/FooterCleanup/FooterCleanup.js
@@ -13,41 +13,46 @@ mw.ready( function() {
 			.remove()
 			.text()
 			// FIXME - Not a long-term solution. This change should be done in the message itself
-			.replace( /\)|\(/g, '' ) )
+			.replace( /\)|\(/g, '' )
+		)
 		.change( function() {
-			if( $j( this ).val().length == 0 ){
+			if ( $j( this ).val().length == 0 ) 
 				$j( this )
 					.addClass( 'inline-hint' )
 					.val( $j( this ).data( 'hint' ) );
-					
 			} else {
 				$j( this ).removeClass( 'inline-hint' );
-			} } )
+			}
+		} )
 		.focus( function() {
-				if( $j( this ).val() == $j( this ).data( 'hint' ) ) {
-					$j( this )
-						.removeClass( 'inline-hint' )
-						.val( "" );
-				}
-			})
+			if ( $j( this ).val() == $j( this ).data( 'hint' ) ) {
+				$j( this )
+					.removeClass( 'inline-hint' )
+					.val( "" );
+			}
+		})
 		.blur( function() { $j( this ).trigger( 'change' ); } )
 		.trigger( 'change' );
-		$j( '#wpSummary' )
-			.add( '.editCheckboxes' )
-			.wrapAll( '<div id="editpage-summary-fields"></div>');
-					
-		// transclusions
-		// FIXME - bad CSS styling here with double class selectors. Should address here. 
-		var transclusionCount = ( $j( '.templatesUsed ul li' ).length );
-		$j( '.templatesUsed ul' )
-			.wrap('<div id="transclusions-list" class="collapsible-list collapsed"></div>')
-			.parent()
-			.prepend( '<label>This page contains <a href="http://en.wikipedia.org/wiki/transclusion">transclusions</a> of <strong>'
-				+ transclusionCount 
-				+ "</strong> other pages.</label>");
-		$j( '.mw-templatesUsedExplanation' ).remove();
+	$j( '#wpSummary' )
+		.add( '.editCheckboxes' )
+		.wrapAll( '<div id="editpage-summary-fields"></div>');
 		
-		$j( '.collapsible-list label' ).click( function() {
+	$j( '#editpage-specialchars' ).remove();
+	
+	// transclusions
+	// FIXME - bad CSS styling here with double class selectors. Should address here. 
+	var transclusionCount = $j( '.templatesUsed ul li' ).size();
+	$j( '.templatesUsed ul' )
+		.wrap( '<div id="transclusions-list" class="collapsible-list collapsed"></div>' )
+		.parent()
+		// FIXME: i18n, remove link from message and let community add link to transclusion page if it exists
+		.prepend( '<label>This page contains <a href="http://en.wikipedia.org/wiki/transclusion">transclusions</a> of <strong>'
+			+ transclusionCount 
+			+ '</strong> other pages.</label>');
+	$j( '.mw-templatesUsedExplanation' ).remove();
+	
+	$j( '.collapsible-list label' )
+		.click( function() {
 			$j( this )
 				.parent()
 				.toggleClass( 'expanded' )
@@ -55,11 +60,11 @@ mw.ready( function() {
 				.find( 'ul' )
 				.slideToggle( 'fast' );
 			return false;
-			})
-			.trigger( 'click' );
-		$j( '#wpPreview, #wpDiff, .editHelp, #editpage-specialchars' )
-			.remove();
-		$j( '#mw-editform-cancel' )
-			.remove()
-			.appendTo('.editButtons');
+		})
+		.trigger( 'click' );
+	$j( '#wpPreview, #wpDiff, .editHelp, #editpage-specialchars' )
+		.remove();
+	$j( '#mw-editform-cancel' )
+		.remove()
+		.appendTo('.editButtons');
 } );
