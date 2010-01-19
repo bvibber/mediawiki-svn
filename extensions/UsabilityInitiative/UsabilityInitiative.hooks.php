a469914505a406d7dfe99d24b9e05037ece82b44
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-19 22:03:58 +0000

    Massive changes to wikiEditor.toc.js to better handling resize events.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61272 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -25,16 +25,16 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 10 ),
 				array( 'src' => 'css/wikiEditor.preview.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.templateEditor.css', 'version' => 1 ),
-				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 26 ),
+				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 27 ),
 				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 9 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 49 ),
+				array( 'src' => 'css/combined.css', 'version' => 50 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 49 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 50 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
@@ -173,7 +173,7 @@ class UsabilityInitiativeHooks {
 				array(
 					'src' => 'js/plugins/jquery.wikiEditor.toc.js',
 					'class' => 'j.wikiEditor.modules.toc',
-					'version' => 62
+					'version' => 63
 				),
 				array(
 					'src' => 'js/plugins/jquery.wikiEditor.preview.js',
@@ -191,10 +191,10 @@ class UsabilityInitiativeHooks {
 					'version' => 1 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 155 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 156 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 155 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 156 ),
 			),
 		),
 	);
