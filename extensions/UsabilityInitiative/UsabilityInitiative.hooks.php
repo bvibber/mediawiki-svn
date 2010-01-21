e842689e141fe8f74d43382d8f3f44ffe8e3278a
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-21 23:15:45 +0000

    Switched use of a completely undefined variable "range" to selection - however the part of beforeSelection that is suposed to handle cases where the selection is between elements is not working.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -153,7 +153,7 @@ class UsabilityInitiativeHooks {
 				array(
 					'src' => 'js/plugins/jquery.wikiEditor.js',
 					'class' => 'j.wikiEditor',
-					'version' => 57
+					'version' => 58
 				),
 				array(
 					'src' => 'js/plugins/jquery.wikiEditor.highlight.js',
@@ -191,10 +191,10 @@ class UsabilityInitiativeHooks {
 					'version' => 1 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 164 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 165 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 164 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 165 ),
 			),
 		),
 	);
