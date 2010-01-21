9b8eb7097e80609e5d201079f46bad42d8a71cef
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-21 23:12:02 +0000

    A line added in r61308 is causing bug 22205.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61344 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/WikiEditor.hooks.php
+++ WikiEditor/WikiEditor.hooks.php
@@ -35,7 +35,7 @@ class WikiEditorHooks {
 			array(
 				'src' => 'Modules/Toolbar/Toolbar.js',
 				'class' => 'wikiEditor.config.toolbar',
-				'version' => 22
+				'version' => 23
 			),
 			array(
 				'src' => 'Modules/TemplateEditor/TemplateEditor.js',
@@ -44,10 +44,10 @@ class WikiEditorHooks {
 			),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 23 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 24 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 23 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 24 ),
 		),
 	);
 	static $messages = array(
