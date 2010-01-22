b488972f6cda80aca53745fd4de21caf1e9b9c7e
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-22 19:50:14 +0000

    Follow up to r61308 that addresses bug 22205 without losing any functionality.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61383 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/WikiEditor.hooks.php
+++ WikiEditor/WikiEditor.hooks.php
@@ -35,7 +35,7 @@ class WikiEditorHooks {
 			array(
 				'src' => 'Modules/Toolbar/Toolbar.js',
 				'class' => 'wikiEditor.config.toolbar',
-				'version' => 23
+				'version' => 24
 			),
 			array(
 				'src' => 'Modules/TemplateEditor/TemplateEditor.js',
@@ -44,10 +44,10 @@ class WikiEditorHooks {
 			),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 25 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 26 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 25 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 26 ),
 		),
 	);
 	static $messages = array(
