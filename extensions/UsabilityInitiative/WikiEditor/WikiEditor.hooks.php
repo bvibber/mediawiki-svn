c81224755258ccc15cfea5a22e075a69e884d607
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-23 19:17:02 +0000

    UsabilityInitiative: Fix bugs in interaction between target-text sync and textbox tooltips in link dialog
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61427 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -35,7 +35,7 @@ class WikiEditorHooks {
 			array(
 				'src' => 'Modules/Toolbar/Toolbar.js',
 				'class' => 'wikiEditor.config.toolbar',
-				'version' => 25
+				'version' => 26
 			),
 			array(
 				'src' => 'Modules/TemplateEditor/TemplateEditor.js',
@@ -44,10 +44,10 @@ class WikiEditorHooks {
 			),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 27 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 28 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 27 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 28 ),
 		),
 	);
 	static $messages = array(
