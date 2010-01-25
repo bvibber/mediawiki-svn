16461f01158a564036d217fadcaaea72609c3f4b
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-25 13:59:10 +0000

    Followup r61489, rename variable with wg prefix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61490 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -35,7 +35,7 @@ class WikiEditorHooks {
 			array(
 				'src' => 'Modules/Toolbar/Toolbar.js',
 				'class' => 'wikiEditor.config.toolbar',
-				'version' => 29
+				'version' => 30
 			),
 			array(
 				'src' => 'Modules/TemplateEditor/TemplateEditor.js',
@@ -44,10 +44,10 @@ class WikiEditorHooks {
 			),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 31 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 32 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 31 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 32 ),
 		),
 	);
 	static $messages = array(
