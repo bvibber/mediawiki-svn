a94809d1e7a5a0cee39b7461dd99dc7c2af0ae40
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-26 00:39:53 +0000

    Using CSS widths in tables is a dangerous game! Also, IE has very strange ideas of how tables should negotiate widths, so when in doubt, specify!
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61506 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -35,7 +35,7 @@ class WikiEditorHooks {
 			array(
 				'src' => 'Modules/Toolbar/Toolbar.js',
 				'class' => 'wikiEditor.config.toolbar',
-				'version' => 31
+				'version' => 32
 			),
 			array(
 				'src' => 'Modules/TemplateEditor/TemplateEditor.js',
@@ -44,10 +44,10 @@ class WikiEditorHooks {
 			),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 33 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 34 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 33 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 34 ),
 		),
 	);
 	static $messages = array(
