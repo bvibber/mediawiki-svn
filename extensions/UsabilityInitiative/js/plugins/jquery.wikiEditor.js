82a7753ffeee29293248218fc04ebdf4ff6261d1
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-27 18:58:33 +0000

    Comment change.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61586 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.js
+++ extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.js
@@ -369,7 +369,7 @@ if ( typeof context == 'undefined' ) {
 					.replace( /&nbsp;/g, " " ) // We inserted these to prevent IE from collapsing spaces
 					.replace( /\<p[^\>]*\>/gi, "\n" ) // IE uses </p><p> for user-inserted line breaks
 					.replace( /\<\/p[^\>]*\>/gi, "" )
-					.replace( /\<div[^\>]*\>/gi, "\n" ) // Webkit uses </p><p> for user-inserted line breaks
+					.replace( /\<div[^\>]*\>/gi, "\n" ) // Webkit uses </div><div> for user-inserted line breaks
 					.replace( /\<\/div[^\>]*\>/gi, "" )
 				+ '</pre>' );
 			// Get rid of the noincludes when getting text
