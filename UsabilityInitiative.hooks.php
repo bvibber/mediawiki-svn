1cb8f9fa51f82665b3c84f440ff566f0c15413d1
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-27 01:18:42 +0000

    RTL Dialog changes
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61549 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -22,18 +22,18 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/vector.collapsibleNav.css', 'version' => 7 ),
 				array( 'src' => 'css/vector.footerCleanup.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.css', 'version' => 8 ),
-				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 14 ),
+				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 15 ),
 				array( 'src' => 'css/wikiEditor.preview.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 28 ),
 				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 10 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 57 ),
+				array( 'src' => 'css/combined.css', 'version' => 58 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 57 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 58 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
