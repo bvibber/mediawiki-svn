fe3b28dcd207f379753f4b1ea827708b565d040d
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-27 01:03:14 +0000

    RTL changes for collapsedNav
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@61547 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -19,7 +19,7 @@ class UsabilityInitiativeHooks {
 		'base_sets' => array(
 			'raw' => array(
 				array( 'src' => 'css/suggestions.css', 'version' => 6 ),
-				array( 'src' => 'css/vector.collapsibleNav.css', 'version' => 6 ),
+				array( 'src' => 'css/vector.collapsibleNav.css', 'version' => 7 ),
 				array( 'src' => 'css/vector.footerCleanup.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.css', 'version' => 8 ),
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 14 ),
@@ -29,11 +29,11 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 56 ),
+				array( 'src' => 'css/combined.css', 'version' => 57 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 56 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 57 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
