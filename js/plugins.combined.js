b31015b85cfef3f98648047d08dec343b4f8cd40
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-01-28 00:31:10 +0000

    UsabilityInitiative: Fix bug where placing the cursor on an empty line confuses TOC highlighting (for real this time). Also comment out handling of <div>s for Webkit: doesn't fix Webkit properly and breaks all others
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61617 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- js/plugins.combined.js
+++ js/plugins.combined.js
@@ -6793,8 +6793,8 @@ if ( typeof context == 'undefined' ) {
 					.replace( /&nbsp;/g, " " ) // We inserted these to prevent IE from collapsing spaces
 					.replace( /\<p[^\>]*\>/gi, "\n" ) // IE uses </p><p> for user-inserted line breaks
 					.replace( /\<\/p[^\>]*\>/gi, "" )
-					.replace( /\<div[^\>]*\>/gi, "\n" ) // Webkit uses </div><div> for user-inserted line breaks
-					.replace( /\<\/div[^\>]*\>/gi, "" )
+					//.replace( /\<div[^\>]*\>/gi, "\n" ) // Webkit uses </div><div> for user-inserted line breaks
+					//.replace( /\<\/div[^\>]*\>/gi, "" )
 				+ '</pre>' );
 			// Get rid of the noincludes when getting text
 			$pre.find( '.wikiEditor-noinclude' ).each( function() { $( this ).remove(); } );
@@ -7076,7 +7076,7 @@ if ( typeof context == 'undefined' ) {
 					// Start at the selection's start and traverse the DOM backwards
 					// This is done by traversing an element's children first, then the element itself, then its parent
 					e = selection.getRangeAt( 0 ).startContainer;
-					offset = selection.startOffset;
+					offset = selection.getRangeAt( 0 ).startOffset;
 				} else {
 					return $( [] );
 				}
