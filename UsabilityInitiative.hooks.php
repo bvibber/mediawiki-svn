856dde1658a140b1e4745b29b323dd70016936a2
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-01 22:23:39 +0000

    UsabilityInitiative: Fix bug where getSelection() strips newlines in Opera. Thanks to Adam for reporting and confirming the fix
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@61826 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.namespaceSelect.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 26 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 93 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 94 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 29 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 46 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 11 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 2 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 210 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 211 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 210 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 211 ),
 			),
 		),
 	);
