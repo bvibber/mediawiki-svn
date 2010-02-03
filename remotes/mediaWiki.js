0fa9b8716a323d7486359c9aee655dd81d2a16dd
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-03 17:36:57 +0000

    * bumped remote version for update
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work/phase3/js/mwEmbed@61939 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r87';
+var mwRemoteVersion = 'r88';
 var mwUseScriptLoader = true;
 
 //Log the mwRemote version ( will determine what version of js we get )
