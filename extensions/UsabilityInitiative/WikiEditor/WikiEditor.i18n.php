06ff29ba60ac8e249e0f13560bce99f14f11ece8
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-04 23:27:18 +0000

    wmf-deployment: Copy UsabilityInitiative from trunk
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@61999 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.i18n.php
@@ -0,0 +1,563 @@
+<?php
+/**
+ * Internationalisation for Usability Initiative WikiEditor extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Trevor Parscal
+ */
+$messages['en'] = array(
+	'wikieditor' => 'Advanced wikitext editing interface',
+	'wikieditor-desc' => 'Provides an extendable wikitext editing interface and many feature-providing modules',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Message documentation (Message documentation)
+ * @author Fryed-peach
+ * @author Lloffiwr
+ */
+$messages['qqq'] = array(
+	'wikieditor-desc' => "{{desc}}
+
+I guess that 'feature-providing modules' means the same as 'modules providing features'.",
+	'wikieditor-wikitext-tab' => 'Caption of the tab containing the edit box',
+);
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'wikieditor' => 'Uitgebreide teksverwerker vir wikiteks',
+	'wikieditor-wikitext-tab' => 'Wikiteks',
+);
+
+/** Arabic (العربية)
+ * @author Meno25
+ * @author OsamaK
+ */
+$messages['ar'] = array(
+	'wikieditor' => 'واجعة تعديل نص ويكي متقدمة',
+	'wikieditor-desc' => 'يوفر واجهة تعديل نص ويكي قابلة للتمديد والعديد من العناصر الموفرة للخصائص',
+	'wikieditor-wikitext-tab' => 'نص ويكي',
+);
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$messages['arz'] = array(
+	'wikieditor' => 'واجعه تعديل نص ويكى متقدمة',
+	'wikieditor-desc' => 'يوفر واجهه تعديل نص ويكى قابله للتمديد والعديد من العناصر الموفره للخصائص',
+	'wikieditor-wikitext-tab' => 'نص ويكي',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'wikieditor' => 'Пашыраны інтэрфэйс рэдагаваньня вікі-тэксту',
+	'wikieditor-desc' => 'Дадае пашыраны інтэрфэйс рэдагаваньня вікі-тэксту і шмат модульных магчымасьцяў',
+	'wikieditor-wikitext-tab' => 'Вікі-тэкст',
+);
+
+/** Bulgarian (Български)
+ * @author Spiritia
+ */
+$messages['bg'] = array(
+	'wikieditor' => 'Подобрен интерфейс за редактиране на уикитекст',
+	'wikieditor-wikitext-tab' => 'Уикитекст',
+);
+
+/** Bengali (বাংলা)
+ * @author Bellayet
+ */
+$messages['bn'] = array(
+	'wikieditor' => 'উন্নত উইকিটেক্সট সম্পাদনা ইন্টারফেস',
+);
+
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'wikieditor' => 'Etrefas kemmañ wikitestenn klokaet',
+	'wikieditor-desc' => "Poruchas a ra un etrefas kemmañ wikitestenn astennidik ha meur a arc'hwel all.",
+	'wikieditor-wikitext-tab' => 'Wikitestenn',
+);
+
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'wikieditor' => 'Napredni interfejs za uređivanje wikiteksta',
+	'wikieditor-desc' => 'Omogućava proširivi interfejs za uređivanje wikiteksta i mnoge module sa brojnim mogućnostima',
+	'wikieditor-wikitext-tab' => 'Wikitekst',
+);
+
+/** Catalan (Català)
+ * @author Solde
+ */
+$messages['ca'] = array(
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Czech (Česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'wikieditor' => 'Vylepšené rozhraní pro editaci wikitextu',
+	'wikieditor-desc' => 'Poskytuje rozšiřitelné editační rozhraní a mnoho modulů s novými funkcemi',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Welsh (Cymraeg)
+ * @author Lloffiwr
+ */
+$messages['cy'] = array(
+	'wikieditor' => 'Rhyngwyneb golygu testun wici uwch',
+	'wikieditor-wikitext-tab' => 'Testun wici',
+);
+
+/** German (Deutsch)
+ * @author Als-Holder
+ * @author Imre
+ * @author Metalhead64
+ */
+$messages['de'] = array(
+	'wikieditor' => 'Fortgeschrittener Wikitext-Editor',
+	'wikieditor-desc' => 'Stellt eine erweiterbare Oberfläche zur Wikitext-Beabeitung sowie weitere funktionelle Module zur Verfügung',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Zazaki (Zazaki)
+ * @author Mirzali
+ * @author Xoser
+ */
+$messages['diq'] = array(
+	'wikieditor' => 'Interfaceye wikitext ke raver siyayo',
+	'wikieditor-desc' => 'Yew pedê nuştişiyê girdi u moduleyanê binan dano',
+	'wikieditor-wikitext-tab' => 'Wikimeqale',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'wikieditor' => 'Rozšyrjony pówjerch za wobźěkowabnje wikiteksta',
+	'wikieditor-desc' => 'Stoj rozšyrjobny pówjerch za wobźěłowanje wikiteksta a wjele modulow k dispoziciji',
+	'wikieditor-wikitext-tab' => 'WikiTekst',
+);
+
+/** Greek (Ελληνικά)
+ * @author ZaDiak
+ */
+$messages['el'] = array(
+	'wikieditor-wikitext-tab' => 'Βικικείμενο',
+);
+
+/** Esperanto (Esperanto)
+ * @author Yekrats
+ */
+$messages['eo'] = array(
+	'wikieditor' => 'Progresa redaktilo por vikiteksto',
+	'wikieditor-desc' => 'Ebligas redaktan interfacon por etendebla vikiteksto kaj multaj utilaj moduloj',
+	'wikieditor-wikitext-tab' => 'Vikiteksto',
+);
+
+/** Spanish (Español)
+ * @author Crazymadlover
+ * @author Translationista
+ */
+$messages['es'] = array(
+	'wikieditor' => 'FUZZY!!! Interfaz avanzada de edición de Wikitexto',
+	'wikieditor-desc' => 'Provee de una interfaz extensible de edición de wikitexto y varios módulos que ofrecen características',
+	'wikieditor-wikitext-tab' => 'Wikitexto',
+);
+
+/** Estonian (Eesti)
+ * @author Avjoska
+ */
+$messages['et'] = array(
+	'wikieditor-wikitext-tab' => 'Vikitekst',
+);
+
+/** Basque (Euskara)
+ * @author An13sa
+ */
+$messages['eu'] = array(
+	'wikieditor-wikitext-tab' => 'Wikitestua',
+);
+
+/** Finnish (Suomi)
+ * @author Crt
+ * @author ZeiP
+ */
+$messages['fi'] = array(
+	'wikieditor' => 'Edistynyt wikitext-muokkauskäyttöliittymä',
+	'wikieditor-desc' => 'Tarjoaa laajennettavan wikitext-muokkauskäyttöliittymän ja monia toimintoja tarjoavia laajennuksia.',
+	'wikieditor-wikitext-tab' => 'Wikiteksti',
+);
+
+/** French (Français)
+ * @author IAlex
+ * @author Jean-Frédéric
+ */
+$messages['fr'] = array(
+	'wikieditor' => 'Interface avancée de modification de wikitexte',
+	'wikieditor-desc' => 'Fournit un interface de modification de wikitexte extensible et plusieurs fonctionnalités.',
+	'wikieditor-wikitext-tab' => 'WikiTexte',
+);
+
+/** Franco-Provençal (Arpetan)
+ * @author ChrisPtDe
+ */
+$messages['frp'] = array(
+	'wikieditor' => 'Entèrface de changement de vouiquitèxto avanciê',
+	'wikieditor-desc' => 'Balye una entèrface de changement de vouiquitèxto èxtensibla et un mouél de fonccionalitâts.',
+	'wikieditor-wikitext-tab' => 'Vouiquitèxto',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'wikieditor' => 'Interface avanzada de edición de texto wiki',
+	'wikieditor-desc' => 'Proporciona unha interface extensible de edición de texto wiki e moitas funcionalidades de módulos',
+	'wikieditor-wikitext-tab' => 'Texto wiki',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'wikieditor' => 'Oberflechi fir s fortgschritte Wikitext-Bearbeite',
+	'wikieditor-desc' => 'Stellt e uusklappbari Wikitext-Bearbeitigsoberflechi z Verfiegig un vili Modul mit extra Megligkeite',
+	'wikieditor-wikitext-tab' => 'WikiText',
+);
+
+/** Hebrew (עברית)
+ * @author YaronSh
+ */
+$messages['he'] = array(
+	'wikieditor' => 'מנשק מתקדם לעריכת טקסט ויקי',
+	'wikieditor-desc' => 'מספקת מנשק עריכת ויקיטקסט הניתן להרחבה ומודולים המאפשרים להוסיף יכולות',
+	'wikieditor-wikitext-tab' => 'ויקיטקסט',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'wikieditor' => 'Rozšěrjeny powjerch za wobdźěłowanje wikiteksta',
+	'wikieditor-desc' => 'Steji rozšěrjujomny powjerch za wobdźěłowanje wikiteksta a wjele modulow k dispoziciji.',
+	'wikieditor-wikitext-tab' => 'WikiTekst',
+);
+
+/** Hungarian (Magyar)
+ * @author Dani
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'wikieditor' => 'Fejlett szerkesztőfelület wikiszöveghez',
+	'wikieditor-desc' => 'Bővíthető szerkesztőfelület wikiszöveghez és több, új funkciókat kínáló modul',
+	'wikieditor-wikitext-tab' => 'Wikiszöveg',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'wikieditor' => 'Interfacie avantiate de modification de wikitexto',
+	'wikieditor-desc' => 'Forni un interfacie de modification de wikitexto extensibile con multe modulos a functiones additional',
+	'wikieditor-wikitext-tab' => 'Wikitexto',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'wikieditor' => 'Antarmuka penyuntingan teks wiki canggih',
+	'wikieditor-desc' => 'Menyediakan antarmuka penyuntingan teks wiki yang lebih berkembang dan banyak modul penyedia fitur',
+	'wikieditor-wikitext-tab' => 'TeksWiki',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'wikieditor' => '高度なウィキテキスト編集インターフェイス',
+	'wikieditor-desc' => '拡張可能なウィキテキスト編集インターフェイスと多数の機能モジュールを提供する',
+	'wikieditor-wikitext-tab' => 'ウィキテキスト',
+);
+
+/** Georgian (ქართული)
+ * @author BRUTE
+ */
+$messages['ka'] = array(
+	'wikieditor-wikitext-tab' => 'ვიკიტექსტი',
+);
+
+/** Ripoarisch (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'wikieditor' => 'Fottjeschredde Eddiror för de WikiTäx',
+	'wikieditor-desc' => 'Brängk en Müjjeleschkeit för Wikitäx enzejävve un ze ändere en et Wiki, di mer ußboue kann, un noch allerhand Kröhmsches met dobei hät.',
+	'wikieditor-wikitext-tab' => 'Wikitäx',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'wikieditor' => "Erweiderten Interface fir d'Änner vu Wikitext",
+	'wikieditor-desc' => "Liwwert en erweiderbaren Interface fir d'Ännere vu Wikitext a vill Module mat Fonctiounen",
+	'wikieditor-wikitext-tab' => 'WikiText',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'wikieditor' => 'Интерфејс за напредно уредување со викитекст',
+	'wikieditor-desc' => 'Дава проширлив интерфејс за уредување со викитекст и многу модули за разни други алатки',
+	'wikieditor-wikitext-tab' => 'ВикиТекст',
+);
+
+/** Malayalam (മലയാളം)
+ * @author Praveenp
+ */
+$messages['ml'] = array(
+	'wikieditor' => 'വിക്കിഎഴുത്തുകൾ തിരുത്തുവാനുള്ള നൂതന ദൃശ്യമണ്ഡലം',
+	'wikieditor-desc' => 'വികസിപ്പിക്കാവുന്ന വിധത്തിലുള്ള വിക്കിഎഴുത്ത് തിരുത്തൽ ദൃശ്യമണ്ഡലവും മറ്റ് സവിശേഷ ഘടകങ്ങളും നൽകുന്നു',
+	'wikieditor-wikitext-tab' => 'വിക്കിഎഴുത്ത്',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wikieditor' => 'Uitgebreide tekstverwerker voor wikitekst',
+	'wikieditor-desc' => 'Voegt een uitbreidbare tekstverwerker voor wikitekst toe en modules met extra mogelijkheden',
+	'wikieditor-wikitext-tab' => 'Wikitekst',
+);
+
+/** Norwegian Nynorsk (‪Norsk (nynorsk)‬)
+ * @author Gunnernett
+ * @author Harald Khan
+ */
+$messages['nn'] = array(
+	'wikieditor' => 'Avansert grensesnitt for endring av wikitekst',
+	'wikieditor-wikitext-tab' => 'Wikitekst',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Laaknor
+ */
+$messages['no'] = array(
+	'wikieditor' => 'Avansert grensesnitt for redigering av wikitekst',
+	'wikieditor-desc' => 'Gir et utvidbart tekstredigeringsgrensesnitt og mange utvidelsesmoduler',
+	'wikieditor-wikitext-tab' => 'Wikitekst',
+);
+
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'wikieditor' => 'Interfàcia avançada de modificacion de wikitèxte',
+	'wikieditor-desc' => 'Provesís una interfàcia de modificacion de wikitèxte extensible e mantuna foncionalitats.',
+	'wikieditor-wikitext-tab' => 'Wikitèxte',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ * @author Wpedzich
+ */
+$messages['pl'] = array(
+	'wikieditor' => 'Zaawansowany interfejs edycji wikitekstu',
+	'wikieditor-desc' => 'Dostarcza rozszerzalny interfejs edycji wikitekstu oraz wiele funkcji oferowane przez moduły',
+	'wikieditor-wikitext-tab' => 'Wikikod',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'wikieditor' => 'Antërfacia ëd modìfica avansà ëd wikitext',
+	'wikieditor-desc' => "A dà n'antërfacia ëd modìfica estendìbil ëd wikitext e motobin ëd modoj ch'a dan ëd fonsionalità",
+	'wikieditor-wikitext-tab' => 'Test Wiki',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'wikieditor' => 'Interface avançada de edição de wikitexto',
+	'wikieditor-desc' => 'Fornece uma interface extensível de edição de wikitexto e vários módulos funcionais',
+	'wikieditor-wikitext-tab' => 'Texto Wiki',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Daemorris
+ */
+$messages['pt-br'] = array(
+	'wikieditor' => 'Interface avançada de edição de textowiki',
+	'wikieditor-desc' => 'Fornece uma interface extensível de edição de textowiki e vários módulos funcionais',
+	'wikieditor-wikitext-tab' => 'textowiki',
+);
+
+/** Romanian (Română)
+ * @author Firilacroco
+ */
+$messages['ro'] = array(
+	'wikieditor' => 'Interfaţă de modificare wikitext avansată',
+	'wikieditor-desc' => 'Afişează o interfaţă de modificare wikitext ce se poate extinde cu multe funcţionalităţi',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'wikieditor' => "Wikitext avanzate pe le cangiaminde de l'inderfacce",
+	'wikieditor-desc' => "Dè 'nu wikitext estendibbele pe le cangiaminde de l'inderfacce e 'nu sbuenne de funzionalità de cchiù previste sus a le module",
+	'wikieditor-wikitext-tab' => 'Uicchiteste',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'wikieditor' => 'Расширенный интерфейс редактирования викитекста',
+	'wikieditor-desc' => 'Обеспечивает расширяемый интерфейс редактирования викитекста и многие функции, предоставляемые модулями',
+	'wikieditor-wikitext-tab' => 'Викитекст',
+);
+
+/** Yakut (Саха тыла)
+ * @author HalanTul
+ */
+$messages['sah'] = array(
+	'wikieditor' => 'Биики тиэкиһи эрэдээксийэлиир кэҥэтиллибит интерфейс',
+	'wikieditor-desc' => 'Биики тиэкис кэҥэтиллэр интерфейсын уонна модуллар көмөлөрүнэн атын кыахтары хааччыйар',
+	'wikieditor-wikitext-tab' => 'Биики тиэкис',
+);
+
+/** Sinhala (සිංහල)
+ * @author Calcey
+ */
+$messages['si'] = array(
+	'wikieditor' => 'දියුණු කළ විකි පෙළ සංස්කරණය කිරීමේ අතුරු මුහුණත',
+	'wikieditor-wikitext-tab' => 'විකි පෙළ',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'wikieditor' => 'Pokročilé rozhranie na úpravu wikitextu',
+	'wikieditor-desc' => 'Poskytuje rozšíriteľné rozhranie na úpravu wikitextu a mnohé moduly poskytujúce funkcionalitu',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Swedish (Svenska)
+ * @author Boivie
+ */
+$messages['sv'] = array(
+	'wikieditor' => 'Avancerat gränssnitt för redigering av wikitext',
+	'wikieditor-desc' => 'Tillhandahåller ett utökat gränssnitt för redigering av wikitext och många moduler som tillhandahåller funktioner',
+	'wikieditor-wikitext-tab' => 'Wikitext',
+);
+
+/** Swahili (Kiswahili)
+ * @author Lloffiwr
+ */
+$messages['sw'] = array(
+	'wikieditor-wikitext-tab' => 'Kodi ya wiki',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'wikieditor' => 'ఉన్నత వికీపాఠ్య దిద్దుబాటు ముఖాంతరం',
+	'wikieditor-wikitext-tab' => 'వికీపాఠ్యం',
+);
+
+/** Turkmen (Türkmençe)
+ * @author Hanberke
+ */
+$messages['tk'] = array(
+	'wikieditor' => 'Giňeldilen wikitekst redaktirleme interfeýsi',
+	'wikieditor-desc' => 'Giňeldilýän wikitekst redkatirleme interfeýsini we köp aýratynlykly modullary üpjün edýär',
+	'wikieditor-wikitext-tab' => 'Wikitekst',
+);
+
+/** Turkish (Türkçe)
+ * @author Joseph
+ * @author Vito Genovese
+ */
+$messages['tr'] = array(
+	'wikieditor' => 'Gelişmiş viki metni düzenleme arayüzü',
+	'wikieditor-desc' => 'Genişletilebilir bir viki metni düzenleme arayüzü ve birçok özellik sağlayan modüller sağlar',
+	'wikieditor-wikitext-tab' => 'Vikimetin',
+);
+
+/** Ukrainian (Українська)
+ * @author AS
+ * @author Prima klasy4na
+ */
+$messages['uk'] = array(
+	'wikieditor' => 'Розширений інтерфейс редагування вікітексту',
+	'wikieditor-desc' => 'Забезпечує розширюваний інтерфейс редагування вікітексту і багато функціональних модулів',
+	'wikieditor-wikitext-tab' => 'Вікітекст',
+);
+
+/** Vèneto (Vèneto)
+ * @author Candalua
+ */
+$messages['vec'] = array(
+	'wikieditor' => 'Interfacia avansada de modifica testo wiki',
+	'wikieditor-desc' => 'Fornisse na interfacia avansada de modifica testo wiki e vari moduli funsionali',
+	'wikieditor-wikitext-tab' => 'Testo wiki',
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'wikieditor' => 'Giao diện sửa đổi văn bản wiki nâng cao',
+	'wikieditor-desc' => 'Cung cấp giao diện sửa đổi văn bản wiki có thể mở rộng, cũng như nhiều tính năng khác',
+	'wikieditor-wikitext-tab' => 'Mã wiki',
+);
+
+/** Yiddish (ייִדיש)
+ * @author פוילישער
+ */
+$messages['yi'] = array(
+	'wikieditor-wikitext-tab' => 'וויקיטעקסט',
+);
+
+/** Simplified Chinese (‪中文(简体)‬)
+ * @author Bencmq
+ * @author PhiLiP
+ */
+$messages['zh-hans'] = array(
+	'wikieditor' => '高级维基文本编辑界面',
+	'wikieditor-desc' => '提供可扩充的维基文本编辑界面及功能组件',
+	'wikieditor-wikitext-tab' => 'Wiki文本',
+);
+
+/** Traditional Chinese (‪中文(繁體)‬)
+ * @author Gaoxuewei
+ * @author Liangent
+ */
+$messages['zh-hant'] = array(
+	'wikieditor' => '高級維基文本編輯界面',
+	'wikieditor-desc' => '提供可擴充的維基文本編輯界面及功能組件',
+	'wikieditor-wikitext-tab' => '維基文本',
+);
+
