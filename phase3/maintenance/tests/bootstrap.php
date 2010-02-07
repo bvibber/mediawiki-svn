e63fcaad7fc514475b912b4e9098177ab057e192
happy-melon <happy-melon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-07 14:14:25 +0000

    Branch for parser work, material mainly from than4213 (currently at commit access requests).
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/parser-work@62083 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ phase3/maintenance/tests/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Set up the MediaWiki environment when running tests with "phpunit" command
+ *
+ * Warning: this file is not included from global scope!
+ * @file
+ */
+
+global $wgCommandLineMode, $IP;
+$wgCommandLineMode = true;
+$IP = dirname( dirname( dirname( __FILE__ ) ) );
+
+define( 'MEDIAWIKI', true );
+define( 'MW_PHPUNIT_TEST', true );
+
+require_once( "$IP/includes/Defines.php" );
+require_once( "$IP/includes/AutoLoader.php" );
+require_once( "$IP/LocalSettings.php" );
+require_once( "$IP/includes/ProfilerStub.php" );
+require_once( "$IP/includes/GlobalFunctions.php" );
+require_once( "$IP/includes/Hooks.php" );
+$self = __FILE__;
+require_once( "$IP/includes/Setup.php" );
+
+
