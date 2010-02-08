f89ed3d2950bb977e78a9a2709e8355d79b9c60e
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-08 16:57:55 +0000

    Adding a tabindex of 1 to the iframe in response to bug 22311
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62127 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.namespaceSelect.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 27 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 107 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 108 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 29 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 47 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 12 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 2 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 225 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 226 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 225 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 226 ),
 			),
 		),
 	);
