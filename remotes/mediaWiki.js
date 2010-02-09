a7a3212f0eb16e8b60dde2aff86bedce6bf20566
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-09 01:10:30 +0000

    * testing fix to minification bug
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work/phase3/js/mwEmbed@62151 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r93';
+var mwRemoteVersion = 'r94';
 var mwUseScriptLoader = true;
 
 // Log the mwRemote version ( will determine what version of js we get )
