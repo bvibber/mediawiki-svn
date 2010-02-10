b8fbdb6b8a70ba89511395578da9232200544eca
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-10 19:32:33 +0000

    Wrap <br> tags with <p> tags, to give browsers the needed hint on how to create new lines surrounding them. This fixes bug #22401 which was occuring when <br> or <div> was being used instead of <p> for new lines in the body.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@62258 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.namespaceSelect.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 27 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 119 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 118 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 29 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 48 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 12 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 2 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 237 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 238 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 237 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 238 ),
 			),
 		),
 	);
