08292edc60cc711e7ccc9cc62966cbb475599139
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-10 01:04:39 +0000

    Adjusted the hover color of toolbar select widgets.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@62212 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -25,15 +25,15 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 19 ),
 				array( 'src' => 'css/wikiEditor.preview.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 28 ),
-				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 10 ),
+				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 11 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 65 ),
+				array( 'src' => 'css/combined.css', 'version' => 66 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 65 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 66 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
