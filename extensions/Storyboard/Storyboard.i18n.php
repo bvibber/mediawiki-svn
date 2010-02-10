54c83cccb947ea5224fb8ecff1077341a32dc62f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-10 03:15:23 +0000

    Storyboard extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62220 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Storyboard/Storyboard.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalization file for the Storyboard extension.
+ *
+ * @file Storyboard.i18n.php
+ * @ingroup Storyboard
+ *
+ * @author Jeroen De Dauw
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	// General
+	'storyboard_name' => 'Storyboard',
+	'storyboard_desc' => 'Provides a landing page for donors, a page where stories can be submitted, and a story moderation interface.',
+);
---
+++ extensions/Storyboard/Storyboard.php
@@ -0,0 +1,64 @@
+<?php
+
+/**  
+ * Initialization file for the Storyboard extension.
+ * Extension documentation: http://www.mediawiki.org/wiki/Extension:Storyboard
+ *
+ * @file Storyboard.php
+ * @ingroup Storyboard
+ * 
+ * @author Jeroen De Dauw
+ */
+
+/**
+ * This documenation group collects source code files belonging to Storyboard.
+ *
+ * Please do not use this group name for other code.
+ *  
+ * @defgroup Storyboard Storyboard
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+define('Storyboard_VERSION', '0');
+
+$egStoryboardScriptPath 	= $wgScriptPath . '/extensions/Storyboard';
+$egStoryboardDir 			= dirname( __FILE__ ) . '/';
+
+// Include the settings file.
+require_once($egStoryboardDir . 'Storyboard_Settings.php');
+
+// Register the initialization function of Storyboard.
+$wgExtensionFunctions[] = 'efStoryboardSetup'; 
+
+// Register the initernationalization and aliasing files of Storyboard.
+$wgExtensionMessagesFiles['Storyboard'] = $egStoryboardDir . 'Storyboard.i18n.php';
+$wgExtensionAliasesFiles['Storyboard'] = $egStoryboardDir . 'Storyboard.alias.php';
+
+// Load and register the StoryReview special page and register it's group.
+$wgAutoloadClasses['SpecialStoryReview'] = $egStoryboardDir . 'StoryReview_body.php';
+$wgSpecialPages['StoryReview'] = 'SpecialStoryReview';
+$wgSpecialPageGroups['StoryReview'] = 'contribution';
+
+/**
+ * Initialization function for the Storyboard extension.
+ */
+function efStoryboardSetup() {
+	global $wgExtensionCredits;
+	
+	wfLoadExtensionMessages( 'Storyboard' ); 	
+	
+	$wgExtensionCredits['parserhook'][] = array(
+		'path' => __FILE__,
+		'name' => wfMsg('storyboard_name'),
+		'version' => Storyboard_VERSION,
+		'author' => array('[http://bn2vs.com Jeroen De Dauw]'),
+		'url' => 'http://www.mediawiki.org/wiki/Extension:Storyboard',
+		'description' =>  wfMsg( 'storyboard_desc' ),
+		'descriptionmsg' => wfMsg( 'storyboard_desc' ),
+	);
+}
+
+
