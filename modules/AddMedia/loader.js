9d46d174799321ddb1ae3b12eff969bf39326b81
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-11 17:33:47 +0000

    * refactor of upload handling into interfaceDispatch and uploadHandler * "incomplete / untested" will finish up shortly. * some firefogg gui updates
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work/phase3/js/mwEmbed@62314 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- modules/AddMedia/loader.js
+++ modules/AddMedia/loader.js
@@ -1,126 +1,129 @@
 /*
 * Loader for libAddMedia module:
 */
-mw.addMessages( {
-	"mwe-loading-add-media-wiz" : "Loading add media wizard"
-});
 
-// Add class file paths ( From ROOT )
-mw.addClassFilePaths( {
-	"$j.fn.dragDropFile"	: "modules/AddMedia/jquery.dragDropFile.js",	
-	"$j.fn.simpleUploadForm": "modules/AddMedia/jquery.simpleUploadForm.js",
+// Scope everythign in "mw"  ( keeps the global namespace clean ) 
+( function( mw ) {
+
+	mw.addMessages( {
+		"mwe-loading-add-media-wiz" : "Loading add media wizard"
+	});
 	
-	"mw.BaseUploadInterface": "modules/AddMedia/mw.BaseUploadInterface.js",
-	"mw.Firefogg"			: "modules/AddMedia/mw.Firefogg.js",
-	"mw.FirefoggGUI"		: "modules/AddMedia/mw.FirefoggGUI.js",
-	"mw.FirefoggRender"		: "modules/libSequencer/mw.FirefoggRender.js",
-	"mw.RemoteSearchDriver"	: "modules/AddMedia/mw.RemoteSearchDriver.js",		
+	// Add class file paths ( From ROOT )
+	mw.addClassFilePaths( {
+		"$j.fn.dragDropFile"	: "modules/AddMedia/jquery.dragDropFile.js",	
+		"$j.fn.simpleUploadForm": "modules/AddMedia/jquery.simpleUploadForm.js",
+		
+		"mw.BaseUploadHandler": "modules/AddMedia/mw.BaseUploadHandler.js",
+		"mw.Firefogg"			: "modules/AddMedia/mw.Firefogg.js",
+		"mw.FirefoggGUI"		: "modules/AddMedia/mw.FirefoggGUI.js",
+		"mw.FirefoggRender"		: "modules/libSequencer/mw.FirefoggRender.js",
+		"mw.RemoteSearchDriver"	: "modules/AddMedia/mw.RemoteSearchDriver.js",	
+		"mw.InterfaceDispatch"	: "modules/AddMedia/mw.InterfaceDispatch.js",	
+		
+		"baseRemoteSearch"		: "modules/AddMedia/searchLibs/baseRemoteSearch.js",
+		"mediaWikiSearch"		: "modules/AddMedia/searchLibs/mediaWikiSearch.js",
+		"metavidSearch"			: "modules/AddMedia/searchLibs/metavidSearch.js",
+		"archiveOrgSearch"		: "modules/AddMedia/searchLibs/archiveOrgSearch.js",
+		"flickrSearch"			: "modules/AddMedia/searchLibs/flickrSearch.js",
+		"baseRemoteSearch"		: "modules/AddMedia/searchLibs/baseRemoteSearch.js",
+		"kalturaSearch"			: "modules/AddMedia/searchLibs/kalturaSearch.js"
+	});	
 	
-	"baseRemoteSearch"		: "modules/AddMedia/searchLibs/baseRemoteSearch.js",
-	"mediaWikiSearch"		: "modules/AddMedia/searchLibs/mediaWikiSearch.js",
-	"metavidSearch"			: "modules/AddMedia/searchLibs/metavidSearch.js",
-	"archiveOrgSearch"		: "modules/AddMedia/searchLibs/archiveOrgSearch.js",
-	"flickrSearch"			: "modules/AddMedia/searchLibs/flickrSearch.js",
-	"baseRemoteSearch"		: "modules/AddMedia/searchLibs/baseRemoteSearch.js",
-	"kalturaSearch"			: "modules/AddMedia/searchLibs/kalturaSearch.js"
-});	
-
-/**
-* Note: We should move relevant parts of these style sheets to the addMedia/css folder 
-* phase 2: We should separate out sheet sets per sub-module:
-*/ 
-
-//Setup the addMediaWizard module
-mw.addModuleLoader( 'AddMedia.addMediaWizard', function( callback ){
-	// Load all the required libs:
-	var request = [
-		[	'mw.RemoteSearchDriver',
-			'$j.cookie',
-			'$j.fn.textSelection',
-			'$j.browserTest',
-			'$j.ui'
-		], [
-			'$j.ui.resizable',
-			'$j.ui.draggable',
-			'$j.ui.dialog',
-			'$j.ui.tabs',
-			'$j.ui.sortable'
-		]
-	];
-	mw.load( request , function() {
-		callback( 'AddMedia.addMediaWizard' );
-	} );
-});
-
-/*
-* Upload interface loader: 
-*/
-
-mw.addModuleLoader( 'AddMedia.baseUploadInterface', function( callback ){
-	mw.load( [
+	/**
+	* Note: We should move relevant parts of these style sheets to the addMedia/css folder 
+	* phase 2: We should separate out sheet sets per sub-module:
+	*/ 
+	
+	//Setup the addMediaWizard module
+	mw.addModuleLoader( 'AddMedia.addMediaWizard', function( callback ){
+		// Load all the required libs:
+		var request = [
+			[	'mw.RemoteSearchDriver',
+				'$j.cookie',
+				'$j.fn.textSelection',
+				'$j.browserTest',
+				'$j.ui'
+			], [
+				'$j.ui.resizable',
+				'$j.ui.draggable',
+				'$j.ui.dialog',
+				'$j.ui.tabs',
+				'$j.ui.sortable'
+			]
+		];
+		mw.load( request , function() {
+			callback( 'AddMedia.addMediaWizard' );
+		} );
+	});
+	
+	//Set a variable for the base upload interface for easy inclution
+	var baseUploadlibs = [
 		[
-			'mw.BaseUploadInterface',
+			'mw.BaseUploadHandler',
+			'mw.InterfaceDispatch',
 			'$j.ui'
 		],
 		[
 			'$j.ui.progressbar',
-			'$j.ui.dialog'
+			'$j.ui.dialog',
+			'$j.ui.draggable'
 		]
-	], function() {
-		callback( 'AddMedia.baseUploadInterface' );
-	});
-});
-
-/**
- * The Firefogg loaders
- *
- * Includes both firefogg & firefogg "GUI" which share some loading logic: 
- */ 
-var mwBaseFirefoggReq = [
-	[
-		'mw.BaseUploadInterface',
-		'mw.Firefogg',
-		'$j.ui'
-	],
-	[
-		'$j.ui.progressbar',
-		'$j.ui.dialog',
-		'$j.ui.draggable'
-	]
-];
-
-mw.addModuleLoader( 'AddMedia.firefogg', function( callback ){
+	];
 	
-	//Load firefogg libs
-	mw.load( mwBaseFirefoggReq, function() {
-		callback( 'AddMedia.firefogg' );
-	});
-} );
-
-mw.addModuleLoader( 'AddMedia.FirefoggGUI', function( callback ){
+	/*
+	* Upload interface loader: 
+	*/
 	
-	//Clone the array: 
-	var request = mwBaseFirefoggReq.slice( 0 ) ;
-	//Add firefogg gui classes to a new "request" var: 
-	request.push( [
-		'mw.FirefoggGUI',
-		'$j.cookie',
-		'$j.ui.accordion',
-		'$j.ui.slider',
-		'$j.ui.datepicker'
-	] );
-	
-	mw.load( request, function() {
-		callback( 'AddMedia.FirefoggGUI' );
+	mw.addModuleLoader( 'AddMedia.BaseUploadHandler', function( callback ){
+		mw.load( baseUploadlibs , function() {
+			callback( 'AddMedia.BaseUploadHandler' );
+		});
 	});
-} );
+	
+	/**
+	 * The Firefogg loaders
+	 *
+	 * Includes both firefogg & firefogg "GUI" which share some loading logic: 
+	 */ 
+	
+	var mwBaseFirefoggReq = baseUploadlibs.slice( 0 )
+	mwBaseFirefoggReq[0].push('mw.Firefogg');
+	
+	mw.addModuleLoader( 'AddMedia.firefogg', function( callback ){
+		
+		//Load firefogg libs
+		mw.load( mwBaseFirefoggReq, function() {
+			callback( 'AddMedia.firefogg' );
+		});
+	} );
+	
+	mw.addModuleLoader( 'AddMedia.FirefoggGUI', function( callback ){
+		
+		//Clone the array: 
+		var request = mwBaseFirefoggReq.slice( 0 ) ;
+		//Add firefogg gui classes to a new "request" var: 
+		request.push( [
+			'mw.FirefoggGUI',
+			'$j.cookie',
+			'$j.ui.accordion',
+			'$j.ui.slider',
+			'$j.ui.datepicker'
+		] );
+		
+		mw.load( request, function() {
+			callback( 'AddMedia.FirefoggGUI' );
+		});
+	} );
+	
+	mw.addModuleLoader( 'AddMedia.firefoggRender', function( callback ){
+		mw.load( [
+			'mw.BaseUploadHandler',
+			'mw.Firefogg',
+			'mw.FirefoggRender'
+		], function() {
+			callback( 'AddMedia.firefoggRender' );
+		});	
+	});
 
-mw.addModuleLoader( 'AddMedia.firefoggRender', function( callback ){
-	mw.load( [
-		'mw.BaseUploadInterface',
-		'mw.Firefogg',
-		'mw.FirefoggRender'
-	], function() {
-		callback( 'AddMedia.firefoggRender' );
-	});	
-});
+} )( window.mw );
