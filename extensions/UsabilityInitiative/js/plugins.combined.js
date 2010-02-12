a9e1132efcdce225d6dbe8c1b3137ca64708b7db
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-12 21:16:07 +0000

    UsabilityInitiative: Add a godawful hack so the iframe doesn't get loaded if only the toolbar is enabled
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62384 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/js/plugins.combined.js
+++ extensions/UsabilityInitiative/js/plugins.combined.js
@@ -7881,6 +7881,8 @@ if ( typeof context == 'undefined' ) {
 	// Trigger the "resize" event anytime the window is resized
 	$( window ).resize( function( event ) { context.fn.trigger( 'resize', event ); } );
 	// Create an iframe in place of the text area
+	// FAT UGLY HACK: Don't do the iframe when only the toolbar is enabled
+	context.fn.setupIframe = function() {
 	context.$iframe = $( '<iframe></iframe>' )
 		.attr( {
 			'frameBorder': 0,
@@ -8004,6 +8006,11 @@ if ( typeof context == 'undefined' ) {
 			return context.fallbackWindowOnBeforeUnload();
 		}
 	};
+	};
+}
+// FAT UGLY HACK PART 2: Don't do the iframe when only the toolbar is enabled
+if ( typeof context.$iframe === 'undefined' && ( arguments[0] !== 'addModule' || typeof arguments[1].toolbar === 'undefined' ) ) {
+	context.fn.setupIframe();
 }
 
 /* API Execution */
