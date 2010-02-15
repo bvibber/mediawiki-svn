341ec88cd8bf1d3d02fa2d8ab0ff52bf30146f63
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-15 15:41:29 +0000

    Run code through stylize.php, use indention instead of spaces Update extension credits Add right definition Add i18n file Add extension to translatewiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62523 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UserPageEditProtection/UserPageEditProtection.i18n.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalisation file for the UserPageEditProtection extension
+ *
+ * @author Lisa Ridley
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Lisa Ridley
+ */
+$messages['en'] = array(
+	'userpageeditprotection-desc' => 'Restricts editing on user pages to user and allowed editors',
+	'right-editalluserpages' => 'Edit user pages',
+);
+
--- extensions/UserPageEditProtection/UserPageEditProtection.php
+++ extensions/UserPageEditProtection/UserPageEditProtection.php
@@ -1,7 +1,7 @@
 <?php
-if(! defined( 'MEDIAWIKI' ) ) {
+if ( ! defined( 'MEDIAWIKI' ) ) {
    echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
-   die( -1 );
+   die( - 1 );
 }
 /**
  * Extension: UserPageEditProtection.php
@@ -30,47 +30,56 @@ if(! defined( 'MEDIAWIKI' ) ) {
  *
  *
  * Usage:
- * Save this file in your extensions folder of your MediaWiki installation.  Add the following to LocalSettings.php:
+ * Save this file in your extensions folder of your MediaWiki installation.
+ * Add the following lines to LocalSettings.php:
+ *    require_once('extensions/UserPageEditProtection.php');
  *    //turn on user page protection
  *    $wgOnlyUserEditUserPage = true;
- *    //allow sysops to edit user pages by adding the following setting
- *    $wgGroupPermissions['sysop']['editalluserpages'] = true;
- *    require_once('extensions/UserPageEditProtection.php');
  **/
   
 /* register extension */
 $wgExtensionCredits['other'][] = array(
-   'name' => 'UserPageEditProtection',
-   'author' => 'Lisa Ridley, Eric Gingell',
-   'version' => '2.0',
-   'url' => 'http://www.mediawiki.org/wiki/Extension:UserPageEditProtection',
-   'description' => 'This Extension restricts editing on user pages to User and allowed editors');
+	'path' => __FILE__,
+	'name' => 'UserPageEditProtection',
+	'author' => array( 'Lisa Ridley', 'Eric Gingell' ),
+	'version' => '2.0',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:UserPageEditProtection',
+	'description' => 'Restricts editing on user pages to user and allowed editors',
+	'descriptionmsg' => 'userpageeditprotection-desc',
+);
+
+# Internationalisation file
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['UserPageEditProtection'] = $dir . 'UserPageEditProtection.i18n.php';
+
+# Define the new user right
+$wgAvailableRights[] = 'editalluserpages';
+$wgGroupPermissions['sysop']['editalluserpages'] = true;
 
 /* use the userCan hook to check user page edit permissions */
-$wgHooks[ 'userCan' ][] = 'fnUserPageEditProtection';
+$wgHooks['userCan'][] = 'fnUserPageEditProtection';
 
 function fnUserPageEditProtection( $title, $user, $action, &$result ) {
-    global $wgOnlyUserEditUserPage;
-    $lTitle = explode('/', $title->getText());
-    if (!($action == 'edit'||$action == 'move')) {
-            $result = null;
-            return true;
+	global $wgOnlyUserEditUserPage;
+	$lTitle = explode( '/', $title->getText() );
+	if ( !( $action == 'edit' || $action == 'move' ) ) {
+		$result = null;
+	return true;
         }
-    if (NS_USER !== $title->mNamespace) {
-            $result = null;
-            return true;
-    }
-    if ($wgOnlyUserEditUserPage) {
-        if ($user->isAllowed('editalluserpages') || ($lTitle[0] == $user->getname())) {
-            $result = null;
-            return true;
-        } else {
-            $result = false;
-            return false;
-        }
-    }
-    $result = null;
-    return true;
- 
+	if ( $title->mNamespace !== NS_USER ) {
+		$result = null;
+		return true;
+	}
+	if ( $wgOnlyUserEditUserPage ) {
+		if ( $user->isAllowed( 'editalluserpages' ) || ( $user->getname() == $lTitle[0] ) ) {
+			$result = null;
+			return true;
+		} else {
+			$result = false;
+			return false;
+		}
+	}
+	$result = null;
+	return true;
 }
 
