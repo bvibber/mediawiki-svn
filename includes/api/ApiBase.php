56a03b67c2b104c73343dff59d406791c081450a
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-15 20:53:37 +0000

    wmf-deployment: MFT r62545
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@62546 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/api/ApiBase.php
+++ includes/api/ApiBase.php
@@ -642,10 +642,14 @@ abstract class ApiBase {
 	protected function parseMultiValue($valueName, $value, $allowMultiple, $allowedValues) {
 		if( trim($value) === "" && $allowMultiple)
 			return array();
-		$sizeLimit = $this->mMainModule->canApiHighLimits() ? self::LIMIT_SML2 : self::LIMIT_SML1;
-		$valuesList = explode('|', $value, $sizeLimit + 1);
-		if( self::truncateArray($valuesList, $sizeLimit) ) {
-			$this->setWarning("Too many values supplied for parameter '$valueName': the limit is $sizeLimit");
+
+		// This is a bit awkward, but we want to avoid calling canApiHighLimits() because it unstubs $wgUser
+		$valuesList = explode( '|', $value, self::LIMIT_SML2 + 1 );
+		$sizeLimit = count( $valuesList ) > self::LIMIT_SML1 && $this->mMainModule->canApiHighLimits() ?
+			self::LIMIT_SML2 : self::LIMIT_SML1;
+
+		if ( self::truncateArray( $valuesList, $sizeLimit ) ) {
+			$this->setWarning( "Too many values supplied for parameter '$valueName': the limit is $sizeLimit" );
 		}
 		if (!$allowMultiple && count($valuesList) != 1) {
 			$possibleValues = is_array($allowedValues) ? "of '" . implode("', '", $allowedValues) . "'" : '';
