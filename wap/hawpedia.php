fe40b4f3804be74b2edaac65e2e98b8eb573aa5e
nhuffschmid <nhuffschmid@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-17 18:54:05 +0000

    fixed export error
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62649 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- wap/hawpedia.php
+++ wap/hawpedia.php
@@ -202,6 +202,10 @@ function export_wikipedia($searchTerm)
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+	// WP otherwise says: "Please provide a User-Agent header"
+	curl_setopt($ch, CURLOPT_USERAGENT, "hawpedia");
+	
 	$curlResultString = curl_exec($ch);
 	if (!is_string($curlResultString))
 		hawpedia_error(hawtra("Wikipedia currently not available")); // exits internally
