0af8f1f3d28306fec739737ddaaf72450f2cbab3
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-17 19:31:08 +0000

    Ficksed spaling mestake in r62651.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62654 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.js
+++ extensions/UsabilityInitiative/js/plugins/jquery.wikiEditor.js
@@ -124,10 +124,10 @@ $.wikiEditor = {
 	/**
 	 * Checks if a module has a specific requirement
 	 */
-	'isRequired': function( module, reqirement ) {
+	'isRequired': function( module, requirement ) {
 		if ( typeof $.wikiEditor.modules[module]['req'] !== 'undefined' ) {
 			for ( req in $.wikiEditor.modules[module]['req'] ) {
-				if ( $.wikiEditor.modules[module]['req'][req] == reqirement ) {
+				if ( $.wikiEditor.modules[module]['req'][req] == requirement ) {
 					return true;
 				}
 			}
