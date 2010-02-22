1393286023a74e7d34f971daa1502c3c56daed5c
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-22 19:15:43 +0000

    UsabilityInitiative: Recombine, bump style versions for r62724
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@62832 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -78,14 +78,14 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 18 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 93 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.preview.js', 'version' => 11 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.templateEditor.js', 'version' => 21 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.templateEditor.js', 'version' => 22 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 3 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 273 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 274 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 273 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 274 ),
 			),
 		),
 	);
