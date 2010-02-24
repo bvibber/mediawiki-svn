9a844915b6b00285bee927ab7a96b949c446ae57
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-24 13:18:22 +0000

    Rewrite svnImport to subclass Maintenance.php, rename "*" to "all" for all repos, also address concerns from Tim and Bryan on r62510
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62914 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/svnImport.php
+++ extensions/CodeReview/svnImport.php
@@ -1,134 +1,141 @@
 <?php
 
-$optionsWithArgs = array( 'precache' );
-
 $IP = getenv( 'MW_INSTALL_PATH' );
-if ( $IP === false )
+if ( $IP === false ) {
 	$IP = dirname( __FILE__ ) . '/../..';
-require "$IP/maintenance/commandLine.inc";
-
-if ( !isset( $args[0] ) ) {
-	echo "Usage: php svnImport.php <repo> [<start>] [--precache=<N>]\n";
-	echo "  <repo>\n";
-	echo "       The name of the repo. Use * to import from all defined repos.\n";
-	echo "  <start>\n";
-	echo "       The revision to begin the import from.  If not specified then\n";
-	echo "       it starts from the last repo imported to the wiki.  Ignored if\n";
-	echo "       * is specified for <repo>.\n";
-	echo "  --precache=<N>\n";
-	echo "       (default N=50) Pre-cache diffs for last N revisions.  Use 0 to \n";
-	echo "       disable pre-caching, or -1 to pre-cache the entire repository.\n";
-	echo "       Already-cached revisions do not count as part of this number.\n";
-	die;
 }
+require( "$IP/maintenance/Maintenance.php" );
 
-$cacheSize = 50;
-if ( isset( $options['precache'] ) ) {
-	if ( is_numeric( $options['precache'] ) && $options['precache'] >= -1 )
-		$cacheSize = intval( $options['precache'] );
-	else
-		die( "Invalid argument for --precache (must be a positive integer, or -1 for all)" );
-}
+class SvnImport extends Maintenance {
 
-if ( $args[0] == "*" ) {
-	$repoList = CodeRepository::getRepoList();
-	foreach ( $repoList as $repoInfo ) {
-		importRepo(	$repoInfo->getName() );
-	}
-} else {
-	importRepo(	$args[0], @$args[1] );
-}
-
-function importRepo( $repoName, $start = null ) {
-	global $wgCodeReviewImportBatchSize, $cacheSize;
-
-	$repo = CodeRepository::newFromName( $repoName );
-
-	if ( !$repo ) {
-		echo "Invalid repo $repoName\n";
-		die;
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Import revisions to Code Review from a Subversion repo";
+		$this->addOption( 'precache', 'Pre-cache diffs for last N revisions, -1 means entire repo', false, true );
+		$this->addArg( 'repo', 'The name of the repo. Use * to import from all defined repos' );
+		$this->addArg( 'start', "The revision to begin the import from.  If not specified then\n" .
+						"\t\tit starts from the last repo imported to the wiki.  Ignored if\n" .
+						"\t\t'all' is specified for <repo>", false );
 	}
 
-	$svn = SubversionAdaptor::newFromRepo( $repo->getPath() );
-	$lastStoredRev = $repo->getLastStoredRev();
-
-	$chunkSize = $wgCodeReviewImportBatchSize;
-
-	$startTime = microtime( true );
-	$revCount = 0;
-	$start = isset( $start ) ? intval( $start ) : $lastStoredRev + 1;
-	if ( $start > ( $lastStoredRev + 1 ) ) {
-		echo "Invalid starting point r{$start}\n";
-		die;
-	}
-
-	echo "Syncing repo $repoName from r$start to HEAD...\n";
-
-	if ( !$svn->canConnect() ) {
-		die( "Unable to connect to repository.\n" );
-	}
-
-	while ( true ) {
-		$log = $svn->getLog( '', $start, $start + $chunkSize - 1 );
-		if ( empty( $log ) ) {
-			# Repo seems to give a blank when max rev is invalid, which
-			# stops new revisions from being added. Try to avoid this
-			# by trying less at a time from the last point.
-			if ( $chunkSize <= 1 ) {
-				break; // done!
+	public function execute() {
+		$cacheSize = null;
+		if ( $this->hasOption( 'precache' ) ) {
+			$cacheSize = $this->getOption( 'precache' );
+			if ( is_numeric( $cacheSize ) && $cacheSize >= -1 ) {
+				$cacheSize = intval( $cacheSize );
+			} else {
+				$this->error( "Invalid argument for --precache (must be a positive integer, or -1 for all)", true );
 			}
-			$chunkSize = max( 1, floor( $chunkSize / 4 ) );
-			continue;
+		}
+
+		$repo = $this->getArg();
+		if ( $repo == "all" ) {
+			$repoList = CodeRepository::getRepoList();
+			foreach ( $repoList as $repoInfo ) {
+				$this->importRepo( $repoInfo->getName(), null, $cacheSize );
+			}
 		} else {
-			$start += $chunkSize;
-		}
-		if ( !is_array( $log ) ) {
-			var_dump( $log );
-			die( 'wtf' );
-		}
-		foreach ( $log as $data ) {
-			$revCount++;
-			$delta = microtime( true ) - $startTime;
-			$revSpeed = $revCount / $delta;
-
-			$codeRev = CodeRevision::newFromSvn( $repo, $data );
-			$codeRev->save();
-
-			printf( "%d %s %s (%0.1f revs/sec)\n",
-				$codeRev->mId,
-				wfTimestamp( TS_DB, $codeRev->mTimestamp ),
-				$codeRev->mAuthor,
-				$revSpeed );
-		}
-		wfWaitForSlaves( 5 );
-	}
-
-	if ( $cacheSize != 0 ) {
-		if ( $cacheSize == -1 )
-			echo "Pre-caching all uncached diffs...\n";
-		elseif ( $cacheSize == 1 )
-			echo "Pre-caching the latest diff...\n";
-		else
-			echo "Pre-caching the latest $cacheSize diffs...\n";
-
-		$dbw = wfGetDB( DB_MASTER );
-		$options = array( 'ORDER BY' => 'cr_id DESC' );
-		if ( $cacheSize > 0 )
-			$options['LIMIT'] = $cacheSize;
-
-		$res = $dbw->select( 'code_rev', 'cr_id',
-			array( 'cr_repo_id' => $repo->getId(), 'cr_diff IS NULL OR cr_diff = ""' ), 
-			__METHOD__,
-			$options
-		);
-		while ( $row = $dbw->fetchObject( $res ) ) {
-			$rev = $repo->getRevision( $row->cr_id );
-			$diff = $repo->getDiff( $row->cr_id ); // trigger caching
-			echo "Diff r{$row->cr_id} done\n";
+			$this->importRepo( $repo, $this->getArg( 1 ), $cacheSize );
 		}
 	}
-	else
-		echo "Pre-caching skipped.\n";
 
-	echo "Done!\n";
+	private function importRepo( $repoName, $start = null, $cacheSize = null ) {
+		global $wgCodeReviewImportBatchSize;
+
+		$repo = CodeRepository::newFromName( $repoName );
+
+		if ( !$repo ) {
+			$this->error( "Invalid repo $repoName" );
+			return;
+		}
+
+		$svn = SubversionAdaptor::newFromRepo( $repo->getPath() );
+		$lastStoredRev = $repo->getLastStoredRev();
+
+		$chunkSize = $wgCodeReviewImportBatchSize;
+
+		$startTime = microtime( true );
+		$revCount = 0;
+		$start = $start === null ? intval( $start ) : $lastStoredRev + 1;
+		if ( $start > ( $lastStoredRev + 1 ) ) {
+			$this->error( "Invalid starting point r{$start}" );
+			return;
+		}
+
+		$this->output( "Syncing repo $repoName from r$start to HEAD..." );
+
+		if ( !$svn->canConnect() ) {
+			$this->error( "Unable to connect to repository." );
+			return;
+		}
+
+		while ( true ) {
+			$log = $svn->getLog( '', $start, $start + $chunkSize - 1 );
+			if ( empty( $log ) ) {
+				# Repo seems to give a blank when max rev is invalid, which
+				# stops new revisions from being added. Try to avoid this
+				# by trying less at a time from the last point.
+				if ( $chunkSize <= 1 ) {
+					break; // done!
+				}
+				$chunkSize = max( 1, floor( $chunkSize / 4 ) );
+				continue;
+			} else {
+				$start += $chunkSize;
+			}
+			if ( !is_array( $log ) ) {
+				var_dump( $log );
+				$this->error( 'wtf', true );
+			}
+			foreach ( $log as $data ) {
+				$revCount++;
+				$delta = microtime( true ) - $startTime;
+				$revSpeed = $revCount / $delta;
+
+				$codeRev = CodeRevision::newFromSvn( $repo, $data );
+				$codeRev->save();
+
+				$this->output( sprintf( "%d %s %s (%0.1f revs/sec)\n",
+					$codeRev->mId,
+					wfTimestamp( TS_DB, $codeRev->mTimestamp ),
+					$codeRev->mAuthor,
+					$revSpeed ) );
+			}
+			wfWaitForSlaves( 5 );
+		}
+
+		if ( $cacheSize !== null ) {
+			if ( $cacheSize == -1 ) {
+				$this->output( "Pre-caching all uncached diffs..." );
+			} elseif ( $cacheSize == 1 ) {
+				$this->output( "Pre-caching the latest diff..." );
+			} else {
+				$this->output( "Pre-caching the latest $cacheSize diffs..." );
+			}
+
+			$dbw = wfGetDB( DB_MASTER );
+			$options = array( 'ORDER BY' => 'cr_id DESC' );
+			if ( $cacheSize > 0 )
+				$options['LIMIT'] = $cacheSize;
+
+			$res = $dbw->select( 'code_rev', 'cr_id',
+				array( 'cr_repo_id' => $repo->getId(), 'cr_diff IS NULL OR cr_diff = ""' ),
+				__METHOD__,
+				$options
+			);
+			while ( $row = $dbw->fetchObject( $res ) ) {
+				$rev = $repo->getRevision( $row->cr_id );
+				$diff = $repo->getDiff( $row->cr_id ); // trigger caching
+				$this->output( "Diff r{$row->cr_id} done" );
+			}
+		}
+		else {
+			$this->output( "Pre-caching skipped." );
+		}
+		$this->output( "Done!" );
+	}
 }
+
+$maintClass = "SvnImport";
+require_once( DO_MAINTENANCE );
