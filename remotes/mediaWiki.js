4ab8bb1a0f23e0ad697ee5eb0e25bde98b51a4cd
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-26 02:09:33 +0000

    * updated remote version for deploying updated gadget
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work/phase3/js/mwEmbed@62991 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r99';
+var mwRemoteVersion = 'r100';
 var mwUseScriptLoader = true;
 
 // Log the mwRemote version ( will determine what version of js we get )
