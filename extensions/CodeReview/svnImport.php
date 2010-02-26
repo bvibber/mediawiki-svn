bvibber/mediawiki-svn

commit f6833fedd7c4b34ee6e942771cf7efbde6458854

CodeReview: Changed the 'precache' argument to svnImport.php so that precaching all diffs is now specified by using 'all' instead of -1, as per Tim Starling's suggestion for r62509 (http://www.mediawiki.org/wiki/Special:Code/MediaWiki/62509#c5935): "It's a weakly typed language, you don't need hacks like this. Get rid of this "-1 means all" rubbish and just use the string "all"."
I also made the following changes, relating to the above:
* Updated the description of the parameter to match the above (and the error message if param is invalid).
* Removed the new-line/indentation from the 'start' parameter - this should be handled in the output function, not here (I have a patch which I'll post soon).
* $cacheSize defaults to 0, rather than null (though they have the same meaning).  The previous version didn't quite work correctly (in terms of reporting) if 0 was explicitly given on the command-line.
* I moved the setup of $dbw and $options to the top of the if block so that we can add the LIMIT parameter within the if/else structure used for status output, rather than requiring a separate check.



git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@62954 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CodeReview/svnImport.php
@@ -11,21 +11,24 @@ class SvnImport extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->mDescription = "Import revisions to Code Review from a Subversion repo";
-		$this->addOption( 'precache', 'Pre-cache diffs for last N revisions, -1 means entire repo', false, true );
+		$this->addOption( 'precache', 'Pre-cache diffs for last N revisions.  ' .
+						'May be a positive integer, 0 (for none) or \'all\'.  Default is 0', false, true );
 		$this->addArg( 'repo', 'The name of the repo. Use \'all\' to import from all defined repos' );
-		$this->addArg( 'start', "The revision to begin the import from.  If not specified then\n" .
-						"\t\tit starts from the last repo imported to the wiki.  Ignored if\n" .
-						"\t\t'all' is specified for <repo>", false );
+		$this->addArg( 'start', "The revision to begin the import from.  If not specified then " .
+						"it starts from the last repo imported to the wiki.  Ignored if " .
+						"'all' is specified for <repo>", false );
 	}
 
 	public function execute() {
-		$cacheSize = null;
+		$cacheSize = 0;
 		if ( $this->hasOption( 'precache' ) ) {
 			$cacheSize = $this->getOption( 'precache' );
-			if ( is_numeric( $cacheSize ) && $cacheSize >= -1 ) {
-				$cacheSize = intval( $cacheSize );
-			} else {
-				$this->error( "Invalid argument for --precache (must be a positive integer, or -1 for all)", true );
+			if ( $cacheSize != "all" ) {
+				if ( is_numeric( $cacheSize ) && $cacheSize >= 0 ) {
+					$cacheSize = intval( $cacheSize );
+				} else {
+					$this->error( "Invalid argument for --precache (must be a positive integer, 0 or 'all')", true );
+				}
 			}
 		}
 
@@ -40,7 +43,7 @@ class SvnImport extends Maintenance {
 		}
 	}
 
-	private function importRepo( $repoName, $start = null, $cacheSize = null ) {
+	private function importRepo( $repoName, $start = null, $cacheSize = 0 ) {
 		global $wgCodeReviewImportBatchSize;
 
 		$repo = CodeRepository::newFromName( $repoName );
@@ -105,19 +108,20 @@ class SvnImport extends Maintenance {
 			wfWaitForSlaves( 5 );
 		}
 
-		if ( $cacheSize !== null ) {
-			if ( $cacheSize == -1 ) {
-				$this->output( "Pre-caching all uncached diffs..." );
-			} elseif ( $cacheSize == 1 ) {
-				$this->output( "Pre-caching the latest diff..." );
-			} else {
-				$this->output( "Pre-caching the latest $cacheSize diffs..." );
-			}
-
+		if ( $cacheSize != 0 ) {
 			$dbw = wfGetDB( DB_MASTER );
 			$options = array( 'ORDER BY' => 'cr_id DESC' );
-			if ( $cacheSize > 0 )
+
+			if ( $cacheSize == "all" ) {
+				$this->output( "Pre-caching all uncached diffs..." );
+			} else {
+				if ( $cacheSize == 1 ) {
+					$this->output( "Pre-caching the latest diff..." );
+				} else {
+					$this->output( "Pre-caching the latest $cacheSize diffs..." );
+				}
 				$options['LIMIT'] = $cacheSize;
+			}
 
 			$res = $dbw->select( 'code_rev', 'cr_id',
 				array( 'cr_repo_id' => $repo->getId(), 'cr_diff IS NULL OR cr_diff = ""' ),

commit 09f714bd7296e9d266b475674a935ed0b403a587

CodeReview: Fixed some errors I introduced in r62954 (thanks nikerabbit).
* $cacheSize != "all" didn't work due to type conversion, so it now uses !==.  I also force the supplied argument to lower-case to make it case-insensitive (which actually negates the need for !==, but there you go).
* Changed the is_numeric() check to use a regex to match a string of digits (i.e. a positive integer or zero), as is_numeric() allows through all kinds of weird input.
* The later check for ($cacheSize != 0) was incorrect because "all" would be type-cast to 0.  It now uses !== instead.

git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@63007 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

extensions/CodeReview/svnImport.php
@@ -23,8 +23,8 @@ class SvnImport extends Maintenance {
 		$cacheSize = 0;
 		if ( $this->hasOption( 'precache' ) ) {
 			$cacheSize = $this->getOption( 'precache' );
-			if ( $cacheSize != "all" ) {
-				if ( is_numeric( $cacheSize ) && $cacheSize >= 0 ) {
+			if ( strtolower( $cacheSize ) !== "all" ) {
+				if ( preg_match( '/^\d+$/', $cacheSize ) ) {
 					$cacheSize = intval( $cacheSize );
 				} else {
 					$this->error( "Invalid argument for --precache (must be a positive integer, 0 or 'all')", true );
@@ -108,7 +108,7 @@ class SvnImport extends Maintenance {
 			wfWaitForSlaves( 5 );
 		}
 
-		if ( $cacheSize != 0 ) {
+		if ( $cacheSize !== 0 ) {
 			$dbw = wfGetDB( DB_MASTER );
 			$options = array( 'ORDER BY' => 'cr_id DESC' );
 