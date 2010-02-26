c0b9ed49d3f2a97663dc8521f4dbef2f9636f3de
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-26 18:46:35 +0000

    (bug 11125) Update missing.php from http://meta.wikimedia.org/w/index.php?title=Wikimedia_missing_site_template&oldid=1865262
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@63012 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/web-scripts/missing.php
+++ tools/web-scripts/missing.php
@@ -1,116 +1,163 @@
 <?php
 header("HTTP/1.x 404 Not Found"); // don't cache me
 header("Content-Type: text/html; charset=utf-8");
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="meta" lang="meta" dir="ltr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link rel="shortcut icon" href="http://meta.wikipedia.org/favicon.ico" />
-
-<title>Wiki does not exist</title>
-		<link rel="stylesheet" href="http://upload.wikimedia.org/skins/common/shared.css?195xx" type="text/css" media="screen" />
-		<link rel="stylesheet" href="http://upload.wikimedia.org/skins/common/commonPrint.css?195xx" type="text/css" media="print" />
-
-		<link rel="stylesheet" href="http://upload.wikimedia.org/skins/monobook/main.css?195xx" type="text/css" media="screen" />
-		<link rel="stylesheet" href="http://upload.wikimedia.org/skins/chick/main.css?195xx" type="text/css" media="handheld" />
-
-		<!--[if lt IE 5.5000]><link rel="stylesheet" href="http://upload.wikimedia.org/skins/monobook/IE50Fixes.css?195xx" type="text/css" media="screen" /><![endif]-->
-		<!--[if IE 5.5000]><link rel="stylesheet" href="http://upload.wikimedia.org/skins/monobook/IE55Fixes.css?195xx" type="text/css" media="screen" /><![endif]-->
-		<!--[if IE 6]><link rel="stylesheet" href="http://upload.wikimedia.org/skins/monobook/IE60Fixes.css?195xx" type="text/css" media="screen" /><![endif]-->
-		<!--[if IE 7]><link rel="stylesheet" href="http://upload.wikimedia.org/skins/monobook/IE70Fixes.css?195xx" type="text/css" media="screen" /><![endif]-->
-</head>
-
-<body class="ns-0">
-<div style="position: absolute;x: 0; y: 0"><img src="http://meta.wikipedia.org/upload/f/f7/Ncwikimediafound.png" alt="" /><br />
-<!--		  <img src="http://www.wikipedia.org/upload/wiki.png"><br />
-<img src="http://wiktionary.org/images/wiktionary-en.png"><br />
-<img src="http://quote.wikipedia.org/upload/1/19/Wikiquote.png"><br />
-<img src="http://wikibooks.org/upload/Wikibooks.png"><br />-->
-</div>
-<div id="globalWrapper">
-<div id="column-content">
-<div id="content">
-
-<a name="top" id="contentTop"></a>
-<h1 class="firstHeading">Wiki does not exist</h1>
-<div id="bodyContent">
-    
-<h3 id="siteSub"><I>From Meta</I></h3>
-<div id="contentSub"></div>
-   
-	    
-<!-- <esi:include src="userxy/message"/> cached per user, purged on new message-->
-
-<!-- the content area is cached with the remaining inexpensive skeleton, one version for all.
-The edit links and right-click section stuff can be attached from js -->
-<!-- start content -->
-
-<p>This wiki does not exist yet. Perhaps you are looking for one of our other projects:</p>
-
-<div style="margin-top: 1.5em; width: 100%; vertical-align: middle; text-align: center; font-weight: bold;">
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 12px;">&#160;&#160;<a href="http://www.wiktionary.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Wiktionary-logo-en.png/35px-Wiktionary-logo-en.png" width="35" height="38" alt="" /></a>&#160;<a href="http://www.wiktionary.org/">Wiktionary</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 19px;">&#160;&#160;<a href="http://www.wikinews.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Wikinews-logo.png/35px-Wikinews-logo.png" width="35" height="19" alt="" /></a>&#160;<a href="http://www.wikinews.org/">Wikinews</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 10px;">&#160;&#160;<a href="http://www.wikiquote.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikiquote-logo.svg/35px-Wikiquote-logo.svg.png" width="35" height="41" alt="" /></a>&#160;<a href="http://www.wikiquote.org/">Wikiquote</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 11px;">&#160;&#160;<a href="http://www.wikibooks.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikibooks-logo.svg/35px-Wikibooks-logo.svg.png" width="35" height="35" alt="" /></a>&#160;<a href="http://www.wikibooks.org/">Wikibooks</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 13px;">&#160;&#160;<a href="http://species.wikimedia.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Wikispecies-logo.svg/35px-Wikispecies-logo.svg.png" width="35" height="41" alt="" /></a>&#160;<a href="http://species.wikimedia.org/">Wikispecies</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 10px;">&#160;&#160;<a href="http://www.wikisource.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Wikisource-logo.svg/35px-Wikisource-logo.svg.png" width="35" height="37" alt="" /></a>&#160;<a href="http://www.wikisource.org/">Wikisource</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 22px;">&#160;&#160;<a href="http://www.wikiversity.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Wikiversity-logo.svg/35px-Wikiversity-logo.svg.png" width="35" height="28" alt="" /></a>&#160;<a href="http://www.wikiversity.org/">Wikiversity</a></div>
-
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 7px;">&#160;&#160;<a href="http://commons.wikimedia.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/35px-Commons-logo.svg.png" width="35" height="47" alt="" /></a>&#160;<a href="http://commons.wikimedia.org/">Commons</a></div>
-</div>
-
-<div style="float: left; width: 33%; min-width: 12em; height: 60px;">
-<div style="margin-top: 13px;">&#160;&#160;<a href="http://meta.wikimedia.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Wikimedia-logo.svg/35px-Wikimedia-logo.svg.png" width="35" height="35" alt="" /></a>&#160;<a href="http://meta.wikimedia.org/">Meta-Wiki</a></div>
-</div>
-
-</div><br style="clear: both;" />
-
-<div style="margin: 1.5em 0 2em 0; text-align: center;">
-<a href="http://wikimediafoundation.org/"><img src="http://upload.wikimedia.org/wikipedia/commons/8/8b/Wikimedia-button-for-homepage.png" width="88" height="31" alt="Wikimedia" /></a>
-</div>
-
-</div>
-</td>
-</tr>
-</table>
-
-
-
-<!-- end content -->
-<div class="visualClear"></div>
-</div>
-</div>
-</div>
-<!-- end of main content block -->
-<!-- start of the left (by default at least) column -->
-<!--<esi:include src="thisurl?esiview=toolbox&loggedin=0"/> static footer, same for all pages but contains a few messaged that are worth caching --> 
-<div class="visualClear"></div>
-
-<div id="colophon"></div>
-</div>
-</body>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<!-- Sysops: Please do not edit the main template directly; update /temp and synchronise. --> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"> 
+ <head> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+  <meta http-equiv="Content-Language" content="en" /> 
+  <meta http-equiv="Content-Style-Type" content="text/css" /> 
+  <meta http-equiv="Content-Script-Type" content="text/javascript" /> 
+  <!--[if lt IE 7]>
+  <meta http-equiv="imagetoolbar" content="no" />
+  <![endif]--> 
+  <title>Wiki does not exist</title> 
+  <meta name="title" content="Wiki does not exist" /> 
+  <meta name="description" content="Wikimedia Foundation site error page when a wiki does not exist." /> 
+  <meta name="author" content="The Wikimedia Foundation" /> 
+  <meta name="copyright" content="Creative Commons Attribution-Share Alike 3.0"/> 
+  <meta name="publisher" content="The Wikimedia Foundation" /> 
+  <meta name="language" content="English" /> 
+  <meta name="robots" content="index, follow" /> 
+  <meta name="MSSmartTagsPreventParsing" content="true" /> 
+  <link rel="shortcut icon" href="http://wikimediafoundation.org/favicon.ico" /> 
+  <link rel="apple-touch-icon" href="http://wikimediafoundation.org/favicon.ico" /> 
+  <link rel="copyright" href="http://creativecommons.org/licenses/by-sa/3.0/" /> 
+  <link rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/" /> 
+  <style type="text/css"> 
+body
+{
+ background-color: White;
+}
+ 
+a
+{
+ text-decoration: none;
+}
+ 
+a:hover
+{
+ text-decoration: underline; /* moot */
+}
+ 
+img
+{
+ border: 0;
+}
+ 
+table#content
+{
+ width: 75%;
+ margin-left: auto;
+ margin-right: auto;
+}
+ 
+table#content th,
+table#content td
+{
+ padding: 0.5em 1em;
+ text-align: center;
+ vertical-align: middle;
+}
+ 
+table#content th
+{
+ font-family: "Lucida Sans", Verdana, sans-serif;
+}
+ 
+table#content th h1
+{
+ font-size: 190%;
+ font-weight: bold;
+ margin: 0 0 .3em 0;
+}
+ 
+table#content th p
+{
+ font-size: 110%;
+ font-weight: normal;
+ margin: .3em 0;
+}
+  </style> 
+  <!-- commonPrint.css? --> 
+  <style type="text/css" media="print"> 
+/*<![CDATA[*/
+body
+{
+ background-color: transparent;
+}
+ 
+table#content
+{
+ width: 100%;
+}
+ 
+a
+{
+ color: Black !important;
+}
+/*]]>*/
+  </style> 
+ </head> 
+ <body id="www-wikimedia-org"> 
+  <table id="content" cellspacing="2" cellpadding="2"> 
+   <tr> 
+    <th colspan="3" style="text-align: left; vertical-align: top; width: 75%;"> 
+     <h1> 
+      Welcome to Wikimedia
+     </h1> 
+     <p> 
+      Unfortunately, this wiki does not exist yet, or it has been closed.  You may be looking for one of our other projects below.  If you would like to request that this wiki be created, see the <a href="http://meta.wikimedia.org/wiki/Requests_for_new_languages" title="Requests for new languages - Meta">requests for new languages</a> page on Meta-Wiki.
+     </p> 
+    </th> 
+    <td style="width: 25%;"> 
+     <a href="http://wikimediafoundation.org/" title="Wikimedia Foundation"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Wikimedia_logo_text_RGB.svg/135px-Wikimedia_logo_text_RGB.svg.png" alt="Wikimedia Foundation" title="Wikimedia Foundation" /></a> 
+    </td> 
+   </tr> 
+   <tr> 
+    <td> 
+     <a href="http://www.wikipedia.org/" title="Wikipedia — The Free Encyclopedia"><img src="http://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png" width="135" height="155" alt="Wikipedia" title="Wikipedia — The Free Encyclopedia" /></a> 
+    </td> 
+    <td> 
+     <a href="http://www.wiktionary.org/" title="Wiktionary"><img src="http://wiktionary.org/images/wiktionary-en.png" width="135" height="135" alt="Wiktionary" title="Wiktionary" /></a> 
+    </td> 
+    <td> 
+     <a href="http://www.wikiquote.org/" title="Wikiquote"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Wikiquote-logo-en.png/120px-Wikiquote-logo-en.png" width="120" height="120" alt="Wikiquote" title="Wikiquote" /></a> 
+    </td> 
+    <td> 
+     <a href="http://www.wikibooks.org/" title="Wikibooks"><img src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wikibooks-logo-en-noslogan.svg/140px-Wikibooks-logo-en-noslogan.svg.png" width="140" height="140" alt="Wikibooks" title="Wikibooks" /></a> 
+    </td> 
+   </tr> 
+   <tr> 
+    <td> 
+     <a href="http://www.wikisource.org/" title="Wikisource"><img src="http://upload.wikimedia.org/wikipedia/sources/b/bc/Wiki.png" alt="Wikisource" title="Wikisource" /></a> 
+    </td> 
+    <td> 
+     <a href="http://www.wikinews.org/" title="Wikinews"><img src="http://upload.wikimedia.org/wikinews/en/b/bc/Wiki.png" width="135" height="104" alt="Wikinews" title="Wikinews" /></a> 
+    </td> 
+    <td> 
+     <a href="http://www.wikiversity.org/" title="Wikiversity"><img src="http://upload.wikimedia.org/wikiversity/en/b/bc/Wiki.png" width="135" height="119" alt="Wikiversity" title="Wikiversity" /></a> 
+    </td> 
+    <td> 
+     <a href="http://species.wikimedia.org/" title="Wikispecies, a free directory of life!"><img src="http://upload.wikimedia.org/wikipedia/species/b/bc/Wiki.png" width="125" height="177" alt="Wikispecies" title="Wikispecies, a free directory of life!" /></a> 
+    </td> 
+   </tr> 
+   <tr> 
+    <td> 
+     <a href="http://mediawiki.org/" title="MediaWiki"><img src="http://upload.wikimedia.org/wikipedia/mediawiki/b/bc/Wiki.png" alt="MediaWiki" title="MediaWiki" /></a> 
+    </td> 
+    <td> 
+     <a href="http://meta.wikimedia.org/" title="Wikimedia project coordination"><img src="http://upload.wikimedia.org/wikipedia/meta/b/bc/Wiki.png" alt="Meta-Wiki" title="Wikimedia project coordination" /></a> 
+    </td> 
+    <td> 
+     <a href="http://commons.wikimedia.org/" title="Wikimedia Commons — Free media repository"><img src="http://upload.wikimedia.org/wikipedia/commons/7/79/Wiki-commons.png" width="135" height="155" alt="Wikimedia Commons" title="Wikimedia Commons — Free media repository" /></a> 
+    </td> 
+    <td> 
+     <a href="http://incubator.wikimedia.org/" title="Wikimedia Incubator"><img src="http://upload.wikimedia.org/wikipedia/incubator/b/bc/Wiki.png" alt="Wikimedia Incubator" title="Wikimedia Incubator" /></a> 
+    </td> 
+   </tr> 
+  </table> 
+ </body> 
 </html>
+
