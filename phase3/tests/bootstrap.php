82e75fdb3867a2a5cc782d930d924ce030f518a8
maxsem <maxsem@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-02-28 14:58:12 +0000

    rm old tests from new-installer
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-installer@63070 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$IP = realpath(dirname( __FILE__ ) . '/..');
-define('MEDIAWIKI', 1);
-global $optionsWithArgs;
-$optionsWithArgs = array();
-
-require_once( '../maintenance/commandLine.inc' );
