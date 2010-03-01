35bc1f9ab7caf3fdcd56427f28a202d2ea16611b
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-01 23:50:43 +0000

    Carp carp carp. Forgot this in r6315
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@63156 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -78,14 +78,14 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 19 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 96 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.preview.js', 'version' => 11 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.templateEditor.js', 'version' => 32 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.templateEditor.js', 'version' => 33 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 3 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 299 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 300 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 299 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 300 ),
 			),
 		),
 	);
