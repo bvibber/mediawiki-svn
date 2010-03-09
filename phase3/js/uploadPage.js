d1049c945962cbd4a2c62990e0c80f7392d011d3
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-09 21:25:36 +0000

    fixed fullscreen position to use scroll offset
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work@63509 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js/uploadPage.js
+++ phase3/js/uploadPage.js
@@ -24,7 +24,7 @@ var mwUploadHelper = {
 					'apiUrl': mw.getLocalApiUrl(),
 					
 					// MediaWiki API supports chunk uploads: 
-					'enable_chunks' : false, 
+					'enable_chunks' : false,
 					
 					// Set the interface type
 					'interface_type' : 'dialog',
@@ -75,6 +75,7 @@ var mwUploadHelper = {
 		.focus( function() {
 			_this.toggleUpType( this.id == 'wpUploadFile' );
 		} )
+		
 		// Also setup the onChange event binding:
 		.change( function() {
 			if ( wgUploadAutoFill ) {
