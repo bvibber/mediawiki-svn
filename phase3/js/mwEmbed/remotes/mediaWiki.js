d58de3a30c452ae6e94e79f0e4cec2ea938e6da3
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-09 18:11:48 +0000

    * updated language files * some ie fixes * updated urid version for gadget update
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work@63477 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js/mwEmbed/remotes/mediaWiki.js
+++ phase3/js/mwEmbed/remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r106';
+var mwRemoteVersion = 'r107';
 
 // Log the mwRemote version ( will determine what version of js we get )
 if( window.console ){
