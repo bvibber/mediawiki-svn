30b99a1dd9f5c145fc4577386f7fdcfb34d41198
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-16 23:00:16 +0000

    Updated OptIn information for March 17th release.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@63843 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/OptIn/OptIn.i18n.php
+++ extensions/UsabilityInitiative/OptIn/OptIn.i18n.php
@@ -96,20 +96,17 @@ We would like to know what you think about our new interface, so we would apprec
 	'optin-survey-answer-changes-custom' => 'Allow greater user customization.',
 	'optin-survey-answer-changes-other' => 'Other:',
 	'optin-improvements' => "== What has been improved? ==
-[[File:UsabilityNavigation.png|left|link=|Screenshot of the new navigation tabs]]
-;Enhanced navigation
-The new navigation system makes it easier to see where you are and what you are doing, as well as where you can go and how to get there. With the search box located in the top right corner, the search feature is easier to find and quicker to use.
-<div style=\"clear:both;\"></div>
-
 [[File:UsabilityToolbar.png|left|link=|Screenshot of the enhanced edit toolbar]]
-;Edit toolbar improvements
-New-and-improved icons make it clearer what action each tool performs. Expandable sections reduce clutter while keeping infrequently used tools just a click away.
-The help section provides quick access to a reference for common uses of wiki-markup.
-<div style=\"clear:both;\"></div>
+[[File:UsabilityDialogs.png|left|link=|Screenshot of the new content generation dialogs]]
+;Enhanced Toolbar
+The enhanced toolbar makes it easier to edit articles. New and improved icons make it clearer what action each tool performs. Expanded sections reduce clutter while keeping infrequently used tools just a click away.
+;Dialogues
+When creating links or tables, clicking the tool icon now opens a dialogue box which helps you to create links and tables via a filling out a simple form.
+;And More
+We've introduced a number of other improvements such as enhanced navigation, cascading tabs, improved search, and more.
 == How to get involved ==
-To learn more about the project or to get involved, please visit the [http://usability.wikimedia.org Usability Initiative's wiki].  There you will find information about our grant, research, design + development, and releases.  To ask questions or leave general feedback, please use our [http://usability.wikimedia.org/wiki/Talk:Prototype prototype discussion page].  To address particular subjects, please use the associated discussion pages.  If you find bugs, please leave your comments on the [http://usability.wikimedia.org/wiki/Talk:Releases/Babaco Babaco release discussion page] or by using [https://bugzilla.wikimedia.org Bugzilla].
-
-Thanks and we look forward to hearing from you!",
+To find out more about the Usability Initiative, please visit our [http://usability.wikimedia.org/wiki/Wikipedia_Usability_Initiative homepage].
+<div style=\"clear:both\"></div>",
 );
 
 /** Message documentation (Message documentation)
