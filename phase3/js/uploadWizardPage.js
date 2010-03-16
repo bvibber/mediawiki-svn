a8bb2633853de7a49b970c7e0869b5b61511b950
neilk <neilk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-16 05:01:48 +0000

    remove language config, unnecessary now with mw.Language
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work@63799 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js/uploadWizardPage.js
+++ phase3/js/uploadWizardPage.js
@@ -16,20 +16,21 @@ mw.ready( function() {
 	// change title on page too -- this doesn't have to be internationalized yet since this is just for testing
 	// when it has its own page it will have a real title
 	$j('#firstHeading').html("Upload wizard");
+
+	// configure languages
+	// mdale does this statically, we'll do this for now since it will have about the same impact as loading another library anyway
 	
 	mw.load( 'UploadWizard.UploadWizardTest', function () {
+		
+		mw.setConfig('debug', true); 
 
 		mw.setDefaultConfig('uploadHandlerClass', null);
 		mw.setConfig('userName', wgUserName); 
 		mw.setConfig('userLanguage', wgUserLanguage);
 		mw.setConfig('fileExtensions', wgFileExtensions);
 		mw.setConfig('token', token);
-		mw.setConfig('languages', [
-				{ code: 'en', name: 'English' },
-				{ code: 'fr', name: 'Fran&ccedil;ais' },
-				{ code: 'es', name: 'Espagnol' },
-			    ]);
 		mw.setConfig('thumbnailWidth', 220); // new standard size
+
 
 		var uploadWizard = new mw.UploadWizard();
 		uploadWizard.createInterface(wizardDiv);
