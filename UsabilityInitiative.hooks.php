51a99ca3b15369290b9828342ff3c75804428d2d
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-17 19:54:33 +0000

    Fixing a general IE bug and enabling saveSelection and restoreSelection for the textarea
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@63875 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.delayedBind.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 27 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 175 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 176 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 51 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 52 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 21 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 4 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 352 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 353 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 352 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 353 ),
 			),
 		),
 	);
