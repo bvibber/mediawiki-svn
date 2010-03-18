9daa247282770b7a858ed204c31f87f68e1e549f
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-18 23:13:42 +0000

    UsabilityInitiative: Fix bug in code detecting whether to set up the iframe
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@63937 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -72,7 +72,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.delayedBind.js', 'version' => 1 ),
 				array( 'src' => 'js/plugins/jquery.suggestions.js', 'version' => 7 ),
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 27 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 177 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 178 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 51 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 52 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 21 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.publish.js', 'version' => 5 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 355 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 356 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 355 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 356 ),
 			),
 		),
 	);
