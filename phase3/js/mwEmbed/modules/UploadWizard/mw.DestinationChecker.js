c3eabc03602ef6c49abd5dbf5f0f3692a9a8e070
neilk <neilk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-19 19:42:10 +0000

    More abstract, can override events it fires on. Fixed caching bug
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work@63953 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js/mwEmbed/modules/UploadWizard/mw.DestinationChecker.js
+++ phase3/js/mwEmbed/modules/UploadWizard/mw.DestinationChecker.js
@@ -13,43 +13,65 @@
  * 		apiUrl    optional url to call for api. falls back to local api url
  * 		delay     optional how long to delay after a change in ms. falls back to configured default
  *		preprocess optional: function to apply to the contents of selector before testing
+ *		events 	  what events on the input trigger a check.
  */ 
 mw.DestinationChecker = function( options ) {
 
 	var _this = this;
 	_this.selector = options.selector;		
 	_this.spinner = options.spinner;
-	_this.cachedResult = {};
 	_this.processResult = options.processResult;
 	
+	// optional overrides
+
 	if (options.apiUrl) {
 		_this.apiUrl = options.apiUrl;
 	} else {
 		_this.apiUrl = mw.getLocalApiUrl();
 	}
-	
-	if (options.preprocess) {
-		_this.preprocess = options.preprocess;
-	} else {
-		_this.preprocess = function (x) { return x };
-	}
 
-	if (options.delay) {
-		_this.delay = options.delay;
-	} else {
-		_this.delay = 500; // ms;
-	}
+	$j.each( ['preprocess', 'delay', 'events'], function( i, option ) {
+		if ( options[option] ) {
+			_this[option] = options[option];
+		}
+	} );
+
+
+	// initialize!
 
 	var check = _this.getDelayedChecker();
-	$j( _this.selector ).change( check ).keypress( check );
+	
+	$j.each( _this.events, function(i, eventName) {
+		$j( _this.selector )[eventName]( check );
+	} );
 
 }
 
 mw.DestinationChecker.prototype = {
 
+	// events that the input undergoes which fire off a check
+	events: [ 'change', 'keypress' ],
+
+	// how long the input muse be "idle" before doing call (don't want to check on each key press)
+	delay: 500, // ms;
+
+	// what tracks the wait
 	timeoutId: false,
 
-	responseCache: {},
+	// cached results from api calls
+	cachedResult: {},
+
+	/**
+	 * There is an option to preprocess the name (in order to perhaps convert it from
+	 * title to path, e.g. spaces to underscores, or to add the "File:" part.) Depends on 
+	 * exactly what your input field represents.
+	 * In the event that the invoker doesn't supply a name preprocessor, use this identity function
+	 * as default
+	 *
+	 * @param something
+	 * @return that same thing
+	 */
+	preprocess: function(x) { return x },
 
 	/**
 	 * fire when the input changes value or keypress
@@ -58,7 +80,7 @@ mw.DestinationChecker.prototype = {
 	getDelayedChecker: function() {
 		var checker = this;
 		return function() {
-			var el = this; // but we don't use it...
+			var el = this; // but we don't use it, since we already have it in _this.selector
 
 			// if we changed before the old timeout ran, clear that timeout.
 			if ( checker.timeoutId ) {
@@ -82,8 +104,8 @@ mw.DestinationChecker.prototype = {
 		var found = false;
 		var name = _this.preprocess( $j( _this.selector ).val() );
 		
-		if ( _this.responseCache[name] !== undefined ) {
-			_this.doResult( name, _this.responseCache[name] );
+		if ( _this.cachedResult[name] !== undefined ) {
+			_this.doResult( name, _this.cachedResult[name] );
 			return;
 		} 
 
