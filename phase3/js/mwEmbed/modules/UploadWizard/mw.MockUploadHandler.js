c95d76f03f60ff4b9c14670a90a5cc0b6d0302f2
neilk <neilk@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-23 05:54:05 +0000

    refactor to use jquery events -- not only much simpler, this makes step 2 -> step 3 much easier
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/js2-work@64060 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/js/mwEmbed/modules/UploadWizard/mw.MockUploadHandler.js
+++ phase3/js/mwEmbed/modules/UploadWizard/mw.MockUploadHandler.js
@@ -4,74 +4,34 @@
 // will have to figure this out.
 
 // this should be loaded with test suites when appropriate. separate file.
-mw.MockUploadHandler = function(ui) {
-	this.ui = ui;
+mw.MockUploadHandler = function(upload) {
+	this.upload = upload;
 	this.nextState = null;
 	this.progress = 0.0;
-	this.isCompleted = false;
 
-	_this.completedCallbacks = [];
-	_this.progressCallbacks = [];
-	_this.warningCallbacks = [];
-
-	// if we ever get a pause control, 
-	// may need to turn this into an array of pairs, start & stops
-	this.beginTime = null; 
 };
 
 mw.MockUploadHandler.prototype = {
-	addProgressCb: function(fn) {
-		var _this = this;
-		_this.progressCallbacks.push(function(progress) { fn(_this, progress) }); 				
-	},
-
-
-	addCompletedCb: function(fn) {
-		var _this = this;
-		_this.completedCallbacks.push(function() { fn(_this) });		
-	},
-
-	addWarningCb: function(fn) {
-		var _this = this;
-		_this.warningCallbacks.push(function() { fn(_this, warning) });				
-	},
-
-
+	
 	start: function () {
 		var _this = this;
 		_this.beginTime = (new Date()).getTime();
 		_this.nextState = _this.cont;
-		_this.ui.start();
 		_this.nextState();
-	},   
+	},  
+ 
 	cont: function () {
 		var _this = this;
 		var delta = 0.0001; // static?
-		_this.progress += 0.1;
-		_this.ui.progress(_this.progress);
-		_this.progressCb(_this, _this.progress);
+		_this.progress += ( Math.random() * 0.1 );
+		_this.upload.setTransportProgress(_this.progress);
 		if (1.0 - _this.progress < delta) {
-			_this.completed();
+			_this.upload.setTransported();
 		} else {
-			setTimeout( function() { _this.nextState() }, 1000 );
+			setTimeout( function() { _this.nextState() }, 200 );
 		}
 	},
-
-	completed: function() {
-		var _this = this;
-		_this.isCompleted = true;
-		_this.ui.completed();
-		_this.completedCb(_this);
-	},
-	stop: function () {
-		// tell the interface that we're stopped
-	},
-
-	//pause: function () { // },   
-	//resume: function () { // this.nextState = this.stop(); },   
-	remove: function () {
-		// ??
-	}  
+ 
 };
 
 
