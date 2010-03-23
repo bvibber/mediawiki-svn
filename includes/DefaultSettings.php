2e3b6dd0a25f9df43dfca253e98249c376b62ad2
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-23 02:26:03 +0000

    Numbering the WMF deployment branches and reorganising them: * old -> 1.16wmf1 * 2009-10-01 -> 1.16wmf2 * wmf-deployment -> 1.16wmf3
    The current wmf-deployment can stay where it is for now to avoid breaking things.
    
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf-deployment@64057 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ if ( !defined( 'MW_PHP4' ) ) {
 }
 
 /** MediaWiki version number */
-$wgVersion = '1.16alpha-wmf';
+$wgVersion = '1.16wmf3';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
