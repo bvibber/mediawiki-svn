64cf252387284670af9275dec97927031ac71b1a
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-25 01:28:35 +0000

    wfArrayMap() was moved to GlobalFunctions
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-installer@64143 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/config/Installer.php
+++ phase3/config/Installer.php
@@ -2270,16 +2270,6 @@ function getShellLocale( $wikiLang ) {
 	return false;
 }
 
-function wfArrayMap( $function, $input ) {
-	$ret = array_map( $function, $input );
-	foreach ( $ret as $key => $value ) {
-		$taint = istainted( $input[$key] );
-		if ( $taint ) {
-			taint( $ret[$key], $taint );
-		}
-	}
-	return $ret;
-}
 
 ?>
 
