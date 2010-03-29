028180809b78163292b09b3b3cb3afa22a60d33d
demon <demon@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-03-29 16:36:25 +0000

    Missing global declaration
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/new-installer@64349 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/installer/WebInstallerOutput.php
+++ phase3/includes/installer/WebInstallerOutput.php
@@ -168,6 +168,7 @@ class WebInstallerOutput {
 	}
 
 	function outputShortHeader() {
+		global $wgVersion;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
