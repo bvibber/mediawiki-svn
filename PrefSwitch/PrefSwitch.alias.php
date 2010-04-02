1f0cdf00ba20da4a0df8fb2a6188b4a9ae15fdb4
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-02 09:20:09 +0000

    Initial checking of PrefSwitch extension - a simplified and refactored version of OptIn designed specifically for changing user settings and capturing feedback.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@64518 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ PrefSwitch/PrefSwitch.alias.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$aliases = array();
+
+$aliases['en'] = array(
+	'PrefSwitch' => array( 'UsabilityInitiativePrefSwitch' ),
+);
---
+++ PrefSwitch/PrefSwitch.i18n.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Internationalisation for Usability Initiative PrefSwitch extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Roan Kattouw
+ */
+$messages['en'] = array(
+	'prefswitch' => 'Usability Initiative Preference Switch',
+	'prefswitch-desc' => 'Allow users to swtich sets of preferences',
+	'prefswitch-survey-yes' => 'Yes',
+	'prefswitch-survey-no' => 'No',
+	'prefswitch-survey-question-likedislike' => 'What did you like about the new features? What did you dislike about the features?',
+	'prefswitch-survey-question-whyoff' => 'Why are you turning off the new features? (Please select all that apply.)',
+	'prefswitch-survey-answer-whyoff-hard' => 'It was too hard to use.',
+	'prefswitch-survey-answer-whyoff-didntwork' => 'It did not function properly.',
+	'prefswitch-survey-answer-whyoff-notpredictable' => 'It did not perform predictably.',
+	'prefswitch-survey-answer-whyoff-didntlike-look' => 'I did not like the way it looked.',
+	'prefswitch-survey-answer-whyoff-didntlike-layout' => 'I did not like the new tabs and layout.',
+	'prefswitch-survey-answer-whyoff-didntlike-toolbar' => 'I did not like the new toolbar.',
+	'prefswitch-survey-answer-whyoff-other' => 'Other reason:',
+	'prefswitch-survey-question-browser' => 'Which browser do you use?',
+	'prefswitch-survey-answer-browser-ie5' => 'Internet Explorer 5',
+	'prefswitch-survey-answer-browser-ie6' => 'Internet Explorer 6',
+	'prefswitch-survey-answer-browser-ie7' => 'Internet Explorer 7',
+	'prefswitch-survey-answer-browser-ie8' => 'Internet Explorer 8',
+	'prefswitch-survey-answer-browser-ff1' => 'Firefox 1',
+	'prefswitch-survey-answer-browser-ff2' => 'Firefox 2',
+	'prefswitch-survey-answer-browser-ff3' => 'Firefox 3',
+	'prefswitch-survey-answer-browser-cb' => 'Google Chrome Beta',
+	'prefswitch-survey-answer-browser-c1' => 'Google Chrome 1',
+	'prefswitch-survey-answer-browser-c2' => 'Google Chrome 2',
+	'prefswitch-survey-answer-browser-c3' => 'Google Chrome 3',
+	'prefswitch-survey-answer-browser-c4' => 'Google Chrome 4',
+	'prefswitch-survey-answer-browser-s3' => 'Safari 3',
+	'prefswitch-survey-answer-browser-s4' => 'Safari 4',
+	'prefswitch-survey-answer-browser-o9' => 'Opera 9',
+	'prefswitch-survey-answer-browser-o9.5' => 'Opera 9.5',
+	'prefswitch-survey-answer-browser-o10' => 'Opera 10',
+	'prefswitch-survey-answer-browser-other' => 'Other browser:',
+	'prefswitch-survey-question-os' => 'Which operating system do you use?',
+	'prefswitch-survey-answer-os-windows' => 'Windows',
+	'prefswitch-survey-answer-os-windowsmobile' => 'Windows Mobile',
+	'prefswitch-survey-answer-os-macos' => 'Mac OS',
+	'prefswitch-survey-answer-os-iphoneos' => 'iPhone OS',
+	'prefswitch-survey-answer-os-linux' => 'Linux',
+	'prefswitch-survey-answer-os-other' => 'Other operating system:',
+	'prefswitch-survey-question-res' => 'What is the resolution of your screen?',
+);
