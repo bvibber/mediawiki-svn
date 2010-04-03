6065159eac335071849a96c0cb0ff749939f916a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-03 16:50:18 +0000

    * update messages for PrefSwitch * add PrefSwitch and PrefSwitchLink to Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@64565 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrefSwitch/PrefSwitch.i18n.php
+++ PrefSwitch/PrefSwitch.i18n.php
@@ -12,16 +12,18 @@ $messages = array();
  * @author Roan Kattouw
  */
 $messages['en'] = array(
-	'prefswitch' => 'Usability Initiative Preference Switch',
+	'prefswitch' => 'Usability Initiative preference switch',
 	'prefswitch-desc' => 'Allow users to switch sets of preferences',
 	'prefswitch-survey-true' => 'Yes',
 	'prefswitch-survey-false' => 'No',
 	'prefswitch-survey-submit-off' => 'Turn new features off',
 	'prefswitch-survey-cancel-off' => 'If you would like to continue using the new features, you can return to $1.',
 	'prefswitch-survey-submit-feedback' => 'Send feedback',
-	'prefswitch-survey-cancel-feedback' => 'If you don\'t want to provide feedback, you can return to $1.',
-	'prefswitch-survey-question-likedislike' => 'What did you like about the new features? What did you dislike about the features?',
-	'prefswitch-survey-question-whyoff' => 'Why are you turning off the new features? (Please select all that apply.)',
+	'prefswitch-survey-cancel-feedback' => 'If you do not want to provide feedback, you can return to $1.',
+	'prefswitch-survey-question-likedislike' => 'What did you like about the new features?
+What did you dislike about the features?',
+	'prefswitch-survey-question-whyoff' => 'Why are you turning off the new features?
+Please select all that apply.',
 	'prefswitch-survey-answer-whyoff-hard' => 'It was too hard to use.',
 	'prefswitch-survey-answer-whyoff-didntwork' => 'It did not function properly.',
 	'prefswitch-survey-answer-whyoff-notpredictable' => 'It did not perform predictably.',
@@ -66,25 +68,24 @@ $messages['en'] = array(
 	'prefswitch-success-feedback' => 'Your feedback has been sent.',
 	'prefswitch-return' => '<hr style="clear:both">
 Return to <span class="plainlinks">[$1 $2]</span>.',
-	'prefswitch-main' => "We've been working hard to make things easier for our users. We're excited to share some improvements, including a new look and feel and simplified editing features.  Improving the usability of our projects is a priority of the Wikimedia Foundation and we'll be sharing more updates in the future.  
+	'prefswitch-main' => "We have been working hard to make things easier for our users. We're excited to share some improvements, including a new look and feel and simplified editing features.  Improving the usability of our projects is a priority of the Wikimedia Foundation and we will be sharing more updates in the future.
 
 [[File:UsabilityNavigation.png|right|link=|Screenshot of the new navigation]]
 [[File:UsabilityToolbar.png|right|link=|Screenshot of the enhanced edit toolbar]]
 [[File:UsabilityDialogs.png|right|link=|Screenshot of the new content generation dialogs]]
-===Here's what we've changed:===
-* Navigation: We've improved the navigation for reading and editing articles.  Now, the tabs at the top of each page more clearly define whether you're viewing the article page or discussion page, and whether you're reading or editing an article. 
-* Editing toolbar improvements:  We've reorganized the editing toolbar to make easier to use.  Now, formatting articles is simpler and more intuitive.
-* Link wizard:  An easy-to-use tool allows you to add links to other Wikipedia articles as well as links to external sites.
-* Search improvements: We've improved search suggestions to get you to the article you're looking for more quickly. 
-* Other new features:  We've also introduced a table wizard to make creating tables easier and find and a replace feature to simplify article editing.",
-	'prefswitch-main-on' => "
-===Feedback?===
+===Here's what we have changed===
+* Navigation: We have improved the navigation for reading and editing pages. Now, the tabs at the top of each page more clearly define whether you are viewing the page or discussion page, and whether you are reading or editing a page.
+* Editing toolbar improvements:  We have reorganized the editing toolbar to make easier to use.  Now, formatting pages is simpler and more intuitive.
+* Link wizard:  An easy-to-use tool allows you to add links to other Wikipedia pages as well as links to external sites.
+* Search improvements: We have improved search suggestions to get you to the page you are looking for more quickly.
+* Other new features:  We have also introduced a table wizard to make creating tables easier and find and a replace feature to simplify page editing.",
+	'prefswitch-main-on' => "===Feedback?===
 We would love to hear from you.  Please visit our <span class=\"plainlinks\">[$1 feedback page]</span>.
 ===Take me back!===
 If you would like to turn off the new features, please <span class=\"plainlinks\">[$2 click here]</span>.",
-	'prefswitch-main-off' => "
-===Try them out!===
+	'prefswitch-main-off' => "===Try them out!===
 If you would like to turn on the new features, please <span class=\"plainlinks\">[$1 click here]</span>.",
-	'prefswitch-survey-intro-feedback' => "We'd love to hear from you. Please fill out the optional survey below.",
+	'prefswitch-survey-intro-feedback' => "We would love to hear from you.
+Please fill out the optional survey below.",
 	'prefswitch-survey-intro-off' => 'Thanks for trying out our new features. To help us improve them, please fill out the optional survey below ',
 );
