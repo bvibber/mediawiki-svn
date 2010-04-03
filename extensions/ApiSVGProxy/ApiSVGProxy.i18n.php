21a7374fd80482133e85dd069a7a804f9fbd3fb3
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-03 23:09:39 +0000

    * stylize.php, trailing whitespace removed * i18n for description message * add support for ApiSVGProxy to Translate
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@64584 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ApiSVGProxy/ApiSVGProxy.i18n.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for extension ApiSVGProxy.
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Roan Kattouw
+ */
+$messages['en'] = array(
+	'apisvgproxy-desc'      => 'Proxies SVG files from a (possibly remote) file repository to the local domain',
+);
