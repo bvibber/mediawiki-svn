38b1054c20998420ad5d85bcfffc1e88444fa778
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-05 22:31:17 +0000

    stylize.php, trailing whitespace removed, superfluous newlines removed
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@64639 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/BlockTitles/BlockTitles.php
+++ extensions/BlockTitles/BlockTitles.php
@@ -1,9 +1,10 @@
 <?php
 if ( ! defined( 'MEDIAWIKI' ) )
 	die();
-    
+
 /**#@+
- * Provides a basic way of preventing articles with certain titles from being saved or created
+ * Provides a basic way of preventing articles with certain titles from being
+ * saved or created
  * @addtogroup Extensions
  *
  * @link http://www.mediawiki.org/wiki/Extension:BlockTitles Documentation
@@ -22,11 +23,11 @@ $wgExtensionCredits['other'][] = array(
 	'url' => 'http://www.mediawiki.org/wiki/Extension:BlockTitles',
 );
 
-$dir = dirname(__FILE__) . '/';
+$dir = dirname( __FILE__ ) . '/';
 $wgExtensionMessagesFiles['BlockTitles'] = $dir . 'BlockTitles.i18n.php';
-// CONFIGURE - place any regular expressions you want here.  
+// CONFIGURE - place any regular expressions you want here.
 $wgBlockTitlePatterns = array (
-		#"/^http/i",  // if you want to block titles of articles that are URLs
+		# "/^http/i",  // if you want to block titles of articles that are URLs
 	);
 
 $wgHooks['ArticleSave'][] = 'wfCheckBlockTitles';
@@ -35,17 +36,17 @@ function wfBlockTitles() {
 	wfLoadExtensionMessages( 'BlockTitles' );
 }
 
-function wfCheckBlockTitles (&$article ) {
+function wfCheckBlockTitles ( &$article ) {
 	global $wgBlockTitlePatterns;
 	global $wgOut;
 	$title = $article->getTitle();
 	$t = $title->getFullText();
-	foreach ($wgBlockTitlePatterns as $re) {
-		if (preg_match($re, $t)) {
+	foreach ( $wgBlockTitlePatterns as $re ) {
+		if ( preg_match( $re, $t ) ) {
 			// too bad you can't pass parameter to errorpage
-			$wgOut->errorpage('block_title_error_page_title', 'block_title_error' );
+			$wgOut->errorpage( 'block_title_error_page_title', 'block_title_error' );
 			return false;
-		}	
+		}
 	}
 
 	return true;
