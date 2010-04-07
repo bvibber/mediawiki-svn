bf9284e1f33b219f12ec303edf57714fb15a1fd1
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-07 23:31:26 +0000

    UpdateMyPrefs extension... allow users to indicate whether or not they want us to update their preferences as we roll out with new features or not
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@64725 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/UsabilityInitiative/UpdateMyPrefs/UpdateMyPrefs.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Internationalisation for "UpdateMyPrefs" extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Nimish Gautam
+ */
+$messages['en'] = array(
+	'prefs-updates' => 'Updates',
+	'updatemyprefs-desc' => 'Preference for users to to specify whether they want periodic user interface updates as they are made available',
+	'updatemyprefs-label-message' => 'Automatically update my preferences with new default settings and tools as they become available',
+);
+
+
+$messages['qqq'] = array(
+	'prefs-updates' => 'In the context of software updates',
+);
