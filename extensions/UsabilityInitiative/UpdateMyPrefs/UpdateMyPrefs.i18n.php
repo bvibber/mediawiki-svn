c2915517cbee272af9af01ff558be0fb400f8390
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-08 07:49:03 +0000

    Follow-up r64725: Fix typo
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@64733 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UpdateMyPrefs/UpdateMyPrefs.i18n.php
+++ extensions/UsabilityInitiative/UpdateMyPrefs/UpdateMyPrefs.i18n.php
@@ -13,10 +13,9 @@ $messages = array();
  */
 $messages['en'] = array(
 	'prefs-updates' => 'Updates',
-	'updatemyprefs-desc' => 'Preference for users to to specify whether they want periodic user interface updates as they are made available',
+	'updatemyprefs-desc' => 'Preference for users to specify whether they want periodic user interface updates as they are made available',
 	'updatemyprefs-label-message' => 'Automatically update my preferences with new default settings and tools as they become available',
 );
-
 
 $messages['qqq'] = array(
 	'prefs-updates' => 'In the context of software updates',
