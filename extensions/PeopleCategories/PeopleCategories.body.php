7ac60e899a5804edd890d40f7f5d97e651f639f0
roberthl <roberthl@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-13 20:44:34 +0000

    Add PeopleCategories extension.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65003 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/PeopleCategories/PeopleCategories.body.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Main classes for the PeopleCategories extension.
+ *
+ * @addtogroup Extensions
+ */
+
+class PeopleCategoriesPage extends CategoryPage {
+	function view() {
+		// From CategoryPage::view()
+		if( NS_CATEGORY == $this->mTitle->getNamespace() ) {
+		$this->openShowCategory();
+		}
+
+		Article::view();
+
+		// Customization
+		if( NS_CATEGORY == $this->mTitle->getNamespace() ) {
+			$this->closeShowCategory();
+		}
+	}
+
+	function closeShowCategory() {
+		// From CategoryPage::view();
+		global $wgOut, $wgRequest;
+		$from = $wgRequest->getVal( 'from' );
+		$until = $wgRequest->getVal( 'until' );
+
+		// Customization.
+		$viewer = new PeopleCategoriesViewer( $this->mTitle, $from, $until );
+
+		// From CategoryPage::view();
+		$wgOut->addHTML( $viewer->getHTML() );
+	}
+}
+
+class PeopleCategoriesViewer extends CategoryViewer {
+	function addPage( $title, $sortkey, $pageLength, $isRedirect = false ) {
+		// From CategoryViewer::addPage
+		global $wgContLang;
+		$this->articles_start_char[] = $wgContLang->convert( $wgContLang->firstChar( $sortkey ) );
+
+		// Customization
+		global $wgPeopleCategories;
+		$name = $title->getText();
+		if( in_array( $this->title->getText(), $wgPeopleCategories ) && strpos( $name, ' ' ) ) {
+			$first = substr( $name, 0, strrpos( $name, ' ' ) );
+			$last = substr( $name, strrpos( $name, ' ' ) + 1 );
+			$name = $last . ', ' . $first;
+		}
+		if( $title->getNamespace() !== NS_MAIN ) {
+			$name = $title->getNsText() . ':' . $name;
+		}
+
+		$linker = new Linker;
+		if( $isRedirect ) {
+			$this->articles[] =
+				'<span class="redirect-in-category">' .
+				$linker->link( $title, $name ) .
+				'</span>';
+		} else {
+			$this->articles[] = $linker->link( $title, $name );
+		}
+	}
+}
---
+++ extensions/PeopleCategories/PeopleCategories.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation file for PeopleCategories extension.
+ *
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Robert Leverington <robert@rhl.me.uk>
+ */
+$messages['en'] = array(
+	'peoplecategories-desc'  => 'Changes displayed page names from "Firstname Lastname" to "Lastname, Firstname" on specified category pages.'
+);
+
+/** Message documentation (Message documentation)
+ * @author Robert Leverington <robert@rhl.me.uk>
+ */
+$messages['qqq'] = array(
+	'peoplecategories-desc' => 'Description on [[Special:Version]] of the PeopleCategories extension.'
+);
