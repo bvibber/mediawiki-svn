71ee35db27b893865153c6fe6a6cef74f997d0b1
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-14 13:06:01 +0000

    1.16wmf4: MFT r65020
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@65023 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.16wmf4/extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
+++ 1.16wmf4/extensions/UsabilityInitiative/WikiEditor/WikiEditor.hooks.php
@@ -16,14 +16,14 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/Preview/Preview.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Publish/Publish.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 7 ),
-			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 81 ),
+			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 82 ),
 			array( 'src' => 'Modules/TemplateEditor/TemplateEditor.js', 'version' => 6 ),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 85 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 86 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 85 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 86 ),
 		),
 	);
 	static $messages = array(
