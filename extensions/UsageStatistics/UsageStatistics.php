f94827f139e09072ccebd3125621c43cd4826a4b
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-18 16:12:37 +0000

    Rename to name files as parent folder. Update Configure and Translate accordingly.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65238 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsageStatistics/UsageStatistics.php
+++ extensions/UsageStatistics/UsageStatistics.php
@@ -14,7 +14,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 $wgExtensionCredits['specialpage'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'UserStats',
-	'version'        => 'v1.11.5',
+	'version'        => 'v1.12.0',
 	'author'         => 'Paul Grinberg',
 	'email'          => 'gri6507 at yahoo dot com',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:Usage_Statistics',
@@ -35,8 +35,8 @@ $wgGroupPermissions['manager'][$wgUserStatsGlobalRight] = true;
 $wgGroupPermissions['sysop'][$wgUserStatsGlobalRight] = true;
 
 $dir = dirname( __FILE__ ) . '/';
-$wgExtensionMessagesFiles['UserStats'] = $dir . '/SpecialUserStats.i18n.php';
-$wgExtensionAliasesFiles['UserStats'] = $dir . 'SpecialUserStats.alias.php';
-$wgAutoloadClasses['SpecialUserStats'] = $dir . '/SpecialUserStats_body.php';
+$wgExtensionMessagesFiles['UserStats'] = $dir . '/UsageStatistics.i18n.php';
+$wgExtensionAliasesFiles['UserStats'] = $dir . 'UsageStatistics.alias.php';
+$wgAutoloadClasses['SpecialUserStats'] = $dir . '/UsageStatistics_body.php';
 $wgSpecialPages['SpecialUserStats'] = 'SpecialUserStats';
 $wgSpecialPageGroups['SpecialUserStats'] = 'wiki';
