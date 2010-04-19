8dfc0426102298003aae263677a38f95a9e71928
bhagya <bhagya@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-19 11:41:59 +0000

    Rename the file name and adding detailed scenarios
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65270 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ testing/selenium/UsabilityInitiative/WikiAutomationTC/testCases/WikiToolBarNTOC.php
@@ -0,0 +1,245 @@
+<?php
+require_once 'WikiCommonTC.php';
+/**
+ * This test case will be handling the NTOC related functions.
+ * Adding different header levels via tool bar and verify the output
+ * Date : Apr - 2010
+ * @author : BhagyaG - Calcey
+ */
+class WikiToolBarNTOC extends WikiCommonTC {
+    
+    // Set up the testing environment
+    function setup(){
+        parent::setUp();
+    }
+
+    
+    // Add header level 2 and verify Header output
+     function testHeaderLevel2(){
+
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Heading");
+        $this->click("link=Level 2");
+        $this->type("wpTextbox1", "==Heading text==");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h2"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+
+    }
+
+    // Add header level 3 and verify Header output
+    function testHeaderLevel3(){
+
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Heading");
+        $this->click("link=Level 3");
+        $this->type("wpTextbox1", "===Heading text===");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h3"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+
+    }
+
+    // Add header level 4 and verify Header output
+    function testHeaderLevel4(){
+
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Heading");
+        $this->click("link=Level 4");
+        $this->type("wpTextbox1", "====Heading text====");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h4"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+
+    }
+
+    // Add header level 5 and verify Header output
+    function testHeaderLevel5(){
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Heading");
+        $this->click("link=Level 5");
+        $this->type("wpTextbox1", "=====Heading text=====");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h5"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+
+    }
+    
+     // Add header level 2 & 3 and verify Header output
+    function testHeaderLevel2and3(){
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Level 2");
+        $this->click("link=Heading");
+        $this->click("link=Level 3");
+        $this->type("wpTextbox1", "==Heading text==\n===Heading text===");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h2"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+         try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h3"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+    }
+
+ // Add header level 2 , 3 & 4 and verify Header output
+    function testHeaderLevel23and4(){
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Level 2");
+        $this->click("link=Heading");
+        $this->click("link=Level 3");
+        $this->click("link=Heading");
+        $this->click("link=Level 4");
+        $this->type("wpTextbox1", "==Heading text==\n===Heading text===\n====Heading text====");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h2"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+         try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h3"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h4"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+    }
+
+    // Add header level 2 , 3 , 4 & 5 and verify Header output
+    function testHeaderLevel234and5(){
+        parent::doOpenLink();
+        parent::doLogin();
+        $this->open("/deployment-en/Main_Page");
+        $this->waitForPageToLoad("30000");
+        $this->click("link=Random article");
+        $this->waitForPageToLoad("30000");
+        $this->click("//li[@id='ca-edit']/a/span");
+        $this->waitForPageToLoad("30000");
+        parent::doExpandAdvanceSection();
+        $this->click("link=Heading");
+        $this->click("link=Level 2");
+        $this->click("link=Heading");
+        $this->click("link=Level 3");
+        $this->click("link=Heading");
+        $this->click("link=Level 4");
+        $this->click("link=Heading");
+        $this->click("link=Level 5");
+        $this->type("wpTextbox1", "==Heading text==\n===Heading text===\n====Heading text====\n=====Heading text=====");
+        $this->click("wpPreview");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h2"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+         try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h3"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h4"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+        try {
+            $this->assertEquals("Heading text", $this->getText("//*[@id='wikiPreview']/h5"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+
+        parent::doLogout();
+    }
+}
+?>
