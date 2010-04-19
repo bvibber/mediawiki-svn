bd66e081c2ea4cd41c22bc3dcb3e531fbc70c27c
bhagya <bhagya@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-19 11:45:39 +0000

    Add test cases to the test suite
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65271 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- testing/selenium/UsabilityInitiative/WikiAutomationTC/testCases/WikiTestSuite.php
+++ testing/selenium/UsabilityInitiative/WikiAutomationTC/testCases/WikiTestSuite.php
@@ -2,15 +2,25 @@
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-
 require_once 'WikiAcaiSearchTC.php';
+require_once 'WikiWatchUnWatchTC.php';
+require_once 'WikiToolBarDialogsTC.php';
+require_once 'WikiToolBarNTOC.php';
+require_once 'WikiToolBarOtherTC.php';
+require_once 'WikiToolBarTextFormatTC.php';
 
 $suite = new PHPUnit_Framework_TestSuite('Wiki Tests');
 
 require_once('WikiListener.php');
 $wLis = new WikiListener();
 $result = new PHPUnit_Framework_TestResult();
-$suite->addTestSuite("WikiAcaiSearchTC"); //Add your test case to the test suite
+//Add test case to the test suite
+$suite->addTestSuite("WikiAcaiSearchTC"); 
+$suite->addTestSuite("WikiWatchUnWatchTC");
+$suite->addTestSuite("WikiToolBarDialogsTC");
+$suite->addTestSuite("WikiToolBarNTOC");
+$suite->addTestSuite("WikiToolBarOtherTC");
+$suite->addTestSuite("WikiToolBarTextFormatTC");
 $result->addListener($wLis); //Define your listener which the test result will use to give output
 $suite->run($result); //And of course run this tests
 
