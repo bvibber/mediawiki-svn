338d007a20bbc396b3a0255c9100f9171fdb9305
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-20 10:20:06 +0000

    Active task force list as requested by Eugene, see http://strategy.wikimedia.org/wiki/Proposal:Activity_bot_for_strategy_proposals,_task_forces
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65294 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
@@ -0,0 +1,7 @@
+<?php
+
+$messages = array();
+$messages['en'] = array(
+	'activetaskforces' => 'Active task forces',
+	'active-task-forces-intro' => 'Number of edits to task force pages in the last $1 days.',
+);
