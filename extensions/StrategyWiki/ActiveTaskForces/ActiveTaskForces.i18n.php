1a6a122e436a78c08710758c5b24751784710df2
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-20 13:13:07 +0000

    * Add PLURAL to message per Siebrand's CR * Add extension to Translatewiki * eol-style for README
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65310 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
+++ extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
@@ -3,5 +3,6 @@
 $messages = array();
 $messages['en'] = array(
 	'activetaskforces' => 'Active task forces',
-	'active-task-forces-intro' => 'Number of edits to task force pages in the last $1 days.',
+	'active-task-forces-intro' => 'Number of edits to task force pages in the last {{PLURAL:$1|$1 day|$1 days}}.',
 );
+
