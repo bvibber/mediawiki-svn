a7ec07aeab824121c143c0cdf1db4e4e1a9fa278
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-25 22:55:43 +0000

    MFT r65529
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@65530 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.16wmf4/includes/HttpFunctions.php
+++ 1.16wmf4/includes/HttpFunctions.php
@@ -723,7 +723,7 @@ class CurlHttpRequest extends HttpRequest {
 		}
 		$this->curlOptions[CURLOPT_USERAGENT] = $this->reqHeaders['User-Agent'];
 
-		if ( $this->sslVerifyHost ) {
+		if ( isset( $this->sslVerifyHost ) ) {
 			$this->curlOptions[CURLOPT_SSL_VERIFYHOST] = $this->sslVerifyHost;
 		}
 
