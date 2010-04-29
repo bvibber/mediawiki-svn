3a15d1f67403813e2c9940c5528cdcfa3638c5ea
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-29 17:49:48 +0000

    * follow up to r65660 added check for wgWikiEditorEnabledModules.addMediaWizard In usability Javascript system module loaders are included regardless of whether they are enabled or not .. ( they could really use a script-loader :P )
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@65661 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
+++ WikiEditor/Modules/AddMediaWizard/AddMediaWizard.js
@@ -1,10 +1,11 @@
 
 /* JavaScript for AddMediaWizard gadget */
+if ( wgWikiEditorEnabledModules.addMediaWizard ) { 
+	if( typeof mwAddMediaConfig == 'undefined'){
+	    mwAddMediaConfig = {};
+	}
+	mwAddMediaConfig['enabled_providers'] = [ 'wiki_commons', 'upload' ];
 
-if( typeof mwAddMediaConfig == 'undefined'){
-    mwAddMediaConfig = {};
+	// Transclude mwEmbed support
+	importScriptURI('http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );	
 }
-mwAddMediaConfig['enabled_providers'] = [ 'wiki_commons', 'upload' ];
-
-// Transclude mwEmbed.js
-importScriptURI('http://prototype.wikimedia.org/s-2/js/mwEmbed/remotes/mediaWiki.js?&uselang=' +  wgUserLanguage );
