6fcf1cc995323a8f19f39af7342e14930b0c2348
bhagya <bhagya@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-04-30 11:53:15 +0000

    Config file of the application
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@65686 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ testing/selenium/UsabilityInitiative/WikiAutomationTC/testCases/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This will contain all the common values for the test suite.
+ * Date : Apr - 2010
+ * @author : BhagyaG - Calcey
+ */
+$_SESSION["WIKI_WEB_URL"]="http://prototype.wikimedia.org/"; // The web site URL
+$_SESSION["WIKI_OPEN_PAGE"]= "/en.wikipedia.org/Main_Page"; // Main page URL
+$_SESSION["WIKI_TEST_WAIT_TIME"]= "30000"; // Waiting time
+$_SESSION["WIKI_USER_NAME"]= "bhagya_ca"; // User name
+$_SESSION["WIKI_USER_PASSWORD"]= "test"; // Password
+$_SESSION["WIKI_USER_DISPLAY_NAME"]= "Bhagya ca"; // Display name of the user
+$_SESSION["WIKI_SEARCH_PAGE"]= "Hair (musical)"; // Page name to search
+$_SESSION["WIKI_SEARCH_TEXT"]= "TV"; // Text to search
+$_SESSION["WIKI_INTERNAL_LINK"]= "Daimler-Chrysler"; // Exisiting page name to add as an internal tag
+$_SESSION["WIKI_EXTERNAL_LINK"]= "www.google.com"; // External web site name
+$_SESSION["WIKI_EXTERNAL_LINK_TITLE"] = "Google"; // Page title of the external web site name
+$_SESSION["WIKI_SAMPLE_TEXT"] = "calcey qa\n\ncalcey qa\n\ncalcey qa"; // String to type in the editor to use for search and replace
+$_SESSION["WIKI_SEARCH_TEXT"] = "calcey qa"; // Text use for search
+$_SESSION["WIKI_REPLACE_TEXT"] = "test team" ; // Text use for replace search text
+?>
