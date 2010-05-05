889fdc0d5907a8735f1a62418bef73a9abd76965
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-05 21:55:54 +0000

    1.16wmf4: MFT r65965
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@65966 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- 1.16wmf4/includes/DefaultSettings.php
+++ 1.16wmf4/includes/DefaultSettings.php
@@ -1687,7 +1687,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '274';
+$wgStyleVersion = '276';
 
 
 # Server-side caching:
