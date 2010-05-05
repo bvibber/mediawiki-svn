362d5105588143f1257cd70630dbc3766a141278
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-05 20:30:35 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-05-05 20:10:00 UTC)
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@65963 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/Modules/Templates/Templates.i18n.php
+++ WikiEditor/Modules/Templates/Templates.i18n.php
@@ -15,3 +15,108 @@ $messages['en'] = array(
 	'wikieditor-templates-preference' => 'Enable template collapsing',
 );
 
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'wikieditor-templates-preference' => 'Уключыць хаваньне шаблёнаў',
+);
+
+/** Czech (Česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'wikieditor-templates-preference' => 'Zapnout sbalování šablon',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'wikieditor-templates-preference' => 'Aktivierung des Ein- und Ausblendens von Vorlagen',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'wikieditor-templates-preference' => 'Fałdowanje pśedłogow zmóžniś',
+);
+
+/** Spanish (Español)
+ * @author Crazymadlover
+ */
+$messages['es'] = array(
+	'wikieditor-templates-preference' => 'Habilitar colapso de plantilla',
+);
+
+/** French (Français)
+ * @author Peter17
+ */
+$messages['fr'] = array(
+	'wikieditor-templates-preference' => 'Activer le repliement des modèles',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'wikieditor-templates-preference' => 'Fałdowanje předłohow zmóžnić',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'wikieditor-templates-preference' => 'Activar patronos plicabile',
+);
+
+/** Japanese (日本語)
+ * @author Aotake
+ */
+$messages['ja'] = array(
+	'wikieditor-templates-preference' => 'テンプレート折り畳みを有効にする',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'wikieditor-templates-preference' => 'Овозможи расклопување на шаблоните',
+);
+
+/** Malayalam (മലയാളം)
+ * @author Praveenp
+ */
+$messages['ml'] = array(
+	'wikieditor-templates-preference' => 'ഫലകം ചുരുക്കൽ സജ്ജമാക്കുക',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wikieditor-templates-preference' => 'Inklapbare sjablonen inschakelen',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ */
+$messages['pms'] = array(
+	'wikieditor-templates-preference' => 'Abilité lë sgnacament ëd jë stamp',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'wikieditor-templates-preference' => 'Possibilitar a recolha e expansão de predefinições',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'wikieditor-templates-preference' => 'Включить сворачивание шаблонов',
+);
+
