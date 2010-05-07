d0c0e3925d8750a1ec48b85f8218a41ecd997ad2
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-07 14:50:18 +0000

    Initial awesome release
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66021 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Awesomeness/Awesomeness.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalization file for the Awesomeness extension.
+ *
+ * @file Awesomeness.i18n.php
+ * @ingroup Awesomeness
+ *
+ * @author Jeroen De Dauw
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'awesomeness' => 'Awesomeness',
+	'awesomeness-desc' => 'Makes your wiki more awesome.',
+	'awesome' => 'Awesome'
+);
---
+++ extensions/Awesomeness/Awesomeness.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Initialization file for the Awesomeness extension.
+ *
+ * @file Awesomeness.php
+ * @ingroup Awesomeness
+ *
+ * @author Jeroen De Dauw
+ */
+
+/**
+ * This documenation group collects source code files belonging to Awesomeness.
+ *
+ * @defgroup Awesomeness Awesomeness
+ */
+
+define( 'Awesomeness_VERSION', 'An awesome one' );
+
+$wgExtensionFunctions[] = 'efAwesomenessSetup';
+
+$wgExtensionMessagesFiles['Awesomeness'] = dirname( __FILE__ ) . '/Awesomeness.i18n.php';
+
+$wgHooks['ArticleSave'][] = 'efAwesomenessInsertion';
+
+function efAwesomenessSetup() {
+	global $wgExtensionCredits;
+	
+	wfLoadExtensionMessages( 'Awesomeness' );
+	
+	$wgExtensionCredits['other'][] = array(
+		'path' => __FILE__,
+		'name' => wfMsg( 'awesomeness' ),
+		'version' => Awesomeness_VERSION,
+		'author' => array( '[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]' ),
+		'url' => 'http://www.mediawiki.org/wiki/Extension:Maps',
+		'description' => wfMsg( 'awesomeness-desc' ),
+	);	
+}
+
+function efAwesomenessInsertion( &$article, &$user, &$text, &$summary, $minor, $watch, $sectionanchor, &$flags ) {
+	$awesomeness = array( 'awesomeness', 'awesome' );
+
+	foreach( $awesomeness as $awesome ) {
+		$awesome = wfMsg( $awesome );
+		$text = preg_replace( "/(^|\s|-)({$awesome}[\?!\.\,]?)(\s|$)/i", " '''$2''' ", $text );
+	}
+	
+	return true;
+}
