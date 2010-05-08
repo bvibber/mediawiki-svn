a8e69b90a7a4a748b6d82b7b33be31d4a684722d
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-08 01:00:18 +0000

    Move StrategyWiki extensions into 1.16wmf4 in preparation for deployment
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@66058 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.16wmf4/extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
@@ -0,0 +1,161 @@
+<?php
+
+$messages = array();
+$messages['en'] = array(
+	'activetaskforces' => 'Active task forces',
+	'active-task-forces-intro' => 'Number of edits to task force pages in the last {{PLURAL:$1|$1 day|$1 days}}.',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'activetaskforces' => 'Актыўныя працоўныя групы',
+	'active-task-forces-intro' => 'Колькасьць рэдагаваньняў старонак працоўнай групай за $1 {{PLURAL:$1|апошні дзень|апошнія дні|апошніх дзён}}.',
+);
+
+/** Breton (Brezhoneg)
+ * @author Y-M D
+ */
+$messages['br'] = array(
+	'activetaskforces' => 'Strolladoù labour oberient',
+	'active-task-forces-intro' => 'Niver a gemm pajennoù evit ar strollad labour e-kerzh an {{PLURAL:$1|devezh ziwezhañ|$1 zevezh ziwezhañ}}.',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'activetaskforces' => 'Aktive Arbeitsgruppen',
+	'active-task-forces-intro' => 'Anzahl der Bearbeitungen auf den Seiten der Arbeitsgruppen während {{PLURAL:$1|des letzten Tages|der letzten $1 Tage}}.',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'activetaskforces' => 'Aktiwne źěłowe kupki',
+	'active-task-forces-intro' => 'Licba změnow na źěłowych kupkach za {{PLURAL:$1|zachadny $1 źeń|zachadnej $1 dnja|zachadne $1 dny|zachadnych $1 dnjow}}.',
+);
+
+/** Spanish (Español)
+ * @author Ascánder
+ */
+$messages['es'] = array(
+	'activetaskforces' => 'Fuerzas de trabajo activas',
+	'active-task-forces-intro' => 'Número de ediciones a las páginas del grupo de trabajo en los últimos {{PLURAL:$1|$1 día|$1 días}}.',
+);
+
+/** French (Français)
+ * @author Peter17
+ */
+$messages['fr'] = array(
+	'activetaskforces' => 'Groupes de travail actifs',
+	'active-task-forces-intro' => 'Nombre de modifications des pages du groupe de travail au cours {{PLURAL:$1|du dernier jour|des $1 derniers jours}}.',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'activetaskforces' => 'Grupos de traballo activos',
+	'active-task-forces-intro' => 'Número de edicións das páxinas do grupo de traballo {{PLURAL:$1|no último día|nos últimos $1 días}}.',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'activetaskforces' => 'Aktivi Arbetsgruppe',
+	'active-task-forces-intro' => 'Aazahl vu Bearbeitige an Arbetsgruppesyte {{PLURAL:$1|am letschte Tag|in dr letschte $1 Täg}}.',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'activetaskforces' => 'Aktiwne dźěłarnički',
+	'active-task-forces-intro' => 'Ličba změnow na stronach dźěłarničkow za {{PLURAL:$1|zańdźeny $1 dźeń|zańdźenej $1 dnjej|zańdźene $1 dny|zańdźenych $1 dnjow}}.',
+);
+
+/** Hungarian (Magyar)
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'activetaskforces' => 'Aktív munkacsoportok',
+	'active-task-forces-intro' => 'Szerkesztések száma a munkacsoport lapokon az elmúlt $1 napban.',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'activetaskforces' => 'Gruppos de labor active',
+	'active-task-forces-intro' => 'Numero de modificationes in paginas de gruppos de labor in le ultime {{PLURAL:$1|die|$1 dies}}.',
+);
+
+/** Japanese (日本語)
+ * @author Aotake
+ */
+$messages['ja'] = array(
+	'activetaskforces' => '活動中の特別チーム',
+	'active-task-forces-intro' => '最近$1日間に特別チームのページに加えられた編集回数',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'activetaskforces' => 'Aktiv Aarbechtsgruppen',
+	'active-task-forces-intro' => 'Zuel vun den Ännerungen op Säite vum Aarbechtsgrupp {{PLURAL:$1|de leschten Dag|an de leschten $1 Deeg}}.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'activetaskforces' => 'Активни работни групи',
+	'active-task-forces-intro' => 'Број на уредувања на страници на работни групи во {{PLURAL:$1|изминатиот $1 ден|изминатите $1 дена}}.',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'activetaskforces' => 'Actieve taskforces',
+	'active-task-forces-intro' => "Aantal bewerkingen aan taskforcepagina's in de afgelopen {{PLURAL:$1|dag|$1 dagen}}.",
+);
+
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'activetaskforces' => 'Gropes de trabalh actius',
+	'active-task-forces-intro' => 'Nombre de modificacions de las paginas del grop de trabalh al cors {{PLURAL:$1|del darrièr jorn|dels $1 darrièrs jorns}}.',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ */
+$messages['pms'] = array(
+	'activetaskforces' => 'Echip ëd travaj ative',
+	'active-task-forces-intro' => "Nùmer ëd modìfiche a le pàgine dl'echip ëd travaj ant {{PLURAL:$1|l'ùltim di|j'ùltim $1 di}}.",
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'activetaskforces' => 'Grupos de trabalho activos',
+	'active-task-forces-intro' => 'Número de edições às páginas dos grupos de trabalho {{PLURAL:$1|no último dia|nos últimos $1 dias}}.',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'activetaskforces' => 'Активные комиссии',
+	'active-task-forces-intro' => 'Количество правок страниц комиссий за {{PLURAL:$1|последний $1 день|последние $1 дня|последние $1 дней}}.',
+);
+
