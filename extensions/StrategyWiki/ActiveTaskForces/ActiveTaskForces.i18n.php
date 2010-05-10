89ec552faab4c192c2a60e7b5f9d4768ba160534
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-10 08:47:30 +0000

    Add extension credits Add special page group
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66129 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
+++ extensions/StrategyWiki/ActiveTaskForces/ActiveTaskForces.i18n.php
@@ -3,6 +3,7 @@
 $messages = array();
 $messages['en'] = array(
 	'activetaskforces' => 'Active task forces',
+	'active-task-forces-desc' => 'Shows number of edits to task force pages',
 	'active-task-forces-intro' => 'Number of edits to task force pages in the last {{PLURAL:$1|$1 day|$1 days}}.',
 );
 
