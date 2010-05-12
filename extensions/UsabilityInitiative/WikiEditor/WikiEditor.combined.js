4eebfb321dd2eee779d0cca6cbb44f44d2606674
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-12 21:08:47 +0000

    Resolves regression caused in r66286 by not declaring the context var before using it. Resolves bug #23493.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66301 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/WikiEditor/WikiEditor.combined.js
+++ extensions/UsabilityInitiative/WikiEditor/WikiEditor.combined.js
@@ -1425,6 +1425,7 @@ $j(document).ready( function() {
 				},
 				'wikieditor-toolbar-tool-link-cancel': function() {
 					// Clear any saved selection state
+					var context = $j(this).data( 'context' );
 					context.fn.restoreStuffForIE();
 					$j(this).dialog( 'close' );
 				}
