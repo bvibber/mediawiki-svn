6abcce495c9b27a593313434c49e9877a4b52bb1
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-12 00:03:39 +0000

    Resolves bug #22480 by adding monospace font styling to special characters.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@66244 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -25,15 +25,15 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 28 ),
 				array( 'src' => 'css/wikiEditor.preview.css', 'version' => 1 ),
 				array( 'src' => 'css/wikiEditor.toc.css', 'version' => 30 ),
-				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 15 ),
+				array( 'src' => 'css/wikiEditor.toolbar.css', 'version' => 16 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 88 ),
+				array( 'src' => 'css/combined.css', 'version' => 89 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 88 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 89 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
