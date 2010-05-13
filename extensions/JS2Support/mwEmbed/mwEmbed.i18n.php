b296317eff202b4466959ce007365ea592f227b5
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-13 12:51:04 +0000

    Follow-up r66317: Tweak i18n file to let it work with translatewiki Tweak messages a bit Add module to translatewiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66337 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/JS2Support/mwEmbed/mwEmbed.i18n.php
+++ extensions/JS2Support/mwEmbed/mwEmbed.i18n.php
@@ -2,6 +2,9 @@
 /**
  * Localization file for mwEmbed.js
  */
+ 
+$messages = array();
+
 $messages['en'] = array(
 	'mwe-loading_txt' => 'Loading ...',
 	'mwe-size-gigabytes' => '$1 GB',
@@ -13,21 +16,24 @@ $messages['en'] = array(
 	'mwe-load-drag-item' => 'Loading dragged item',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Cancel',
-	'mwe-enable-gadget' => 'Enable multimedia beta ( mwEmbed ) for all pages',
-	'mwe-enable-gadget-done' => 'multimedia beta gadget has been enabled',
+	'mwe-enable-gadget' => 'Enable multimedia beta (mwEmbed) for all pages',
+	'mwe-enable-gadget-done' => 'Multimedia beta gadget has been enabled',
 	'mwe-must-login-gadget' => 'To enable gadget you must <a target="_new" href="$1">login</a>',
 	'mwe-test-plural' => 'I ran {{PLURAL:$1|$1 test|$1 tests}}',
 );
+
 $messages['af'] = array(
 	'mwe-loading_txt' => 'laai ...',
 	'mwe-error_load_lib' => 'Fout: JavaScript $1 kon nie gevind word of definieer nie $2 nie',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Kanselleer',
 );
+
 $messages['ar'] = array(
 	'mwe-loading_txt' => 'يحمل...',
 	'mwe-cancel' => 'ألغِ',
 );
+
 $messages['be-tarask'] = array(
 	'mwe-loading_txt' => 'загрузка …',
 	'mwe-error_load_lib' => 'Памылка: JavaScript $1 не даступны альбо не вызначае $2',
@@ -41,6 +47,7 @@ $messages['br'] = array(
 	'mwe-ok' => 'Mat eo',
 	'mwe-cancel' => 'Nullañ',
 );
+
 $messages['bs'] = array(
 	'mwe-loading_txt' => 'učitavam ...',
 	'mwe-apiproxy-setup' => 'Postavljanje API proksija',
@@ -51,9 +58,11 @@ $messages['cs'] = array(
 	'mwe-loading_txt' => 'Načítá se …',
 	'mwe-cancel' => 'Storno',
 );
+
 $messages['cy'] = array(
 	'mwe-loading_txt' => 'wrthi\'n llwytho ...',
 );
+
 $messages['de'] = array(
 	'mwe-loading_txt' => 'lade …',
 	'mwe-error_load_lib' => 'Fehler: JavaScript $1 war nicht abrufbar oder lässt $2 nicht zu',
@@ -61,6 +70,7 @@ $messages['de'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Abbrechen',
 );
+
 $messages['diq'] = array(
 	'mwe-loading_txt' => 'bar beno...',
 	'mwe-error_load_lib' => 'xeta: $1 JavaScript re nêresiyeno ya zi pê $2yi şınasnaye niyo.',
@@ -69,6 +79,7 @@ $messages['diq'] = array(
 	'mwe-ok' => 'temam',
 	'mwe-cancel' => 'iptal',
 );
+
 $messages['dsb'] = array(
 	'mwe-loading_txt' => 'zacytujo se ...',
 	'mwe-error_load_lib' => 'Zmólka: JavaScript $1 njejo wótwołujobny był abo njedefiněrujo $2',
@@ -77,6 +88,7 @@ $messages['dsb'] = array(
 	'mwe-ok' => 'W pórědku',
 	'mwe-cancel' => 'Pśetergnuś',
 );
+
 $messages['el'] = array(
 	'mwe-loading_txt' => 'φόρτωση ...',
 );
@@ -84,20 +96,24 @@ $messages['eo'] = array(
 	'mwe-loading_txt' => 'ŝarĝante ...',
 	'mwe-cancel' => 'Nuligi',
 );
+
 $messages['es'] = array(
 	'mwe-loading_txt' => 'cargando ...',
 	'mwe-load-drag-item' => 'Cargando el objeto arrastrado',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Cancelar',
 );
+
 $messages['eu'] = array(
 	'mwe-loading_txt' => 'Kargatzen ...',
 	'mwe-ok' => 'Ados',
 	'mwe-cancel' => 'Ezeztatu',
 );
+
 $messages['fi'] = array(
 	'mwe-cancel' => 'Peruuta',
 );
+
 $messages['fr'] = array(
 	'mwe-loading_txt' => 'chargement ...',
 	'mwe-error_load_lib' => 'Erreur : JavaScript $1 n\'a pas pu être trouvé ou ne définit pas $2',
@@ -106,6 +122,7 @@ $messages['fr'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Annuler',
 );
+
 $messages['gl'] = array(
 	'mwe-loading_txt' => 'cargando...',
 	'mwe-error_load_lib' => 'Erro: o JavaScript $1 non se puido atopar ou non definía $2',
@@ -114,6 +131,7 @@ $messages['gl'] = array(
 	'mwe-ok' => 'De acordo',
 	'mwe-cancel' => 'Cancelar',
 );
+
 $messages['gsw'] = array(
 	'mwe-loading_txt' => 'Am Lade ...',
 	'mwe-error_load_lib' => 'Fähler: s JavaScript $1 het nit chenne glade wäre oder s losst $2 nit zue',
@@ -122,10 +140,12 @@ $messages['gsw'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Abbräche',
 );
+
 $messages['he'] = array(
 	'mwe-loading_txt' => 'בטעינה ...',
 	'mwe-error_load_lib' => 'שגיאה: לא ניתן היה לטעון את ספריית ה־JavaScript בשם $1 או שהיא אינה מגדירה את $2',
 );
+
 $messages['hsb'] = array(
 	'mwe-loading_txt' => 'začituje so ...',
 	'mwe-size-gigabytes' => '$1 GB',
@@ -138,6 +158,7 @@ $messages['hsb'] = array(
 	'mwe-ok' => 'W porjadku',
 	'mwe-cancel' => 'Přetorhnyć',
 );
+
 $messages['hu'] = array(
 	'mwe-loading_txt' => 'betöltés…',
 	'mwe-error_load_lib' => 'Hiba: a(z) $1 JavaScript-fájl nem elérhető, vagy nem definiálja a(z) $2 osztályt.',
@@ -146,6 +167,7 @@ $messages['hu'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Mégse',
 );
+
 $messages['ia'] = array(
 	'mwe-loading_txt' => 'cargamento ...',
 	'mwe-error_load_lib' => 'Error: JavaScript $1 non esseva recuperabile o non defini $2',
@@ -154,6 +176,7 @@ $messages['ia'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Cancella',
 );
+
 $messages['id'] = array(
 	'mwe-loading_txt' => 'memuat...',
 	'mwe-error_load_lib' => 'Kesalahan: JavaScript $1 tidak dapat diambil atau tidak mendefinisikan $2',
@@ -162,9 +185,11 @@ $messages['id'] = array(
 	'mwe-ok' => 'Oke',
 	'mwe-cancel' => 'Batalkan',
 );
+
 $messages['it'] = array(
 	'mwe-cancel' => 'Annulla',
 );
+
 $messages['ja'] = array(
 	'mwe-loading_txt' => '読み込み中 …',
 	'mwe-error_load_lib' => 'エラー：JavaScript $1 は参照不能か、$2 を定義していません。',
@@ -172,11 +197,12 @@ $messages['ja'] = array(
 	'mwe-load-drag-item' => 'ドラッグされた項目を読み込み中',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => '中止',
-);
+)
 $messages['ko'] = array(
 	'mwe-loading_txt' => '불러오는 중...',
 	'mwe-cancel' => '취소',
 );
+
 $messages['ksh'] = array(
 	'mwe-loading_txt' => 'aam Laade …',
 	'mwe-size-gigabytes' => '$1&nbsp;GB',
@@ -186,12 +212,14 @@ $messages['ksh'] = array(
 	'mwe-error_load_lib' => 'Fähler: Dat JavaSkrep „<code lang="en">$1</code>“ wohr nit ze laade udder et leiht „<code lang="en">$2</code>“ nit faß.',
 	'mwe-cancel' => 'Affbräsche',
 );
+
 $messages['lb'] = array(
 	'mwe-loading_txt' => 'lueden ...',
 	'mwe-apiproxy-setup' => 'API-Proxy astellen',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Ofbriechen',
 );
+
 $messages['ml'] = array(
 	'mwe-loading_txt' => 'ശേഖരിക്കുന്നു ...',
 	'mwe-error_load_lib' => 'പിശക്: ജാവാസ്ക്രിപ്റ്റ് $1 ശേഖരിക്കാൻ കഴിഞ്ഞില്ല അല്ലെങ്കിൽ $2 നിർവ്വചിച്ചിട്ടില്ല',
@@ -200,6 +228,7 @@ $messages['ml'] = array(
 	'mwe-ok' => 'ശരി',
 	'mwe-cancel' => 'റദ്ദാക്കുക',
 );
+
 $messages['nl'] = array(
 	'mwe-loading_txt' => 'bezig met laden ...',
 	'mwe-error_load_lib' => 'Fout: JavaScript $1 kon niet opgehaald worden of definieert $2 niet',
@@ -208,6 +237,7 @@ $messages['nl'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Annuleren',
 );
+
 $messages['oc'] = array(
 	'mwe-loading_txt' => 'cargament ...',
 	'mwe-error_load_lib' => 'Error : JavaScript $1 a pas pogut èsser trobat o definís pas $2',
@@ -216,13 +246,16 @@ $messages['oc'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Anullar',
 );
+
 $messages['pdc'] = array(
 	'mwe-ok' => 'OK',
 );
+
 $messages['pl'] = array(
 	'mwe-loading_txt' => 'ładowanie ...',
 	'mwe-error_load_lib' => 'Błąd – W plik JavaScript $1 brak jest możliwości pobrania oraz definicji $2',
 );
+
 $messages['pt'] = array(
 	'mwe-loading_txt' => 'A carregar ...',
 	'mwe-error_load_lib' => 'Erro: O JavaScript $1 não pode ser importado ou não define $2',
@@ -231,6 +264,7 @@ $messages['pt'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Cancelar',
 );
+
 $messages['ru'] = array(
 	'mwe-loading_txt' => 'загрузка …',
 	'mwe-error_load_lib' => 'Ошибка. JavaScript $1 не доступен или не определяет $2',
@@ -239,18 +273,22 @@ $messages['ru'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Отмена',
 );
+
 $messages['sk'] = array(
 	'mwe-loading_txt' => 'načítava sa ...',
 	'mwe-error_load_lib' => 'Error: JavaScript $1 nebolo možné získať alebo neobsahuje definíciu $2',
 );
+
 $messages['sv'] = array(
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Avbryt',
 );
+
 $messages['te'] = array(
 	'mwe-ok' => 'సరే',
 	'mwe-cancel' => 'రద్దుచేయి',
 );
+
 $messages['tr'] = array(
 	'mwe-loading_txt' => 'yükleniyor ...',
 	'mwe-error_load_lib' => 'Hata: $1 JavaScripti erişilebilir değil ya da $2 tanımlı değil',
@@ -259,14 +297,17 @@ $messages['tr'] = array(
 	'mwe-ok' => 'Tamam',
 	'mwe-cancel' => 'İptal',
 );
+
 $messages['vec'] = array(
 	'mwe-loading_txt' => 'so\' drio cargar ...',
 	'mwe-ok' => 'Va ben',
 	'mwe-cancel' => 'Anula',
 );
+
 $messages['vi'] = array(
 	'mwe-loading_txt' => 'đang tải …',
 	'mwe-size-kilobytes' => '$1 kB',
 	'mwe-ok' => 'OK',
 	'mwe-cancel' => 'Hủy bỏ',
 );
+
