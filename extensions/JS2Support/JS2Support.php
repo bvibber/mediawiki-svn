d87d65148285560999fbf71d1a6c4a55457e82db
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-14 00:16:02 +0000

    re-branch JS2Support to querypage-work2
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/querypage-work2@66407 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/JS2Support/JS2Support.php
@@ -0,0 +1,159 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) die();
+/**
+ * @copyright Copyright © 2010 Michael Dale <michael.dale@kaltura.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'JS2Support',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:JS2Support',
+	'author' => array( 'Michael Dale' ),
+	'descriptionmsg' => 'js2support-desc',
+);
+
+$js2Dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles[ 'JS2Support' ] = $js2Dir . 'JS2Support.i18n.php';
+
+
+/**
+ * Setup the js2 extension:
+ */
+$wgExtensionFunctions[] = 'wfSetupJS2';
+function wfSetupJS2(){
+	global $wgOut, $js2Dir, $wgAutoloadClasses, $wgScriptLoaderNamedPaths,
+	$wgExtensionJavascriptLoader, $wgEnableTestJavascriptModules;
+
+	// Remap output page as part of the extension setup
+	$wgOut = new StubObject( 'wgOut', 'ScriptLoaderOutputPage' );
+	$wgAutoloadClasses[ 'ScriptLoaderOutputPage' ] = $js2Dir . 'ScriptLoaderOutputPage.php';
+
+	// Include all the mediaWiki autoload classes:
+	require( $js2Dir . 'JS2AutoLoader.php');
+
+	// Add the core test module loaders (extensions can add their own test modules referencing this global )
+	if( $wgEnableTestJavascriptModules ) {
+		$wgExtensionJavascriptLoader[] = 'extensions/JS2Support/tests/loader.js';
+	}
+
+
+	// Update all the javascript modules classNames and localization by reading respective loader.js files
+	// @dependent on all extensions defining $wgExtensionJavascriptLoader paths in config file ( not in setup )
+	// @NOTE parsing javascript could be delayed or avoided if we require more php extension configuration
+	// 		extension would have to configure the path to javascript module localizations and possibly class paths
+	//
+	// @NOTE runtime for loadClassPaths with 8 or so loaders with 100 or so named paths is
+	// is around .002 seconds on my laptop. Could probably be further optimized and of course it only runs
+	// on non-cached pages.
+	jsClassLoader::loadClassPaths();
+}
+
+/**
+ * MakeGlobalVariablesScript hook ( add the wgScriptLoaderPath var )
+ */
+$wgHooks['MakeGlobalVariablesScript'][] = 'js2SupportAddJSVars';
+function js2SupportAddJSVars( &$vars ) {
+	global $wgExtensionAssetsPath;
+	$vars = array_merge( $vars,
+		array(
+			'wgScriptLoaderLocation' => $wgExtensionAssetsPath . 'JS2Support/mwScriptLoader.php'
+		)
+	);
+	return true;
+}
+
+/****************************
+* Configuration
+* Could eventually go into DefaultSettings.php
+*****************************/
+
+/*
+ * Simple global to tell extensions JS2 support is available
+ */
+$wgEnableJS2system = true;
+
+
+/**
+ * For defining the location of loader.js files of
+ * for js-modules. ( ie modules hosted inside of extensions )
+ */
+$wgExtensionJavascriptLoader = array();
+
+/**
+ * The set of script-loader Named Paths, populated via extensions and javascript module loaders
+ */
+$wgScriptLoaderNamedPaths = array();
+
+/*
+ * $wgEnableScriptLoader; If the script loader should be used to group all javascript requests.
+ * more about the script loader: http://www.mediawiki.org/wiki/ScriptLoader
+ *
+ */
+$wgEnableScriptLoader = false;
+
+
+/**
+ * wgEnableTestJavascriptModules if the test modules should be loaded and enabled
+ * In production environments its recommend to disabled wgEnableTestJavascriptModules
+ * since some tests can be very resource intensive.
+ */
+
+$wgEnableTestJavascriptModules = false;
+
+/**
+ * $wgScriptModifiedCheck should run a file modified check on javascript files when
+ * generating unique request ids for javascript include using the script-loader
+ *
+ * note this will only check core scripts that are directly included on the page.
+ * (not scripts loaded after the initial page display since after initial page
+ * display scripts inherit the unique request id)
+ *
+ * You can also update $wgStyleVersion
+ */
+$wgScriptModifiedFileCheck = true;
+
+/**
+ * The google closure compiler path
+ */
+$wgClosureCompilerPath = false;
+
+/**
+ * The path to java run time environment
+ */
+$wgJavaPath = false;
+
+/**
+ *  The level of optimization for the closure compiler
+ *  NOTE: SIMPLE_OPTIMIZATIONS is recommended since it preserves
+ *  functionality of syntactically valid JavaScript
+ *
+ *  for more info see: http://code.google.com/closure/compiler/docs/compilation_levels.html
+ */
+$wgClosureCompilerLevel = 'SIMPLE_OPTIMIZATIONS';
+
+/*
+ * $wgScriptModifiedMsgCheck Checks MediaWiki NS for latest messege
+ * Revision for generating the request id.
+ *
+ */
+$wgScriptModifiedMsgCheck = false;
+
+
+/**
+ * boolean; if we should enable javascript localization (it loads mw.addMessages json
+ * call with mediaWiki msgs)
+ */
+$wgEnableScriptLocalization = true;
+
+/**
+ * Path for mwEmbed normally js/mwEmbed/
+ */
+$wgMwEmbedDirectory = "extensions/JS2Support/mwEmbed";
+
+/**
+ * Enables javascript on debugging
+ * forces fresh non-minified copies of javascript to be generated
+ * on every request.
+ */
+$wgDebugJavaScript = false;
---
+++ extensions/JS2Support/mwEmbed/includes/jsClassLoader.php
@@ -0,0 +1,283 @@
+<?php
+/**
+ * The javascript class loader handles loading lists of available
+ * javascript classes into php from their defined locations in javascript.
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) die( 1 );
+
+class jsClassLoader {
+	// The list of mwEmbed core components that make up the base mwEmbed class
+	private static $coreComponentsList = array();
+
+	// The list of mwEmbed modules that are enabled
+	private static $moduleList = array();
+
+	// Stores the contents of the combined loader.js files
+	private static $combinedLoadersJs = '';
+
+	// Reg Exp that supports extracting classes from loaders
+	private static $classReplaceExp = '/mw\.addClassFilePaths\s*\(\s*{(.*)}\s*\)\s*\;/siU';
+
+	// Flag to specify if the javascript class paths have been loaded.
+	private static $classesLoaded = false;
+
+	// The current directory context. Used in loading javascript modules outside of the mwEmbed folder
+	private static $directoryContext = '';
+
+	/**
+	 * Get the javascript class paths from javascript files
+	 */
+	public static function loadClassPaths(){
+		global $wgMwEmbedDirectory, $wgExtensionJavascriptLoader,
+		$wgScriptLoaderNamedPaths, $wgExtensionMessagesFiles, $IP;
+
+		// Only run once
+		if( self::$classesLoaded ) {
+			return false;
+		}
+		self::$classesLoaded = true;
+
+		// Start the profiler if running
+		$fname = 'jsClassLoader::loadClassPaths';
+		wfProfileIn( $fname );
+
+
+		$mwEmbedAbsolutePath = ( $wgMwEmbedDirectory == '' ) ? $IP :  $IP .'/' .$wgMwEmbedDirectory;
+		// Add the mwEmbed localizations
+		$wgExtensionMessagesFiles[ 'mwEmbed' ] = $mwEmbedAbsolutePath . '/mwEmbed.i18n.php';
+
+
+		// Load javascript classes from mwEmbed.js
+		if ( !is_file( $mwEmbedAbsolutePath . '/loader.js' ) ) {
+			// throw error no mwEmbed found
+			throw new MWException( "mwEmbed loader.js missing check \$wgMwEmbedDirectory path\n" );
+			return false;
+		}
+
+		// Read the mwEmbed loader file:
+		$fileContent = file_get_contents( $mwEmbedAbsolutePath . '/loader.js' );
+
+		// Get class paths from mwEmbed.js
+		self::$directoryContext = $wgMwEmbedDirectory;
+		self::proccessLoaderContent( $fileContent );
+
+		// Get the list of core component into self::$coreComponentsList
+		preg_replace_callback(
+			'/mwCoreComponentList\s*\=\s*\[(.*)\]/siU',
+			'jsClassLoader::preg_buildComponentList',
+			$fileContent
+		);
+
+		// Get the list of enabled modules into $moduleList
+		preg_replace_callback(
+			'/mwEnabledModuleList\s*\=\s*\[(.*)\]/siU',
+			'jsClassLoader::preg_buildModuleList',
+			$fileContent
+		);
+
+		// Change to the root mediawiki directory ( loader.js paths are relative to root mediawiki directory )
+		// ( helpful for when running maintenance scripts )
+		if( defined( 'DO_MAINTENANCE' ) ) {
+			$initialPath = getcwd();
+			chdir( $IP );
+		}
+
+		// Get all the classes from the enabled mwEmbed modules folder
+		foreach( self::$moduleList as  $na => $moduleName){
+			$relativeSlash = ( $wgMwEmbedDirectory == '' )? '' : '/';
+			self::proccessModulePath( $wgMwEmbedDirectory . $relativeSlash . 'modules/' . $moduleName );
+		}
+
+		// Get all the extension loader paths registered mwEmbed modules
+		foreach( $wgExtensionJavascriptLoader as $na => $loaderPath){
+			// Setup the directory context for extensions relative to loader.js file:
+			$modulePath = str_replace('/loader.js', '' , $loaderPath);
+			self::proccessModulePath( $modulePath );
+		}
+
+		if( defined( 'DO_MAINTENANCE' ) ) {
+			chdir( $initialPath );
+		}
+
+		wfProfileOut( $fname );
+	}
+	/**
+	 * Process a loader path, passes off to proccessLoaderContent
+	 *
+	 * @param String $path Path to module to be processed
+	 */
+	private static function proccessModulePath( $path ){
+		global $wgExtensionMessagesFiles;
+
+		// Get the module name
+		$moduleName = end( explode('/', $path ) );
+
+		// Set the directory context for relative js/css paths
+		self::$directoryContext = $path;
+
+		// Check for the loader.js
+		if( !is_file(  $path . '/loader.js' ) ){
+			throw new MWException( "Javascript Module $moduleName missing loader.js file\n" );
+			return false;
+		}
+
+		$fileContent = file_get_contents( $path . '/loader.js');
+		self::proccessLoaderContent( $fileContent );
+
+		$i18nPath = realpath( $path . '/' . $moduleName . '.i18n.php' );
+
+		// Add the module localization file if present:
+		if( is_file( $i18nPath ) ) {
+			$wgExtensionMessagesFiles[ $moduleName ] = $i18nPath;
+		}
+	}
+
+	/**
+	 * Process loader content
+	 *
+	 * parses the loader files and adds
+	 *
+	 * @param String $fileContent content of loader.js file
+	 */
+	private static function proccessLoaderContent( & $fileContent ){
+		// Add the mwEmbed loader js to its global collector:
+		self::$combinedLoadersJs .=  $fileContent;
+
+		preg_replace_callback(
+			self::$classReplaceExp,
+			'jsClassLoader::preg_classPathLoader',
+			$fileContent
+		);
+	}
+	/**
+	 * Get the language file javascript
+	 * @param String $languageJs The language file javascript
+	 */
+	public static function getLanguageJs( $langKey = 'en' ){
+		global $wgMwEmbedDirectory;
+		$path =  $wgMwEmbedDirectory . '/languages/classes/Language' . ucfirst( $langKey ) . '.js';
+		if( is_file( $path ) ){
+			$languageJs = file_get_contents( $path );
+			return $languageJs;
+		}
+		return '';
+	}
+
+	/**
+	 * Get combined core component javascript
+	 *
+	 * NOTE: Component JS is javascript that is part of the
+	 * core mwEmbed javascript lib but in a separate file
+	 * for core library maintainability
+	 *
+	 * @return String combined component javascript
+	 */
+	public static function getCombinedComponentJs( $scriptLoader ) {
+		self::loadClassPaths();
+		$jsOut = '';
+		foreach(  self::$coreComponentsList as $componentClassName ) {
+			// Output the core component via the script loader:
+			$jsOut .= $scriptLoader->getLocalizedScriptText( $componentClassName );
+		}
+		return $jsOut;
+	}
+
+	/**
+	 * Get the combined loader javascript
+	 *
+	 * @return the combined loader jss
+	 */
+	public static function getCombinedLoaderJs(){
+		self::loadClassPaths();
+		return self::$combinedLoadersJs;
+	}
+
+	/**
+	 * Get the list of enabled modules
+	 */
+	public static function getModuleList(){
+		self::loadClassPaths();
+		return self::$moduleList;
+	}
+
+	/**
+	* Get the list of enabled components
+	*/
+	public static function getComponentsList(){
+		self::loadClassPaths();
+		return self::$coreComponentsList;
+	}
+
+	/**
+	 * Build a list of components to be included with mwEmbed
+	 */
+	private static function preg_buildComponentList( $jsvar ){
+		if(! isset( $jsvar[1] )){
+			return false;
+		}
+		$componentSet = explode(',', $jsvar[1] );
+		foreach( $componentSet as $na => $componentName ) {
+			$componentName = str_replace( array( '../', '\'', '"'), '', trim( $componentName ));
+			// Add the component to the $coreComponentsList
+			if( trim( $componentName ) != '' ) {
+				array_push( self::$coreComponentsList, trim( $componentName ) );
+			}
+		}
+	}
+
+	/**
+	 * Build the list of modules from the mwEnabledModuleList replace callback
+	 * @param String $jsvar Coma delimited list of modules
+	 */
+	private static function preg_buildModuleList( $jsvar ){
+		global $IP, $wgMwEmbedDirectory;
+		if(! isset( $jsvar[1] )){
+			return false;
+		}
+		$moduleSet = explode(',', $jsvar[1] );
+
+		$mwEmbedAbsolutePath = ( $wgMwEmbedDirectory == '' )? $IP:  $IP .'/' .$wgMwEmbedDirectory;
+
+		foreach( $moduleSet as $na => $moduleName ){
+			// Skip empty module names
+			if(trim( $moduleName ) == '' ){
+				continue;
+			}
+			$moduleName = str_replace( array( '../', '\'', '"'), '', trim( $moduleName ));
+			// Check if there is there are module loader files
+			if( is_file( $mwEmbedAbsolutePath . '/modules/' . $moduleName . '/loader.js' )){
+				array_push( self::$moduleList, $moduleName );
+			} else {
+				// Not valid module ( missing loader.js )
+				throw new MWException( "Missing module: $moduleName \n" );
+			}
+		}
+	}
+	/**
+	 * Adds javascript autoloader class names and paths
+	 * to $wgScriptLoaderNamedPaths global
+	 *
+	 * @param string $jvar Json string with class name list
+	 */
+	private static function preg_classPathLoader( $jsvar ) {
+		global $wgScriptLoaderNamedPaths;
+		if ( !isset( $jsvar[1] ) ) {
+			return false;
+		}
+
+		$jClassSet = FormatJson::decode( '{' . $jsvar[1] . '}', true );
+		// Check for null json decode:
+		if( $jClassSet == NULL ){
+			return false;
+		}
+
+		foreach ( $jClassSet as $className => $classPath ) {
+			// Strip $ from class (as they are stripped on URL request parameter input)
+			$className = str_replace( '$', '', $className );
+			$classPath =  ( self::$directoryContext == '' )? $classPath :  self::$directoryContext . '/' . $classPath;
+			$wgScriptLoaderNamedPaths[ $className ] = $classPath;
+		}
+	}
+}
+
