45447de454bb305fbbcc6c6843cf7c627b755a26
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-18 22:07:12 +0000

    CSS change to compliment r66623
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@66631 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -18,7 +18,7 @@ class UsabilityInitiativeHooks {
 	private static $styleFiles = array(
 		'base_sets' => array(
 			'raw' => array(
-				array( 'src' => 'css/suggestions.css', 'version' => 12 ),
+				array( 'src' => 'css/suggestions.css', 'version' => 13 ),
 				array( 'src' => 'css/vector.collapsibleNav.css', 'version' => 9 ),
 				array( 'src' => 'css/vector.footerCleanup.css', 'version' => 2 ),
 				array( 'src' => 'css/wikiEditor.css', 'version' => 13 ),
@@ -29,11 +29,11 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 92 ),
+				array( 'src' => 'css/combined.css', 'version' => 93 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 92 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 93 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
