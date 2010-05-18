a3606198c83397f904c0f2a3dc2b0597de6bd6f8
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-18 19:25:35 +0000

    removed duplicate mw.FirefoggRender
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/MwEmbedStandAlone@66616 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- modules/AddMedia/loader.js
+++ modules/AddMedia/loader.js
@@ -17,8 +17,7 @@
 		"mw.UploadHandler"		: "mw.UploadHandler.js",
 		"mw.UploadInterface"	: "mw.UploadInterface.js",
 		"mw.Firefogg"			: "mw.Firefogg.js",
-		"mw.FirefoggGUI"		: "mw.FirefoggGUI.js",
-		"mw.FirefoggRender"		: "modules/libSequencer/mw.FirefoggRender.js",
+		"mw.FirefoggGUI"		: "mw.FirefoggGUI.js",		
 		"mw.RemoteSearchDriver"	: "mw.RemoteSearchDriver.js",		
 
     "mw.style.AddMedia" : "css/mw.style.AddMedia.css",
