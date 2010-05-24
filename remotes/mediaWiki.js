dd5bf43df6f7d44c83e2d8896723b72530d61b7f
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-24 22:27:34 +0000

    fixed reference to old language key location
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/MwEmbedStandAlone@66855 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r124';
+var mwRemoteVersion = 'r125';
 var mwUseScriptLoader = true;
 
 // Log the mwRemote version ( will determine what version of js we get )
