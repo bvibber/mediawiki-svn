969fe0f86c69ea90f32ca2217df34d9fc1df727d
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-25 18:53:35 +0000

    Followup to r66854 - adding the files to the makefile and recombining
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66889 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/Vector/Vector.combined.js
+++ extensions/UsabilityInitiative/Vector/Vector.combined.js
@@ -188,6 +188,49 @@ $j(document).ready( function() {
 });
 //Global storage of fallback for onbeforeunload hook
 var fallbackWindowOnBeforeUnload = null;
+/* JavaScript for ExpandableSearch extension */
+$j( document ).ready( function() {
+	
+	// Only use this function in conjuction with the Vector skin
+	if( !wgVectorEnabledModules.expandablesearch || skin != 'vector' ) {
+		return true;
+	}
+	$j( '#searchInput' )
+		.expandableField( { 
+			'beforeExpand': function( context ) {
+				// animate the containers border
+				$j( this )
+					.parent()
+					.animate( {
+						'borderTopColor': '#a0d8ff',
+						'borderLeftColor': '#a0d8ff',
+						'borderRightColor': '#a0d8ff',
+						'borderBottomColor': '#a0d8ff' }, 'fast' );
+			},
+			'beforeCondense': function( context ) {
+				// animate the containers border
+				$j( this )
+					.parent()
+					.animate( {
+						'borderTopColor': '#aaaaaa',
+						'borderLeftColor': '#aaaaaa',
+						'borderRightColor': '#aaaaaa',
+						'borderBottomColor': '#aaaaaa' }, 'fast' );
+			},
+			'afterExpand': function( context ) {
+				//trigger the collapsible tabs resize handler
+				if ( typeof $j.collapsibleTabs != 'undefined' ){
+					$j.collapsibleTabs.handleResize();
+				}
+			},
+			'afterCondense': function( context ) {
+				//trigger the collapsible tabs resize handler
+				if ( typeof $j.collapsibleTabs != 'undefined' ){
+					$j.collapsibleTabs.handleResize();
+				}
+			}
+		} );
+});
 /* Prototype code to demonstrate proposed edit page footer cleanups */
 /* First draft and will be changing greatly */
 
