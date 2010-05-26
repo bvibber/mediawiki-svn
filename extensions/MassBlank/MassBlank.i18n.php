8f0bb8d931bea96a73d0b244617700cdabdb8dba
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-05-26 14:42:43 +0000

    Tweak messages for consistency Add Mass Blank extension to translatewiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@66922 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MassBlank/MassBlank.i18n.php
+++ extensions/MassBlank/MassBlank.i18n.php
@@ -13,23 +13,19 @@ $messages = array();
 $messages['en'] = array(
 	'massblank'               => 'Mass blank',
 	'massblank-desc'          => 'Gives administrators the ability to [[Special:MassBlank|mass blank]] pages',
-	'massblank-nopages'       => "No new pages by [[Special:Contributions/$1|$1]] in recent changes.",
-	'massblank-list'          => "The following pages were recently created by [[Special:Contributions/$1|$1]];
-put in a comment and hit the button to blank them.",
-	'massblank-defaultreason' => "Mass blanking of pages added by $1",
-	'massblank-tools'         => 'This tool allows for mass blanking of pages recently added by a given user or IP.
-Input the username or IP to get a list of pages to blank.',
+	'massblank-nopages'       => 'No new pages by [[Special:Contributions/$1|$1]] in recent changes.',
+	'massblank-list'          => 'The following pages were recently created by [[Special:Contributions/$1|$1]];
+put in a comment and hit the button to blank them.',
+	'massblank-defaultreason' => 'Mass blanking of pages added by $1',
+	'massblank-tools'         => 'This tool allows for mass blanking of pages recently added by a given user or an IP address.
+Input the username or IP address to get a list of pages to blank.',
 	'massblank-submit-user'   => 'Go',
-	'massblank-submit-blank' => 'Blank selected',
+	'massblank-submit-blank'  => 'Blank selected',
+	'massblank-blankcomment'  => 'Reason for blanking:',
 	'right-massblank'         => 'Mass blank pages',
-        'massblank-blankcomment'  => 'Reason for blanking: ',
 );
 
-/** Message documentation (Message documentation)
- * @author Jon Harald Søby
- * @author Meno25
- * @author Purodha
- */
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
 	'massblank-desc' => 'Short description of the MassBlank extension, shown in [[Special:Version]]. Do not translate or change links.',
 	'massblank-submit-user' => '{{Identical|Go}}',
