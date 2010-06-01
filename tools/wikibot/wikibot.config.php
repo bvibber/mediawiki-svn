0393c20f6b9f94593ab621aaf6a380d8182e2fb4
tisane <tisane@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-01 07:32:13 +0000

    Deleting wikibot pending approval of Cobi's SVN commit access request
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67178 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/wikibot/wikibot.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$wikibotSetting['user']['TestBot']['Testwiki'] = 'TestBot';
-$wikibotSetting['pass']['TestBot']['Testwiki'] = 'password';
-$wikibotSetting['apiurl']['*']['Testwiki'] = 'http://testwiki.org/w/api.php';
-$wikibotSetting['queryurl']['*']['Testwiki'] = 'http://testwiki.org/w/query.php';
-$wikibotSetting['indexurl']['*']['Testwiki'] = 'http://testwiki.org/w/index.php';
