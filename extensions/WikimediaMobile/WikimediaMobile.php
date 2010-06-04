d18045a53a1031e9bfb137ed81f40e9ae6bb0f92
tstarling <tstarling@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-04 09:21:47 +0000

    Redirect script for integration with wikimedia-mobile, packaged as an extension so it can be version controlled, and enabled and disabled by sysadmins. Based on the site JS hack by Hampton and Brion at http://meta.wikimedia.org/w/index.php?title=Mobile&oldid=1959389#Redirecting_users
    This module is not itself Wikimedia-specific, it's just called WikimediaMobile because it's for integration with a thing called wikimedia-mobile (which by the way is currently Wikimedia-specific).
    
    
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67329 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/WikimediaMobile/WikimediaMobile.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * MediaWiki support functions for the wikimedia-mobile project hosted 
+ * at http://github.com/hcatlin/wikimedia-mobile
+ */
+
+/**
+ * Increment this when the JS file changes
+ */
+$wgWikimediaMobileVersion = '1';
+
+/**
+ * The base URL of the mobile gateway
+ */
+$wgWikimediaMobileUrl = 'http://en.m.wikipedia.org/wiki';
+
+
+$wgHooks['BeforePageDisplay'][] = 'wfWikimediaMobileAddJs';
+$wgHooks['MakeGlobalVariablesScript'][] = 'wfWikimediaMobileVars';
+
+function wfWikimediaMobileAddJs( &$outputPage, &$skin ) {
+	global $wgOut, $wgExtensionAssetsPath, $wgWikimediaMobileVersion;
+	
+	$wgOut->addScript( Html::linkedScript( 
+		"$wgExtensionAssetsPath/WikimediaMobile/MobileRedirect.js?$wgWikimediaMobileVersion"
+	) );
+	return true;
+}
+
+function wfWikimediaMobileVars( &$vars ) {
+	global $wgWikimediaMobileUrl;
+	$vars['wgWikimediaMobileUrl'] = $wgWikimediaMobileUrl;
+	return true;
+}
+
