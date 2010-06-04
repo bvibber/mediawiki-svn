cb514e1ff73c77e0f964673219c3d3c46d39e814
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-04 00:29:06 +0000

    Tag for version 0.6.1.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@67311 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Maps/REL_0_6_1/Services/OpenLayers/Maps_OpenLayersDispPoint.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * File holding the MapsOpenLayersDispPoint class.
+ *
+ * @file Maps_OpenLayersDispPoint.php
+ * @ingroup MapsOpenLayers
+ *
+ * @author Jeroen De Dauw
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Class for handling the display_point(s) parser functions with OpenLayers.
+ *
+ * @author Jeroen De Dauw
+ */
+class MapsOpenLayersDispPoint extends MapsBasePointMap {
+	
+	public $serviceName = MapsOpenLayers::SERVICE_NAME;
+	
+	protected $markerStringFormat = 'getOLMarkerData(lon, lat, "title", "label", "icon")';
+
+	protected function getDefaultZoom() {
+		global $egMapsOpenLayersZoom;
+		return $egMapsOpenLayersZoom;
+	}
+	
+	/**
+	 * @see MapsBaseMap::doMapServiceLoad()
+	 */
+	public function doMapServiceLoad() {
+		global $egOpenLayersOnThisPage;
+		
+		MapsOpenLayers::addOLDependencies( $this->parser );
+		$egOpenLayersOnThisPage++;
+		
+		$this->elementNr = $egOpenLayersOnThisPage;
+	}
+	
+	/**
+	 * @see MapsBaseMap::addSpecificMapHTML()
+	 */
+	public function addSpecificMapHTML() {
+		global $egMapsOpenLayersPrefix, $egOpenLayersOnThisPage;
+		
+		$layerItems = MapsOpenLayers::createLayersStringAndLoadDependencies( $this->output, $this->layers );
+		
+		$mapName = $egMapsOpenLayersPrefix . '_' . $egOpenLayersOnThisPage;
+		
+		$this->output .= Html::element(
+			'div',
+			array(
+				'id' => $mapName,
+				'style' => "width: $this->width; height: $this->height; background-color: #cccccc; overflow: hidden;",
+			),
+			wfMsg( 'maps-loading-map' )
+		);
+		
+		$this->parser->getOutput()->addHeadItem(
+			Html::inlineScript( <<<EOT
+addOnloadHook(
+	function() {
+		initOpenLayer(
+			'$mapName',
+			$this->centreLon,
+			$this->centreLat,
+			$this->zoom,
+			[$layerItems],
+			[$this->controls],
+			[$this->markerString]
+		);
+	}
+);
+EOT
+		) );
+	}
+
+}
---
+++ Maps/REL_0_6_1/Services/YahooMaps/Maps_YahooMapsDispPoint.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * File holding the MapsYahooMapsDispPoint class.
+ *
+ * @file Maps_YahooMapsDispPoint.php
+ * @ingroup MapsYahooMaps
+ *
+ * @author Jeroen De Dauw
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Class for handling the display_point(s) parser functions with Yahoo! Maps.
+ *
+ * @author Jeroen De Dauw
+ */
+class MapsYahooMapsDispPoint extends MapsBasePointMap {
+	
+	public $serviceName = MapsYahooMaps::SERVICE_NAME;
+	
+	protected $markerStringFormat = 'getYMarkerData(lat, lon, "title", "label", "icon")';
+	
+	protected function getDefaultZoom() {
+		global $egMapsYahooMapsZoom;
+		return $egMapsYahooMapsZoom;
+	}
+	
+	/**
+	 * @see MapsBaseMap::doMapServiceLoad()
+	 *
+	 */
+	public function doMapServiceLoad() {
+		global $egYahooMapsOnThisPage;
+		
+		MapsYahooMaps::addYMapDependencies( $this->parser );
+		$egYahooMapsOnThisPage++;
+		
+		$this->elementNr = $egYahooMapsOnThisPage;
+	}
+	
+	/**
+	 * @see MapsBaseMap::addSpecificMapHTML()
+	 *
+	 */
+	public function addSpecificMapHTML() {
+		global $egMapsYahooMapsPrefix, $egYahooMapsOnThisPage;
+		
+		$mapName = $egMapsYahooMapsPrefix . '_' . $egYahooMapsOnThisPage;
+		
+		$this->output .= Html::element(
+			'div',
+			array(
+				'id' => $mapName,
+				'style' => "width: $this->width; height: $this->height; background-color: #cccccc; overflow: hidden;",
+			),
+			wfMsg( 'maps-loading-map' )
+		);
+		
+		$this->parser->getOutput()->addHeadItem(
+			Html::inlineScript( <<<EOT
+addOnloadHook(
+	function() {
+		initializeYahooMap(
+			'$mapName',
+			$this->centreLat,
+			$this->centreLon,
+			$this->zoom,
+			$this->type,
+			[$this->types],
+			[$this->controls],
+			$this->autozoom,
+			[$this->markerString]
+		);
+	}
+);
+EOT
+		) );
+	}
+
+}
