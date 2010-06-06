022abcd696cdd92670db59948eb238d24b6041ec
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-06 16:29:09 +0000

    Update indentation, stylize.php, update messages, headers, svn properties, i18n for description message.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67471 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/EmbedVideo/EmbedVideo.i18n.php
+++ extensions/EmbedVideo/EmbedVideo.i18n.php
@@ -1,11 +1,19 @@
 <?php
+/**
+ * Internationalisation file for extension EmbedVideo.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
 $messages = array();
+
 $messages['en'] = array(
-    'embedvideo-missing-params' => 'EmbedVideo is missing a required parameter.',
-    'embedvideo-bad-params' => 'EmbedVideo received a bad parameter.',
-    'embedvideo-unparsable-param-string' => 'EmbedVideo received the unparsable parameter string "<tt>$1</tt>".',
-    'embedvideo-unrecognized-service' => 'EmbedVideo does not recognize the video service "<tt>$1</tt>".',
-    'embedvideo-bad-id' => 'EmbedVideo received the bad id "$1" for the service "$2".',
-    'embedvideo-illegal-width' => 'EmbedVideo received the illegal width parameter "$1".',
+	'embedvideo-desc' => 'Adds a parser function for embedding video from popular sources',
+	'embedvideo-missing-params' => 'EmbedVideo is missing a required parameter.',
+	'embedvideo-bad-params' => 'EmbedVideo received a bad parameter.',
+	'embedvideo-unparsable-param-string' => 'EmbedVideo received the unparsable parameter string "<code>$1</code>".',
+	'embedvideo-unrecognized-service' => 'EmbedVideo does not recognize the video service "<code>$1</code>".',
+	'embedvideo-bad-id' => 'EmbedVideo received a bad id "$1" for the service "$2".',
+	'embedvideo-illegal-width' => 'EmbedVideo received the illegal width parameter "$1".',
 );
-?>
