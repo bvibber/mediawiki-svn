f3b29f91f1f70b5026bc04aac7de622228711c32
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-07 14:52:03 +0000

    UsabilityInitiative: (bug 22901) Work around jQuery UI bug that causes the close [x] in the top right corner of a dialog to retain focus after being clicked
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67542 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -78,7 +78,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 191 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 53 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 65 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 24 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 25 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 98 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.preview.js', 'version' => 11 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.templateEditor.js', 'version' => 77 ),
@@ -89,10 +89,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/thirdparty/contentCollector.js', 'version' => 1 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 416 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 417 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 416 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 417 ),
 			),
 		),
 	);
