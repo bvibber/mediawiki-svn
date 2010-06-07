6ff00650407fd8bc0500834bfa3a711ad9e905a2
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-07 04:15:41 +0000

    Tag for version 0.6.2.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@67513 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Maps/REL_0_6_2/Services/GoogleMaps/Maps_GoogleMapsDispPoint.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * File holding the MapsGoogleMapsDispPoint class.
+ *
+ * @file Maps_GoogleMapsDispPoint.php
+ * @ingroup MapsGoogleMaps
+ *
+ * @author Jeroen De Dauw
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Class for handling the display_point(s) parser functions with Google Maps.
+ *
+ * @ingroup MapsGoogleMaps
+ *
+ * @author Jeroen De Dauw
+ */
+final class MapsGoogleMapsDispPoint extends MapsBasePointMap {
+	
+	public $serviceName = MapsGoogleMaps::SERVICE_NAME;
+
+	protected $markerStringFormat = 'getGMarkerData(lat, lon, "title", "label", "icon")';
+	
+	protected function getDefaultZoom() {
+		global $egMapsGoogleMapsZoom;
+		return $egMapsGoogleMapsZoom;
+	}
+	
+	public function getSpecificParameterInfo() {
+		global $egMapsGMapOverlays;
+		// TODO: it'd be cool to have this static so it can be cheched in order to only init it once.
+		$this->specificParameters = array(
+			'overlays' => array(
+				'type' => array( 'string', 'list' ),
+				'criteria' => array(
+					'is_google_overlay' => array()
+				),
+				'default' => $egMapsGMapOverlays,
+			),
+		);
+		return $this->specificParameters;
+	}
+
+	/**
+	 * @see MapsBaseMap::doMapServiceLoad()
+	 */
+	public function doMapServiceLoad() {
+		global $egGoogleMapsOnThisPage;
+		
+		MapsGoogleMaps::addGMapDependencies( $this->parser );
+		$egGoogleMapsOnThisPage++;
+		
+		$this->elementNr = $egGoogleMapsOnThisPage;
+	}
+	
+	/**
+	 * @see MapsBaseMap::addSpecificMapHTML()
+	 */
+	public function addSpecificMapHTML() {
+		global $egMapsGoogleMapsPrefix, $egGoogleMapsOnThisPage;
+		
+		$mapName = $egMapsGoogleMapsPrefix . '_' . $egGoogleMapsOnThisPage;
+		
+		MapsGoogleMaps::addOverlayOutput( $this->output, $this->parser, $mapName, $this->overlays, $this->controls );
+
+		$this->output .= Html::element(
+			'div',
+			array(
+				'id' => $mapName,
+				'style' => "width: $this->width; height: $this->height; background-color: #cccccc; overflow: hidden;",
+			),
+			wfMsg( 'maps-loading-map' )
+		);
+		
+		$this->parser->getOutput()->addHeadItem(
+			Html::inlineScript(
+				<<<EOT
+addOnloadHook(
+	function() {
+		initializeGoogleMap('$mapName', 
+			{
+			lat: $this->centreLat,
+			lon: $this->centreLon,
+			zoom: $this->zoom,
+			type: $this->type,
+			types: [$this->types],
+			controls: [$this->controls],
+			scrollWheelZoom: $this->autozoom
+			},
+			[$this->markerString]
+		);
+	}
+);
+EOT
+			)
+		);
+
+	}
+	
+}
+
---
+++ Maps/REL_0_6_2/Services/GoogleMaps3/Maps_GoogleMaps3DispMap.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Class for handling the display_map parser function with Google Maps v3.
+ *
+ * @file Maps_GoogleMaps3DispMap.php
+ * @ingroup MapsGoogleMaps3
+ *
+ * @author Jeroen De Dauw
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+/**
+ * Class for handling the display_map parser functions with Google Maps v3.
+ *
+ * @ingroup MapsGoogleMaps3
+ *
+ * @author Jeroen De Dauw
+ */
+final class MapsGoogleMaps3DispMap extends MapsBaseMap {
+	
+	public $serviceName = MapsGoogleMaps3::SERVICE_NAME;
+	
+	protected function getDefaultZoom() {
+		global $egMapsGMaps3Zoom;
+		return $egMapsGMaps3Zoom;
+	}
+	
+	/**
+	 * @see MapsBaseMap::doMapServiceLoad()
+	 *
+	 */
+	public function doMapServiceLoad() {
+		global $egGMaps3OnThisPage;
+		
+		MapsGoogleMaps3::addGMap3Dependencies( $this->parser );
+		$egGMaps3OnThisPage++;
+		
+		$this->elementNr = $egGMaps3OnThisPage;
+	}
+	
+	/**
+	 * @see MapsBaseMap::addSpecificMapHTML()
+	 *
+	 */
+	public function addSpecificMapHTML() {
+		global $egMapsGMaps3Prefix, $egGMaps3OnThisPage;
+		
+		$mapName = $egMapsGMaps3Prefix . '_' . $egGMaps3OnThisPage;
+		
+		$this->output .= Html::element(
+			'div',
+			array(
+				'id' => $mapName,
+				'style' => "width: $this->width; height: $this->height; background-color: #cccccc; overflow: hidden;"
+			),
+			null
+		);
+		
+		$this->parser->getOutput()->addHeadItem(
+			Html::inlineScript( <<<EOT
+addOnloadHook(
+	function() {
+		initGMap3(
+			'$mapName',
+			{
+				zoom: $this->zoom,
+				lat: $this->centreLat,
+				lon: $this->centreLon,	
+				types: [],
+				mapTypeId: $this->type
+			},
+			[]
+		);
+	}
+);
+EOT
+			)
+		);
+	}
+	
+}
+
