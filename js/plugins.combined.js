b17e858dd0ea3221248d6d98f80ee723ae5d47ff
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-07 14:52:03 +0000

    UsabilityInitiative: (bug 22901) Work around jQuery UI bug that causes the close [x] in the top right corner of a dialog to retain focus after being clicked
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@67542 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- js/plugins.combined.js
+++ js/plugins.combined.js
@@ -8732,6 +8732,12 @@ api: {
 				$.wikiEditor.modules.dialogs.fn.reallyCreate( context, mod );
 				$dialog = $( '#' + mod.id );
 			}
+			
+			// Workaround for bug in jQuery UI: close button in top right retains focus
+			$dialog.closest( '.ui-dialog' )
+				.find( '.ui-dialog-titlebar-close' )
+				.removeClass( 'ui-state-focus' );
+			
 			$dialog.dialog( 'open' );
 		}
 	},
