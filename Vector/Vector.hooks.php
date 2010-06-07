c7b5a80d2a3910f85d45e9d2c79a439cce4aad8f
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-07 20:47:14 +0000

    UsabilityInitiative: Use Accept-Language to prioritize languages in CollapsibleNav. Not done on first request, because Accept-Language grabbing is async.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@67563 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector/Vector.hooks.php
+++ Vector/Vector.hooks.php
@@ -12,7 +12,7 @@ class VectorHooks {
 	
 	static $scripts = array(
 		'raw' => array(
-			array( 'src' => 'Modules/CollapsibleNav/CollapsibleNav.js', 'version' => 17 ),
+			array( 'src' => 'Modules/CollapsibleNav/CollapsibleNav.js', 'version' => 18 ),
 			array( 'src' => 'Modules/CollapsibleTabs/CollapsibleTabs.js', 'version' => 8 ),
 			array( 'src' => 'Modules/ExpandableSearch/ExpandableSearch.js', 'version' => 2 ),
 			array( 'src' => 'Modules/EditWarning/EditWarning.js', 'version' => 8 ),
@@ -20,10 +20,10 @@ class VectorHooks {
 			array( 'src' => 'Modules/SimpleSearch/SimpleSearch.js', 'version' => 15 ),
 		),
 		'combined' => array(
-			array( 'src' => 'Vector.combined.js', 'version' => 39 ),
+			array( 'src' => 'Vector.combined.js', 'version' => 40 ),
 		),
 		'minified' => array(
-			array( 'src' => 'Vector.combined.min.js', 'version' => 39 ),
+			array( 'src' => 'Vector.combined.min.js', 'version' => 40 ),
 		),
 	);
 	static $modules = array(
