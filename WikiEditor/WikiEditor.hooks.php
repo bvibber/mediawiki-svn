711882599aedddec52c83091e406a7951eecd5da
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-07 14:40:47 +0000

    UsabilityInitiative: (bug 23625) Change A/A to B/I for Hebrew bold/italic icons. Also change sprite offsets for r67525
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@67541 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- WikiEditor/WikiEditor.hooks.php
+++ WikiEditor/WikiEditor.hooks.php
@@ -16,16 +16,16 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/Preview/Preview.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Publish/Publish.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 7 ),
-			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 91 ),
+			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 92 ),
 			array( 'src' => 'Modules/TemplateEditor/TemplateEditor.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Templates/Templates.js', 'version' => 1 ),
 			array( 'src' => 'Modules/AddMediaWizard/AddMediaWizard.js', 'version' => 6 ),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 96 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 97 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 96 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 97 ),
 		),
 	);
 	static $messages = array(
