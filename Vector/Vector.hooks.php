a3ad45a6f6f9c4ea85f201cb27c78f0d11fe6153
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-09 15:28:45 +0000

    Declare some more vars, fix for r67548
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@67728 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector/Vector.hooks.php
+++ Vector/Vector.hooks.php
@@ -12,7 +12,7 @@ class VectorHooks {
 	
 	static $scripts = array(
 		'raw' => array(
-			array( 'src' => 'Modules/CollapsibleNav/CollapsibleNav.js', 'version' => 22 ),
+			array( 'src' => 'Modules/CollapsibleNav/CollapsibleNav.js', 'version' => 23 ),
 			array( 'src' => 'Modules/CollapsibleTabs/CollapsibleTabs.js', 'version' => 8 ),
 			array( 'src' => 'Modules/ExpandableSearch/ExpandableSearch.js', 'version' => 3 ),
 			array( 'src' => 'Modules/EditWarning/EditWarning.js', 'version' => 8 ),
@@ -20,10 +20,10 @@ class VectorHooks {
 			array( 'src' => 'Modules/SimpleSearch/SimpleSearch.js', 'version' => 15 ),
 		),
 		'combined' => array(
-			array( 'src' => 'Vector.combined.js', 'version' => 45 ),
+			array( 'src' => 'Vector.combined.js', 'version' => 46 ),
 		),
 		'minified' => array(
-			array( 'src' => 'Vector.combined.min.js', 'version' => 45 ),
+			array( 'src' => 'Vector.combined.min.js', 'version' => 46 ),
 		),
 	);
 	static $modules = array(
