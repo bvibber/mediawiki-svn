1580f41085698140b655d11d88976d2512070d4a
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-09 12:37:56 +0000

    Tweak 'interwikiintegration-setuptext' and update indentation.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67712 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/InterwikiIntegration/InterwikiIntegration.i18n.php
+++ extensions/InterwikiIntegration/InterwikiIntegration.i18n.php
@@ -12,9 +12,9 @@ $messages = array();
 $messages['en'] = array(
 	'interwikiintegration' => 'Interwiki integration',
 	'interwikiintegration-desc' => 'Comprehensive interwiki integration',
-        'populateinterwikiintegrationtable' => 'Populate interwiki integration table',
-        'interwikiintegration-setuptext' => "$1s tables have been configured.
+	'populateinterwikiintegrationtable' => 'Populate interwiki integration table',
+	'interwikiintegration-setuptext' => "The tables for $1 have been configured.
 Be sure to configure the tables on your other wikis as well.",
-        'right-integration' => 'Populate the interwiki integration table',
+	'right-integration' => 'Populate the interwiki integration table',
 );
 
