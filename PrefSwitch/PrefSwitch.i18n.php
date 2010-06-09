d382a671b96e2ff449b14c7f9444655b4bb51603
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-09 00:10:34 +0000

    Updated main and feedback sections for English translation, other translations need to follow suit.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@67678 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- PrefSwitch/PrefSwitch.i18n.php
+++ PrefSwitch/PrefSwitch.i18n.php
@@ -68,12 +68,29 @@ Please select all that apply.',
 	'prefswitch-success-feedback' => 'Your feedback has been sent.',
 	'prefswitch-return' => '<hr style="clear:both">
 Return to <span class="plainlinks">[$1 $2]</span>.',
-	'prefswitch-main' => "<div class=\"plainlinks\">
-[[file:UsabilityNavigation.png|thumb|446px|<big><font color=green>'''''Wikipedia's new theme, \"Vector\"'''''</font></big>]]
-We at the Wikimedia Foundation have been working hard to make things easier for our users. We are excited to share some improvements, including a new look and feel and simplified editing features. These changes are intended to make it easier for new contributors to get started, and are based on our [http://usability.wikimedia.org/wiki/Usability,_Experience,_and_Evaluation_Study usability testing conducted over the last year]. Improving the usability of our projects is a priority of the Wikimedia Foundation and we will be sharing more updates in the future. For more details, visit the related Wikimedia [http://blog.wikimedia.org/2010/05/13/a-new-look-for-wikipedia/ blog post].
-<div style=\"clear:both\"></div>
-[[file:UsabilityToolbar.png|thumb|446px|<big><font color=green>'''''New editing toolbar'''''</font></big>]]
-[[file:UsabilityDialogs.png|thumb|446px|<big><font color=green>'''''New dialog box for entering links'''''</font></big>]]
+	'prefswitch-main' => "{| border=\"0\" align=\"right\" style=\"margin-left:1em\"
+| align=\"center\" |
+{| border=\"0\" style=\"background:#F3F3F3;border: 1px solid #CCCCCC;padding:10px;\" 
+| [[Image:VectorNavigation-en.png|401px|]]
+|-
+| A screenshot of Wikipedia's new navigation interface <small>[[Media:VectorNavigation.png|(enlarge)]]</small>
+|}
+|-
+| align=\"center\" |
+{| border=\"0\" style=\"background:#F3F3F3;border: 1px solid #CCCCCC;padding:10px;\"
+| [[Image:VectorEditorBasic-en.png|401px|]]
+|-
+| A screenshot of the basic page editing interface <small>[[Media:VectorEditorBasic.png|(enlarge)]]</small>
+|}
+|-
+| align=\"center\" |
+{| border=\"0\" style=\"background:#F3F3F3;border: 1px solid #CCCCCC;padding:10px;\"
+| [[Image:VectorLinkDialog-en.png|401px|]]
+|-
+| A screenshot of the new dialog box for entering links
+|}
+|}
+The Wikimedia Foundation's User Experience Team has been working with volunteers from the community to make things easier for you. We are excited to share some improvements, including a new look and feel and simplified editing features. These changes are intended to make it easier for new contributors to get started, and are based on our [http://usability.wikimedia.org/wiki/Usability,_Experience,_and_Evaluation_Study usability testing conducted over the last year]. Improving the usability of our projects is a priority of the Wikimedia Foundation and we will be sharing more updates in the future. For more details, visit the related Wikimedia [http://blog.wikimedia.org/2010/05/13/a-new-look-for-wikipedia blog post].
 
 === Here's what we have changed ===
 * '''Navigation:''' We have improved the navigation for reading and editing pages. Now, the tabs at the top of each page more clearly define whether you are viewing the page or discussion page, and whether you are reading or editing a page.
@@ -81,12 +98,11 @@ We at the Wikimedia Foundation have been working hard to make things easier for 
 * '''Link wizard:''' An easy-to-use tool allows you to add links to other wiki pages as well as links to external sites.
 * '''Search improvements:''' We have improved search suggestions to get you to the page you are looking for more quickly.
 * '''Other new features:''' We have also introduced a table wizard to make creating tables easier and find and a replace feature to simplify page editing.
-* '''Wikipedia logo:''' We have updated our logo. Read more at the [http://blog.wikimedia.org/2010/05/13/wikipedia-in-3d/ Wikimedia blog].</div>",
+* '''Wikipedia logo:''' We have updated our logo. Read more at the [http://blog.wikimedia.org/2010/05/13/wikipedia-in-3d Wikimedia blog].",
 	'prefswitch-main-anon' => "===Take me back===
 If you would like to turn off the new features, <span class=\"plainlinks\">[$1 click here]</span>. You will be asked to login or create an account first.",
 	'prefswitch-main-on' => "===Feedback?===
-We would love to hear from you.  Please visit our <span class=\"plainlinks\">[$1 feedback page]</span> or, if you are interested in our ongoing efforts to improve the software, visit our [http://usability.wikimedia.org usability wiki] for more information.
-
+We would love to hear from you. Please visit our [$1 feedback page] or, if you are interested in our ongoing efforts to improve the software, visit our [http://usability.wkimedia.org usability wiki] for more information.
 ===Take me back!===
 If you would like to turn off the new features, please <span class=\"plainlinks\">[$2 click here]</span>.",
 	'prefswitch-main-off' => "===Try them out!===
