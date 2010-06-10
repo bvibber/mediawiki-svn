c2bedd1829c36ec7d48d1df12c6b663bccdfc257
devunt <devunt@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-10 13:54:42 +0000

    oops. Delete comments and change WebIRC to LogoFunction
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67809 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LogoFunction/LogoFunction.php
+++ extensions/LogoFunction/LogoFunction.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WebIRC
+ * LogoFunction
  *
  * Add PaserFunctions about wiki's logo
  *
@@ -24,31 +24,21 @@ $wgExtensionCredits[ 'parserhook' ][] = array(
     'version'        => '0.9',
 );
  
-# Define a setup function
 $wgHooks['ParserFirstCallInit'][] = 'efLogoFunction_Setup';
-# Add a hook to initialise the magic word
 $wgHooks['LanguageGetMagic'][]       = 'efLogoFunction_Magic';
  
 function efLogoFunction_Setup( &$parser ) {
-	# Set a function hook associating the "example" magic word with our function
 	$parser->setFunctionHook( 'setlogo', 'efSetLogo_Render' );
 	return true;
 }
  
 function efLogoFunction_Magic( &$magicWords, $langCode ) {
-        # Add the magic word
-        # The first array element is whether to be case sensitive, in this case (0) it is not case sensitive, 1 would be sensitive
-        # All remaining elements are synonyms for our parser function
         $magicWords['setlogo'] = array( 0, 'setlogo' );
-        # unless we return true, other parser functions extensions won't get loaded.
         return true;
 }
  
 function efSetLogo_Render( $parser, $logo = '') {
 		global $wgLogo;
-        # The parser function itself
-        # The input parameters are wikitext with templates expanded
-        # The output should be wikitext too
 		$imageobj = wfFindFile($logo);
 		if ($imageobj == null) return "<strong class=\"error\">File not exist error: [[File:$logo]] is not exist</strong>";
 		$thumb_arr = array(
