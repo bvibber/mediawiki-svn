afdffd12f22ef3e75a44ce9a24763f44f2d27526
devunt <devunt@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-10 14:50:26 +0000

    Change Extension Name LogoFunction to LogoFunctions
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67818 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/LogoFunctions/LogoFunctions.i18n.php
+++ extensions/LogoFunctions/LogoFunctions.i18n.php
@@ -1,27 +1,28 @@
 <?php
 /**
- * LogoFunction
+ * LogoFunctions
  *
- * Add PaserFunctions about wiki's logo
+ * Add Paser hooks about wiki's logo
  *
- * @link http://www.mediawiki.org/wiki/Extension:LogoFunction
+ * @link http://www.mediawiki.org/wiki/Extension:LogoFunctions
  *
  * @author Devunt <devunt@devunt.kr>
  * @authorlink http://www.mediawiki.org/wiki/User:Devunt
  * @copyright Copyright © 2010 Devunt (Bae June Hyeon).
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
+
 $messages = array();
  
 /** English
  */
 $messages['en'] = array(
-    'logofunction-desc' => 'Add Paser hooks about wiki\'s logo.',
+    'logofunctions-desc' => 'Add Paser hooks about wiki\'s logo.',
 );
  
 /** Korean (한국어)
  * @author Devunt
  */
 $messages['ko'] = array(
-    'logofunction-desc' => '위키의 로고에 관한 파서 훅을 추가합니다.',
+    'logofunctions-desc' => '위키의 로고에 관한 파서 훅들을 추가합니다.',
 );
