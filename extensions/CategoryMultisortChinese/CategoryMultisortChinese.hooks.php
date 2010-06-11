a35a006c56028157712288851a98dd62f3fb557b
liangent <liangent@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-11 02:57:15 +0000

    Replace 4 continuous spaces with a tab
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67838 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CategoryMultisortChinese/CategoryMultisortChinese.hooks.php
+++ extensions/CategoryMultisortChinese/CategoryMultisortChinese.hooks.php
@@ -100,66 +100,66 @@ class CategoryMultisortChineseHooks {
 	}
 	
 	function onCategoryMultisortSortkeys_splitString( $str ) {
-	    global $wgContLang;
-	    
-	    $result = array();
-	    while ( $str ) {
-	        $fc = $wgContLang->firstChar( $str );
-	        $result[] = $fc;
-	        $str = substr( $str, strlen( $fc ) );
-	    }
-	    return $result;
+		global $wgContLang;
+		
+		$result = array();
+		while ( $str ) {
+			$fc = $wgContLang->firstChar( $str );
+			$result[] = $fc;
+			$str = substr( $str, strlen( $fc ) );
+		}
+		return $result;
 	}
 	
 	function onCategoryMultisortSortkeys_buildMandarinSortkey( $data, $str ) {
-	    $result = '';
-	    foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
-	        # One UTF-8 character can have 4 bytes max.
-	        $c = str_pad( $ch, 4 );
-	        $chcp = utf8ToCodepoint( $ch );
-	        # One Mandarin entry can have 7 bytes max.
-	        $md = str_pad( array_key_exists( $chcp, $data->mandarin ) ? $data->mandarin[$chcp] : '', 7 );
-	        $result .= $md . $c;
-	    }
-	    return $result;
+		$result = '';
+		foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
+			# One UTF-8 character can have 4 bytes max.
+			$c = str_pad( $ch, 4 );
+			$chcp = utf8ToCodepoint( $ch );
+			# One Mandarin entry can have 7 bytes max.
+			$md = str_pad( array_key_exists( $chcp, $data->mandarin ) ? $data->mandarin[$chcp] : '', 7 );
+			$result .= $md . $c;
+		}
+		return $result;
 	}
 	
 	function onCategoryMultisortSortkeys_buildCantoneseSortkey( $data, $str ) {
-	    $result = '';
-	    foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
-	        # One UTF-8 character can have 4 bytes max.
-	        $c = str_pad( $ch, 4 );
-	        $chcp = utf8ToCodepoint( $ch );
-	        # One Cantonese entry can have 7 bytes max ([a-z]{1,6}[1-6]).
-	        $md = str_pad( array_key_exists( $chcp, $data->cantonese ) ? $data->cantonese[$chcp] : '', 7 );
-	        $result .= $md . $c;
-	    }
-	    return $result;
+		$result = '';
+		foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
+			# One UTF-8 character can have 4 bytes max.
+			$c = str_pad( $ch, 4 );
+			$chcp = utf8ToCodepoint( $ch );
+			# One Cantonese entry can have 7 bytes max ([a-z]{1,6}[1-6]).
+			$md = str_pad( array_key_exists( $chcp, $data->cantonese ) ? $data->cantonese[$chcp] : '', 7 );
+			$result .= $md . $c;
+		}
+		return $result;
 	}
 	
 	function onCategoryMultisortSortkeys_buildStrokeSortkey( $data, $str ) {
-	    $result = '';
-	    foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
-	        # One UTF-8 character can have 4 bytes max.
-	        $c = str_pad( $ch, 4 );
-	        $chcp = utf8ToCodepoint( $ch );
-	        # One stroke entry always has 3 bytes, or blank if unavailable.
-	        $s = str_pad( $this->onCategoryMultisortSortkeys_getStroke( $data, $chcp ), 3 );
-	        $result .= $s . $c;
-	    }
-	    return $result;
+		$result = '';
+		foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
+			# One UTF-8 character can have 4 bytes max.
+			$c = str_pad( $ch, 4 );
+			$chcp = utf8ToCodepoint( $ch );
+			# One stroke entry always has 3 bytes, or blank if unavailable.
+			$s = str_pad( $this->onCategoryMultisortSortkeys_getStroke( $data, $chcp ), 3 );
+			$result .= $s . $c;
+		}
+		return $result;
 	}
 	
 	function onCategoryMultisortSortkeys_buildRadicalSortkey( $data, $str ) {
-	    $result = '';
-	    foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
-	        # One UTF-8 character can have 4 bytes max.
-	        $c = str_pad( $ch, 4 );
-	        $chcp = utf8ToCodepoint( $ch );
-	        # One radical-stroke entry always has 3 (radical) + 3 (stroke) = 6 bytes, or blank if unavailable.
-	        $r = str_pad( $this->onCategoryMultisortSortkeys_getRadical( $data, $chcp ), 6 );
-	        $result .= $r . $c;
-	    }
-	    return $result;
+		$result = '';
+		foreach ( $this->onCategoryMultisortSortkeys_splitString( $str ) as $ch ) {
+			# One UTF-8 character can have 4 bytes max.
+			$c = str_pad( $ch, 4 );
+			$chcp = utf8ToCodepoint( $ch );
+			# One radical-stroke entry always has 3 (radical) + 3 (stroke) = 6 bytes, or blank if unavailable.
+			$r = str_pad( $this->onCategoryMultisortSortkeys_getRadical( $data, $chcp ), 6 );
+			$result .= $r . $c;
+		}
+		return $result;
 	}
 }
