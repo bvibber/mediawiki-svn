8ad9f2e7631442449bd0191d6f7e81dcdf9b696a
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-11 22:12:01 +0000

    SimpleSearch - converting some ems to px for better consistency in label positioning
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@67872 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
+++ extensions/UsabilityInitiative/UsabilityInitiative.hooks.php
@@ -20,7 +20,7 @@ class UsabilityInitiativeHooks {
 			'raw' => array(
 				array( 'src' => 'css/suggestions.css', 'version' => 15 ),
 				array( 'src' => 'css/vector.collapsibleNav.css', 'version' => 12 ),
-				array( 'src' => 'css/vector.expandableSearch.css', 'version' => 4 ),
+				array( 'src' => 'css/vector.expandableSearch.css', 'version' => 5 ),
 				array( 'src' => 'css/vector.footerCleanup.css', 'version' => 2 ),
 				array( 'src' => 'css/wikiEditor.css', 'version' => 14 ),
 				array( 'src' => 'css/wikiEditor.dialogs.css', 'version' => 29 ),
@@ -30,11 +30,11 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'combined' => array(
-				array( 'src' => 'css/combined.css', 'version' => 103 ),
+				array( 'src' => 'css/combined.css', 'version' => 104 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 			'minified' => array(
-				array( 'src' => 'css/combined.min.css', 'version' => 103 ),
+				array( 'src' => 'css/combined.min.css', 'version' => 104 ),
 				array( 'src' => 'css/vector/jquery-ui-1.7.2.css', 'version' => '1.7.2y' ),
 			),
 		)
@@ -91,7 +91,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins.combined.js', 'version' => 425 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 425 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 426 ),
 			),
 		),
 	);
