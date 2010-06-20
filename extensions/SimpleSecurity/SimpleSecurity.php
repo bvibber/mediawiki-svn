eb9b51c6f417063d8eb8d275bc2c5f8bdfa9ffca
nad <nad@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-20 00:53:43 +0000

    update version and date info
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@68297 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SimpleSecurity/SimpleSecurity.php
+++ extensions/SimpleSecurity/SimpleSecurity.php
@@ -20,9 +20,9 @@
  */
 
 if ( !defined( 'MEDIAWIKI' ) )                     die( 'Not an entry point.' );
-if ( version_compare( $wgVersion, '1.12.1' ) < 0 ) die( 'Sorry, this extension requires at least MediaWiki version 1.12.0' );
+if ( version_compare( $wgVersion, '1.12.0' ) < 0 ) die( 'Sorry, this extension requires at least MediaWiki version 1.12.0' );
 
-define( 'SIMPLESECURITY_VERSION', '4.4.1, 2010-06-12' );
+define( 'SIMPLESECURITY_VERSION', '4.4.2, 2010-06-20' );
 
 # Load the SimpleSecurity class and messages
 $dir = dirname( __FILE__ ) . '/';
