569f90f9ef6b794b503228931c1ccc52d7d59f5f
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-21 20:48:24 +0000

    Basic stuff (init and i18n) for new Deployment extension
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/tags/extensions@68376 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ Deployment/Deployment.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Internationalization file for the Deployment extension.
+ *
+ * @file Deployment.i18n.php
+ * @ingroup Deployment
+ *
+ * @author Jeroen De Dauw
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	// General
+	'deployment-name' => 'Deployment',
+	'deployment-desc' => 'Provides a way to install extensions via GUI and update them and your wiki itself via another GUI.',
+);
