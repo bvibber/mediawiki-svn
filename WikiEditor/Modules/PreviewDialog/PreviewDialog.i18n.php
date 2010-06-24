81d3df3d113492414e6c5af6c395cb1f8b62fe6e
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-24 16:03:10 +0000

    Adding the previewDialog module for prototyping new ideas for preview
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@68520 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ WikiEditor/Modules/PreviewDialog/PreviewDialog.i18n.php
@@ -0,0 +1,733 @@
+<?php
+/**
+ * Internationalisation for the Preview module of the Usability Initiative wikiEditor extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Roan Kattouw
+ */
+$messages['en'] = array(
+	'wikieditor-previewDialog-preference' => 'Enable preview dialog',
+	'wikieditor-previewDialog-tab' => 'Preview',
+	'wikieditor-previewDialog-loading' => 'Loading...',
+);
+
+/** Message documentation (Message documentation)
+ * @author EugeneZelenko
+ * @author Fryed-peach
+ * @author Hamilton Abreu
+ * @author Lloffiwr
+ * @author Mormegil
+ * @author Roan Kattouw
+ */
+$messages['qqq'] = array(
+	'wikieditor-previewDialog-tab' => 'Caption of the tab containing the preview
+
+{{Identical|Preview}}',
+	'wikieditor-previewDialog-loading' => 'Alt text for spinner displayed while preview is being loaded
+{{Identical|Loading}}',
+);
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'wikieditor-previewDialog-tab' => 'Voorskou',
+	'wikieditor-previewDialog-loading' => 'Besig om te laai...',
+);
+
+/** Gheg Albanian (Gegë)
+ * @author Mdupont
+ */
+$messages['aln'] = array(
+	'wikieditor-previewDialog-tab' => 'Preview',
+	'wikieditor-previewDialog-loading' => 'Loading ...',
+);
+
+/** Arabic (العربية)
+ * @author Meno25
+ * @author OsamaK
+ */
+$messages['ar'] = array(
+	'wikieditor-previewDialog-tab' => 'عاين',
+	'wikieditor-previewDialog-loading' => 'يُحمّل...',
+);
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Dudi
+ * @author Meno25
+ */
+$messages['arz'] = array(
+	'wikieditor-previewDialog-tab' => 'پروڤه',
+	'wikieditor-previewDialog-loading' => 'بيلوّد...',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author EugeneZelenko
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'wikieditor-previewDialog-tab' => 'Папярэдні прагляд',
+	'wikieditor-previewDialog-loading' => 'Загрузка…',
+);
+
+/** Bulgarian (Български)
+ * @author DCLXVI
+ * @author Spiritia
+ */
+$messages['bg'] = array(
+	'wikieditor-previewDialog-tab' => 'Предварителен преглед',
+	'wikieditor-previewDialog-loading' => 'Зареждане...',
+);
+
+/** Bengali (বাংলা)
+ * @author Bellayet
+ */
+$messages['bn'] = array(
+	'wikieditor-previewDialog-tab' => 'প্রাকদর্শন',
+	'wikieditor-previewDialog-loading' => 'লোডিং...',
+);
+
+/** Breton (Brezhoneg)
+ * @author Fohanno
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'wikieditor-previewDialog-tab' => 'Rakwelet',
+	'wikieditor-previewDialog-loading' => 'O kargañ...',
+);
+
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'wikieditor-previewDialog-tab' => 'Pregled',
+	'wikieditor-previewDialog-loading' => 'Učitavam...',
+);
+
+/** Catalan (Català)
+ * @author Paucabot
+ * @author Solde
+ * @author Vriullop
+ */
+$messages['ca'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsualització',
+	'wikieditor-previewDialog-loading' => 'Carregant ...',
+);
+
+/** Chechen (Нохчийн)
+ * @author Sasan700
+ */
+$messages['ce'] = array(
+	'wikieditor-previewDialog-tab' => 'Хьалха муха ю хьажа',
+	'wikieditor-previewDialog-loading' => 'Чуйолуш…',
+);
+
+/** Czech (Česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'wikieditor-previewDialog-tab' => 'Náhled',
+	'wikieditor-previewDialog-loading' => 'Načítá se…',
+);
+
+/** Kashubian (Kaszëbsczi)
+ * @author Kuvaly
+ */
+$messages['csb'] = array(
+	'wikieditor-previewDialog-loading' => 'Ladowanié...',
+);
+
+/** Welsh (Cymraeg)
+ * @author Lloffiwr
+ */
+$messages['cy'] = array(
+	'wikieditor-previewDialog-tab' => 'Rhagolwg',
+	'wikieditor-previewDialog-loading' => "Wrthi'n llwytho...",
+);
+
+/** Danish (Dansk)
+ * @author Sarrus
+ */
+$messages['da'] = array(
+	'wikieditor-previewDialog-tab' => 'Forhåndsvisning',
+);
+
+/** German (Deutsch)
+ * @author Als-Holder
+ * @author DaSch
+ * @author Lyzzy
+ * @author MF-Warburg
+ */
+$messages['de'] = array(
+	'wikieditor-previewDialog-tab' => 'Vorschau',
+	'wikieditor-previewDialog-loading' => 'lädt …',
+);
+
+/** Zazaki (Zazaki)
+ * @author Mirzali
+ * @author Xoser
+ */
+$messages['diq'] = array(
+	'wikieditor-previewDialog-tab' => 'Verqeyd',
+	'wikieditor-previewDialog-loading' => 'Ho bar keno...',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'wikieditor-previewDialog-tab' => 'Pśeglěd',
+	'wikieditor-previewDialog-loading' => 'Zacytujo se...',
+);
+
+/** Greek (Ελληνικά)
+ * @author Dead3y3
+ * @author ZaDiak
+ */
+$messages['el'] = array(
+	'wikieditor-previewDialog-tab' => 'Προεπισκόπηση',
+	'wikieditor-previewDialog-loading' => 'Φόρτωση...',
+);
+
+/** Esperanto (Esperanto)
+ * @author Lucas
+ * @author Marcos
+ * @author Yekrats
+ * @author Ziko
+ */
+$messages['eo'] = array(
+	'wikieditor-previewDialog-tab' => 'Antaŭvido',
+	'wikieditor-previewDialog-loading' => 'Ŝargante...',
+);
+
+/** Spanish (Español)
+ * @author Crazymadlover
+ * @author PerroVerd
+ * @author Translationista
+ */
+$messages['es'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsualizar',
+	'wikieditor-previewDialog-loading' => 'Cargando...',
+);
+
+/** Estonian (Eesti)
+ * @author Avjoska
+ * @author Pikne
+ */
+$messages['et'] = array(
+	'wikieditor-previewDialog-tab' => 'Eelvaade',
+	'wikieditor-previewDialog-loading' => 'Laadimine...',
+);
+
+/** Basque (Euskara)
+ * @author An13sa
+ */
+$messages['eu'] = array(
+	'wikieditor-previewDialog-tab' => 'Aurreikuspena',
+	'wikieditor-previewDialog-loading' => 'Kargatzen...',
+);
+
+/** Persian (فارسی)
+ * @author Ebraminio
+ */
+$messages['fa'] = array(
+	'wikieditor-previewDialog-tab' => 'پیش‌نمایش',
+	'wikieditor-previewDialog-loading' => 'در حال بارگذاری...',
+);
+
+/** Finnish (Suomi)
+ * @author Crt
+ * @author Str4nd
+ * @author ZeiP
+ */
+$messages['fi'] = array(
+	'wikieditor-previewDialog-tab' => 'Esikatselu',
+	'wikieditor-previewDialog-loading' => 'Ladataan…',
+);
+
+/** French (Français)
+ * @author IAlex
+ * @author Jean-Frédéric
+ */
+$messages['fr'] = array(
+	'wikieditor-previewDialog-tab' => 'Prévisualisation',
+	'wikieditor-previewDialog-loading' => 'Chargement...',
+);
+
+/** Franco-Provençal (Arpetan)
+ * @author ChrisPtDe
+ */
+$messages['frp'] = array(
+	'wikieditor-previewDialog-tab' => 'Prèvisualisacion',
+	'wikieditor-previewDialog-loading' => 'Chargement...',
+);
+
+/** Friulian (Furlan)
+ * @author Klenje
+ */
+$messages['fur'] = array(
+	'wikieditor-previewDialog-tab' => 'Anteprime',
+	'wikieditor-previewDialog-loading' => 'Daûr a cjamâ…',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'wikieditor-previewDialog-tab' => 'Vista previa',
+	'wikieditor-previewDialog-loading' => 'Cargando...',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'wikieditor-previewDialog-tab' => 'Vorschau',
+	'wikieditor-previewDialog-loading' => 'Am Lade ...',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ * @author YaronSh
+ */
+$messages['he'] = array(
+	'wikieditor-previewDialog-tab' => 'תצוגה מקדימה',
+	'wikieditor-previewDialog-loading' => 'נטען...',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'wikieditor-previewDialog-tab' => 'Přehlad',
+	'wikieditor-previewDialog-loading' => 'Začituje so...',
+);
+
+/** Hungarian (Magyar)
+ * @author Dani
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'wikieditor-previewDialog-tab' => 'Előnézet',
+	'wikieditor-previewDialog-loading' => 'Betöltés…',
+);
+
+/** Armenian (Հայերեն)
+ * @author Xelgen
+ */
+$messages['hy'] = array(
+	'wikieditor-previewDialog-tab' => 'Նախադիտել',
+	'wikieditor-previewDialog-loading' => 'Բեռնում…',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsualisation',
+	'wikieditor-previewDialog-loading' => 'Cargamento...',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'wikieditor-previewDialog-tab' => 'Pratayang',
+	'wikieditor-previewDialog-loading' => 'Memuat...',
+);
+
+/** Italian (Italiano)
+ * @author Aushulz
+ * @author F. Cosoleto
+ * @author Una giornata uggiosa '94
+ */
+$messages['it'] = array(
+	'wikieditor-previewDialog-tab' => 'Anteprima',
+	'wikieditor-previewDialog-loading' => 'Caricamento...',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ * @author Hosiryuhosi
+ */
+$messages['ja'] = array(
+	'wikieditor-previewDialog-tab' => 'プレビュー',
+	'wikieditor-previewDialog-loading' => '読み込み中…',
+);
+
+/** Georgian (ქართული)
+ * @author BRUTE
+ * @author გიორგიმელა
+ */
+$messages['ka'] = array(
+	'wikieditor-previewDialog-tab' => 'წინასწარ',
+	'wikieditor-previewDialog-loading' => 'იტვირთება...',
+);
+
+/** Kannada (ಕನ್ನಡ)
+ * @author Nayvik
+ */
+$messages['kn'] = array(
+	'wikieditor-previewDialog-tab' => 'ಮುನ್ನೋಟ',
+);
+
+/** Korean (한국어)
+ * @author Devunt
+ * @author Kwj2772
+ */
+$messages['ko'] = array(
+	'wikieditor-previewDialog-tab' => '미리 보기',
+	'wikieditor-previewDialog-loading' => '불러오는 중...',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'wikieditor-previewDialog-tab' => '{{int:preview}}',
+	'wikieditor-previewDialog-loading' => 'Ben aam Laade{{int:ellipsis}}',
+);
+
+/** Latin (Latina)
+ * @author UV
+ */
+$messages['la'] = array(
+	'wikieditor-previewDialog-tab' => 'Praevisum',
+	'wikieditor-previewDialog-loading' => 'Depromens...',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'wikieditor-previewDialog-tab' => 'Kucken ouni ze späicheren',
+	'wikieditor-previewDialog-loading' => 'Lueden...',
+);
+
+/** Latvian (Latviešu)
+ * @author Marozols
+ * @author Papuass
+ */
+$messages['lv'] = array(
+	'wikieditor-previewDialog-tab' => 'Pirmskats',
+	'wikieditor-previewDialog-loading' => 'Ielādē…',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'wikieditor-previewDialog-tab' => 'Преглед',
+	'wikieditor-previewDialog-loading' => 'Вчитувам...',
+);
+
+/** Malayalam (മലയാളം)
+ * @author Praveenp
+ */
+$messages['ml'] = array(
+	'wikieditor-previewDialog-tab' => 'എങ്ങനെയുണ്ടെന്നു കാണുക',
+	'wikieditor-previewDialog-loading' => 'ശേഖരിക്കുന്നു...',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Diagramma Della Verita
+ */
+$messages['ms'] = array(
+	'wikieditor-previewDialog-tab' => 'Pratonton',
+	'wikieditor-previewDialog-loading' => 'Sedang dimuat turun...',
+);
+
+/** Maltese (Malti)
+ * @author Chrisportelli
+ */
+$messages['mt'] = array(
+	'wikieditor-previewDialog-tab' => 'Dehra proviżorja',
+	'wikieditor-previewDialog-loading' => "Tiela'...",
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wikieditor-previewDialog-tab' => 'Voorvertoning',
+	'wikieditor-previewDialog-loading' => 'Bezig met laden...',
+);
+
+/** Norwegian Nynorsk (‪Norsk (nynorsk)‬)
+ * @author Eirik
+ * @author Gunnernett
+ * @author Harald Khan
+ */
+$messages['nn'] = array(
+	'wikieditor-previewDialog-tab' => 'Førehandsvising',
+	'wikieditor-previewDialog-loading' => 'Lastar…',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ * @author Laaknor
+ */
+$messages['no'] = array(
+	'wikieditor-previewDialog-tab' => 'Forhåndsvisning',
+	'wikieditor-previewDialog-loading' => 'Laster…',
+);
+
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsualizacion',
+	'wikieditor-previewDialog-loading' => 'Cargament...',
+);
+
+/** Deitsch (Deitsch)
+ * @author Xqt
+ */
+$messages['pdc'] = array(
+	'wikieditor-previewDialog-loading' => 'am laade…',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ * @author Wpedzich
+ */
+$messages['pl'] = array(
+	'wikieditor-previewDialog-tab' => 'Podgląd',
+	'wikieditor-previewDialog-loading' => 'Wczytywanie...',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsualisassion',
+	'wikieditor-previewDialog-loading' => 'A caria ...',
+);
+
+/** Pashto (پښتو)
+ * @author Ahmed-Najib-Biabani-Ibrahimkhel
+ */
+$messages['ps'] = array(
+	'wikieditor-previewDialog-tab' => 'مخليدنه',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ * @author Malafaya
+ */
+$messages['pt'] = array(
+	'wikieditor-previewDialog-tab' => 'Antevisão',
+	'wikieditor-previewDialog-loading' => 'A carregar...',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author 555
+ * @author Hamilton Abreu
+ * @author Heldergeovane
+ * @author Luckas Blade
+ */
+$messages['pt-br'] = array(
+	'wikieditor-previewDialog-tab' => 'Previsão',
+	'wikieditor-previewDialog-loading' => 'Carregando...',
+);
+
+/** Quechua (Runa Simi)
+ * @author AlimanRuna
+ */
+$messages['qu'] = array(
+	'wikieditor-previewDialog-tab' => 'Ñawpaqta qhawallay',
+	'wikieditor-previewDialog-loading' => 'Chaqnamuspa…',
+);
+
+/** Romanian (Română)
+ * @author Firilacroco
+ * @author Minisarm
+ */
+$messages['ro'] = array(
+	'wikieditor-previewDialog-tab' => 'Previzualizare',
+	'wikieditor-previewDialog-loading' => 'Încărcare...',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'wikieditor-previewDialog-tab' => 'Andeprime',
+	'wikieditor-previewDialog-loading' => 'Carecamende...',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'wikieditor-previewDialog-tab' => 'Предпросмотр',
+	'wikieditor-previewDialog-loading' => 'Загрузка…',
+);
+
+/** Yakut (Саха тыла)
+ * @author HalanTul
+ */
+$messages['sah'] = array(
+	'wikieditor-previewDialog-tab' => 'Инники көрүү',
+	'wikieditor-previewDialog-loading' => 'Хачайдана турар...',
+);
+
+/** Sinhala (සිංහල)
+ * @author Calcey
+ */
+$messages['si'] = array(
+	'wikieditor-previewDialog-tab' => 'පෙර දසුන',
+	'wikieditor-previewDialog-loading' => 'ප්‍රවේශනය වෙමින් පවතී...',
+);
+
+/** Slovak (Slovenčina)
+ * @author Helix84
+ */
+$messages['sk'] = array(
+	'wikieditor-previewDialog-tab' => 'Náhľad',
+	'wikieditor-previewDialog-loading' => 'Načítava sa...',
+);
+
+/** Slovenian (Slovenščina)
+ * @author Dbc334
+ */
+$messages['sl'] = array(
+	'wikieditor-previewDialog-tab' => 'Predogled',
+	'wikieditor-previewDialog-loading' => 'Nalaganje ...',
+);
+
+/** Serbian Cyrillic ekavian (Српски (ћирилица))
+ * @author Михајло Анђелковић
+ */
+$messages['sr-ec'] = array(
+	'wikieditor-previewDialog-loading' => 'Учитавање...',
+);
+
+/** Serbian Latin ekavian (Srpski (latinica)) */
+$messages['sr-el'] = array(
+	'wikieditor-previewDialog-loading' => 'Učitavanje...',
+);
+
+/** Swedish (Svenska)
+ * @author Boivie
+ */
+$messages['sv'] = array(
+	'wikieditor-previewDialog-tab' => 'Förhandsgranska',
+	'wikieditor-previewDialog-loading' => 'Laddar...',
+);
+
+/** Swahili (Kiswahili)
+ * @author Lloffiwr
+ */
+$messages['sw'] = array(
+	'wikieditor-previewDialog-tab' => 'Hakiki',
+	'wikieditor-previewDialog-loading' => 'Inapakiwa...',
+);
+
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages['te'] = array(
+	'wikieditor-previewDialog-tab' => 'మునుజూపు',
+	'wikieditor-previewDialog-loading' => 'లోడవుతోంది...',
+);
+
+/** Thai (ไทย)
+ * @author Octahedron80
+ * @author Woraponboonkerd
+ */
+$messages['th'] = array(
+	'wikieditor-previewDialog-tab' => 'แสดงตัวอย่าง',
+	'wikieditor-previewDialog-loading' => 'กำลังโหลด...',
+);
+
+/** Turkmen (Türkmençe)
+ * @author Hanberke
+ */
+$messages['tk'] = array(
+	'wikieditor-previewDialog-tab' => 'Deslapky syn',
+	'wikieditor-previewDialog-loading' => 'Ýüklenilýär...',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'wikieditor-previewDialog-tab' => 'Paunang tanaw',
+	'wikieditor-previewDialog-loading' => 'Ikinakarga...',
+);
+
+/** Turkish (Türkçe)
+ * @author Joseph
+ * @author Vito Genovese
+ */
+$messages['tr'] = array(
+	'wikieditor-previewDialog-tab' => 'Önizleme',
+	'wikieditor-previewDialog-loading' => 'Yükleniyor...',
+);
+
+/** Ukrainian (Українська)
+ * @author NickK
+ * @author Prima klasy4na
+ */
+$messages['uk'] = array(
+	'wikieditor-previewDialog-tab' => 'Попередній перегляд',
+	'wikieditor-previewDialog-loading' => 'Завантаження...',
+);
+
+/** Vèneto (Vèneto)
+ * @author Candalua
+ */
+$messages['vec'] = array(
+	'wikieditor-previewDialog-tab' => 'Anteprima',
+	'wikieditor-previewDialog-loading' => "So' drio cargar...",
+);
+
+/** Vietnamese (Tiếng Việt)
+ * @author Minh Nguyen
+ */
+$messages['vi'] = array(
+	'wikieditor-previewDialog-tab' => 'Xem trước',
+	'wikieditor-previewDialog-loading' => 'Đang tải…',
+);
+
+/** Yiddish (ייִדיש)
+ * @author פוילישער
+ */
+$messages['yi'] = array(
+	'wikieditor-previewDialog-tab' => 'פֿאראויסשטעלונג',
+	'wikieditor-previewDialog-loading' => 'לאָדט…',
+);
+
+/** Cantonese (粵語)
+ * @author Horacewai2
+ */
+$messages['yue'] = array(
+	'wikieditor-previewDialog-tab' => '預覽',
+	'wikieditor-previewDialog-loading' => '載入緊…',
+);
+
+/** Simplified Chinese (‪中文(简体)‬)
+ * @author Bencmq
+ * @author PhiLiP
+ */
+$messages['zh-hans'] = array(
+	'wikieditor-previewDialog-tab' => '预览',
+	'wikieditor-previewDialog-loading' => '载入中……',
+);
+
+/** Traditional Chinese (‪中文(繁體)‬)
+ * @author Gaoxuewei
+ */
+$messages['zh-hant'] = array(
+	'wikieditor-previewDialog-tab' => '預覽',
+	'wikieditor-previewDialog-loading' => '載入中...',
+);
+
