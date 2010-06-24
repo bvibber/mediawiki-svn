1d1e9fdf0d2f1a6801ad71178e0f55ce6697d2c8
juliano <juliano@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-24 00:50:43 +0000

    Increase textfield space for users to write the article title more comfortably. Suggested by PerkinsTaylor from mediawiki.org.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@68497 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Wikilog/WikilogMainPage.php
+++ extensions/Wikilog/WikilogMainPage.php
@@ -255,7 +255,7 @@ class WikilogMainPage
 		$fields[] = Xml::hidden( 'title', $this->mTitle->getPrefixedText() );
 		$fields[] = Xml::hidden( 'action', 'wikilog' );
 		$fields[] = Xml::inputLabel( wfMsg( 'wikilog-item-name' ),
-			'wlItemName', 'wl-item-name', 25 );
+			'wlItemName', 'wl-item-name', 50 );
 		$fields[] = Xml::submitButton( wfMsg( 'wikilog-new-item-go' ),
 			array( 'name' => 'wlActionNewItem' ) );
 
