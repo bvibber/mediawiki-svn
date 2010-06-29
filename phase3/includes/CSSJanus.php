7e1f31a7fef61d9f424f7405a9042ab52998a52c
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-06-29 13:54:22 +0000

    resourceloader: Finish CSSJanus port. Seems to work fine: testing it on skins/vector/main-ltr.css produced output identical to the original implementation's
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/resourceloader@68721 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/CSSJanus.php
+++ phase3/includes/CSSJanus.php
@@ -31,20 +31,30 @@
  */
 class CSSJanus {
 	// Patterns defined as null are built dynamically by buildPatterns()
-	// We wrap tokens in ` , not ~ like the original implementation does.
-	// This was done because ` is not a legal character in CSS and can only
-	// occur in URLs, where we escape it to %60 before inserting our tokens.
 	private static $patterns = array(
 		'newline' => '/\r?\n/',
 		'newlineToken' => '`NL`',
 		'tmpToken' => '`TMP`',
 		'nonAscii' => '[\200-\377]',
-		'unicode' => '((\\[0-9a-f]{1,6})(\r\n|\s)?)',
+		'unicode' => '(?:(?:\\[0-9a-f]{1,6})(?:\r\n|\s)?)',
+		'num' => '(?:[0-9]*\.[0-9]+|[0-9]+)',
+		'unit' => '(?:em|ex|px|cm|mm|in|pt|pc|deg|rad|grad|ms|s|hz|khz|%)',
 		'body_selector' => 'body\s*{\s*',
 		'direction' => 'direction\s*:\s*',
 		'escape' => null,
+		'nmstart' => null,
 		'nmchar' => null,
+		'ident' => null,
+		'quantity' => null,
+		'possibly_negative_quantity' => null,
+		'color' => null,
+		'url_special_chars' => '[!#$%&*-~]',
+		'valid_after_uri_chars' => '[\'\"]?\s*',
+		'url_chars' => null,
 		'lookahead_not_open_brace' => null,
+		'lookahead_not_closing_paren' => null,
+		'lookahead_for_closing_paren' => null,
+		'lookbehind_not_letter' => '(?<![a-zA-Z])',
 		'chars_within_selector' => '[^\}]*?',
 		'noflip_annotation' => '\/\*\s*@noflip\s*\*\/',
 		'noflip_single' => null,
@@ -52,19 +62,56 @@ class CSSJanus {
 		'comment' => '/\/\*[^*]*\*+([^\/*][^*]*\*+)*\//',
 		'body_direction_ltr' => null,
 		'body_direction_rtl' => null,
+		'left' => null,
+		'right' => null,
+		'left_in_url' => null,
+		'right_in_url' => null,
+		'ltr_in_url' => null,
+		'rtl_in_url' => null,
+		'cursor_east' => null,
+		'cursor_west' => null,
+		'four_notation_quantity' => null,
+		'four_notation_color' => null,
+		'bg_horizontal_percentage' => null,
+		'bg_horizontal_percentage_x' => null,
 	);
 	
 	private static function buildPatterns() {
+		if ( !is_null( self::$patterns['escape'] ) ) {
+			// Patterns have already been built
+			return;
+		}
 		$patterns =& self::$patterns;
-		$patterns['escape'] = "({$patterns['unicode']}|\\[^\r\n\f0-9a-f])";
-		$patterns['nmchar'] = "([_a-z0-9-]|{$patterns['nonAscii']}|{$patterns['escape']})";
+		$patterns['escape'] = "(?:{$patterns['unicode']}|\\[^\r\n\f0-9a-f])";
+		$patterns['nmstart'] = "(?:[_a-z]|{$patterns['nonAscii']}|{$patterns['escape']})";
+		$patterns['nmchar'] = "(?:[_a-z0-9-]|{$patterns['nonAscii']}|{$patterns['escape']})";
+		$patterns['ident'] = "-?{$patterns['nmstart']}{$patterns['nmchar']}*";
+		$patterns['quantity'] = "{$patterns['num']}(?:\s*{$patterns['unit']}|{$patterns['ident']})?";
+		$patterns['possibly_negative_quantity'] = "((?:-?{$patterns['quantity']})|(?:inherit|auto))";
+		$patterns['color'] = "(#?{$patterns['nmchar']}+)";
+		$patterns['url_chars'] = "(?:{$patterns['url_special_chars']}|{$patterns['nonAscii']}|{$patterns['escape']})*";
 		$patterns['lookahead_not_open_brace'] = "(?!({$patterns['nmchar']}|{$patterns['newlineToken']}|\s|#|\:|\.|\,|\+|>)*?{)";
-		$pattners['chars_within_selector'] = '[^\}]*?';
+		$patterns['lookahead_not_closing_paren'] = "(?!{$patterns['url_chars']}?{$patterns['valid_after_uri_chars']}\))";
+		$patterns['lookahead_for_closing_paren'] = "(?={$patterns['url_chars']}?{$patterns['valid_after_uri_chars']}\))";
 		
 		$patterns['noflip_single'] = "/({$patterns['noflip_annotation']}{$patterns['lookahead_not_open_brace']}[^;}]+;?)/i";
 		$patterns['noflip_class'] = "/({$patterns['noflip_annotation']}{$patterns['chars_within_selector']}})/i";
 		$patterns['body_direction_ltr'] = "/({$patterns['body_selector']}{$patterns['chars_within_selector']}{$patterns['direction']})ltr/i";
 		$patterns['body_direction_rtl'] = "/({$patterns['body_selector']}{$patterns['chars_within_selector']}{$patterns['direction']})rtl/i";
+		$patterns['left'] = "/{$patterns['lookbehind_not_letter']}(left){$patterns['lookahead_not_closing_paren']}{$patterns['lookahead_not_open_brace']}/i";
+		$patterns['right'] = "/{$patterns['lookbehind_not_letter']}(right){$patterns['lookahead_not_closing_paren']}{$patterns['lookahead_not_open_brace']}/i";
+		$patterns['left_in_url'] = "/{$patterns['lookbehind_not_letter']}(left){$patterns['lookahead_for_closing_paren']}/i";
+		$patterns['right_in_url'] = "/{$patterns['lookbehind_not_letter']}(right){$patterns['lookahead_for_closing_paren']}/i";
+		$patterns['ltr_in_url'] = "/{$patterns['lookbehind_not_letter']}(ltr){$patterns['lookahead_for_closing_paren']}/i";
+		$patterns['rtl_in_url'] = "/{$patterns['lookbehind_not_letter']}(rtl){$patterns['lookahead_for_closing_paren']}/i";
+		$patterns['cursor_east'] = "/{$patterns['lookbehind_not_letter']}([ns]?)e-resize/";
+		$patterns['cursor_west'] = "/{$patterns['lookbehind_not_letter']}([ns]?)w-resize/";
+		$patterns['four_notation_quantity'] = "/{$patterns['possibly_negative_quantity']}\s+{$patterns['possibly_negative_quantity']}\s+{$patterns['possibly_negative_quantity']}\s+{$patterns['possibly_negative_quantity']}/i";
+		$patterns['four_notation_color'] = "/(-color\s*:\s*){$patterns['color']}\s+{$patterns['color']}\s+{$patterns['color']}\s+{$patterns['color']}/i";
+		// The two regexes below are parenthesized differently then in the original implementation to make the
+		// callback's job more straightforward
+		$patterns['bg_horizontal_percentage'] = "/(background(?:-position)?\s*:\s*[^%]*?)({$patterns['num']})(%\s*(?:{$patterns['quantity']}|{$patterns['ident']}))/";
+		$patterns['bg_horizontal_percentage_x'] = "/(background-position-x\s*:\s*)({$patterns['num']})(%)/";
 	}
 	
 	/**
@@ -75,8 +122,13 @@ class CSSJanus {
 	 * @return Transformed stylesheet
 	 */
 	public function transform( $css, $swapLtrRtlInURL = false, $swapLeftRightInURL = false ) {
-		// TODO: Escape ` to %60
-		self::buildPatterns(); // TODO: Conditionally
+		// We wrap tokens in ` , not ~ like the original implementation does.
+		// This was done because ` is not a legal character in CSS and can only
+		// occur in URLs, where we escape it to %60 before inserting our tokens.	
+		$css = str_replace( '`', '%60', $css );
+		
+		self::buildPatterns();
+		
 		// Tokenize newlines so all CSS is on one line
 		$newlines = new CSSJanus_Tokenizer( self::$patterns['newline'], self::$patterns['newlineToken'] );
 		$css = $newlines->tokenize( $css );
@@ -114,44 +166,95 @@ class CSSJanus {
 		return $css;
 	}
 	
+	/**
+	 * Replace direction: ltr; with direction: rtl; and vice versa, but *only*
+	 * those inside a body { .. } selector.
+	 * 
+	 * This function suffers from a bug also present in the original
+	 * implementation where it misses body selectors with a newline between
+	 * 'body' and the opening brace.
+	 */
 	private static function fixBodyDirection( $css ) {
 		$css = preg_replace( self::$patterns['body_direction_ltr'],
-			'$1$2$3' . self::$patterns['tmpToken'], $css );
-		$css = preg_replace( self::$patterns['body_direction_rtl'], '$1$2$3ltr', $css );
+			'$1' . self::$patterns['tmpToken'], $css );
+		$css = preg_replace( self::$patterns['body_direction_rtl'], '$1ltr', $css );
 		$css = str_replace( self::$patterns['tmpToken'], 'rtl', $css );
 		return $css;
 	}
 	
+	/**
+	 * Replace 'ltr' with 'rtl' and vice versa in background URLs
+	 */
 	private static function fixLtrRtlInURL( $css ) {
-		// TODO
+		$css = preg_replace( self::$patterns['ltr_in_url'], self::$patterns['tmpToken'], $css );
+		$css = preg_replace( self::$patterns['rtl_in_url'], 'ltr', $css );
+		$css = str_replace( self::$patterns['tmpToken'], 'rtl', $css );
 		return $css;
 	}
 	
+	/**
+	 * Replace 'left' with 'right' and vice versa in background URLs
+	 */
 	private static function fixLeftRightInURL( $css ) {
-		// TODO
+		$css = preg_replace( self::$patterns['left_in_url'], self::$patterns['tmpToken'], $css );
+		$css = preg_replace( self::$patterns['right_in_url'], 'left', $css );
+		$css = str_replace( self::$patterns['tmpToken'], 'right', $css );
 		return $css;
 	}
 	
+	/**
+	 * Flip rules like left: , padding-right: , etc.
+	 */
 	private static function fixLeftAndRight( $css ) {
-		// TODO
+		$css = preg_replace( self::$patterns['left'], self::$patterns['tmpToken'], $css );
+		$css = preg_replace( self::$patterns['right'], 'left', $css );
+		$css = str_replace( self::$patterns['tmpToken'], 'right', $css );
 		return $css;
 	}
 	
+	/**
+	 * Flip East and West in rules like cursor: nw-resize;
+	 */
 	private static function fixCursorProperties( $css ) {
-		// TODO
+		$css = preg_replace( self::$patterns['cursor_east'],
+			'$1' . self::$patterns['tmpToken'], $css );
+		$css = preg_replace( self::$patterns['cursor_west'], '$1e-resize', $css );
+		$css = str_replace( self::$patterns['tmpToken'], 'w-resize', $css );
 		return $css;
 	}
 	
+	/**
+	 * Swap the second and fourth parts in four-part notation rules like
+	 * padding: 1px 2px 3px 4px;
+	 * 
+	 * This function suffers from a bug also present in the original
+	 * implementation where all whitespace between the parts is collapsed to
+	 * a single space.
+	 */
 	private static function fixFourPartNotation( $css ) {
-		// TODO
+		$css = preg_replace( self::$patterns['four_notation_quantity'], '$1 $4 $3 $2', $css );
+		$css = preg_replace( self::$patterns['four_notation_color'], '$1$2 $5 $4 $3', $css );
 		return $css;
 	}
 	
+	/**
+	 * Flip horizontal background percentages. 
+	 */
 	private static function fixBackgroundPosition( $css ) {
-		// TODO
+		$css = preg_replace_callback( self::$patterns['bg_horizontal_percentage'],
+			array( 'self', 'calculateNewBackgroundPosition' ), $css );
+		$css = preg_replace_callback( self::$patterns['bg_horizontal_percentage_x'],
+			array( 'self', 'calculateNewBackgroundPosition' ), $css );
 		return $css;
 	}
-}
+	
+	/**
+	 * Callback for calculateNewBackgroundPosition() 
+	 */
+	private static function calculateNewBackgroundPosition( $matches ) {
+		return $matches[1] . ( 100 - $matches[2] ) . $matches[3];
+	}
+}	
 
 /**
  * Utility class used by CSSJanus that tokenizes and untokenizes things we want
