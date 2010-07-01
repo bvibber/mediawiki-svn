d2a7e4cacd99e643da9a5125e8ebf5ad414695d5
catrope <catrope@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-01 17:53:40 +0000

    UsabilityInitiative: (bug 24208) "undefinedundefinedundefined" shown in special characters. Caused by a combination of user scripts adding stuff to Array.prototype and a bad for loop
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@68841 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- UsabilityInitiative.hooks.php
+++ UsabilityInitiative.hooks.php
@@ -69,7 +69,7 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/plugins/jquery.textSelection.js', 'version' => 36 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.js', 'version' => 195 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.highlight.js', 'version' => 53 ),
-				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 70 ),
+				array( 'src' => 'js/plugins/jquery.wikiEditor.toolbar.js', 'version' => 71 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.dialogs.js', 'version' => 28 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.toc.js', 'version' => 98 ),
 				array( 'src' => 'js/plugins/jquery.wikiEditor.preview.js', 'version' => 12 ),
@@ -82,10 +82,10 @@ class UsabilityInitiativeHooks {
 				array( 'src' => 'js/thirdparty/contentCollector.js', 'version' => 2 ),
 			),
 			'combined' => array(
-				array( 'src' => 'js/plugins.combined.js', 'version' => 445 ),
+				array( 'src' => 'js/plugins.combined.js', 'version' => 446 ),
 			),
 			'minified' => array(
-				array( 'src' => 'js/plugins.combined.min.js', 'version' => 455 ),
+				array( 'src' => 'js/plugins.combined.min.js', 'version' => 456 ),
 			),
 		),
 	);
