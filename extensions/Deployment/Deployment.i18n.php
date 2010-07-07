9177413774a70681f8899d979f39e9b07d709ed3
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-07 21:42:06 +0000

    Added SSH2 error messages
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@69157 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Deployment/Deployment.i18n.php
+++ extensions/Deployment/Deployment.i18n.php
@@ -19,7 +19,6 @@ $messages['en'] = array(
 	'deployment-desc' => 'Provides a way to install extensions via GUI and update them and the wiki itself via another GUI',
 
 	// Filesystem: Direct
-	
 
 	// Filesystem: FTP
 	'deploy-ftp-not-loaded' => 'The FTP PHP extension is not available',
@@ -27,5 +26,16 @@ $messages['en'] = array(
 	'deploy-ftp-username-required' => 'FTP username is required',
 	'deploy-ftp-password-required' => 'FTP password is required',
 	'deploy-ftp-hostname-required' => 'FTP hostname is required',
-	'deploy-ftp-connect-failed' => 'Failed to connect to FTP server $1:$2'
+	'deploy-ftp-connect-failed' => 'Failed to connect to FTP server $1:$2',
+	
+	// Filesystem: FTP
+	'deploy-ssh-not-loaded' => 'The FTP SSH2 extension is not available',
+	'deploy-ssh2-no-stream-get-contents'  => 'The SSH2 PHP extension is available, however, the PHP5 function stream_get_contents() is also required',
+	'deploy-ssh2-username-required' => 'SSH username is required',
+	'deploy-ssh2-password-required' => 'SSH password or private key is required',
+	'deploy-ssh2-hostname-required' => 'SSH hostname is required',
+	'deploy-ftp-connect-failed' => 'Failed to connect to SSH2 server $1:$2',
+	'deploy-ssh2-key-authentication-failed' => 'Public and private keys are incorrect for username $1',
+	'deploy-ssh2-password-authentication-failed' => 'Username or password incorrect for username $1',
+	
 );
