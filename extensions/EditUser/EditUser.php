94043289eb29ee2d4549f4991083c8de6aae324e
platonides <platonides@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-15 18:30:49 +0000

    Recover EditUser extension and put it up to date with the new preferences.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@69398 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/EditUser/EditUser.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* EditUser extension by Ryan Schmidt
+*/
+
+if(!defined('MEDIAWIKI')) {
+	echo "This file is an extension to the MediaWiki software and is not a valid access point";
+	die(1);
+}
+
+$dir = dirname(__FILE__) . '/';
+
+#in case we're running a maintenance script and GlobalFunctions.php isn't loaded...
+require_once("$IP/includes/GlobalFunctions.php");
+
+$wgExtensionCredits['specialpage'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'EditUser',
+	'version'        => '1.6',
+	'author'         => 'Ryan Schmidt',
+	'descriptionmsg' => 'edituser-desc',
+	'url'            => 'http://www.mediawiki.org/wiki/Extension:EditUser',
+);
+
+$wgExtensionMessagesFiles['EditUser'] = $dir . 'EditUser.i18n.php';
+$wgExtensionAliasesFiles['EditUser'] = $dir . 'EditUser.alias.php';
+$wgAutoloadClasses['EditUser'] = $dir . '/EditUser_body.php';
+$wgSpecialPages['EditUser'] = 'EditUser';
+$wgAvailableRights[] = 'edituser';
+$wgAvailableRights[] = 'edituser-exempt';
+$wgSpecialPageGroups['EditUser'] = 'users';
+
+#Default group permissions
+$wgGroupPermissions['bureaucrat']['edituser'] = true;
+$wgGroupPermissions['sysop']['edituser-exempt'] = true;
+
+#Debug mode, enable only if you are testing this extension or if you are having an issue
+$wgEditUserDebug = false;
+$wgEditUserDebugLog = $dir . 'debug.log';
+
+$wgHooks['SavePreferences'][] = 'efEditUserDebug';
+
+function efEditUserDebug( $eu, $user, &$msg, $old = array() ) {
+	global $wgEditUserDebug, $wgEditUserDebugLog;
+	if( !$wgEditUserDebug || !$eu instanceOf EditUser )
+		return true;
+	// $old was added in 1.13, so let's have this work for earlier versions :)
+	if( $old === array() ) {
+		wfErrorLog( "\n===== BEGIN EDITUSER REQUEST =====\nTime: "
+			. wfTime() . "\nNew user state: ".var_export( $user->mOptions, true ), $wgEditUserDebugLog );
+	} else {
+		wfErrorLog( "\n===== BEGIN EDITUSER REQUEST =====\nTime: "
+			. wfTime() . "\nCurrent user state: " . var_export( $old, true )
+			. "\nNew user state: ".var_export( $user->mOptions, true ), $wgEditUserDebugLog );
+	}
+	return true;
+}
---
+++ extensions/EditUser/EditUser_body.php
@@ -0,0 +1,175 @@
+<?php
+/* Shamelessly copied and modified from /includes/specials/SpecialPreferences.php v1.15 */
+class EditUser extends SpecialPage {
+	var $mQuickbar, $mStubs;
+	var $mRows, $mCols, $mSkin, $mMath, $mDate, $mUserEmail, $mEmailFlag, $mNick;
+	var $mUserLanguage, $mUserVariant;
+	var $mSearch, $mRecent, $mRecentDays, $mTimeZone, $mHourDiff, $mSearchLines, $mSearchChars, $mAction;
+	var $mReset, $mPosted, $mToggles, $mSearchNs, $mRealName, $mImageSize;
+	var $mUnderline, $mWatchlistEdits;
+	# password changing was removed from prefs in 1.14, so we just add it back in now :)
+	var $mNewpass, $mRetypePass;
+	
+	function __construct() {
+		SpecialPage::SpecialPage('EditUser', 'edituser');
+	}
+	function execute( $par ) {
+		global $wgOut, $wgUser, $wgRequest;
+	
+		if( !$wgUser->isAllowed( 'edituser' ) ) {
+			$wgOut->permissionRequired( 'edituser' );
+			return false;
+		}
+
+		wfLoadExtensionMessages( 'EditUser' );
+
+		$this->setHeaders();
+		$this->target = ( isset( $par ) ) ? $par : $wgRequest->getText( 'username', '' );
+		if( $this->target === '' ) {
+			$wgOut->addHtml( $this->makeSearchForm() );
+			return;
+		}
+		$targetuser = User::NewFromName( $this->target );
+		if( $targetuser->getID() == 0 ) {
+			$wgOut->addWikiText( wfMsg( 'edituser-nouser' ) );
+			return;
+		}
+		#Allow editing self via this interface
+		if( $targetuser->isAllowed( 'edituser-exempt' ) && $targetuser->getName() != $wgUser->getName() ) {
+			$wgOut->addWikiText( wfMsg( 'edituser-exempt' ) );
+			return;
+		}
+
+		$this->setHeaders();
+		$this->outputHeader();
+		$wgOut->disallowUserJs();  # Prevent hijacked user scripts from sniffing passwords etc.
+
+		if ( wfReadOnly() ) {
+			$wgOut->readOnlyPage();
+			return;
+		}
+		
+		if ( $wgRequest->getCheck( 'reset' ) ) {
+			$this->showResetForm();
+			return;
+		}
+		
+		$wgOut->addScriptFile( 'prefs.js' );
+		
+		//$this->loadGlobals( $this->target );
+		$wgOut->addHtml( $this->makeSearchForm() . '<br />' );
+		#End EditUser additions
+
+		if ( $wgRequest->getCheck( 'success' ) ) {
+			$wgOut->wrapWikiMsg(
+				"<div class=\"successbox\"><strong>\n$1\n</strong></div><div id=\"mw-pref-clear\"></div>",
+				'savedprefs'
+			);
+		}
+		
+		if ( $wgRequest->getCheck( 'eauth' ) ) {
+			$wgOut->wrapWikiMsg( "<div class='error' style='clear: both;'>\n$1\n</div>",
+									'eauthentsent', $this->target );
+		}
+
+		$htmlForm = Preferences::getFormObject( $targetuser, 'EditUserPreferencesForm' );
+		$htmlForm->setSubmitCallback( array( $this, 'tryUISubmit' ) );
+		$htmlForm->setTitle( $this->getTitle() );
+		$htmlForm->addHiddenField( 'username', $this->target );
+		$htmlForm->mEditUserUsername = $this->target;
+		
+		$htmlForm->show();
+	}
+
+	function showResetForm() {
+		global $wgOut;
+
+		$wgOut->addWikiMsg( 'prefs-reset-intro' );
+
+		$htmlForm = new HTMLForm( array(), 'prefs-restore' );
+
+		$htmlForm->setSubmitText( wfMsg( 'restoreprefs' ) );
+		$htmlForm->setTitle( $this->getTitle() );
+		$htmlForm->addHiddenField( 'username', $this->target );
+		$htmlForm->addHiddenField( 'reset' , '1' );
+		$htmlForm->setSubmitCallback( array( $this, 'submitReset' ) );
+		$htmlForm->suppressReset();
+
+		$htmlForm->show();
+	}
+
+	function submitReset( $formData ) {
+		global $wgUser, $wgOut;
+		$wgUser->resetOptions();
+		$wgUser->saveSettings();
+
+		$url = $this->getTitle()->getFullURL( array( 'success' => 1, 'username'=>$this->target ) );
+
+		$wgOut->redirect( $url );
+
+		return true;
+	}
+	
+	function tryUISubmit( $formData ) {
+		global $wgUser;
+		
+		$targetuser = User::NewFromName( $this->target );
+		if( $targetuser->getID() == 0 ) {
+			return  wfMsg( 'edituser-nouser' ) ;
+		}
+		
+		$realUser = $wgUser;
+		$wgUser = $targetuser;
+		$res = Preferences::tryFormSubmit( $formData, 'uiEditUser' );
+		$wgUser = $realUser;
+		
+		if ( $res ) {
+			$urlOptions = array( 'success' => 1);
+
+			if ( $res === 'eauth' ) {
+				$urlOptions['eauth'] = 1;
+			}
+
+			//$queryString = implode( '&', $urlOptions );
+			$urlOptions['username'] = $this->target;
+
+			$url = $this->getTitle()->getFullURL( $urlOptions );
+
+			global $wgOut;
+			$wgOut->redirect( $url );
+		}
+
+		return true;
+	}
+
+	
+	function makeSearchForm() {
+		$thisTitle = Title::makeTitle( NS_SPECIAL, $this->getName() );
+		$form = Xml::openElement( 'form', array( 'method' => 'post', 'action' => $thisTitle->getLocalUrl() ) );
+		$form .= Xml::element( 'label', array( 'for' => 'username' ), wfMsg( 'edituser-username' ) ) . ' ';
+		$form .= Xml::element( 'input', array( 'type' => 'text', 'name' => 'username', 'id' => 'username', 'value' => $this->target ) ) . ' ';
+		$form .= Xml::element( 'input', array( 'type' => 'submit', 'name' => 'dosearch', 'value' => wfMsg( 'edituser-dosearch' ) ) );
+		$form .= Xml::element( 'input', array( 'type' => 'hidden', 'name' => 'issearch', 'value' => '1' ) );
+		$form .= Xml::closeElement( 'form' );
+		return $form;
+	}
+}
+
+class EditUserPreferencesForm extends PreferencesForm {
+	var $mEditUserUsername;
+	
+	function getButtons() {
+		$html = HTMLForm::getButtons();
+
+		global $wgUser;
+
+		$sk = $wgUser->getSkin();
+		$url = SpecialPage::getTitleFor( 'EditUser' )->getFullURL( array( 'reset' => 1, 'username' => $this->mEditUserUsername ) );
+
+		$html .= "\n" . Xml::element('a', array( 'href'=> $url ), wfMsgHtml( 'restoreprefs' ) );
+
+		$html = Xml::tags( 'div', array( 'class' => 'mw-prefs-buttons' ), $html );
+
+		return $html;
+	}
+}
