29a3e3854361bbaabbb647b0359825a79ea8293f
nimishg <nimishg@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-19 20:16:45 +0000

    switching to use STDIN
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@69569 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- tools/analysis/StreamingXMLHistoryFilter.php
+++ tools/analysis/StreamingXMLHistoryFilter.php
@@ -1,23 +1,21 @@
 #!/usr/bin/php -q
 <?php
-if(count($argv) != 3){
-	print("\n\tUsage: $argv[0] <inputfile> <historyfile>\n");
+if(count($argv) != 2){
+	print("\n\tUsage: $argv[0] <inputfile> \n");
 	exit(-1);		
 }
 
-$parser = new StreamingXMLHistoryFilter($argv[1], $argv[2]);
+$parser = new StreamingXMLHistoryFilter($argv[1]);
 $parser->run();
 
 class StreamingXMLHistoryFilter{
 
 	public $fileList;
 	public $pageNameArray;
-	public $historyFile;
 	
-	public function __construct( $fileList, $historyFile){
+	public function __construct( $fileList ){
 		$this->pageNameArray = array();
 		$this->fileList = $fileList;
-		$this->historyFile = $historyFile;
 	}
     	
 	public function writeOut($title, $pageText){
@@ -41,7 +39,7 @@ class StreamingXMLHistoryFilter{
 		$this->createFileList();
 		
 		$reader = new XMLReader();
-		$reader->open($this->historyFile);		
+		$reader->open("php://stdin");		
 		
 		// NOTE: in the interests of a smaller memory footprint
 		// this relies on the XML title being the next element after the page
