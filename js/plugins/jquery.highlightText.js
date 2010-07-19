6571950f0bc55f71cae91fc19529dc6581f928cb
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-19 19:44:29 +0000

    Follow up to r69165. Adding better comments and trimming out some unnecissary code
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@69564 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- js/plugins/jquery.highlightText.js
+++ js/plugins/jquery.highlightText.js
@@ -16,32 +16,37 @@ $.highlightText = {
 		}
 		return node;
 	},
-	// adapted from Johann Burkard's highlight plugin
-	// http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html
 	// scans a node looking for the pattern and wraps a span around each match 
 	innerHighlight: function( node, pat ) {
-		var skip = 0;
+		// if this is a text node
 		if ( node.nodeType == 3 ) {
 			// TODO - need to be smarter about the character matching here. 
 			// non latin characters can make regex think a new word has begun. 
+			// look for an occurence of our pattern and store the starting position 
 			var pos = node.data.search( new RegExp( "\\b" + RegExp.escape( pat ), "i" ) );
 			if ( pos >= 0 ) {
+				// create the span wrapper for the matched text
 				var spannode = document.createElement( 'span' );
 				spannode.className = 'highlight';
+				// shave off the characters preceding the matched text
 				var middlebit = node.splitText( pos );
-				var endbit = middlebit.splitText( pat.length );
+				// shave off any unmatched text off the end
+				middlebit.splitText( pat.length );
+				// clone for appending to our span
 				var middleclone = middlebit.cloneNode( true );
+				// append the matched text node to the span
 				spannode.appendChild( middleclone );
+				// replace the matched node, with our span-wrapped clone of the matched node
 				middlebit.parentNode.replaceChild( spannode, middlebit );
-				skip = 1;
 			}
+		// if this is an element with childnodes, and not a script, style or an element we created
 		} else if ( node.nodeType == 1 && node.childNodes && !/(script|style)/i.test( node.tagName )
 				&& !( node.tagName.toLowerCase() == 'span' && node.className.match( 'highlight' ) ) ) {
 			for ( var i = 0; i < node.childNodes.length; ++i ) {
-				i += $.highlightText.innerHighlight( node.childNodes[i], pat );
+				// call the highlight function for each child node
+				$.highlightText.innerHighlight( node.childNodes[i], pat );
 			}
 		}
-		return skip;
 	}
 };
 
