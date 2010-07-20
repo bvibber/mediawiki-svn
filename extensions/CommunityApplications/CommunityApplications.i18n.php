ef1576df43f59f33f722049cc7ddfef8a044d427
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-20 13:09:59 +0000

    Commit CommunityApplications extension, the "reading" part of the CommunityHiring ecosystem. Still needs alternative (CSV) output.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@69607 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/CommunityApplications/CommunityApplications.i18n.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages = array();
+
+$messages['en'] = array(
+	'community-applications-title' => 'Community Applications',
+	'community-applications-application-title' => 'Application $1: $2, $3',
+);
---
+++ extensions/CommunityApplications/SpecialCommunityApplications.php
@@ -0,0 +1,47 @@
+<?php
+
+class SpecialCommunityApplications extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'CommunityApplications', 'view-community-applications' );
+		wfLoadExtensionMessages( 'CommunityApplications' );
+	}
+	
+	function execute($par) {
+		global $wgUser, $wgOut;
+		
+		if ( !$this->userCanExecute( $wgUser ) ) {
+			$this->displayRestrictionError();
+			return;
+		}
+		
+		$wgOut->setPageTitle( wfMsg( 'community-applications-title' ) );
+		
+		$dbr = wfGetDB( DB_SLAVE );
+		
+		$res = $dbr->select( 'community_hiring_application', '*', 1, __METHOD__ );
+		
+		foreach( $res as $row ) {
+			$this->showEntry( $row );
+		}
+	}
+	
+	function showEntry( $row ) {
+		global $wgOut;
+		$wgOut->addHTML( "<hr/>" );
+		$wgOut->addHTML( "<table><tbody>" );
+		
+		$data = get_object_vars(json_decode( $row->ch_data ));
+		
+		$header = wfMsg( 'community-applications-application-title', $row->ch_id,
+				$data['family-name'], $data['given-name'] );
+		$wgOut->addHTML( Xml::element( 'h2', null, $header ) );
+		foreach( $data as $key => $value ) {
+			$html = Xml::element( 'td', null, $key );
+			$html .= Xml::element( 'td', null, $value );
+			$html = Xml::tags( 'tr', null, $html ) . "\n";
+			$wgOut->addHTML( $html );
+		}
+		
+		$wgOut->addHTML( "</tbody></table>" );
+	}
+}
