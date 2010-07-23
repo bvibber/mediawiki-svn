68bbf17e172aecea449c5cd10de92c2f572b2d5a
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-23 07:15:56 +0000

    Use parentheses message to enclose the tool links
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@69775 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/CodeReview/ui/CodeRepoListView.php
+++ extensions/CodeReview/ui/CodeRepoListView.php
@@ -21,7 +21,6 @@ class CodeRepoListView {
 	public static function getNavItem( $repo ) {
 		global $wgLang, $wgUser;
 		$name = $repo->getName();
-		$text = "'''[[Special:Code/$name|$name]]''' (";
 		$links[] = "[[Special:Code/$name/comments|" . wfMsgHtml( 'code-notes' ) . "]]";
 		$links[] = "[[Special:Code/$name/statuschanges|" . wfMsgHtml( 'code-statuschanges' ) . "]]";
 		if ( $wgUser->getId() ) {
@@ -35,8 +34,7 @@ class CodeRepoListView {
 		$links[] = "[[Special:Code/$name/status|" . wfMsgHtml( 'code-status' ) . "]]";
 		$links[] = "[[Special:Code/$name/releasenotes|" . wfMsgHtml( 'code-releasenotes' ) . "]]";
 		$links[] = "[[Special:Code/$name/stats|" . wfMsgHtml( 'code-stats' ) . "]]";
-		$text .= $wgLang->pipeList( $links );
-		$text .= ")";
+		$text = "'''[[Special:Code/$name|$name]]''' " . wfMsg( 'parentheses', $wgLang->pipeList( $links ) );
 		return $text;
 	}
 }
