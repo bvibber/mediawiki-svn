02e5eb0dc34c5d15186ee89faef00067d2ab3bb6
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-28 21:57:56 +0000

    Fix for bug 24350
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk/extensions/UsabilityInitiative@70117 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- Vector/Vector.hooks.php
+++ Vector/Vector.hooks.php
@@ -17,13 +17,13 @@ class VectorHooks {
 			array( 'src' => 'Modules/ExpandableSearch/ExpandableSearch.js', 'version' => 5 ),
 			array( 'src' => 'Modules/EditWarning/EditWarning.js', 'version' => 10 ),
 			array( 'src' => 'Modules/FooterCleanup/FooterCleanup.js', 'version' => 5 ),
-			array( 'src' => 'Modules/SimpleSearch/SimpleSearch.js', 'version' => 25 ),
+			array( 'src' => 'Modules/SimpleSearch/SimpleSearch.js', 'version' => 26 ),
 		),
 		'combined' => array(
-			array( 'src' => 'Vector.combined.js', 'version' => 69 ),
+			array( 'src' => 'Vector.combined.js', 'version' => 70 ),
 		),
 		'minified' => array(
-			array( 'src' => 'Vector.combined.min.js', 'version' => 70 ),
+			array( 'src' => 'Vector.combined.min.js', 'version' => 71 ),
 		),
 	);
 	static $modules = array(
--- WikiEditor/WikiEditor.hooks.php
+++ WikiEditor/WikiEditor.hooks.php
@@ -16,17 +16,17 @@ class WikiEditorHooks {
 			array( 'src' => 'Modules/Preview/Preview.js', 'version' => 9 ),
 			array( 'src' => 'Modules/PreviewDialog/PreviewDialog.js', 'version' => 2 ),
 			array( 'src' => 'Modules/Publish/Publish.js', 'version' => 6 ),
-			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 9 ),
+			array( 'src' => 'Modules/Toc/Toc.js', 'version' => 10 ),
 			array( 'src' => 'Modules/Toolbar/Toolbar.js', 'version' => 104 ),
 			array( 'src' => 'Modules/TemplateEditor/TemplateEditor.js', 'version' => 6 ),
 			array( 'src' => 'Modules/Templates/Templates.js', 'version' => 1 ),
 			array( 'src' => 'Modules/AddMediaWizard/AddMediaWizard.js', 'version' => 6 ),
 		),
 		'combined' => array(
-			array( 'src' => 'WikiEditor.combined.js', 'version' => 119 ),
+			array( 'src' => 'WikiEditor.combined.js', 'version' => 120 ),
 		),
 		'minified' => array(
-			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 119 ),
+			array( 'src' => 'WikiEditor.combined.min.js', 'version' => 120 ),
 		),
 	);
 	static $messages = array(
