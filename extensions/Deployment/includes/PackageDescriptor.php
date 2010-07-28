a151cf65035908f0bb9e63ce35ee1ba895eeb79d
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-07-28 22:44:33 +0000

    Added some class stubs
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@70120 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/Deployment/includes/PackageDescriptor.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * File holding the PackageDescriptor class.
+ * Partly based on DeployDescriptor from Ontoprises Deployment Framework.
+ *
+ * @file PackageDescriptor.php
+ * @ingroup Deployment
+ *
+ * @author Jeroen De Dauw
+ * @author Kai Kühn
+ */
+
+/**
+ * Package description class.
+ * 
+ * @author Jeroen De Dauw
+ * @author Kai Kühn
+ */
+class PackageDescriptor {
+	
+}
---
+++ extensions/Deployment/includes/PackageRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * File holding the PackageRepository class.
+ *
+ * @file PackageRepository.php
+ * @ingroup Deployment
+ *
+ * @author Jeroen De Dauw
+ */
+
+/**
+ * Package description processing class.
+ * 
+ * @author Jeroen De Dauw
+ */
+class PackageRepository {
+	
+}
