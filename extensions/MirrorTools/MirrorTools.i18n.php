17fb0ea8fa6345bee75e64bd2443ba0a5cd72e8c
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-02 18:23:34 +0000

    Add variable for new user rights Add i18n file Add extension to translatewiki
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@70345 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/MirrorTools/MirrorTools.i18n.php
+++ extensions/MirrorTools/MirrorTools.i18n.php
@@ -6,10 +6,15 @@
  
 $messages = array();
  
-/* English
+/** English
  * @author Tisane
  */
 $messages['en'] = array(
 	'mirrortools' => 'MirrorTools',
-	'mirrortools-desc' => 'Allows edits to be made under any username'
+	'mirrortools-desc' => 'Allows edits via API to be made under any username',
+	'right-mirroredit' => 'Edit via API under any username',
+	'group-MirrorTools' => 'Mirror tool users',
+	'group-MirrorTools-member' => 'mirror tool user',
+	'grouppage-MirrorTools' => '{{ns:project}}:Mirror tool users',
 );
+
