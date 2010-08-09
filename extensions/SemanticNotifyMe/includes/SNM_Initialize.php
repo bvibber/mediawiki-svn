41e6fe857a578a842667b952b585e52457fdd732
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-09 03:37:21 +0000

    Follow up to r70664, added support for SEMANTIC extension type
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@70738 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SemanticNotifyMe/includes/SNM_Initialize.php
+++ extensions/SemanticNotifyMe/includes/SNM_Initialize.php
@@ -150,11 +150,12 @@ function smwgNMSetupExtension() {
 	}
 
 	// Register Credits
-	$wgExtensionCredits['parserhook'][] = array(
-	'name' => 'Semantic&#160;NotifyMe&#160;Extension', 'version' => SMW_NM_VERSION,
-			'author' => "Ning Hu, Justin Zhang, [http://smwforum.ontoprise.com/smwforum/index.php/Jesse_Wang Jesse Wang], sponsored by [http://projecthalo.com Project Halo], [http://www.vulcan.com Vulcan Inc.]",
-			'url' => 'http://wiking.vulcan.com/dev',
-			'description' => 'Notify wiki user with specified queries.' );
+	$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'parserhook'][] = array(
+		'name' => 'Semantic&#160;NotifyMe&#160;Extension', 'version' => SMW_NM_VERSION,
+		'author' => "Ning Hu, Justin Zhang, [http://smwforum.ontoprise.com/smwforum/index.php/Jesse_Wang Jesse Wang], sponsored by [http://projecthalo.com Project Halo], [http://www.vulcan.com Vulcan Inc.]",
+		'url' => 'http://wiking.vulcan.com/dev',
+		'description' => 'Notify wiki user with specified queries.'
+	);
 
 	return true;
 }
