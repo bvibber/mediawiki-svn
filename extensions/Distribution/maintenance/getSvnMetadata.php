e91d02439186a8d902922e7f7a6cf1d98d17d048
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-09 02:32:04 +0000

    Added some code copied from ExtensionDistributor
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@70734 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/Distribution/maintenance/getSvnMetadata.php
+++ extensions/Distribution/maintenance/getSvnMetadata.php
@@ -22,8 +22,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
  *
+ * @since 0.1
+ *
  * @author Jeroen De Dauw
- * @author Yaron Koren
+ * 
  * @ingroup Maintenance
  */
 
@@ -31,19 +33,77 @@ require_once( dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
 
 class GetSvnMetadata extends Maintenance {
 	
+	/**
+	 * @var array or false
+	 */
+	protected $extensionList = false;
+	
+	/**
+	 * @see Maintenance::execute
+	 * 
+	 * @since 0.1
+	 */	
 	public function __construct() {
 		parent::__construct();
 		
 		$this->mDescription = "Scrapes the WMF SVN repo for package meta-data and stores it so it can be queried via the API.";
 	}
 	
+	/**
+	 * @see Maintenance::execute
+	 * 
+	 * @since 0.1
+	 */
 	public function execute() {
 		$dbr = wfGetDB( DB_SLAVE );
 		
-		// TODO
+		$extensions = $this->getExtensionList();
 		
 		$this->output( "..." );
 	}
+	
+	/**
+	 * Goes through a local checkout of the svn repository and returns a list
+	 * with all found extensions. Based on getExtensionList from ExtensionDistributor.
+	 * 
+	 * @since 0.1
+	 * 
+	 * @return array
+	 */
+	protected function getExtensionList() {
+		global $wgExtDistWorkingCopy, $wgExtDistBranches;
+
+		if ( $this->extensionList !== false ) {
+			return $this->extensionList;
+		}
+
+		$this->extensionList = array();
+		
+		foreach ( $wgExtDistBranches as $branchPath => $branch ) {
+			$wc = "$wgExtDistWorkingCopy/$branchPath/extensions";
+			$dir = opendir( $wc );
+			
+			if ( !$dir ) {
+				return array();
+			}
+
+			$this->extensionList[$branchPath] = array();
+			
+			while ( false !== ( $file = readdir( $dir ) ) ) {
+				if ( substr( $file, 0, 1 ) == '.' 
+					|| !is_dir( "$wc/$file" )
+					|| file_exists( "$wc/$file/NO-DIST" ) ) {
+						continue;
+				}
+
+				$this->extensionList[$branchPath][] = $file;
+			}
+			
+			natcasesort( $this->extensionList[$branchPath] );
+		}
+		
+		return $this->extensionList;
+	}	
 	
 }
 
