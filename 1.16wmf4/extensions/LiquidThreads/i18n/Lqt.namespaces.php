d814ff3b18b8a5da2cbbfcdac419c70c995e55b0
werdna <werdna@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-17 10:31:15 +0000

    Merge in ability to localise namespace names
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@71196 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.16wmf4/extensions/LiquidThreads/i18n/Lqt.namespaces.php
@@ -0,0 +1,24 @@
+<?php
+
+$namespaceNames = array();
+
+$namespaceNames['en'] = array(
+	NS_LQT_THREAD       => 'Thread',
+	NS_LQT_THREAD_TALK  => 'Thread_talk',
+	NS_LQT_SUMMARY      => 'Summary',
+	NS_LQT_SUMMARY_TALK => 'Summary_talk',
+);
+
+$namespaceNames['fi'] = array(
+	NS_LQT_THREAD       => 'Viestiketju',
+	NS_LQT_THREAD_TALK  => 'Keskustelu_viestiketjusta',
+	NS_LQT_SUMMARY      => 'Yhteenveto',
+	NS_LQT_SUMMARY_TALK => 'Keskustelu_yhteenvedosta',
+);
+
+$namespaceNames['pt'] = array(
+	NS_LQT_THREAD => 'Tópico',
+	NS_LQT_THREAD_TALK => 'Tópico_discussão',
+	NS_LQT_SUMMARY => 'Resumo',
+	NS_LQT_SUMMARY_TALK => 'Resumo_discussão',
+);
