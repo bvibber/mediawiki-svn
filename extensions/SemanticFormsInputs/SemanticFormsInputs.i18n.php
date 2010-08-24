e954cce89eccdbb60cba8090db39bbf9fb95d9e6
yaron <yaron@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-24 14:49:25 +0000

    Removed 'today' and 'clear' messages entirely - apparently, they're never used
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@71561 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/SemanticFormsInputs/SemanticFormsInputs.i18n.php
+++ extensions/SemanticFormsInputs/SemanticFormsInputs.i18n.php
@@ -7,19 +7,13 @@ $messages = array();
 
 $messages['en'] = array(
 	'semanticformsinputs-desc' => 'Additional input types for [http://www.mediawiki.org/wiki/Extension:Semantic_Forms Semantic Forms].',
-	'today' => 'Today:', // @todo FIXME: Change to key 'semanticformsinputs-today'
-	'clear' => 'Clear', // @todo FIXME: Change to key 'semanticformsinputs-clear'
 	'semanticformsinputs-wrongformat' => 'Wrong format!'
 );
 
 $messages['de'] = array(
-	'today' => 'Heute:',
-	'clear' => 'Löschen',
 	'semanticformsinputs-wrongformat' => 'Falsches Format!'
 );
 
 $messages['fr'] = array(
-	'today' => 'Aujourd\'hui:',
-	'clear' => 'Supprimer',
 	'semanticformsinputs-wrongformat' => 'Format erroné!'
 );
