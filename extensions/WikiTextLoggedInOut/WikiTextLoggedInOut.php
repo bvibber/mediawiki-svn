aa8e45f0d4d33a05e8f170493a929452591495ac
ashley <ashley@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-26 15:33:29 +0000

    follow-up to r70046: now the extension works again, was spewing out errors about missing function or incorrect name. also bumped version number and tweaked the names of two functions as per http://www.mediawiki.org/wiki/Manual:Coding_conventions#Code_elements
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@71702 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/WikiTextLoggedInOut/WikiTextLoggedInOut.php
+++ extensions/WikiTextLoggedInOut/WikiTextLoggedInOut.php
@@ -7,10 +7,10 @@
  *
  * @file
  * @ingroup Extensions
+ * @version 1.3
  * @author Aaron Wright
  * @author David Pean
  * @author Jack Phoenix <jack@countervandalism.net>
- * @version 1.0
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  * @link http://www.mediawiki.org/wiki/Extension:WikiTextLoggedInOut Documentation
  */
@@ -19,7 +19,7 @@
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'WikiTextLoggedInOut',
-	'version' => '1.2',
+	'version' => '1.3',
 	'author' => array( 'Aaron Wright', 'David Pean', 'Jack Phoenix' ),
 	'url' => 'http://www.mediawiki.org/wiki/Extension:WikiTextLoggedInOut',
 	'descriptionmsg' => 'wikitextloggedinout-desc'
@@ -29,13 +29,13 @@ $dir = dirname( __FILE__ ) . '/';
 $wgExtensionMessagesFiles['WikiTextLoginInOut'] = $dir . 'WikiTextLoggedInOut.i18n.php';
 
 $wgHooks['ParserFirstCallInit'][] = 'efWikiTextLoggedInOut';
-function efWikiTextLoggedIn( &$parser ) {
-	$parser->setHook( 'loggedin', 'OutputLoggedInText' );
-	$parser->setHook( 'loggedout', 'OutputLoggedOutText' );
+function efWikiTextLoggedInOut( &$parser ) {
+	$parser->setHook( 'loggedin', 'outputLoggedInText' );
+	$parser->setHook( 'loggedout', 'outputLoggedOutText' );
 	return true;
 }
 
-function OutputLoggedInText( $input, $args, $parser, $frame ) {
+function outputLoggedInText( $input, $args, $parser, $frame ) {
 	global $wgUser;
 
 	if( $wgUser->isLoggedIn() ) {
@@ -45,7 +45,7 @@ function OutputLoggedInText( $input, $args, $parser, $frame ) {
 	return '';
 }
 
-function OutputLoggedOutText( $input, $args, $parser, $frame ) {
+function outputLoggedOutText( $input, $args, $parser, $frame ) {
 	global $wgUser;
 
 	if( !$wgUser->isLoggedIn() ) {
