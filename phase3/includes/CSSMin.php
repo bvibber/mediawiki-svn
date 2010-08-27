47ac621e97db17dd3a01069866e6b7ab5bd8be13
tparscal <tparscal@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-27 21:31:45 +0000

    Added CSSMin::getLocalFileReferences
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/resourceloader@71810 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- phase3/includes/CSSMin.php
+++ phase3/includes/CSSMin.php
@@ -14,50 +14,67 @@ class CSSMin {
 	
 	/* Static Methods */
 	
-	/*
+	/**
+	 * Gets a list of local file paths which are referenced in a CSS style sheet
+	 * 
+	 * @param $source string CSS data to remap
+	 * @param $path string File path where the source was read from
+	 * @return array List of local file references
+	 */
+	public static function getLocalFileReferences( $source, $path ) {
+		$pattern = '/url\([\'"]?(?<file>[^\?\)\:]*)\??[^\)]*[\'"]?\)/';
+		$files = array();
+		if ( preg_match_all( $pattern, $source, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER ) ) {
+			foreach ( $matches as $match ) {
+				$file = "{$path}/{$match['file'][0]}";
+				// Only proceed if we can access the file
+				if ( file_exists( $file ) ) {
+					$files[] = $file;
+				}
+			}
+		}
+		return $files;
+	}
+	
+	/**
 	 * Remaps CSS URL paths and automatically embeds data URIs for URL rules preceded by an /* @embed * / comment
 	 * 
 	 * @param $source string CSS data to remap
 	 * @param $path string File path where the source was read from
+	 * @return string Remapped CSS data
 	 */
 	public static function remap( $source, $path ) {
-		$pattern = '/((?<embed>\s*\/\*\s*\@embed\s*\*\/)(?<rule>[^\;\}]*))?url\((?<file>[^)]*)\)(?<extra>[^;]*)[\;]?/';
+		$pattern = '/((?<embed>\s*\/\*\s*\@embed\s*\*\/)(?<rule>[^\;\}]*))?url\([\'"]?(?<file>[^\?\)\:]*)\??[^\)]*[\'"]?\)(?<extra>[^;]*)[\;]?/';
 		$offset = 0;
 		while ( preg_match( $pattern, $source, $match, PREG_OFFSET_CAPTURE, $offset ) ) {
-			// Remove single or double quotes
-			$url = trim( $match['file'][0], "'\"" );
-			// Only proceed if the URL is to a local file
-			if ( !preg_match( '/^[a-zA-Z]*\:\/\//', $url ) ) {
-				// Shortcuts
-				$embed = $match['embed'][0];
-				$rule = $match['rule'][0];
-				$extra = $match['extra'][0];
-				// Strip query string from URL
-				$file = "{$path}/" . preg_replace( '/([^\?]*)(.*)/', '$1', $url );
-				// Only proceed if we can access the file
-				if ( file_exists( $file ) ) {
-					// Add version parameter as a time-stamp in ISO 8601 format, using Z for the timezone, meaning GMT
-					$url = "{$file}?" . gmdate( 'Y-m-d\TH:i:s\Z', round( filemtime( $file ), -2 ) );
-					// Detect when URLs were preceeded with embed tags, and also verify file size is below the limit
-					if ( $match['embed'][1] > 0 && filesize( $file ) < self::EMBED_SIZE_LIMIT ) {
-						// If we ever get to PHP 5.3, we should use the Fileinfo extension instead of mime_content_type
-						$type = mime_content_type( $file );
-						// Strip off any trailing = symbols (makes browsers freak out)
-						$data = rtrim( base64_encode( file_get_contents( $file ) ), '=' );
-						// Build 2 CSS properties; one which uses a base64 encoded data URI in place of the @embed
-						// comment to try and retain line-number integrity , and the other with a remapped an versioned
-						// URL and an Internet Explorer hack making it ignored in all browsers that support data URIs
-						$replacement = "{$rule}url(data:{$type};base64,{$data}){$extra};{$rule}url({$url}){$extra}!ie;";
-					} else {
-						// Build a CSS property with a remapped and versioned URL
-						$replacement = "{$embed}{$rule}url({$url}){$extra};";
-					}
-					// Perform replacement on the source
-					$source = substr_replace( $source, $replacement, $match[0][1], strlen( $match[0][0] ) );
-					// Move the offset to the end of the replacement in the source
-					$offset = $match[0][1] + strlen( $replacement );
-					continue;
+			// Shortcuts
+			$embed = $match['embed'][0];
+			$rule = $match['rule'][0];
+			$extra = $match['extra'][0];
+			$file = "{$path}/{$match['file'][0]}";
+			// Only proceed if we can access the file
+			if ( file_exists( $file ) ) {
+				// Add version parameter as a time-stamp in ISO 8601 format, using Z for the timezone, meaning GMT
+				$url = "{$file}?" . gmdate( 'Y-m-d\TH:i:s\Z', round( filemtime( $file ), -2 ) );
+				// Detect when URLs were preceeded with embed tags, and also verify file size is below the limit
+				if ( $match['embed'][1] > 0 && filesize( $file ) < self::EMBED_SIZE_LIMIT ) {
+					// If we ever get to PHP 5.3, we should use the Fileinfo extension instead of mime_content_type
+					$type = mime_content_type( $file );
+					// Strip off any trailing = symbols (makes browsers freak out)
+					$data = rtrim( base64_encode( file_get_contents( $file ) ), '=' );
+					// Build 2 CSS properties; one which uses a base64 encoded data URI in place of the @embed
+					// comment to try and retain line-number integrity , and the other with a remapped an versioned
+					// URL and an Internet Explorer hack making it ignored in all browsers that support data URIs
+					$replacement = "{$rule}url(data:{$type};base64,{$data}){$extra};{$rule}url({$url}){$extra}!ie;";
+				} else {
+					// Build a CSS property with a remapped and versioned URL
+					$replacement = "{$embed}{$rule}url({$url}){$extra};";
 				}
+				// Perform replacement on the source
+				$source = substr_replace( $source, $replacement, $match[0][1], strlen( $match[0][0] ) );
+				// Move the offset to the end of the replacement in the source
+				$offset = $match[0][1] + strlen( $replacement );
+				continue;
 			}
 			// Move the offset to the end of the match, leaving it alone
 			$offset = $match[0][1] + strlen( $match[0][0] );
@@ -65,10 +82,11 @@ class CSSMin {
 		return $source;
 	}
 	
-	/*
+	/**
 	 * Removes whitespace from CSS data
 	 * 
 	 * @param $source string CSS data to minify
+	 * @return string Minified CSS data 
 	 */
 	public static function minify( $css ) {
 		return trim(
