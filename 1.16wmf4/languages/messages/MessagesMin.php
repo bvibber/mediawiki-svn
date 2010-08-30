f38601c3a625ca2e8c9f7d3e8b7a6a79e48abbdc
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-30 21:07:43 +0000

    MFT r71926: add support for 'min'.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/wmf@71979 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ 1.16wmf4/languages/messages/MessagesMin.php
@@ -0,0 +1,17 @@
+<?php
+/** Minangkabau (Baso Minangkabau)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Naval Scene
+ * @author VoteITP
+ */
+
+$fallback = 'id';
+
+$messages = array(
+);
