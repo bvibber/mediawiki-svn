3a49b0c80a5ddd4088d4e4791f07be6384e25451
adam <adam@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-08-31 00:11:49 +0000

    End of the day ArticleAssessment commit. Progress was made. True story.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@71995 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleAssessmentPilot/js/ArticleAssessment.js
+++ extensions/ArticleAssessmentPilot/js/ArticleAssessment.js
@@ -18,7 +18,7 @@
 			'fieldPrefix': 'articleassessment-rating-',
 			'fieldHTML': '<div class="field-wrapper"> \
 				<label for="rating_{FIELD}" original-title="{HINT}" class="rating-field-label">{LABEL}</label> \
-				<select id="rating_{FIELD}" name="rating[{FIELD}]" class="rating-field"> \
+				<select id="rating_{FIELD}" name="rating{FIELD}" class="rating-field"> \
 					<option value="1">1</option> \
 					<option value="2">2</option> \
 					<option value="3">3</option> \
@@ -48,7 +48,7 @@
 					</fieldset> \
 				</form> \
 			</div>',
-			'ratingHTML': '<div class="article-assessment-rating"> \
+			'ratingHTML': '<div class="article-assessment-rating" id="articleassessment-rating-{FIELD}"> \
 					<span class="article-assessment-rating-field-name">{LABEL}</span> \
 					<span class="article-assessment-rating-field-value-wrapper"> \
 						<span class="article-assessment-rating-field-value">{VALUE}</span> \
@@ -76,24 +76,63 @@
 					.replace( /\{FEEDBACK\}/g,  mw.usability.getMsg('articleassessment-featurefeedback') )
 					.replace( /\{YOURFEEDBACK\}/g,  mw.usability.getMsg('articleassessment-yourfeedback') )
 					.replace( /\{ARTICLERATING\}/g,  mw.usability.getMsg('articleassessment-articlerating' ) ) 
-					.replace( /\{RESULTSHIDE\}/g,  mw.usability.getMsg('articleassessment-results-hide' ) ) 
-					.replace( /\{RESULTSSHOW\}/g,  mw.usability.getMsg('articleassessment-results-show' ) ) );
+					.replace( /\{RESULTSHIDE\}/g,  mw.usability.getMsg('articleassessment-results-hide' )
+						.replace( /\[\[\|([^\]]*)\]\]/, '<a href="#">$1</a>' ) ) 
+					.replace( /\{RESULTSSHOW\}/g,  mw.usability.getMsg('articleassessment-results-show' )
+						.replace( /\[\[\|([^\]]*)\]\]/, '<a href="#">$1</a>' ) ) );
 				for( var field in settings.fieldMessages ) { 
 					$output.find( '.article-assessment-rating-fields' )
 						.append( $( settings.fieldHTML
 							.replace( /\{LABEL\}/g, mw.usability.getMsg( settings.fieldPrefix + settings.fieldMessages[field] ) )
-							.replace( /\{FIELD\}/g, mw.usability.getMsg( settings.fieldMessages[field] ) )
+							.replace( /\{FIELD\}/g, "[" + settings.fieldMessages[field] + "]" )
 							.replace( /\{HINT\}/g, mw.usability.getMsg( settings.fieldPrefix + settings.fieldMessages[field] + settings.fieldHintSuffix ) ) ) );
 					$output.find( '#article-assessment-ratings' )
 						.append( $( settings.ratingHTML
 							.replace( /\{LABEL\}/g, mw.usability.getMsg(settings.fieldPrefix + settings.fieldMessages[field]) )
+							.replace( /\{FIELD\}/g, settings.fieldMessages[field] )
 							.replace( /\{VALUE\}/g, '0%' ) 
 							.replace( /\{COUNT\}/g, mw.usability.getMsg( 'field-count' ) ) ) 
 							);
 				}
-				
+				$output.find( '#article-assessment' ).data( 'articleAssessment-context', { 'settings': settings });
+				// hook up the ratings show/hide
+				$output
+					.find( '.article-assessment-show-ratings a' )
+					.click( function() {
+						$( this )
+							.parent()
+							.hide();
+						$output
+							.find( '#article-assessment-ratings' )
+							.removeClass( 'article-assessment-ratings-disabled' )
+							.end()
+							.find( '.article-assessment-hide-ratings' )
+							.show();
+							return false;
+					} )
+					.end()
+					.find( '.article-assessment-hide-ratings a' )
+					.click( function() {
+						$( this )
+							.parent()
+							.hide();
+						$output
+							.find( '#article-assessment-ratings' )
+							.addClass( 'article-assessment-ratings-disabled' )
+							.end()
+							.find( '.article-assessment-show-ratings' )
+							.show();
+							return false;
+					} )
+					.click();
 				$( '#catlinks' ).before( $output );
 				
+				// set the height of our smaller fieldset to match the taller
+				if( $( '#article-assessment-rate' ).height() > $( '#article-assessment-ratings' ).height() ) {
+					$( '#article-assessment-ratings' ).css( 'minHeight',  $( '#article-assessment-rate' ).height() );
+				} else {
+					$( '#article-assessment-rate' ).css( 'minHeight',  $( '#article-assessment-ratings' ).height() );
+				}
 				// attempt to fetch the ratings 
 				$.ArticleAssessment.fn.getRatingData();
 				
@@ -115,6 +154,51 @@
 							}
 						 } );
 				});
+				// intialize the tooltips
+				$( '.field-wrapper label[original-title]' ).each(function() {
+					$( this )
+						.after( $( '<span class="rating-field-hint" />' )
+							.attr( 'original-title', $( this ).attr( 'original-title' ) )
+							.tipsy( { gravity : 'se', opacity: '0.9',  } ) );
+				} );
+				// bind submit event to the form
+				$( '#article-assessment' ).submit( function() { $.ArticleAssessment.fn.submitRating(); return false; } );
+				// prevent the submit button for being active until all ratings are filled out
+				
+			},
+			'getRatingData': function() {
+				var request = $.ajax( {
+					url: wgScriptPath + '/api.php',
+					data: {
+						'action': 'query',
+						'list': 'articleassessment',
+						'aarevid': wgCurRevisionId,
+						'aapageid': wgArticleId,
+						'format': 'json'
+					},
+					dataType: 'json',
+					success: function( data ) {
+						$.ArticleAssessment.fn.afterGetRatingData( data );
+					},
+					error: function(XMLHttpRequest, textStatus, errorThrown) {
+						// console.log(XMLHttpRequest, textStatus, errorThrown);
+					}
+				} );
+			},
+			'afterGetRatingData' : function( data ) {
+				var settings = $( '#article-assessment' ).data( 'articleAssessment-context' ).settings;
+				// add the correct data to the markup
+				for( rating in data.query.articleassessment[0].ratings) {
+					var rating = data.query.articleassessment[0].ratings[rating],
+						$rating = $( '#' + rating.ratingdesc ),
+						label = mw.usability.getMsg( 'articleassessment-noratings', [rating.total, rating.count] );
+					$rating
+						.find( '.article-assessment-rating-field-value' )
+						.text( rating.total )
+						.end()
+						.find( '.article-assessment-rating-count' )
+						.text( label );
+				}
 				// if the rating is stale, add the stale class
 				if( true /* replace with conditional based on returned results of past user ratings */ ) {
 					// add the stale star class to each on star
@@ -124,45 +208,16 @@
 					$( '.article-assessment-submit' )
 						.append( settings.staleMSG.replace( /\{MSG\}/g, mw.usability.getMsg( 'articleassessment-stalemessage-revisioncount' ) ) );
 				}
-				// intialize the tooltips
-				$( '.field-wrapper label[original-title]' ).each(function() {
-					$( this )
-						.after( $( '<span class="rating-field-hint" />' )
-							.attr( 'original-title', $( this ).attr( 'original-title' ) )
-							.tipsy( { gravity : 'se', opacity: '0.9',  } ) );
-				} );
 				// initialize the ratings 
 				$( '.article-assessment-rating-field-value' ).each( function() {
 					$( this )
 						.css( {
-							'width': 120 - ( 120 * ( parseInt( $( this ).text() ) / 100 ) ) + "px"
+							'width': 120 - ( 120 * ( parseFloat( $( this ).text() ) / 5 ) ) + "px"
 						} )
-				} );
-				// bind submit event to the form
-				
-				// prevent the submit button for being active until all ratings are filled out
-				
-			},
-			'getRatingData': function() {
-				var request = $j.ajax( {
-					url: wgScriptPath + '/api.php',
-					data: {
-						'action': 'articleassessment',
-						'getCumulativeResults': 1, 
-						'pageId': wgArticleId,
-						'revId': wgCurRevisionId
-					},
-					dataType: 'json',
-					success: function( data ) {
-						console.log(data);
-					},
-					error: function(XMLHttpRequest, textStatus, errorThrown) {
-						console.log(XMLHttpRequest, textStatus, errorThrown);
-					}
 				} );
 			},
 			'getUserRatingData': function() {
-				var request = $j.ajax( {
+				var request = $.ajax( {
 					url: wgScriptPath + '/api.php',
 					data: {
 						'action': 'articleassessment',
@@ -176,21 +231,34 @@
 						console.log(data);
 					},
 					error: function(XMLHttpRequest, textStatus, errorThrown) {
-						console.log(XMLHttpRequest, textStatus, errorThrown);
+						// console.log( XMLHttpRequest, textStatus, errorThrown );
 					}
 				} );
 			},
-			'submit': function() {
+			'submitRating': function() {
 				// clear out the stale message
-				var request = $j.ajax( {
+				var results = {};
+				$( '.rating-field input' ).each( function() {
+					// expects the hidden inputs to have names like 'rating[field-name]' which we use to
+					// be transparent about what values we're sending to the server
+					var fieldName = $( this ).attr('name').match(/\[([a-zA-Z0-9\-]*)\]/)[1];
+					results[ fieldName ] = $( this ).val();
+				} );
+				var request = $.ajax( {
 					url: wgScriptPath + '/api.php',
 					data: {
-						'action': 'ratearticle',
-						'data': ratingsData
+						'action': 'articleassessment',
+						'aarevid': wgCurRevisionId,
+						'aapageid': wgArticleId,
+						'aar1' : results['wellsourced'],
+						'aar2' : results['neutrality'],
+						'aar3' : results['completeness'],
+						'aar4' : results['readability'],
+						'format': 'json'
 					},
 					dataType: 'json',
 					success: function( data ) {
-						$this.suggestions( 'suggestions', data[1] );
+						console.log(data);
 					}
 				} );
 			}
@@ -198,7 +266,6 @@
 	};
 	// FIXME - this should be moved out of here
 	$( document ).ready( function () {
-		console.log(this);
-		$.ArticleAssessment.fn.init( { 'endpoint': wgScriptPath + "/api.php" } );
+		$.ArticleAssessment.fn.init( );
 	} ); //document ready
 } )( jQuery );
