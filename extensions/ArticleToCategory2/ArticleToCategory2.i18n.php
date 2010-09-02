33754be9c65f04f745cbf7b8b705cc232bffe3d3
mikaellindmark <mikaellindmark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-02 16:43:12 +0000

    First check in.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@72201 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/ArticleToCategory2/ArticleToCategory2.i18n.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Internationalisation file for the Add Article to Category 2 extension 
+ *
+ * @file
+ * @addtogroup Extensions
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0 or later
+*/
+ 
+$messages = array();
+
+/** Dutch
+ * */
+$messages['nl'] = array(
+	'articletocategory2-desc'               => 'Zet een inputbox op elke categorie pagina, en u kunt direct een nieuw artikel te cre&euml;ren om die categorie',
+	'create-article-under-category-Text' 	=> 'Maak een artikel voor deze categorie',
+	'create-article-under-category-Button'	=> 'Aanmaken',
+	'create-category-under-category-Text'	=> 'Maak een subcategorie voor deze categorie',
+	'create-category-under-category-Button'	=> 'Aanmanen',
+);
+
+ 
+/** English
+ */
+$messages['en'] = array(
+	'articletocategory2-desc'		=> 'Puts an inputbox on each category page, and you can create a new article directly to that category',
+	'create-article-under-category-Text'	=> 'Add an article to this category',
+	'create-article-under-category-Button'	=> 'Create',
+	'create-category-under-category-Text'	=> 'Add a category to this category',
+        'create-category-under-category-Button'	=> 'Create',
+);
+
+/** German 
+ */
+$messages['de'] = array(
+	'articletocategory2-desc'		=> 'F&uuml;gt ein Eingabefeld auf jeder Seite der Kategorie, so k&ouml;nnen Sie einen Artikel erstellen, die direkt in dieser Kategorie',
+        'create-article-under-category-Text'	=> 'Ein Artikel zu dieser Kategorie hinzuf&uuml;gen',
+        'create-article-under-category-Button '	=> 'Erstellen',
+        'create-category-under-category-Text'	=> 'Eine Kategorie zu dieser Kategorie hinzuf&uuml;gen',
+        'create-category-under-category-Button'	=> 'Erstellen',
+);
+
+/** Swedish
+ */
+$messages['sv'] = array(
+	'articletocategory2-desc'		=> 'L&auml;gger ett inmatningsf&auml;lt p&aring; varje kategorisida, s&aring; att du kan skapa en artikel direkt i denna kategori',
+        'create-article-under-category-Text'	=> 'L&auml;gg till artikel till denna kategori',
+        'create-article-under-category-Button'	=> 'Skapa',
+	'create-category-under-category-Text'	=> 'L&auml;gg till kategori till denna kategori',
+        'create-category-under-category-Button'	=> 'Skapa',
+);
