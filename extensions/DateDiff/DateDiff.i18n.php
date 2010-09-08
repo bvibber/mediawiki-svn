dce880a20ed282155da52d9e40b6a2f463582e3b
jeroendedauw <jeroendedauw@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-08 18:13:26 +0000

    Cleaned up a bit and added i18n file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@72598 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/DateDiff/DateDiff.i18n.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Internationalization file for the DateDiff extension.
+ *
+ * @file DateDiff.i18n.php
+ *
+ * @author Jeroen De Dauw
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'datediff_desc' => 'Takes two dates and returns all intermediary days'
+);
--- extensions/DateDiff/DateDiff.php
+++ extensions/DateDiff/DateDiff.php
@@ -1,59 +1,78 @@
-<?php 
-//http://www.mediawiki.org/wiki/Manual:Extensions#Writing_Extensions
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
+<?php
 
+/**
+ * DateDiff extension.
+ *
+ * On MediaWiki.org: 		http://www.mediawiki.org/wiki/Extension:DateDiff
+ *
+ * @file DateDiff.php
+ *
+ * @author David Raison
+ * @author Jeroen De Dauw
+ */
 
-if( !defined( 'MEDIAWIKI' ) ) {
-        die( 'Not an entry point.' );
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
 }
 
+define( 'Datediff_VERSION', '0.1' );
+
+$wgExtensionMessagesFiles['DateDiff'] = dirname( __FILE__ ) . '/DateDiff.i18n.php';
 $wgExtensionFunctions[] = "efDateDiff";
 $wgHooks['LanguageGetMagic'][] = 'efDatesFunctionMagic';
 
- 
 // Extension credits that show up on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
-        'name' => 'Datediff',
-        'author' => 'David Raison',
-        'url' => 'http://www.hackerspace.lu',
-        'description' => 'Takes two dates and returns all intermediary days'
+    'name' => 'Datediff',
+    'author' => 'David Raison',
+    'url' => 'http://www.mediawiki.org/wiki/Extension:DateDiff',
+	'path' => __FILE__,
+    'descriptionmsg' => 'datediff_desc',
+	'version' => Datediff_VERSION,
 );
 
-function efDateDiff(){
-    global $wgParser; 
-    $wgParser->setFunctionHook('dates', 'calcdates');
+function efDateDiff() {
+    global $wgParser;
+    $wgParser->setFunctionHook( 'dates', 'calcdates' );
 }
 
 /**
  * Adds the magic words for the parser functions
  */
 function efDatesFunctionMagic( &$magicWords, $langCode ) {
-        $magicWords['dates'] = array( 0, 'dates' );
+    $magicWords['dates'] = array( 0, 'dates' );
 	return true;
 }
 
-function calcdates(&$parser){
+function calcdates( &$parser ) {
 	$params = func_get_args();
-        array_shift( $params ); // We already know the $parser ...
-	while(empty($params[0])) array_shift($params);	// quite common
+    array_shift( $params ); // We already know the $parser ...
+    
+	while ( empty( $params[0] ) ) {
+		array_shift( $params );
+	}
 
 	$dates = array();
-	foreach($params as $pair)
-		$dates[] = substr($pair,strpos($pair,'=')+1);	// we currently ignore the label of the date
+	
+	foreach ( $params as $pair ) {
+		// We currently ignore the label of the date.
+		$dates[] = substr( $pair, strpos( $pair, '=' ) + 1 );
+	}
+		
 
-	$time1 = strtotime($dates[0]);
-	$time2 = strtotime($dates[1]);
+	$time1 = strtotime( $dates[0] );
+	$time2 = strtotime( $dates[1] );
 
-	$a = ($time2 > $time1) ? $time2 : $time1;       // higher
-	$b = ($a == $time1) ? $time2 : $time1;          // lower
+	$a = ( $time2 > $time1 ) ? $time2 : $time1;       // higher
+	$b = ( $a == $time1 ) ? $time2 : $time1;          // lower
 	$datediff = $a - $b;
 
 	$oneday = 86400;
 	$days = array();
-	for($i=0;$i <= $datediff; $i+=$oneday){
-		$days[] = date('c',strtotime($dates[0])+$i);
+	
+	for ( $i = 0; $i <= $datediff; $i += $oneday ) {
+		$days[] = date( 'c', strtotime( $dates[0] ) + $i );
 	}
-	return implode(',',$days);
-
+	
+	return implode( ',', $days );
 }
