9f34386dee2652809eabdffadea256b92d38d48b
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-12 15:07:12 +0000

    Tweak messages a bit for consistency Add extension to Translatewiki.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@72863 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleAssessmentPilot/ArticleAssessmentPilot.i18n.php
+++ extensions/ArticleAssessmentPilot/ArticleAssessmentPilot.i18n.php
@@ -10,7 +10,7 @@ $messages['en'] = array(
 	'articleassessment' => 'Article assessment',
 	'articleassessment-desc' => 'Article assessment pilot version',
 	'articleassessment-yourfeedback' => 'Your feedback',
-	'articleassessment-pleaserate' => "Please take a moment to rate this page below.",
+	'articleassessment-pleaserate' => 'Please take a moment to rate this page below.',
 	'articleassessment-submit' => 'Submit',
 	'articleassessment-rating-wellsourced' => 'Well-Sourced:',
 	'articleassessment-rating-neutrality' => 'Neutral:',
@@ -21,20 +21,24 @@ $messages['en'] = array(
 	'articleassessment-rating-completeness-tooltip' => 'Do you feel that this article covers the essential topic areas that it should?',
 	'articleassessment-rating-readability-tooltip' => 'Do you feel that this article is well-organized and well written?',
 	'articleassessment-articlerating' => 'Article rating',
-	'articleassessment-error' => "We're sorry! An error has occurred. Please try again later.",
+	'articleassessment-error' => 'An error has occurred.
+Please try again later.',
 	'articleassessment-thanks' => 'Thanks! Your ratings have been saved.',
 
 	# This special page doesn't exist yet, but it will soon.
 	'articleassessment-featurefeedback' => 'Give us [[Special:Article Assessment Feedback|feedback]] about this feature.',
 	'articleassessment-noratings' => '$1 ($2 {{PLURAL:$2|rating|ratings}})',
 	# Some of these messages are unused in the code as it is but may be in the future depending on performance.  They should still be translated.
-	'articleassessment-stalemessage-revisioncount' => "This article has been ''revised'' '''$1 times''' since you last reviewed it. You may wish to re-rate it.",
-	'articleassessment-stalemessage-greaterthancount' => "This article has been ''revised'' more than $1 times since you last reviewed it. You may wish to re-rate it.",
-	'articleassessment-stalemessage-norevisioncount' => "This article has been ''revised'' since you last reviewed it. You may wish to re-rate it.",
+	'articleassessment-stalemessage-revisioncount' => "This article has been ''revised'' '''$1 times''' since you last reviewed it.
+You may wish to re-rate it.",
+	'articleassessment-stalemessage-greaterthancount' => "This article has been ''revised'' more than $1 times since you last reviewed it.
+You may wish to re-rate it.",
+	'articleassessment-stalemessage-norevisioncount' => "This article has been ''revised'' since you last reviewed it.
+You may wish to re-rate it.",
 
 	# Links get rewritten in javascript.
 	'articleassessment-results-show' => "(Results hidden. [[|Show]] them.)",
-	'articleassessment-results-hide' => "([[|Hide Results]])",
+	'articleassessment-results-hide' => "([[|Hide results]])",
 );
 
 /** Message documentation (Message documentation)
@@ -42,8 +46,10 @@ $messages['en'] = array(
  * @author Brandon Harris
  */
 $messages['qqq'] = array(
-	'articleassessment' => 'The title of the feature. It is about reader feedback.',
-	'articleassessment-desc' => 'A description of the purpose of this feature',
+	'articleassessment' => 'The title of the feature. It is about reader feedback.
+	
+Please visit http://prototype.wikimedia.org/articleassess/Main_Page for a prototype installation.',
+	'articleassessment-desc' => '{{desc}}',
 	'articleassessment-yourfeedback' => 'This is a box or section header.  It indicates that the contents of the box are personal to the user.',
 	'articleassessment-pleaserate' => 'This is a call to action for the user to provide their ratings about the page.',
 	'articleassessment-submit' => 'This is for when the user submits their ratings to the server.',
@@ -64,6 +70,9 @@ $messages['qqq'] = array(
 	'articleassessment-stalemessage-revisioncount' => 'This is a message shown to the user when their ratings are "stale" and includes the number of revisions since.',
 	'articleassessment-stalemessage-greaterthancount' => 'This is a message shown to the user when their ratings are "stale". It allows for not looking up the total number of revisions.',
 	'articleassessment-stalemessage-norevisioncount' => 'This is a message shown to the user when their ratings are "stale" and does NOT include the number of revisions. This is an ambiguous reason, and allows for us to have complicated staleness patterns. This is the preferred message.',
-	'articleassessment-results-show' => 'This is an explanatory control that, when clicked, will display hidden aggregate ratings.',
-	'articleassessment-results-hide' => 'This is a control that, when clicked, will hide the aggregate ratings.',
+	'articleassessment-results-show' => 'This is an explanatory control that, when clicked, will display hidden aggregate ratings.
+The incomplete looking wikilinks get rewritten in javascript.',
+	'articleassessment-results-hide' => 'This is a control that, when clicked, will hide the aggregate ratings.
+The incomplete looking wikilinks get rewritten in javascript.',
 );
+
