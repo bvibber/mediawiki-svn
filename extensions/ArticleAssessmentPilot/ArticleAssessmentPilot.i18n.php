e211b59027860c47452d7a15f8584bbca454cc6e
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-13 22:41:57 +0000

    Use page instead of article.
    I'd even suggest to call the extension page assessment. Very Wikipedia centric now, and this extension does not only allow articles to be rated but all MediaWiki pages, whatever the content may be.
    
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@72937 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/ArticleAssessmentPilot/ArticleAssessmentPilot.i18n.php
+++ extensions/ArticleAssessmentPilot/ArticleAssessmentPilot.i18n.php
@@ -16,20 +16,20 @@ $messages['en'] = array(
 	'articleassessment-rating-neutrality' => 'Neutral:',
 	'articleassessment-rating-completeness' => 'Complete:',
 	'articleassessment-rating-readability' => 'Readable:',
-	'articleassessment-rating-wellsourced-tooltip' => 'Do you feel this article has sufficient citations and that those citations come from trustworthy sources?',
-	'articleassessment-rating-neutrality-tooltip' => 'Do you feel that this article shows a fair representation of all perspectives on the issue?',
-	'articleassessment-rating-completeness-tooltip' => 'Do you feel that this article covers the essential topic areas that it should?',
-	'articleassessment-rating-readability-tooltip' => 'Do you feel that this article is well-organized and well written?',
-	'articleassessment-articlerating' => 'Article rating',
+	'articleassessment-rating-wellsourced-tooltip' => 'Do you feel this page has sufficient citations and that those citations come from trustworthy sources?',
+	'articleassessment-rating-neutrality-tooltip' => 'Do you feel that this page shows a fair representation of all perspectives on the issue?',
+	'articleassessment-rating-completeness-tooltip' => 'Do you feel that this page covers the essential topic areas that it should?',
+	'articleassessment-rating-readability-tooltip' => 'Do you feel that this page is well-organized and well written?',
+	'articleassessment-articlerating' => 'Page rating',
 	'articleassessment-error' => 'An error has occurred.
 Please try again later.',
 	'articleassessment-thanks' => 'Thanks! Your ratings have been saved.',
 
 	# This special page doesn't exist yet, but it will soon.
-	'articleassessment-featurefeedback' => 'Give us [[Special:Article Assessment Feedback|feedback]] about this feature.',
+	'articleassessment-featurefeedback' => 'Give us [[Special:ArticleAssessmentFeedback|feedback]] about this feature.',
 	'articleassessment-noratings' => '$1 ($2 {{PLURAL:$2|rating|ratings}})',
-	'articleassessment-stalemessage-norevisioncount' => "This article has been ''revised'' since you last reviewed it.
-You may wish to re-rate it.",
+	'articleassessment-stalemessage-norevisioncount' => "This page has been ''revised'' since you last reviewed it.
+You may wish to rate it again.",
 
 	# Links get rewritten in javascript.
 	'articleassessment-results-show' => "(Results hidden. [[#|Show]] them.)",
