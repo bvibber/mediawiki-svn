b1745ff1fefb538736cb8f406f64fb2572842b94
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-09-19 15:10:22 +0000

    Use $specialPageAliases and keep backwards compatibility with MediaWiki 1.15 and earlier.
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@73324 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/PackageForce/PackageForce.alias.php
+++ extensions/PackageForce/PackageForce.alias.php
@@ -1,8 +1,19 @@
 <?php
-$aliases = array();
- 
-/** English */
-$aliases['en'] = array(
-    'PackageForce' => array( 'PackageForce' ),
-    'PackageForceAdmin' => array( 'PackageForceAdmin' ),
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+	'PackageForce' => array( 'PackageForce' ),
+	'PackageForceAdmin' => array( 'PackageForceAdmin' ),
 );
+
+/**
+ * For backwards compatibility with MediaWiki 1.15 and earlier.
+ */
+$aliases =& $specialPageAliases;
