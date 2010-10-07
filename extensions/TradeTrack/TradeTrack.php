fc6439f95d3a66e194569769f8568443cb1a21fb
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-07 07:24:51 +0000

    Add defintion for the special page Add special page alias file
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@74415 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/TradeTrack/TradeTrack.php
+++ extensions/TradeTrack/TradeTrack.php
@@ -14,15 +14,20 @@ $wgSpecialPages['TradeTrack'] = 'SpecialTradeTrack';
 $wgTradeTrackEmailSubject = "A new Trademark Request has Arrived";
 $wgTradeTrackFromEmail = "tradetrack@wikimedia.org";
 
-$wgAutoloadClasses['SpecialTradeTrack'] = dirname(__FILE__) . "/SpecialTradeTrack.php";
-$wgAutoloadClasses['TradeTrackScreen'] = dirname(__FILE__) . "/templates/TradeTrackScreen.php";
-$wgAutoloadClasses['TradeTrackScreenDetailsForm'] = dirname(__FILE__) . "/templates/TradeTrackScreenDetailsForm.php";
-$wgAutoloadClasses['TradeTrackScreenNonComAgreement'] = dirname(__FILE__) . "/templates/TradeTrackScreenNonComAgreement.php";
-$wgAutoloadClasses['TradeTrackScreenRouting'] = dirname(__FILE__) . "/templates/TradeTrackScreenRouting.php";
-$wgAutoloadClasses['TradeTrackScreenThanks'] = dirname(__FILE__) . "/templates/TradeTrackScreenThanks.php";
-$wgAutoloadClasses['TradeTrackEmail'] = dirname(__FILE__) . "/templates/TradeTrackEmail.php";
+$dir = dirname(__FILE__);
+$wgAutoloadClasses['SpecialTradeTrack'] = $dir . "/SpecialTradeTrack.php";
+$wgAutoloadClasses['TradeTrackScreen'] = $dir . "/templates/TradeTrackScreen.php";
+$wgAutoloadClasses['TradeTrackScreenDetailsForm'] = $dir . "/templates/TradeTrackScreenDetailsForm.php";
+$wgAutoloadClasses['TradeTrackScreenNonComAgreement'] = $dir . "/templates/TradeTrackScreenNonComAgreement.php";
+$wgAutoloadClasses['TradeTrackScreenRouting'] = $dir . "/templates/TradeTrackScreenRouting.php";
+$wgAutoloadClasses['TradeTrackScreenThanks'] = $dir . "/templates/TradeTrackScreenThanks.php";
+$wgAutoloadClasses['TradeTrackEmail'] = $dir . "/templates/TradeTrackEmail.php";
 
-$wgExtensionMessagesFiles['TradeTrack'] = dirname( __FILE__ ) . "/TradeTrack.i18n.php";
+$wgSpecialPages['TradeTrack'] = 'SpecialTradeTrack';
+$wgSpecialPageGroups['TradeTrack'] = 'other';
+
+$wgExtensionMessagesFiles['TradeTrack'] = $dir . "/TradeTrack.i18n.php";
+$wgExtensionAliasesFiles['TradeTrack'] = $dir . "/TradeTrack.alias.php";
 
 $wgTradeTrackEmailCommercial = "bharris@wikimedia.org";  // Who gets commercial requests (Kul)
 $wgTradeTrackEmailNonCommercial = "bharris@wikimedia.org"; // Who gets non-commercial requests (Mike)
