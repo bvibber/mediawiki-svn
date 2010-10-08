bvibber/mediawiki-svn

commit 782bd4cea0376b4463c0f07fde9adc6735462b55

Made ResourceLoader an instantiable object, rather than a static one, making it more modular and testable.
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@73971 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/MessageBlobStore.php
@@ -31,7 +31,7 @@
 class MessageBlobStore {
 	/**
 	 * Get the message blobs for a set of modules
-	 * @param $modules array Array of module names
+	 * @param $modules array Array of module objects keyed by module name
 	 * @param $lang string Language code
 	 * @return array An array mapping module names to message blobs
 	 */
@@ -43,14 +43,14 @@ class MessageBlobStore {
 			return array();
 		}
 		// Try getting from the DB first
-		$blobs = self::getFromDB( $modules, $lang );
+		$blobs = self::getFromDB( array_keys( $modules ), $lang );
 
 		// Generate blobs for any missing modules and store them in the DB
-		$missing = array_diff( $modules, array_keys( $blobs ) );
-		foreach ( $missing as $module ) {
-			$blob = self::insertMessageBlob( $module, $lang );
+		$missing = array_diff( array_keys( $modules ), array_keys( $blobs ) );
+		foreach ( $missing as $name ) {
+			$blob = self::insertMessageBlob( $name, $modules[$name], $lang );
 			if ( $blob ) {
-				$blobs[$module] = $blob;
+				$blobs[$name] = $blob;
 			}
 		}
 
@@ -66,8 +66,8 @@ class MessageBlobStore {
 	 * @param $lang string Language code
 	 * @return mixed Message blob or false if the module has no messages
 	 */
-	public static function insertMessageBlob( $module, $lang ) {
-		$blob = self::generateMessageBlob( $module, $lang );
+	public static function insertMessageBlob( $name, ResourceLoaderModule $module, $lang ) {
+		$blob = self::generateMessageBlob( $name, $module, $lang );
 
 		if ( !$blob ) {
 			return false;
@@ -76,7 +76,7 @@ class MessageBlobStore {
 		$dbw = wfGetDB( DB_MASTER );
 		$success = $dbw->insert( 'msg_resource', array(
 				'mr_lang' => $lang,
-				'mr_resource' => $module,
+				'mr_resource' => $name,
 				'mr_blob' => $blob,
 				'mr_timestamp' => $dbw->timestamp()
 			),
@@ -88,7 +88,7 @@ class MessageBlobStore {
 			if ( $dbw->affectedRows() == 0 ) {
 				// Blob was already present, fetch it
 				$blob = $dbw->selectField( 'msg_resource', 'mr_blob', array(
-						'mr_resource' => $module,
+						'mr_resource' => $name,
 						'mr_lang' => $lang,
 					),
 					__METHOD__
@@ -96,11 +96,10 @@ class MessageBlobStore {
 			} else {
 				// Update msg_resource_links
 				$rows = array();
-				$mod = ResourceLoader::getModule( $module );
 
-				foreach ( $mod->getMessages() as $key ) {
+				foreach ( $module->getMessages() as $key ) {
 					$rows[] = array(
-						'mrl_resource' => $module,
+						'mrl_resource' => $name,
 						'mrl_message' => $key
 					);
 				}
@@ -120,14 +119,14 @@ class MessageBlobStore {
 	 * @return mixed If $lang is set, the new message blob for that language is 
 	 *    returned if present. Otherwise, null is returned.
 	 */
-	public static function updateModule( $module, $lang = null ) {
+	public static function updateModule( $name, ResourceLoaderModule $module, $lang = null ) {
 		$retval = null;
 
 		// Find all existing blobs for this module
 		$dbw = wfGetDB( DB_MASTER );
 		$res = $dbw->select( 'msg_resource',
 			array( 'mr_lang', 'mr_blob' ),
-			array( 'mr_resource' => $module ),
+			array( 'mr_resource' => $name ),
 			__METHOD__
 		);
 
@@ -139,13 +138,13 @@ class MessageBlobStore {
 
 		foreach ( $res as $row ) {
 			$oldBlob = $row->mr_blob;
-			$newBlob = self::generateMessageBlob( $module, $row->mr_lang );
+			$newBlob = self::generateMessageBlob( $name, $module, $row->mr_lang );
 
 			if ( $row->mr_lang === $lang ) {
 				$retval = $newBlob;
 			}
 			$newRows[] = array(
-				'mr_resource' => $module,
+				'mr_resource' => $name,
 				'mr_lang' => $row->mr_lang,
 				'mr_blob' => $newBlob,
 				'mr_timestamp' => $now
@@ -166,7 +165,7 @@ class MessageBlobStore {
 		// Delete removed messages, insert added ones
 		if ( $removed ) {
 			$dbw->delete( 'msg_resource_links', array(
-					'mrl_resource' => $module,
+					'mrl_resource' => $name,
 					'mrl_message' => $removed
 				), __METHOD__
 			);
@@ -176,7 +175,7 @@ class MessageBlobStore {
 
 		foreach ( $added as $message ) {
 			$newLinksRows[] = array(
-				'mrl_resource' => $module,
+				'mrl_resource' => $name,
 				'mrl_message' => $message
 			);
 		}
@@ -343,11 +342,10 @@ class MessageBlobStore {
 	 * @param $lang string Language code
 	 * @return string JSON object
 	 */
-	private static function generateMessageBlob( $module, $lang ) {
-		$mod = ResourceLoader::getModule( $module );
+	private static function generateMessageBlob( $name, ResourceLoaderModule $module, $lang ) {
 		$messages = array();
 
-		foreach ( $mod->getMessages() as $key ) {
+		foreach ( $module->getMessages() as $key ) {
 			$messages[$key] = wfMsgExt( $key, array( 'language' => $lang ) );
 		}
 

extensions/ArticleEmblems/ArticleEmblems.hooks.php
@@ -84,8 +84,8 @@ class ArticleEmblemsHooks {
 	/*
 	 * ResourceLoaderRegisterModules hook
 	 */
-	public static function resourceLoaderRegisterModules() {
-		ResourceLoader::register(
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		$resourceLoader->register(
 			'ext.articleEmblems',
 			new ResourceLoaderFileModule( array(
 				'styles' => 'extensions/ArticleEmblems/modules/ext.articleEmblems.css',

extensions/ClickTracking/ClickTracking.hooks.php
@@ -71,8 +71,8 @@ class ClickTrackingHooks {
 	 *
 	 * Adds modules to ResourceLoader
 	 */
-	public static function resourceLoaderRegisterModules() {
-		ResourceLoader::register( array(
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		$resourceLoader->register( array(
 			'jquery.clickTracking' => new ResourceLoaderFileModule( array(
 				'scripts' => 'extensions/ClickTracking/modules/jquery.clickTracking.js',
 				'dependencies' => 'jquery.cookie',

extensions/PrefSwitch/PrefSwitch.hooks.php
@@ -83,9 +83,9 @@ class PrefSwitchHooks {
 	/*
 	 * ResourceLoaderRegisterModules hook
 	 */
-	public static function resourceLoaderRegisterModules() {
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
 		foreach ( self::$modules as $name => $resources ) {
-			ResourceLoader::register( $name, new ResourceLoaderFileModule( $resources ) );
+			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources ) );
 		}
 		return true;
 	}

extensions/ProofreadPage/ProofreadPage_body.php
@@ -71,8 +71,8 @@ class ProofreadPage {
 
 	}
 
-	public static function resourceLoaderRegisterModules() {
-		ResourceLoader::register(
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
+		$resourceLoader->register(
 			'ext.proofreadpage.page',
 			new ResourceLoaderFileModule(
 				array(
@@ -93,7 +93,7 @@ class ProofreadPage {
 			)
 		);
 
-		ResourceLoader::register(
+		$resourceLoader->register(
 			'ext.proofreadpage.article',
 			new ResourceLoaderFileModule(
 				array(
@@ -102,7 +102,7 @@ class ProofreadPage {
 			)
 		);
 
-		ResourceLoader::register(
+		$resourceLoader->register(
 			'ext.proofreadpage.index',
 			new ResourceLoaderFileModule( array( 'scripts' => 'extensions/ProofreadPage/proofread_index.js' ) )
 		);

extensions/Translate/Translate.php
@@ -411,8 +411,8 @@ $wgTranslateYamlLibrary = 'spyc';
 
 
 # Startup code
-function efTranslateResources() {
-	ResourceLoader::register( array( 'translate-css' =>
+function efTranslateResources( &$resourceLoader ) {
+	$resourceLoader->register( array( 'translate-css' =>
 		new ResourceLoaderFileModule( array( 'styles' => 'extensions/Translate/Translate.css' ) )
 	) );
 	return true;

extensions/Vector/Vector.hooks.php
@@ -212,9 +212,9 @@ class VectorHooks {
 	 * 
 	 * Adds modules to ResourceLoader
 	 */
-	public static function resourceLoaderRegisterModules() {
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
 		foreach ( self::$modules as $name => $resources ) {
-			ResourceLoader::register( $name, new ResourceLoaderFileModule( $resources ) );
+			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources ) );
 		}
 		return true;
 	}

extensions/WikiEditor/WikiEditor.hooks.php
@@ -687,9 +687,9 @@ class WikiEditorHooks {
 	 * 
 	 * Adds modules to ResourceLoader
 	 */
-	public static function resourceLoaderRegisterModules() {
+	public static function resourceLoaderRegisterModules( &$resourceLoader ) {
 		foreach ( self::$modules as $name => $resources ) {
-			ResourceLoader::register( $name, new ResourceLoaderFileModule( $resources ) );
+			$resourceLoader->register( $name, new ResourceLoaderFileModule( $resources ) );
 		}
 		return true;
 	}

commit 0a9da3dbbafdc176d726a4d71c6283d0bd643479

* Improved on r73971 by removing the $name parameter from MessageBlobStore::gerenateMessageBlob, since it was not being used. * Fixed bug introduced in r73971 caused by using a module name rather than a module object in MessageBlobStore::updateModule
git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@74525 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

phase3/includes/MessageBlobStore.php
@@ -66,7 +66,7 @@ class MessageBlobStore {
 	 * @return mixed Message blob or false if the module has no messages
 	 */
 	public static function insertMessageBlob( $name, ResourceLoaderModule $module, $lang ) {
-		$blob = self::generateMessageBlob( $name, $module, $lang );
+		$blob = self::generateMessageBlob( $module, $lang );
 
 		if ( !$blob ) {
 			return false;
@@ -137,7 +137,7 @@ class MessageBlobStore {
 
 		foreach ( $res as $row ) {
 			$oldBlob = $row->mr_blob;
-			$newBlob = self::generateMessageBlob( $name, $module, $row->mr_lang );
+			$newBlob = self::generateMessageBlob( $module, $row->mr_lang );
 
 			if ( $row->mr_lang === $lang ) {
 				$retval = $newBlob;
@@ -330,7 +330,7 @@ class MessageBlobStore {
 			// older than $wgCacheEpoch
 			if ( array_keys( FormatJson::decode( $row->mr_blob, true ) ) !== $module->getMessages() ||
 					wfTimestamp( TS_MW, $row->mr_timestamp ) <= $wgCacheEpoch ) {
-				$retval[$row->mr_resource] = self::updateModule( $row->mr_resource, $lang );
+				$retval[$row->mr_resource] = self::updateModule( $module, $lang );
 			} else {
 				$retval[$row->mr_resource] = $row->mr_blob;
 			}
@@ -345,7 +345,7 @@ class MessageBlobStore {
 	 * @param $lang string Language code
 	 * @return string JSON object
 	 */
-	private static function generateMessageBlob( $name, ResourceLoaderModule $module, $lang ) {
+	private static function generateMessageBlob( ResourceLoaderModule $module, $lang ) {
 		$messages = array();
 
 		foreach ( $module->getMessages() as $key ) {