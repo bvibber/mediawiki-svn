4aba2e519d034685a1608e5ad9e9bd119bc92d8a
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-08 00:10:31 +0000

    * stubs for mwEmbed resource loader support ( incomplete )
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@74477 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ extensions/MwEmbed/MwEmbed.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for MwEmbed extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Michael Dale
+ */
+$messages['en'] = array(
+	/* Editing Preference Categories */
+	'mwembed-desc' => 'MwEmbed extension provides support for mwEmbed modules and extensions',
+);
--- extensions/MwEmbed/MwEmbedModule/loader.js
+++ extensions/MwEmbed/MwEmbedModule/loader.js
@@ -161,7 +161,7 @@ mw.addResourcePaths( {
 	"$j.ui.dialog"			: "libraries/jquery/jquery.ui/ui/ui.dialog.js",
 	"$j.ui.resizable"		: "libraries/jquery/jquery.ui/ui/ui.resizable.js",
 	"$j.ui.tabs"			: "libraries/jquery/jquery.ui/ui/ui.tabs.js",
-	"$j.effects"		: "libraries/jquery/jquery.ui/ui/effects.core.js",
+	"$j.effects"			: "libraries/jquery/jquery.ui/ui/effects.core.js",
 	"$j.effects.highlight"	: "libraries/jquery/jquery.ui/ui/effects.highlight.js",
 	"$j.effects.slide"		: "libraries/jquery/jquery.ui/ui/effects.slide.js",
 	"$j.ui.accordion"		: "libraries/jquery/jquery.ui/ui/ui.accordion.js",
