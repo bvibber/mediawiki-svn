6e8e9f660e70e90d2d6ec347f94487a5df9d60b8
dale <dale@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-13 17:06:09 +0000

    bumped remote version tag to 160
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/branches/MwEmbedStandAlone@74722 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- remotes/mediaWiki.js
+++ remotes/mediaWiki.js
@@ -4,7 +4,7 @@
  */
 var urlparts = getRemoteEmbedPath();
 var mwEmbedHostPath = urlparts[0];
-var mwRemoteVersion = 'r159';
+var mwRemoteVersion = 'r160';
 var mwUseScriptLoader = true;
 
 // Log the mwRemote version makes it easy to debug cache issues
