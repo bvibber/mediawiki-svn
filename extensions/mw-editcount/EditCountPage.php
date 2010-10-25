7b27d4d9451855245ac3f43b5462da795022920e
dantman <dantman@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2010-10-25 17:51:35 +0000

    Cleanup some stuff in mw-editcount noted in comment c10391 of r68613
    git-svn-id: file:///home/git-svn-mirror/pushmi-svn-mirror/trunk@75369 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- extensions/mw-editcount/EditCountPage.php
+++ extensions/mw-editcount/EditCountPage.php
@@ -46,11 +46,11 @@ class EditCountPage extends SpecialPage {
 	 * @param string $par the user that was entered into the form
 	 * @access public
 	 */
-	function execute($par = null) 
+	function execute($par) 
 	{
 		global $wgRequest, $wgOut;
 		wfProfileIn(__METHOD__);
-		$this->target = ($par !== null) ? $par : $wgRequest->getVal("target");
+		$this->target = $wgRequest->getVal("target", isset($par) ? $par : null);
 		
 		if ($this->target === null || !strlen($this->target)) {
 			$this->showInputForm();
@@ -60,7 +60,7 @@ class EditCountPage extends SpecialPage {
 		
 		$this->showInputForm($this->target);
 		
-		$nt = Title::newFromURL($this->target);
+		$nt = Title::newFromText($this->target);
 		if (!$nt) {
 			$wgOut->addWikiMsg("editcount-notuser", $this->target);
 			wfProfileOut(__METHOD__);
@@ -92,12 +92,12 @@ class EditCountPage extends SpecialPage {
 		}
 		
 		$table = "";
-		$table .= wfElement("table", array("style" => "$this->border text-align: center; margin-left: 25%; margin-right: auto; margin-top: 7px; border-collapse: collapse"), null, null);
-		$table .= wfElement("tr", array("style" => $this->border), null);
-		$table .= wfElement("th", array("style" => $this->border), wfMsg("editcount-namespace"));
-		$table .= wfElement("th", array("style" => $this->border), wfMsg("editcount-edits"));
-		$table .= wfElement("th", array("style" => $this->border), wfMsg("editcount-percent"));
-		$table .= wfCloseElement("tr");
+		$table .= Html::openElement("table", array("style" => "{$this->border} text-align: center; margin-left: 25%; margin-right: auto; margin-top: 7px; border-collapse: collapse;"));
+		$table .= Html::openElement("tr", array("style" => $this->border));
+		$table .= Html::element("th", array("style" => $this->border), wfMsg("editcount-namespace"));
+		$table .= Html::element("th", array("style" => $this->border), wfMsg("editcount-edits"));
+		$table .= Html::element("th", array("style" => $this->border), wfMsg("editcount-percent"));
+		$table .= Xml::closeElement("tr");
 		
 		$totalEC = $ec->getTotal();
 		$nsCount = $ec->getNamspaces();
@@ -108,16 +108,16 @@ class EditCountPage extends SpecialPage {
 			$table .= $this->doRow(array(
 				$nsNames[$nsInt],
 				$nsEC,
-				round(($nsEC/$totalEC*100), 1) . wfMsg("editcount-percentsym"))
-				);
+				round(($nsEC/$totalEC*100), 1) . wfMsg("editcount-percentsym")
+			));
 		}
 
-		$table .= wfElement("tr", null, null);
-		$table .= wfElement("td", array("style" => "font-weight: bold; {$this->border}"), wfMsg("editcount-total"));
-		$table .= wfElement("td", array("colspan" => "2", "style" => $this->border), $totalEC);
-		$table .= wfCloseElement("tr");
+		$table .= Html::openElement("tr", null, null);
+		$table .= Html::element("td", array("style" => "font-weight: bold; {$this->border}"), wfMsg("editcount-total"));
+		$table .= Html::element("td", array("colspan" => "2", "style" => $this->border), $totalEC);
+		$table .= Xml::closeElement("tr");
 		
-		$table .= wfCloseElement("table");
+		$table .= Xml::closeElement("table");
 		$wgOut->addHtml($table);
 		
 		wfProfileOut(__METHOD__);
@@ -133,17 +133,17 @@ class EditCountPage extends SpecialPage {
 	function doRow($cells)
 	{
 		wfProfileIn(__METHOD__);
-		$ret = wfElement("tr", null, null);
+		$ret = Html::openElement("tr", null, null);
 		if (count($cells) == 2) {
-			$ret .= wfElement("td", array("style" => $this->border), $cells[0]);
-			$ret .= wfElement("td", array("style" => $this->border, "colspan" => "2"), $cells[1]);
+			$ret .= Html::element("td", array("style" => $this->border), $cells[0]);
+			$ret .= Html::element("td", array("style" => $this->border, "colspan" => "2"), $cells[1]);
 		}
 		else {
 			for ($i = 0; $i < 3; ++$i) {
-				$ret .= wfElement("td", array("style" => $this->border), $cells[$i]);
+				$ret .= Html::element("td", array("style" => $this->border), $cells[$i]);
 			}
 		}
-		$ret .= wfCloseElement("tr");
+		$ret .= Xml::closeElement("tr");
 		
 		wfProfileOut(__METHOD__);
 		return $ret;
@@ -156,20 +156,20 @@ class EditCountPage extends SpecialPage {
 	 * @access private
 	 */
 	function showInputForm($user = "") {
-		global $wgOut, $wgScriptPath;
+		global $wgOut, $wgScript;
 		
 		wfProfileIn(__METHOD__);
 		
 		$ct = $this->getTitle();
 		$form = "";
 		// FIXME: use Xml::inputLabel etc
-		$form .= wfElement("p", null, wfMsg("editcount-des"));
-		$form .= wfElement("form", array("name" => "editcountform", "method" => "get", "action" => $wgScriptPath . "/index.php"), null); 
-		$form .= wfElement("input", array("type" => "hidden", "name" => "title", "value" => "Special:EditCount"), "") . " ";
-		$form .= wfElement("label", array("for" => "target"), wfMsg("editcount-username")). " ";
-		$form .= wfElement("input", array("type" => "textbox", "name" => "target", "size" => "24", "value" => $user), "") . " ";
-		$form .= wfElement("input", array("type" => "submit", "name" => "doeditcount", "value" => wfMsg("editcount-show")));
-		$form .= wfCloseElement("form");
+		$form .= Html::element("p", null, wfMsg("editcount-des"));
+		$form .= Html::openElement("form", array("name" => "editcountform", "method" => "get", "action" => $wgScript));
+		$form .= Html::hidden("title", "Special:EditCount") . " ";
+		$form .= Html::element("label", array("for" => "target"), wfMsg("editcount-username")). " ";
+		$form .= Html::input("target", $user, "textbox", array("size" => "24")) . " ";
+		$form .= Html::input("doeditcount", wfMsg("editcount-show"), "submit");
+		$form .= Xml::closeElement("form");
 		
 		$this->setHeaders();
 		$wgOut->addHtml($form);
@@ -236,15 +236,16 @@ class EditCount {
 	function getByNamespace($ns)
 	{
 		wfProfileIn(__METHOD__);
-		global $wgDBprefix;
-		//FIXME: don't construct SQL by hand, use selectField
-		$cond = ($this->user->isAnon()) ? "r.rev_user_text = '" . $this->user->getName() . "'"
-			: "r.rev_user = " . $this->id;
-		$result = $this->db->query("SELECT COUNT(*) AS count
-			FROM {$wgDBprefix}page p JOIN {$wgDBprefix}revision r ON p.page_id = r.rev_page
-			WHERE $cond AND p.page_namespace = $ns
-			GROUP BY p.page_namespace");
-		$row = $this->db->fetchRow($result);
+		$conds = array("page_namespace" => $ns);
+		if ( $this->user->isAnon() )
+			$conds["rev_user_text"] = $this->user->getName();
+		else
+			$conds["rev_user"] = $this->id;
+		
+		$row = $this->db->selectRow("page", "COUNT(*) AS count",
+			$conds, __METHOD__,
+			array("GROUP BY" => "page_namespace"),
+			array("revision" => array("JOIN", "page_id = rev_page"));
 		
 		wfProfileOut(__METHOD__);
 		return ($row["count"]) ? $row["count"] : 0;
@@ -259,17 +260,23 @@ class EditCount {
 	function getNamspaces()
 	{
 		wfProfileIn(__METHOD__);
-		global $wgContLang, $wgDBprefix;
-		$cond = ($this->user->isAnon()) ? "rev_user_text = '" . $this->user->getName() . "'" 
-			: "rev_user = " . $this->id;
-		$result = $this->db->query("SELECT page_namespace as ns, COUNT(*) as count
-			FROM {$wgDBprefix}revision JOIN {$wgDBprefix}page p ON rev_page = p.page_id
-			WHERE $cond GROUP BY ns", __METHOD__);
+		global $wgContLang;
+		$conds = array();
+		if ( $this->user->isAnon() )
+			$conds["rev_user_text"] = $this->user->getName();
+		else
+			$conds["rev_user"] = $this->id;
+		
+		$res = $this->db->select(array("revision", "page"), array("page_namespace", "COUNT(*) as count"),
+			$conds, __METHOD__,
+			array("GROUP BY" => "page_namespace"),
+			array("page" => array("JOIN", "rev_page = page_id")));
 		$nsResults = array();
-
-		// Use foreach
-		while (($row = $this->db->fetchRow($result)) !== false) {
-			$nsResults[$row["ns"]] = $row["count"];
+		
+		foreach ($res as $row) {
+		//while (($row = $this->db->fetchRow($result)) !== false) {
+			//$nsResults[$row["ns"]] = $row["count"];
+			$nsResults[$row->page_namespace] = $row->count;
 		}
 		
 		$nsNumbers = array_keys($wgContLang->getNamespaces());
@@ -293,11 +300,10 @@ class EditCount {
 	function getTotal()
 	{
 		wfProfileIn(__METHOD__);
-		global $wgDBprefix;
 		if ($this->id == 0) {
 			wfProfileOut(__METHOD__);
 			// This actually works?
-			return $this->db->selectField("{$wgDBprefix}revision", "COUNT(*)", array("rev_user_text" => $this->user->getName()), __METHOD__);
+			return $this->db->selectField("revision", "COUNT(*)", array("rev_user_text" => $this->user->getName()), __METHOD__);
 		}
 		
 		wfProfileOut(__METHOD__);
